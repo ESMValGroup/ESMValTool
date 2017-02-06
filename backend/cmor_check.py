ed2dc6da123f6d3c9b260565a51b4faa35c66d5d
Paul Earnshaw <paul@localhost.localdomain>
2017-02-06 11:29:04 +0000

    Added check for names of coordinates.

--- backend/cmor_check.py
+++ backend/cmor_check.py
@@ -1,5 +1,10 @@
 import iris
 
+FIELD_TYPES = {
+'T3M': ('time', 'air_pressure', 'latitude', 'longitude')
+}
+
+
 
 class CMORCheck(object):
 
@@ -10,23 +15,31 @@ class CMORCheck(object):
     def check(self):
         try:
             self._check_rank()
-        except CMORCheckError:
+        except CMORCheckError,e:
+            print e
             return False
         return True
 
     def _check_rank(self):
         # Field_type is like T3m or T3Om
-        rank = int(self.field_type[1])
-        dim_coords = self.cube.coords(dim_coord=True)
+        dim_names = FIELD_TYPES[self.field_type]
+        rank = len(dim_names)
+        dim_coords = self.cube.coords(dim_coords=True)
+        # Check number of dim_coords matches rank required
         if len(dim_coords) != rank:
-            raise CMORCheckError('Coordinates does not match')
-
+            raise CMORCheckError('Coordinate rank does not match')
+        # Check names of dimensions
+        for coord_name in dim_names:
+            if not self.cube.coords(coord_name):
+                raise CMORCheckError('Coordinate {} does not exist'.format(coord_name))
 
 class CMORCheckError(Exception):
     pass
 
 if __name__ == '__main__':
-    cube = iris.load('/Users/nube/esmval_data/ETHZ_CMIP5/historical/Amon/ta/CMCC-CESM/r1i1p1/'
-                     'ta_Amon_CMCC-CESM_historical_r1i1p1_200001-200212.nc')
+    cube = iris.load_cube('/home/paul/ESMValTool/data/BACKEND-DATA/ETHZ_CMIP5/'
+                          'historical/Amon/ta/CMCC-CESM/r1i1p1/'
+                          'ta_Amon_CMCC-CESM_historical_r1i1p1_200001-200212.nc')
     checker = CMORCheck(cube)
     checker.field_type = 'T3M'
+    print(checker.check())
