452dea95addb8a2974022ebb0ca0f4bcf61065a8
Paul Earnshaw <pdearnshaw@gmail.com>
2017-02-07 14:02:57 +0000

    Prettifying and expanding set of real data tests.

--- backend/cmor_check.py
+++ backend/cmor_check.py
@@ -15,42 +15,46 @@ def merge_protect_callback(cube, field, file):
 
 class CMORCheck(object):
 
-    def __init__(self, cube, fail_on_error=False):
+    def __init__(self, cube, table, fail_on_error=False):
         self.cube = cube
         self._failerr = fail_on_error
         self._errors = list()
-        self._cmor_tables_folder = os.path.join(os.path.dirname(os.path.realpath(__file__)),
-                                                'cmip6-cmor-tables',
-                                                'Tables')
+        cwd = os.path.dirname(os.path.realpath(__file__))
+        self._cmor_tables_folder = os.path.join(cwd, 'cmip6-cmor-tables', 'Tables')
+        self._cmor_tables_file = 'CMIP6_{}.json'.format(table)
         self._load_variable_information()
         self._load_coord_information()
 
     def _load_coord_information(self):
-        json_data = open(os.path.join(self._cmor_tables_folder,
-                                      'CMIP6_coordinate.json')).read()
+        table_file = os.path.join(self._cmor_tables_folder,
+                                  'CMIP6_coordinate.json')
+        json_data = open(table_file).read()
         self.coord_json_data = json.loads(json_data)
         self.coord_json_data = self.coord_json_data['axis_entry']
         # Don't look here!
         self.coord_json_data['time']['units'] = u'days since 1850-1-1 00:00:00'
 
     def _load_variable_information(self):
-        json_data = open(os.path.join(self._cmor_tables_folder,
-                                      'CMIP6_Amon.json')).read()
+        table_file = os.path.join(self._cmor_tables_folder,
+                                  self._cmor_tables_file)
+        json_data = open(table_file).read()
         self.var_json_data = json.loads(json_data)
-        self.var_json_data = self.var_json_data['variable_entry'][self.cube.var_name]
+        self.var_json_data = self.var_json_data['variable_entry']
+        self.var_json_data = self.var_json_data[self.cube.var_name]
 
     def check(self):
         self._check_rank()
         self._check_dim_names()
         self._check_coords()
         self._check_time_coord()
-        #self._check_var_metadata()
-        #self._check_fill_value()
-        #self._check_data_range()
+        # self._check_var_metadata()
+        # self._check_fill_value()
+        # self._check_data_range()
 
         if len(self._errors) > 0:
-            raise CMORCheckError('There were errors in variable {0}:\n  {1}'.format(self.cube.standard_name,
-                                                                                    '\n  '.join(self._errors)))
+            msg = 'There were errors in variable {0}:\n {1}'
+            msg = msg.format(self.cube.standard_name, '\n '.join(self._errors))
+            raise CMORCheckError(msg)
 
     def has_errors(self):
         return len(self._errors) > 0
@@ -87,10 +91,8 @@ class CMORCheck(object):
 
     def _check_time_coord(self):
         coord = self.cube.coord('time', dim_coords=True)
-        if not coord.units.is_time():
-            self.report_error('Coord time does not have time units')
-        if not coord.units.calendar:
-            self.report_error('Coord time units does not contain a calendar')
+        if not coord.units.is_time_reference():
+            self.report_error('Coord time does not have time reference units')
 
     def _check_coords(self):
         if self.var_json_data['dimensions']:
@@ -105,7 +107,8 @@ class CMORCheck(object):
                 out_var_name = cmor['out_name']
                 # Get coordinate out_var_name as it exists!
                 try:
-                    coord = self.cube.coord(var_name=out_var_name, dim_coords=True)
+                    coord = self.cube.coord(var_name=out_var_name,
+                                            dim_coords=True)
                 except iris.exceptions.CoordinateNotFoundError:
                     continue
 
@@ -160,30 +163,42 @@ class CMORCheckError(Exception):
 
 
 def main():
-    #data_folder = '/Users/nube/esmval_data'
+    # data_folder = '/Users/nube/esmval_data'
     data_folder = '/home/paul/ESMValTool/data'
-    #example_data = 'ETHZ_CMIP5/historical/Amon/ta/CMCC-CESM/r1i1p1'
-    example_data = 'CMIP6/1pctCO2/Amon/ua/MPI-ESM-LR/r1i1p1f1'
+    example_datas = [
+        ('ETHZ_CMIP5/historical/Amon/ta/CMCC-CESM/r1i1p1', 'Amon'),
+        ('CMIP6/1pctCO2/Amon/ua/MPI-ESM-LR/r1i1p1f1', 'Amon'),
+        ('CMIP6/1pctCO2/Amon/tas/MPI-ESM-LR/r1i1p1f1', 'Amon'),
+        ('CMIP6/1pctCO2/day/tas/MPI-ESM-LR/r1i1p1f1', 'day'),
+        ('CMIP6/1pctCO2/day/pr/MPI-ESM-LR/r1i1p1f1', 'day'),
+        # ('CMIP6/1pctCO2/cfDay/hur/MPI-ESM-LR/r1i1p1f1', 'CFday'),
+        ('CMIP6/1pctCO2/LImon/snw/MPI-ESM-LR/r1i1p1f1', 'LImon'),
+        ('CMIP6/1pctCO2/Lmon/cropFrac/MPI-ESM-LR/r1i1p1f1', 'Lmon'),
+        # ('CMIP6/1pctCO2/Oyr/co3/MPI-ESM-LR/r1i1p1f1', 'Oyr'),
+        ]
 
-    try:
-        # Load cubes
-        files = os.path.join(data_folder, example_data, '*.nc')
-        cubes = iris.load(files, callback=merge_protect_callback)
-        # Concatenate data to single cube to deal with multiple netcdf files
-        cube = cubes.concatenate_cube()
-        # Create checker for loaded cube
-        checker = CMORCheck(cube) #, fail_on_error=True)
-        # Run checks
-        checker.check()
+    for (example_data, table) in example_datas:
+        print(example_data)
 
-    except iris.exceptions.ConstraintMismatchError, ex:
-        print ex
+        try:
+            # Load cubes
+            files = os.path.join(data_folder, example_data, '*.nc')
+            cubes = iris.load(files, callback=merge_protect_callback)
+            # Concatenate data to single cube
+            cube = cubes.concatenate_cube()
+            # Create checker for loaded cube
+            checker = CMORCheck(cube, table)  # , fail_on_error=True)
+            # Run checks
+            checker.check()
 
-    except iris.exceptions.ConcatenateError, ex:
-        print ex
+        except iris.exceptions.ConstraintMismatchError as ex:
+            print(ex)
 
-    except CMORCheckError, ex:
-        print(ex)
+        except iris.exceptions.ConcatenateError as ex:
+            print(ex)
+
+        except CMORCheckError as ex:
+            print(ex)
 
 if __name__ == '__main__':
     main()
