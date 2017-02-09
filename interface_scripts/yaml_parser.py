893b222ffe7ef8993943ae40d7d408b9393ea308
Mattia Righi <mattia.righi@dlr.de>
2017-02-09 14:18:27 +0100

    Extend the template for the yaml namelist

--- interface_scripts/yaml_parser.py
+++ interface_scripts/yaml_parser.py
@@ -7,7 +7,17 @@ test_nml = "../nml/namelist_template.yaml"
 with open(test_nml, 'r') as stream:
     data_loaded = yaml.load(stream)
 
+for ii in data_loaded["MODELS"]:
+    print ii
 
-print(data_loaded["GLOBAL"])
+print "---------------------------------------"
 
-print(data_loaded["MODELS"])
+print data_loaded["PREPROCESS"]
+
+print "---------------------------------------"
+
+for diag in data_loaded["DIAGNOSTICS"]:
+    for key, value in diag.items():
+        for ii in value.items():
+            print ii
+
