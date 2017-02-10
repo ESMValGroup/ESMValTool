407afc9880d9a2381d9666c9629d2fe8c46e83a2
lbdreyer <laura.dreyer@metoffice.gov.uk>
2017-02-10 11:09:32 +0000

    Add calculate_lwp

--- backend/derived_variables/calculate_variables.py
+++ backend/derived_variables/calculate_variables.py
@@ -1,7 +1,13 @@
+"""
+Miscellaneous functions for deriving variables.
+
+"""
+
+
 import cf_units
+import numba
 import numpy as np
 import iris
-import numba
 from scipy import constants
 
 
@@ -15,6 +21,43 @@ mw_O3 = 48
 mw_O3_unit = cf_units.Unit('g mol^-1')
 Dobson_unit = cf_units.Unit('2.69e20 m^-2')
 
+
+def calc_lwp(cubes):
+    """
+    Liquid water path is calculated by subtracting clivi (ice water) from clwvi
+    (condensed water path).
+    Note: Some models output the variable "clwvi" which only contains lwp. In
+    these cases, the input clwvi cube is just returned.
+    
+    Args:
+        * cubes: cubelist containing clwvi_cube and clivi_cube
+    Returns:
+        Cube containing liquid water path.
+    """
+    # TODO: find out correct names for clwvi/clivi
+    clwvi_cube = cubes.extract('clwvi')
+    clivi_cube = cubes.extract('clivi')
+
+    model = clwvi_cube.attributes['model_id']
+    project = clwvi_cube.attributes['project_id']
+    # Should we check that the model/project_id are the same on both cubes?
+    
+    BAD_MODELS = ['CESM1-CAM5-1-FV2', 'CESM1-CAM5', 'CMCC-CESM', 'CMCC-CM',
+                  'CMCC-CMS', 'IPSL-CM5A-MR', 'IPSL-CM5A-LR', 'IPSL-CM5B-LR',
+                  'CCSM4', 'IPSL-CM5A-MR', 'MIROC-ESM', 'MIROC-ESM-CHEM',
+                  'MIROC-ESM', 'CSIRO-Mk3-6-0', 'MPI-ESM-MR', 'MPI-ESM-LR',
+                  'MPI-ESM-P']
+    if (project in ["CMIP5", "CMIP5_ETHZ"] and model in BAD_MODELS) or \
+        (project == 'OBS' and model == 'UWisc'):
+            print("INFO: assuming that variable clwvi from {} model {} "
+                  "contains only liquid water".format(project, model)
+            lwp_cube = clwvi_cube
+    else:
+        lwp_cube = clwvi_cube - clivi_cube
+
+    # TODO: Rename cube lwp_cube.name('liquid_water_path') here?
+    # TODO: Fix units? lwp_cube.units = cf_units.Unit('kg') here?
+    return lwp_cube
 
 def total_column_ozone(tro3_cube, ps_cube):
     """
