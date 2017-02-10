9b9f9687fce5abf9034da5b2c8e5fc135f9af1a1
Mattia Righi <mattia@linux-qxq1.suse>
2017-02-10 12:15:10 +0100

    Add yaml test case for perfmetrics_CMIP5 namelist

--- interface_scripts/yaml_parser.py
+++ interface_scripts/yaml_parser.py
@@ -2,22 +2,9 @@ import yaml
 
 """Quick parser for playing with the yaml syntax"""
 
-test_nml = "../nml/namelist_template.yaml"
+test_nml = "../nml/namelist_perfmetrics_CMIP5_testset.yaml"
 
 with open(test_nml, 'r') as stream:
     data_loaded = yaml.load(stream)
 
-for ii in data_loaded["MODELS"]:
-    print ii
-
-print "---------------------------------------"
-
-print data_loaded["PREPROCESS"]
-
-print "---------------------------------------"
-
-for diag in data_loaded["DIAGNOSTICS"]:
-    for key, value in diag.items():
-        for ii in value.items():
-            print ii
-
+print(data_loaded)
