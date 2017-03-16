c2a9fa81a57ca1ea99a65fa52f09b25c0f840a5d
Nikolay Koldunov <koldunovn@gmail.com>
2017-03-16 10:24:32 +0100

    more syntax fixes

--- diag_scripts/sst_ESACCI.py
+++ diag_scripts/sst_ESACCI.py
@@ -77,7 +77,7 @@ def main(project_info):
             # only models are read
             for inc in range(len(project_info['MODELS'])):
                 model=project_info['MODELS'][inc]
-                currProject = getattr(vars()['projects'],\
+                currProject = getattr(vars()['projects'],
                                       model.split_entries()[0])()
                 model_name = currProject.get_model_name(model)
 
@@ -86,7 +86,7 @@ def main(project_info):
                 if not model_name == refference_model:
                 
                     model_filename = model_filelist[model_name]
-                    reference_filename=model_filelist[project_info['RUNTIME']['currDiag'].variables[v].ref_model]
+                    reference_filename=model_filelist[refference_model]
                 
                     # copy old data to provide data that is needed again                                                                                                                                                           # copy old data to provide data that is needed again
                     D_old=copy(Diag)
