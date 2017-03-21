57777d4c68de2d71c7d28471502b6205d2e7db95
Ben <b.mueller@iggf.geo.uni-muenchen.de>
2017-03-21 17:53:39 +0100

    Committing the test suite for testing.

---
+++ tests/test_diagnostics/test_namelist_Reporting_TEST/test_namelist_Reporting_TEST.py
@@ -0,0 +1,72 @@
+# -*- coding: utf-8 -*-
+
+# This file is part of ESMValTool
+
+"""
+Test script 
+"""
+
+import os
+import unittest
+import sys
+sys.path.append('../..')
+
+from esmvaltool_testlib import ESMValToolTest
+
+
+class DiagnosticTest(ESMValToolTest):
+    """
+    Define class for Test metric here
+    """
+    def __init__(self, **kwargs):
+        xml_single = 'namelist_Reporting_TEST.xml'
+        xmlfile = os.path.dirname(os.path.realpath(__file__)) + os.sep + xml_single  # the realpath ensures that the file is also foudn wehn nosetests runs from an upper directory
+        super(DiagnosticTest, self).__init__(nml=xmlfile, **kwargs)
+
+    def get_field_definitions(self):
+        """
+        routine to specify the structure of the sample data to be generated
+        """
+        r = {}
+        rpath = self._default_input_dir
+        r.update({'ts' : {'method' : 'uniform', 'filename' : rpath + os.sep + '@{VAR_FILE}', 'ndim' : 2}})
+        return r
+
+
+class TestDiagnostic(unittest.TestCase):
+
+    def setUp(self):
+        pass
+
+    def tearDown(self):
+        pass
+
+    def test_general_output(self):
+        # specify list with expected output files as tuples with directory and filename
+        # reffiles=[('plot','ro_coefficient-rel-pr_biases.png'),('plot','ro-et_coefficient_biases.png')]
+        # example if output files based on model names need to be processed
+        # models = ['MPIESM', 'MPIESM-LR'] # models as specified in testing namelist
+        # for m in models:
+            # reffiles.append(('plot',m + '_bias_ET_catchments.txt'))
+            # reffiles.append(('plot',m + '_bias_precip_catchments.txt'))
+            # reffiles.append(('plot',m + '_bias_runoff_catchments.txt'))
+            # reffiles.append(('plot',m + '_ET_catchments.txt'))
+            # reffiles.append(('plot',m + '_precip_catchments.txt'))
+            # reffiles.append(('plot',m + '_runoff_catchments.txt'))
+            # reffiles.append(('plot',m + '_sep-bias-plot-ET.png'))
+            # reffiles.append(('plot',m + '_sep-bias-plot-precip.png'))
+            # reffiles.append(('plot',m + '_sep-bias-plot-runoff.png'))
+	# example if output file list is produced with 'ls [-a] > filenames.txt'
+	with open('filenames.txt', 'rU') as f:
+  	    for line in f: 
+		reffiles.append(('plot',f))
+
+        T = DiagnosticTest(files=reffiles)  
+        T.run_nml()
+        T.run_tests(execute=False, graphics=None, checksum_files=None, files='all', check_size_gt_zero=True)
+        self.assertTrue(T.sucess)
+
+if __name__ == "__main__":
+    unittest.main()
+
+
