2ba2ec65cab7aa966a90a3b271a6a6de2e266da8
Ben <b.mueller@iggf.geo.uni-muenchen.de>
2017-05-12 10:17:00 +0200

    added cleanup

---
+++ reporting/utils/report_cleanup.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python2
+# -*- coding: utf-8 -*-
+"""
+report_cleanup
+
+* cleans directories that were produced by a former reporting job
+* is run before a new report is started
+
+Created on Fri May 12 10:13:39 2017
+
+@author: bmueller
+"""
+
