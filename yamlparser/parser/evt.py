4239bebe1fcb2a54196e475b521fc117ca5d6bd1
Björn Brötz <Bjoern.Broetz@dlr.de>
2017-05-17 16:12:01 +0200

    yaml parser structure added

---
+++ yamlparser/parser/evt.py
@@ -0,0 +1,35 @@
+class Namelist(object):
+    """
+    Class to hold the information from the namelist
+    """
+    def __init__(self):
+        self.__name__       = "Namelist" 
+        self.write_plots    = None
+        self.verbosity      = None
+        self.exit_on_warning = None
+        self.ouput_file_type = None
+        self.models         = None # sould be []
+        self.preprocess     = None # sould be {}
+        self.diagnostics    = None # sould be {}
+
+class Config(object):
+    """
+    Class to hold the information from the configfile 
+    """
+    def __init__(self):
+        self.__name__ = "Config" 
+
+class Diagnostic(object):
+    """
+    """
+    def __init__(self):
+        self.__name__ = "Diagnostic" 
+
+class Variable(object):
+    """
+    """
+    def __init__(self):
+        self.__name__ = "Variable" 
+
+
+
