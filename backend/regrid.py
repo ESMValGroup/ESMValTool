vinterp masking + regrid unstructured spatial fix.

backend/regrid.py
@@ -15,6 +15,7 @@ import re
 
 import iris
 from iris.analysis import AreaWeighted, Linear, Nearest, UnstructuredNearest
+from numpy import ma
 import numpy as np
 import stratify
 
@@ -80,12 +81,12 @@ def _stock_cube(spec):
     dx = float(cell_group['dx'])
     dy = float(cell_group['dy'])
 
-    if (_LON_RANGE % dx) != 0:
+    if (np.trunc(_LON_RANGE / dx) * dx) != _LON_RANGE:
         emsg = ('Invalid longitude delta in MxN cell specification '
                 'for stock cube, got {!r}.')
         raise ValueError(emsg.format(dx))
 
-    if (_LAT_RANGE % dy) != 0:
+    if (np.trunc(_LAT_RANGE / dy) * dy) != _LAT_RANGE:
         emsg = ('Invalid latitude delta in MxN cell specification '
                 'for stock cube, got {!r}.')
         raise ValueError(emsg.format(dy))
@@ -174,6 +175,16 @@ def regrid(src_cube, tgt_grid, scheme):
     elif not isinstance(tgt_grid, iris.cube.Cube):
         emsg = 'Expecting a cube or cell-specification, got {}.'
         raise ValueError(emsg.format(type(tgt_grid)))
+
+    # Unstructured regridding requires x2 2d spatial coordinates,
+    # so ensure to purge any 1d native spatial dimension coordinates
+    # for the regridder.
+    if scheme == 'unstructured_nearest':
+        for axis in ['x', 'y']:
+            coords = src_cube.coords(axis=axis, dim_coords=True)
+            if coords:
+                [coord] = coords
+                src_cube.remove_coord(coord)
 
     # Perform the horizontal regridding.
     result = src_cube.regrid(tgt_grid, horizontal_schemes[scheme])
@@ -358,6 +369,14 @@ def vinterp(src_cube, levels, scheme):
             if np.any(mask):
                 # Replace the NaN values with the fill-value.
                 new_data[mask] = _MDI
+
+            # Ensure that any spatial mask is re-applied.
+            if ma.isMaskedArray(src_cube.data):
+                slicer = tuple([0] * (z_axis + 1))
+                # Assume that the spatial mask is invariant.
+                mask = src_cube.data.mask[slicer]
+                mask = np.broadcast_to(mask, new_data.shape)
+                new_data = ma.array(new_data, mask=mask)
 
             # Construct the resulting cube with the interpolated data.
             result = _create_cube(src_cube, new_data, levels)

backend/tests/unit/regrid/test_vinterp.py
@@ -8,6 +8,7 @@ from six.moves import (filter, input, map, range, zip)  # noqa
 import six  # noqa
 
 import mock
+from numpy import ma
 import numpy as np
 import unittest
 
@@ -18,10 +19,11 @@ from backend.regrid import _MDI, vertical_schemes, vinterp
 
 class Test(tests.Test):
     def setUp(self):
-        shape = (3, 2, 1)
-        self.z = shape[0]
+        self.shape = (3, 2, 1)
+        self.z = self.shape[0]
         self.dtype = np.dtype('int8')
-        data = np.arange(np.prod(shape), dtype=self.dtype).reshape(shape)
+        data = np.arange(np.prod(self.shape),
+                         dtype=self.dtype).reshape(self.shape)
         self.cube = _make_cube(data, dtype=self.dtype)
         self.created_cube = mock.sentinel.created_cube
         self.mock_create_cube = self.patch('backend.regrid._create_cube',
@@ -139,6 +141,42 @@ class Test(tests.Test):
         self.assertArrayEqual(args[0], self.cube)
         new_data[np.isnan(new_data)] = _MDI
         self.assertArrayEqual(args[1], new_data)
+        self.assertArrayEqual(args[2], levels)
+        # Check the _create_cube kwargs ...
+        self.assertEqual(kwargs, dict())
+
+    def test_interpolation__masked(self):
+        new_data = np.empty(self.shape)
+        levels = np.array([0.5, 1.5])
+        scheme = 'linear'
+        mask = [[[1], [0]], [[1], [0]], [[1], [0]]]
+        masked = ma.empty(self.shape)
+        masked.mask = mask
+        cube = _make_cube(masked, dtype=self.dtype)
+        with mock.patch('stratify.interpolate',
+                        return_value=new_data) as mocker:
+            result = vinterp(cube, levels, scheme)
+            self.assertEqual(result, self.created_cube)
+            args, kwargs = mocker.call_args
+            # Check the stratify.interpolate args ...
+            self.assertEqual(len(args), 3)
+            self.assertArrayEqual(args[0], levels)
+            pts = cube.coord(axis='z', dim_coords=True).points
+            src_levels_broadcast = np.broadcast_to(pts.reshape(self.z, 1, 1),
+                                                   cube.shape)
+            self.assertArrayEqual(args[1], src_levels_broadcast)
+            self.assertArrayEqual(args[2], cube.data)
+            # Check the stratify.interpolate kwargs ...
+            self.assertEqual(kwargs, dict(axis=0,
+                                          interpolation=scheme,
+                                          extrapolation='nan'))
+        args, kwargs = self.mock_create_cube.call_args
+        # Check the _create_cube args ...
+        self.assertEqual(len(args), 3)
+        self.assertArrayEqual(args[0], cube)
+        self.assertArrayEqual(args[1], new_data)
+        self.assertTrue(ma.isMaskedArray(args[1]))
+        self.assertArrayEqual(args[1].mask, mask)
         self.assertArrayEqual(args[2], levels)
         # Check the _create_cube kwargs ...
         self.assertEqual(kwargs, dict())