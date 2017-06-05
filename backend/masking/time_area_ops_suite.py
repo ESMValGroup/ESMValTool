b030762402bbe65628f73868229e0cad64b5250e
Valeriu Predoi <valeriu.predoi@gmail.com>
2017-06-05 12:41:07 +0100

    added more utility functions

--- backend/masking/time_area_ops_suite.py
+++ backend/masking/time_area_ops_suite.py
@@ -11,7 +11,8 @@ def time_slice(mycube,yr1,mo1,d1,yr2,mo2,d2):
     Function that returns a subset of the original cube (slice)
     given two dates of interest date1 and date2
     date1 and date2 should be given in a yr,mo,d (int)format e.g.
-    time_slice(cube,2006,2,2,2010,1,1) or time_slice(cube,'2006','2','2','2010','1','1')
+    time_slice(cube,2006,2,2,2010,1,1) or time_slice(cube,'2006','2','2','2010','1','1');
+    Returns a cube
     """
     myDate1 = datetime.datetime(int(yr1),int(mo1),int(d1))
     myDate2 = datetime.datetime(int(yr2),int(mo2),int(d2))
@@ -21,23 +22,82 @@ def time_slice(mycube,yr1,mo1,d1,yr2,mo2,d2):
     cubeslice = mycube.extract(myConstraint)
     return cubeslice
 
+# slice cube over a restricted area (box)
+def area_slice(mycube, long1, long2, lat1, lat2):
+    """
+    Function that subsets a cube on a box (long1,long2,lat1,lat2)
+    This function is a restriction of masked_cube_lonlat();
+    Returns a cube
+    """
+    sublon = iris.Constraint(longitude=lambda cell: float(long1) <= cell <= float(long2))
+    sublat = iris.Constraint(latitude=lambda cell: float(lat1) <= cell <= float(lat2))
+    region_subset = mycube.extract(sublon & sublat)
+    return region_subset
+
+# get the time average
+def time_average(mycube):
+    """
+    Function to get the time average over MEAN;
+    Returns a cube
+    """
+    var_mean = mycube.collapsed('time', iris.analysis.MEAN)
+    return var_mean
+
+# get the probability a value is greater than a threshold
+def proportion_greater(mycube, coord1, threshold):
+    """
+    Function that returns the probability
+    that a cetain variable coord1 (string) is greater than
+    a threshold threshold (float or string), across a cube mycube;
+    Returns a cube
+    """
+    thr = float(threshold)
+    result = mycube.collapsed(coord1, iris.analysis.PROPORTION,
+                              function=lambda values: values > thr)
+    return result
+
+# get zonal means
+def zonal_means(mycube, coord1, mean_type):
+    """
+    Function that returns zonal means along a coordinate coord1;
+    the type of mean is controlled by mean_type variable (string):
+        'mean' -> MEAN
+        'stdev' -> STD_DEV
+        'variance' -> VARIANCE
+
+    Returns a cube
+    """
+    if mean_type=='mean':
+        result = mycube.collapsed(coord1, iris.analysis.MEAN)
+    elif mean_type=='stdev':
+        result = mycube.collapsed(coord1, iris.analysis.STD_DEV)
+    elif mean_type=='variance':
+        result = mycube.collapsed(coord1, iris.analysis.VARIANCE)
+    return result
+
 # get the area average
 import iris.analysis.cartography
 def area_average(mycube, coord1, coord2):
     """
     Function that determines the area average
     Can be used with coord1 and coord2 (strings,
-    usually 'longitude' and 'latitude' but depends on the cube)
+    usually 'longitude' and 'latitude' but depends on the cube);
+    Returns a cube
     """
     mycube.coord(coord1).guess_bounds()
     mycube.coord(coord2).guess_bounds()
     grid_areas = iris.analysis.cartography.area_weights(mycube)
-    new_cube = mycube.collapsed([coord1, coord2], iris.analysis.MEAN, weights=grid_areas)
-    return new_cube
+    result = mycube.collapsed([coord1, coord2], iris.analysis.MEAN, weights=grid_areas)
+    return result
 
 # get the seasonal mean
 import iris.coord_categorisation
 def seasonal_mean(mycube):
+    """
+    Function to compute seasonal means with MEAN
+    Chunks time in 3-month periods and computes means over them;
+    Returns a cube
+    """
     iris.coord_categorisation.add_season(mycube, 'time', name='clim_season')
     iris.coord_categorisation.add_season_year(mycube, 'time', name='season_year')
     annual_seasonal_mean = mycube.aggregated_by(['clim_season', 'season_year'], iris.analysis.MEAN)
