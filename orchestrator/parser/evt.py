86a7cfd7efe27326bb7528b1611fc4de32a0a12d
Björn Brötz <Bjoern.Broetz@dlr.de>
2017-06-07 16:56:59 +0200

    Add tests for full namelist

--- orchestrator/parser/evt.py
+++ orchestrator/parser/evt.py
@@ -1,44 +1,49 @@
 import yaml
 
+
 class Namelist(yaml.YAMLObject):
     """
     Class to hold the information from the namelist
     """
     yaml_tag = u'!Namelist'
-    def __init__(self,GLOBAL, PREPROCESS, MODLES, DIAGNOSTICS):
-        self.__name__       = "Namelist" 
-        self.GLOBAL         = GLOBAL
-        self.MODELS         = MODLES
-        self.PREPROCESS     = PREPROCESS
-        self.DIAGNOSTICS    = DIAGNOSTICS
+
+    def __init__(self, GLOBAL, PREPROCESS, MODLES, DIAGNOSTICS):
+        self.__name__ = "Namelist"
+        self.GLOBAL = GLOBAL
+        self.MODELS = MODLES
+        self.PREPROCESS = PREPROCESS
+        self.DIAGNOSTICS = DIAGNOSTICS
 
     def __repr__(self):
         return '{0}(settings={1}, preprocess={2}, \
                models={3}, diagnostics={4})'.format(
-                self.__class__.__name__,
-                self.GLOBAL,
-                self.PREPROCESS,
-                self.MODELS,
-                self.DIAGNOSTICS)
+            self.__class__.__name__,
+            self.GLOBAL,
+            self.PREPROCESS,
+            self.MODELS,
+            self.DIAGNOSTICS)
+
 
 class Config(object):
     """
     Class to hold the information from the configfile 
     """
+
     def __init__(self):
-        self.__name__ = "Config" 
+        self.__name__ = "Config"
+
 
 class Diagnostic(object):
     """
     """
+
     def __init__(self):
-        self.__name__ = "Diagnostic" 
+        self.__name__ = "Diagnostic"
+
 
 class Variable(object):
     """
     """
+
     def __init__(self):
-        self.__name__ = "Variable" 
-
-
-
+        self.__name__ = "Variable"
--- orchestrator/test/test_evt.py
+++ orchestrator/test/test_evt.py
@@ -1,6 +1,13 @@
 import pytest
 import yaml
+import os
 from evt import Namelist
+
+TESTDATA_DIR = os.path.join(
+    os.path.dirname(os.path.realpath(__file__)),
+    'testdata',
+)
+
 
 def test_init_namelist():
     s = """
@@ -10,18 +17,34 @@ PREPROCESS: True
 MODELS: True
 DIAGNOSTICS: True
 """
-    ans = yaml.load(s)
-    assert ans.GLOBAL is True
-    assert ans.PREPROCESS is True
-    assert ans.MODELS is True
-    assert ans.DIAGNOSTICS is True
+    n = yaml.load(s)
+    assert n.GLOBAL is True
+    assert n.PREPROCESS is True
+    assert n.MODELS is True
+    assert n.DIAGNOSTICS is True
+
 
 def test_load_namelist():
-    s = file('test/testdata/namelist_test002.yaml', 'r')
-    ans = yaml.load(s)
-    assert isinstance(ans,Namelist) 
-    assert ans.GLOBAL["write_plots"] == True
-    assert ans.PREPROCESS["pptag1"] == True
-    assert ans.MODELS == ["models1", "models2", "models3"]
-    assert ans.DIAGNOSTICS == ["diag1", "diag2", "diag3"]
+    s = file('{}/namelist_test002.yaml'.format(TESTDATA_DIR), 'r')
+    n = yaml.load(s)
+    assert isinstance(n, Namelist)
+    assert n.GLOBAL["write_plots"] == True
+    assert n.PREPROCESS["pptag1"] == True
+    assert n.MODELS == ["models1", "models2", "models3"]
+    assert n.DIAGNOSTICS == ["diag1", "diag2", "diag3"]
 
+
+def test_load_namelist_full():
+    s = file('{}/namelist_test004.yml'.format(TESTDATA_DIR), 'r')
+    n = yaml.load(s)
+    assert isinstance(n, Namelist)
+    assert n.GLOBAL["write_plots"] == True
+    assert n.GLOBAL["write_netcdf"] == True
+    assert n.GLOBAL["verbosity"] == 1
+    assert n.GLOBAL["exit_on_warning"] == False
+    assert n.GLOBAL["output_file_type"] == "ps"
+    assert 'select_level' in n.PREPROCESS.keys()
+    assert n.MODELS is None
+    assert isinstance(n.DIAGNOSTICS, list)
+    print( n.DIAGNOSTICS[0].keys()) # the keys need to be unique
+    assert False
