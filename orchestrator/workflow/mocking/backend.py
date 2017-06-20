e91f694b00452d0bf3e89d2925628379bd7b5443
Björn Brötz <Bjoern.Broetz@dlr.de>
2017-06-20 12:53:31 +0200

    Add workflow

---
+++ orchestrator/workflow/mocking/backend.py
@@ -0,0 +1,32 @@
+def timeExtract(cb):
+   print("In BACKEND.timeExtract")
+   return cb
+
+def reformat(cb):
+   print("In BACKEND.reformat")
+   return cb
+
+def extractLevel(cb):
+   print("In BACKEND.extractLevel")
+   return cb
+
+def regrid(cb):
+   print("In BACKEND.regrid")
+   return cb
+
+def mask(cb):
+   print("In BACKEND.mask")
+   return cb
+
+def gridPointOperation(cb):
+   print("In BACKEND.gridPointOperation")
+   return cb
+
+def timeOperations(cb):
+   print("In BACKEND.timeOperations")
+   return cb
+
+def multiModelstatistics(cb):
+   print("In BACKEND.multiModelstatistics")
+   return cb
+
