b8e92bc8a92ef48f8eb10321056e2d9137858cd9
Alex <alexander.loew@lmu.de>
2017-06-21 08:56:03 +0200

    running skeleton for Test Runner class

--- tests/run_tests.py
+++ tests/run_tests.py
@@ -11,7 +11,40 @@ import os
 class RunTests(object):
     def __init__(self):
         self.diagnostics = self._get_diagnostic_list()
-        print self.diagnostics
+        self.check_test_availability()
+        self.info()
+
+    def info(self):
+        print('*** EXISTING TESTS ***')
+        print('For the following diagnostics tests are available:')
+        for i, d in enumerate(self.valid_diagnostics):
+            print('%i. %s' % (i,d))
+
+        print('')
+        print('*** MISSING TESTS ***')
+        print('For the following diagnostics tests are missing:')
+        for i, d in enumerate(self.missing_diagnostics):
+            print('%i. %s' % (i,d))
+
+    def check_test_availability(self):
+        """ checks if tests are available and generates two lists """
+        self.valid_diagnostics = []
+        self.missing_diagnostics = []
+        for d in self.diagnostics:
+            if self._test_exists(d):
+                self.valid_diagnostics.append(d)
+            else:
+                self.missing_diagnostics.append(d)
+
+        assert len(self.missing_diagnostics) + len(self.valid_diagnostics) == len(self.diagnostics)
+
+    def _test_exists(self, d):
+        cd = os.path.dirname(os.path.abspath(__file__)) + os.sep + 'test_diagnostics' + os.sep + 'test_' + os.path.splitext(d)[0]
+        fname = cd + os.sep +  d
+        print fname
+        return os.path.exists(fname)
+
+
 
     def _get_diagnostic_list(self):
         nml_dir = self._get_nml_dir()
@@ -30,7 +63,7 @@ class RunTests(object):
         print('Backend tests not integrated yet')
 
     def test_diagnostics(self):
-        for d in self.diagnostics:
+        for d in self.valid_diagnostics:
             self._test_diagnostic(d)
 
     def _test_diagnostic(self, d):
