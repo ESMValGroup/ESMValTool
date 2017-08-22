9e5996db32f269c0a573aae79143044c925173c9
Javier Vegas-Regidor <javier.vegas@bsc.es>
2017-08-22 15:35:08 +0200

    Added some tests

--- tests/integration/orchestrator/interface_scripts/fixes/test_fix.py
+++ tests/integration/orchestrator/interface_scripts/fixes/test_fix.py
@@ -6,7 +6,7 @@ class TestFix(unittest.TestCase):
 
     def test_get_fix(self):
         from orchestrator.interface_scripts.fixes.CMIP5.BNU_ESM import ch4
-        self.assertIs(Fix.get_fix('CMIP5', 'BNU-ESM', 'ch4'), ch4)
+        self.assertIsInstance(Fix.get_fix('CMIP5', 'BNU-ESM', 'ch4'), ch4)
 
     def test_get_fix_no_project(self):
         self.assertIsNone(Fix.get_fix('BAD_PROJECT', 'BNU-ESM', 'ch4'))
