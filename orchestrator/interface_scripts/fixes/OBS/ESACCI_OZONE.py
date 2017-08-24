a52c6918af0cf565fe3ef048644813deb9818072
Javier Vegas-Regidor <javier.vegas@bsc.es>
2017-08-24 18:45:51 +0200

    Added fixes for obs

---
+++ orchestrator/interface_scripts/fixes/OBS/ESACCI_OZONE.py
@@ -0,0 +1,22 @@
+from orchestrator.interface_scripts.fixes.fix import Fix
+import cf_units
+
+
+class tro3prof(Fix):
+    def fix_metadata(self, cube):
+        old = cube.coord('air_pressure')
+        dims = cube.coord_dims(old)
+        cube.remove_coord(old)
+        points = old.points * 100
+        if old.bounds is None:
+            bounds = None
+        else:
+            bounds = old.bounds * 100
+        plev = old.copy(points, bounds)
+        plev.var_name = 'plev'
+        plev.standard_name = 'air_pressure'
+        plev.long_name = 'Pressure '
+        plev.units = cf_units.Unit('Pa')
+        cube.add_dim_coord(plev, dims)
+        return cube
+
