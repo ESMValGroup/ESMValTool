0568d5aa100b0e617b01a9c11ba3c1a74dc71018
Bouwe Andela <b.andela@esciencecenter.nl>
2017-08-29 17:51:41 +0200

    Added standard library logger
    Code not tested yet

--- orchestrator/interface_scripts/auxiliary.py
+++ orchestrator/interface_scripts/auxiliary.py
@@ -2,7 +2,9 @@ import pdb
 import sys
 import commands
 import string
+import logging
 
+logger = logging.getLogger(__name__)
 
 class nclExecuteError(Exception):
     def __init__(self, value):
@@ -57,34 +59,39 @@ def print_header():
     """ @brief Print the ESMValTool header
         @param project_info dictionary with the necessary information
     """
-    
+
     vv = 1
     line1 = 54 * "_"
     line2 = 70 * "_"
 
-    info("", vv, 1)
-    info(line1, vv, 1)
-    info("  _____ ____  __  ____     __    _ _____           _  ", vv, 1)
-    info(" | ____/ ___||  \/  \ \   / /_ _| |_   _|__   ___ | | ", vv, 1)
-    info(" |  _| \___ \| |\/| |\ \ / / _` | | | |/ _ \ / _ \| | ", vv, 1)
-    info(" | |___ ___) | |  | | \ V / (_| | | | | (_) | (_) | | ", vv, 1)
-    info(" |_____|____/|_|  |_|  \_/ \__,_|_| |_|\___/ \___/|_| ", vv, 1)
-    info(line1, vv, 1)
-    info("", vv, 1)
-    info(" http://www.esmvaltool.org/", vv, 1)
-    info(line2, vv, 1)
-    info("", vv, 1)
-    info("CORE DEVELOPMENT TEAM AND CONTACTS:", vv, 1)
-    info("  Veronika Eyring (PI; DLR, Germany - veronika.eyring@dlr.de)", vv, 1)
-    info("  Bjoern Broetz (DLR, Germany - bjoern.broetz@dlr.de)", vv, 1)
-    info("  Nikolay Koldunov (AWI, Germany - nikolay.koldunov@awi.de)", vv, 1)
-    info("  Axel Lauer (DLR, Germany - axel.lauer@dlr.de)", vv, 1)
-    info("  Benjamin Mueller (LMU, Germany - b.mueller@iggf.geo.uni-muenchen.de)", vv, 1)
-    info("  Valeriu Predoi (URead, UK - valeriu.predoi@ncas.ac.uk)", vv, 1)
-    info("  Mattia Righi (DLR, Germany - mattia.righi@dlr.de)", vv, 1)
-    info("  Javier Vegas-Regidor (BSC, Spain - javier.vegas@bsc.es)", vv, 1)
-    info(line2, vv, 1)
-    info("", vv, 1)
+    header = [
+        "",
+        line1,
+        "  _____ ____  __  ____     __    _ _____           _  ",
+        " | ____/ ___||  \/  \ \   / /_ _| |_   _|__   ___ | | ",
+        " |  _| \___ \| |\/| |\ \ / / _` | | | |/ _ \ / _ \| | ",
+        " | |___ ___) | |  | | \ V / (_| | | | | (_) | (_) | | ",
+        " |_____|____/|_|  |_|  \_/ \__,_|_| |_|\___/ \___/|_| ",
+        line1,
+        "",
+        " http://www.esmvaltool.org/",
+        line2,
+        "",
+        "CORE DEVELOPMENT TEAM AND CONTACTS:",
+        "  Veronika Eyring (PI; DLR, Germany - veronika.eyring@dlr.de)",
+        "  Bjoern Broetz (DLR, Germany - bjoern.broetz@dlr.de)",
+        "  Nikolay Koldunov (AWI, Germany - nikolay.koldunov@awi.de)",
+        "  Axel Lauer (DLR, Germany - axel.lauer@dlr.de)",
+        "  Benjamin Mueller (LMU, Germany - b.mueller@iggf.geo.uni-muenchen.de)",
+        "  Valeriu Predoi (URead, UK - valeriu.predoi@ncas.ac.uk)",
+        "  Mattia Righi (DLR, Germany - mattia.righi@dlr.de)",
+        "  Javier Vegas-Regidor (BSC, Spain - javier.vegas@bsc.es)",
+        line2,
+        "",
+    ]
+
+    logger.info("\n".join(header))
+
 
 def ncl_version_check():
     """ @brief Check the NCL version
@@ -92,15 +99,15 @@ def ncl_version_check():
     out = commands.getstatusoutput("ncl -V")
 
     if out[0] != 0:
-        error("NCL not found")
+        logger.error("NCL not found")
 
     if out[1] == "6.3.0":
-        error("NCL version " + out[1] + 
-              " not supported due to a bug " + 
+        logger.error("NCL version " + out[1] +
+              " not supported due to a bug " +
               "(see Known Issues in the ESMValTool user guide)")
 
     if int(out[1].split(".")[0]) < 6:
-        error("NCL version " + out[1] + " not supported, need version 6.2.0 or higher")
+        logger.error("NCL version " + out[1] + " not supported, need version 6.2.0 or higher")
 
     if int(out[1].split(".")[0]) == 6 and int(out[1].split(".")[1]) < 2:
-        error("NCL version " + out[1] + " not supported, need version 6.2.0 or higher")
+        logger.error("NCL version " + out[1] + " not supported, need version 6.2.0 or higher")
