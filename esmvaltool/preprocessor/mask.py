Fix wrong values at the mask boundaries (thx to V. Predoi)

esmvaltool/preprocessor/mask.py
@@ -258,8 +258,11 @@ def mask_cube_counts(mycube, value_threshold, counts_threshold, window_size):
     mask = counts_windowed_cube.data > counts_threshold
     mask.astype(np.int)
     # preserving the original cube metadata
+    dummyar = np.ones(mycube.data.shape)
+    newmask = dummyar * mask
+    newmask[newmask==0] = np.nan
     masked_cube = mycube.copy()
-    masked_cube.data = mycube.data * mask
+    masked_cube.data = masked_cube.data * newmask
     return counts_windowed_cube, mask, masked_cube
 
 