de27d57c5d342f3f102020e63dfa4eb6435bf32c
Bouwe Andela <b.andela@esciencecenter.nl>
2017-12-08 12:22:45 +0100

    Prevent downloading overlapping files

--- esmvaltool/preprocessor/download.py
+++ esmvaltool/preprocessor/download.py
@@ -1,8 +1,19 @@
-import subprocess
 import logging
 import os
+import subprocess
 
 logger = logging.getLogger(__name__)
+
+
+def get_start_end_year(filename):
+    """Get the start and end year from a file name.
+
+    This works for filenames matching *_YYYY*-YYYY*.*
+    """
+    name = os.path.splitext(filename)[0]
+    start, end = name.split('_')[-1].split('-')
+    start_year, end_year = int(start[:4]), int(end[:4])
+    return start_year, end_year
 
 
 def synda_search(variable):
@@ -25,18 +36,29 @@ def synda_search(variable):
     cmd = ' '.join(cmd)
     logger.debug("Running: %s", cmd)
     result = subprocess.check_output(cmd, shell=True, universal_newlines=True)
-    logger.debug('Result: %s', result)
+    logger.debug('Result:\n%s', result.strip())
 
     files = []
     for line in result.split('\n'):
         if line.startswith('new'):
             synda_name = line.split()[-1]
-            name = os.path.splitext(synda_name)[0]
-            start, end = name.split('_')[-1].split('-')
-            start_year, end_year = int(start[:4]), int(end[:4])
-            if (start_year <= variable['end_year']
-                    and end_year >= variable['start_year']):
+            start, end = get_start_end_year(synda_name)
+            if start <= variable['end_year'] and end >= variable['start_year']:
                 files.append(synda_name)
+
+    # filter partially overlapping files
+    intervals = {get_start_end_year(name): name for name in files}
+    files = []
+    for (start, end), filename in intervals.items():
+        for _start, _end in intervals:
+            if start == _start and end == _end:
+                continue
+            if start >= _start and end <= _end:
+                break
+        else:
+            files.append(filename)
+
+    logger.debug("Selected files:\n%s", '\n'.join(files))
 
     return files
 
