d002f68eb5e7897267a637e45ea51bad631aab9c
Bouwe Andela <b.andela@esciencecenter.nl>
2017-12-15 14:00:55 +0100

    Change info message to debug

--- esmvaltool/preprocessor/__init__.py
+++ esmvaltool/preprocessor/__init__.py
@@ -60,8 +60,8 @@ def _save_cubes(cubes, **args):
 
     if (os.path.exists(filename)
             and all(cube.has_lazy_data() for cube in cubes)):
-        logger.info("Not saving cubes %s to %s to avoid data loss. "
-                    "The cube is probably unchanged.", cubes, filename)
+        logger.debug("Not saving cubes %s to %s to avoid data loss. "
+                     "The cube is probably unchanged.", cubes, filename)
     else:
         logger.debug("Saving cubes %s to %s", cubes, filename)
         iris.save(cubes, **args)
