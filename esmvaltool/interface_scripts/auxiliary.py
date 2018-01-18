81410615c782327aa75281a00548cca8508051b4
Valeriu Predoi <valeriu.predoi@gmail.com>
2018-01-18 11:18:34 +0000

    changed version control output to text so piping will work

--- esmvaltool/interface_scripts/auxiliary.py
+++ esmvaltool/interface_scripts/auxiliary.py
@@ -9,12 +9,12 @@ def ncl_version_check():
     """ @brief Check the NCL version"""
     try:
         cmd = ['ncl', '-V']
-        version = subprocess.check_output(cmd)
+        version = subprocess.check_output(cmd, universal_newlines=True)
     except subprocess.CalledProcessError:
         logger.error("Failed to execute '%s'", ' '.join(cmd))
         raise
 
-    version = version.decode(sys.stdout.encoding)
+    #version = version.decode(sys.stdout.encoding)
 
     if version == "6.3.0":
         logger.error("NCL version " + version + " not supported due to a bug "
