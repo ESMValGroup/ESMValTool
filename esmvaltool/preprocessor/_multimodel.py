417211dec79d7ea88ad7bba40c8301fa4b22e06a
Valeriu Predoi <valeriu.predoi@gmail.com>
2018-01-19 15:13:17 +0000

    rewrote functions for better pylint results

--- esmvaltool/preprocessor/_multimodel.py
+++ esmvaltool/preprocessor/_multimodel.py
@@ -2,6 +2,7 @@
 import logging
 from functools import reduce
 
+import os
 import iris
 import numpy as np
 
@@ -10,17 +11,28 @@ from ._io import save_cubes
 logger = logging.getLogger(__name__)
 
 
+def _find_index(flist, idm):
+    """Find nearest element to idm in flist"""
+    max_t = min(flist, key=lambda x: abs(x - idm))
+    id_min = flist.index(max_t)
+    return id_min
+
+
+def _put_in_cube(stats, stats_name, ncfiles, fname):
+    """quick cube building"""
+    stats_cube = iris.cube.Cube(stats, long_name=stats_name)
+    stats_cube.attributes['_filename'] = fname
+    stats_cube.attributes['NCfiles'] = str(ncfiles)
+    return stats_cube
+
+
 def _get_overlap(cubes):
     """Get discrete time overlaps."""
     all_times = [cube.coord('time').points for cube in cubes]
     bounds = [range(int(np.min(b)), int(np.max(b)) + 1) for b in all_times]
     time_pts = reduce(np.intersect1d, (i for i in bounds))
     if len(time_pts) > 1:
-        time1 = time_pts[0]
-        time2 = time_pts[-1]
-        return time1, time2
-    else:
-        return 0, 0
+        return time_pts[0], time_pts[-1]
 
 
 def multi_model_mean(cubes, span, filename, exclude):
@@ -35,80 +47,71 @@ def multi_model_mean(cubes, span, filename, exclude):
         logger.info("Single model in list: will not compute statistics.")
         return cubes
 
-    means = []
-    medians = []
-    data_size = []
-    file_names = []
-    tx1, tx2 = _get_overlap(cubes)
-
     # check if we have any time overlap
-    if tx1 == tx2:
+    if _get_overlap(cubes) is None:
         logger.info("Time overlap between cubes is none or a single point.")
         logger.info("check models: will not compute statistics.")
         return cubes
+    else:
+        # empty lists to hold data
+        means = []
+        medians = []
+        data_size = []
 
-    for cube in selection:
-        file_name = cube.attributes.get('_filename').split('/')[-1]
-        file_names.append(file_name)
-        for coord in cube.coords():
-            if coord.standard_name == 'time':
-                if span == 'overlap':
+        # add file name info
+        file_names = [
+            os.path.basename(cube.attributes.get('_filename'))
+            for cube in cubes
+        ]
 
-                    logger.debug("Using common time overlap between \
-                                 models to compute statistics.")
-                    # find the nearest points to the overlap region
-                    all_times = list(cube.coord('time').points)
-                    min_t = min(all_times, key=lambda x: abs(x - tx1))
-                    max_t = min(all_times, key=lambda x: abs(x - tx2))
-                    id_min = all_times.index(min_t)
-                    id_max = all_times.index(max_t)
-                    tot = len(all_times) - 1
-                    logger.debug("Indexing time axis for overlap on \
-                                 indices %i and %i \
-                                 of %i", id_min, id_max, tot)
+        # look at overlap type
+        if span == 'overlap':
+            tx1, tx2 = _get_overlap(cubes)
+            for cube in selection:
+                logger.debug("Using common time overlap between "
+                             "models to compute statistics.")
+                # find the nearest points to the overlap region
+                id_min = _find_index(list(cube.coord('time').points), tx1)
+                id_max = _find_index(list(cube.coord('time').points), tx2)
+                logger.debug("Indexing time axis for overlap on "
+                             "indices %i and %i "
+                             "of %i", id_min, id_max,
+                             len(list(cube.coord('time').points)) - 1)
 
-                    # index data on these points and
-                    # get rid of masked values
-                    flat_data = cube.data[id_min:id_max + 1, :, :]
-                    cdata = flat_data[~flat_data.mask]
-                    data_size.append(cdata.shape[0])
+                # index data on these points and
+                # get rid of masked values
+                flat_data = cube.data[id_min:id_max + 1, :, :]
+                data_size.append(flat_data[~flat_data.mask].shape[0])
 
-                    # compute stats and append to lists
-                    cmean = np.mean(cdata)
-                    cmedian = np.median(cdata)
-                    means.append(cmean)
-                    medians.append(cmedian)
+                # compute stats and append to lists
+                means.append(np.mean(flat_data[~flat_data.mask]))
+                medians.append(np.median(flat_data[~flat_data.mask]))
 
-                elif span == 'full':
+        elif span == 'full':
+            for cube in selection:
+                logger.debug("Using full time spans "
+                             "to compute statistics.")
+                # get rid of masked
+                data_size.append(cube.data[~cube.data.mask].shape[0])
 
-                    logger.debug("Using full time spans \
-                                 to compute statistics.")
-                    # get rid of masked
-                    flat_data = cube.data
-                    cdata = flat_data[~flat_data.mask]
-                    data_size.append(cdata.shape[0])
+                # compute stats and append to lists
+                means.append(np.mean(cube.data[~cube.data.mask]))
+                medians.append(np.median(cube.data[~cube.data.mask]))
 
-                    # compute stats and append to lists
-                    cmean = np.mean(cdata)
-                    cmedian = np.median(cdata)
-                    means.append(cmean)
-                    medians.append(cmedian)
+        else:
+            logger.debug("No type of time overlap specified "
+                         "- will not compute cubes statistics")
+            return cubes
 
     logger.debug("Global means: %s", means)
-    means_cube = iris.cube.Cube(means, long_name='means')
-    means_cube.attributes['_filename'] = filename
-    means_cube.attributes['NCfiles'] = str(file_names)
+    c_mean = _put_in_cube(means, 'means', file_names, filename)
 
     logger.debug("Global medians: %s", medians)
-    medians_cube = iris.cube.Cube(medians, long_name='medians')
-    medians_cube.attributes['_filename'] = filename
-    medians_cube.attributes['NCfiles'] = str(file_names)
+    c_med = _put_in_cube(medians, 'medians', file_names, filename)
 
     logger.debug("Data sizes: %s", data_size)
-    datasize_cube = iris.cube.Cube(data_size, long_name='data_size')
-    datasize_cube.attributes['_filename'] = filename
-    datasize_cube.attributes['NCfiles'] = str(file_names)
+    c_dat = _put_in_cube(data_size, 'data_size', file_names, filename)
 
-    save_cubes([means_cube, medians_cube, datasize_cube])
+    save_cubes([c_mean, c_med, c_dat])
 
     return cubes
