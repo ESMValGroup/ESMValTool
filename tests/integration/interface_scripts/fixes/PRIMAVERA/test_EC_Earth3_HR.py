03cc8e0f01acc5f70d39cc1444309eca54b7ba0d
Javier Vegas-Regidor <javier.vegas@bsc.es>
2018-01-24 13:06:05 +0100

    Fix pep8 errors

--- tests/integration/interface_scripts/fixes/PRIMAVERA/test_EC_Earth3_HR.py
+++ tests/integration/interface_scripts/fixes/PRIMAVERA/test_EC_Earth3_HR.py
@@ -10,13 +10,14 @@ class TestAllVars(unittest.TestCase):
     def setUp(self):
         self.cube = Cube([[1, 2], [3, 4]], var_name='var')
         self.cube.add_dim_coord(
-            DimCoord([1, 2], standard_name='latitude', var_name='latitude'), 0)
+            DimCoord([1, 2], standard_name='latitude',
+                     var_name='latitude'), 0)
         self.cube.add_dim_coord(
-            DimCoord([1, 2], standard_name='longitude', var_name='longitude'), 1)
+            DimCoord([1, 2], standard_name='longitude',
+                     var_name='longitude'), 1)
         self.fix = allvars()
 
     def test_fix_lat_lon_names(self):
         cube = self.fix.fix_metadata(self.cube)
         self.assertEqual(cube.coord('latitude').var_name, 'lat')
         self.assertEqual(cube.coord('longitude').var_name, 'lon')
-
