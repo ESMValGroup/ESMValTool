709120ff4580591ce4d9bcab424e6a84bbaf7a69
Javier Vegas-Regidor <javier.vegas@bsc.es>
2018-01-24 12:23:33 +0100

    Added capability to read external cmor_tables

---
+++ esmvaltool/interface_scripts/fixes/PRIMAVERA/EC_Earth3_HR.py
@@ -0,0 +1,10 @@
+from esmvaltool.interface_scripts.fixes.fix import Fix
+
+
+class allvars(Fix):
+    def fix_metadata(self, cube):
+        latitude = cube.coord('latitude')
+        latitude.var_name = 'lat'
+
+        longitude = cube.coord('longitude')
+        longitude.var_name = 'lon'
