28f94744fe09b17e55ea507d76fdd2b26105f8f8
Javier Vegas-Regidor <javier.vegas@bsc.es>
2018-01-25 11:25:56 +0100

    Fixed codacy warnings

--- tests/integration/interface_scripts/fixes/PRIMAVERA/test_EC_Earth3_HR.py
+++ tests/integration/interface_scripts/fixes/PRIMAVERA/test_EC_Earth3_HR.py
@@ -1,3 +1,4 @@
+"""Test for the fixes for EC-Earth3-HR model from PRIMAVERA project"""
 import unittest
 
 from iris.coords import DimCoord
@@ -7,7 +8,9 @@ from esmvaltool.interface_scripts.fixes.PRIMAVERA.EC_Earth3_HR import allvars
 
 
 class TestAllVars(unittest.TestCase):
+    """Test for the common fixes for all the variables"""
     def setUp(self):
+        """Prepare to test"""
         self.cube = Cube([[1, 2], [3, 4]], var_name='var')
         self.cube.add_dim_coord(
             DimCoord([1, 2], standard_name='latitude',
@@ -18,6 +21,12 @@ class TestAllVars(unittest.TestCase):
         self.fix = allvars()
 
     def test_fix_lat_lon_names(self):
+        """
+        Test latitude and longitude var names
+
+        They should be lat and lon instead of the original latitude and
+        longitude
+        """
         cube = self.fix.fix_metadata(self.cube)
         self.assertEqual(cube.coord('latitude').var_name, 'lat')
         self.assertEqual(cube.coord('longitude').var_name, 'lon')
