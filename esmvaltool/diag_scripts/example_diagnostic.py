570c2b47fd4e4e37e60cb8a5c517dd677ba2a178
Bouwe Andela <b.andela@esciencecenter.nl>
2018-02-02 15:22:28 +0100

    Fix imports

--- esmvaltool/diag_scripts/example_diagnostic.py
+++ esmvaltool/diag_scripts/example_diagnostic.py
@@ -1,14 +1,12 @@
 """Python example diagnostic."""
-import yaml
+import logging
+import os
 import sys
-import cartopy.crs as ccrs
-import matplotlib.pyplot as plt
 
 import iris
-import iris.plot as iplt
 import iris.quickplot as qplt
-import os
-import logging
+import matplotlib.pyplot as plt
+import yaml
 
 logger = logging.getLogger(__name__)
 
@@ -59,8 +57,6 @@ def main():
 
 if __name__ == '__main__':
     iris.FUTURE.netcdf_promote = True
-    logging.basicConfig(
-        format="%(asctime)s [%(process)d] %(levelname)-8s "
-               "%(name)s,%(lineno)s\t%(message)s"
-    )
+    logging.basicConfig(format="%(asctime)s [%(process)d] %(levelname)-8s "
+                        "%(name)s,%(lineno)s\t%(message)s")
     main()
