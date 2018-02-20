fd6d64f4d9f53d83d17e237e8e9ac160d5d04c88
Björn Brötz <bjoern.broetz@dlr.de>
2018-02-20 10:26:14 +0100

    Cleanup&Enhancement for routine eval wrapper

--- util/wrapper/routineEval.py
+++ util/wrapper/routineEval.py
@@ -1,3 +1,13 @@
+import yaml
+import os
+import sys
+
+sys.path.insert(0, os.path.join(
+    os.path.dirname(os.path.abspath(__file__)), '../nml-utils/generateNML'))
+
+from generateNML import get_namelist
+
+
 yfile="""
 --- !CMIP6Dataset
 project: PROJECT
@@ -6,28 +16,30 @@ product: PRODUCT
 institute: INSTITUTE
 model: MODEL
 experiment: EXPERIMENT
-mip: MIP
+mip: Amon
 ensemble: ENSEMBLE
 start_year: START_YEAR
 end_year: END_YEAR
-variable: VARIABLE
+variable: tas
 """
-import yaml
+
 class CMIP6Dataset(yaml.YAMLObject):
     yaml_tag = u'!CMIP6Dataset'
     def __init__(self, project, name, product, institute, model, experiment,
             mip, ensemble, start_year, end_year, variable):
-            self.project = project
-            self.name = name
-            self.product = product
-            self.institute = institute
-            self.model = model
-            self.experiment = experiment
-            self.mip = mip
-            self.ensemble = ensemble
-            self.start_year = start_year
-            self.end_year = end_year
-            self.variable = variable
+        self.project = project
+        self.name = name
+        self.product = product
+        self.institute = institute
+        self.model = model
+        self.experiment = experiment
+        self.mip = mip
+        self.ensemble = ensemble
+        self.start_year = start_year
+        self.end_year = end_year
+        self.variable = variable
+    def get_dict(self):
+        return self.__dict__
     def __repr__(self):
         return "%s( project=%r, name=%r, product=%r, institute=%r, model=%r, experiment=%r, mip=%r, ensemble=%r, start_year=%r, end_year=%r, variable=%r )" % (
             self.__class__.__name__, self.project , self.name ,
@@ -39,12 +51,5 @@ class CMIP6Dataset(yaml.YAMLObject):
 h = yaml.load_all(yfile)
 for d in h:
     print(d)
-import os
-import sys
-
-sys.path.insert(0, os.path.join(
-    os.path.dirname(os.path.abspath(__file__)), '../nml-utils/generateNML'))
-
-from generateNML import get_namelist
-
-print(get_namelist())
+    print(d.get_dict())
+    print(get_namelist(**d.get_dict()))
