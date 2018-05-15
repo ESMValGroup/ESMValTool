fdcd7fdac5b8be5bcb03fe9dd1b5e72f8fef42da
Mattia Righi <mattia.righi@dlr.de>
2018-05-15 17:23:09 +0200

    Remove v1.0 files

--- diag_scripts/lib/python/sample_METAdata_for_ncl/sample_run_MD_for_ncl.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-# editing METAdata with an additional txt file
-python ../running_MD_for_ncl_with_file.py sample_MD.txt
-
-# editing METAdata with commands
-python ../running_MD_for_ncl_from_command.py sample_File.jpg --dtype=both --tags=mobile,home,flying --cap="This is a flying mobile home." --block=#IDtest
-
-
