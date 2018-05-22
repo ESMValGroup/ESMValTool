b9eac641224505556f71c7dd69a53346f0a7b5de
Bouwe Andela <b.andela@esciencecenter.nl>
2018-05-22 15:07:53 +0200

    Remove some whitespace to force readthedocs rebuild

--- doc/sphinx/source/conf.py
+++ doc/sphinx/source/conf.py
@@ -212,7 +212,6 @@ html_static_path = ['_static']
 # Output file base name for HTML help builder.
 htmlhelp_basename = 'ESMValTooldoc'
 
-
 # -- Options for LaTeX output ---------------------------------------------
 
 latex_elements = {
