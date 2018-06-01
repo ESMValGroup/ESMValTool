2310f2d05a373fc46217e5a3d5b6eecfa78d17ed
Lee de Mora <ledm@pml.ac.uk>
2018-06-01 09:22:57 +0100

    Added Model Specific fix for HadGEM2-ES and HadGEM2-CC, where latitude went beyond 90 degrees North.

---
+++ esmvaltool/cmor/_fixes/CMIP5/HadGEM2-CC.py
@@ -0,0 +1,24 @@
+"""Fixes for HadGEM2-CC"""
+from ..fix import Fix
+import numpy as np
+
+
+class lat(Fix):
+    """Fixes for lat"""
+
+    def fix_data(self, cube):
+        """
+        Fix data
+        Fixes problem where HadGEM2-CC data has latitude >  90. N
+        Parameters
+        ----------
+        cube: iris.cube.Cube
+        Returns
+        -------
+        iris.cube.Cube
+        """
+        lat = cube.coord('latitude')
+        lat.points = np.clip(lat.points,-90.,90.)
+	lat.bounds = np.clip(lat.bounds,-90.,90.)
+	
+        return cube * 1e6
