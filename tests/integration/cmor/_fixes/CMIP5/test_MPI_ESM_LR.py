Added fix for psu units, at they are not supported by cf-units

tests/integration/cmor/_fixes/CMIP5/test_MPI_ESM_LR.py
@@ -1,14 +1,9 @@
 import unittest
-import tempfile
 
-import numpy as np
 from cf_units import Unit
-import iris
 from iris.cube import Cube
-import netCDF4
-import os
 
-from esmvaltool.cmor._fixes.CMIP5.MPI_ESM_LR import pctisccp, so
+from esmvaltool.cmor._fixes.CMIP5.MPI_ESM_LR import pctisccp
 
 
 class TestPctisccp2(unittest.TestCase):
@@ -20,27 +15,3 @@ class TestPctisccp2(unittest.TestCase):
         cube = self.fix.fix_data(self.cube)
         self.assertEqual(cube.data[0], 100)
         self.assertEqual(cube.units, Unit('J'))
-
-
-class TestSo(unittest.TestCase):
-    def setUp(self):
-        file_handler, self.filename = tempfile.mkstemp('.nc')
-        os.close(file_handler)
-        handler = netCDF4.Dataset(self.filename, 'w')
-        handler.createDimension('time', 2)
-        var = handler.createVariable('so', float, ('time',))
-        var.units = 'psu'
-        var[:] = np.array((1.0, 1.0))
-        handler.close()
-        self.fix = so()
-
-    def tearDown(self):
-        os.remove(self.filename)
-
-    def test_fix_data(self):
-        cube = iris.load_cube(self.filename)
-        cube = self.fix.fix_metadata(cube)
-        self.assertEqual(cube.data[0], 1)
-        self.assertEqual(cube.data[1], 1)
-        self.assertEqual(cube.units, Unit('1'))
-        self.assertNotIn('invalid_units', cube.attributes)