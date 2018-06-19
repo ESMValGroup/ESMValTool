56bca29092ed60c7e74ca043680f6b69ec11fe1a
Javier Vegas <javier.vegas@bsc.es>
2018-06-19 10:03:24 +0200

    Added fix for psu units, at they are not supported by cf-units

--- esmvaltool/cmor/_fixes/CMIP5/MPI_ESM_LR.py
+++ esmvaltool/cmor/_fixes/CMIP5/MPI_ESM_LR.py
@@ -21,26 +21,3 @@ class pctisccp(Fix):
 
         """
         return cube * 100
-
-
-class so(Fix):
-    """Fixes for pctisccp"""
-
-    def fix_metadata(self, cube):
-        """
-        Fix data
-
-        Fixes discrepancy between declared units and real units
-
-        Parameters
-        ----------
-        cube: iris.cube.Cube
-
-        Returns
-        -------
-        iris.cube.Cube
-
-        """
-        cube.units = '1.0'
-        del cube.attributes['invalid_units']
-        return cube
