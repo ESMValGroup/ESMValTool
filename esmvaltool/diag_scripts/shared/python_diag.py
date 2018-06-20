21e692a3ab8c90376d06c051361fa9d6e5c77a4e
Manuel Schlund <manuel.schlund@dlr.de>
2018-06-20 12:22:11 +0200

    Added proper docstrings to general python diagnostic file

--- esmvaltool/diag_scripts/shared/python_diag.py
+++ esmvaltool/diag_scripts/shared/python_diag.py
@@ -1,6 +1,19 @@
+"""Convenience classes and functions to implement python diagnostics.
+
+Example
+-------
+Import and use these basic classes by e.g.::
+
+    import esmvaltool.diag_scripts.shared as e
+    models = e.Models(cfg)
+
+Notes
+-----
+An example diagnostic using these classes is given in
+`diag_scripts/examples/diagnostic.py`
+
 """
-Convenience classes and functions to implement python diagnostics.
-"""
+
 
 import collections
 import logging
@@ -34,6 +47,7 @@ VAR_STD_NAME_STR = 'standard_name'
 VAR_UNITS_STR = 'units'
 
 
+# Variable class containing all relevant information
 Variable = collections.namedtuple('Variable', [VAR_SHORT_NAME_STR,
                                                VAR_STD_NAME_STR,
                                                VAR_LONG_NAME_STR,
@@ -41,20 +55,40 @@ Variable = collections.namedtuple('Variable', [VAR_SHORT_NAME_STR,
 
 
 class Variables(object):
-    """
-    Class containing all variables. Short names of variables can be called by
-    member.var, all other information by member.VAR.
+    """Class to easily access a namelist's variables.
 
-    Methods:
-        add_var
-        short_names
-        standard_names
+    This class is designed to easily access variables in the diagnostic script.
+
+    Examples
+    --------
+    Get all variables of a namelist configuration `cfg`::
+
+        vars = Variables(cfg)
+
+    Access `short_name` (as str) of a variable `tas`::
+
+        vars.tas
+
+    Access all other information of a variable `tas`::
+
+        vars.TAS.short_name
+        vars.TAS.standard_name
+        vars.TAS.long_name
+        vars.TAS.units
+
     """
 
     def __init__(self, cfg=None, **names):
-        """
-        Create (private) dictionary containing all variable information and add
-        attributes to the class with the variables' short names.
+        """Load variables.
+
+        Parameters
+        ----------
+        cfg : dict, optional
+            Configuation dictionary of the namelist.
+        **names : tuple or Variable, optional
+            Keyword arguments of the form `short_name=Variable_object` where
+            `Variable_object` can be given as tuple or Variable.
+
         """
         self._dict = {}
 
@@ -88,14 +122,21 @@ class Variables(object):
             logger.warning("No variables found!")
 
     def __repr__(self):
-        """
-        Return all the short names of the included variables.
+        """Representation of the class.
+
         """
         return repr(self.short_names())
 
     def _add_to_dict(self, name, attr):
-        """
-        Add variables to the class member.
+        """Internal method to add a variable to class.
+
+        Parameters
+        ----------
+        name : str
+            `short_name` of the variable.
+        attr : Variable
+            All other information of the variable.
+
         """
         if (name not in self._dict):
             logger.debug("Added variable '{}' to collection".format(name))
@@ -104,61 +145,80 @@ class Variables(object):
         self._dict[name] = attr
 
     def add_var(self, **names):
-        """
-        Add a costum variable to the class member.
+        """Add a costum variable to the class member.
+
+        Parameters
+        ----------
+        **names : tuple or Variable, optional
+            Keyword arguments of the form `short_name=Variable_object` where
+            `Variable_object` can be given as tuple or Variable.
+
         """
         for name in names:
             attr = Variable(*names[name])
             self._add_to_dict(name, attr)
 
     def short_names(self):
-        """
-        Return a list of the variables' short names.
+        """Get list of all `short_names`.
+
+        Returns
+        -------
+        list
+            List of all `short_names`.
         """
         return list(self._dict)
 
     def standard_names(self):
-        """
-        Return a list of the variables' standard names.
+        """Get list of all `standard_names`.
+
+        Returns
+        -------
+        list
+            List of all `standard_names`.
+
         """
         return [getattr(self._dict[name], VAR_STD_NAME_STR) for
                 name in self._dict]
 
 
 class Models(object):
-    """
-    Class to easily save and access model data.
+    """Class to easily access a namelist's datasets
 
-    Methods:
-        add_model
-        add_to_data
-        get_data
-        get_data_list
-        get_exp
-        get_model
-        get_model_info
-        get_model_info_list
-        get_path
-        get_path_list
-        get_short_name
-        set_data
+    This class is designed to easily access datasets in the diagnostic script.
+
+    Examples
+    --------
+    Get all variables of a namelist configuration `cfg`::
+
+        models = Models(cfg)
+
+    Access data of a model with path `path`::
+
+        models.get_data(model_path=path)
+
+    Access model information of the model::
+
+        models.get_model_info(model_path=path)
+
+    Access the data of all models with `exp=piControl'::
+
+        models.get_data_list(exp=piControl)
+
     """
 
     def __init__(self, cfg):
+        """Load models.
+
+        Load all datasets of the namelist and store them in three internal
+        dictionaries/lists (`self._paths`, `self._data` and `self._models`).
+
+        Parameters
+        ----------
+        cfg : dict, optional
+            Configuation dictionary of the namelist.
+
         """
-        Create (private) lists and dictionaries of the form
-            [path1, path2, path3, ...]
-        and
-            {path1: {model1=..., project1=..., ...},
-             path2: {model2=..., project2=..., ...},
-             ...}
-             (identical to 'input_data')
-        and
-            {path1: data1,
-             path2: data2,
-             ...}
-        to get easy access to the models' data.
-        """
+
         self._paths = []
         self._data = {}
         success = True
@@ -183,8 +243,8 @@ class Models(object):
         self._n_models = len(self._paths)
 
     def __repr__(self):
-        """
-        Return the representation of the class member's dictionary keys.
+        """Representation of the class.
+
         """
         output = ''
         for path in self._models:
@@ -192,15 +252,15 @@ class Models(object):
         return output
 
     def __iter__(self):
-        """
-        Allow iteration through class member.
+        """Allow iteration through class.
+
         """
         self._iter_counter = 0
         return self
 
     def __next__(self):
-        """
-        Allow iteration through class member.
+        """Allow iteration through class.
+
         """
         if (self._iter_counter >= self._n_models):
             raise StopIteration()
@@ -210,9 +270,18 @@ class Models(object):
             return next_element
 
     def add_model(self, path, data=None, **model_info):
-        """
-        Add costum model to the class member. Hint: 'path' can also be a unique
-        identifier for the model if not used for file access.
+        """Add model to class.
+
+        Parameters
+        ----------
+        path : str
+            (Unique) path to the dataset.
+        data, optional
+            Arbitrary object to be save as data for the model.
+        **model_info, optional
+            Keyword arguments describing the model, e.g. `model=CanESM2`,
+            `exp=piControl` or `short_name=tas`.
+
         """
         if (path in self._paths):
             logger.warning("{} already exists! ".format(path) +
@@ -223,8 +292,24 @@ class Models(object):
         self._models[path] = model_info
 
     def add_to_data(self, data, model_path=None, **model_info):
-        """
-        Add data to already given data (the description must be unique).
+        """Add element to a model's data.
+
+        Notes
+        -----
+        Either `model_path` or a unique `model_info` description have to be
+        given. Prints warning and does nothing if given information is
+        ambiguous.
+
+        Parameters
+        ----------
+        data
+            Element to be added to the model's data.
+        model_path : str, optional
+            Path to the dataset
+        **model_info, optional
+            Keyword arguments describing the model, e.g. `model=CanESM2`,
+            `exp=piControl` or `short_name=tas`.
+
         """
         if (model_path is not None):
             if (model_path in self._paths):
@@ -249,9 +334,31 @@ class Models(object):
         self._data[path] += data
 
     def get_data(self, model_path=None, **model_info):
-        """
-        Return data that matches the given model information or path. Fails if
-        ambiguous.
+        """Access a model's data.
+
+        Notes
+        -----
+        Either `model_path` or a unique `model_info` description have to be
+        given. Fails when given information is ambiguous.
+
+        Parameters
+        ----------
+        model_path : str, optional
+            Path to the dataset
+        **model_info, optional
+            Keyword arguments describing the model, e.g. `model=CanESM2`,
+            `exp=piControl` or `short_name=tas`.
+
+        Returns
+        -------
+        data_object
+            Data of the selected model
+
+        Raises
+        ------
+        RuntimeError
+            If data given by `model_info` is ambiguous.
+
         """
         if (model_path is not None):
             if (model_path in self._paths):
@@ -275,9 +382,23 @@ class Models(object):
         return self._data[paths[0]]
 
     def get_data_list(self, **model_info):
-        """
-        Return all data in a list that match the given model information
-        (sorted alphabetically by path).
+        """Access the models' data in a list.
+
+        Notes
+        -----
+        The returned data is sorted alphabetically respective to the `paths`.
+
+        Parameters
+        ----------
+        **model_info, optional
+            Keyword arguments describing the model, e.g. `model=CanESM2`,
+            `exp=piControl` or `short_name=tas`.
+
+        Returns
+        -------
+        list
+            Data of the selected models.
+
         """
         paths = list(self._models)
         for info in model_info:
@@ -290,8 +411,22 @@ class Models(object):
         return [self._data[path] for path in paths]
 
     def get_exp(self, model_path):
-        """
-        Return 'exp' entry of given model.
+        """Access a model's `exp`.
+
+        Notes
+        -----
+        If the `model_info` does not contain an `exp` value, returns None.
+
+        Parameters
+        ----------
+        model_path : str
+            Path to the dataset
+
+        Returns
+        -------
+        str
+            `exp` information of the given model.
+
         """
         if (model_path in self._paths):
             output = self._models[model_path].get(EXP_STR)
@@ -305,8 +440,22 @@ class Models(object):
             return None
 
     def get_model(self, model_path):
-        """
-        Return 'model' entry of given model.
+        """Access a model's `model`.
+
+        Notes
+        -----
+        If the `model_info` does not contain a `model` value, returns None.
+
+        Parameters
+        ----------
+        model_path : str
+            Path to the dataset
+
+        Returns
+        -------
+        str
+            `model` information of the given model.
+
         """
         if (model_path in self._paths):
             output = self._models[model_path].get(MODEL_STR)
@@ -320,9 +469,31 @@ class Models(object):
             return None
 
     def get_model_info(self, model_path=None, **model_info):
-        """
-        Return model info that matches the given model information or path.
-        Fails if ambiguous.
+        """Access a model's information.
+
+        Notes
+        -----
+        Either `model_path` or a unique `model_info` description have to be
+        given. Fails when given information is ambiguous.
+
+        Parameters
+        ----------
+        model_path : str, optional
+            Path to the dataset
+        **model_info, optional
+            Keyword arguments describing the model, e.g. `model=CanESM2`,
+            `exp=piControl` or `short_name=tas`.
+
+        Returns
+        -------
+        dict
+            All model information.
+
+        Raises
+        ------
+        RuntimeError
+            If data given by `model_info` is ambiguous.
+
         """
         if (model_path is not None):
             if (model_path in self._paths):
@@ -345,18 +516,25 @@ class Models(object):
             raise RuntimeError(msg)
         return self._models[paths[0]]
 
-    def get_model_info_list(self, model_path=None, **model_info):
+    def get_model_info_list(self, **model_info):
+        """Access models information in a list.
+
+        Notes
+        -----
+        The returned data is sorted alphabetically respective to the `paths`.
+
+        Parameters
+        ----------
+        **model_info, optional
+            Keyword arguments describing the model, e.g. `model=CanESM2`,
+            `exp=piControl` or `short_name=tas`.
+
+        Returns
+        -------
+        list
+            Information dictionaries of the selected models.
+
         """
-        Return all model info in a list that match the given model
-        information (sorted alphabetically by path).
-        """
-        if (model_path is not None):
-            if (model_path in self._paths):
-                return [self._models.get(model_path)]
-            else:
-                logger.warning("{} is not a valid ".format(model_path) +
-                               "model path")
-                return None
         paths = list(self._models)
         for info in model_info:
             paths = [path for path in paths if
@@ -368,9 +546,29 @@ class Models(object):
         return [self._models[path] for path in paths]
 
     def get_path(self, **model_info):
-        """
-        Return a path that match the given model information. Fails if
-        ambiguous.
+        """Access a model's path
+
+        Notes
+        -----
+        A unique `model_info` description has to be given. Fails when given
+        information is ambiguous.
+
+        Parameters
+        ----------
+        **model_info, optional
+            Keyword arguments describing the model, e.g. `model=CanESM2`,
+            `exp=piControl` or `short_name=tas`.
+
+        Returns
+        -------
+        str
+            Path of the selected model.
+
+        Raises
+        ------
+        RuntimeError
+            If data given by `model_info` is ambiguous.
+
         """
         paths = list(self._models)
         for info in model_info:
@@ -387,9 +585,23 @@ class Models(object):
         return paths[0]
 
     def get_path_list(self, **model_info):
-        """
-        Return all paths in a list that match the given model information
-        (sorted alphabetically).
+        """Access models paths in a list.
+
+        Notes
+        -----
+        The returned data is sorted alphabetically respective to the `paths`.
+
+        Parameters
+        ----------
+        **model_info, optional
+            Keyword arguments describing the model, e.g. `model=CanESM2`,
+            `exp=piControl` or `short_name=tas`.
+
+        Returns
+        -------
+        list
+            Paths of the selected models.
+
         """
         paths = list(self._models)
         for info in model_info:
@@ -402,8 +614,22 @@ class Models(object):
         return paths
 
     def get_project(self, model_path):
-        """
-        Return 'project' entry of given model.
+        """Access a model's `project`.
+
+        Notes
+        -----
+        If the `model_info` does not contain a `project` value, returns None.
+
+        Parameters
+        ----------
+        model_path : str
+            Path to the dataset
+
+        Returns
+        -------
+        str
+            `project` information of the given model.
+
         """
         if (model_path in self._paths):
             output = self._models[model_path].get(PROJECT_STR)
@@ -417,8 +643,23 @@ class Models(object):
             return None
 
     def get_short_name(self, model_path):
-        """
-        Return 'short_name' entry of given model.
+        """Access a model's `short_name`.
+
+        Notes
+        -----
+        If the `model_info` does not contain a `short_name` value, returns
+        None.
+
+        Parameters
+        ----------
+        model_path : str
+            Path to the dataset
+
+        Returns
+        -------
+        str
+            `short_name` information of the given model.
+
         """
         if (model_path in self._paths):
             output = self._models[model_path].get(SHORT_NAME_STR)
@@ -432,8 +673,24 @@ class Models(object):
             return None
 
     def set_data(self, data, model_path=None, **model_info):
-        """
-        Set the data of a specified model (the description must be unique).
+        """Set element as a model's data.
+
+        Notes
+        -----
+        Either `model_path` or a unique `model_info` description have to be
+        given. Prints warning and does nothing if given information is
+        ambiguous.
+
+        Parameters
+        ----------
+        data
+            Element to be set as the model's data.
+        model_path : str, optional
+            Path to the dataset
+        **model_info, optional
+            Keyword arguments describing the model, e.g. `model=CanESM2`,
+            `exp=piControl` or `short_name=tas`.
+
         """
         if (model_path is not None):
             if (model_path in self._paths):
