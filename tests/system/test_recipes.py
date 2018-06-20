ee3d4800feacd9361a7b5b64c1fc9374b5c43543
Mattia Righi <mattia.righi@dlr.de>
2018-06-20 10:37:22 +0200

    Apply renamings in tests

--- tests/system/test_recipes.py
+++ tests/system/test_recipes.py
@@ -5,7 +5,7 @@ import tempfile
 
 import pytest
 
-from .esmvaltool_testlib import NAMELISTS, ESMValToolTest
+from .esmvaltool_testlib import RECIPES, ESMValToolTest
 
 
 @pytest.fixture
@@ -16,11 +16,11 @@ def output_directory():
     shutil.rmtree(tmp, ignore_errors=True)
 
 
-@pytest.mark.parametrize("namelist", NAMELISTS)
-def test_namelist(output_directory, namelist):  # noqa
-    """Create a test for each namelist in NAMELISTS and run those."""
+@pytest.mark.parametrize("recipe", RECIPES)
+def test_recipe(output_directory, recipe):  # noqa
+    """Create a test for each recipe in RECIPES and run those."""
     test = ESMValToolTest(
-        namelist=namelist,
+        recipe=recipe,
         output_directory=output_directory,
         ignore=['tmp/*/*', '*log*.txt', '*.log'],
         checksum_exclude=['pdf', 'ps', 'png', 'eps', 'epsi', 'nc'])
