6476cf673adea13c4283eb1ce59d667ff086cdf3
Lee de Mora <ledm@pml.ac.uk>
2018-07-04 15:40:37 +0100

    Moved testing dataset into the SetUp function and added empty __init__.py

--- tests/unit/preprocessor/_volume_pp/test_volume_pp.py
+++ tests/unit/preprocessor/_volume_pp/test_volume_pp.py
@@ -19,27 +19,39 @@ from esmvaltool.preprocessor._volume_pp import extract_trajectory
 
 
 class Test(tests.Test):
-    def test_area_volume_slice(self):
-        """
-        Test to extract the top two layers of a 3 layer depth column.
-        """
+    def setUp(self):
+        """ Prepare tests """
         cs = iris.coord_systems.GeogCS(iris.fileformats.pp.EARTH_RADIUS)
-        data = np.arange(3)
-        grid = iris.cube.Cube(data)
+        data2 = np.ones((3, 2, 2))
 
+        grid_3d = iris.cube.Cube(data2)
+        lons2 = iris.coords.DimCoord(
+            [1.5, 2.5],
+            standard_name='longitude',
+            bounds=[[1., 2.], [2., 3.]],
+            units='degrees_east',
+            coord_system=cs)
+        lats2 = iris.coords.DimCoord(
+            [1.5, 2.5],
+            standard_name='latitude',
+            bounds=[[1., 2.], [2., 3.]],
+            units='degrees_north',
+            coord_system=cs)
         depth = iris.coords.DimCoord(
             [0.5, 5., 50.],
             standard_name='depth',
             bounds=[[0., 2.5], [2.5, 25.], [25., 250.]],
-            units='m',
-            coord_system=cs)
+            units='m')
+        coords_spec3 = [(depth, 0), (lats2, 1), (lons2, 2)]
+        self.grid_3d = iris.cube.Cube(data2, dim_coords_and_dims=coords_spec3)
 
-        coords_spec = [(depth, 0), ]
-        grid = iris.cube.Cube(data, dim_coords_and_dims=coords_spec)
-
-        result = volume_slice(grid, 0., 10.)
-
-        expected = np.array([0., 1.])
+    def test_area_volume_slice(self):
+        """
+        Test to extract the top two layers of a 3 layer depth column.
+        """
+        result = volume_slice(self.grid_3d, 0., 10.)
+        expected = np.ones((2, 2, 2))
+        print(result.data, expected.data)
         self.assertArrayEqual(result.data, expected)
 
     def test_area_volume_average(self):
@@ -47,123 +59,49 @@ class Test(tests.Test):
         Test to take the volume weighted average of a (3,2,2) cube.
         """
         cs = iris.coord_systems.GeogCS(iris.fileformats.pp.EARTH_RADIUS)
-        data2 = np.ones((3, 2, 2))
-
-        grid2 = iris.cube.Cube(data2)
-        lons2 = iris.coords.DimCoord(
-            [1.5, 2.5],
-            standard_name='longitude',
-            bounds=[[1., 2.], [2., 3.]],
-            units='degrees_east',
-            coord_system=cs)
-        lats2 = iris.coords.DimCoord(
-            [1.5, 2.5],
-            standard_name='latitude',
-            bounds=[[1., 2.], [2., 3.]],
-            units='degrees_north',
-            coord_system=cs)
-        depth = iris.coords.DimCoord(
-            [0.5, 5., 50.],
-            standard_name='depth',
-            bounds=[[0., 2.5], [2.5, 25.], [25., 250.]],
-            units='m',
-            coord_system=cs)
-        coords_spec3 = [(depth, 0), (lats2, 1), (lons2, 2)]
-        grid2 = iris.cube.Cube(data2, dim_coords_and_dims=coords_spec3)
-
-        result = volume_average(grid2, 'depth', 'latitude', 'longitude')
-
+        result = volume_average(self.grid_3d, 'depth', 'latitude', 'longitude')
         expected = np.array([1.])
         self.assertArrayEqual(result.data, expected)
 
-    def test_area_depth_integration(self):
+    def test_area_depth_integration_1D(self):
         """
         Test to take the depth integration of a 3 layer cube.
         """
-        cs = iris.coord_systems.GeogCS(iris.fileformats.pp.EARTH_RADIUS)
-        data = np.ones((3, ))
-        grid = iris.cube.Cube(data)
-
-        depth = iris.coords.DimCoord(
-            [0.5, 5., 50.],
-            standard_name='depth',
-            bounds=[[0., 2.5], [2.5, 25.], [25., 250.]],
-            units='m',
-            coord_system=cs)
-
-        coords_spec = [(depth, 0), ]
-        grid = iris.cube.Cube(data, dim_coords_and_dims=coords_spec)
-
-        result = depth_integration(grid, 'depth')
-
-        expected = np.array([250., ])
+        result = depth_integration(self.grid_3d[:, 0, 0], 'depth')
+        expected = np.ones((1, 1)) * 250.
+        print(result.data, expected.data)
         self.assertArrayEqual(result.data, expected)
 
-    def test_area_extract_transect(self):
+    def test_area_depth_integration_3D(self):
         """
-        Test to extract a transect from a (3,2,2) cube.
+        Test to take the depth integration of a 3 layer cube.
         """
-        cs = iris.coord_systems.GeogCS(iris.fileformats.pp.EARTH_RADIUS)
-        data2 = np.ones((3, 2, 2))
+        result = depth_integration(self.grid_3d, 'depth')
+        expected = np.ones((2, 2)) * 250.
+        print(result.data, expected.data)
+        self.assertArrayEqual(result.data, expected)
 
-        grid2 = iris.cube.Cube(data2)
-        lons2 = iris.coords.DimCoord(
-            [1.5, 2.5],
-            standard_name='longitude',
-            bounds=[[1., 2.], [2., 3.]],
-            units='degrees_east',
-            coord_system=cs)
-        lats2 = iris.coords.DimCoord(
-            [1.5, 2.5],
-            standard_name='latitude',
-            bounds=[[1., 2.], [2., 3.]],
-            units='degrees_north',
-            coord_system=cs)
-        depth = iris.coords.DimCoord(
-            [0.5, 5., 50.],
-            standard_name='depth',
-            bounds=[[0., 2.5], [2.5, 25.], [25., 250.]],
-            units='m',
-            coord_system=cs)
-        coords_spec3 = [(depth, 0), (lats2, 1), (lons2, 2)]
-        grid2 = iris.cube.Cube(data2, dim_coords_and_dims=coords_spec3)
+    def test_area_extract_transect_latitude(self):
+        """
+        Test to extract a transect from a (3, 2, 2) cube.
+        """
+        result = extract_transect(self.grid_3d, latitude=1.5)
+        expected = np.ones((3, 2))
+        self.assertArrayEqual(result.data, expected)
 
-        result = extract_transect(grid2, latitude=1.5)
-
+    def test_area_extract_transect_longitude(self):
+        """
+        Test to extract a transect from a (3, 2, 2) cube.
+        """
+        result = extract_transect(self.grid_3d, longitude=1.5)
         expected = np.ones((3, 2))
         self.assertArrayEqual(result.data, expected)
 
     def test_area_extract_trajectory(self):
         """
-        Test to extract a trajectory from a (3,2,2) cube.
+        Test to extract a trajectory from a (3, 2, 2) cube.
         """
-        cs = iris.coord_systems.GeogCS(iris.fileformats.pp.EARTH_RADIUS)
-        data2 = np.ones((3, 2, 2))
-
-        grid2 = iris.cube.Cube(data2)
-        lons2 = iris.coords.DimCoord(
-            [1.5, 2.5],
-            standard_name='longitude',
-            bounds=[[1., 2.], [2., 3.]],
-            units='degrees_east',
-            coord_system=cs)
-        lats2 = iris.coords.DimCoord(
-            [1.5, 2.5],
-            standard_name='latitude',
-            bounds=[[1., 2.], [2., 3.]],
-            units='degrees_north',
-            coord_system=cs)
-        depth = iris.coords.DimCoord(
-            [0.5, 5., 50.],
-            standard_name='depth',
-            bounds=[[0., 2.5], [2.5, 25.], [25., 250.]],
-            units='m',
-            coord_system=cs)
-        coords_spec3 = [(depth, 0), (lats2, 1), (lons2, 2)]
-        grid2 = iris.cube.Cube(data2, dim_coords_and_dims=coords_spec3)
-
-        result = extract_trajectory(grid2, [1.5, 2.5], [2., 2.], 2)
-
+        result = extract_trajectory(self.grid_3d, [1.5, 2.5], [2., 2.], 2)
         expected = np.ones((3, 2))
         self.assertArrayEqual(result.data, expected)
 
