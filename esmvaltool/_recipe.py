9771baeed4fefd244303c73ef6878904aaa71e25
Bouwe Andela <b.andela@esciencecenter.nl>
2018-07-05 16:09:15 +0200

    Make land/ocean masking work, even if required fx_file not specified at variable

--- esmvaltool/_recipe.py
+++ esmvaltool/_recipe.py
@@ -454,22 +454,23 @@ def _get_default_settings(variable, config_user, derive=False):
     return settings
 
 
-def _update_fx_settings(settings, variable):
+def _update_fx_settings(variable, settings, config_user):
     """Find and set the FX mask settings"""
-    if 'mask_landocean' in settings.keys():
-        # Configure ingestion of landocean masks
-        logger.debug('Getting FX mask settings now...')
-        if variable.get('fx_files'):
-            if 'sftlf' in variable['fx_files'].keys():
-                settings['mask_landocean']['fx_file'] = \
-                    variable['fx_files']['sftlf']
-            elif 'sftof' in variable['fx_files'].keys():
-                settings['mask_landocean']['fx_file'] = \
-                    variable['fx_files']['sftof']
-            # add more file options here with elif
-            # else: return an empty value
-            else:
-                settings['mask_landocean']['fx_file'] = None
+    if 'mask_landocean' in settings:
+        logger.debug('Getting land/ocean mask settings now.')
+        masks = {
+            'land': 'sftlf',
+            'ocean': 'sftof',
+        }
+        mask = masks[settings['mask_landocean']['mask_out']]
+        if variable.get('fx_files', {}).get(mask):
+            fx_files = variable['fx_files']
+        else:
+            fx_files = get_input_fx_filelist(
+                variable=variable,
+                rootpath=config_user['rootpath'],
+                drs=config_user['drs'])
+        settings['mask_landocean']['fx_file'] = fx_files.get(mask)
 
 
 def _get_input_files(variable, config_user):
@@ -555,8 +556,8 @@ def _get_preprocessor_settings(variables, profile, config_user):
             variables=variables,
             settings=settings,
             config_user=config_user)
-        _update_fx_settings(settings=settings,
-                            variable=variable)
+        _update_fx_settings(
+            variable=variable, settings=settings, config_user=config_user)
         _update_target_grid(
             variable=variable,
             variables=variables,
