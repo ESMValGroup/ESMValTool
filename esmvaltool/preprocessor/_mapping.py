7b1793a3409463daff2248eaddb9bfa1017ac77f
Klaus Zimmermann <klaus.zimmermann@smhi.se>
2018-07-06 13:30:21 +0200

    Improved ESMPY based regridding with a generalized mapping routine
    Signed-off-by: Klaus Zimmermann <klaus.zimmermann@smhi.se>

---
+++ esmvaltool/preprocessor/_mapping.py
@@ -0,0 +1,151 @@
+# -*- coding: utf-8 -*-
+
+import collections
+import itertools
+
+import iris
+import numpy as np
+import six
+
+
+def _is_single_item(testee):
+    """
+    Return whether this is a single item, rather than an iterable.
+    We count string types as 'single', also.
+    """
+    return (isinstance(testee, six.string_types) or
+            not isinstance(testee, collections.Iterable))
+
+
+def _as_list_of_coords(cube, names_or_coords):
+    """
+    Convert a name, coord, or list of names/coords to a list of coords.
+    """
+    # If not iterable, convert to list of a single item
+    if _is_single_item(names_or_coords):
+        names_or_coords = [names_or_coords]
+    coords = []
+    for name_or_coord in names_or_coords:
+        if (isinstance(name_or_coord, six.string_types) or
+                isinstance(name_or_coord, iris.coords.Coord)):
+            coords.append(cube.coord(name_or_coord))
+        else:
+            # Don't know how to handle this type
+            msg = ("Don't know how to handle coordinate of type %s. "
+                   "Ensure all coordinates are of type six.string_types "
+                   "or iris.coords.Coord.") % (type(name_or_coord), )
+            raise TypeError(msg)
+    return coords
+
+
+def ref_to_dims_index(cube, ref_to_slice):
+    # Required to handle a mix between types
+    if _is_single_item(ref_to_slice):
+        ref_to_slice = [ref_to_slice]
+    dim_to_slice = []
+    for ref in ref_to_slice:
+        try:
+            # attempt to handle as coordinate
+            coord = cube._as_list_of_coords(ref)[0]
+            dims = cube.coord_dims(coord)
+            if not dims:
+                msg = ('Requested an iterator over a coordinate ({}) '
+                       'which does not describe a dimension.')
+                msg = msg.format(coord.name())
+                raise ValueError(msg)
+            dim_to_slice.extend(dims)
+        except TypeError:
+            try:
+                # attempt to handle as dimension index
+                dim = int(ref)
+            except ValueError:
+                raise ValueError('{} Incompatible type {} for '
+                                 'slicing'.format(ref, type(ref)))
+            if dim < 0 or dim > cube.ndim:
+                msg = ('Requested an iterator over a dimension ({}) '
+                       'which does not exist.'.format(dim))
+                raise ValueError(msg)
+            dim_to_slice.append(dim)
+    dim_to_slice = np.unique(dim_to_slice)
+    if len(set(dim_to_slice)) != len(dim_to_slice):
+        msg = 'The requested coordinates are not orthogonal.'
+        raise ValueError(msg)
+    return dim_to_slice
+
+
+def get_associated_coords(cube, dimensions):
+    dims = []
+    dim_set = set()
+    for d in dimensions:
+        if d not in dim_set:
+            dims.append(d)
+            dim_set.add(d)
+    dim_coords = set(itertools.chain.from_iterable(
+        [cube.coords(contains_dimension=i, dim_coords=True)
+         for i in dims]
+    ))
+    aux_coords = set(itertools.chain.from_iterable(
+        [cube.coords(contains_dimension=i, dim_coords=False)
+         for i in dims]
+    ))
+    return list(dim_coords), list(aux_coords)
+
+
+def get_empty_data(shape):
+    data = np.empty(shape)
+    mask = np.empty(shape, dtype=bool)
+    return np.ma.masked_array(data, mask)
+
+
+def get_slice_spec(cube, ref_to_slice):
+    slice_dims = ref_to_dims_index(cube, ref_to_slice)
+    slice_shape = tuple(cube.shape[d] for d in slice_dims)
+    dim_coords, aux_coords = get_associated_coords(cube, slice_dims)
+    return slice_shape, dim_coords, aux_coords
+
+
+def check_slice_spec(shape, dim_coords):
+    if shape is None:
+        shape = tuple(c.shape[0] for c in dim_coords)
+    if dim_coords is not None:
+        for s, c in zip(shape, dim_coords):
+            assert(s == c.shape[0])
+    return shape
+
+
+def index_iterator(dims_to_slice, shape):
+    dst_slices = (slice(None, None),)*len(dims_to_slice)
+    dims = [1 if n in dims_to_slice else i for n, i in enumerate(shape)]
+    for index_tuple in np.ndindex(*dims):
+        src_ind = tuple(
+            slice(None, None) if n in dims_to_slice else i
+            for n, i in enumerate(index_tuple)
+        )
+        dst_ind = tuple(i for n, i in enumerate(index_tuple)
+                        if n not in dims_to_slice) + dst_slices
+        yield src_ind, dst_ind
+
+
+def map_slices(src, func, ref_to_slice,
+               sub_shape=None, sub_dim_coords=None, sub_aux_coords=None):
+    src_slice_dims = ref_to_dims_index(src, ref_to_slice)
+    src_keep_dims = list(set(range(src.ndim))-set(src_slice_dims))
+    src_keep_spec = get_slice_spec(src, src_keep_dims)
+    sub_shape = check_slice_spec(sub_shape, sub_dim_coords)
+    res_shape = src_keep_spec[0]+sub_shape
+    dim_coords = src_keep_spec[1]+sub_dim_coords
+    dim_coords_and_dims = [(c, i) for i, c in enumerate(dim_coords)]
+    dst = iris.cube.Cube(
+        data=get_empty_data(res_shape),
+        standard_name=src.standard_name,
+        long_name=src.long_name,
+        var_name=src.var_name,
+        units=src.units,
+        attributes=src.attributes,
+        cell_methods=src.cell_methods,
+        dim_coords_and_dims=dim_coords_and_dims,
+    )
+    for src_ind, dst_ind in index_iterator(src_slice_dims, src.shape):
+        res = func(src[src_ind])
+        dst.data[dst_ind] = res
+    return dst
--- esmvaltool/preprocessor/_regrid_esmpy.py
+++ esmvaltool/preprocessor/_regrid_esmpy.py
@@ -1,12 +1,12 @@
+#!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
 import ESMF
-ESMF_MANAGER = ESMF.Manager(debug=False)
-import iris  # noqa
-import numpy as np  # noqa
-from scipy.interpolate import NearestNDInterpolator  # noqa
-from scipy.ndimage.morphology import binary_closing  # noqa
+import iris
+from ._mapping import ref_to_dims_index, get_slice_spec, map_slices
+import numpy as np
 
+ESMF_MANAGER = ESMF.Manager(debug=False)
 
 ESMF_LON, ESMF_LAT = 0, 1
 
@@ -62,7 +62,8 @@ def coords_iris_to_esmpy(lat, lon, circular):
     return esmpy_lat, esmpy_lon, esmpy_lat_corners, esmpy_lon_corners
 
 
-def get_grid(esmpy_lat, esmpy_lon, esmpy_lat_corners, esmpy_lon_corners, circular):
+def get_grid(esmpy_lat, esmpy_lon,
+             esmpy_lat_corners, esmpy_lon_corners, circular):
     if circular:
         num_peri_dims = 1
     else:
@@ -72,24 +73,27 @@ def get_grid(esmpy_lat, esmpy_lon, esmpy_lat_corners, esmpy_lon_corners, circula
     grid.get_coords(ESMF_LON)[...] = esmpy_lon
     grid.get_coords(ESMF_LAT)[...] = esmpy_lat
     grid.add_coords([ESMF.StaggerLoc.CORNER])
-    gridLonCorner = grid.get_coords(ESMF_LON, staggerloc=ESMF.StaggerLoc.CORNER)
-    gridLatCorner = grid.get_coords(ESMF_LAT, staggerloc=ESMF.StaggerLoc.CORNER)
-    gridLonCorner[...] = esmpy_lon_corners
-    gridLatCorner[...] = esmpy_lat_corners
+    grid_lon_corners = grid.get_coords(ESMF_LON,
+                                       staggerloc=ESMF.StaggerLoc.CORNER)
+    grid_lat_corners = grid.get_coords(ESMF_LAT,
+                                       staggerloc=ESMF.StaggerLoc.CORNER)
+    grid_lon_corners[...] = esmpy_lon_corners
+    grid_lat_corners[...] = esmpy_lat_corners
+    if grid.mask[0] is None:
+        grid.add_item(ESMF.GridItem.MASK, ESMF.StaggerLoc.CENTER)
     return grid
 
 
-def get_field(cube, grid):
+def get_empty_field(cube, grid, remove_mask=False):
     field = ESMF.Field(grid, name=cube.long_name,
                        staggerloc=ESMF.StaggerLoc.CENTER)
-    if grid.mask[0] is None:
-        center_mask = grid.add_item(ESMF.GridItem.MASK, ESMF.StaggerLoc.CENTER)
-    else:
-        center_mask = grid.get_item(ESMF.GridItem.MASK, ESMF.StaggerLoc.CENTER)
+    center_mask = grid.get_item(ESMF.GridItem.MASK, ESMF.StaggerLoc.CENTER)
     if np.ma.isMaskedArray(cube.data):
         field.data[...] = cube.data.data.T
-        mask = cube.data.mask.T
-        center_mask[...] = mask
+        if remove_mask:
+            center_mask[...] = 0
+        else:
+            center_mask[...] = cube.data.mask.T
     else:
         field.data[...] = cube.data.T
         center_mask[...] = 0
@@ -120,62 +124,52 @@ def is_lon_circular(lat, lon):
     return circular
 
 
-def cube_to_field(cube, circular_lon=None):
+def cube_to_empty_field(cube, circular_lon=None, remove_mask=False):
     lat = cube.coord('latitude')
     lon = cube.coord('longitude')
     if circular_lon is None:
         circular = is_lon_circular(lat, lon)
     else:
         circular = circular_lon
-    try:
-        esmpy_coords = coords_iris_to_esmpy(lat, lon, circular)
-    except:
-        print(cube)
-        raise
+    esmpy_coords = coords_iris_to_esmpy(lat, lon, circular)
     grid = get_grid(*esmpy_coords, circular)
-    field = get_field(cube, grid)
+    field = get_empty_field(cube, grid, remove_mask)
     return field
 
 
-def build_regridded_cube(src_cube, dst_cube, regridded_field):
-    regridded_cube = dst_cube.copy()
-    regridded_cube.metadata = src_cube.metadata
-    regridded_cube.data.data[...] = regridded_field.data[...].T
-    regridded_cube.data.mask[...] = regridded_field.grid.mask[0][...].T
-    for ac in src_cube.aux_coords:
-        if len(src_cube.coord_dims(ac)) == 0:
-            regridded_cube.add_aux_coord(ac)
-    return regridded_cube
+def get_representant(cube, ref_to_slice):
+    slice_dims = ref_to_dims_index(cube, ref_to_slice)
+    rep_ind = [0]*cube.ndim
+    for d in slice_dims:
+        rep_ind[d] = slice(None, None)
+    rep_ind = tuple(rep_ind)
+    return cube[rep_ind]
 
 
-def prepare_fields(src_field, dst_field, regrid_method):
-    src_mask_field = ESMF.Field(src_field.grid)
-    src_mask = src_field.grid.get_item(ESMF.GridItem.MASK)
-    src_mask_field.data[...] = src_mask[...]
-    dst_field.data[...] = 1
-    regridder = ESMF.Regrid(src_mask_field, dst_field,
-                            regrid_method=regrid_method,
-                            src_mask_values=np.array([-1]),
-                            dst_mask_values=np.array([-1]),
-                            unmapped_action=ESMF.UnmappedAction.IGNORE,
-                            ignore_degenerate=True)
-    regridded_field = regridder(src_mask_field, dst_field)
-    mask = regridded_field.data[...].copy()
-    closed_mask = binary_closing(mask)
-    closed_mask[0, :] = mask[0, :]
-    closed_mask[:, -1] = mask[:, -1]
-    closed_mask[-1, :] = mask[-1, :]
-    closed_mask[:, 0] = mask[:, 0]
-    mask = closed_mask
-    mask[mask > 0.] = 1
-    mask = mask.astype(int)
-    dst_field.grid.get_item(ESMF.GridItem.MASK)[...] = mask
-    src_mask_values = np.array([1])
-    return src_mask_values
+def build_regridder(src_rep, dst_rep, method):
+    regrid_method = ESMF_REGRID_METHODS[method]
+    src_field = cube_to_empty_field(src_rep)
+    dst_field = cube_to_empty_field(dst_rep, remove_mask=True)
+    esmf_regridder = ESMF.Regrid(src_field, dst_field,
+                                 regrid_method=regrid_method,
+                                 src_mask_values=np.array([1]),
+                                 unmapped_action=ESMF.UnmappedAction.IGNORE,
+                                 ignore_degenerate=True)
+
+    def regridder(src):
+        src_field.data[...] = src.data.data.T
+        regr_field = esmf_regridder(src_field, dst_field)
+        data = regr_field.data[...].T
+        res = np.ma.masked_array(data, data == 0.)
+        return res
+    return regridder
 
 
-def regrid_esmpy(src_cube, dst_cube,
-                 regrid_method='linear'):
+def correct_metadata(cube):
+    pass
+
+
+def regrid(src, dst, method='linear'):
     """
     Regrids src_cube to the grid defined by dst_cube.
 
@@ -195,47 +189,16 @@ def regrid_esmpy(src_cube, dst_cube,
         Can be 'linear', 'area_weighted',
         or 'nearest'. See ESMPy_
 
-    .. _ESMPy: http://www.earthsystemmodeling.org/esmf_releases/non_public/ESMF_7_0_0/esmpy_doc/html/RegridMethod.html#ESMF.api.constants.RegridMethod
+    .. _ESMPy:
+    http://www.earthsystemmodeling.org/esmf_releases/non_public/ESMF_7_0_0/
+    esmpy_doc/html/RegridMethod.html#ESMF.api.constants.RegridMethod
 
     """
-    regrid_method = ESMF_REGRID_METHODS[regrid_method]
-    src_field = cube_to_field(src_cube)
-    dst_field = cube_to_field(dst_cube)
-    src_mask_values = prepare_fields(src_field, dst_field, regrid_method)
-    regridder = ESMF.Regrid(src_field, dst_field, regrid_method=regrid_method,
-                            src_mask_values=src_mask_values,
-                            dst_mask_values=np.array([1]),
-                            unmapped_action=ESMF.UnmappedAction.IGNORE,
-                            ignore_degenerate=True)
-    regridded_field = regridder(src_field, dst_field)
-    regridded_cube = build_regridded_cube(src_cube, dst_cube, regridded_field)
-    return regridded_cube
-
-
-def regrid(src_cube, dst_cube, method):
-    regrid_method = ESMF_REGRID_METHODS[method]
-    src_representant = src_cube[0]
-    dst_representant = dst_cube[0]
-    src_field = cube_to_field(src_representant)
-    dst_field = cube_to_field(dst_representant)
-    src_mask_values = prepare_fields(src_field, dst_field, regrid_method)
-    regridder = ESMF.Regrid(src_field, dst_field, regrid_method=regrid_method,
-                            src_mask_values=src_mask_values,
-                            dst_mask_values=np.array([1]),
-                            unmapped_action=ESMF.UnmappedAction.IGNORE,
-                            ignore_degenerate=True)
-
-    def do_regrid(src):
-        src_field = cube_to_field(src)
-        regridded_field = regridder(src_field, dst_field)
-        regridded_cube = build_regridded_cube(src,
-                                              dst_representant,
-                                              regridded_field)
-        return regridded_cube
-    lat_dims = src_cube.coord_dims('latitude')
-    lon_dims = src_cube.coord_dims('longitude')
-    slice_dims = set(lat_dims + lon_dims)
-    cl = iris.cube.CubeList([do_regrid(src)
-                             for src in src_cube.slices(slice_dims)])
-    print(cl)
-    return cl.merge_cube()
+    ref_to_slice = ['latitude', 'longitude']
+    src_rep = get_representant(src, ref_to_slice)
+    dst_rep = get_representant(dst, ref_to_slice)
+    regridder = build_regridder(src_rep, dst_rep, method)
+    dst_slice_spec = get_slice_spec(dst, ref_to_slice)
+    res = map_slices(src, regridder, ref_to_slice, *dst_slice_spec)
+    correct_metadata(res)
+    return res
