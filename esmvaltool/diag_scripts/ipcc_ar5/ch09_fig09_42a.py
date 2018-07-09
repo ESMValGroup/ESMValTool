22154790a22f32a3606da3442f9ebd40057d9547
Manuel Schlund <manuel.schlund@dlr.de>
2018-07-09 16:36:13 +0200

    Modified structure of general python diagnostic classes

--- esmvaltool/diag_scripts/ipcc_ar5/ch09_fig09_42a.py
+++ esmvaltool/diag_scripts/ipcc_ar5/ch09_fig09_42a.py
@@ -34,8 +34,6 @@ import os
 from datetime import datetime
 
 import iris
-import numpy as np
-from scipy import stats
 
 import esmvaltool.diag_scripts.shared as e
 import esmvaltool.diag_scripts.shared.names as n
@@ -43,90 +41,7 @@ import esmvaltool.diag_scripts.shared.names as n
 logger = logging.getLogger(os.path.basename(__file__))
 
 
-def calculate_ecs(cfg, datasets, variables):
-    """Calculate ECS (Andrews et al. 2012)."""
-    for dataset_path in \
-            datasets.get_path_list(short_name=variables.tas, exp=DIFF):
-        dataset = datasets.get_info(n.DATASET, dataset_path)
-        data_tas = datasets.get_data(dataset_path)
-        data_rtmt = datasets.get_data(short_name=variables.rtmt, exp=DIFF,
-                                      dataset=dataset)
-        reg = stats.linregress(data_tas, data_rtmt)
-        datasets.add_dataset(variables.ecs + DIFF + dataset,
-                             -reg.intercept / (2 * reg.slope),
-                             short_name=variables.ecs, exp=DIFF,
-                             dataset=dataset)
-
-        # Plot ECS regression if desired
-        if not (cfg[n.WRITE_PLOTS] and cfg.get('plot_ecs_regression')):
-            continue
-        filepath = os.path.join(cfg[n.PLOT_DIR],
-                                dataset + '.' + cfg[n.OUTPUT_FILE_TYPE])
-
-        # Regression line
-        x_reg = np.linspace(-1.0, 8.0, 2)
-        y_reg = reg.slope * x_reg + reg.intercept
-
-        # Plot data
-        text = 'r = {:.2f}, '.format(reg.rvalue) + \
-               r'$\alpha$ = {:.2f}, '.format(-reg.slope) + \
-               'F = {:.2f}, '.format(reg.intercept) + \
-               'ECS = {:.2f}'.format(-reg.intercept / (2 * reg.slope))
-        e.plot.scatterplot(
-            [data_tas, x_reg],
-            [data_rtmt, y_reg],
-            filepath,
-            plot_kwargs=[{'linestyle': 'none',
-                          'markeredgecolor': 'b',
-                          'markerfacecolor': 'none',
-                          'marker': 's'},
-                         {'color': 'k',
-                          'linestyle': '-'}],
-            save_kwargs=cfg.get('save'),
-            axes_functions={'set_title': dataset,
-                            'set_xlabel': (variables.tas + " / " +
-                                           variables.TAS.units),
-                            'set_ylabel': (variables.rtmt + " / " +
-                                           variables.RTMT.units),
-                            'set_xlim': [0.0, 7.0],
-                            'set_ylim': [-2.0, 10.0],
-                            'text': {'args': [0.05, 0.9, text],
-                                     'kwargs': {'transform': 'transAxes'}}})
-
-        # Write netcdf file for every plot
-        if not cfg[n.WRITE_NETCDF]:
-            continue
-        tas_coord = iris.coords.AuxCoord(
-            data_tas,
-            standard_name=variables.TAS.standard_name,
-            long_name=variables.TAS.long_name,
-            var_name=variables.TAS.short_name,
-            units=variables.TAS.units)
-        attr = {'model': dataset,
-                'regression_r_value': reg.rvalue,
-                'regression_slope': reg.slope,
-                'regression_interception': reg.intercept,
-                'climate_sensitivity': -reg.slope,
-                'ECS': -reg.intercept / (2 * reg.slope),
-                'created_by': 'ESMValTool version {}'.format(cfg[n.VERSION]) +
-                              ', diagnostic {}'.format(cfg[n.SCRIPT]),
-                'creation_date': datetime.utcnow().isoformat(' ') + 'UTC'}
-        cube = iris.cube.Cube(data_rtmt,
-                              standard_name=variables.RTMT.standard_name,
-                              long_name=variables.RTMT.long_name,
-                              var_name=variables.RTMT.short_name,
-                              units=variables.RTMT.units,
-                              attributes=attr,
-                              aux_coords_and_dims=[(tas_coord, 0)])
-
-        # Save file
-        filepath = os.path.join(cfg[n.WORK_DIR],
-                                'ecs_regression_' + dataset + '.nc')
-        iris.save(cube, filepath)
-        logger.info("Writing %s", filepath)
-
-
-def plot_data(cfg, datasets, variables):
+def plot_data(cfg, datasets):
     """Plot data."""
     if not cfg[n.WRITE_PLOTS]:
         return
@@ -137,14 +52,12 @@ def plot_data(cfg, datasets, variables):
     y_data = []
     dataset_names = []
     plot_kwargs = []
-    names = datasets.get_info_list(n.DATASET, short_name=variables.ecs,
-                                   exp=DIFF)
-    ecs_data = datasets.get_data_list(short_name=variables.ecs, exp=DIFF)
+    names = datasets.get_info_list(n.DATASET, short_name='ecs', exp=DIFF)
+    ecs_data = datasets.get_data_list(short_name='ecs', exp=DIFF)
 
     # Historical
     x_data.extend(ecs_data)
-    y_data.extend(datasets.get_data_list(short_name=variables.tas,
-                                         exp=HISTORICAL))
+    y_data.extend(datasets.get_data_list(short_name='tas', exp=HISTORICAL))
     dataset_names.extend(names)
     for name in names:
         plot_kwargs.append({'label': name, 'linestyle': 'none',
@@ -152,7 +65,7 @@ def plot_data(cfg, datasets, variables):
 
     # piControl
     x_data.extend(ecs_data)
-    y_data.extend(datasets.get_data_list(short_name=variables.tas,
+    y_data.extend(datasets.get_data_list(short_name='tas',
                                          exp=PICONTROL_TEMP_MEAN))
     dataset_names.extend(names)
     for name in names:
@@ -174,51 +87,48 @@ def plot_data(cfg, datasets, variables):
 def write_data(cfg, datasets, variables):
     """Write netcdf file."""
     if cfg[n.WRITE_PLOTS]:
-        data_ecs = datasets.get_data_list(short_name=variables.ecs, exp=DIFF)
-        data_tas_hist = datasets.get_data_list(short_name=variables.tas,
+        data_ecs = datasets.get_data_list(short_name='ecs', exp=DIFF)
+        data_tas_hist = datasets.get_data_list(short_name='tas',
                                                exp=HISTORICAL)
-        data_tas_picontrol = datasets.get_data_list(short_name=variables.tas,
+        data_tas_picontrol = datasets.get_data_list(short_name='tas',
                                                     exp=PICONTROL_TEMP_MEAN)
-        models = datasets.get_info_list(n.DATASET, short_name=variables.ecs,
-                                        exp=DIFF)
+        models = datasets.get_info_list(n.DATASET, short_name='ecs', exp=DIFF)
         dataset_coord = iris.coords.AuxCoord(models, long_name='models')
         tas_hist_coord = iris.coords.AuxCoord(
             data_tas_hist,
-            standard_name=variables.TAS.standard_name,
-            long_name=variables.TAS.long_name,
-            var_name=variables.TAS.short_name + '_' + HISTORICAL,
-            units=variables.TAS.units,
+            **variables.iris_dict('tas'),
             attributes={'experiment': HISTORICAL})
         tas_picontrol_coord = iris.coords.AuxCoord(
             data_tas_picontrol,
-            standard_name=variables.TAS.standard_name,
-            long_name=variables.TAS.long_name,
-            var_name=variables.TAS.short_name + '_' + PICONTROL,
-            units=variables.TAS.units,
+            **variables.iris_dict('tas'),
             attributes={'experiment': PICONTROL})
         attr = {'created_by': 'ESMValTool version {}'.format(cfg[n.VERSION]) +
                               ', diagnostic {}'.format(cfg[n.SCRIPT]),
                 'creation_date': datetime.utcnow().isoformat(' ') + 'UTC'}
-        cube = iris.cube.Cube(data_ecs, long_name=variables.ECS.long_name,
-                              var_name=variables.ecs,
-                              units=variables.ECS.units,
+        cube = iris.cube.Cube(data_ecs, long_name=variables.long_name('ecs'),
+                              var_name='ecs', units=variables.units('ecs'),
                               aux_coords_and_dims=[(dataset_coord, 0),
                                                    (tas_hist_coord, 0),
                                                    (tas_picontrol_coord, 0)],
                               attributes=attr)
 
         # Save file
-        filepath = os.path.join(cfg[n.WORK_DIR], variables.ecs + '.nc')
+        filepath = os.path.join(cfg[n.WORK_DIR],
+                                cfg.get('output_name', 'fig09_42a') + '.nc')
         iris.save(cube, filepath)
         logger.info("Writing %s", filepath)
 
 
-###########################################################################
+###############################################################################
 # Setup diagnostic
-###########################################################################
+###############################################################################
 
 # Variables
-ECS = e.Variable('ecs', 'ecs', 'equilibrium climate sensitivity', 'K')
+ECS = e.Variable('ecs',
+                 'equilibrium_climate_sensitivity',
+                 'Change in global mean surface temperature at equilibrium '
+                 'caused by a doubling of the atmospheric CO2 concentration',
+                 'K')
 
 # Experiments
 PICONTROL = 'piControl'
@@ -258,48 +168,18 @@ def main(cfg):
     for dataset_path in data:
         cube = iris.load(dataset_path, var.standard_names())[0]
 
-        # Global mean
-        for coord in [cube.coord(n.LAT), cube.coord(n.LON)]:
-            if not coord.has_bounds():
-                coord.guess_bounds()
-        area_weights = iris.analysis.cartography.area_weights(cube)
-        cube = cube.collapsed([n.LAT, n.LON], iris.analysis.MEAN,
-                              weights=area_weights)
-
-        # Historical: total temporal mean; else: annual mean
-        if data.get_info(n.EXP, dataset_path) == HISTORICAL:
-            cube = cube.collapsed([n.TIME], iris.analysis.MEAN)
-        else:
-            cube = cube.aggregated_by(n.YEAR, iris.analysis.MEAN)
+        # Temporal means
+        cube = cube.collapsed([n.TIME], iris.analysis.MEAN)
 
         data.set_data(cube.data, dataset_path)
 
-    ###########################################################################
-    # Process data
-    ###########################################################################
-
-    # Substract piControl experiment from abrupt4xCO2 experiment and add total
-    # temporal mean of piControl
-    for dataset_path in data.get_path_list(exp=PICONTROL):
-        dataset = data.get_info(n.DATASET, dataset_path)
-        short_name = data.get_info(n.SHORT_NAME, dataset_path)
-        new_data = data.get_data(dataset_path)
-        data_diff = data.get_data(short_name=short_name, exp=ABRUPT4XCO2,
-                                  dataset=dataset) - new_data
-        data.add_dataset(short_name + DIFF + dataset, data_diff,
-                         short_name=short_name, exp=DIFF, dataset=dataset)
-        data.add_dataset(short_name + PICONTROL_TEMP_MEAN + dataset,
-                         np.mean(new_data), short_name=short_name,
-                         exp=PICONTROL_TEMP_MEAN, dataset=dataset)
-
-    # Calculate ECS (cf. Andrews et al. 2015)
-    calculate_ecs(cfg, data, var)
+    # TODO: Read ECS data
 
     ###########################################################################
     # Plot data
     ###########################################################################
 
-    plot_data(cfg, data, var)
+    plot_data(cfg, data)
 
     ###########################################################################
     # Write nc file
