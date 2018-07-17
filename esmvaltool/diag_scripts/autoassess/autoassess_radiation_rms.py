33efacb1031f9dd586aa97e3f1b46e5089016055
Valeriu Predoi <valeriu.predoi@gmail.com>
2018-07-17 17:08:30 +0100

    working version of diagnostic with UNderived variables; need to implement the variable derivation for the vars that need to be derived and are currently commented out in the recipe

--- esmvaltool/diag_scripts/autoassess/autoassess_radiation_rms.py
+++ esmvaltool/diag_scripts/autoassess/autoassess_radiation_rms.py
@@ -1,14 +1,18 @@
 """
-;; Original Description from Version 1 Diagnostic
-;; Ported to Version 2 with implementation of v2-specific changes
-;; Valeriu Predoi, UREAD, July 2018
+Port to Version 2 with implementation of v2-specific changes
+
+Valeriu Predoi, UREAD, July 2018
+
+Porting replicates the functionality to minimum errors.
+
+Original Description from Version 1 Diagnostic:
 ;;###########################################################################
 ;; AutoAssess_radiation_rms.py
 ;; Author: Yoko Tsushima (Met Office, UK)
 ;; CMUG project
 ;;###########################################################################
 ;; Description
-;;    This script is the RMS error metric script of 
+;;    This script is the RMS error metric script of
 ;;    AutoAssess radiation
 ;;
 ;; Required diag_script_info attributes (diagnostics specific)
@@ -125,23 +129,33 @@
 
 import os
 import logging
-import sys
-import numpy as np
 import iris
 from esmvaltool.diag_scripts.autoassess.autoassess_source import rms
-from esmvaltool.diag_scripts.autoassess.autoassess_source import valmod_radiation as vm
+from esmvaltool.diag_scripts.autoassess.autoassess_source \
+    import valmod_radiation as vm
 from esmvaltool.diag_scripts.shared import (group_metadata, run_diagnostic,
-                                            select_metadata, sorted_metadata)
+                                            select_metadata)
 
 logger = logging.getLogger(os.path.basename(__file__))
 
-
 # dict of model var-to-obs name
-VAR_DICT = {'pr': 'MPI-ESM-MR',
-            'rlut': 'MPI-ESM-MR'}
+# replace test MPI-ESM-MR with actual OBS datasets
+VAR_DICT = {
+    'pr': 'MPI-ESM-MR',
+    'rlut': 'MPI-ESM-MR',
+    'rsut': 'MPI-ESM-MR',
+    'rsdt': 'MPI-ESM-MR',  # used for derivation only
+    'rsutcs': 'MPI-ESM-MR',
+    'rlutcs': 'MPI-ESM-MR',
+    'rsds': 'MPI-ESM-MR',  # used for derivation only
+    'rsus': 'MPI-ESM-MR',  # used for derivation only
+    'prw': 'MPI-ESM-MR',
+    'rldscs': 'MPI-ESM-MR',
+    'rlus': 'MPI-ESM-MR'
+}  # used for derivation only
 
 
-def apply_supermeans(cfg, ctrl, exper, obs):
+def apply_supermeans(ctrl, exper, obs):
     """Grab data and apply supermeans"""
     ctrl_file = ctrl['filename']
     exper_file = exper['filename']
@@ -172,15 +186,14 @@ def apply_rms(data_1, data_2, cfg, component_dict, var_name):
     rms_list = rms.start(data_names[0], data_names[1])
     filename = var_name + '_' + data_names[0] + '_vs_' + data_names[1]
     analysis_type = cfg['analysis_type']
-    landsea_mask_file = os.path.join(os.path.dirname(__file__),
-                                     'autoassess_source',
-                                     cfg['landsea_mask'])
+    landsea_mask_file = os.path.join(
+        os.path.dirname(__file__), 'autoassess_source', cfg['landsea_mask'])
     landsea_mask_cube = iris.load_cube(landsea_mask_file)
     data1_vs_data2 = vm.perform_equation(data_1, data_2, analysis_type)
 
-    # apply rms
-    rms_float = rms.calc_all(rms_list, data1_vs_data2, landsea_mask_cube,
-                             plot_title, filename)
+    # call to calc_all to compute rms
+    rms.calc_all(rms_list, data1_vs_data2, landsea_mask_cube,
+                 plot_title, filename)
     rms.end(rms_list, cfg['work_dir'])
 
 
@@ -199,12 +212,10 @@ def main(cfg):
     # select variables and their corresponding
     # obs files
     for short_name in grouped_input_data:
-        dataset_selection = select_metadata(input_data,
-                                            short_name=short_name,
-                                            project='CMIP5')
-        obs_selection = select_metadata(input_data,
-                                        short_name=short_name,
-                                        dataset=VAR_DICT[short_name])
+        dataset_selection = select_metadata(
+            input_data, short_name=short_name, project='CMIP5')
+        obs_selection = select_metadata(
+            input_data, short_name=short_name, dataset=VAR_DICT[short_name])
         logger.info("Processing variable %s", short_name)
 
         # determine CONTROL and EXPERIMENT datasets
@@ -226,35 +237,46 @@ def main(cfg):
             obs = None
 
         # apply the supermeans
-        ctrl_sm, exper_sm, obs_sm = apply_supermeans(cfg, ctrl, exper, obs)
+        ctrl_sm, exper_sm, obs_sm = apply_supermeans(ctrl, exper, obs)
 
         # assemble a dict that contains various params depending
         # on the data combinations for RMS computations
         if obs_sm:
-            data_component_dict = {'ct-ex': {'ctrl': ctrl, 'exper': exper},
-                                   'ct-obs': {'ctrl': ctrl, 'obs': obs},
-                                   'ex-obs': {'exper': exper, 'obs': obs}}
+            data_component_dict = {
+                'ct-ex': {
+                    'ctrl': ctrl,
+                    'exper': exper
+                },
+                'ct-obs': {
+                    'ctrl': ctrl,
+                    'obs': obs
+                },
+                'ex-obs': {
+                    'exper': exper,
+                    'obs': obs
+                }
+            }
         else:
             data_component_dict = {'ct-ex': {'ctrl': ctrl, 'exper': exper}}
 
         if obs_sm:
             # ctrl-exper
             logger.info("Computing CONTROL-EXPERIMENT RMS...")
-            apply_rms(ctrl_sm, exper_sm, cfg,
-                      data_component_dict['ct-ex'], short_name)
+            apply_rms(ctrl_sm, exper_sm, cfg, data_component_dict['ct-ex'],
+                      short_name)
             # ctrl-obs
             logger.info("Computing CONTROL-OBS RMS...")
-            apply_rms(ctrl_sm, obs_sm, cfg,
-                      data_component_dict['ct-obs'], short_name)
+            apply_rms(ctrl_sm, obs_sm, cfg, data_component_dict['ct-obs'],
+                      short_name)
             # exper-obs
             logger.info("Computing EXPERIMENT-OBS RMS...")
-            apply_rms(exper_sm, obs_sm, cfg,
-                      data_component_dict['ex-obs'], short_name)
+            apply_rms(exper_sm, obs_sm, cfg, data_component_dict['ex-obs'],
+                      short_name)
         else:
             # only ctrl-exper
             logger.info("Computing CONTROL-EXPERIMENT RMS...")
-            apply_rms(ctrl_sm, exper_sm, cfg,
-                      data_component_dict['ct-ex'], short_name)
+            apply_rms(ctrl_sm, exper_sm, cfg, data_component_dict['ct-ex'],
+                      short_name)
 
 
 if __name__ == '__main__':
