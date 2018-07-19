45c0edddd37fcb8ee5494cda4f968a2b6f1c66be
Javier Vegas-Regidor <javier.vegas@bsc.es>
2018-07-19 13:13:08 +0200

    Unified extract month and season tests

--- tests/unit/preprocessor/test_time_area.py
+++ tests/unit/preprocessor/test_time_area.py
@@ -1,4 +1,4 @@
-"""Unit tests for extract_season preprocessor function"""
+"""Unit tests for extract_month and season preprocessor function."""
 
 from __future__ import absolute_import, division, print_function
 
@@ -10,22 +10,42 @@ from cf_units import Unit
 import numpy as np
 
 import tests
-from esmvaltool.preprocessor._time_area import extract_season
+from esmvaltool.preprocessor._time_area import extract_month, extract_season
 
 
-class Test(tests.Test):
+def _create_sample_cube():
+    cube = Cube(np.arange(1, 25), var_name='co2', units='J')
+    cube.add_dim_coord(
+        iris.coords.DimCoord(np.arange(15., 720., 30.),
+                             standard_name='time',
+                             units=Unit('days since 1950-01-01',
+                                        calendar='gregorian')),
+        0)
+    iris.coord_categorisation.add_month_number(cube, 'time')
+    return cube
+
+
+class TestExtractMonth(tests.Test):
+    """Tests for extract_month`."""
+
+    def setUp(self):
+        """Prepare tests"""
+        self.cube = _create_sample_cube()
+
+    def test_get_january(self):
+        """Test january extraction"""
+        sliced = extract_month(self.cube, 1)
+        print(sliced)
+        self.assertTrue((np.array([1, 1]) ==
+                         sliced.coord('month_number').points).all())
+
+
+class TestExtractSeason(tests.Test):
     """Tests for extract_season"""
 
     def setUp(self):
         """Prepare tests"""
-        self.cube = Cube(np.arange(1, 25), var_name='co2', units='J')
-        self.cube.add_dim_coord(
-            iris.coords.DimCoord(
-                np.arange(15., 720., 30.),
-                standard_name='time',
-                units=Unit('days since 1950-01-01', calendar='gregorian')),
-            0)
-        iris.coord_categorisation.add_month_number(self.cube, 'time')
+        self.cube = _create_sample_cube()
 
     def test_get_djf(self):
         """Test function for winter"""
