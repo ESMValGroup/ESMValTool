94c05abdc7d7858202aa69f27e9dbfa60e2a3ea1
Valeriu Predoi <valeriu.predoi@gmail.com>
2018-07-23 17:23:21 +0100

    generalized OBS data handling basically solving the problems imposed by the other two autoassess recipes that need porting

--- esmvaltool/diag_scripts/autoassess/autoassess_radiation_rms.py
+++ esmvaltool/diag_scripts/autoassess/autoassess_radiation_rms.py
@@ -138,25 +138,30 @@ from esmvaltool.diag_scripts.shared import (group_metadata, run_diagnostic,
 
 logger = logging.getLogger(os.path.basename(__file__))
 
-# dict of model var-to-obs
-VAR_DICT = {
-    'pr': 'GPCP-SG',          # underived
-    'rlut': 'CERES-EBAF',     # underived
-    'rsut': 'CERES-EBAF',     # underived
-    'rsutcs': 'CERES-EBAF',   # underived
-    'rlutcs': 'JRA-55',       # underived
-    'prw': 'SSMI-MERIS',      # underived
-    'rldscs': 'JRA-55',       # underived
-    'rtnt': 'CERES-EBAF',     # derived
-    'rsnt': 'CERES-EBAF',     # derived
-    'rsns': 'CERES-EBAF',     # derived
-    'rlns': 'CERES-EBAF',     # derived
-    'lwcre': 'CERES-EBAF',    # derived
-    'swcre': 'CERES-EBAF'     # derived
-}
+
+def map_var_to_obs(cfg):
+    """Get the preferred OBS files for each var"""
+    if cfg['diagnostic_class'] == 'Amon_all':
+        var_dict = {
+            'pr': ['GPCP-SG'],  # underived
+            'rlut': ['CERES-EBAF'],  # underived
+            'rsut': ['CERES-EBAF'],  # underived
+            'rsutcs': ['CERES-EBAF'],  # underived
+            'rlutcs': ['JRA-55'],  # underived
+            'prw': ['SSMI-MERIS'],  # underived
+            'rldscs': ['JRA-55'],  # underived
+            'rtnt': ['CERES-EBAF'],  # derived
+            'rsnt': ['CERES-EBAF'],  # derived
+            'rsns': ['CERES-EBAF'],  # derived
+            'rlns': ['CERES-EBAF'],  # derived
+            'lwcre': ['CERES-EBAF'],  # derived
+            'swcre': ['CERES-EBAF']  # derived
+        }
+
+    return var_dict
 
 
-def apply_supermeans(ctrl, exper, obs):
+def apply_supermeans(ctrl, exper, obs_list):
     """Grab data and apply supermeans"""
     ctrl_file = ctrl['filename']
     exper_file = exper['filename']
@@ -168,16 +173,19 @@ def apply_supermeans(ctrl, exper, obs):
     logger.debug("Time-averaged control %s", ctrl_cube)
     exper_cube = exper_cube.collapsed('time', iris.analysis.MEAN)
     logger.debug("Time-averaged experiment %s", exper_cube)
-    if obs:
-        obs_file = obs['filename']
-        logger.info("Loading %s", obs_file)
-        obs_cube = iris.load_cube(obs_file)
-        obs_cube = obs_cube.collapsed('time', iris.analysis.MEAN)
-        logger.debug("Time-averaged obs %s", obs_cube)
+    if obs_list:
+        obs_cube_list = []
+        for obs in obs_list:
+            obs_file = obs['filename']
+            logger.info("Loading %s", obs_file)
+            obs_cube = iris.load_cube(obs_file)
+            obs_cube = obs_cube.collapsed('time', iris.analysis.MEAN)
+            logger.debug("Time-averaged obs %s", obs_cube)
+            obs_cube_list.append(obs_cube)
     else:
-        obs_cube = None
+        obs_cube_list = None
 
-    return ctrl_cube, exper_cube, obs_cube
+    return ctrl_cube, exper_cube, obs_cube_list
 
 
 def apply_rms(data_1, data_2, cfg, component_dict, var_name):
@@ -193,88 +201,100 @@ def apply_rms(data_1, data_2, cfg, component_dict, var_name):
     data1_vs_data2 = vm.perform_equation(data_1, data_2, analysis_type)
 
     # call to calc_all to compute rms
-    rms.calc_all(rms_list, data1_vs_data2, landsea_mask_cube,
-                 plot_title, filename)
+    rms.calc_all(rms_list, data1_vs_data2, landsea_mask_cube, plot_title,
+                 filename)
     rms.end(rms_list, cfg['work_dir'])
 
 
-def main(cfg):
-    """Execute the radiation rms diag"""
-    logger.setLevel(cfg['log_level'].upper())
+def do_preamble(cfg):
+    """Execute some preamble functionality"""
+    # prepare output dirs
     if not os.path.exists(cfg['plot_dir']):
         os.makedirs(cfg['plot_dir'])
     if not os.path.exists(cfg['work_dir']):
         os.makedirs(cfg['work_dir'])
 
+    # get data
     input_data = cfg['input_data'].values()
     grouped_input_data = group_metadata(
         input_data, 'short_name', sort='dataset')
+    return input_data, grouped_input_data
+
+
+def get_all_datasets(short_name, input_data, cfg):
+    """Get control, exper and obs datasets"""
+    var_dict = map_var_to_obs(cfg)
+    dataset_selection = select_metadata(
+        input_data, short_name=short_name, project='CMIP5')
+    obs_selection = [
+        select_metadata(input_data, short_name=short_name,
+                        dataset=var_to_obs)[0]
+        for var_to_obs in var_dict[short_name]
+    ]
+
+    # determine CONTROL and EXPERIMENT datasets
+    for model in dataset_selection:
+        if model['dataset'] == cfg['control_model']:
+            logger.info("Control dataset %s", model['dataset'])
+            control = model
+        elif model['dataset'] == cfg['exper_model']:
+            logger.info("Experiment dataset %s", model['dataset'])
+            experiment = model
+
+    if obs_selection:
+        logger.info("Observations dataset(s) %s",
+                    [obs['dataset'] for obs in obs_selection])
+
+    return control, experiment, obs_selection
+
+
+def main(cfg):
+    """Execute the radiation rms diag"""
+    logger.setLevel(cfg['log_level'].upper())
+    input_data, grouped_input_data = do_preamble(cfg)
 
     # select variables and their corresponding
     # obs files
     for short_name in grouped_input_data:
-        dataset_selection = select_metadata(
-            input_data, short_name=short_name, project='CMIP5')
-        obs_selection = select_metadata(
-            input_data, short_name=short_name, dataset=VAR_DICT[short_name])
         logger.info("Processing variable %s", short_name)
 
-        # determine CONTROL and EXPERIMENT datasets
-        for model in dataset_selection:
-            if model['dataset'] == cfg['control_model']:
-                logger.info("Control dataset %s", model['dataset'])
-                ctrl = model
-            elif model['dataset'] == cfg['exper_model']:
-                logger.info("Experiment dataset %s", model['dataset'])
-                exper = model
-
-        # determine OBS dataset
-        if obs_selection:
-            if len(obs_selection) > 1:
-                logger.error("This diag works with a single OBS dataset!")
-            obs = obs_selection[0]
-            logger.info("Observations dataset %s", obs['dataset'])
-        else:
-            obs = None
+        # control, experiment and obs's
+        ctrl, exper, obslist = get_all_datasets(short_name, input_data, cfg)
 
         # apply the supermeans
-        ctrl_sm, exper_sm, obs_sm = apply_supermeans(ctrl, exper, obs)
+        ctrl_sm, exper_sm, obs_sm_list = apply_supermeans(ctrl, exper, obslist)
 
         # assemble a dict that contains various params depending
         # on the data combinations for RMS computations
-        if obs_sm:
-            data_component_dict = {
-                'ct-ex': {
-                    'ctrl': ctrl,
-                    'exper': exper
-                },
-                'ct-obs': {
-                    'ctrl': ctrl,
-                    'obs': obs
-                },
-                'ex-obs': {
-                    'exper': exper,
-                    'obs': obs
+        # control-experiment
+        data_component_dict = {'ct-ex': {'ctrl': ctrl, 'exper': exper}}
+        logger.info("Computing CONTROL-EXPERIMENT RMS...")
+        apply_rms(ctrl_sm, exper_sm, cfg, data_component_dict['ct-ex'],
+                  short_name)
+        if obs_sm_list:
+            for obs, obsfile in zip(obs_sm_list, obslist):
+                data_component_dict = {
+                    'ct-obs': {
+                        'ctrl': ctrl,
+                        'obs': obsfile
+                    },
+                    'ex-obs': {
+                        'exper': exper,
+                        'obs': obsfile
+                    }
                 }
-            }
-        else:
-            data_component_dict = {'ct-ex': {'ctrl': ctrl, 'exper': exper}}
 
-        if obs_sm:
-            # ctrl-exper
-            logger.info("Computing CONTROL-EXPERIMENT RMS...")
-            apply_rms(ctrl_sm, exper_sm, cfg, data_component_dict['ct-ex'],
-                      short_name)
-            # ctrl-obs
-            logger.info("Computing CONTROL-OBS RMS...")
-            apply_rms(ctrl_sm, obs_sm, cfg, data_component_dict['ct-obs'],
-                      short_name)
-            # exper-obs
-            logger.info("Computing EXPERIMENT-OBS RMS...")
-            apply_rms(exper_sm, obs_sm, cfg, data_component_dict['ex-obs'],
-                      short_name)
+                # ctrl-obs
+                logger.info("Computing CONTROL-OBS RMS...")
+                apply_rms(ctrl_sm, obs, cfg, data_component_dict['ct-obs'],
+                          short_name)
+                # exper-obs
+                logger.info("Computing EXPERIMENT-OBS RMS...")
+                apply_rms(exper_sm, obs, cfg, data_component_dict['ex-obs'],
+                          short_name)
         else:
             # only ctrl-exper
+            data_component_dict = {'ct-ex': {'ctrl': ctrl, 'exper': exper}}
             logger.info("Computing CONTROL-EXPERIMENT RMS...")
             apply_rms(ctrl_sm, exper_sm, cfg, data_component_dict['ct-ex'],
                       short_name)
