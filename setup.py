e5f3c3fd44d71f5caf60bafed25bbfa46a274ed8
Bouwe Andela <b.andela@esciencecenter.nl>
2018-07-30 12:35:51 +0200

    Fix whitespace

--- setup.py
+++ setup.py
@@ -33,7 +33,7 @@ REQUIREMENTS = {
         'cdo',
         'cf_units',
         'cython',
-        # 'scitools-iris', # Only iris 2 is on PyPI  
+        # 'scitools-iris',  # Only iris 2 is on PyPI
         'matplotlib',
         'netCDF4',
         'numba',
