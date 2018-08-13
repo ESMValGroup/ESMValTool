40e3fccbec62d2d98ed861d6fa16b6665e93af90
Mattia Righi <mattia.righi@dlr.de>
2018-08-13 18:05:54 +0200

    Change tests accordingly

--- tests/integration/cmor/_fixes/CMIP5/test_CESM1_BGC.py
+++ tests/integration/cmor/_fixes/CMIP5/test_CESM1_BGC.py
@@ -11,36 +11,6 @@ from iris.cube import Cube
 from esmvaltool.cmor._fixes.CMIP5.CESM1_BGC import allvars, co2, nbp
 
 
-class TestAll(unittest.TestCase):
-    def setUp(self):
-        self.cube = Cube([1, 2], var_name='co2', units='J')
-        self.cube.add_dim_coord(
-            DimCoord(
-                [0, 1],
-                standard_name='time',
-                units=Unit(
-                    'days since 0001-01-01 00:00:00.0000000 UTC',
-                    calendar='gregorian')),
-            0)
-        self.fix = allvars()
-
-    def test_fix_metadata(self):
-        cube = self.fix.fix_metadata(self.cube)
-
-        time = cube.coord('time')
-        self.assertEqual(time.units.origin, 'days since 1850-01-01 00:00:00')
-        self.assertEqual(time.units.calendar, 'gregorian')
-
-    def test_fix_metadata_good_units(self):
-        self.cube.coord('time').units = Unit('days since 1950-01-01 00:00:00',
-                                             calendar='gregorian')
-        cube = self.fix.fix_metadata(self.cube)
-
-        time = cube.coord('time')
-        self.assertEqual(time.units.origin, 'days since 1950-01-01 00:00:00')
-        self.assertEqual(time.units.calendar, 'gregorian')
-
-
 class TestCo2(unittest.TestCase):
     def setUp(self):
         self.cube = Cube([1], var_name='co2', units='J')
