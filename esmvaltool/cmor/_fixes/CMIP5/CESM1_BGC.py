d44c93650be573e81519244eb135cd01ab3675fa
Mattia Righi <mattia.righi@dlr.de>
2018-08-13 17:54:34 +0200

    Removed unused fixes in time coordinate

--- esmvaltool/cmor/_fixes/CMIP5/CESM1_BGC.py
+++ esmvaltool/cmor/_fixes/CMIP5/CESM1_BGC.py
@@ -84,28 +84,3 @@ class co2(Fix):
         cube *= 28.966 / 44.0
         cube.metadata = metadata
         return cube
-
-
-class allvars(Fix):
-    """Fixes common to all vars"""
-
-    def fix_metadata(self, cube):
-        """
-        Fix metadata
-
-        Fixes time units
-
-        Parameters
-        ----------
-        cube: iris.cube.Cube
-
-        Returns
-        -------
-        iris.cube.Cube
-
-        """
-        time = cube.coord('time')
-        if time.units.name == 'day since 1-01-01 00:00:00.000000 UTC':
-            time.units = Unit('days since 1850-01-01 00:00:00',
-                              time.units.calendar)
-        return cube
