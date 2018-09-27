dd729e4255da6ac08cd36174fe60f7b04efd6c61
Lee de Mora <ledm@pml.ac.uk>
2018-09-27 13:53:14 +0100

    Added new cmip5 fixes for o2.

--- esmvaltool/cmor/_fixes/CMIP5/BNU_ESM.py
+++ esmvaltool/cmor/_fixes/CMIP5/BNU_ESM.py
@@ -1,4 +1,5 @@
 """Fixes for BNU ESM model"""
+import iris
 from cf_units import Unit
 
 from ..fix import Fix
@@ -153,6 +154,61 @@ class spco2(Fix):
         cube.metadata = metadata
         return cube
 
+
+class o2(Fix):
+    """Fixes for o2"""
+
+    def fix_metadata(self, cube):
+        """
+        Fix metadata
+
+        Fixes cube units
+
+        Parameters
+        ----------
+        cube: iris.cube.Cube
+
+        Returns
+        -------
+        iris.cube.Cube
+
+        """
+        depth = cube.coord('generic ocean level')
+        depth.standard_name = 'depth'
+        depth.long_name = 'depth'
+        return cube
+
+    def fix_file(self, filepath, output_dir):
+        """
+        Apply fixes to the files prior to creating the cube.
+
+        Should be used only to fix errors that prevent loading or can
+        not be fixed in the cube (i.e. those related with missing_value
+        and _FillValue or missing standard_name).
+        Parameters
+        ----------
+        filepath: basestring
+            file to fix.
+        output_dir: basestring
+            path to the folder to store the fix files, if required.
+        Returns
+        -------
+        basestring
+            Path to the corrected file. It can be different from the original
+            filepath if a fix has been applied, but if not it should be the
+            original filepath.
+        """
+        new_path = Fix.get_fixed_filepath(output_dir, filepath)
+        cube = iris.load_cube(filepath)
+
+        std = 'mole_concentration_of_dissolved_molecular_oxygen_in_sea_water'
+        long_name = 'Dissolved Oxygen Concentration'
+
+        cube.long_name = long_name
+        cube.standard_name = std
+
+        iris.save(cube, new_path)
+        return new_path
 
 # No clear way to apply this fix now that we are working with cubes, not files
 
