fc6dbac229c936ebf3f3d7e0d365fdfd25658de2
peter.berg <a001850@c20834.ad.smhi.se>
2018-10-01 16:46:17 +0200

    updated recipes

--- esmvaltool/diag_scripts/droughtindex/diag_spei.r
+++ esmvaltool/diag_scripts/droughtindex/diag_spei.r
@@ -1,4 +1,5 @@
 library(yaml)
+library(ncdf4)
 
 #Parsing input file paths and creating output dirs
 args <- commandArgs(trailingOnly = TRUE)
@@ -10,19 +11,14 @@ work_dir <- params$work_dir
 #dir.create(plot_dir, recursive = TRUE)
 #dir.create(run_dir, recursive = TRUE)
 #dir.create(work_dir, recursive = TRUE)
-print(plot_dir)
-print(run_dir)
-print(work_dir)
 
-print(params)
-names(params)
 
 input_files_per_var <- yaml::read_yaml(params$input_files)
-print(input_files_per_var)
-
 var_names <- names(input_files_per_var)
 model_names <- lapply(input_files_per_var, function(x) x$model)
 model_names <- unname(model_names)
+print(model_names)
+
 var0 <- lapply(input_files_per_var, function(x) x$short_name)
 fullpath_filenames <- names(var0)
 var0 <- unname(var0)[1]
@@ -30,10 +26,16 @@ experiment <- lapply(input_files_per_var, function(x) x$exp)
 experiment <- unlist(unname(experiment))
 rcp_scenario <- experiment
 
-model_names <-  lapply(input_files_per_var, function(x) x$model)
-model_names <- unlist(unname(model_names))
+#model_names <-  lapply(input_files_per_var, function(x) x$model)
+#model_names <- unlist(unname(model_names))
 
 start_year <- lapply(input_files_per_var, function(x) x$start_year)
 start_year <- c(unlist(unname(start_year)))[1]
 end_year <- lapply(input_files_per_var, function(x) x$end_year)
 end_year <- c(unlist(unname(end_year)))[1]
+
+print(fullpath_filenames)
+# Load data
+#precip <- ncvar_get(nc_open(fullpath_filenames),var=var0)
+#temp <- ncvar_get(nc_open(fullpath_filenames),var=var0)
+
