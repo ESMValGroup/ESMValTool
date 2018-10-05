5a6a42846fb98f00176bfc9ce7df43d0d0df12dd
Bouwe Andela <b.andela@esciencecenter.nl>
2018-10-05 15:23:12 +0200

    Add provenance to correlate example recipe

--- esmvaltool/diag_scripts/examples/correlate.py
+++ esmvaltool/diag_scripts/examples/correlate.py
@@ -6,10 +6,31 @@ import iris
 from iris.analysis import MEAN
 from iris.analysis.stats import pearsonr
 
-from diagnostic import plot_results
+from diagnostic import plot_diagnostic
 from esmvaltool.diag_scripts.shared import group_metadata, run_diagnostic
 
 logger = logging.getLogger(os.path.basename(__file__))
+
+
+def get_provenance_record(attributes, ancestor_files, plot_type):
+    """Create a provenance record describing the diagnostic data and plot."""
+    caption = ("Correlation of {long_name} between {dataset} and "
+               "{reference_dataset}.".format(**attributes))
+
+    record = {
+        'caption': caption,
+        'statistics': ['corr'],
+        'domain': 'global',
+        'plot_type': plot_type,
+        'authors': [
+            'ande_bo',
+        ],
+        'references': [
+            'acknow_project',
+        ],
+        'ancestors': ancestor_files,
+    }
+    return record
 
 
 def main(cfg):
@@ -24,10 +45,11 @@ def main(cfg):
             if attributes['reference_dataset'] == attributes['dataset']:
                 reference_name = attributes['dataset']
                 logger.info("Using %s as a reference dataset", reference_name)
-                filename = attributes['filename']
-                reference = iris.load_cube(filename)
+                reference_filename = attributes['filename']
+                reference = iris.load_cube(reference_filename)
                 reference = reference.collapsed('time', MEAN)
-                logger.info("Reference cube:\n%s\n%s", filename, reference)
+                logger.info("Reference cube:\n%s\n%s", reference_filename,
+                            reference)
                 break
         else:
             raise ValueError("No reference_dataset defined in recipe.")
@@ -50,7 +72,11 @@ def main(cfg):
             name = '{}_correlation_with_{}'.format(
                 os.path.splitext(os.path.basename(filename))[0],
                 reference_name)
-            plot_results(cube, name, cfg)
+            provenance_record = get_provenance_record(
+                attributes,
+                ancestor_files=[reference_filename, filename],
+                plot_type=cfg['plot_type'])
+            plot_diagnostic(cube, name, provenance_record, cfg)
 
 
 if __name__ == '__main__':
