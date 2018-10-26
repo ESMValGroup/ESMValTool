cb1fd3155f323027971f0d459e235aee5c864fc6
Mattia Righi <mattia.righi@dlr.de>
2018-10-26 10:26:48 +0200

    Distinguish between scalars and lists, and account for single variable case

--- esmvaltool/_task.py
+++ esmvaltool/_task.py
@@ -134,20 +134,30 @@ def write_ncl_settings(settings, filename, mode='wt'):
         return txt
 
     def _format_dict(name, dictionary):
-        """Format dict as NCL"""
-        lines = ['{}[i] = True'.format(name)]
+        """Format dict as NCL (list of) logical(s) with attributes."""
+        if name in ('diag_script_info', 'config_user_info'):
+            str = ['{} = True', '{}@{} = {}']
+        else:
+            str = ['{}[i] = True', '{}[i]@{} = {}']
+        lines = [str[0].format(name)]
         for key, value in sorted(dictionary.items()):
-            lines.append('{}[i]@{} = {}'.format(name, key, _format(value)))
+            lines.append(str[1].format(name, key, _format(value)))
         txt = '\n'.join(lines)
         return txt
 
     def _header(name):
-        """Define a NCL list as `name` if not already defined."""
-        return('if (.not. isdefined("{name}")) then\n'
-               '  {name} = NewList("fifo")\n'
-               'end if\n'
-               'ListAppend({name}, new(1, logical))\n'
-               'i = ListCount({name}) - 1\n'.format(name=name))
+        """Create NCL header either as a logical or as a list of logicals."""
+        if name in ('diag_script_info', 'config_user_info'):
+
+            return ('if (isvar("{name}")) then\n'
+                    '    delete({name})\n'
+                    'end if\n'.format(name=name))
+        else:
+            return('if (.not. isdefined("{name}")) then\n'
+                   '  {name} = NewList("fifo")\n'
+                   'end if\n'
+                   'ListAppend({name}, new(1, logical))\n'
+                   'i = ListCount({name}) - 1\n'.format(name=name))
 
     lines = []
     for key, value in sorted(settings.items()):
