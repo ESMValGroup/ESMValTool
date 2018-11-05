00a0c3d0da7ba18247a926279305421854586e2b
Manuel Schlund <manuel.schlund@dlr.de>
2018-11-05 16:07:46 +0100

    Adapted derived variables to new scheme

--- esmvaltool/preprocessor/_derive/asr.py
+++ esmvaltool/preprocessor/_derive/asr.py
@@ -1,6 +1,5 @@
 """Derivation of variable `asr`."""
 
-
 from iris import Constraint
 
 from ._derived_variable_base import DerivedVariableBase
@@ -9,42 +8,19 @@ from ._derived_variable_base import DerivedVariableBase
 class DerivedVariable(DerivedVariableBase):
     """Derivation of variable `asr`."""
 
-    def get_required(self, frequency):
-        """Get variable `short_name` and `field` pairs required for derivation.
+    # Required variables
+    _required_variables = {
+        'vars': [{
+            'short_name': 'rsdt',
+            'field': 'T2{frequency}s'
+        }, {
+            'short_name': 'rsut',
+            'field': 'T2{frequency}s'
+        }]
+    }
 
-        Parameters
-        ----------
-        frequency : str
-            Frequency of the desired derived variable.
-
-        Returns
-        -------
-        list of tuples
-            List of tuples (`short_name`, `field`) of all variables required
-            for derivation.
-
-        """
-        return [('rsdt', 'T2' + frequency + 's'),
-                ('rsut', 'T2' + frequency + 's')]
-
-    def calculate(self, cubes, fx_files=None):
-        """Compute absorbed shortwave radiation.
-
-        Parameters
-        ----------
-        cubes : iris.cube.CubeList
-            `CubeList` containing `rsdt` (`toa_incoming_shortwave_flux`) and
-            `rsut` (`toa_outgoing_shortwave_flux`).
-        fx_files : dict, optional
-            If required, dictionary containing fx files  with `short_name`
-            (key) and path (value) of the fx variable.
-
-        Returns
-        -------
-        iris.cube.Cube
-            `Cube` containing absorbed shortwave radiation.
-
-        """
+    def calculate(self, cubes):
+        """Compute absorbed shortwave radiation."""
         rsdt_cube = cubes.extract_strict(
             Constraint(name='toa_incoming_shortwave_flux'))
         rsut_cube = cubes.extract_strict(
--- esmvaltool/preprocessor/_derive/uajet.py
+++ esmvaltool/preprocessor/_derive/uajet.py
@@ -1,12 +1,10 @@
 """Derivation of variable `uajet`."""
 
-
 import iris
 from iris import Constraint
 import numpy as np
 
 from ._derived_variable_base import DerivedVariableBase
-
 
 # Constants (Southern hemisphere at 850 hPa)
 LAT = [-80.0, -30.0]
@@ -16,46 +14,22 @@ PLEV = 85000
 class DerivedVariable(DerivedVariableBase):
     """Derivation of variable `uajet`."""
 
-    def get_required(self, frequency):
-        """Get variable `short_name` and `field` pairs required for derivation.
+    # Required variables
+    _required_variables = {
+        'vars': [{
+            'short_name': 'ua',
+            'field': 'T3{frequency}'
+        }]
+    }
 
-        Parameters
-        ----------
-        frequency : str
-            Frequency of the desired derived variable.
-
-        Returns
-        -------
-        list of tuples
-            List of tuples (`short_name`, `field`) of all variables required
-            for derivation.
-
-        """
-        return [('ua', 'T3' + frequency)]
-
-    def calculate(self, cubes, fx_files=None):
-        """Compute latitude of maximum meridional wind speed.
-
-        Parameters
-        ----------
-        cubes : iris.cube.CubeList
-            `CubeList` containing `ua` (`eastward_wind`).
-        fx_files : dict, optional
-            If required, dictionary containing fx files  with `short_name`
-            (key) and path (value) of the fx variable.
-
-        Returns
-        -------
-        iris.cube.Cube
-            `Cube` containing latitude of maximum meridional wind speed.
-
-        """
+    def calculate(self, cubes):
+        """Compute latitude of maximum meridional wind speed."""
         # Load cube, extract correct region and perform zonal mean
         ua_cube = cubes.extract_strict(Constraint(name='eastward_wind'))
-        ua_cube = ua_cube.interpolate([('air_pressure', PLEV)],
-                                      scheme=iris.analysis.Linear())
-        ua_cube = ua_cube.extract(iris.Constraint(
-            latitude=lambda cell: LAT[0] <= cell <= LAT[1]))
+        ua_cube = ua_cube.interpolate(
+            [('air_pressure', PLEV)], scheme=iris.analysis.Linear())
+        ua_cube = ua_cube.extract(
+            iris.Constraint(latitude=lambda cell: LAT[0] <= cell <= LAT[1]))
         ua_cube = ua_cube.collapsed('longitude', iris.analysis.MEAN)
 
         # Calculate maximum jet position
@@ -77,8 +51,10 @@ class DerivedVariable(DerivedVariableBase):
         uajet_cube = iris.cube.Cube(
             uajet_vals,
             dim_coords_and_dims=[(ua_cube.coord('time'), 0)],
-            attributes={'plev': PLEV,
-                        'lat_range_0': LAT[0],
-                        'lat_range_1': LAT[1]})
+            attributes={
+                'plev': PLEV,
+                'lat_range_0': LAT[0],
+                'lat_range_1': LAT[1]
+            })
 
         return uajet_cube
