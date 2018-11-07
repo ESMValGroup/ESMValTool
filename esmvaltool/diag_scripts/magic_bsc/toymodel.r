bec11efacb1204a0fff583e5000c99a4c5d51362
ahunter <alasdair.hunter@bsc.es>
2018-11-07 08:16:27 +0100

    Bug fixes for toymodel and simple plot included

--- esmvaltool/diag_scripts/magic_bsc/toymodel.r
+++ esmvaltool/diag_scripts/magic_bsc/toymodel.r
@@ -43,7 +43,10 @@ g <- params$gamma
 nm <- params$number_of_members
 nstartd <- 1
 nleadt = params$no_of_lead_times
-
+print(args)
+print(var0)
+print(params)
+print(fullpath_filenames)
 #fullpath_filenames <-  "/scratch/Earth/ahunter/esmvaltool_input/CMIP5/Tier2/NCEP/OBS_NCEP_reanaly_1_T3M_ta_200001-200212.nc"
 data <- Start(model = fullpath_filenames,
                         var = var0,
@@ -69,20 +72,104 @@ data <- WeightedMean(data, lat = lat, lon = lon, londim = lon_dim, latdim = lat_
 names(dim(data)) <- dim_names[-c(lon_dim, lat_dim)]
 time_dim <- which(names(dim(data)) == "time")
 
-
-Obs2ToyForecast <- function(x) {
-nleadt <- length(x)
-x <- array(x, c(1, 1, 1, length(x)))
-x <- ToyModel(alpha = a, beta = b, gamma = g, nmemb = nm,
-              obsini = x, nstartd = 1, nleadt = nleadt)
-x$obs <- adrop(x$obs, c(1,3))
-x$mod <- adrop(x$mod, c(1,3))
-return(x)
+ToyModel <- function (alpha = 0.1, beta = 0.4, gamma = 1, sig = 1, trend = 0, 
+    nstartd = 30, nleadt = 4, nmemb = 10, obsini = NULL, fxerr = NULL) 
+{
+    if (any(!is.numeric(c(alpha, beta, gamma, sig, trend, nstartd, 
+        nleadt, nmemb)))) {
+        stop(paste("Parameters 'alpha', 'beta', 'gamma', 'sig', 'trend', 'nstartd',", 
+            "nleadt and nmemb must be numeric."))
+    }
+    nstartd <- round(nstartd)
+    nleadt <- round(nleadt)
+    nmemb <- round(nmemb)
+    if (!is.null(obsini)) {
+        if (!is.numeric(obsini) || !is.array(obsini)) {
+            stop("Parameter 'obsini' must be a numeric array.")
+        }
+        if (length(dim(obsini)) != 4) {
+            stop("Parameter 'obsini' must be an array with dimensions c(1, 1, nleadt, nstartd).")
+        }
+        if (dim(obsini)[3] != nstartd || dim(obsini)[4] != nleadt) {
+            stop(paste0("The dimensions of parameter 'obsini' and the parameters 'nleadt' and 'nstartd' must match:\n  dim(obsini) = c(", 
+                dim(obsini)[3], ", ", dim(obsini)[4], ")\n  nstartd = ", 
+                nstartd, "  nleadt = ", nleadt))
+        }
+    }
+    if (!is.null(fxerr)) {
+        if (!is.numeric(fxerr)) {
+            stop("Parameter 'fxerr' must be numeric.")
+        }
+    }
+    time <- seq(1, nstartd)
+    if (!(sig^2 - alpha^2 - beta^2 > 0)) {
+        stop("Model variability not constrained: respect condition \"sig^2-alpha^2-beta^2 > 0\")")
+    }
+    if (nstartd < 0) {
+        stop("Number of start dates must be positive")
+    }
+    if (nleadt < 0) {
+        stop("Number of lead-times must be positive")
+    }
+    if (nmemb < 0) {
+        stop("Number of members must be positive")
+    }
+    if (!is.null(obsini)) {
+        obs_ano <- obsini
+    }
+    else {
+        obs_ano <- array(rnorm(nleadt * nstartd, mean = 0, sd = sig), 
+            dim = c(1, 1, nstartd, nleadt))
+        obs_trend <- array(t(time) * rep(trend, times = nstartd), 
+            , dim = c(1, 1, nstartd, nleadt))
+        obs <- obs_ano + obs_trend
+        trend <- rep(c(trend), times = nleadt)
+        sig <- rep(c(sig), times = nleadt)
+    }
+    forecast <- array(dim = c(length(gamma), nmemb, nstartd, 
+        nleadt))
+    for (j in 1:nstartd) {
+        for (f in 1:nleadt) {
+            for (g in 1:length(gamma)) {
+                auto_term <-  obs_ano[1, 1, j, f]
+                if (is.numeric(fxerr)) {
+                  conf_term <- fxerr
+                }
+                else {
+                  conf_term <- rnorm(1, mean = 0, sd = beta)
+                }
+                trend_term <- gamma[g] * trend * j
+                var_corr <- rnorm(nmemb, mean = 0, sd = sqrt(sig - 
+                  alpha^2 - beta^2))
+                forecast[g, , j, f] <- matrix(auto_term, c(nmemb, 
+                  1)) + matrix(conf_term, c(nmemb, 1)) + matrix(trend_term, 
+                  c(nmemb, 1)) + var_corr
+            }
+        }
+    }
+    list(mod = forecast, obs = obs_ano)
 }
+#Obs2ToyForecast <- function(x) {
+#nleadt <- length(x)
+#x <- array(x, c(1, 1, 1, length(x)))
+forecast <- ToyModel(alpha = a, beta = b, gamma = g, nmemb = nm,
+              obsini = InsertDim(data,3,1), nstartd = 1, nleadt = dim(data)[time_dim])
+#x$obs <- adrop(x$obs, c(1,3))
+#x$mod <- adrop(x$mod, c(1,3))
+#return(x)
+#}
+#Quick plot of results
+jpeg(paste0(plot_dir, "/syntheticforecast.jpg"))
+ plot(time,forecast$obs, type = "l")
 
+for (i in 1:nm){
+ lines(time,forecast$mod[1,i,,], col = 2)
+}
+dev.off()
 
-
-data <- Apply(list(data), target_dims = list(time_dim), AtomicFun = "Obs2ToyForecast")$mod
+#data <- Apply(list(data), target_dims = list(time_dim), AtomicFun = "Obs2ToyForecast")$mod
+obs_data <- forecast$obs
+data <- forecast$mod[1,,1,]
 names(dim(data))[c(1, 2)] <- c("number", "time")
 
 attributes(time) <- NULL
@@ -94,6 +181,7 @@ names(metadata)[1] <- var0
 attr(data, 'variables') <- metadata
 variable_list <- list(variable = data, time = time)
 names(variable_list)[1] <- var0
+print(str(data))
 ArrayToNetCDF(variable_list,
               paste0(plot_dir, "/", "synthetic_", basename(fullpath_filenames)))
 
