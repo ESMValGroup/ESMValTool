f07fdc8c8f7ed1e71d817b53c42ddd0d1bf67023
Valeriu Predoi <valeriu.predoi@gmail.com>
2018-11-08 16:02:24 +0000

    changed the location to executable

--- setup.py
+++ setup.py
@@ -221,7 +221,7 @@ with open('README.md') as readme:
             'console_scripts': [
                 'esmvaltool = esmvaltool._main:run',
                 'nclcodestyle = esmvaltool.utils.nclcodestyle.nclcodestyle:_main',
-                'mip_convert_setup = esmvaltool.utils.esmvt_mipconv_setup:main'
+                'mip_convert_setup = esmvaltool.utils.mip_convert.esmvt_mipconv_setup:main'
             ],
         },
         cmdclass={
