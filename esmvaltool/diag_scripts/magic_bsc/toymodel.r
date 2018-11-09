fced2677801276ae174aa617d4fde9f7c7804886
nperezzanon <nuria.perez@bsc.es>
2018-11-09 17:03:36 +0100

    Toy model plot with title, axis names, size etc.

--- esmvaltool/diag_scripts/magic_bsc/toymodel.r
+++ esmvaltool/diag_scripts/magic_bsc/toymodel.r
@@ -16,8 +16,8 @@ library(startR, lib.loc='/home/Earth/ahunter/R/x86_64-unknown-linux-gnu-library/
 library(multiApply)
 library(yaml)
 library(abind)
-library(magic.bsc, lib.loc = '/home/Earth/nperez/git/magic.bsc.Rcheck/')
-#source('https://earth.bsc.es/gitlab/es/s2dverification/raw/develop-Magic_WP6/R/WeightedMean.R')
+library(ClimProjDiags)
+library(RColorBrewer)
 
 #Parsing input file paths and creating output dirs
 args <- commandArgs(trailingOnly = TRUE)
@@ -43,10 +43,7 @@ g <- params$gamma
 nm <- params$number_of_members
 nstartd <- 1
 nleadt = params$no_of_lead_times
-print(args)
-print(var0)
-print(params)
-print(fullpath_filenames)
+
 #fullpath_filenames <-  "/scratch/Earth/ahunter/esmvaltool_input/CMIP5/Tier2/NCEP/OBS_NCEP_reanaly_1_T3M_ta_200001-200212.nc"
 data <- Start(model = fullpath_filenames,
                         var = var0,
@@ -54,13 +51,13 @@ data <- Start(model = fullpath_filenames,
                         time = 'all',
                         lat = 'all',
                         lon = 'all',
-#                        plev = "all",
                         lon_var = 'lon',
                         return_vars = list(time = 'model', lon = 'model', lat = 'model'),
                         retrieve = TRUE)
 
 
 dim_names <- names(dim(data))
+units <- (attr(data,"Variables")$common)[[2]]$units
 time <- attributes(data)$Variables$dat1$time
 lon_dim <- which(names(dim(data)) == "lon")
 lat_dim <- which(names(dim(data)) == "lat")
@@ -72,12 +69,12 @@ data <- WeightedMean(data, lat = lat, lon = lon, londim = lon_dim, latdim = lat_
 names(dim(data)) <- dim_names[-c(lon_dim, lat_dim)]
 time_dim <- which(names(dim(data)) == "time")
 
-ToyModel <- function (alpha = 0.1, beta = 0.4, gamma = 1, sig = 1, trend = 0, 
-    nstartd = 30, nleadt = 4, nmemb = 10, obsini = NULL, fxerr = NULL) 
+ToyModel <- function (alpha = 0.1, beta = 0.4, gamma = 1, sig = 1, trend = 0,
+    nstartd = 30, nleadt = 4, nmemb = 10, obsini = NULL, fxerr = NULL)
 {
-    if (any(!is.numeric(c(alpha, beta, gamma, sig, trend, nstartd, 
+    if (any(!is.numeric(c(alpha, beta, gamma, sig, trend, nstartd,
         nleadt, nmemb)))) {
-        stop(paste("Parameters 'alpha', 'beta', 'gamma', 'sig', 'trend', 'nstartd',", 
+        stop(paste("Parameters 'alpha', 'beta', 'gamma', 'sig', 'trend', 'nstartd',",
             "nleadt and nmemb must be numeric."))
     }
     nstartd <- round(nstartd)
@@ -91,8 +88,8 @@ ToyModel <- function (alpha = 0.1, beta = 0.4, gamma = 1, sig = 1, trend = 0,
             stop("Parameter 'obsini' must be an array with dimensions c(1, 1, nleadt, nstartd).")
         }
         if (dim(obsini)[3] != nstartd || dim(obsini)[4] != nleadt) {
-            stop(paste0("The dimensions of parameter 'obsini' and the parameters 'nleadt' and 'nstartd' must match:\n  dim(obsini) = c(", 
-                dim(obsini)[3], ", ", dim(obsini)[4], ")\n  nstartd = ", 
+            stop(paste0("The dimensions of parameter 'obsini' and the parameters 'nleadt' and 'nstartd' must match:\n  dim(obsini) = c(",
+                dim(obsini)[3], ", ", dim(obsini)[4], ")\n  nstartd = ",
                 nstartd, "  nleadt = ", nleadt))
         }
     }
@@ -118,15 +115,15 @@ ToyModel <- function (alpha = 0.1, beta = 0.4, gamma = 1, sig = 1, trend = 0,
         obs_ano <- obsini
     }
     else {
-        obs_ano <- array(rnorm(nleadt * nstartd, mean = 0, sd = sig), 
+        obs_ano <- array(rnorm(nleadt * nstartd, mean = 0, sd = sig),
             dim = c(1, 1, nstartd, nleadt))
-        obs_trend <- array(t(time) * rep(trend, times = nstartd), 
+        obs_trend <- array(t(time) * rep(trend, times = nstartd),
             , dim = c(1, 1, nstartd, nleadt))
         obs <- obs_ano + obs_trend
         trend <- rep(c(trend), times = nleadt)
         sig <- rep(c(sig), times = nleadt)
     }
-    forecast <- array(dim = c(length(gamma), nmemb, nstartd, 
+    forecast <- array(dim = c(length(gamma), nmemb, nstartd,
         nleadt))
     for (j in 1:nstartd) {
         for (f in 1:nleadt) {
@@ -139,37 +136,36 @@ ToyModel <- function (alpha = 0.1, beta = 0.4, gamma = 1, sig = 1, trend = 0,
                   conf_term <- rnorm(1, mean = 0, sd = beta)
                 }
                 trend_term <- gamma[g] * trend * j
-                var_corr <- rnorm(nmemb, mean = 0, sd = sqrt(sig - 
+                var_corr <- rnorm(nmemb, mean = 0, sd = sqrt(sig -
                   alpha^2 - beta^2))
-                forecast[g, , j, f] <- matrix(auto_term, c(nmemb, 
-                  1)) + matrix(conf_term, c(nmemb, 1)) + matrix(trend_term, 
+                forecast[g, , j, f] <- matrix(auto_term, c(nmemb,
+                  1)) + matrix(conf_term, c(nmemb, 1)) + matrix(trend_term,
                   c(nmemb, 1)) + var_corr
             }
         }
     }
     list(mod = forecast, obs = obs_ano)
 }
-#Obs2ToyForecast <- function(x) {
-#nleadt <- length(x)
-#x <- array(x, c(1, 1, 1, length(x)))
-forecast <- ToyModel(alpha = a, beta = b, gamma = g, nmemb = nm,
-              obsini = InsertDim(data,3,1), nstartd = 1, nleadt = dim(data)[time_dim])
-#x$obs <- adrop(x$obs, c(1,3))
-#x$mod <- adrop(x$mod, c(1,3))
-#return(x)
-#}
-#Quick plot of results
-jpeg(paste0(plot_dir, "/syntheticforecast.jpg"))
- plot(time,forecast$obs, type = "l")
 
-for (i in 1:nm){
- lines(time,forecast$mod[1,i,,], col = 2)
-}
+forecast <- ToyModel(alpha = a, beta = b, gamma = g, nmemb = nm,
+              obsini = InsertDim(data, 3, 1), nstartd = 1, nleadt = dim(data)[time_dim])
+
+#Quick plot of results
+print(brewer.pal(n = nm, name = 'Reds'))
+jpeg(paste0(plot_dir, "/", "synthetic_", gsub(".nc", "", basename(fullpath_filenames)), ".jpg"),
+     height = 460, width = 600)#, units = "cm", res = 300)
+plot(time, forecast$obs, type = "l",
+     ylim = c(min(c(forecast$obs, forecast$mod)),
+              max(c(forecast$obs, forecast$mod))),
+     ylab = paste(var0, "(", units, ")"),
+     main = paste(nm, "synthetic members generated"), bty = 'n')
+matlines(time, t(forecast$mod[1, , 1, ]), col = brewer.pal(n = nm, name = 'Blues'))
+lines(time, forecast$obs, lwd = 2)
 dev.off()
 
 #data <- Apply(list(data), target_dims = list(time_dim), AtomicFun = "Obs2ToyForecast")$mod
 obs_data <- forecast$obs
-data <- forecast$mod[1,,1,]
+data <- forecast$mod[1, , 1, ]
 names(dim(data))[c(1, 2)] <- c("number", "time")
 
 attributes(time) <- NULL
