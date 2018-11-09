064eb5b46419e4dc603ce7887de311f59ce62401
Valeriu Predoi <valeriu.predoi@gmail.com>
2018-11-09 12:11:23 +0000

    adding to and refining utility script

--- esmvaltool/utils/mip_convert/esmvt_mipconv_setup.py
+++ esmvaltool/utils/mip_convert/esmvt_mipconv_setup.py
@@ -5,20 +5,28 @@ This script sets up the correct rose suite directories to run mip_convert
 on different UM suite data.
 """
 import argparse
+import logging
 import os
+import sys
 import shutil
-import configparser as ConfigParser
-import yaml
+from distutils.version import LooseVersion
+# configparser has changed names in python 3.x
+if LooseVersion(sys.version) < LooseVersion("3.0"):
+    import ConfigParser
+else:
+    import configparser as ConfigParser
+import yaml  # noqa
+
+
+# set up logging
+logger = logging.getLogger(__name__)
 
 
 # print the header
 HEADER = r"""
 ______________________________________________________________________
-          _____ ____  __  ____     __    _ _____           _
-         | ____/ ___||  \/  \ \   / /_ _| |_   _|__   ___ | |
-         |  _| \___ \| |\/| |\ \ / / _` | | | |/ _ \ / _ \| |
-         | |___ ___) | |  | | \ V / (_| | | | | (_) | (_) | |
-         |_____|____/|_|  |_|  \_/ \__,_|_| |_|\___/ \___/|_|
+
+     ESMValTool and mip_convert: setting up rose suites.
 ______________________________________________________________________
 
 """ + __doc__
@@ -40,8 +48,30 @@ def get_args():
         '--recipe-files',
         type=str,
         help='Recipe files (list or single file)')
+    parser.add_argument(
+        '-l',
+        '--log-level',
+        default='info',
+        choices=['debug', 'info', 'warning', 'error'])
     args = parser.parse_args()
     return args
+
+
+def _set_logger(logging, out_dir, log_level):
+    # set logging for screen and file output
+    root_logger = logging.getLogger()
+    out_fmt = "%(asctime)s %(levelname)-8s %(name)s,%(lineno)s\t%(message)s"
+    logging.basicConfig(
+        filename=os.path.join(out_dir, 'esmvaltool_mip_convert_log.txt'),
+        filemode='a',
+        format=out_fmt,
+        datefmt='%H:%M:%S',
+        level=logging.DEBUG)
+    root_logger.setLevel(log_level.upper())
+    logfmt = logging.Formatter(out_fmt)
+    console_handler = logging.StreamHandler()
+    console_handler.setFormatter(logfmt)
+    root_logger.addHandler(console_handler)
 
 
 def read_yaml_file(yaml_file):
@@ -62,7 +92,8 @@ def map_var_to_stream(diagnostics, stream_map):
     return stream_list
 
 
-def write_rose_conf(rose_config_template, recipe_file, config_file):
+def write_rose_conf(rose_config_template, recipe_file,
+                    config_file, log_level):
     """Write the new rose conf file per suite."""
     # Build the ConfigParser object
     Config = ConfigParser.ConfigParser()
@@ -85,6 +116,10 @@ def write_rose_conf(rose_config_template, recipe_file, config_file):
         stream_components[stream] = conf_file['STREAM_COMPONENTS'][stream]
         cycling_frequencies[stream] = 'P1Y'
 
+    # set the logger to start outputting
+    _set_logger(logging, conf_file['ROSES_OUTPUT'], log_level)
+    logger.info(HEADER)
+
     # loop through datasets (different suites for different datasets)
     for dataset in datasets:
 
@@ -103,8 +138,15 @@ def write_rose_conf(rose_config_template, recipe_file, config_file):
         if not os.path.exists(rose_output):
             os.makedirs(rose_output)
 
+        # start logging
+        logger.info("Working on dataset: %s", dataset)
+        logger.info("Mapping dataset to suite: %s", rose_suite)
+        logger.info("Output and logs written to: %s", rose_output)
+        logger.info("Creating rose suite directories...")
+        logger.info("Use rose-suite.conf template %s", rose_config_template)
+        logger.info("Use user config file %s", config_file)
+
         # write the file
-        rose_config = open(os.path.join(rose_suite, 'rose-suite.conf'), 'w')
         Config.set('jinja2:suite.rc', 'INPUT_DIR',
                    '"' + conf_file['INPUT_DIR'] + '"')
         Config.set('jinja2:suite.rc', 'OUTPUT_DIR', '"' + rose_output + '"')
@@ -122,17 +164,23 @@ def write_rose_conf(rose_config_template, recipe_file, config_file):
             'jinja2:suite.rc', 'TARGET_SUITE_NAME',
             '"' + conf_file['DATASET_TO_SUITE'][dataset['dataset']] + '"'
         )
-        Config.write(rose_config)
+        with open(os.path.join(rose_suite, 'rose-suite.conf'), 'w') as r_c:
+            logger.info("Writing rose-suite.conf file %s",
+                        os.path.join(rose_suite, 'rose-suite.conf'))
+            Config.write(r_c)
 
 
 def main():
     """Run the the meat of the code."""
     args = get_args()
-    rose_config_template = "rose-suite-template.conf"
+    rose_config_template = os.path.join(os.path.dirname(__file__),
+                                        "rose-suite-template.conf")
     recipe_files = args.recipe_files
     config_file = args.config_file
+    log_level = args.log_level
     for recipe_file in recipe_files.split(','):
-        write_rose_conf(rose_config_template, recipe_file, config_file)
+        write_rose_conf(rose_config_template, recipe_file,
+                        config_file, log_level)
 
 
 if __name__ == '__main__':
