30dd9c55fc9f78234889a538e6f64f2dc556cad8
Javier Vegas-Regidor <javier.vegas@bsc.es>
2018-11-12 13:10:07 +0100

    Clean code

--- esmvaltool/diag_scripts/seaice_feedback/negative_seaice_feedback.py
+++ esmvaltool/diag_scripts/seaice_feedback/negative_seaice_feedback.py
@@ -1,28 +1,33 @@
-#!/usr/bin/python
-# Author:    Francois Massonnet
-# Date:      October 2017
-# Purpose:   Set of functions to evaluate the negative ice growth-
-#            ice thickness feedback
-# Reference: Massonnet et al., in prep.
-# Other remarks: The codes presented here is a copy of
-#                TECLIM's GitHub code developed by F. Massonnet:
-#                http://www.climate.be:3000/TECLIM/ClimateData.git
-#                branch develop-fmasson
-#
+"""
+Diagnostic to evaluate the negative ice growth-ice thickness feedback
 
+The codes presented here is a copy of
+TECLIM's GitHub code developed by F. Massonnet:
+http://www.climate.be:3000/TECLIM/ClimateData.git
+branch develop-fmasson
+"""
 
 import os
 import logging
-from netCDF4 import Dataset
 import numpy as np
-import scipy.stats
 
+import scipy.stats
 import iris
+
 import esmvaltool.diag_scripts.shared
 import esmvaltool.diag_scripts.shared.names as n
 
 logger = logging.getLogger(os.path.basename(__file__))
 class NegativeSeaIceFeedback(object):
+    """
+    Diagnostic to evaluate the negative ice growth-ice thickness feedback
+
+    Parameters
+    ----------
+    conf : dict
+        Diagnostic execution info
+
+    """
 
     def __init__(self, conf):
         self.cfg = conf
@@ -30,6 +35,10 @@ class NegativeSeaIceFeedback(object):
         self.variables = esmvaltool.diag_scripts.shared.Variables(self.cfg)
 
     def compute(self):
+        """
+        Compute diagnostic
+
+        """
 
         for dataset in self.datasets:
             # Load cell area
@@ -54,17 +63,17 @@ class NegativeSeaIceFeedback(object):
             volume = self.compute_volume(sit, cellarea, mask=mask)
             del cellarea, sit
 
-            nf, stats, _ = self.negative_seaice_feedback(
+            neg_feedback, stats, _ = self.negative_seaice_feedback(
                 volume, period=12, order=2
             )
             del volume
 
-            logger.info("Negative feedback: ".ljust(20) + str(nf))
-            logger.info("P-Value: ".ljust(20) + str(stats[1]))
+            logger.info("Negative feedback: %20.4f", neg_feedback)
+            logger.info("P-Value: %20.4f", stats[1])
 
-    # 1. Function to evaluate sea-ice volume
     def compute_volume(self, avg_thick, cellarea, mask=1):
-        """[summary]
+        """
+        Compute sea ice volume
 
         Parameters
         ----------
@@ -88,16 +97,20 @@ class NegativeSeaIceFeedback(object):
         if np.max(mask) != 1.0 or np.min(mask) < 0.0:
             raise ValueError("Mask not between 0 and 1")
         max_thick = avg_thick.collapsed(avg_thick.coords(),
-                                            iris.analysis.MAX)
+                                        iris.analysis.MAX)
         if float(max_thick.data) > 20.0:
             logger.warning("Large sea ice thickness:"
                            "Max = %f",
-                           max_thick)
+                           max_thick.data)
 
         if avg_thick.coords('time'):
-            nt = avg_thick.shape[0]
-            vol = [np.sum(avg_thick.data[jt, :, :] * cellarea.data * mask.data) / 1e12
-                   for jt in range(nt)]
+            vol = []
+            for thick_slice in avg_thick.slices_over('time'):
+                vol.append(
+                    np.sum(
+                        thick_slice.data * cellarea.data * mask.data
+                    )
+                )
             vol = np.asarray(vol)
         elif len(avg_thick.shape) == 2:
             vol = np.sum(avg_thick * cellarea * mask) / 1e12
@@ -105,8 +118,6 @@ class NegativeSeaIceFeedback(object):
             raise ValueError("avgthickness has not 2 nor 3 dimensions")
         return vol
 
-
-    # 2. Function to detrend time series
     def detrend(self, data, order=1, period=None):
         """
         Detrend signal
@@ -160,8 +171,9 @@ class NegativeSeaIceFeedback(object):
         if period is None:
             time_nonan = np.arange(len(data_nonan))
             time = np.arange(len(data))
-            p = np.polyfit(time_nonan, data_nonan, order)
-            residuals = data - np.sum([p[i] * time ** (order - i) for i in range(order + 1)], axis=0)
+            polynom = np.polyfit(time_nonan, data_nonan, order)
+            residuals = data - np.sum([polynom[i] * time ** (order - i)
+                                       for i in range(order + 1)], axis=0)
 
         # If the signal contains a periodical component, we do the regression
         # time step per time step
@@ -169,46 +181,61 @@ class NegativeSeaIceFeedback(object):
             residuals = np.empty([n])
 
             # For each time step of the period, detrend
-            for jP in np.arange(period):
-                raw = data[np.arange(jP, n, period)]
+            # Note that another common option is to first remove a seasonal
+            # cycle and then detrend the anomalies. However this assumes that
+            # a cycle can be estimated, which in presence of a trend is tricky
+            # because the trend component interferes with the mean. I have
+            # tried that and it gives ugly step-wise anomalies. Detrending day
+            # per day seems the most natural way to do, at least as long as we
+            # assume that the raw signal at some time is the result of a
+            # seasonal cycle depending on the position of the time step in the
+            # period, plus a common trend, plus some noise.
+            for i in np.arange(period):
+                raw = data[np.arange(i, n, period)]
                 raw_nonan = raw[~np.isnan(raw)]
                 time = np.arange(len(raw))
                 time_nonan = np.arange(len(raw_nonan))
-                p = np.polyfit(time_nonan, raw_nonan, order)
-                residuals[np.arange(jP, n, period)] = \
-                    raw - np.sum([p[i] * time ** (order - i) for i in range(order + 1)], axis=0)
-
-                # Note that another common option is to first remove a seasonal cycle
-                # and then detrend the anomalies. However this assumes that a cycle can
-                # be estimated, which in presence of a trend is tricky because the
-                # trend component interferes with the mean. I have tried that and it
-                # gives ugly step-wise anomalies. Detrending day per day seems the
-                # the most natural way to do, at least as long as we assume that the
-                # raw signal at some time is the result of a seasonal cycle depending
-                # on the position of the time step in the period, plus a common trend,
-                # plus some noise.
-
+                polynom = np.polyfit(time_nonan, raw_nonan, order)
+                residuals[np.arange(i, n, period)] = \
+                    raw - np.sum([polynom[i] * time ** (order - i)
+                                  for i in range(order + 1)], axis=0)
         return residuals
 
-
-    # 3. Function to estimate the negative feedback
     def negative_seaice_feedback(self, volume, period, order=1):
-        """ Function to estimate the negative ice-thickness ice growth feedback
-            and its significance.
+        """
+        Function to estimate the negative ice-thickness ice growth feedback
+        and its significance.
 
-            INPUTS
-            (1) volume = 1-D numpy array containing time series of sea ice volume
-            (2) period = period of the signal (period expressed
-                        in time steps: 12 for monthly, 365 for daily...)
-            (3) order  = order of the polynomial detrending (integer >=0)
+        Parameters
+        ----------
+        volume : iris.Cube
+            Time series of sea ice volume
+        period : int
+            period of the signal (period expressed in time steps:
+            12 for monthly, 365 for daily...)
+        order : int, optional
+            order of the polynomial detrending (>=0) (the default is 1)
 
-            OUTPUTS
-            (1) Feedback parameter expressed as the regression
-                between dV on V_min (see (3))
-            (2) Correlation between those two and the p-value under the null
-                hypothesis of no correlation between dV and V_min
-            (3) [V_min, dV]: detrended time series of annual minimum of sea ice
-                volume, detrended series of wintertime volume production
+        Raises
+        ------
+        ValueError
+            If volume is not 1D or timeseries length is not a multiple of
+            the period
+
+        Returns
+        -------
+        nv: float
+            Feedback parameter expressed as the regression
+            between dV on V_min
+
+        corr: tuple(float, float, float)
+            Correlation between those two, the p-value under the null
+            hypothesis of no correlation between dV and V_min and standard
+            deviation
+
+        volume: tuple(float, float(
+            [V_min, dV]: detrended time series of annual minimum of sea ice
+            volume, detrended series of wintertime volume production
         """
 
         if len(volume.shape) != 1:
@@ -220,49 +247,57 @@ class NegativeSeaIceFeedback(object):
             )
 
         # 1. Locate the minima for each year
-        imin = [t + np.nanargmin(volume.data[t:t + period]) for t in range(0, volume.size, period)]
+        imin = [t + np.nanargmin(volume.data[t:t + period])
+                for t in range(0, volume.size, period)]
 
         # 2. Locate the maxima for each year
-        imax = [t + np.nanargmax(volume.data[t:t + period]) for t in np.arange(0, volume.size, period)]
+        imax = [t + np.nanargmax(volume.data[t:t + period])
+                for t in np.arange(0, volume.size, period)]
 
         # 3. Detrend series. A one-year shift is introduced to make sure we
         #    compute volume production *after* the summer minimum
-        v_min = self.detrend(volume[imin[:-1]], order=order)
-        dv = self.detrend(volume[imax[1:]] - volume[imin[:-1]], order=order)
+        vol_min = self.detrend(volume[imin[:-1]], order=order)
+        dvol = self.detrend(volume[imax[1:]] - volume[imin[:-1]], order=order)
 
         # 4. Compute diagnostics
-        # If all Vmins are zero or all dVs are zero, return Nan (pathological case)
-        if np.max(v_min) == 0.0 or np.max(dv == 0.0):
-            nf = np.nan
-            r = np.nan
+        # If all Vmins are zero or all dVs are zero, return Nan
+        # (pathological case)
+        if np.max(vol_min) == 0.0 or np.max(dvol == 0.0):
+            fit = np.nan
+            corr = np.nan
             pval = np.nan
             std = np.nan
         else:
-            r = np.corrcoef(v_min, dv)[0, 1]
-            tstat = r / np.sqrt((1 - r ** 2) / (len(v_min) - 2))  # The t-statistic.
+            corr = np.corrcoef(vol_min, dvol)[0, 1]
+            # The t-statistic.
+            tstat = corr / np.sqrt((1 - corr ** 2) / (len(vol_min) - 2))
             # Under the null hypothesis of no correlation,
             # tstat follows a student's law with  N - 2 dof.
-            pval = 1.0 - scipy.stats.t.cdf(np.abs(tstat), len(v_min) - 2)
+            pval = 1.0 - scipy.stats.t.cdf(np.abs(tstat), len(vol_min) - 2)
 
             if pval > 0.05:
-                logger.warning("Check the scatterplot of dV versus V_min, it is most likely "
-                            "suspicious, and the feedback  factor likely meaningless: p-value: " + str(pval))
+                logger.warning(
+                    "Check the scatterplot of dV versus V_min, it is most "
+                    "likely suspicious, and the feedback factor likely "
+                    "meaningless: p-value: %f", pval
+                )
 
             try:
-                fit, cov = np.polyfit(v_min, dv, 1, cov=True)
-                nf = fit[0]  # Fit parameter
+                fit, cov = np.polyfit(vol_min, dvol, 1, cov=True)
+                fit = fit[0]  # Fit parameter
                 std = np.sqrt(cov[0, 0])  # Standard deviation on it
             except ValueError:
                 logger.error("(negative_seaice_feedback) PROBLEM,"
                              "series badly conditioned: "
                              "Input volume: %f Vmin: %f dv: %f",
-                             volume, v_min, dv)
+                             volume, vol_min, dvol)
                 raise
 
-        return [nf, [r, pval, std], [v_min, dv]]
+        return [fit, [corr, pval, std], [vol_min, dvol]]
 
 
 def main():
+    """Run diagnostic"""
     with esmvaltool.diag_scripts.shared.run_diagnostic() as config:
         NegativeSeaIceFeedback(config).compute()
 
