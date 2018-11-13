b3bbe060ca0a081426170f1de626d9141588206a
peter.berg <a001850@c20834.ad.smhi.se>
2018-11-13 05:37:09 +0100

    Corrected coding standards

--- esmvaltool/diag_scripts/shapeselect/diag_shapeselect.py
+++ esmvaltool/diag_scripts/shapeselect/diag_shapeselect.py
@@ -1,25 +1,27 @@
 """Diagnostic to select grid points within a shapefile."""
-from copy import deepcopy
 import logging
 import os
 import sys
-from netCDF4 import Dataset, num2date, stringtochar
+from copy import deepcopy
+
 import fiona
-#from shapely.geometry import shape, mapping, Point, Polygon, MultiPolygon
-from shapely.geometry import MultiPoint, shape
-from shapely.ops import nearest_points
-#import shapefile
+# import matplotlib.pyplot as plt
+# from mpl_toolkits.basemap import Basemap
+import iris
+# import shapefile
 import numpy as np
 import xlsxwriter
-#import csv
-from matplotlib import pyplot as p 
-#import matplotlib.pyplot as plt
-#from mpl_toolkits.basemap import Basemap
-import iris
+from matplotlib import pyplot as p
+from netCDF4 import Dataset, num2date, stringtochar
+# from shapely.geometry import shape, mapping, Point, Polygon, MultiPolygon
+from shapely.geometry import MultiPoint, shape
+from shapely.ops import nearest_points
+
 from esmvaltool.diag_scripts.shared import run_diagnostic
 from esmvaltool.diag_scripts.shared.plot import quickplot
 
 logger = logging.getLogger(os.path.basename(__file__))
+
 
 def main(cfg):
     """Select grid points within shapefiles."""
@@ -39,24 +41,26 @@ def main(cfg):
             )
         write_netcdf(path, ncts, nclon, nclat, cube, cfg)
 
+
 def write_keyvalue_toxlsx(worksheet, row, key, value):
     if type(value).__name__ == 'dict':
-        worksheet.write(row, 0, key)        
+        worksheet.write(row, 0, key)
         row += 1
         for dictkey, dictvalue in value.items():
             row = write_keyvalue_toxlsx(worksheet, row, dictkey, dictvalue)
     elif type(value).__name__ == 'list':
         for listvalue in value:
-            row = write_keyvalue_toxlsx(worksheet, row, key, listvalue)            
+            row = write_keyvalue_toxlsx(worksheet, row, key, listvalue)
     else:
         worksheet.write(row, 0, key)
-        if type(value).__name__ == 'bool':                    
+        if type(value).__name__ == 'bool':
             worksheet.write(row, 1, str(int(value)))
-        else:    
+        else:
             worksheet.write(row, 1, value)
-        row += 1            
+        row += 1
 
-    return row        
+    return row
+
 
 def writexls(cfg, filename, ncts, nclon, nclat):
     """Write the content of a netcdffile as .xls"""
@@ -77,17 +81,18 @@ def writexls(cfg, filename, ncts, nclon, nclat):
     worksheet.write(0, 1, 'Lon/Lat')
     worksheet.write_column(2, 0, wtime)
     for row in range(ncts.shape[1]):
-        worksheet.write(1, row+1, 
-                        str("%#.3f" % round(float(nclon[row]), 3)) +
-                        '_' + str("%#.3f" % round(float(nclat[row]), 3)))
-        worksheet.write_column(2, row+1, np.around(np.squeeze(ncts[:, row]),
-                                                   decimals=8))
-        worksheet.set_column(0, row+1, 20)
+        worksheet.write(
+            1, row + 1,
+            str("%#.3f" % round(float(nclon[row]), 3)) + '_' + str(
+                "%#.3f" % round(float(nclat[row]), 3)))
+        worksheet.write_column(2, row + 1,
+                               np.around(np.squeeze(ncts[:, row]), decimals=8))
+        worksheet.set_column(0, row + 1, 20)
     worksheet = workbook.add_worksheet('NetCDFheader')
     worksheet.set_column(0, 0, 20)
-    for row, attr in enumerate(ncfile.ncattrs()): 
+    for row, attr in enumerate(ncfile.ncattrs()):
         worksheet.write(row, 0, attr)
-        worksheet.write(row, 1, getattr(ncfile, attr))       
+        worksheet.write(row, 1, getattr(ncfile, attr))
     worksheet = workbook.add_worksheet('ESMValTool')
     worksheet.set_column(0, 0, 20)
     row = 0
@@ -96,19 +101,21 @@ def writexls(cfg, filename, ncts, nclon, nclat):
         row = write_keyvalue_toxlsx(worksheet, row, key, value)
     workbook.close()
 
+
 def dates(otime):
     """ Converts iris time to human readable format """
     calendar = otime.units.calendar
     origin = otime.units.origin
 
+
 def shapeselect(cfg, cube, filename):
     """ New solution with fiona! """
     shppath = cfg['shppath']
     wgtmet = cfg['wgtmet']
-    if ((cube.coord('latitude').ndim == 1 and
-         cube.coord('longitude').ndim == 1)):
+    if ((cube.coord('latitude').ndim == 1
+         and cube.coord('longitude').ndim == 1)):
         coordpoints = [(x, y) for x in cube.coord('longitude').points
-                  for y in cube.coord('latitude').points]
+                       for y in cube.coord('latitude').points]
     else:
         logger.info("Support for 2-d coords not yet implemented!")
         sys.exit(1)
@@ -116,29 +123,29 @@ def shapeselect(cfg, cube, filename):
     if cfg['evalplot']:
         # Set limits for map (This can definitely be improved!)
         shap = fiona.open(shppath)
-        llcrnrlon=shap.bounds[0]-1
-        llcrnrlat=max((shap.bounds[1]-1,-90))
-        urcrnrlon=shap.bounds[2]+1
-        urcrnrlat=min((shap.bounds[3]+1,90))
+        llcrnrlon = shap.bounds[0] - 1
+        llcrnrlat = max((shap.bounds[1] - 1, -90))
+        urcrnrlon = shap.bounds[2] + 1
+        urcrnrlat = min((shap.bounds[3] + 1, 90))
         alons = []
         alats = []
         for lon, lat in coordpoints:
-            if ( lat > llcrnrlat and lat < urcrnrlat
-                 and lon > llcrnrlon and lon < urcrnrlon ):
+            if (lat > llcrnrlat and lat < urcrnrlat and lon > llcrnrlon
+                    and lon < urcrnrlon):
                 alons.append(lon)
                 alats.append(lat)
-        cnt=0
-        #rngx = np.arange(np.min(alons), np.max(alons), 1)
-        #rngy = np.arange(np.min(alats), np.max(alats), 1)
+        cnt = 0
+        # rngx = np.arange(np.min(alons), np.max(alons), 1)
+        # rngy = np.arange(np.min(alats), np.max(alats), 1)
         p.gcf().clear()
-        #ax.set_xticks(rngx)
-        #ax.set_yticks(rngy)
-        #for shapp in shap.shapeRecords(): #shape.next()
+        # ax.set_xticks(rngx)
+        # ax.set_yticks(rngy)
+        # for shapp in shap.shapeRecords(): #shape.next()
         for shapp in shap:
             xm = [i[0] for i in shapp['geometry']['coordinates'][0][:]]
             ym = [i[1] for i in shapp['geometry']['coordinates'][0][:]]
             p.plot(xm, ym)
-            p.plot(alons, alats, 'ro', markersize = 2)
+            p.plot(alons, alats, 'ro', markersize=2)
             cnt += 1
         p.xlabel('Longitude')
         p.ylabel('Latitude')
@@ -147,7 +154,7 @@ def shapeselect(cfg, cube, filename):
         gpy = []
         cnt = -1
         ncts = np.zeros((cube.coord('time').shape[0], len(shp)))
-        nclon = np.zeros((len(shp))) # Takes representative point
+        nclon = np.zeros((len(shp)))  # Takes representative point
         nclat = np.zeros((len(shp)))
         for ishp, multipol in enumerate(shp):
             cnt += 1
@@ -157,28 +164,31 @@ def shapeselect(cfg, cube, filename):
                 if len(gpx) == 0:
                     gpx, gpy = representative(gpx, gpy, points, multi, cube,
                                               cfg)
-                    pth='g+'
+                    pth = 'g+'
                 else:
-                    pth='b+'
+                    pth = 'b+'
             elif wgtmet == 'representative':
                 gpx, gpy = representative(gpx, gpy, points, multi, cube, cfg)
             if cfg['evalplot']:
-                p.plot(cube.coord('longitude').points[gpx],
-                       cube.coord('latitude').points[gpy],
-                       pth, markersize=10)
+                p.plot(
+                    cube.coord('longitude').points[gpx],
+                    cube.coord('latitude').points[gpy],
+                    pth,
+                    markersize=10)
             if len(gpx) == 1:
-                ncts[:,ishp] = np.reshape(cube.data[:, gpy, gpx],
-                                          (cube.data.shape[0],))
+                ncts[:, ishp] = np.reshape(cube.data[:, gpy, gpx],
+                                           (cube.data.shape[0], ))
             else:
-                ncts[:,ishp] = np.mean(cube.data[:, gpy, gpx],axis=1)
+                ncts[:, ishp] = np.mean(cube.data[:, gpy, gpx], axis=1)
             gx, gy = representative([], [], points, multi, cube, cfg)
             nclon[ishp] = cube.coord('longitude').points[gx]
             nclat[ishp] = cube.coord('latitude').points[gy]
     if cfg['evalplot']:
         name = os.path.splitext(os.path.basename(filename))[0]
-        path = os.path.join(cfg['plot_dir'],name + '.png')
+        path = os.path.join(cfg['plot_dir'], name + '.png')
         p.savefig(path)
     return ncts, nclon, nclat
+
 
 def mean_inside(gpx, gpy, points, multi, cube, cfg):
     for point in points:
@@ -187,12 +197,13 @@ def mean_inside(gpx, gpy, points, multi, cube, cfg):
                 addx = 360.
             else:
                 addx = 0.
-            x, y = best_match(cube.coord('longitude').points,
-                              cube.coord('latitude').points,
-                              point.x + addx, point.y)
+            x, y = best_match(
+                cube.coord('longitude').points,
+                cube.coord('latitude').points, point.x + addx, point.y)
             gpx.append(x)
             gpy.append(y)
     return gpx, gpy
+
 
 def representative(gpx, gpy, points, multi, cube, cfg):
     reprpoint = multi.representative_point()
@@ -201,28 +212,29 @@ def representative(gpx, gpy, points, multi, cube, cfg):
     npy = nearest[1].coords[0][1]
     if npx < 0 and np.min(cube.coord('longitude').points) >= 0:
         npx = npx + 360.
-    x, y = best_match(cube.coord('longitude').points,
-                      cube.coord('latitude').points,
-                      npx, npy)
+    x, y = best_match(
+        cube.coord('longitude').points,
+        cube.coord('latitude').points, npx, npy)
     gpx.append(x)
     gpy.append(y)
     return gpx, gpy
+
 
 def best_match(i, j, px, py):
     """ Identifies the grid points in 2-d with minimum distance. """
     if i.shape != 2 or j.shape != 2:
         x = deepcopy(i)
         y = deepcopy(j)
-        xx = np.zeros((len(x),len(y)))
-        yy = np.zeros((len(x),len(y)))
-        for i in range(0,len(y)):
-            xx[:,i] = x
-        for j in range(0,len(x)):
-            yy[j,:] = y
+        xx = np.zeros((len(x), len(y)))
+        yy = np.zeros((len(x), len(y)))
+        for i in range(0, len(y)):
+            xx[:, i] = x
+        for j in range(0, len(x)):
+            yy[j, :] = y
     else:
         xx = deepcopy(i)
         yy = deepcopy(j)
-    distance = ( (xx - px)**2 + (yy - py)**2 )**0.5
+    distance = ((xx - px)**2 + (yy - py)**2)**0.5
     ind = np.unravel_index(np.argmin(distance, axis=None), distance.shape)
     return ind[0], ind[1]
 
@@ -233,10 +245,11 @@ def write_netcdf(path, var, plon, plat, cube, cfg):
     # wgtmet = cfg['wgtmet']
     polyid = []
     for row in range(var.shape[1]):
-        polyid.append(str("%#.3f" % round(plon[row], 3)) +
-                        '_' + str("%#.3f" % round(plat[row], 3)))
-    #polyid = stringtochar(np.array(polyid, 'S4'))
-    print(polyid)
+        polyid.append(
+            str("%#.3f" % round(plon[row], 3)) + '_' +
+            str("%#.3f" % round(plat[row], 3)))
+    # polyid = stringtochar(np.array(polyid, 'S4'))
+    # print(polyid)
     ncout = Dataset(path, mode='w')
     ncout.createDimension('time', None)
     ncout.createDimension('polygon', len(polyid))
@@ -249,18 +262,19 @@ def write_netcdf(path, var, plon, plat, cube, cfg):
     polys.setncattr_string('standard_name', 'polygon')
     polys.setncattr_string('long_name', 'polygon')
     polys.setncattr_string('shapefile', shppath)
-    lon = ncout.createVariable(cube.coord('longitude').var_name, 'f8', 'polygon',
-                               zlib=True)
-    lon.setncattr_string('standard_name', cube.coord('longitude').standard_name)
+    lon = ncout.createVariable(
+        cube.coord('longitude').var_name, 'f8', 'polygon', zlib=True)
+    lon.setncattr_string('standard_name',
+                         cube.coord('longitude').standard_name)
     lon.setncattr_string('long_name', cube.coord('longitude').long_name)
     lon.setncattr_string('units', cube.coord('longitude').units.origin)
-    lat = ncout.createVariable(cube.coord('latitude').var_name, 'f8', 'polygon',
-                               zlib=True)
+    lat = ncout.createVariable(
+        cube.coord('latitude').var_name, 'f8', 'polygon', zlib=True)
     lat.setncattr_string('standard_name', cube.coord('latitude').standard_name)
     lat.setncattr_string('long_name', cube.coord('latitude').long_name)
     lat.setncattr_string('units', cube.coord('latitude').units.origin)
-    data = ncout.createVariable(cube.var_name, 'f4', ('time', 'polygon'),
-                                zlib=True)
+    data = ncout.createVariable(
+        cube.var_name, 'f4', ('time', 'polygon'), zlib=True)
     data.setncattr_string('standard_name', cube.standard_name)
     data.setncattr_string('long_name', cube.long_name)
     data.setncattr_string('units', cube.units.origin)
