6eb0d451c1d16b394c8d55b81c110bc945f69eb9
Faruk D <f.diblen@esciencecenter.nl>
2018-11-13 12:35:09 +0100

    add post hook to install R dependencies after the package installation

---
+++ language_support.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+tools=(R Rscript)
+
+for tool in "${tools[@]}"; do
+    if ! command -v "$tool" > /dev/null 2>&1; then
+        echo "Executable $tool not found! Exiting..." >> $PREFIX/.messages.txt
+        exit 1
+    fi
+done
+
+Rscript $PREFIX/lib/python*/site-packages/esmvaltool/R/setup.R >> $PREFIX/.messages.txt
