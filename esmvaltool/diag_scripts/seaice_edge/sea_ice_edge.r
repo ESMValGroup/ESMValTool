99f0c9f8520dcf20d7d32c4e0b77a4b344ce0c2b
Javier Vegas-Regidor <javier.vegas@bsc.es>
2018-11-13 09:04:52 +0100

    Adapted sea ice edge

--- esmvaltool/diag_scripts/seaice_edge/sea_ice_edge.r
+++ esmvaltool/diag_scripts/seaice_edge/sea_ice_edge.r
@@ -1,78 +1,61 @@
-interface_data <- file.path(Sys.getenv('ESMValTool_interface_data'), 'r.interface')
-source(interface_data)
-source(diag_script_cfg)
-source('diag_scripts/lib/R/info_output.r')
+library(yaml)
+library(s2dverification)
+library(startR)
+library(tools)
+library(RColorBrewer)
+args <- commandArgs(trailingOnly = TRUE)
+params <- read_yaml(args[1])
+
+plot_dir <- params$plot_dir
+run_dir <- params$run_dir
+work_dir <- params$work_dir
+input_files <- read_yaml(params$input_files)
 
 ## Do not print warnings
 options(warn=-1)
 
-var0 <- variables[1]
-field_type0 <- field_types[1]
-
-info_output(paste0("<<<<<<<< Entering ", diag_script), verbosity, 4)
-info_output("+++++++++++++++++++++++++++++++++++++++++++++++++", verbosity, 1)
-info_output(paste0("plot - ", diag_script, " (var: ", variables[1], ")"), verbosity, 1)
-info_output("+++++++++++++++++++++++++++++++++++++++++++++++++", verbosity, 1)
-
-library(tools)
-diag_base = file_path_sans_ext(diag_script)
-library(s2dverification)
-library(startR)
-
 ## Create working dirs if they do not exist
-dir.create(plot_dir, showWarnings = FALSE)
-dir.create(file.path(plot_dir, diag_base), showWarnings = FALSE)
-dir.create(climo_dir, showWarnings = FALSE)
+dir.create(plot_dir, recursive=TRUE, showWarnings=FALSE)
+dir.create(work_dir, recursive=TRUE, showWarnings=FALSE)
 
 ## Plot function
-
-
-##
-## Run it all
-##
-print(models_name)
-for (model_idx in c(1:length(models_name))) {
-    fullpath_filename <- interface_get_fullpath(var0, field_type0, model_idx)
-    n_diag_script <- nchar(diag_script)
-    diag_script_base <- substr(diag_script, 0, n_diag_script - 2)
-
-
-    data <- Start(dat = fullpath_filename,
-                  var = var0,
+for (dataset_id in c(1:length(input_files))) {
+    dataset <- input_files[[dataset_id]]
+    print(paste0('Computing ', dataset$project, '_', dataset$dataset, '_', dataset$exp, '_', dataset$ensemble))
+    data <- Start(dat = dataset$filename,
+                  var = dataset$short_name,
+                  var_var = 'var_names',
                   time = 'all',
                   lat = 'all',
                   lon = 'all',
-                  var_var = 'var_names',
                   return_vars = list(time = NULL,
                                      lat = NULL,
                                      lon = NULL),
                   retrieve = TRUE)
+    print(str(data))
+    print(data[,,1,,1])
+    data[which(data < params$concentration_treshold)] <- 0
+    data[which(data >= params$concentration_treshold)] <- 1
 
-    data[which(data < concentration_treshold)] <- 0
-    data[which(data >= concentration_treshold)] <- 1
-
-    lat = attr(data, 'Variables')$common$lon
+    lat = attr(data, 'Variables')$common$lon1
     lon = attr(data, 'Variables')$common$lat
-
-
 
     time_dim <- which(names(dim(data)) == 'time')
     data_dim = dim(data)
     data_dim[time_dim] <- 12
     season_mean <- array(0, data_dim)
+    print(str(data))
+    print(data[,,1,,1])
     for( i in 1:12){
         season_mean[,,i,,] <- Mean1Dim(Season(data, posdim=time_dim, 1, i, i), 3)
     }
+    print(str(season_mean))
+    print(season_mean[,,6,,1])
+    figure_filename <- paste0(dataset$project, '_', dataset$dataset, '_', dataset$exp, '_', dataset$ensemble, '_seaiceedge_', dataset$start_year, '-', dataset$end_year)
+    figure_path <- file.path(plot_dir, paste0(figure_filename, '.', params$output_file_type))
+    title <- paste0('Probability of being inside sea-ice edge (', dataset$start_year,'-', dataset$end_year,')')
 
-    figure_filename <- interface_get_figure_filename(diag_script_base,
-                                                 '',
-                                                 field_type0,
-                                                 '',
-                                                 model_idx)
-    figure_path <- file.path(plot_dir, diag_base, paste0(figure_filename, '.', output_file_type))
-    info_output(figure_path, verbosity, 1)
-    title <- paste0('Probability of being inside sea-ice edge (', models_start_year[model_idx],'-', models_end_year[model_idx],')')
-    library(RColorBrewer)
+
     colors<-colorRampPalette(rev(brewer.pal(9, 'Blues')))(10)
     PlotLayout(PlotStereoMap, c('lat', 'lon'),
                Subset(season_mean, 'time', 1:12),
@@ -87,6 +70,4 @@ for (model_idx in c(1:length(models_name))) {
                titles = as.character(months(attr(data, 'Variables')$common$time[1:12], abbreviate=FALSE)),
                toptitle = title,
                fileout = figure_path)
-                   # cols=c('blue', 'white'),
 }
-info_output(paste0(">>>>>>>> Leaving ", diag_script), verbosity, 4)
