fd4e60cf7831972fac3066617cff8e4667725a46
Tobias Stacke <tobias.stacke@mpimet.mpg.de>
2018-11-14 10:14:53 +0100

    Rearranged output fields

--- esmvaltool/diag_scripts/landcover/landcover.py
+++ esmvaltool/diag_scripts/landcover/landcover.py
@@ -1,6 +1,7 @@
 """Python example diagnostic."""
 import logging
 import os
+import pdb
 
 import iris
 import esmvaltool.diag_scripts.shared as diag
@@ -26,13 +27,12 @@ def main(cfg):
 
     # Read data and compute long term means
     # to check: Shouldn't this be part of preprocessing?
-    allcubes = {}
+    allcubes = {key: [] for key in varlist.short_names()}
     for dataset_path in datasets:
         # Get dataset information
         datainfo = datasets.get_dataset_info(path=dataset_path)
-        dset = datainfo['dataset']
-        if dset not in allcubes.keys():
-            allcubes[dset] = []
+        ds = datainfo['dataset']
+        var = datainfo['short_name']
         # Load data into iris cube
         new_cube = iris.load(dataset_path, varlist.standard_names())[0]
         # Check for expected unit
@@ -41,18 +41,21 @@ def main(cfg):
                              new_cube.long_name.lower(), ' area fraction')
         # Compute long term mean
         mean_cube = new_cube.collapsed([diag.names.TIME], iris.analysis.MEAN)
+        # Rename variable in cube
+        mean_cube._var_name = "_".join([var,ds])
+        mean_cube.long_name = " ".join([var,'for dataset',ds])
         # Update data for dataset
         datasets.set_data(mean_cube.data, dataset_path)
         # Append to cubelist for temporary output
-        allcubes[dset].append(mean_cube)
+        allcubes[var].append(mean_cube)
 
     # Write regridded and temporal aggregated netCDF data files (one per model)
     # to do: update attributes
-    for model in allcubes.keys():
+    for var in allcubes.keys():
         filepath = os.path.join(cfg[diag.names.WORK_DIR],
-                                '_'.join(['postproc', model]) + '.nc')
+                                '_'.join(['postproc', var]) + '.nc')
         if cfg[diag.names.WRITE_NETCDF]:
-            iris.save(allcubes[model], filepath)
+            iris.save(allcubes[var], filepath)
             logger.info("Writing %s", filepath)
 
 
