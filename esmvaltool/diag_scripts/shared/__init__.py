546b56ad9cc99525b3dcd294ae81a494fdb2b81d
Manuel Schlund <manuel.schlund@dlr.de>
2018-11-15 15:13:32 +0100

    Readded python object-oriented diagnostics

--- esmvaltool/diag_scripts/shared/__init__.py
+++ esmvaltool/diag_scripts/shared/__init__.py
@@ -1,14 +1,19 @@
 """Code that is shared between multiple diagnostic scripts."""
-from . import plot
+from . import names, plot
 from ._base import (get_cfg, group_metadata, run_diagnostic, select_metadata,
                     sorted_group_metadata, sorted_metadata,
                     variables_available, save_iris_cube)
 from ._validation import (get_control_exper_obs, apply_supermeans)
+from ._diag import Datasets, Variable, Variables
 
 __all__ = [
+    'names',
     'get_cfg',
     'plot',
     'run_diagnostic',
+    'Variable',
+    'Variables',
+    'Datasets',
     'select_metadata',
     'sorted_metadata',
     'group_metadata',
