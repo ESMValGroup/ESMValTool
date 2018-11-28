cc8990658be794bb7a998652c4a94a548b1f823c
Valeriu Predoi <valeriu.predoi@gmail.com>
2018-11-28 13:25:59 +0000

    fully functional executable module

--- esmvaltool/utils/acsis/acsismoc.py
+++ esmvaltool/utils/acsis/acsismoc.py
@@ -1,21 +1,69 @@
-"""Compute annual average for moc_mar_hc10."""
+"""
+Compute MOC annual average and Greenland - Iceland airp DJF means.
+
+This module is part of the ACSIS indicators computations. It is an
+ESMValTool executable that can be called directly from the command line:
+example:
+
+acsismoc -m moc_transports.nc -s hadslp2r.nc -o FULL_PATH_TO_ACSIS_OUTPUT
+
+Data availability:
+------------------
+This module uses data that must be supplied as comand line arguments
+(see below for options). ACSIS data on CEDA-JASMIN can be found in:
+
+group_workspaces
+jasmin4
+esmeval
+acsis_data
+
+with subdirs labelled by data creation date.
+
+Usage:
+------
+Arguments:
+
+-m --moc-file     [OPTIONAL]   full path to the moc_transport file (netCDF);
+-s --hadslp-file  [OPTIONAL]   full path to the hadslp file (netCDF);
+-o --output       [OPTIONAL]   full path to output dir;
+                               DEFAULT $HOME/$USER/ACSIS;
+-l --log-level    [OPTIONAL]   logging level;
+                               OPTIONS: 'debug', 'info', 'warning', 'error';
+                               DEFAULT: 'info';
+
+Contact:
+--------
+Ed Hawkins, UREAD, e.hawkins@reading.ac.uk
+Valeriu Predoi, UREAD, valeriu.predoi@ncas.ac.uk
+First working version: November 2018
+"""
 import argparse
 from datetime import datetime
 import logging
 import os
+import collections
 import numpy as np
 import iris
 from esmvaltool.preprocessor import extract_region, extract_season
+
+# decalre the global variables for analysis
+# TODO Ed Hawkins - check these
+MOC_VARIABLE = 'moc_mar_hc10'
+AIRPRESS_VARIABLE = 'UM_0_fc8_vn405'
+SEASON = 'DJF'
 
 # set up logging
 logger = logging.getLogger(__name__)
 
 # print the header
 HEADER = r"""
-______________________________________________________________________
+___________________________________________________________________________
 
-            ESMValTool ACSIS Indicators Computation
-______________________________________________________________________
+ ESMValTool ACSIS Indicators Computation: annual MOC and Greenland/Iceland
+___________________________________________________________________________
+
+This module of the ACSIS indicators computation computes the annual mean
+for MOC and Greenland - Iceland DJF mean difference for air pressure.
 
 """ + __doc__
 
@@ -27,21 +75,14 @@ def get_args():
         description=HEADER,
         formatter_class=argparse.RawDescriptionHelpFormatter)
     parser.add_argument(
-        '-m',
-        '--moc-file',
-        type=str,
-        nargs='+',
-        help='MOC files')
+        '-m', '--moc-file', type=str, nargs='+', help='MOC files')
     parser.add_argument(
-        '-s',
-        '--hadslp-file',
-        type=str,
-        nargs='+',
-        help='hadslp files')
+        '-s', '--hadslp-file', type=str, nargs='+', help='hadslp files')
     parser.add_argument(
         '-o',
         '--output',
         type=str,
+        default=os.path.join(os.environ['HOME'], 'ACSIS'),
         help='Output directory [FULLPATH]')
     parser.add_argument(
         '-l',
@@ -69,6 +110,20 @@ def _set_logger(logging, out_dir, log_file, log_level):
     root_logger.addHandler(console_handler)
 
 
+def _make_dir_tree(base_dir):
+    """Create output directory three for storing output."""
+    if not os.path.exists(base_dir):
+        os.makedirs(base_dir)
+    moc_dir = os.path.join(base_dir, 'moc_annual')
+    if not os.path.exists(moc_dir):
+        os.makedirs(moc_dir)
+    greenland_iceland_dir = os.path.join(base_dir, 'greenland_iceland')
+    if not os.path.exists(greenland_iceland_dir):
+        os.makedirs(greenland_iceland_dir)
+
+    return moc_dir, greenland_iceland_dir
+
+
 def _extract_yearly_data(cube):
     """Return yearly data."""
     # get time cells
@@ -80,8 +135,7 @@ def _extract_yearly_data(cube):
     # extract years info
     all_years = []
     for date_obj in time_cells:
-        real_date = datetime(date_obj.year, date_obj.month,
-                             date_obj.day, 0, 0)
+        real_date = datetime(date_obj.year, date_obj.month, date_obj.day, 0, 0)
         all_years.append(real_date.year)
 
     # compute means
@@ -96,29 +150,90 @@ def _extract_yearly_data(cube):
     return yearly_cube, measurements
 
 
-def _yearly_mean(data_file, variable, var_constraint):
+def _yearly_mean(data_file, output_dir, variable, var_constraint):
     """Process needed data."""
     cube = iris.load(data_file, constraints=var_constraint)[0]
     datas, no_measurements = _extract_yearly_data(cube)
+    data_dict = {}
     for y_r, ydat in datas.items():
-        logger.info('For year %s : %s number of measurements: %s',
-                    y_r, variable, no_measurements[y_r])
+        logger.info('For year %s : %s number of measurements: %s', y_r,
+                    variable, no_measurements[y_r])
         moc_yearly_mean = np.ma.mean(ydat.data)
-        logger.info('For year %s : %s yearly average: %s',
-                    y_r, variable, moc_yearly_mean)
+        logger.info('For year %s : %s yearly average: %s', y_r, variable,
+                    moc_yearly_mean)
+        data_dict[float(y_r)] = moc_yearly_mean
+
+    # assemble output cube and save it
+    data_dict = collections.OrderedDict(sorted(data_dict.items()))
+    times = iris.coords.DimCoord(
+        np.array([f for f in data_dict.keys()]),
+        standard_name='time',
+        units='years')
+    cspec = [(times, 0)]
+    stats_cube = iris.cube.Cube(
+        np.array([f for f in data_dict.values()]),
+        dim_coords_and_dims=cspec,
+        long_name=variable)
+    cube_name = '_'.join((variable, 'AnnualMean.nc'))
+    logger.info('Saving %s', os.path.join(output_dir, cube_name))
+    iris.save(stats_cube, os.path.join(output_dir, cube_name))
 
 
-def _djf_greenland_iceland(data_file, var_constraint, season):
+def _djf_greenland_iceland(data_file, output_dir, var_constraint, season):
     """Get the DJF mean for Greenland-Iceland."""
+    season = SEASON
     cube = iris.load(data_file, constraints=var_constraint)[0]
-    greenland = extract_region(cube, 25., 35., 30., 40.)
-    iceland = extract_region(cube, 15., 25., 60., 70.)
-    greenland = greenland.collapsed(['longitude', 'latitude'],
+    greenland_map = extract_region(cube, 25., 35., 30., 40.)
+    iceland_map = extract_region(cube, 15., 25., 60., 70.)
+    greenland = greenland_map.collapsed(['longitude', 'latitude'],
+                                        iris.analysis.MEAN)
+    iceland = iceland_map.collapsed(['longitude', 'latitude'],
                                     iris.analysis.MEAN)
-    iceland = iceland.collapsed(['longitude', 'latitude'],
-                                iris.analysis.MEAN)
+    # get cubes of interest
+    greenland_djf = extract_season(greenland, season)
+    iceland_djf = extract_season(iceland, season)
     diff = greenland - iceland
     season_geo_diff = extract_season(diff, season)
+    # save to disk
+    logger.info('%s: Saving Greenland in %s', AIRPRESS_VARIABLE, output_dir)
+    iris.save(
+        greenland_map,
+        os.path.join(output_dir,
+                     ('').join(['Greenland_', AIRPRESS_VARIABLE, '.nc'])))
+    logger.info('%s: Saving Iceland in %s', AIRPRESS_VARIABLE, output_dir)
+    iris.save(
+        iceland_map,
+        os.path.join(output_dir,
+                     ('').join(['Iceland_', AIRPRESS_VARIABLE, '.nc'])))
+    logger.info('%s: Saving Greenland Mean in %s', AIRPRESS_VARIABLE,
+                output_dir)
+    iris.save(
+        greenland,
+        os.path.join(output_dir,
+                     ('').join(['Greenland_Mean_', AIRPRESS_VARIABLE, '.nc'])))
+    logger.info('%s: Saving Iceland Mean in %s', AIRPRESS_VARIABLE, output_dir)
+    iris.save(
+        iceland,
+        os.path.join(output_dir,
+                     ('').join(['Iceland_Mean_', AIRPRESS_VARIABLE, '.nc'])))
+    logger.info('%s: Saving Greenland Mean %s in %s', AIRPRESS_VARIABLE,
+                season, output_dir)
+    iris.save(
+        greenland_djf,
+        os.path.join(output_dir, ('').join(
+            ['Greenland_Mean_DJF_', AIRPRESS_VARIABLE, '.nc'])))
+    logger.info('%s: Saving Iceland Mean %s in %s', AIRPRESS_VARIABLE, season,
+                output_dir)
+    iris.save(
+        iceland_djf,
+        os.path.join(output_dir, ('').join(
+            ['Iceland_Mean_DJF_', AIRPRESS_VARIABLE, '.nc'])))
+    logger.info('%s: Saving Greenland - Iceland Mean %s in %s',
+                AIRPRESS_VARIABLE, season, output_dir)
+    iris.save(
+        season_geo_diff,
+        os.path.join(output_dir, ('').join(
+            ['Greenland-Iceland_Mean_DJF_', AIRPRESS_VARIABLE, '.nc'])))
     return season_geo_diff
 
 
@@ -126,35 +241,38 @@ def main():
     """Run the the meat of the code."""
     args = get_args()
 
+    # base dir needs timestamp
+    now = datetime.utcnow().strftime("%Y%m%d_%H%M%S")
+    base_output = '_'.join((args.output, now))
+
+    # build output tree
+    moc_dir, greice_dir = _make_dir_tree(base_output)
+
     # set logging
     log_level = args.log_level
-    _set_logger(logging, '.', 'moc.log',
-                log_level)
+    _set_logger(logging, base_output, 'acsismoc.log', log_level)
     logger.info(HEADER)
     logger.info("Running main function...")
 
-    # get command args
+    # get input files
     moc_file = args.moc_file
     hadslp_file = args.hadslp_file
 
     # compute means and write to file
-
-    # first moc
+    # moc
     moc_constraint = iris.Constraint(
-        cube_func=(lambda c: c.var_name == 'moc_mar_hc10')
-    )
-    _yearly_mean(moc_file, 'moc_mar_hc10', moc_constraint)
+        cube_func=(lambda c: c.var_name == MOC_VARIABLE))
+    _yearly_mean(moc_file, moc_dir, MOC_VARIABLE, moc_constraint)
 
-    # then vn405
+    # vn405
     vn_constraint = iris.Constraint(
-        cube_func=(lambda c: c.var_name == 'UM_0_fc8_vn405')
-    )
-    _yearly_mean(hadslp_file, 'UM_0_fc8_vn405', vn_constraint)
+        cube_func=(lambda c: c.var_name == AIRPRESS_VARIABLE))
+    _yearly_mean(hadslp_file, moc_dir, AIRPRESS_VARIABLE, vn_constraint)
 
-    # now get greenland-iceland
-    gre_ic_djf = _djf_greenland_iceland(hadslp_file, vn_constraint, 'DJF')
-    iris.save(gre_ic_djf, os.path.join(args.output,
-                                       'DJF_GreIce_UM_0_fc8_vn405.nc'))
+    # greenland-iceland
+    gre_ic_djf = _djf_greenland_iceland(hadslp_file, greice_dir, vn_constraint,
+                                        SEASON)
+    logger.debug('Greenland - Iceland Mean DJF cube %s', gre_ic_djf)
 
 
 if __name__ == '__main__':
