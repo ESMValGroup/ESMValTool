52eb0f46e6866fda3b072a291afd0a5a02d8a5b7
Björn Brötz <bjoern.broetz@dlr.de>
2018-11-30 22:32:06 +0100

    Fix codacy issues

--- esmvaltool/diag_scripts/cvdp/cvdp_wrapper.py
+++ esmvaltool/diag_scripts/cvdp/cvdp_wrapper.py
@@ -1,13 +1,11 @@
-"""wrapper diagnostic for the NCAR CVDP (p)ackage.  """
+"""wrapper diagnostic for the NCAR CVDP (p)ackage."""
 import logging
 import os
 import re
 import subprocess
-from pprint import pformat
 
 from esmvaltool.diag_scripts.shared import (group_metadata, run_diagnostic,
-                                            select_metadata, sorted_metadata)
-from esmvaltool.diag_scripts.shared.plot import quickplot
+                                            select_metadata)
 from esmvaltool._task import DiagnosticError
 
 logger = logging.getLogger(os.path.basename(__file__))
@@ -26,66 +24,65 @@ def setup_driver(cfg):
     if not os.path.isdir(cvdp_root):
         raise DiagnosticError("CVDP is not available.")
 
-    SETTINGS = {
+    settings = {
         'outdir': "{0}/".format(cfg['work_dir']),
         'obs': 'False',
         'zp': os.path.join(cvdp_root, "ncl_scripts/"),
         'run_style': 'serial',
         'webpage_title': 'CVDP run via ESMValTool'
     }
-    SETTINGS['output_data'] = "True" if _nco_available() else "False"
+    settings['output_data'] = "True" if _nco_available() else "False"
 
     def _update_settings(line):
 
-        for k, v in SETTINGS.items():
-            pattern = r'\s*{0}\s*=.*\n'.format(k)
-            s = re.findall(pattern, line)
-            if s == []:
+        for key, value in settings.items():
+            pattern = r'\s*{0}\s*=.*\n'.format(key)
+            search_results = re.findall(pattern, line)
+            if search_results == []:
                 continue
-            return re.sub(r'".+?"', '"{0}"'.format(v), s[0], count=1)
+            return re.sub(
+                r'".+?"', '"{0}"'.format(value), search_results[0], count=1)
 
         return line
 
     content = []
     driver = os.path.join(cvdp_root, "driver.ncl")
 
-    with open(driver, 'r') as f:
-        for line in f:
+    with open(driver, 'r') as driver_file:
+        for line in driver_file:
             content.append(_update_settings(line))
 
     new_driver = os.path.join(cfg['work_dir'], "driver.ncl")
 
-    with open(new_driver, 'w') as f:
-        f.write("".join(content))
+    with open(new_driver, 'w') as new_driver_file:
+        new_driver_file.write("".join(content))
 
 
-def create_link(cfg, p):
+def create_link(cfg, inpath):
     """Create link for the input file that matches the naming convention
     of the cvdp package. Return the path to the link.
 
     cfg: configuration dict
-    p: path to infile
+    inpath: path to infile
 
     """
 
-    def _create_link_name(p):
-        import re
-        h, t = os.path.split(p)
-        s = re.search(r'[0-9]{4}-[0-9]{4}', t).group(0)
-        return t.replace(s, "{0}01-{1}12".format(*s.split('-')))
+    def _create_link_name(inpath):
+        head, tail = os.path.split(inpath)
+        search_result = re.search(r'[0-9]{4}-[0-9]{4}', tail).group(0)
+        return t.replace(search_result,
+                         "{0}01-{1}12".format(*search_result.split('-')))
 
-    if not os.path.isdir(p):
-        # TODO: Exception if p doesn't exists
-        # raise DiagnosticError("Path {0} does not exist".format(p))
-        logger.debug("Path %s does not exist! Continue", p)
+    if not os.path.isdir(inpath):
+        raise DiagnosticError("Path {0} does not exist".format(inpath))
 
     lnk_dir = os.path.join(cfg['work_dir'], "links")
 
     if not os.path.isdir(lnk_dir):
         os.mkdir(lnk_dir)
 
-    link = os.path.join(lnk_dir, _create_link_name(p))
-    os.symlink(p, link)
+    link = os.path.join(lnk_dir, _create_link_name(inpath))
+    os.symlink(inpath, link)
 
     return link
 
@@ -97,25 +94,26 @@ def setup_namelist(cfg):
     grouped_selection = group_metadata(selection, 'dataset')
 
     content = []
-    for k, v in grouped_selection.items():
-        links = [create_link(cfg, item["filename"]) for item in v]
+    for key, value in grouped_selection.items():
+        links = [create_link(cfg, item["filename"]) for item in value]
         head, tail = os.path.split(links[0])
         head, tail = os.path.split(head)
         tail = "_".join(tail.split('_')[:-1])
         ppath = "{}*/".format(os.path.join(head, tail))
         content.append("{0} | {1} | {2} | {3}\n".format(
-            k, ppath, v[0]["start_year"], v[0]["end_year"]))
+            key, ppath, value[0]["start_year"], v[0]["end_year"]))
 
     namelist = os.path.join(cfg['work_dir'], "namelist")
 
-    with open(namelist, 'w') as f:
-        f.write("\n".join(content))
+    with open(namelist, 'w') as namelist_file:
+        namelist_file.write("\n".join(content))
 
 
 def log_functions(func):
-    """Decorater for check functions."""
+    """Decorater to check functions."""
 
     def inner():
+        """Inner function."""
         ret = func()
         logger.debug("Function %s returns %s", func.__name__, str(ret))
         return ret
@@ -132,7 +130,7 @@ def _nco_available():
             return False
         else:
             return True
-    except OSError as e:
+    except OSError:
         return False
 
 
