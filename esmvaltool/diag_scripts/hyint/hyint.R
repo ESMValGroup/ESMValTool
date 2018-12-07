7ffa0e9afbdec94198564952f6107fae3a8578d5
Enrico Arnone <e.arnone@isac.cnr.it>
2018-12-07 18:20:38 +0100

    styling and removed unused functions

--- esmvaltool/diag_scripts/hyint/hyint.R
+++ esmvaltool/diag_scripts/hyint/hyint.R
@@ -1,52 +1,52 @@
 # #############################################################################
 # hyint.R
 # Authors:       E. Arnone (ISAC-CNR, Italy)
-#                J. von Hardenberg (ISAC-CNR, Italy) 
+#                J. von Hardenberg (ISAC-CNR, Italy)
 # #############################################################################
 # Description
-# HyInt is a tool for calculation of the HY-INT index (Giorgi et al. 2011) 
+# HyInt is a tool for calculation of the HY-INT index (Giorgi et al. 2011)
 # and additional hydroclimatic indices (Giorgi et al. 2014)
-# which allow an estimate of the overall behaviour of the hydroclimatic cycle. 
-# The tool calculates also timeseries and trends over selected regions and 
+# which allow an estimate of the overall behaviour of the hydroclimatic cycle.
+# The tool calculates also timeseries and trends over selected regions and
 # produces a variety of types of plots including maps and timeseries. The timeseries/trend
 # and plotting modules handle also ETCCDI indices data calculated with the climdex library through
 # an ad hoc pre-processing.
-#  
+#
 # Details
 # The following indices are calculated based on input daily precipitation data:
 # PRY = mean annual precipitation
 # INT = mean annual precipitation intensity (intensity during wet days, or simple precipitation intensity index SDII)
 # WSL = mean annual wet spell length (number of consecutive days during each wet spell)
 # DSL = mean annual dry spell lenght (number of consecutive days during each dry spell)
-# PA  = precipitation area (area over which of any given day precipitation occurs) 
+# PA  = precipitation area (area over which of any given day precipitation occurs)
 # R95 = heavy precipitation index (percent of total precipitation above the 95% percentile of the reference distribution)
 # HY-INT = hydroclimatic intensity. HY-INT = normalized(INT) x normalized(DSL).
 #
-# For EC-Earth data and then extended to any model and observational data, producing plots 
-# of data vs. a reference dataset (e.g. ERA-INTERIM). Indices are normalized over a reference 
-# period. Both absolute and normalized values are made available: users can select the indices 
+# For EC-Earth data and then extended to any model and observational data, producing plots
+# of data vs. a reference dataset (e.g. ERA-INTERIM). Indices are normalized over a reference
+# period. Both absolute and normalized values are made available: users can select the indices
 # to be stored and plotted. The tool makes extensives use of the cfg_hyint configuration file
-# for user selectable options and ease feeding needed inputs (e.g. region boundaries for timeseries 
-# or value ranges and labels for figures).     
-# 
+# for user selectable options and ease feeding needed inputs (e.g. region boundaries for timeseries
+# or value ranges and labels for figures).
+#
 # Required
-# It reads daily precipitation data through ESMValTool. If requested, input precipitation data are pre-processed 
-# interpolating on a common grid set by the user in the hyint_parameters file.  
+# It reads daily precipitation data through ESMValTool. If requested, input precipitation data are pre-processed
+# interpolating on a common grid set by the user in the hyint_parameters file.
 # R libraries:"tools","PCICt","ncdf4","maps"
 #
-# Optional 
+# Optional
 # Several options can be selected via the configuration file, e.g. the provision of an
-# external normalization functions for the indices; a reference climatology for the R95 index; type of plots; etc. 
+# external normalization functions for the indices; a reference climatology for the R95 index; type of plots; etc.
 #
 # Caveats
-# Spatial data selection based on elevation works only with regridding at 320x160 (or by producing by hand grid files at needed resolution) 
+# Spatial data selection based on elevation works only with regridding at 320x160 (or by producing by hand grid files at needed resolution)
 #
 # Modification history
 #    20181001-A_arno_en: converted to latest v2.0
-#    20180302-A_arno_en: converted to ESMValTool2  
-#    20171206-A_arno_en: modularized version accepting climdex indices  
-#    20171010-A_arno_en: modularized version  
-#    20170901-A_arno_en: 1st github version  
+#    20180302-A_arno_en: converted to ESMValTool2
+#    20171206-A_arno_en: modularized version accepting climdex indices
+#    20171010-A_arno_en: modularized version
+#    20170901-A_arno_en: 1st github version
 #
 # ############################################################################
 
@@ -56,38 +56,35 @@ library(yaml)
 
 spath <- "esmvaltool/diag_scripts/hyint/"
 
- # get path to script and source subroutines (if needed)
-spath <- paste0((dirname((unlist(strsplit((commandArgs(trailingOnly = FALSE))[6],"="))[2]))),"/")
+# get path to script and source subroutines (if needed)
+args <- commandArgs(trailingOnly = FALSE)
+spath <- paste0(dirname(unlist(strsplit(grep("--file", args, value = TRUE), "="))[2]), "/")
 
 # diag_scripts_dir <- Sys.getenv("diag_scripts")
 # source paste0(diag_scripts_dir,"/subroutine.r")
 
-source(paste0(spath,'hyint_functions.R'))
-source(paste0(spath,'hyint_metadata.R'))
-source(paste0(spath,'hyint_preproc.R'))
-source(paste0(spath,'hyint_diagnostic.R'))
-source(paste0(spath,'hyint_etccdi_preproc.R'))
-source(paste0(spath,'hyint_trends.R'))
-source(paste0(spath,'hyint_plot_maps.R'))
-source(paste0(spath,'hyint_plot_trends.R'))
-source(paste0(spath,'hyint_parameters.R'))
-
-## Do not print warnings
-#options(warn=0)var0 <- names(metadata)[1]
+source(paste0(spath, "hyint_functions.R"))
+source(paste0(spath, "hyint_metadata.R"))
+source(paste0(spath, "hyint_preproc.R"))
+source(paste0(spath, "hyint_diagnostic.R"))
+source(paste0(spath, "hyint_etccdi_preproc.R"))
+source(paste0(spath, "hyint_trends.R"))
+source(paste0(spath, "hyint_plot_maps.R"))
+source(paste0(spath, "hyint_plot_trends.R"))
+source(paste0(spath, "hyint_parameters.R"))
 
 # Read settings and metadata files
 args <- commandArgs(trailingOnly = TRUE)
 settings_file <- args[1]
-# settings_file="/work/users/arnone/esmvaltool_output/namelist_hyint_egu2018_1_20180322_222547/run/hyint/main/settings.yml"
-# settings_file="/work/users/arnone/esmvaltool_output/namelist_hyint_egu2018_20180416_162130/run/hyint/main/settings.yml"
 # settings_file <- "/work/users/arnone/esmvaltool_output/recipe_hyint_20181206_144312/run/hyint/main/settings.yml"
 settings <- yaml::read_yaml(settings_file)
 
-# load data from settings 
+# load data from settings
 for (myname in names(settings)) {
   temp <- get(myname, settings)
   assign(myname, temp)
 }
+
 metadata <- yaml::read_yaml(settings$input_files)
 
 # get name of climofile for selected variable and list associated to first climofile
@@ -101,21 +98,16 @@ varname <- paste0("'", climolist$short_name, "'")
 var0 <- varname
 var0 <- "pr"
 
-diag_base = climolist0$diagnostic
-print(paste(diag_base,": starting routine"))
+diag_base <- climolist0$diagnostic
+print(paste0(diag_base, ": starting routine"))
 
-if (length(etccdi_dir) != 1) {etcddi_dir = work_dir}
-regridding_dir=run_dir
-dir.create(plot_dir, recursive=T,  showWarnings = F)
-dir.create(work_dir, recursive=T, showWarnings = F)
-dir.create(regridding_dir, recursive=T, showWarnings = F)
-
-#models_name=unname(sapply(list0, '[[', 'dataset'))
-#reference_model=unname(sapply(list0, '[[', 'reference_dataset'))[1]
-#models_start_year=unname(sapply(list0, '[[', 'start_year'))
-#models_end_year=unname(sapply(list0, '[[', 'end_year'))
-#models_experiment=unname(sapply(list0, '[[', 'exp'))
-#models_ensemble=unname(sapply(list0, '[[', 'ensemble'))
+if (length(etccdi_dir) != 1) {
+  etcddi_dir <- work_dir
+}
+regridding_dir <- run_dir
+dir.create(plot_dir, recursive = T, showWarnings = F)
+dir.create(work_dir, recursive = T, showWarnings = F)
+dir.create(regridding_dir, recursive = T, showWarnings = F)
 
 # extract metadata
 models_name <- unname(sapply(metadata, "[[", "dataset"))
@@ -125,54 +117,43 @@ models_end_year <- unname(sapply(metadata, "[[", "end_year"))
 models_experiment <- unname(sapply(metadata, "[[", "exp"))
 models_ensemble <- unname(sapply(metadata, "[[", "ensemble"))
 
+# select reference dataset; if not available, use last of list
+ref_idx <- which(models_name == reference_model) 
+if (length(ref_idx) == 0) {
+  ref_idx <- length(models_name)
+}
 
-
-
-# select reference model
-ref_idx=which(models_name == reference_model) # select reference dataset; if not available, use last of list
-if(length(ref_idx)==0) { ref_idx=length(models_name)}
-
-# setup reference grid file if needed
-if (rgrid == "REF") {
-  climofile_ref <- climofiles[ref_idx]
-  grid_file_ref = paste0(grid_file,ref_idx)
-  # generate grid file
-  grid_command<-paste("cdo griddes ",climofile_ref," > ",grid_file_ref)
-  system(grid_command)
-  print(paste(diag_base,": reference grid file ",grid_file_ref))
-  # update rgrid tag with actual grid size from grid file
-  rgrid = get.cdo.res(grid_file_ref) 
-} 
- 
 ## Run regridding and diagnostic
 if (write_netcdf) {
-  
-  # loop through models 
+
+  # loop through models
   for (model_idx in c(1:(length(models_name)))) {
 
-    # Setup filenames 
-    climofile <- climofiles[model_idx] 
-    sgrid <- "noregrid"; if (rgrid != F) {sgrid <- rgrid}
-    regfile <- getfilename.regridded(regridding_dir,sgrid,var0,model_idx)
+    # Setup filenames
+    climofile <- climofiles[model_idx]
+    sgrid <- "noregrid"
+    if (rgrid != F) {
+      sgrid <- rgrid
+    }
+    regfile <- getfilename.regridded(regridding_dir, sgrid, var0, model_idx)
 
-    # If needed, pre-process file regridding, selecting lon/lat region of interest and adding absolute time axis 
+    # If needed, pre-process file regridding, selecting lon/lat region of interest and adding absolute time axis
     if (run_regridding) {
-      if(!file.exists(regfile) | force_regridding) { 
-        dummy=hyint.preproc(work_dir,model_idx,ref_idx,climofile,regfile,rgrid)
-      } else {   
-        gridfile=getfilename.indices(work_dir,diag_base,model_idx,grid=T)
-        grid_command<-paste("cdo griddes ",regfile," > ",gridfile)
+      if (!file.exists(regfile) | force_regridding) {
+        dummy <- hyint.preproc(work_dir, model_idx, climofile, regfile)
+      } else {
+        gridfile <- getfilename.indices(work_dir, diag_base, model_idx, grid = T)
+        grid_command <- paste("cdo griddes ", regfile, " > ", gridfile)
         system(grid_command)
-        print(paste0(diag_base,": data file exists: ", regfile))  
-        print(paste0(diag_base,": corresponding grid: ", gridfile))  
+        print(paste0(diag_base, ": data file exists: ", regfile))
+        print(paste0(diag_base, ": corresponding grid: ", gridfile))
       }
     }
 
-    if (run_diagnostic) {    
+    if (run_diagnostic) {
       # Loop through seasons and call diagnostic
       for (seas in seasons) {
-        
-        hyint.diagnostic(work_dir,regfile,model_idx,seas,rewrite=force_diagnostic)
+        hyint.diagnostic(work_dir, regfile, model_idx, seas, rewrite = force_diagnostic)
       }
     }
   }
@@ -181,31 +162,35 @@ if (write_netcdf) {
 ## Preprocess ETCCDI input files and merge them with HyInt indices
 if (write_netcdf & etccdi_preproc) {
   for (model_idx in c(1:(length(models_name)))) {
-    gridfile=getfilename.indices(work_dir,diag_base,model_idx,grid=T)
-    #grid_file_idx=paste0(grid_file,model_idx)
-    dummy<-hyint.etccdi.preproc(work_dir,etccdi_dir,etccdi_list_import,gridfile,model_idx,"ALL",yrmon="yr")
+    gridfile <- getfilename.indices(work_dir, diag_base, model_idx, grid = T)
+    # grid_file_idx=paste0(grid_file,model_idx)
+    dummy <- hyint.etccdi.preproc(work_dir, etccdi_dir, etccdi_list_import, gridfile, model_idx, "ALL", yrmon = "yr")
   }
 }
 
 ## Calculate timeseries and trends
-if (write_netcdf & run_timeseries) { 
+if (write_netcdf & run_timeseries) {
   for (model_idx in c(1:(length(models_name)))) {
     for (seas in seasons) {
-      hyint.trends(work_dir,model_idx,seas) 
+      hyint.trends(work_dir, model_idx, seas)
     }
   }
 }
 
 ## Create figures
-if (write_plots) { 
-  ref_idx=which(models_name == reference_model) # select reference dataset; if not available, use last of list
-  if(length(ref_idx)==0) { ref_idx=length(models_name)}
+if (write_plots) {
+  ref_idx <- which(models_name == reference_model) # select reference dataset; if not available, use last of list
+  if (length(ref_idx) == 0) {
+    ref_idx <- length(models_name)
+  }
   for (seas in seasons) {
     if (plot_type <= 10) { # Plot maps
-      hyint.plot.maps(work_dir,plot_dir,work_dir,ref_idx,seas) 
-  } else { # Plot timeseries and trends
-      print("HyInt: calling plot.trend")  
-      hyint.plot.trends(work_dir,plot_dir,work_dir,ref_idx,seas)  
+      hyint.plot.maps(work_dir, plot_dir, work_dir, ref_idx, seas)
+    } else { # Plot timeseries and trends
+      print("HyInt: calling plot.trend")
+      hyint.plot.trends(work_dir, plot_dir, work_dir, ref_idx, seas)
     }
   }
 }
+
+print(paste0(diag_base, ": done."))
--- esmvaltool/diag_scripts/hyint/hyint_preproc.R
+++ esmvaltool/diag_scripts/hyint/hyint_preproc.R
@@ -3,43 +3,19 @@
 #-------------E. Arnone (Oct 2017)-------------------#
 ######################################################
 
-hyint.preproc<-function(work_dir,model_idx,ref_idx,climofile,regfile,rgrid) {
+hyint.preproc <- function(work_dir, model_idx, climofile, regfile) {
+  print(paste0(diag_base, ": pre-processing file: ", climofile))
 
-#  absolute axis, remove leap year days, select lonlat box convert to NetCDF4
-
-  # Manage gridding options
-  sgrid<-""
-  if (rgrid !=F) {sgrid <- paste0("-remapcon2,", rgrid)}
-
-  # regrid/reformat climo file
-  print(paste0(diag_base,": pre-processing file: ", climofile))
-
-  cdo_command<-paste(paste0("cdo -L -f nc -a -delete,month=2,day=29"), sgrid, climofile, paste0(regfile,"regtmp"))
-  print(cdo_command)
+  #  add absolute axis, remove leap year days
+  cdo_command <- paste(paste0("cdo -L -f nc -a -delete,month=2,day=29"), climofile, regfile)
   system(cdo_command)
 
-  cdo_command2<-paste("mv ",paste0(regfile,"regtmp")," ",paste0(regfile,"regtmp1"))
-  if (rlonlatdata[1]!=F) {
-    cdo_command2<-paste0("cdo sellonlatbox,",paste(rlonlatdata,sep="",collapse=",")," ",paste0(regfile,"regtmp")," ",paste0(regfile,"regtmp1"))
-  }
-  print(cdo_command2)
-  system(cdo_command2)
-
-  cdo_command3<-paste("cdo -f nc4 -copy ", paste0(regfile,"regtmp1"), regfile)
-  print(cdo_command3)
-  system(cdo_command3)
-
-  rm_command<-paste("rm ", paste0(regfile,"regtmp*"))
-  print(rm_command)
-  system(rm_command)
-
   # generate grid file
-  gridfile<-getfilename.indices(work_dir,diag_base,model_idx,grid=T)
-  grid_command<-paste("cdo griddes ",regfile," > ",gridfile)  
-  print(grid_command)
+  gridfile <- getfilename.indices(work_dir, diag_base, model_idx, grid = T)
+  grid_command <- paste("cdo griddes ", regfile, " > ", gridfile)
   system(grid_command)
 
-  print(paste0(diag_base,": pre-processed file: ", regfile))
+  print(paste0(diag_base, ": pre-processed file: ", regfile))
 
-return(0)
+  return(0)
 }
