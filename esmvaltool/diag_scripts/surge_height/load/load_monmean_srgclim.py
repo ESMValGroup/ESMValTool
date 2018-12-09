ad0938e39652c2a08ef682b0fffa365955d692cb
riddernina <riddernina@web.de>
2018-12-09 10:45:49 +0100

    bug fixes

--- esmvaltool/diag_scripts/surge_height/load/load_monmean_srgclim.py
+++ esmvaltool/diag_scripts/surge_height/load/load_monmean_srgclim.py
@@ -1,16 +1,17 @@
 import xarray as xr
 import os
+import numpy as np
 
 def load_monmean_srgclim(stat,data_dir):
     filename = 'monmean_ERAintWAQUA_surge_1979-2016_speed.nc'
     
     ncpath = os.path.join(data_dir, filename)
-    nc_srg_anom = xr.open_dataset(ncpath, 'r')
+    nc_srg_anom = xr.open_dataset(ncpath)
     srg_anom = nc_srg_anom.WAQUA_surge
 
     monmean_srgclim = {}
-    for ifname in stat:
-        monmean_srgclim[str(ifname)] = srg_anom.sel(stations=s)
+    for s in stat:
+        monmean_srgclim[str(ifname)] = srg_anom.sel(stations=np.bytes_(s.ljust(8)))
 
         #ncpath = os.path.join(data_dir, filename + str(ifname) + '.nc')
         #nc = Dataset(ncpath, 'r')
