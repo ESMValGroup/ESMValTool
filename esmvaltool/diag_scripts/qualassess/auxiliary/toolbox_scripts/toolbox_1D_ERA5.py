65f8bff34bc0d3fadc003a50a4ad90e991de2e4c
Birgit Hassler <33543691+hb326@users.noreply.github.com>
2018-12-11 10:17:43 +0100

    Create toolbox_1D_ERA5.py

---
+++ esmvaltool/diag_scripts/qualassess/auxiliary/toolbox_scripts/toolbox_1D_ERA5.py
@@ -0,0 +1,22 @@
+import cdstoolbox as ct
+
+@ct.application(title='Retrieve Data')
+@ct.output.dataarray()
+def retrieve_sample_data():
+    """
+    Retrieve a variable from sample dataset.
+    """
+
+    data = ct.catalogue.retrieve(
+    'reanalysis-era5-single-levels',
+    {
+        'variable':'sea_surface_temperature',
+        'product_type':'reanalysis',
+        'year':'2017',
+        'month':'04',
+        'day':'10',
+        'time':'08:00'
+    }
+)
+    datas = ct.climate.daily_mean(data)
+return datas
