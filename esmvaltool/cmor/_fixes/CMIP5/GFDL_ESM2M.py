0760be2196c18b65e10cef549700336a2a37e054
Manuel Schlund <manuel.schlund@dlr.de>
2018-12-12 16:25:53 +0100

    Started implementation of tos (needed irregular regridding)

--- esmvaltool/cmor/_fixes/CMIP5/GFDL_ESM2M.py
+++ esmvaltool/cmor/_fixes/CMIP5/GFDL_ESM2M.py
@@ -1,15 +1,16 @@
-"""Fixes for GFDL ESM2M"""
-from cf_units import Unit
+"""Fixes for GFDL ESM2M."""
+from shutil import copyfile
+
+from netCDF4 import Dataset
 
 from ..fix import Fix
 
 
 class sftof(Fix):
-    """Fixes for sftof"""
+    """Fixes for sftof."""
 
     def fix_data(self, cube):
-        """
-        Fix data
+        """Fix data.
 
         Fixes discrepancy between declared units and real units
 
@@ -29,13 +30,12 @@ class sftof(Fix):
 
 
 class co2(Fix):
-    """Fixes for co2"""
+    """Fixes for co2."""
 
     def fix_data(self, cube):
-        """
-        Fix data
+        """Fix data.
 
-        Fixes discrepancy between declared units and real units
+        Fixes discrepancy between declared units and real units.
 
         Parameters
         ----------
@@ -49,4 +49,38 @@ class co2(Fix):
         metadata = cube.metadata
         cube *= 1e6
         cube.metadata = metadata
+
         return cube
+
+
+class tos(Fix):
+    """Fixes for tos."""
+
+    def fix_file(self, filepath, output_dir):
+        """Apply fixes to the files prior to creating the cube.
+
+        Should be used only to fix errors that prevent loading or can
+        not be fixed in the cube (i.e. those related with missing_value
+        and _FillValue or missing standard_name).
+
+        Parameters
+        ----------
+        filepath: basestring
+            file to fix.
+        output_dir: basestring
+            path to the folder to store the fix files, if required.
+
+        Returns
+        -------
+        basestring
+            Path to the corrected file. It can be different from the original
+            filepath if a fix has been applied, but if not it should be the
+            original filepath.
+
+        """
+        new_path = Fix.get_fixed_filepath(output_dir, filepath)
+        copyfile(filepath, new_path)
+        dataset = Dataset(new_path, 'a')
+        dataset.variables['tos'].standard_name = 'sea_surface_temperature'
+        dataset.close()
+        return new_path
