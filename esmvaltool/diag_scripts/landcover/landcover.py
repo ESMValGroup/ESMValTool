b89a6866a17e4673181369187ee0693adf4af315
Tobias Stacke <tobias.stacke@mpimet.mpg.de>
2018-12-12 16:32:03 +0100

    Style corrections

--- esmvaltool/diag_scripts/landcover/landcover.py
+++ esmvaltool/diag_scripts/landcover/landcover.py
@@ -43,7 +43,6 @@ import pdb
 import iris
 import esmvaltool.diag_scripts.shared as diag
 
-
 logger = logging.getLogger(os.path.basename(__file__))
 
 
@@ -66,18 +65,28 @@ def write_plotdata(cfg, regnam, modnam, values, var):
     """
 
     # Header information for different metrics
-    filehead = {'area': 'Accumulated land coverage for '+var+' in different regions [1.0e+6 km2]',
-                'frac': 'Average land cover fraction for '+var+' in different regions [%]',
-                'bias': 'Bias in average land cover fraction for '+var+' compared to reference [%]'}
+    filehead = {
+        'area':
+        'Accumulated land coverage for ' + var +
+        ' in different regions [1.0e+6 km2]',
+        'frac':
+        'Average land cover fraction for ' + var + ' in different regions [%]',
+        'bias':
+        'Bias in average land cover fraction for ' + var +
+        ' compared to reference [%]'
+    }
     # Write experiment data
     for metric in values.keys():
-        filepath = os.path.join(cfg[diag.names.WORK_DIR], '_'.join([metric,var]) + '.txt')
+        filepath = os.path.join(cfg[diag.names.WORK_DIR],
+                                '_'.join([metric, var]) + '.txt')
         ncol = len(regnam)
         with open(filepath, 'w') as f:
             header = '{:35} ' + ncol * ' {:>12}' + '\n'
             body = '{:35} ' + ncol * ' {:12.4f}' + '\n'
-            line = [' ',] + regnam
-            f.write(filehead[metric]+'\n\n')
+            line = [
+                ' ',
+            ] + regnam
+            f.write(filehead[metric] + '\n\n')
             f.write(header.format(*line))
             for ir, row in enumerate(values[metric]):
                 line = [modnam[metric][ir]] + row
@@ -112,33 +121,48 @@ def make_landcover_bars(cfg, regnam, modnam, values, var):
     outtype = cfg.get('output_file_type', 'png')
     logger.info('Generating plots for filetype: ' + outtype)
 
-    nicename = {'baresoilFrac': 'bare soil covered', 'treeFrac': 'tree covered', 'grassFrac': 'grass covered',
-                'cropFrac': 'crop covered', 'shrubFrac': 'shrub covered'}
-    plottitle = {'area': ' '.join(['Accumulated',nicename.get(var,var),'area']),
-                 'frac': ' '.join(['Average',nicename.get(var,var),'fraction']),
-                 'bias': ' '.join(['Average',nicename.get(var,var),'fraction bias'])}
-    ylabel = {'area': r'Area [$10^6$ km$^2$]',
-              'frac': r'Fraction [%]',
-              'bias': r'Bias [%]'}
+    nicename = {
+        'baresoilFrac': 'bare soil covered',
+        'treeFrac': 'tree covered',
+        'grassFrac': 'grass covered',
+        'cropFrac': 'crop covered',
+        'shrubFrac': 'shrub covered'
+    }
+    plottitle = {
+        'area': ' '.join(['Accumulated',
+                          nicename.get(var, var), 'area']),
+        'frac': ' '.join(['Average',
+                          nicename.get(var, var), 'fraction']),
+        'bias': ' '.join(['Average',
+                          nicename.get(var, var), 'fraction bias'])
+    }
+    ylabel = {
+        'area': r'Area [$10^6$ km$^2$]',
+        'frac': r'Fraction [%]',
+        'bias': r'Bias [%]'
+    }
 
     # Create pdf in case of pdf output
     if outtype == 'pdf':
-        filepath = os.path.join(cfg[diag.names.PLOT_DIR],'_'.join(['metrics',var]) + '.' + outtype)
+        filepath = os.path.join(cfg[diag.names.PLOT_DIR],
+                                '_'.join(['metrics', var]) + '.' + outtype)
         pdf = PdfPages(filepath)
 
     # Loop over metrices
     for metric in values.keys():
-        filepath = os.path.join(cfg[diag.names.PLOT_DIR],'_'.join([metric,var]) + '.' + outtype)
+        filepath = os.path.join(cfg[diag.names.PLOT_DIR],
+                                '_'.join([metric, var]) + '.' + outtype)
         fig, ax = plt.subplots(nrows=1, ncols=1, sharex=False)
         ax.set_title(plottitle[metric])
         ax.set_ylabel(ylabel[metric])
         nbar, ncat = np.array(values[metric]).shape
-        index = np.arange(0, (nbar+1)*ncat, nbar+1)
-        xticks = np.linspace((nbar+1) / 2.0, (nbar+1)*ncat - (nbar+1) / 2.0, ncat) - 1.0
+        index = np.arange(0, (nbar + 1) * ncat, nbar + 1)
+        xticks = np.linspace((nbar + 1) / 2.0,
+                             (nbar + 1) * ncat - (nbar + 1) / 2.0, ncat) - 1.0
         ax.set_xticklabels(regnam)
         ax.set_xticks(xticks)
         for ir, row in enumerate(values[metric]):
-            ax.bar(index+ir, row)
+            ax.bar(index + ir, row)
 
         fig.subplots_adjust(bottom=0.20)
         caxe = fig.add_axes([0.05, 0.01, 0.9, 0.20])
@@ -199,9 +223,13 @@ def main(cfg):
         # Compute long term mean
         mean_cube = new_cube.collapsed([diag.names.TIME], iris.analysis.MEAN)
         # Rename variable in cube
-        mean_cube._var_name = "_".join([datainfo.get('cmor_table',''),datainfo.get('dataset',''),
-                              datainfo.get('exp',''),datainfo.get('ensemble','')]).replace('__','_').strip("_")
-        mean_cube.long_name = " ".join([var,'for dataset',ds])
+        mean_cube._var_name = "_".join([
+            datainfo.get('cmor_table', ''),
+            datainfo.get('dataset', ''),
+            datainfo.get('exp', ''),
+            datainfo.get('ensemble', '')
+        ]).replace('__', '_').strip("_")
+        mean_cube.long_name = " ".join([var, 'for dataset', ds])
         # Update data for dataset
         datasets.set_data(mean_cube.data, dataset_path)
         # Append to cubelist for temporary output
@@ -223,7 +251,12 @@ def main(cfg):
             logger.info("Writing %s", filepath)
 
     # Compute aggregated area and average fractional coverage for different regions
-    regdef = {'Global': None, 'Tropics': [-30,30], 'North. Hem.': [30,90], 'South. Hem.': [-90,-30]}
+    regdef = {
+        'Global': None,
+        'Tropics': [-30, 30],
+        'North. Hem.': [30, 90],
+        'South. Hem.': [-90, -30]
+    }
     regnam = list(regdef.keys())
     mydata = {key: {} for key in varlist.short_names()}
     for var in allcubes.keys():
@@ -236,7 +269,8 @@ def main(cfg):
             modnam['frac'].append(dataset_name)
             cellarea = sub_cube.copy()
             cellarea.name = 'cellarea'
-            cellarea.data = iris.analysis.cartography.area_weights(allcubes[var][0], normalize=False)
+            cellarea.data = iris.analysis.cartography.area_weights(
+                allcubes[var][0], normalize=False)
             row = {'area': [], 'frac': []}
             # Compute land cover area in million km2:
             # area = Percentage * 0.01 * area [m2]
@@ -247,19 +281,33 @@ def main(cfg):
             # Sum over area for different regions
             for reg in regnam:
                 if regdef[reg] is not None:
-                    zone = iris.Constraint(latitude=lambda v: regdef[reg][0] < v < regdef[reg][1])
-                    row['area'].append(coverarea.extract(zone).collapsed(['longitude', 'latitude'], iris.analysis.SUM).data.tolist())
-                    row['frac'].append(sub_cube.extract(zone).collapsed(['longitude', 'latitude'], iris.analysis.MEAN, weights=cellarea.extract(zone).data).data.tolist())
+                    zone = iris.Constraint(
+                        latitude=lambda v: regdef[reg][0] < v < regdef[reg][1])
+                    row['area'].append(
+                        coverarea.extract(zone).collapsed(
+                            ['longitude', 'latitude'],
+                            iris.analysis.SUM).data.tolist())
+                    row['frac'].append(
+                        sub_cube.extract(zone).collapsed(
+                            ['longitude', 'latitude'],
+                            iris.analysis.MEAN,
+                            weights=cellarea.extract(zone).data).data.tolist())
 
                 else:
-                    row['area'].append(coverarea.collapsed(['longitude', 'latitude'], iris.analysis.SUM).data.tolist())
-                    row['frac'].append(sub_cube.collapsed(['longitude', 'latitude'], iris.analysis.MEAN, weights=cellarea.data).data.tolist())
+                    row['area'].append(
+                        coverarea.collapsed(['longitude', 'latitude'],
+                                            iris.analysis.SUM).data.tolist())
+                    row['frac'].append(
+                        sub_cube.collapsed(
+                            ['longitude', 'latitude'],
+                            iris.analysis.MEAN,
+                            weights=cellarea.data).data.tolist())
             values['area'].append(row['area'])
             values['frac'].append(row['frac'])
         # Compute relative bias in average fractions compared to reference
         reffrac = np.array(values['frac'][-1])
         for im, modfrac in enumerate(values['frac'][:-1]):
-            row = ((np.array(modfrac) - reffrac) /  reffrac * 100.0).tolist()
+            row = ((np.array(modfrac) - reffrac) / reffrac * 100.0).tolist()
             values['bias'].append(row)
             modnam['bias'].append(modnam['frac'][im])
         mydata[var] = {'values': values, 'groups': modnam}
@@ -270,25 +318,45 @@ def main(cfg):
         for ds in shuffle.keys():
             ids = mydata[var]['groups']['area'].index(ds)
             if refset[var] in ds:
-                shuffle[ds] = {'groups': {'area': [], 'frac': []},
-                               'values': {'area': [], 'frac': []}}
+                shuffle[ds] = {
+                    'groups': {
+                        'area': [],
+                        'frac': []
+                    },
+                    'values': {
+                        'area': [],
+                        'frac': []
+                    }
+                }
             else:
-                shuffle[ds] = {'groups': {'area': [], 'frac': [], 'bias': []},
-                               'values': {'area': [], 'frac': [], 'bias': []}}
+                shuffle[ds] = {
+                    'groups': {
+                        'area': [],
+                        'frac': [],
+                        'bias': []
+                    },
+                    'values': {
+                        'area': [],
+                        'frac': [],
+                        'bias': []
+                    }
+                }
             for var in sorted(allcubes.keys()):
                 for metric in shuffle[ds]['groups'].keys():
                     shuffle[ds]['groups'][metric].append(var)
-                    shuffle[ds]['values'][metric].append(mydata[var]['values'][metric][ids])
+                    shuffle[ds]['values'][metric].append(
+                        mydata[var]['values'][metric][ids])
         mydata = shuffle
-
 
     # Output ascii files and plots
     for target in mydata.keys():
         # Write plotdata as ascii files for user information
-        write_plotdata(cfg, regnam, mydata[target]['groups'], mydata[target]['values'], target)
+        write_plotdata(cfg, regnam, mydata[target]['groups'],
+                       mydata[target]['values'], target)
 
         # Plot area values
-        make_landcover_bars(cfg, regnam, mydata[target]['groups'], mydata[target]['values'], target)
+        make_landcover_bars(cfg, regnam, mydata[target]['groups'],
+                            mydata[target]['values'], target)
 
 
 if __name__ == '__main__':
