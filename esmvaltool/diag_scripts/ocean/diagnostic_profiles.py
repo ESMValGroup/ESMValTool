0eae8152705763b9174b4e599c63cd4dbacaffa1
Lee de Mora <ledm@pml.ac.uk>
2018-12-17 14:45:55 +0000

    Added new Decade averaging, moved several features from the version2_development_OMZ_irregular_2d_regridding branch

--- esmvaltool/diag_scripts/ocean/diagnostic_profiles.py
+++ esmvaltool/diag_scripts/ocean/diagnostic_profiles.py
@@ -49,6 +49,7 @@ import logging
 import os
 import sys
 
+import numpy as np
 import iris
 import iris.quickplot as qplt
 import matplotlib.pyplot as plt
@@ -89,11 +90,16 @@ def determine_profiles_str(cube):
             return str(value) + ' E'
     return ''
 
+def get_decade(coord, value):
+    date = coord.units.num2date(value)
+    return date.year - date.year % 10
 
 def make_profiles_plots(
         cfg,
         metadata,
         filename,
+        obs_metadata={},
+        obs_filename='',
 ):
     """
     Make a profile plot for an individual model.
@@ -108,36 +114,60 @@ def make_profiles_plots(
         The metadata dictionairy for a specific model.
     filename: str
         The preprocessed model file.
+    obs_metadata: dict
+        The metadata dictionairy for the observational dataset.
+    obs_filename: str
+        The preprocessed observational dataset file.
+
     """
     # Load cube and set up units
     cube = iris.load_cube(filename)
     cube = diagtools.bgc_units(cube, metadata['short_name'])
 
-    # Make annual means from:
-    cube = cube.aggregated_by('year', iris.analysis.MEAN)
+    raw_times = diagtools.cube_time_to_float(cube)
+
+    # Make annual or Decadal means from:
+    if np.max(raw_times) - np.min(raw_times) < 20:
+        cube = cube.aggregated_by('year', iris.analysis.MEAN)
+    else:
+        iris.coord_categorisation.add_categorised_coord(cube, 'decade', 'time',
+                                                        get_decade)
+        cube = cube.aggregated_by('decade', iris.analysis.MEAN)
+        print(cube)
+
+    times_float = diagtools.cube_time_to_float(cube)
+    time_0 = times_float[0]
 
     # Is this data is a multi-model dataset?
     multi_model = metadata['dataset'].find('MultiModel') > -1
 
-    times_float = diagtools.cube_time_to_float(cube)
-    time_0 = times_float[0]
 
     cmap = plt.cm.get_cmap('jet')
 
     plot_details = {}
     for time_index, time in enumerate(times_float):
+        if times_float[-1] == time_0:
+            color = 'black'
+        else:
+            color = cmap((time - time_0) / (times_float[-1] - time_0))
 
-        color = cmap((time - time_0) / (times_float[-1] - time_0))
+        qplt.plot(cube[time_index, :], cube[time_index, :].coord('depth'),
+                  c=color)
 
-        qplt.plot(
-            cube[time_index, :], cube[time_index, :].coord('depth'), c=color)
+        plot_details[str(time_index)] = {'c': color, 'ls': '-', 'lw': 1,
+                                         'label': str(int(time))}
 
-        plot_details[time_index] = {
-            'c': color,
-            'ls': '-',
-            'lw': 1,
-            'label': str(int(time))
-        }
+    # Add observational data.
+    if obs_filename:
+        obs_cube = iris.load_cube(obs_filename)
+        obs_cube = diagtools.bgc_units(obs_cube, metadata['short_name'])
+        obs_cube = obs_cube.collapsed('time', iris.analysis.MEAN)
+
+        obs_key = obs_metadata['dataset']
+        qplt.plot(obs_cube, obs_cube.coord('depth'), c='black')
+
+        plot_details[obs_key] = {'c': 'black', 'ls': '-', 'lw': 1,
+                                 'label': obs_key}
 
     # Add title to plot
     title = ' '.join([
@@ -187,7 +217,20 @@ def main(cfg):
         logger.info('metadata filename:\t%s', metadata_filename)
 
         metadatas = diagtools.get_input_files(cfg, index=index)
-        for filename in sorted(metadatas):
+
+        obs_key = 'observational_dataset'
+        obs_filename = ''
+        obs_metadata = {}
+        if obs_key in cfg:
+            obs_filename = diagtools.match_model_to_key(obs_key,
+                                                        cfg[obs_key],
+                                                        metadatas)
+            obs_metadata = metadatas[obs_filename]
+
+        for filename in sorted(metadatas.keys()):
+
+            if filename == obs_filename:
+                continue
 
             logger.info('-----------------')
             logger.info(
@@ -197,7 +240,9 @@ def main(cfg):
 
             ######
             # Time series of individual model
-            make_profiles_plots(cfg, metadatas[filename], filename)
+            make_profiles_plots(cfg, metadatas[filename], filename,
+                                obs_metadata=obs_metadata,
+                                obs_filename=obs_filename)
 
     logger.info('Success')
 
