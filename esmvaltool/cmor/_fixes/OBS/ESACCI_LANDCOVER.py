59825d3f801a24a7868fbc3e96bacf4c8a15280d
Tobias Stacke <tobias.stacke@mpimet.mpg.de>
2018-12-20 12:31:28 +0100

    Added fix for ESACCI-LANDCOVER Observations

---
+++ esmvaltool/cmor/_fixes/OBS/ESACCI_LANDCOVER.py
@@ -0,0 +1,33 @@
+"""Fixes for ESA-CCI Landcover."""
+
+from ..fix import Fix
+
+
+class baresoilFrac(Fix):
+    """Fixes for baresoilFrac."""
+
+    def fix_metadata(self, cube):
+        """
+        Fix missing scalar dimension.
+
+        Parameters
+        ----------
+        cube: iris.cube.Cube
+            Cube to fix
+
+        Returns
+        -------
+        iris.cube.Cube
+
+        """
+        import iris
+
+        typebare = iris.coords.AuxCoord(
+            'bare_ground',
+            standard_name='area_type',
+            long_name='surface type',
+            var_name='type',
+            units='1',
+            bounds=None)
+        cube.add_aux_coord(typebare)
+        return cube
