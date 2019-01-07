f5c2fd2b5679f64b7d245ee5fda839be859b69c6
Manuel Schlund <manuel.schlund@dlr.de>
2019-01-07 17:30:27 +0100

    Added fix for JMA-Transcom

--- esmvaltool/cmor/_fixes/OBS/JMA_TRANSCOM.py
+++ esmvaltool/cmor/_fixes/OBS/JMA_TRANSCOM.py
@@ -21,3 +21,28 @@ class fgco2(Fix):
         """
         cube = cube.intersection(longitude=(0.0, 360.0))
         return cube
+
+
+class nbp(Fix):
+    """Class to fix nbp."""
+
+    def fix_data(self, cube):
+        """
+        Fix data for nbp.
+
+        Data lives on lon = [0, 360] grid, but lon = 0 is East Asia, not
+        Europe. To fix this, the data needs to be rotated.
+
+        Parameters
+        ----------
+        cube: iris.cube.Cube
+
+        Returns
+        -------
+        iris.cube.Cube
+
+        """
+        lon_coord = cube.coord('longitude')
+        lon_coord.points = lon_coord.points - 180.0
+        cube = cube.intersection(longitude=(0.0, 360.0))
+        return cube
