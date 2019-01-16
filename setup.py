4fedf2a94d7b4167e7d716ac8e33999bdf547ba4
Mattia Righi <mattia.righi@dlr.de>
2019-01-16 09:38:18 +0100

    Fix codacy issues

--- setup.py
+++ setup.py
@@ -77,7 +77,6 @@ REQUIREMENTS = {
 
 def discover_python_files(paths, ignore):
     """Discover Python files."""
-
     def _ignore(path):
         """Return True if `path` should be ignored, False otherwise."""
         return any(re.match(pattern, path) for pattern in ignore)
@@ -221,9 +220,12 @@ with open('README.md') as readme:
         entry_points={
             'console_scripts': [
                 'esmvaltool = esmvaltool._main:run',
-                'cmorize_obs = esmvaltool.utils.cmorizers.obs.cmorize_obs:execute_cmorize',
-                'nclcodestyle = esmvaltool.utils.nclcodestyle.nclcodestyle:_main',
-                'mip_convert_setup = esmvaltool.utils.cmorizers.mip_convert.esmvt_mipconv_setup:main'
+                'cmorize_obs = esmvaltool.'
+                'utils.cmorizers.obs.cmorize_obs:execute_cmorize',
+                'nclcodestyle = esmvaltool.'
+                'utils.nclcodestyle.nclcodestyle:_main',
+                'mip_convert_setup = esmvaltool.'
+                'utils.cmorizers.mip_convert.esmvt_mipconv_setup:main'
             ],
         },
         cmdclass={
