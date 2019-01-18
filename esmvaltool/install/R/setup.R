96f161ee050c3df8634de7b6f2a49571d4bb1055
Bouwe Andela <b.andela@esciencecenter.nl>
2019-01-18 12:45:54 +0100

    Clean up log messages and add LinkingTo

--- esmvaltool/install/R/setup.R
+++ esmvaltool/install/R/setup.R
@@ -1,4 +1,8 @@
 #check for present library paths
+log <- function(..., level="INFO") {
+    cat(format(Sys.time(), "%Y-%m-%d %X"), level, ":", ..., "\n")
+}
+
 RLIBPATH <- .libPaths()
 
 #check if we can write in the present R libaries paths
@@ -12,11 +16,11 @@ if (any(file.access(RLIBPATH, 2) == 0)) {
                recursive = TRUE)
 }
 
-cat("\nINFO: Installing packages to --> ", RLIBLOC, "\n\n")
+log("Installing packages to --> ", RLIBLOC)
 
 # define the R mirror to download packages
 pkg_mirror <- "https://cloud.r-project.org"
-print(paste("Using mirror: ", pkg_mirror))
+log("Using mirror: ", pkg_mirror)
 
 # get the script path
 initial_options <- commandArgs(trailingOnly = FALSE)
@@ -24,7 +28,6 @@ file_arg_name <- "--file="
 script_name <- sub(file_arg_name, "",
                    initial_options[grep(file_arg_name, initial_options)])
 script_dirname <- dirname(script_name)
-print(script_dirname)
 
 # read the dependencies
 dependencies <- scan(
@@ -36,9 +39,9 @@ inst_packages <- installed.packages()
 package_list <- dependencies[!(dependencies %in% inst_packages[, "Package"])]
 
 if (length(package_list) == 0) {
-    print("All packages are installed!")
+    log("All packages are already installed!")
 } else {
-    print(paste("Number of packages to be installed: ", length(package_list)))
+    log("Number of packages to be installed: ", length(package_list))
 }
 
 Ncpus <- parallel::detectCores()
@@ -47,12 +50,12 @@ if (is.na(Ncpus)) {
 }
 
 for (package_name in package_list) {
-    print(paste("     Installing package --> ", package_name))
+    log("Installing package:", package_name)
     install.packages(
         package_name,
         repos = pkg_mirror,
         Ncpus = Ncpus,
-        dependencies = c("Depends", "Imports")
+        dependencies = c("Depends", "Imports", "LinkingTo")
     )
     success <- library(
         package_name,
@@ -60,7 +63,9 @@ for (package_name in package_list) {
         logical.return = TRUE
     )
     if ( ! success ) {
-        print(paste("     Failed to install package --> ", package_name))
+        log("Failed to install package:", package_name)
         quit(status = 1, save = "no")
     }
 }
+
+log("Succesfully installed all packages")
