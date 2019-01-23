356c8e3bc387164db42a4ddeae601c3bd9228519
Javier Vegas <javier.vegas@bsc.es>
2019-01-23 13:17:05 +0100

    Remove automatic fixes from test

--- tests/unit/cmor/test_cmor_check.py
+++ tests/unit/cmor/test_cmor_check.py
@@ -341,7 +341,7 @@ class TestCMORCheck(unittest.TestCase):
     def test_time_automatic_fix(self):
         """Test automatic fix for time units"""
         self.cube.coord('time').units = 'days since 1860-1-1 00:00:00'
-        self._check_cube(automatic_fixes=True)
+        self._check_cube()
         self.assertEquals(
             self.cube.coord('time').units.origin,
             'days since 1950-1-1 00:00:00'
