a860dd83e39f802d816abca664e545d5fa4a0d19
jvegasbsc <javier.vegas@bsc.es>
2019-02-08 10:46:25 +0100

    Changed xco2 fix to new way

--- esmvaltool/cmor/_fixes/obs4mips/ESACCI_GHG.py
+++ esmvaltool/cmor/_fixes/obs4mips/ESACCI_GHG.py
@@ -2,12 +2,14 @@
 import numpy as np
 import cf_units
 
+from iris.cube import CubeList
+
 from ..fix import Fix
 
 class xco2(Fix):
     """Fixes for xco2"""
 
-    def fix_metadata(self, cube):
+    def fix_metadata(self, cubes):
         """
         Fix metadata
 
@@ -15,23 +17,19 @@ class xco2(Fix):
 
         Parameters
         ----------
-        cube: iris.cube.Cube
+        cube: iris.cube.CubeList
             Cube to fix
 
         Returns
         -------
-        iris.cube.Cube
+        iris.cube.CubeList
 
         """
-        cube.units = cf_units.Unit('1.0e-6')
-        rlon = cube.coord('longitude').points
-        if (np.amin(rlon) < 0.):
-           n = int(len(rlon) / 2)
-           rlon = np.where(rlon < 0., rlon + 360., rlon)
-           cube.coord('longitude').points = np.roll(rlon, n)
-
         print("*** FIX XCO2 METADATA ***")
-        return cube
+        cube = cubes[0]
+        cube.units = cf_units.Unit('1.0e-6')
+        cube = cube.intersection(longitude=(0, 360))
+        return CubeList(cube)
 
     def fix_data(self, cube):
         """
@@ -53,13 +51,6 @@ class xco2(Fix):
         print("*** FIX XCO2 DATA ***")
         metadata = cube.metadata
         cube *= 1.0e6
-
-        rlon = cube.coord('longitude').points
-        n = int(len(rlon) / 2)
-        # longitude is assumed to be the last dimension in the numpy array
-        ax = len(cube.shape) - 1
-        cube.data = np.roll(cube.data, n, axis=ax)
-
         cube.metadata = metadata
 
         return cube
