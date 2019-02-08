52f0a8d74e2f72e453b0ad7abed1cfbe6ba30979
Manuel Schlund <manuel.schlund@dlr.de>
2019-02-08 12:20:16 +0100

    Added @bouweandela s suggestion

--- esmvaltool/_recipe.py
+++ esmvaltool/_recipe.py
@@ -400,10 +400,9 @@ def _update_fx_settings(settings, variable, config_user):
                 fx_files_dict['sftgif'])
 
     for step in ('average_region', 'average_volume'):
-        if step in settings and settings[step].get('fx_files') is not None:
-            var = dict(variable)
+        if setting.get(step, {}).get('fx_files'):
             settings[step]['fx_files'] = get_input_fx_filelist(
-                variable=var,
+                variable=variable,
                 rootpath=config_user['rootpath'],
                 drs=config_user['drs'],
             )
