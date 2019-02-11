d4f781c376e54e9737dfefd5326363b5d8d6b2e7
Bas Crezee <bascrezee@gmail.com>
2019-02-11 16:09:18 +0100

    changed one line to conform to pep8

--- esmvaltool/preprocessor/_derive/alb.py
+++ esmvaltool/preprocessor/_derive/alb.py
@@ -31,6 +31,6 @@ class DerivedVariable(DerivedVariableBase):
         rsus_cube = cubes.extract_strict(
             Constraint(name='surface_upwelling_shortwave_flux_in_air'))
 
-        rsns_cube = rsus_cube/rsds_cube 
+        rsns_cube = rsus_cube / rsds_cube
 
         return rsns_cube
