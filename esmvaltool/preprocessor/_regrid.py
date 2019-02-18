fe9326303682cb99d57290e4f146e672be8f8823
Valeriu Predoi <valeriu.predoi@gmail.com>
2019-02-18 15:48:07 +0000

    thought better and reverted a couple previous changes for 819; should work now

--- esmvaltool/preprocessor/_regrid.py
+++ esmvaltool/preprocessor/_regrid.py
@@ -481,7 +481,8 @@ def get_reference_levels(filename,
                          project,
                          dataset,
                          short_name,
-                         fix_dir):
+                         fix_dir,
+                         coord):
     """Get level definition from a CMOR coordinate.
 
     Parameters
@@ -505,7 +506,8 @@ def get_reference_levels(filename,
     cubes = fix_metadata(cubes, short_name, project, dataset)
     cube = cubes[0]
     try:
-        coord = cube.coord(axis='z', dim_coords=True)
+        coord = cube.coord(coord)
     except iris.exceptions.CoordinateNotFoundError:
-        raise ValueError('z-coordinate not available in {}'.format(filename))
+        raise ValueError('z-coord {} not available in {}'.format(coord,
+                                                                 filename))
     return coord.points.tolist()
