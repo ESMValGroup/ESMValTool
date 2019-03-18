1383d5f2f7b173a5b27f72d20cb83ef936a96ed9
Bouwe Andela <b.andela@esciencecenter.nl>
2019-03-18 22:52:44 +0100

    Use logger and clean up a bit

--- esmvaltool/install/Julia/setup.jl
+++ esmvaltool/install/Julia/setup.jl
@@ -1,35 +1,29 @@
 #!/usr/bin/env julia
 
-scriptFile=@__FILE__
 scriptDir=@__DIR__
-#println("file: ", scriptFile)
-#println("directory: ", scriptDir)
 
 if VERSION >= v"0.7.0-DEV.2005"
     using Pkg
 end
 
-println("Installing the packages.")
+@info "Installing the packages from" scriptDir * "/julia_requirements.txt"
 pkgName=in
 open(scriptDir * "/julia_requirements.txt") do f
     for i in enumerate(eachline(f))
 
       pkgId=i[1]
       pkgName=i[2]
-      println(pkgId, ": ", pkgName)
+      @info "Installing" pkgName
       Pkg.add(pkgName)
 
-      println("Testing: ", pkgName)
+      @info "Testing: ", pkgName
       # load the package this needs to be called at top-level
       Expr(:toplevel, :(module ($pkgName) end))
 
     end
 end
 
-# Update the packages
-#Pkg.update()
-
 # Show the package list
-println("Installed Julia packages:")
+@info "Installed Julia packages:"
 Pkg.installed()
 Pkg.status()
