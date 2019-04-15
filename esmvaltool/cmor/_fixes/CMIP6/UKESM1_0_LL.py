b99ea77ed9f8005b166f5afa745f42b7a3f29dec
Lee de Mora <ledm@pml.ac.uk>
2019-04-15 10:42:19 +0100

    Added CMOR fix for CMIP6 data.

---
+++ esmvaltool/cmor/_fixes/CMIP6/UKESM1_0_LL.py
@@ -0,0 +1,29 @@
+# pylint: disable=invalid-name, no-self-use, too-few-public-methods
+"""Fixes for UKESM1-0-LL."""
+# import numpy as np
+# import iris
+
+from ..fix import Fix
+
+class allvars(Fix):
+    """Fixes for all vars."""
+
+    def fix_metadata(self, cubelist):
+        """
+        Fix non-standard dimension names.
+
+        Parameters
+        ----------
+        cubelist: iris CubeList
+            List of cubes to fix
+
+        Returns
+        -------
+        iris.cube.CubeList
+        """
+        for cube in cubelist:
+            lats = cube.coord('latitude')
+            lons = cube.coord('longitude')
+            lats.var_name = 'lat'
+            lons.var_name = 'lon'
+        return cubelist
