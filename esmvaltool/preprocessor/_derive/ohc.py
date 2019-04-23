f6dd2821369e0e1fbd814d19960596661ca26363
Valeriu Predoi <valeriu.predoi@gmail.com>
2019-04-23 15:12:36 +0100

    cleaned up derive script and alligned it to current standards

--- esmvaltool/preprocessor/_derive/ohc.py
+++ esmvaltool/preprocessor/_derive/ohc.py
@@ -4,7 +4,7 @@ from iris import Constraint
 
 from cf_units import Unit
 
-from ._derived_variable_base import DerivedVariableBase
+from ._baseclass import DerivedVariableBase
 
 RHO_CP = iris.cube.Cube(4.09169e+6, units=Unit('kg m-3 J kg-1 K-1'))
 
@@ -13,15 +13,24 @@ class DerivedVariable(DerivedVariableBase):
     """Derivation of variable `ohc`."""
 
     # Required variables
-    _required_variables = {
-        'vars': [{'short_name': 'thetao', 'field': 'TO3M'}],
-        'fx_files': ['volcello']
-    }
+    required = [
+        {
+            'short_name': 'thetao',
+            'field': 'TO3M',
+            'fx_files': [
+                'volcello',
+            ],
+        },
+    ]
 
-    def calculate(self, cubes,):
+    def calculate(
+            self,
+            cubes,
+    ):
         """
         Compute ocean heat content.
-        Use c_p*rho_0= 4.09169e+6 J m-3 K-1 
+
+        Use c_p*rho_0= 4.09169e+6 J m-3 K-1
         (Kuhlbrodt et al., 2015, Clim. Dyn.)
 
         Arguments
@@ -35,14 +44,14 @@ class DerivedVariable(DerivedVariableBase):
               Output OHC cube.
         """
         # 1. Load the thetao and volcello cubes
-        cube = cubes.extract_strict(Constraint(cube_func=lambda c: 
-            c.var_name == 'thetao'))
-        volume = cubes.extract_strict(Constraint(cube_func=lambda c: 
-            c.var_name == 'volcello'))
+        cube = cubes.extract_strict(
+            Constraint(cube_func=lambda c: c.var_name == 'thetao'))
+        volume = cubes.extract_strict(
+            Constraint(cube_func=lambda c: c.var_name == 'volcello'))
         # 2. multiply with each other and with cprho0
         # try working with cubes here to save memory
         # this might not be possible because of non-broadcasting in Iris
-#        cube *= volume * RHO_CP
+        #        cube *= volume * RHO_CP
         cube.data *= volume.data * RHO_CP.data
         cube.units *= volume.units * RHO_CP.units
         return cube
