7f2e7fc02d755a233d1e7b669670b1f274a7c9c8
Javier Vegas-Regidor <javier.vegas@bsc.es>
2019-05-13 17:56:02 +0200

    Small refactor

--- esmvaltool/utils/cmorizers/obs/cmorize_obs_PIOMAS.py
+++ esmvaltool/utils/cmorizers/obs/cmorize_obs_PIOMAS.py
@@ -25,9 +25,7 @@ from cf_units import Unit
 import iris
 from iris.coords import AuxCoord, DimCoord
 
-from .utilities import (constant_metadata, fix_coords,
-                        fix_var_metadata, read_cmor_config, save_variable,
-                        set_global_atts)
+from .utilities import read_cmor_config, save_variable, set_global_atts
 
 logger = logging.getLogger(__name__)
 
@@ -40,7 +38,6 @@ NY = 120
 
 def cmorization(in_dir, out_dir):
     """Cmorization func call."""
-    cmor_table = CFG['cmor_table']
     glob_attrs = CFG['attributes']
 
     logger.info("Starting cmorization for Tier%s OBS files: %s",
@@ -48,20 +45,19 @@ def cmorization(in_dir, out_dir):
     logger.info("Input data from: %s", in_dir)
     logger.info("Output will be written to: %s", out_dir)
 
-    scalar_path = os.path.join(in_dir, CFG['custom']['scalar_file'])
-    grids = np.loadtxt(scalar_path).reshape(2, NY, NX)
+    grids = np.loadtxt(os.path.join(in_dir, CFG['custom']['scalar_file']))
+    grids = grids.reshape(2, NY, NX)
     lat_sca, lon_sca = _create_lat_lon_coords(grids[1, ...], grids[0, ...])
 
-    vector_path = os.path.join(in_dir, CFG['custom']['vector_file'])
-    grids = np.loadtxt(vector_path).reshape(7, NY, NX)
+    grids = np.loadtxt(os.path.join(in_dir, CFG['custom']['vector_file']))
+    grids = grids.reshape(7, NY, NX)
     lat_vec, lon_vec = _create_lat_lon_coords(grids[1, ...], grids[0, ...])
     # Area in m2
     area_cello = grids[2, ...] * grids[3, ...] * 1e6
     # run the cmorization
     for var, vals in CFG['variables'].items():
-
-        var_info = cmor_table.get_variable(vals['mip'], var)
-        glob_attrs['mip'] = vals['mip']
+        var_info = CFG['cmor_table'].get_variable(vals['mip'], var)
+        CFG['attributes']['mip'] = vals['mip']
         if vals['type'] == 'scalar':
             lat = lat_sca
             lon = lon_sca
@@ -71,19 +67,24 @@ def cmorization(in_dir, out_dir):
 
         if var == "areacello":
             cube = _create_areacello(lon, lat, area_cello, var_info)
-            set_global_atts(cube, glob_attrs)
-            save_variable(cube, var_info.short_name, out_dir, glob_attrs)
+            set_global_atts(cube, CFG['attributes'])
+            save_variable(
+                cube, var_info.short_name, out_dir, CFG['attributes']
+            )
             continue
 
         file_expression = os.path.join(in_dir, '{0}.H????'.format(vals['raw']))
         for file_path in glob.glob(file_expression):
-            data = _read_binary_file(
-                file_path
+            cube = _create_cube(
+                _read_binary_file(file_path),
+                lon, lat,
+                int(file_path[-4:]),
+                var_info
             )
-            year = int(file_path[-4:])
-            cube = _create_cube(lon, lat, year, data, var_info)
-            set_global_atts(cube, glob_attrs)
-            save_variable(cube, var_info.short_name, out_dir, glob_attrs)
+            set_global_atts(cube, CFG['attributes'])
+            save_variable(
+                cube, var_info.short_name, out_dir, CFG['attributes']
+            )
 
 
 def _create_lat_lon_coords(lat, lon):
@@ -103,7 +104,7 @@ def _create_lat_lon_coords(lat, lon):
     return lat_coord, lon_coord
 
 
-def _create_cube(lon, lat, year, data, var_info):
+def _create_cube(data, lon, lat, year, var_info):
     time_coord = DimCoord(
         np.arange(0, data.shape[0]),
         standard_name='time',
