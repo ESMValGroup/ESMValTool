14be2a4291e02c03f4244bf780dbe786fb3254ad
Javier Vegas-Regidor <javier.vegas@bsc.es>
2019-05-13 17:46:00 +0200

    Add areacello

--- esmvaltool/utils/cmorizers/obs/cmorize_obs_PIOMAS.py
+++ esmvaltool/utils/cmorizers/obs/cmorize_obs_PIOMAS.py
@@ -34,6 +34,9 @@ logger = logging.getLogger(__name__)
 # read in CMOR configuration
 CFG = read_cmor_config('PIOMAS.yml')
 
+NX = 360
+NY = 120
+
 
 def cmorization(in_dir, out_dir):
     """Cmorization func call."""
@@ -45,23 +48,45 @@ def cmorization(in_dir, out_dir):
     logger.info("Input data from: %s", in_dir)
     logger.info("Output will be written to: %s", out_dir)
 
+    scalar_path = os.path.join(in_dir, CFG['custom']['scalar_file'])
+    grids = np.loadtxt(scalar_path).reshape(2, NY, NX)
+    lat_sca, lon_sca = _create_lat_lon_coords(grids[1, ...], grids[0, ...])
+
+    vector_path = os.path.join(in_dir, CFG['custom']['vector_file'])
+    grids = np.loadtxt(vector_path).reshape(7, NY, NX)
+    lat_vec, lon_vec = _create_lat_lon_coords(grids[1, ...], grids[0, ...])
+    # Area in m2
+    area_cello = grids[2, ...] * grids[3, ...] * 1e6
     # run the cmorization
     for var, vals in CFG['variables'].items():
-        grid_path = os.path.join(in_dir, vals['grid_file'])
-        file_expression = os.path.join(in_dir, '{0}.H????'.format(vals['raw']))
+
         var_info = cmor_table.get_variable(vals['mip'], var)
         glob_attrs['mip'] = vals['mip']
+        if vals['type'] == 'scalar':
+            lat = lat_sca
+            lon = lon_sca
+        else:
+            lat = lat_vec
+            lon = lon_vec
+
+        if var == "areacello":
+            cube = _create_areacello(lon, lat, area_cello, var_info)
+            set_global_atts(cube, glob_attrs)
+            save_variable(cube, var_info.short_name, out_dir, glob_attrs)
+            continue
+
+        file_expression = os.path.join(in_dir, '{0}.H????'.format(vals['raw']))
         for file_path in glob.glob(file_expression):
-            lon, lat, data = read_binary_file(
-                file_path, grid_path
+            data = _read_binary_file(
+                file_path
             )
             year = int(file_path[-4:])
-            cube = create_cube(lon, lat, year, data, var_info)
+            cube = _create_cube(lon, lat, year, data, var_info)
             set_global_atts(cube, glob_attrs)
             save_variable(cube, var_info.short_name, out_dir, glob_attrs)
 
 
-def create_cube(lon, lat, year, data, var_info):
+def _create_lat_lon_coords(lat, lon):
     lon_coord = AuxCoord(
         lon,
         standard_name='longitude',
@@ -75,7 +100,10 @@ def create_cube(lon, lat, year, data, var_info):
         var_name='lat',
         units='degrees_north'
     )
+    return lat_coord, lon_coord
 
+
+def _create_cube(lon, lat, year, data, var_info):
     time_coord = DimCoord(
         np.arange(0, data.shape[0]),
         standard_name='time',
@@ -90,23 +118,26 @@ def create_cube(lon, lat, year, data, var_info):
         units='m',
     )
     cube.add_dim_coord(time_coord, 0)
-    cube.add_aux_coord(lon_coord, (1, 2))
-    cube.add_aux_coord(lat_coord, (1, 2))
+    cube.add_aux_coord(lon, (1, 2))
+    cube.add_aux_coord(lat, (1, 2))
     return cube
 
 
-def read_binary_file(data_path, grid_path):
-    nx = 360
-    ny = 120
+def _create_areacello(lon, lat, data, var_info):
+    cube = iris.cube.Cube(
+        data,
+        standard_name=var_info.standard_name,
+        var_name=var_info.short_name,
+        units='m2',
+    )
+    cube.add_aux_coord(lon, (0, 1))
+    cube.add_aux_coord(lat, (0, 1))
+    return cube
 
-    grids = np.loadtxt(grid_path)
-    grids = grids.reshape(2, ny, nx)
-    lon = grids[0, ...]
-    lat = grids[1, ...]
 
+def _read_binary_file(data_path):
     fd_data = open(data_path, 'rb')
     data = np.fromfile(fd_data, dtype=np.dtype('f'), count=-1)
-    days = data.shape[0] // nx // ny
-    data = data.reshape(days, ny, nx)
-
-    return lon, lat, data
+    days = data.shape[0] // NX // NY
+    data = data.reshape(days, NY, NX)
+    return data
