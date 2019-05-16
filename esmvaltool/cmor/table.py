ce68b6919afdb1ffa5860dbdb5b7ffbc9668077f
Lisa Bock <Lisa.Bock@dlr.de>
2019-05-16 16:11:19 +0200

    init try to read CMIP3 datasets

--- esmvaltool/cmor/table.py
+++ esmvaltool/cmor/table.py
@@ -40,7 +40,11 @@ def read_cmor_tables(cfg_developer):
             default = None
         else:
             default = custom
-        if cmor_type == 'CMIP5':
+        if cmor_type == 'CMIP3':
+            CMOR_TABLES[table] = CMIP3Info(
+                table_path, default=default,
+            )
+        elif cmor_type == 'CMIP5':
             CMOR_TABLES[table] = CMIP5Info(
                 table_path, default=default,
             )
@@ -647,3 +651,167 @@ class CustomInfo(CMIP5Info):
                     continue
                 if not self._read_line():
                     return
+
+
+class CMIP3Info(object):
+    """
+    Class to read CMIP3-like data request.
+
+    Parameters
+    ----------
+    cmor_tables_path: basestring
+       Path to the folder containing the Tables folder with the json files
+
+    """
+
+    def __init__(self, cmor_tables_path, default=None):
+        cmor_tables_path = self._get_cmor_path(cmor_tables_path)
+
+        self._cmor_folder = os.path.join(cmor_tables_path, 'Tables')
+        if not os.path.isdir(self._cmor_folder):
+            raise OSError(errno.ENOTDIR, "CMOR tables path is not a directory",
+                          self._cmor_folder)
+
+        self.tables = {}
+        self.coords = {}
+        self.default = default
+        self._current_table = None
+        self._last_line_read = None
+
+        for table_file in glob.glob(os.path.join(self._cmor_folder, '*')):
+            if '_grids' in table_file:
+                continue
+            self._load_table(table_file)
+
+    @staticmethod
+    def _get_cmor_path(cmor_tables_path):
+        if os.path.isdir(cmor_tables_path):
+            return cmor_tables_path
+        cwd = os.path.dirname(os.path.realpath(__file__))
+        cmor_tables_path = os.path.join(cwd, 'tables', cmor_tables_path)
+        return cmor_tables_path
+
+    def _load_table(self, table_file, table_name=''):
+        if table_name and table_name in self.tables:
+            # special case used for updating a table with custom variable file
+            table = self.tables[table_name]
+        else:
+            # default case: table name is first line of table file
+            table = None
+
+        self._read_table_file(table_file, table)
+
+    def _read_table_file(self, table_file, table=None):
+        with open(table_file) as self._current_table:
+            self._read_line()
+            while True:
+                key, value = self._last_line_read
+                if key == 'table_id':
+                    table = TableInfo()
+                    table.name = value[len('Table '):]
+                    self.tables[table.name] = table
+                elif key == 'frequency':
+                    table.frequency = value
+                elif key == 'modeling_realm':
+                    table.realm = value
+                elif key == 'generic_levels':
+                    for dim in value.split(' '):
+                        coord = CoordinateInfo(dim)
+                        coord.generic_level = True
+                        coord.axis = 'Z'
+                        self.coords[dim] = coord
+                elif key == 'axis_entry':
+                    self.coords[value] = self._read_coordinate(value)
+                    continue
+                elif key == 'variable_entry':
+                    table[value] = self._read_variable(value, table.frequency)
+                    continue
+                if not self._read_line():
+                    return
+
+    def _read_line(self):
+        line = self._current_table.readline()
+        if line == '':
+            return False
+        if line.startswith('!'):
+            return self._read_line()
+        line = line.replace('\n', '')
+        if '!' in line:
+            line = line[:line.index('!')]
+        line = line.strip()
+        if not line:
+            self._last_line_read = ('', '')
+        else:
+            index = line.index(':')
+            self._last_line_read = (line[:index].strip(),
+                                    line[index + 1:].strip())
+        return True
+
+    def _read_coordinate(self, value):
+        coord = CoordinateInfo(value)
+        while self._read_line():
+            key, value = self._last_line_read
+            if key in ('variable_entry', 'axis_entry'):
+                return coord
+            if key == 'requested':
+                coord.requested = value.split(' ')
+                continue
+            if hasattr(coord, key):
+                setattr(coord, key, value)
+        return coord
+
+    def _read_variable(self, short_name, frequency):
+        var = VariableInfo('CMIP5', short_name)
+        var.frequency = frequency
+        while self._read_line():
+            key, value = self._last_line_read
+            if key in ('variable_entry', 'axis_entry'):
+                break
+            if key in ('dimensions', 'modeling_realm'):
+                setattr(var, key, value.split())
+            elif hasattr(var, key):
+                setattr(var, key, value)
+        for dim in var.dimensions:
+            var.coordinates[dim] = self.coords[dim]
+        return var
+
+    def get_table(self, table):
+        """
+        Search and return the table info.
+
+        Parameters
+        ----------
+        table: basestring
+            Table name
+
+        Returns
+        -------
+        TableInfo
+            Return the TableInfo object for the requested table if
+            found, returns None if not
+
+        """
+        return self.tables.get(table)
+
+    def get_variable(self, table, short_name):
+        """
+        Search and return the variable info.
+
+        Parameters
+        ----------
+        table: basestring
+            Table name
+        short_name: basestring
+            Variable's short name
+
+        Returns
+        -------
+        VariableInfo
+            Return the VariableInfo object for the requested variable if
+            found, returns None if not
+
+        """
+        var_info = self.tables.get(table, {}).get(short_name, None)
+        if not var_info and self.default:
+            return self.default.get_variable(table, short_name)
+        return var_info
