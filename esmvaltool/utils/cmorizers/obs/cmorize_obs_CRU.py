e25a0a97ee9cea23e673e20e98b7706c2bd0ee12
Manuel Schlund <manuel.schlund@dlr.de>
2019-05-21 13:55:07 +0200

    Adapted description and added unzipping to python script

--- esmvaltool/utils/cmorizers/obs/cmorize_obs_CRU.py
+++ esmvaltool/utils/cmorizers/obs/cmorize_obs_CRU.py
@@ -11,13 +11,15 @@ Last access
 
 Download and processing instructions
     Download the following files:
-        cru_ts4.02.1901.2017.{raw_name}.dat.nc
-    where {raw_name} is the name of the desired variable.
+        {raw_name}/cru_ts4.02.1901.2017.{raw_name}.dat.nc.gz
+    where {raw_name} is the name of the desired variable(s).
 
 """
 
+import gzip
 import logging
 import os
+import shutil
 
 import iris
 
@@ -38,8 +40,22 @@ def _extract_variable(raw_var, cmor_info, attrs, filepath, out_dir):
     utils.set_global_atts(cube, attrs)
     if var in ('tas', ):
         utils.add_height2m(cube)
-    utils.save_variable(
-        cube, var, out_dir, attrs, unlimited_dimensions=['time'])
+    utils.save_variable(cube,
+                        var,
+                        out_dir,
+                        attrs,
+                        unlimited_dimensions=['time'])
+
+
+def _unzip(filepath, out_dir):
+    """Unzip `*.gz` file."""
+    filename = os.path.basename(filepath.replace('.gz', ''))
+    new_path = os.path.join(out_dir, filename)
+    with gzip.open(filepath, 'rb') as zip_file:
+        with open(new_path, 'wb') as new_file:
+            shutil.copyfileobj(zip_file, new_file)
+    logger.info("Succefully extracted file to %s", new_path)
+    return new_path
 
 
 def cmorization(in_dir, out_dir):
@@ -58,9 +74,10 @@ def cmorization(in_dir, out_dir):
         glob_attrs['mip'] = var_info['mip']
         cmor_info = cmor_table.get_variable(var_info['mip'], var)
         raw_var = var_info.get('raw', var)
-        filepath = raw_filepath.format(raw_name=raw_var)
-        if not os.path.isfile(filepath):
-            logger.debug("Skipping '%s', file '%s' not found", var, filepath)
+        zip_file = os.path.join(in_dir, raw_filepath.format(raw_name=raw_var))
+        if not os.path.isfile(zip_file):
+            logger.debug("Skipping '%s', file '%s' not found", var, zip_file)
             continue
-        logger.info("Found input file '%s'", filepath)
+        logger.info("Found input file '%s'", zip_file)
+        filepath = _unzip(zip_file, out_dir)
         _extract_variable(raw_var, cmor_info, glob_attrs, filepath, out_dir)
