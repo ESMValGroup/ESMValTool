d62e8fdcdcedb5691d79b42a0210bdb8661191a8
Till Kuhlbrodt <t.kuhlbrodt@reading.ac.uk>
2019-05-22 17:09:57 +0100

    modify ohc.py to avoid realising cubes, do cube multiplication instead

--- esmvaltool/preprocessor/_derive/ohc.py
+++ esmvaltool/preprocessor/_derive/ohc.py
@@ -6,7 +6,7 @@ from cf_units import Unit
 
 from ._baseclass import DerivedVariableBase
 
-RHO_CP = iris.cube.Cube(4.09169e+6, units=Unit('kg m-3 J kg-1 K-1'))
+RHO_CP = iris.coords.AuxCoord(4.09169e+6, units=Unit('kg m-3 J kg-1 K-1'))
 
 
 class DerivedVariable(DerivedVariableBase):
@@ -49,9 +49,15 @@ class DerivedVariable(DerivedVariableBase):
         volume = cubes.extract_strict(
             Constraint(cube_func=lambda c: c.var_name == 'volcello'))
         # 2. multiply with each other and with cprho0
-        # try working with cubes here to save memory
-        # this might not be possible because of non-broadcasting in Iris
-        #        cube *= volume * RHO_CP
-        cube.data *= volume.data * RHO_CP.data
-        cube.units *= volume.units * RHO_CP.units
-        return cube
+        # some juggling with coordinates needed since Iris is very
+        # restrictive in this regard
+        cube.remove_coord('day_of_month')
+        cube.remove_coord('day_of_year')
+        cube.remove_coord('month_number')
+        cube.remove_coord('year')
+        t_coord = cube.coord('time')
+        cube.remove_coord('time')
+        new_cube = cube * volume
+        new_cube *= RHO_CP
+        new_cube.add_dim_coord(t_coord, 0)
+        return new_cube
