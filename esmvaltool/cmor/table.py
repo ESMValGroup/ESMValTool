b09b7db33056ab2665c0c5680bdf56a6f41324d7
Javier Vegas <javier.vegas@bsc.es>
2019-05-23 11:37:09 +0200

    Minor style fixes

--- esmvaltool/cmor/table.py
+++ esmvaltool/cmor/table.py
@@ -102,12 +102,12 @@ class CMIP6Info(object):
             table.name = header['table_id'].split(' ')[-1]
             generic_levels = header['generic_levels'].split()
             table.realm = header['realm'].split()
-            frequency = header.get('frequency', '')
+            table.frequency = header.get('frequency', '')
             self.var_to_freq[table.name] = {}
 
             for var_name, var_data in raw_data['variable_entry'].items():
                 var = VariableInfo('CMIP6', var_name)
-                var.read_json(var_data, frequency)
+                var.read_json(var_data, table.frequency)
                 self._assign_dimensions(var, generic_levels)
                 table[var_name] = var
                 self.var_to_freq[table.name][var_name] = var.frequency
