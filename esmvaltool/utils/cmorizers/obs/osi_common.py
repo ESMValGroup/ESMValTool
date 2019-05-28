93bf81a41d9bb005cb4c1f38112464973a094c9b
Javier Vegas-Regidor <javier.vegas@bsc.es>
2019-05-28 13:27:07 +0200

    Add empty timesteps when missing data

--- esmvaltool/utils/cmorizers/obs/osi_common.py
+++ esmvaltool/utils/cmorizers/obs/osi_common.py
@@ -3,12 +3,16 @@
 import logging
 import os
 import glob
+from datetime import datetime, timedelta
+from calendar import monthrange, isleap
 
 import numpy as np
 from numba import vectorize
 import iris
-from iris.cube import Cube
-from iris.coord_categorisation import add_month, add_year
+from iris.cube import Cube, CubeList
+from iris.coords import AuxCoord
+from iris.coord_categorisation import (
+    add_month_number, add_year, add_day_of_year)
 from iris.analysis import MEAN
 
 from .utilities import (set_global_atts, convert_timeunits, fix_coords,
@@ -39,19 +43,22 @@ def cmorize_osi(in_dir, out_dir, cfg, hemisphere):
             vals['raw'], hemisphere, vals['grid']
         )
         for year in os.listdir(in_dir):
+            year = int(year)
+            if year != 1986:
+                continue
             logger.info(
-                "CMORizing var %s:%s for year %s", mip, var, year
+                "CMORizing var %s for year %s", var, year
             )
             raw_info = {
                 'name': vals['raw'],
-                'file': os.path.join(in_dir, year, '??', file_pattern)
+                'file': os.path.join(in_dir, str(year), '??', file_pattern)
             }
             extract_variable(
                 var_info, raw_info, out_dir, glob_attrs, year, vals['mip']
             )
             if first_run:
                 sample_file = glob.glob(os.path.join(
-                    in_dir, year, '01', file_pattern))[0]
+                    in_dir, str(year), '01', file_pattern))[0]
                 cube = iris.load_cube(
                     sample_file,
                     iris.Constraint(
@@ -86,19 +93,89 @@ def extract_variable(var_infos, raw_info, out_dir, attrs, year, mips):
         attrs['mip'] = mip
         if var_info.frequency == 'mon':
             cube = _monthly_mean(cube)
-        if var_info.frequency == 'day':
-            if cube.coord('time').shape[0] < 300:
-                logger.warning(
-                    'Only %s days available. Can not generate daily files',
-                    cube.coord('time').shape[0]
-                )
-                continue
+            if cube.coord('time').shape[0] < 12:
+                cubes = CubeList(cube.slices_over('time'))
+                model_cube = cubes[0].copy()
+                for month in range(1, 13):
+                    if cubes.extract(iris.Constraint(
+                        time=lambda cell: cell.point.month == month
+                    )):
+                        continue
+                    cubes.append(_create_nan_cube(
+                        model_cube, month, month=True))
+                cube = cubes.merge_cube()
+                del cubes
+                del model_cube
+        elif var_info.frequency == 'day':
+            # if cube.coord('time').shape[0] < 300:
+            #     logger.warning(
+            #         'Only %s days available. Can not generate daily files',
+            #         cube.coord('time').shape[0]
+            #     )
+            #     continue
+            total_days = 366 if isleap(year) else 365
+            if cube.coord('time').shape[0] < total_days:
+                add_day_of_year(cube, 'time')
+                cubes = CubeList(cube.slices_over('time'))
+                model_cube = cubes[0].copy()
+                model_cube.remove_coord('day_of_year')
+                for day_of_year in range(total_days):
+                    if cubes.extract(
+                        iris.Constraint(day_of_year=day_of_year+1)
+                    ):
+                        continue
+                    nan_cube = _create_nan_cube(
+                        model_cube, day_of_year, month=False
+                    )
+                    add_day_of_year(nan_cube, 'time')
+                    cubes.append(nan_cube)
+                cube = cubes.merge_cube()
+                del cubes
+                del model_cube
+
         logger.debug(cube)
         fix_var_metadata(cube, var_info)
         convert_timeunits(cube, year)
         set_global_atts(cube, attrs)
         save_variable(cube, var_info.short_name, out_dir, attrs)
     return cube
+
+
+def _create_nan_cube(model_cube, num, month):
+    nan_cube = model_cube.copy(
+        np.ma.masked_all(model_cube.shape)
+    )
+    time_coord = nan_cube.coord('time')
+    nan_cube.remove_coord(time_coord)
+    date = time_coord.cell(0).point
+    if month:
+        date = datetime(date.year, num, date.day)
+        bounds = (
+            datetime(date.year, num, 1),
+            datetime(date.year, num, monthrange(date.year, num)[1])
+        )
+    else:
+        date = datetime(date.year, 1, 1, 12) + timedelta(days=num)
+        bounds = (
+            datetime(date.year, 1, 1) + timedelta(days=num),
+            datetime(date.year, 1, 1, 23, 59) + timedelta(days=num)
+        )
+
+    date = time_coord.units.date2num(date)
+    bounds = (
+        time_coord.units.date2num(bounds[0]),
+        time_coord.units.date2num(bounds[1]),
+    )
+    nan_cube.add_aux_coord(AuxCoord(
+        [date],
+        standard_name=time_coord.standard_name,
+        var_name=time_coord.var_name,
+        long_name=time_coord.long_name,
+        units=time_coord.units,
+        attributes=time_coord.attributes,
+        bounds=[bounds],
+    ))
+    return nan_cube
 
 
 @vectorize(['float32(float32)'])
@@ -130,10 +207,10 @@ def _unify_attributes(cubes):
 
 
 def _monthly_mean(cube):
-    add_month(cube, 'time')
+    add_month_number(cube, 'time')
     add_year(cube, 'time')
-    cube = cube.aggregated_by(('month', 'year'), MEAN)
-    cube.remove_coord('month')
+    cube = cube.aggregated_by(('month_number', 'year'), MEAN)
+    cube.remove_coord('month_number')
     cube.remove_coord('year')
     return cube
 
