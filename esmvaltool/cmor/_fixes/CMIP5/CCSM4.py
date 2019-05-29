ecc82348befb12766e39a572b28586e6cea3ee85
“katjaweigel” <weigel@iup.physik.uni-bremen.de>
2019-05-29 10:50:32 +0200

    Extensive use of "# noinspection PyPep8Naming" like for all other model fixes

--- esmvaltool/cmor/_fixes/CMIP5/CCSM4.py
+++ esmvaltool/cmor/_fixes/CMIP5/CCSM4.py
@@ -31,50 +31,62 @@ class rlut(Fix):
         return cubes
 
 
+# noinspection PyPep8Naming
 class rlutcs(rlut):
     """Fixes for rlutcs."""
 
 
+# noinspection PyPep8Naming
 class rsut(rlut):
     """Fixes for rsut."""
 
 
+# noinspection PyPep8Naming
 class rsutcs(rlut):
     """Fixes for rsutcs."""
 
 
+# noinspection PyPep8Naming
 class rlus(rlut):
     """Fixes for rlus."""
 
 
+# noinspection PyPep8Naming
 class rsus(rlut):
     """Fixes for rsus."""
 
 
+# noinspection PyPep8Naming
 class rsuscs(rlut):
     """Fixes for rsuscs."""
 
 
+# noinspection PyPep8Naming
 class rlds(rlut):
     """Fixes for rlds."""
 
 
+# noinspection PyPep8Naming
 class rldscs(rlut):
     """Fixes for rldscs."""
 
 
+# noinspection PyPep8Naming
 class rsds(rlut):
     """Fixes for rsds."""
 
 
+# noinspection PyPep8Naming
 class rsdscs(rlut):
     """Fixes for rsdscs."""
 
 
+# noinspection PyPep8Naming
 class rsdt(rlut):
     """Fixes for rsdt."""
 
 
+# noinspection PyPep8Naming
 class so(Fix):
     """Fixes for so."""
 
