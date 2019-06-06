0e1e5396bfaa7717a69ecae60161139444c1da14
Valeriu Predoi <valeriu.predoi@gmail.com>
2019-06-06 13:59:35 +0100

    added more tests and cleaned up

--- tests/unit/preprocessor/_multimodel/test_multimodel.py
+++ tests/unit/preprocessor/_multimodel/test_multimodel.py
@@ -2,25 +2,25 @@
 
 import unittest
 
+import cftime
 import iris
 import numpy as np
-from numpy.testing import assert_array_equal, assert_equal
-import cftime
 from cf_units import Unit
+from numpy.testing import assert_array_equal, assert_equal
 
 import tests
-from esmvalcore.preprocessor._multimodel import (_get_time_offset,
+from esmvalcore.preprocessor._multimodel import (_assemble_overlap_data,
                                                  _compute_statistic,
-                                                 _put_in_cube,
                                                  _datetime_to_int_days,
-                                                 _assemble_overlap_data)
+                                                 _get_time_offset,
+                                                 _put_in_cube)
 
 
 class Test(tests.Test):
-    """Test class for _volume_pp"""
+    """Test class for preprocessor/_multimodel.py."""
 
     def setUp(self):
-        """Prepare tests"""
+        """Prepare tests."""
         coord_sys = iris.coord_systems.GeogCS(iris.fileformats.pp.EARTH_RADIUS)
         data2 = np.ma.ones((2, 3, 2, 2))
         data3 = np.ma.ones((4, 3, 2, 2))
@@ -40,14 +40,14 @@ class Test(tests.Test):
                                          [0.5, 1.5],
                                          [1.5, 2.5],
                                          [2.5, 3.5],
-                                         [3.5, 4.5],
-                                     ],
+                                         [3.5, 4.5], ],
                                      units=Unit(
                                          'days since 1950-01-01',
                                          calendar='gregorian'))
 
         zcoord = iris.coords.DimCoord([0.5, 5., 50.],
-                                      long_name='zcoord',
+                                      standard_name='air_pressure',
+                                      long_name='air_pressure',
                                       bounds=[[0., 2.5], [2.5, 25.],
                                               [25., 250.]],
                                       units='m',
@@ -69,10 +69,6 @@ class Test(tests.Test):
         coords_spec5 = [(time2, 0), (zcoord, 1), (lats, 2), (lons, 3)]
         self.cube2 = iris.cube.Cube(data3, dim_coords_and_dims=coords_spec5)
 
-        # allow iris to figure out the axis='z' coordinate
-        iris.util.guess_coord_axis(self.cube1.coord('zcoord'))
-        iris.util.guess_coord_axis(self.cube2.coord('zcoord'))
-
     def test_get_time_offset(self):
         """Test time unit."""
         result = _get_time_offset("days since 1950-01-01")
@@ -80,7 +76,7 @@ class Test(tests.Test):
         assert_equal(result, expected)
 
     def test_compute_statistic(self):
-        """Test time unit."""
+        """Test statistic."""
         datas = [self.cube1.data[0], self.cube2.data[0]]
         stat_mean = _compute_statistic(datas, "mean")
         stat_median = _compute_statistic(datas, "median")
@@ -89,6 +85,24 @@ class Test(tests.Test):
         assert_array_equal(stat_mean, expected_mean)
         assert_array_equal(stat_median, expected_median)
 
+    def test_put_in_cube(self):
+        """Test put in cube."""
+        cube_data = np.ma.ones((2, 3, 2, 2))
+        stat_cube = _put_in_cube(self.cube1, cube_data, "mean", t_axis=None)
+        assert_array_equal(stat_cube.data, self.cube1.data)
+
+    def test_datetime_to_int_days(self):
+        """Test _datetime_to_int_days."""
+        computed_dats = _datetime_to_int_days(self.cube1)
+        expected_dats = [0, 31]
+        assert_array_equal(computed_dats, expected_dats)
+
+    def test_assemble_overlap_data(self):
+        """Test overlap data."""
+        comp_ovlap_mean = _assemble_overlap_data([self.cube1, self.cube1],
+                                                 [0, 31], "mean")
+        expected_ovlap_mean = np.ma.ones((2, 3, 2, 2))
+        assert_array_equal(comp_ovlap_mean.data, expected_ovlap_mean)
 
 
 if __name__ == '__main__':
