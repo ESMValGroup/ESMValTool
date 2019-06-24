dda4a4027f8fc88d65074ab300572aa0a051be62
Björn Brötz <bjoernbroetz@users.noreply.github.com>
2019-06-24 17:26:31 +0200

    Hardcode `docu_join` for testing

--- doc/sphinx/source/conf.py
+++ doc/sphinx/source/conf.py
@@ -33,7 +33,7 @@ on_rtd = os.environ.get("READTHEDOCS", None) == "True"
 # This is used for linking and such so we link to the thing we're building
 rtd_version = os.environ.get("READTHEDOCS_VERSION", "latest")
 if rtd_version not in ["stable", "latest"]:
-    rtd_version = "stable"
+    rtd_version = "docu_join"
 
 # Generate gallery
 sys.path.append(os.path.dirname(__file__))
