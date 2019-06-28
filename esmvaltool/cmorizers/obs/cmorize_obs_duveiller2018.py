10b558ee0ce11c5d14e1ba1b1417d3a7be33f347
Bas Crezee <bascrezee@gmail.com>
2019-06-28 16:33:25 +0200

    ran isort and yapf on cmorize_obs_duveiller2018.py

--- esmvaltool/cmorizers/obs/cmorize_obs_duveiller2018.py
+++ esmvaltool/cmorizers/obs/cmorize_obs_duveiller2018.py
@@ -19,55 +19,58 @@ Modification history
 
 """
 
+import calendar
+import datetime
 import logging
 import os
 from warnings import catch_warnings, filterwarnings
 
 import cf_units
-import numpy as np
 import iris
+import numpy as np
 from dask import array as da
-import calendar
-import datetime
 
-
-from .utilities import (set_global_atts, fix_coords, fix_var_metadata,
-                        read_cmor_config, save_variable, constant_metadata, convert_timeunits, flip_dim_coord)
+from .utilities import (constant_metadata, convert_timeunits, fix_coords,
+                        fix_var_metadata, flip_dim_coord, read_cmor_config,
+                        save_variable, set_global_atts)
 
 logger = logging.getLogger(__name__)
 
 
-
-def duveiller2018_callback_function(cube,field,filename):
+def duveiller2018_callback_function(cube, field, filename):
     # Rename 'Month' to 'time'
     cube.coord('Month').rename('time')
 
     # Create arrays for storing datetime objects
-    custom_time = np.zeros((12),dtype=object)
-    custom_time_bounds = np.empty((12,2),dtype=object)
+    custom_time = np.zeros((12), dtype=object)
+    custom_time_bounds = np.empty((12, 2), dtype=object)
     custom_time_units = 'days since 1950-01-01'
-    
+
     # Now fill the object arrays defined above with datetime objects corresponding to correct time and time_bnds
     for i in range(custom_time_bounds.shape[0]):
-        n_month = i+1 # we start with month number 1, at position 0
-        weekday,ndays_in_month = calendar.monthrange(2010,n_month)  # Start with bounds
-        time_bnd_a = datetime.datetime(2010,n_month,1)
-        time_bnd_b = datetime.datetime(2010,n_month,ndays_in_month)
-        time_midpoint = time_bnd_a + 0.5*(time_bnd_b - time_bnd_a)
-        custom_time_bounds[n_month-1,0] = time_bnd_a
-        custom_time_bounds[n_month-1,1] = time_bnd_b
-        custom_time[n_month-1] = time_midpoint
-    
+        n_month = i + 1  # we start with month number 1, at position 0
+        weekday, ndays_in_month = calendar.monthrange(
+            2010, n_month)  # Start with bounds
+        time_bnd_a = datetime.datetime(2010, n_month, 1)
+        time_bnd_b = datetime.datetime(2010, n_month, ndays_in_month)
+        time_midpoint = time_bnd_a + 0.5 * (time_bnd_b - time_bnd_a)
+        custom_time_bounds[n_month - 1, 0] = time_bnd_a
+        custom_time_bounds[n_month - 1, 1] = time_bnd_b
+        custom_time[n_month - 1] = time_midpoint
+
     # Convert them
-    time_bnds = cf_units.date2num(custom_time_bounds,custom_time_units,cf_units.CALENDAR_GREGORIAN)
-    time_midpoints = cf_units.date2num(custom_time,custom_time_units,cf_units.CALENDAR_GREGORIAN)
-    
+    time_bnds = cf_units.date2num(custom_time_bounds, custom_time_units,
+                                  cf_units.CALENDAR_GREGORIAN)
+    time_midpoints = cf_units.date2num(custom_time, custom_time_units,
+                                       cf_units.CALENDAR_GREGORIAN)
+
     # Add them to the cube
     cube.coord('time').bounds = time_bnds
     cube.coord('time').points = time_midpoints
 
     # Set the correct time unit, as defined above
     cube.coord('time').units = cf_units.Unit(custom_time_units)
+
 
 def extract_variable(var_info, raw_info, out_dir, attrs, cfg):
     """Extract to all vars."""
@@ -79,7 +82,8 @@ def extract_variable(var_info, raw_info, out_dir, attrs, cfg):
             category=UserWarning,
             module='iris',
         )
-        cubes = iris.load(raw_info['file'], callback=duveiller2018_callback_function)
+        cubes = iris.load(raw_info['file'],
+                          callback=duveiller2018_callback_function)
     rawvar = raw_info['name']
     print(cubes)
     for cube in cubes:
@@ -87,8 +91,10 @@ def extract_variable(var_info, raw_info, out_dir, attrs, cfg):
             # Extracting a certain vegetation transition code
             # Read iTr parameter from the cfg
             iTr = cfg['parameters']['iTr']
-            iTr_index = np.where(cube.coords('Vegetation transition code')[0].points == iTr)[0][0]
-            cube = cube[iTr_index,:,:,:]
+            iTr_index = np.where(
+                cube.coords('Vegetation transition code')[0].points ==
+                iTr)[0][0]
+            cube = cube[iTr_index, :, :, :]
             # Add the vegetation transition code as an attribute to keep it on the file
             cube.attributes['Vegetation transition code'] = iTr
             # Remove it as a coordinate, since it is not allowed as a CMOR coordinate
@@ -101,13 +107,8 @@ def extract_variable(var_info, raw_info, out_dir, attrs, cfg):
             flip_dim_coord(cube, 'latitude')
             # Global attributes
             set_global_atts(cube, attrs)
-            save_variable(
-                cube,
-                var,
-                out_dir,
-                attrs,
-                local_keys=['positive']
-            )
+            save_variable(cube, var, out_dir, attrs, local_keys=['positive'])
+
 
 def cmorization(in_dir, out_dir, cfg):
     """Cmorization func call."""
@@ -124,7 +125,7 @@ def cmorization(in_dir, out_dir, cfg):
         inpfile = os.path.join(in_dir, vals['file'])
         logger.info("CMORizing var %s from file %s", var, inpfile)
         var_info = cmor_table.get_variable(vals['mip'], var)
-        print("var = ",var)
+        print("var = ", var)
         raw_info = {'name': vals['raw'], 'file': inpfile}
         glob_attrs['mip'] = vals['mip']
         with catch_warnings():
@@ -136,4 +137,3 @@ def cmorization(in_dir, out_dir, cfg):
                 module='iris',
             )
             extract_variable(var_info, raw_info, out_dir, glob_attrs, cfg)
-
