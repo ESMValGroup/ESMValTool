afe003d79a73bf7bda6550882369ca97757b444d
Javier Vegas-Regidor <javier.vegas@bsc.es>
2019-07-04 15:24:17 +0200

    Allow computing all seasons at the same time

--- esmvaltool/diag_scripts/extreme_precipitation/extreme_precipitation.py
+++ esmvaltool/diag_scripts/extreme_precipitation/extreme_precipitation.py
@@ -19,17 +19,6 @@
 # Alexander J. Baker, UREAD
 # 19/10/2017
 # alexander.baker@reading.ac.uk
-
-
-
-# ----------
-# USER INPUT
-# ----------
-r_period = [2.,5.,10.,20.,30.,50.]  # Return periods (in years)
-sig = 0.05                          # Confidence interval
-
-
-
 #############################################################
 
 
@@ -57,18 +46,6 @@ import esmvaltool.diag_scripts.shared
 import esmvaltool.diag_scripts.shared.names as n
 import numpy as np
 
-# R interface
-from rpy2.robjects import r as R
-from rpy2.robjects.packages import importr
-import rpy2.robjects.numpy2ri
-
-extRemes = importr('extRemes')
-r_base = importr('base')
-rpy2.robjects.numpy2ri.activate()
-R['options'](warn = -1)
-R('rm(list = ls())')
-
-
 logger = logging.getLogger(os.path.basename(__file__))
 
 
@@ -80,84 +57,101 @@ class ExtremePrecipitation(object):
         self.return_period = self.cfg['return_period']
         self.confidence_interval = self.cfg['confidence_interval']
 
-        # --------------
-        # GEV PARAMETERS
-        # --------------
-        self.gev_par_sym = ['mu', 'sigma', 'xi']
-        self.gev_par_name = dict(mu='location', sigma='scale', xi='shape')
-        self.r_period_name = np.array(self.return_period).astype(int).astype(str)
-        for r in range(len(self.r_period_name)):
-            self.r_period_name[r] = '{}-year level'.format(self.return_period[r])
+        self.gev_param_symbols = ['mu', 'sigma', 'xi']
+        self.gev_param_name = dict(mu='location', sigma='scale', xi='shape')
         self.return_period = rpy2.robjects.IntVector(self.return_period)
 
+    def _get_return_period_name(self, period):
+        return '{}-year level'.format(self.return_period[r])
+
     def compute(self):
+        from rpy2.robjects import r as R, numpy2ri
+        from rpy2.robjects.packages import importr
+
+        extRemes = importr('extRemes')
+        numpy2ri.activate()
+        R['options'](warn=-1)
         for filename in self.filenames:
             logger.info('Processing %s', filename)
             cube = iris.load_cube(filename)
             logger.info(cube)
-            cube.data
-            logger.info('GEV analysis...')
 
             model_cube = cube[0, ...]
             shape = model_cube.shape
             units = model_cube.units
 
-            fevd = dict()
-            for par in self.gev_par_sym:
-                fevd[par] = np.full(shape, np.nan)
+            for season in set(cube.coords('clim_season')):
+                # Clean R objects
+                R('rm(list = ls())')
+                logger.info('Processing season {}', season)
+                season_cube = cube.extract(iris.Constraint(clim_season=season))
+                season_cube.data
 
-            rl = dict()
-            for r in self.r_period_name:
-                rl[r] = np.full(shape, np.nan)
+                logger.info('GEV analysis...')
+                fevd = dict()
+                for par in self.gev_param_symbols:
+                    fevd[par] = np.full(shape, np.nan)
 
-            logger.info('Apply')
-            evdf = r_base.apply(cube.data, 1, fun=fevd, units=units.origin)
-            logger.debug(evdf)
-            logger.debug('rpy2 only')
-            for x in range(shape[0]):
-                for y in range(shape[1]):
-                    data = cube.data[..., x, y]
-                    if np.any(data):
-                        evdf = extRemes.fevd(data, units=units.origin)
-                        # if evdf.rx2('results').rx2('par').rx2('location')[0] > 0. and\
-                        #     evdf.rx2('results').rx2('par').rx2('scale')[0] > 0.: # -ve mu/sigma invalid
-                        #     for par in self.gev_par_sym:
-                        #         val_ll = evdf.rx2('results').rx2('par').rx2(self.gev_par_name[par])[0]
+                rl = dict()
+                for period in self.return_period:
+                    rl[period] = np.full(shape, np.nan)
 
-                        #         fevd[par][x, y] = val_ll
-                        #     r_level = extRemes.return_level(evdf, return_period=self.return_period,
-                        #                                     qcov=extRemes.make_qcov(evdf))
+                for x in range(shape[0]):
+                    for y in range(shape[1]):
+                        data = cube.data[..., x, y]
+                        if np.any(data):
+                            self._compute_metric(
+                                data, units, fevd, rl, extRemes
+                            )
 
-                        #     for data, name in zip(r_level, self.r_period_name):
-                        #         rl[name][x, y] = data
-                        #     continue
-            logger.debug('finished')
-            for par, data in fevd.items():
-                fevd[par] = self._create_cube(data, par, model_cube)
+                for par, data in fevd.items():
+                    fevd[par] = self._create_cube(data, par, model_cube)
 
-            for name, data in rl.items():
-                rl[name] = self._create_cube(data, name, model_cube)
+                for period, data in rl.items():
+                    rl[period] = self._create_cube(
+                        data,
+                        self._get_return_period_name(period),
+                        model_cube
+                    )
 
+                logger.info('Saving data...')
+                results_subdir = os.path.join(
+                    self.filenames.get_info(n.WORK_DIR, filename),
+                    self.filenames.get_info(n.PROJECT, filename),
+                    self.filenames.get_info(n.DATASET, filename),
+                    season,
+                )
+                for par in self.gev_param_symbols:
+                    par_ffp = os.path.join(results_subdir, '{}.nc'.format(par))
+                    iris.save(fevd[par], par_ffp)
 
-            # Output results
-            results_subdir = os.path.join(
-                self.filenames.get_info(n.WORK_DIR, filename),
-                self.filenames.get_info(n.PROJECT, filename),
-                self.filenames.get_info(n.DATASET, filename),
+                return_periods_path = os.path.join(
+                    results_subdir, 'return_periods.nc'
+                )
+                iris.save(rl.values(), return_periods_path)
+
+    def _compute_metric(self, data, units, fevd, rl, extRemes):
+        evdf = extRemes.fevd(data, units=units.origin)
+        results = evdf.rx2('results').rx2('par')
+        # -ve mu/sigma invalid
+        if results.rx2('location')[0] > 0. and results.rx2('scale')[0] > 0.:
+            for par in self.gev_param_symbols:
+                fevd[par][x, y] = results.rx2(self.gev_param_name[par])[0]
+
+            r_level = extRemes.return_level(
+                evdf,
+                return_period=self.return_period,
+                qcov=extRemes.make_qcov(evdf)
             )
-            for par in self.gev_par_sym:
-                par_ffp = os.path.join(results_subdir, '{}.nc'.format(par))
-                iris.save(fevd[par], par_ffp)
 
-            return_periods_path = os.path.join(results_subdir, 'return_periods.nc')
-            iris.save(rl.values(), return_periods_path)
+            for data, period in zip(r_level, self.return_period):
+                rl[name][x, y] = data
 
     def _create_cube(self, data, name, model_cube):
         cube = model_cube.copy(data)
         cube.standard_name = None
         cube.long_name = name
         cube.var_name = None
-
 
 
 def main():
