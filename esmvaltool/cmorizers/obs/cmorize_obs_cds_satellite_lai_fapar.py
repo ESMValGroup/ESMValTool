5a1934a163726d8c597e923d03540b5c494ce17a
Bas Crezee <bascrezee@gmail.com>
2019-07-05 13:26:54 +0200

    Increased readability and improved formatting

--- esmvaltool/cmorizers/obs/cmorize_obs_cds_satellite_lai_fapar.py
+++ esmvaltool/cmorizers/obs/cmorize_obs_cds_satellite_lai_fapar.py
@@ -1,4 +1,5 @@
 """ESMValTool CMORizer for cds-satellite-lai-fapar data.
+
 Tier
    Tier 3
 Source
@@ -19,47 +20,47 @@ Download and processing instructions
    - According to ESMValTool practice, put them in the right rawobsdir folder
 
 Notes
-   This script regrids and cmorizes the above dataset.
+-----
+   - This script regrids and cmorizes the above dataset.
 
 Caveats
-   Currently variable FAPAR is not supported yet, this first needs custom var def in esmvalcore
+   - Handling of fAPAR not correct yet (standard_name can not be set)
+   - This dataset has custom time bounds (see dataset documentation
+     available from source)
+   - Currently files are written to rawobsdir and not cleaned-up
 
 Modification history
    20190703-A_crez_ba: written.
 """
 
-
+import glob
 import logging
-#from concurrent.futures import ProcessPoolExecutor, as_completed
+import os
 from copy import deepcopy
 from datetime import datetime
-#from os import cpu_count
-from pathlib import Path
 from warnings import catch_warnings, filterwarnings
-from esmvalcore.preprocessor import regrid
-import cf_units
 
+import cf_units
 import iris
-import numpy as np
-import os
-import glob
 
 from esmvalcore.cmor.table import CMOR_TABLES
-
-from . import utilities as utils
+from esmvalcore.preprocessor import regrid
+from esmvaltool.cmorizers.obs import utilities as utils
 
 logger = logging.getLogger(__name__)
 
-#TODO where to set this parameter?
-regridres = '0.25x0.25'
 
 def _cmorize_dataset(in_file, var, cfg, out_dir):
     logger.info("CMORizing variable '%s' from input file '%s'",
                 var['short_name'], in_file)
     attributes = deepcopy(cfg['attributes'])
     attributes['mip'] = var['mip']
-    cmor_table = CMOR_TABLES[attributes['project_id']]
+
+    cmor_table = cfg['cmor_table']
     definition = cmor_table.get_variable(var['mip'], var['short_name'])
+    if not definition:
+        cmor_table = CMOR_TABLES['custom']
+        definition = cmor_table.get_variable(var['mip'], var['short_name'])
 
     with catch_warnings():
         filterwarnings(
@@ -75,13 +76,14 @@ def _cmorize_dataset(in_file, var, cfg, out_dir):
 
     # Set correct names
     cube.var_name = definition.short_name
-    cube.standard_name = definition.standard_name
+    try:
+        cube.standard_name = definition.standard_name
+    except ValueError:
+        logger.warning(
+            "Could not set standard name %s for variable (var_name=) %s",
+            definition.standard_name, cube.var_name)
     cube.long_name = definition.long_name
 
-    for coord_name in 'latitude', 'longitude', 'time':
-        coord = cube.coord(coord_name)
-        coord.points = coord.core_points().astype('float64') #TODO @bouwe needed?  
-   
     # Convert units if required
     cube.convert_units(definition.units)
 
@@ -96,35 +98,48 @@ def _cmorize_dataset(in_file, var, cfg, out_dir):
 
     return in_file
 
-def _regrid_cds_satellite_lai_fapar(filepattern,var_info):
-    '''regrid each file and write to disk appending 'regrid' in front of filename'''
-    filelist = glob.glob(filepattern)
-    for infile in filelist:
-        infile_head,infile_tail = os.path.split(infile)
-        outfile_tail = infile_tail.replace('c3s','c3s_regridded')
-        outfile = os.path.join(infile_head,outfile_tail)
-        logger.info("Reading: {0}".format(infile))
-        lai_cube = iris.load_cube(infile,var_info.standard_name)
-        logger.info("Regridding to: {0}".format(regridres))
-        lai_cube = regrid(lai_cube,regridres,'nearest')
-        logger.info("Saving: {0}".format(outfile))
-        iris.save(lai_cube,outfile)
 
-def _concatenate_cds_satellite_lai_fpar_over_time(filepattern,var_info):
-    """Concatenates single files over time and returns on single cube"""
-    filelist = glob.glob(filepattern)
-    cubelist = iris.load(filelist,var_info.standard_name)
+def _regrid_dataset(in_dir, var, cfg):
+    """
+    Regridding of original files.
+
+    This function regrids each file and write to disk appending 'regrid'
+    in front of filename.
+    """
+    filelist = glob.glob(os.path.join(in_dir, var['file']))
+    for infile in filelist:
+        infile_head, infile_tail = os.path.split(infile)
+        outfile_tail = infile_tail.replace('c3s', 'c3s_regridded')
+        outfile = os.path.join(infile_head, outfile_tail)
+        lai_cube = iris.load_cube(infile,
+                                  constraint=utils.var_name_constraint(
+                                      var['raw']))
+        lai_cube = regrid(lai_cube, cfg['custom']['regrid_resolution'],
+                          'nearest')
+        logger.info("Saving: %s", outfile)
+        iris.save(lai_cube, outfile)
+
+
+def _concatenate_dataset_over_time(in_dir, var):
+    """Concatenates single files over time and returns on single cube."""
+    # This is a complicated expression, but necessary to keep local
+    # variables below the limit, otherwise prospector complains.
+    cubelist = iris.load(glob.glob(os.path.join(
+        in_dir, var['file'].replace('c3s', 'c3s_regridded'))))
 
     # For saving the identifiers
     identifiers = []
-    for n in range(len(cubelist)):
-        logger.info("Fixing time bounds for cube {0}".format(n+1))
-        time_coverage_start = cubelist[n].attributes.pop('time_coverage_start')
-        time_coverage_end = cubelist[n].attributes.pop('time_coverage_end')
-        # The identifier is a combination of the parent_identifier (which is not removed) and
-        identifier = cubelist[n].attributes.pop('identifier')
-        # Keep it in a list
-        identifiers.append(identifier)
+    # The purpose of the following loop is to remove any attributes
+    # that differ between cubes (otherwise concatenation over time fails).
+    # In addition, care is taken of the time coordinate, by adding the
+    # time_coverage attributes as time_bnds to the time coordinate.
+    for n_cube, _ in enumerate(cubelist):
+        time_coverage_start = cubelist[n_cube].\
+            attributes.pop('time_coverage_start')
+        time_coverage_end = cubelist[n_cube].\
+            attributes.pop('time_coverage_end')
+        # Remove identifier from attrs, add to list for later usage
+        identifiers.append(cubelist[n_cube].attributes.pop('identifier'))
 
         # Now put time_coverage_start/end as time_bnds
         # Convert time_coverage_xxxx to datetime
@@ -132,53 +147,49 @@ def _concatenate_cds_satellite_lai_fpar_over_time(filepattern,var_info):
         bnd_b = datetime.strptime(time_coverage_end, "%Y-%m-%dT%H:%M:%SZ")
 
         # Put in shape for time_bnds
-        time_bnds_datetime = [bnd_a,bnd_b]
+        time_bnds_datetime = [bnd_a, bnd_b]
 
         # Read dataset time unit and calendar from file
-        dataset_time_unit = str(cubelist[n].coord('time').units)
-        dataset_time_calender = cubelist[n].coord('time').units.calendar
+        dataset_time_unit = str(cubelist[n_cube].coord('time').units)
+        dataset_time_calender = cubelist[n_cube].coord('time').units.calendar
         # Convert datetime
-        time_bnds = cf_units.date2num(time_bnds_datetime, dataset_time_unit,dataset_time_calender)
+        time_bnds = cf_units.date2num(time_bnds_datetime, dataset_time_unit,
+                                      dataset_time_calender)
         # Put them on the file
-        cubelist[n].coord('time').bounds = time_bnds
-    
+        cubelist[n_cube].coord('time').bounds = time_bnds
+
     # Now the cubes can be concatenated over the time dimension
     cube = cubelist.concatenate_cube()
 
-    # Add identifiers from each cube to the concatenated cube as a comma separated list
-    cube.attributes['identifiers_comma_separated']=','.join(identifiers)
+    # Add identifiers from each cube to the concatenated cube
+    # as a comma separated list
+    cube.attributes['identifiers_comma_separated'] = ','.join(identifiers)
     return cube
+
 
 def cmorization(in_dir, out_dir, cfg):
     """Cmorization func call."""
-    cmor_table = cfg['cmor_table']
-    glob_attrs = cfg['attributes']
-
-
     # run the cmorization
     for short_name, var in cfg['variables'].items():
         var['short_name'] = short_name
+        # Now call every function with the following pattern (in_dir, var, cfg)
+        logger.info("Start regridding to: %s",
+                    cfg['custom']['regrid_resolution'])
+        _regrid_dataset(in_dir, var, cfg)
+        logger.info("Finished regridding")
+
         # First collect all information
         inpfile = os.path.join(in_dir, var['file'])
         logger.info("CMORizing var %s from file %s", short_name, inpfile)
-        var_info = cmor_table.get_variable(var['mip'], short_name)
-        raw_info = {'name': var['raw'], 'file': inpfile}
-        glob_attrs['mip'] = var['mip']
 
-        # Now start regridding
-        # Determine filepattern for all files that need regridding
-        regrid_filepattern = os.path.join(in_dir, var['file'])
-        _regrid_cds_satellite_lai_fapar(regrid_filepattern,var_info)
-
-        # Specify the filepattern of the regridded files
-        concatenate_filepattern = regrid_filepattern.replace('c3s','c3s_regridded')
-
-        result_cube = _concatenate_cds_satellite_lai_fpar_over_time(concatenate_filepattern,var_info)
-
+        logger.info("Start file concatenation over time")
+        result_cube = _concatenate_dataset_over_time(
+            in_dir, var)
         # Write it to disk
-        savename = os.path.join(in_dir, var_info.short_name+'_regridded.nc')
-        logger.info("saving as: {0}".format(savename))
-        iris.save(result_cube,savename)
+        savename = os.path.join(in_dir, var['short_name'] + '_regridded.nc')
+        logger.info("saving as: %s", savename)
+        iris.save(result_cube, savename)
+        logger.info("Finished file concatenation over time")
 
         with catch_warnings():
             filterwarnings(
@@ -188,8 +199,7 @@ def cmorization(in_dir, out_dir, cfg):
                 category=UserWarning,
                 module='iris',
             )
-            print(var_info, raw_info, out_dir, glob_attrs)
             in_file = savename
+            logger.info("Start CMORization of file")
             _cmorize_dataset(in_file, var, cfg, out_dir)
-
-
+        logger.info("Finished regridding and CMORizing %s", in_file)
