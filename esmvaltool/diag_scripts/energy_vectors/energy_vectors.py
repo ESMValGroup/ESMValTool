33caf68c5b1effba192833021eb87cd89e7e19fd
Javier Vegas-Regidor <javier.vegas@bsc.es>
2019-07-08 16:26:12 +0200

    Add plots

--- esmvaltool/diag_scripts/energy_vectors/energy_vectors.py
+++ esmvaltool/diag_scripts/energy_vectors/energy_vectors.py
@@ -17,6 +17,7 @@ IDENTIFY_DATASET = (
     NAMES.PROJECT, NAMES.ACTIVITY, NAMES.DATASET, NAMES.EXP, NAMES.ENSEMBLE
 )
 
+
 class EnergyVectors(object):
 
     def __init__(self, config):
@@ -26,7 +27,8 @@ class EnergyVectors(object):
         self.window = self.cfg['window']
 
     def compute(self):
-        for ua_path in self.datasets.get_path_list(standard_name='eastward_wind'):
+        ua_paths = self.datasets.get_path_list(standard_name='eastward_wind')
+        for ua_path in ua_paths:
             ua_info = self.datasets.get_dataset_info(ua_path)
             logger.info(ua_info)
             va_info = {
@@ -36,13 +38,17 @@ class EnergyVectors(object):
             va_info[NAMES.STANDARD_NAME] = 'eastward_wind'
             va_path = self.datasets.get_path(**va_info)
 
-
             logger.info("Processing %s", ua_path)
-
             ua_cube = iris.load_cube(ua_path)
-            va_cube = iris.load_cube(va_path)
+            self._remove_extra_coords(ua_cube)
 
-            filter_weights = low_pass_weights(self.window, freq=ua_info['frequency'])
+            va_cube = iris.load_cube(va_path)
+            self._remove_extra_coords(va_cube)
+
+            filter_weights = low_pass_weights(
+                self.window,
+                freq=ua_info['frequency']
+            )
             assert abs(sum(filter_weights)-1) < 1e-8
 
             logger.info("Calculating horizontal E-vectors")
@@ -50,21 +56,54 @@ class EnergyVectors(object):
                 ua_cube, va_cube, filter_weights
             )
 
-            ic.add_month_number(evector_x, 'time', 'month_number')
-            ic.add_month_number(evector_y, 'time', 'month_number')
+            evector_x = self._get_monthly_mean(evector_x)
+            evector_y = self._get_monthly_mean(evector_y)
 
-            evector_x = evector_x.aggregated_by(('month_number', 'year'), MEAN)
-            evector_y = evector_y.aggregated_by(('month_number', 'year'), MEAN)
+    def _save(self, evector_x, evector_y, dataset):
+        if not self.cfg[NAMES.WRITE_NETCDF]:
+            return
+        logger.info("Saving results")
+        subdir = os.path.join(
+            self.cfg[NAMES.WORK_DIR],
+            self.datasets.get_info(NAMES.PROJECT, dataset),
+            self.datasets.get_info(NAMES.DATASET, dataset),
+        )
+        os.makedirs(subdir, exist_ok=True)
+        iris.save(evector_x, os.path.join(subdir, 'evector_x.nc'))
+        iris.save(evector_y, os.path.join(subdir, 'evector_y.nc'))
 
-            logger.info("Saving results")
-            subdir = os.path.join(
-                self.cfg[NAMES.WORK_DIR],
-                self.datasets.get_info(NAMES.PROJECT, ua_path),
-                self.datasets.get_info(NAMES.DATASET, ua_path),
-            )
-            os.makedirs(subdir, exist_ok=True)
-            iris.save(evector_x, os.path.join(subdir, 'evector_x.nc'))
-            iris.save(evector_y, os.path.join(subdir, 'evector_x.nc'))
+    def _plot(self, evector_x, evector_y, dataset):
+        if not self.cfg[NAMES.WRITE_NETCDF]:
+            return
+        logger.info("Saving results")
+        subdir = os.path.join(
+            self.cfg[NAMES.PLOT_DIR],
+            self.datasets.get_info(NAMES.PROJECT, dataset),
+            self.datasets.get_info(NAMES.DATASET, dataset),
+        )
+        os.makedirs(subdir, exist_ok=True)
+        quickplot(
+            evector_x,
+            filename=os.path.join(
+                subdir,
+                'evector_x.{}'.format(self.cfg[NAMES.OUTPUT_FILE_TYPE])
+            ),
+            **(self.cfg.get(
+                'quickplot',
+                {'plot_type': 'pcolormesh', 'cmap': 'Reds'}
+            ))
+        )
+        quickplot(
+            evector_y,
+            filename=os.path.join(
+                subdir,
+                'evector_y.{}'.format(self.cfg[NAMES.OUTPUT_FILE_TYPE])
+            ),
+            **(self.cfg.get(
+                'quickplot',
+                {'plot_type': 'pcolormesh', 'cmap': 'Reds'}
+            ))
+        )
 
     def _compute_evectors(self, ua_cube, va_cube, filter_weights):
         '''
@@ -92,14 +131,16 @@ class EnergyVectors(object):
 
         # get high frequency components by subtracting filtered values
         half_window = window_size//2
-        logger.info(ua_cube[half_window:-(half_window)].coord('time'))
-        logger.info(ua_cube_filtered.coord('time'))
+        ua_cube.coord('time').bounds = None
+        va_cube.coord('time').bounds = None
+
         ua_high = ua_cube[half_window:-(half_window)] - ua_cube_filtered
         va_high = va_cube[half_window:-(half_window)] - va_cube_filtered
 
         # calculate E-vector components, filtering them as we go
         evector_x = lanczos_filter(
-            va_high ** 2 - ua_high ** 2,  filter_weights)
+            va_high ** 2 - ua_high ** 2, filter_weights
+        )
         evector_y = lanczos_filter(-1.0 * ua_high * va_high, filter_weights)
 
         # Add some appropriate meta data
@@ -116,8 +157,20 @@ class EnergyVectors(object):
         evector_y.long_name = 'meridional component of E-Vector'
         evector_y.units = units
         evector_y.attributes['filtering'] = filtering
-
         return evector_x, evector_y
+
+    @staticmethod
+    def _remove_extra_coords(cube):
+        cube.remove_coord('year')
+        cube.remove_coord('day_of_year')
+        cube.remove_coord('month_number')
+        cube.remove_coord('day_of_month')
+
+    @staticmethod
+    def _get_monthly_mean(vector):
+        ic.add_year(vector, 'time')
+        ic.add_month_number(vector, 'time')
+        return vector.aggregated_by(('month_number', 'year'), MEAN)
 
 
 def main():
