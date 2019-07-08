efef2e35d241c9b73790ec72da78416e758b393f
Javier Vegas-Regidor <javier.vegas@bsc.es>
2019-07-08 10:59:24 +0200

    Add plots

--- esmvaltool/diag_scripts/extreme_precipitation/extreme_precipitation.py
+++ esmvaltool/diag_scripts/extreme_precipitation/extreme_precipitation.py
@@ -24,27 +24,16 @@
 
 import os
 import logging
-import string
 
-import matplotlib
-matplotlib.use('Agg')  # noqa
-import matplotlib.pyplot as plt
-from matplotlib import colors
-import matplotlib.font_manager
-from matplotlib.offsetbox import TextArea, VPacker, AnnotationBbox
-
-from PIL import Image
-from PIL import ImageDraw
-from PIL import ImageFont
-
+import numpy as np
 import iris
 import iris.cube
 import iris.analysis
 import iris.util
 
 import esmvaltool.diag_scripts.shared
+from esmvaltool.diag_scripts.shared.plot import quickplot
 import esmvaltool.diag_scripts.shared.names as n
-import numpy as np
 
 logger = logging.getLogger(os.path.basename(__file__))
 
@@ -85,7 +74,6 @@ class ExtremePrecipitation(object):
                 R('rm(list = ls())')
                 logger.info('Processing season {}', season)
                 season_cube = cube.extract(iris.Constraint(clim_season=season))
-                season_cube.data
 
                 logger.info('GEV analysis...')
                 fevd = dict()
@@ -113,22 +101,9 @@ class ExtremePrecipitation(object):
                         self._get_return_period_name(period),
                         model_cube
                     )
+                self._plot_results(filename, season, fevd, rl)
+                self._save_results(filename, season, fevd, rl)
 
-                logger.info('Saving data...')
-                results_subdir = os.path.join(
-                    self.filenames.get_info(n.WORK_DIR, filename),
-                    self.filenames.get_info(n.PROJECT, filename),
-                    self.filenames.get_info(n.DATASET, filename),
-                    season,
-                )
-                for par in self.gev_param_symbols:
-                    par_ffp = os.path.join(results_subdir, '{}.nc'.format(par))
-                    iris.save(fevd[par], par_ffp)
-
-                return_periods_path = os.path.join(
-                    results_subdir, 'return_periods.nc'
-                )
-                iris.save(rl.values(), return_periods_path)
 
     def _compute_metric(self, data, units, fevd, rl, extRemes):
         evdf = extRemes.fevd(data, units=units.origin)
@@ -137,13 +112,11 @@ class ExtremePrecipitation(object):
         if results.rx2('location')[0] > 0. and results.rx2('scale')[0] > 0.:
             for par in self.gev_param_symbols:
                 fevd[par][x, y] = results.rx2(self.gev_param_name[par])[0]
-
             r_level = extRemes.return_level(
                 evdf,
                 return_period=self.return_period,
                 qcov=extRemes.make_qcov(evdf)
             )
-
             for data, period in zip(r_level, self.return_period):
                 rl[name][x, y] = data
 
@@ -152,6 +125,63 @@ class ExtremePrecipitation(object):
         cube.standard_name = None
         cube.long_name = name
         cube.var_name = None
+        return cube
+
+    def _plot_results(self, filename, season, fevd, rl):
+        if not self.cfg[n.WRITE_PLOTS]:
+            return
+        logger.info('Plot results')
+        results_subdir = os.path.join(
+            self.filenames.get_info(n.PLOT_DIR, filename),
+            self.filenames.get_info(n.PROJECT, filename),
+            self.filenames.get_info(n.DATASET, filename),
+            season,
+        )
+        for par in self.gev_param_symbols:
+            par_ffp = os.path.join(
+                results_subdir,
+                '{}.{}'.format(
+                    self.gev_param_name[par],
+                    self.cfg[n.OUTPUT_FILE_TYPE]
+                )
+            )
+            quickplot(
+                fevd[par],
+                filename=par_ffp,
+                **(self.cfg.get('gev_quickplot', {}))
+            )
+        for return_period in rl:
+            return_periods_path = os.path.join(
+                results_subdir,
+                'return_period_{}years.{}'.format(
+                    return_period,
+                    self.cfg[n.OUTPUT_FILE_TYPE]
+                )
+            )
+            quickplot(
+                rl[return_period],
+                filename=return_periods_path,
+                **(self.cfg.get('return_period_quickplot', {}))
+            )
+
+    def _save_results(self, filename, season, fevd, rl):
+        if not self.cfg[n.WRITE_NETCDF]:
+            return
+        logger.info('Saving data...')
+        results_subdir = os.path.join(
+            self.filenames.get_info(n.WORK_DIR, filename),
+            self.filenames.get_info(n.PROJECT, filename),
+            self.filenames.get_info(n.DATASET, filename),
+            season,
+        )
+        for par in self.gev_param_symbols:
+            par_ffp = os.path.join(results_subdir, '{}.nc'.format(par))
+            iris.save(fevd[par], par_ffp)
+
+        return_periods_path = os.path.join(
+            results_subdir, 'return_periods.nc'
+        )
+        iris.save(rl.values(), return_periods_path)
 
 
 def main():
