b22adde5840421b53f7c926825bb97a460ad7344
sloosvel <saskia.loosveldt@bsc.es>
2019-07-12 16:29:48 +0200

    Perform temporal mean

--- esmvaltool/diag_scripts/primavera/eady_growth_rate.py
+++ esmvaltool/diag_scripts/primavera/eady_growth_rate.py
@@ -9,15 +9,13 @@ import iris.util
 
 import numpy as np
 
-import numba
-from numba import guvectorize, float64
-
-
 from dask import array as da
 
 import esmvaltool.diag_scripts.shared
 import esmvaltool.diag_scripts.shared.names as n
 from esmvaltool.diag_scripts.shared import group_metadata
+
+from esmvalcore.preprocessor import _time
 
 logger = logging.getLogger(os.path.basename(__file__))
 
@@ -26,48 +24,41 @@ class EadyGrowthRate(object):
     def __init__(self, config):
         self.cfg = config
         self.filenames = esmvaltool.diag_scripts.shared.Datasets(self.cfg)
+        self.fill_value = 1e20
+        self.ref_p = 1000.0
         self.g = 9.80665
         self.con = 0.3098
         self.omega = 7.292e-5
 
     def compute(self):
         data = group_metadata(self.cfg['input_data'].values(), 'dataset')
-        for key in data:
-
-            ta = iris.load_cube(data[key][0]['filename'])
+        for dataset in data:
+            ta = iris.load_cube(data[dataset][0]['filename'])
             plev = ta.dim_coords[1]
-            min_plev = plev.points.min()
 
             theta = self.potential_temperature(ta, plev)
 
             del ta
 
-            zg = iris.load_cube(data[key][1]['filename'])
+            zg = iris.load_cube(data[dataset][1]['filename'])
 
-            brunt =  self.brunt_vaisala_frq(theta, zg)
-
-            del theta
+            brunt = self.brunt_vaisala_frq(theta, zg)
 
             lats = zg.dim_coords[2]
             fcor = self.coriolis(lats, zg.shape)
 
-            ua = iris.load_cube(data[key][2]['filename'])
+            ua = iris.load_cube(data[dataset][2]['filename'])
 
             egr = self.eady_growth_rate(fcor, ua, zg, brunt)
 
+            cube_egr = ua.copy(egr * 86400)
 
-            iris.save(iris.cube.Cube(egr), '/scratch/Earth/sloosvel/esmvaltool/output/brun.nc')
+            cube_egr = cube_egr.collapsed('time', iris.analysis.MEAN)
 
-
-
-
-
-
-        a=1
-
+            self.save(cube_egr, dataset)
 
     def potential_temperature(self, ta, plev):
-        p0 = iris.coords.AuxCoord(1000.0, long_name='reference_pressure',
+        p0 = iris.coords.AuxCoord(self.ref_p, long_name='reference_pressure',
                                   units='hPa')
         p0.convert_units(plev.units)
         p = (p0.points/plev.points)**(2/7)
@@ -76,20 +67,32 @@ class EadyGrowthRate(object):
         return theta
 
     def vertical_integration(self, x, y):
-        # Weird attempt to perform a non-cyclic centered difference to integrate along pressure levels
+        # Perform a non-cyclic centered finite-difference to integrate
+        # along pressure levels
+
         plevs = x.shape[1]
-        dxdy_0 = (x[:, 1, :, :].lazy_data() - x[:, 0, :, :].lazy_data()) / (y[:, 1, :, :].lazy_data() - y[:, 0, :, :].lazy_data())
 
+        dxdy_0 = ((x[:, 1, :, :].lazy_data() - x[:, 0, :, :].lazy_data()) /
+                  (y[:, 1, :, :].lazy_data() - y[:, 0, :, :].lazy_data()))
 
-        dxdy_centre = (x[:, 2:plevs, :, :].lazy_data() - x[:, 0:plevs-2, :, :].lazy_data()) / (y[:, 2:plevs, :, :].lazy_data() - y[:, 0:plevs-2, :, :].lazy_data())
-        dxdy_end = (x[:, plevs-1, :, :].lazy_data() - x[:, plevs-2, :, :].lazy_data()) / (y[:, plevs-1, :, :].lazy_data() - y[:, plevs-2, :, :].lazy_data())
+        dxdy_centre = ((x[:, 2:plevs, :, :].lazy_data() -
+                       x[:, 0:plevs-2, :, :].lazy_data()) /
+                       (y[:, 2:plevs, :, :].lazy_data() -
+                       y[:, 0:plevs-2, :, :].lazy_data()))
+
+        dxdy_end = ((x[:, plevs-1, :, :].lazy_data() -
+                    x[:, plevs-2, :, :].lazy_data()) /
+                    (y[:, plevs-1, :, :].lazy_data() -
+                    y[:, plevs-2, :, :].lazy_data()))
 
         bounds = [dxdy_end, dxdy_0]
         stacked_bounds = da.stack(bounds, axis=1)
         total = [dxdy_centre, stacked_bounds]
 
+        # Concatenate arrays where the last slice is dxdy_0
         dxdy = da.concatenate(total, axis=1)
 
+        # Move dxdy_0 to the beggining of the array
         dxdy = da.roll(dxdy, 1, axis=1)
 
         return dxdy
@@ -99,15 +102,14 @@ class EadyGrowthRate(object):
         dthdz = da.where(dthdz > 0, dthdz, 0)
         buoy = (self.g / theta.lazy_data()) * dthdz
         brunt = da.sqrt(buoy)
-        brunt = da.where(brunt != 0, brunt, 1e20)
+        brunt = da.where(brunt != 0, brunt, self.fill_value)
 
         return brunt
 
     def coriolis(self, lats, ndim):
-        fcor = 2*self.omega*np.sin(np.radians(lats.points))
+        fcor = 2.0*self.omega*np.sin(np.radians(lats.points))
         fcor = fcor[np.newaxis, np.newaxis, :, np.newaxis]
         fcor = da.broadcast_to(fcor, ndim)
-
 
         return fcor
 
@@ -117,8 +119,19 @@ class EadyGrowthRate(object):
 
         return egr
 
+    def save(self, egr, dataset):
+        egr.standard_name = None
+        egr.long_name = 'eady_growth_rate'
+        egr.var_name = 'egr'
+        egr.units = 'day-1'
 
+        script = self.cfg[n.SCRIPT]
+        filename = '{dataset}_{script}.nc'.format(
+            dataset=dataset,
+            script=script
+        )
 
+        iris.save(egr, os.path.join(self.cfg[n.WORK_DIR], filename))
 
 
 def main():
