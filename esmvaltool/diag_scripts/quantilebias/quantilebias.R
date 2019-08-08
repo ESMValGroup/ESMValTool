93860d78b400e5b14c754f2ce896d74e88e7a504
jhardenberg <hardenberg@gmail.com>
2019-08-08 17:54:41 +0200

    using external cdo

--- esmvaltool/diag_scripts/quantilebias/quantilebias.R
+++ esmvaltool/diag_scripts/quantilebias/quantilebias.R
@@ -26,39 +26,13 @@
 library(tools)
 library(yaml)
 
-cdo <- function(command, args = "", input = "", options = "", output = "",
-                stdout = "", noout = F) {
-  if (args != "") args <- paste0(",", args)
-  if (stdout != "") stdout <- paste0(" > '", stdout, "'")
-  if (input[1] != "") {
-    for (i in 1:length(input)) {
-      input[i] <- paste0("'", input[i], "'")
-    }
-    input <- paste(input, collapse = " ")
-  }
-  output0 <- output
-  if (output != "") {
-    output <- paste0("'", output, "'")
-  } else if ( !noout ) {
-    output <- tempfile()
-    output0 <- output
-  }
-  argstr <- paste0(options, " ", command, args, " ", input, " ", output,
-                   " ", stdout)
-  print(paste("cdo", argstr))
-  ret <- system2("cdo", args = argstr)
-  if (ret != 0) {
-    stop(paste("Failed (", ret, "): cdo", argstr))
-  }
-  return(output0)
-}
-
 # get path to script and source subroutines (if needed)
 args <- commandArgs(trailingOnly = FALSE)
 spath <- paste0(dirname(unlist(strsplit(grep("--file", args,
                                              value = TRUE), "="))[2]), "/")
 
 source(paste0(spath, "quantilebias_functions.R"))
+source(paste0(spath, "../shared/external.R"))  # nolint
 
 # read settings and metadata files
 args <- commandArgs(trailingOnly = TRUE)
