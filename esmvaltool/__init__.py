9ab5b2b12f89d458f3b5500e9842e5ad32337fe5
Björn Brötz <bjoern.broetz@dlr.de>
2019-08-12 16:51:00 +0200

    Get get_script_root function back to ESMValTool.

--- esmvaltool/__init__.py
+++ esmvaltool/__init__.py
@@ -1,2 +1,9 @@
 """ESMValTool diagnostics package."""
+import os
+
 __version__ = '2.0.0b0'
+
+
+def get_script_root():
+    """Return the location of the ESMValTool installation."""
+    return os.path.abspath(os.path.dirname(__file__))
