b083e5ced5f24c15000f337aebc01cc53e26453d
Javier Vegas-Regidor <javier.vegas@bsc.es>
2019-08-12 17:01:01 +0200

    Add a bit more of computation

--- esmvaltool/diag_scripts/primavera/jet_latitude.py
+++ esmvaltool/diag_scripts/primavera/jet_latitude.py
@@ -3,6 +3,9 @@ import logging
 
 import matplotlib.pyplot as plt
 
+import dask.array as da
+import numpy as np
+
 import iris
 import iris.cube
 import iris.analysis
@@ -27,12 +30,34 @@ class JetLatitude(object):
 
     def compute(self):
         data = group_metadata(self.cfg['input_data'].values(), 'alias')
+        lanczos_weight = np.load('/home/users/panos/work_space/jetlat/python/LF_weights.npy')
         for alias in data:
             ua = iris.load_cube(data[alias][0]['filename'])
             logger.debug(ua)
+            ua_filtered = da.apply_along_axis(
+                lambda m: np.convolve(m, lanczos_weight, mode='same'),
+                axis=ua.dim_coords('time')[0],
+                arr=ua.core_data()
+            )
+            ua_filtered = ua.copy(ua_filtered)
+            ua_max = ua_filtered.collapsed('latitude', iris.analysis.MAX)
+            ua_max_lat = da.argmax(
+                ua_filtered.core_data(),
+                axis=ua.dim_coords('latitude')[0]
+            )
+            ua_max_lat = ua_max.copy(ua_max_lat)
             qp.pcolor(ua)
             plt.savefig(os.path.join(
                 self.cfg[n.PLOT_DIR], '{}.png'.format(alias)))
+            qp.pcolor(ua_filtered)
+            plt.savefig(os.path.join(
+                self.cfg[n.PLOT_DIR], '{}_filtered.png'.format(alias)))
+            qp.plot(ua_max)
+            plt.savefig(os.path.join(
+                self.cfg[n.PLOT_DIR], '{}_max.png'.format(alias)))
+            qp.plot(ua_max_lat)
+            plt.savefig(os.path.join(
+                self.cfg[n.PLOT_DIR], '{}_maxlat.png'.format(alias)))
 
 
 def main():
