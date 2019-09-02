d7dd32e4f5ce42a06d9427d107a0900770da4fd4
Bas Crezee <bascrezee@gmail.com>
2019-09-02 13:26:39 +0200

    some formatting

--- esmvaltool/cmorizers/obs/cmorize_obs_cds_uerra_reanalysis.py
+++ esmvaltool/cmorizers/obs/cmorize_obs_cds_uerra_reanalysis.py
@@ -1,4 +1,4 @@
-"""ESMValTool CMORizer for XXXXXXXXXXXXXXXXX
+"""ESMValTool CMORizer for cds-uerra-reanalysis version UERRA-HARMONIE
 
 Tier
    Tier 3
@@ -33,41 +33,20 @@ import glob
 import logging
 import os
 from copy import deepcopy
-from datetime import datetime
-from warnings import catch_warnings, filterwarnings
 
 import cf_units
 import iris
 import xarray as xr
 import xesmf as xe
 
-from .utilities import (constant_metadata, convert_timeunits, fix_coords,
-                        fix_var_metadata, save_variable, set_global_atts)
-from esmvalcore.preprocessor import regrid
 from esmvalcore.preprocessor._regrid import _stock_cube
-
 from esmvaltool.cmorizers.obs import utilities as utils
+
+from .utilities import (convert_timeunits, fix_coords,
+                        fix_var_metadata, save_variable, set_global_atts)
 
 logger = logging.getLogger(__name__)
 
-
-def _attrs_are_the_same(cubelist):
-    # assume they are the same
-    attrs_the_same = True
-    allattrs = cubelist[0].attributes
-    for key in allattrs:
-        try:
-            unique_attr_vals = {cube.attributes[key] for cube in cubelist}
-        # This exception is needed for valid_range, which is an
-        # array and therefore not hashable
-        except TypeError:
-            unique_attr_vals = {tuple(cube.attributes[key])
-                                for cube in cubelist}
-        if len(unique_attr_vals) > 1:
-            attrs_the_same = False
-            print("Different values found for {0}-attribute: {1}"
-                  .format(key, unique_attr_vals))
-    return attrs_the_same
 
 def _cmorize_dataset(in_file, var, cfg, out_dir):
     logger.info("CMORizing variable '%s' from input file '%s'",
@@ -78,21 +57,18 @@ def _cmorize_dataset(in_file, var, cfg, out_dir):
     cmor_table = cfg['cmor_table']
     definition = cmor_table.get_variable(var['mip'], var['short_name'])
 
-    cube = iris.load_cube(
-        str(in_file),
-        constraint=utils.var_name_constraint(var['raw']))
-
-    #import IPython;IPython.embed()
-
+    cube = iris.load_cube(str(in_file),
+                          constraint=utils.var_name_constraint(var['raw']))
 
     # The following lines are essential before applying the common function fix_coords
     # Convert time calendar from proleptic_gregorian to gregorian
-    cube.coord('time').units = cf_units.Unit(cube.coord('time').units.origin,'gregorian')
+    cube.coord('time').units = cf_units.Unit(
+        cube.coord('time').units.origin, 'gregorian')
 
     # Set standard_names for lat and lon
     cube.coord('lat').standard_name = 'latitude'
     cube.coord('lon').standard_name = 'longitude'
-    
+
     cube = fix_coords(cube)
 
     # Set correct names
@@ -110,6 +86,7 @@ def _cmorize_dataset(in_file, var, cfg, out_dir):
     utils.save_variable(cube, cube.var_name, out_dir, attributes)
 
     return in_file
+
 
 def _regrid_dataset(in_dir, var, cfg):
     """
@@ -130,7 +107,7 @@ def _regrid_dataset(in_dir, var, cfg):
         targetgrid_ds = xr.DataArray.from_iris(targetgrid)
         input_ds = xr.open_dataset(infile)
         # Do renaming for consistency of coordinate names
-        input_ds = input_ds.rename({'latitude': 'lat','longitude': 'lon'})
+        input_ds = input_ds.rename({'latitude': 'lat', 'longitude': 'lon'})
         # TODO check selection of soil level
         input_da = input_ds[var['raw']].isel(level=0)
         logger.info("Regridding... ")
@@ -138,15 +115,17 @@ def _regrid_dataset(in_dir, var, cfg):
         # related to Github issue
         constantval = 10
         input_da = input_da + constantval
-        assert(int((input_da==0.).sum())==0) # Make sure that there 
-                                             # are no zero's in the data, 
-                                             # since they will be masked out
-        #TODO configure such that weightfile is written into workdir
-        regridder = xe.Regridder(input_ds,targetgrid_ds,'bilinear',reuse_weights=True)
+        assert (int((input_da == 0.).sum()) == 0)  # Make sure that there
+        # are no zero's in the data,
+        # since they will be masked out
+        regridder = xe.Regridder(input_ds,
+                                 targetgrid_ds,
+                                 'bilinear',
+                                 reuse_weights=True)
         da_out = regridder(input_da)
-        da_out = da_out.where(da_out!=0.)
+        da_out = da_out.where(da_out != 0.)
         da_out = da_out - constantval
-        
+
         # Save it.
         logger.info("Saving: %s", outfile)
         da_out.to_netcdf(outfile)
@@ -169,29 +148,31 @@ def cmorization(in_dir, out_dir, cfg, cfg_user):
         logger.info("Processing var %s", short_name)
 
         # Regridding
-        logger.info("Start regridding to: %s",
-                    cfg['custom']['regrid'])
+        logger.info("Start regridding to: %s", cfg['custom']['regrid'])
         #TODO set back
         #_regrid_dataset(in_dir, var, cfg)
         logger.info("Finished regridding")
 
         #TODO move this block to a function
-        for year in range(2008,2009):
+        for year in range(2008, 2009):
             # File concatenation
-            logger.info("Concatenating files over time for year {0}".format(year))
+            logger.info(
+                "Concatenating files over time for year %s".format(year))
 
-            filelist = glob.glob(cfg['work_dir']+'uerra_regridded_{0}??.nc'.format(year))
+            filelist = glob.glob(cfg['work_dir'] +
+                                 'uerra_regridded_{0}??.nc'.format(year))
             cubelist = iris.load(filelist)
             for cube in cubelist:
-                cube.remove_coord('time') # Time has strange values, so use 
-                                          # forecast_reference_time instead
+                cube.remove_coord('time')  # Time has strange values, so use
+                # forecast_reference_time instead
                 cube.coord('forecast_reference_time').rename('time')
 
             cube_concatenated = cubelist.concatenate()[0]
 
-            in_file = os.path.join(cfg['work_dir'],'concatenated_{0}.nc'.format(year))
-            logger.info("Saving as: {0}".format(in_file))
-            iris.save(cube_concatenated,in_file)
+            in_file = os.path.join(cfg['work_dir'],
+                                   'concatenated_{0}.nc'.format(year))
+            logger.info("Saving as: %s".format(in_file))
+            iris.save(cube_concatenated, in_file)
 
             # Read in the full dataset here from 'workdir'
             logger.info("CMORizing")
