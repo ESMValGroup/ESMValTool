d967ab3c150ca322eed84761e623eaa6af1a3966
Mattia Righi <mattia.righi@dlr.de>
2019-09-11 15:00:33 +0200

    Update esmvaltool/cmorizers/obs/cmorize_obs_duveiller2018.py

--- esmvaltool/cmorizers/obs/cmorize_obs_duveiller2018.py
+++ esmvaltool/cmorizers/obs/cmorize_obs_duveiller2018.py
@@ -18,7 +18,7 @@ Download and processing instructions
 
 Modification history
    20190701-A_crez_ba: submitted PR
-   20190430-A_crez_ba: started with cmorize_obs_Landschuetzer2016.py as an
+   20190430-crezee_bas: written based on cmorize_obs_Landschuetzer2016.py.
 
 Caveats
    Please be aware that the selected vegetation transition code is not written
