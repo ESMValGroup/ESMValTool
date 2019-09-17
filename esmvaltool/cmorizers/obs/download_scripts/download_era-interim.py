800f60b58f3d49be0f14434da62a3a0865a6f30f
SarahAlidoost <f.alidoost@esciencecenter.nl>
2019-09-17 13:31:21 +0200

    solving some pylint errors

--- esmvaltool/cmorizers/obs/download_scripts/download_era-interim.py
+++ esmvaltool/cmorizers/obs/download_scripts/download_era-interim.py
@@ -21,7 +21,8 @@ cmorize_obs -c config-user.yml
 ```
 ''')
 parser.add_argument('--config_file', '-c',
-                    default=os.path.join(os.path.dirname(__file__), 'config-user.yml'),
+                    default=os.path.join(os.path.dirname(__file__),
+                                         'config-user.yml'),
                     help='Config file')
 parser.add_argument('--start_year', type=int, default=1979, help='Start year')
 parser.add_argument('--end_year', type=int, default=2019, help='End year')
@@ -34,7 +35,8 @@ config_file = os.path.abspath(
 with open(config_file, 'r') as f:
     config = yaml.safe_load(f)
 
-rawobs_dir = os.path.abspath(os.path.expandvars(os.path.expanduser(config['rootpath']['RAWOBS'])))
+rawobs_dir = os.path.abspath(
+    os.path.expandvars(os.path.expanduser(config['rootpath']['RAWOBS'])))
 erainterim_dir = f'{rawobs_dir}/Tier3/ERA-Interim'
 os.makedirs(erainterim_dir, exist_ok=True)
 
@@ -60,20 +62,21 @@ day_timesteps = {
 }
 
 day_params = [
-    ('165.128', 'u10', 'an'),  # 10 metre U wind component
-    ('166.128', 'v10', 'an'),  # 10 metre V wind component
-    ('167.128', 't2m', 'an'),  # 2 metre temperature
-    ('168.128', 'd2m', 'an'),  # 2 metre dewpoint temperature
-    ('169.128', 'ssrd', 'accu'),  # Surface solar radiation downwards
-    ('201.128', 'mx2t', 'fc'),  # Maximum temperature at 2 metres since previous post-processing
-    ('202.128', 'mn2t', 'fc'),  # Minimum temperature at 2 metres since previous post-processing
-    ('228.128', 'tp', 'accu'),  # Total precipitation
-    ('151.128', 'msl', 'an'),  # Mean sea level pressure
-    ('182.128', 'e', 'accu'),  # Evaporation
+    ('165.128', 'u10', 'an'),  #10 metre U wind component
+    ('166.128', 'v10', 'an'),  #10 metre V wind component
+    ('167.128', 't2m', 'an'),  #2 metre temperature
+    ('168.128', 'd2m', 'an'),  #2 metre dewpoint temperature
+    ('169.128', 'ssrd', 'accu'),  #Surface solar radiation downwards
+    ('201.128', 'mx2t', 'fc'),  #Max. temp at 2m since previous post-processing
+    ('202.128', 'mn2t', 'fc'),  #Min. temp at 2m since previous post-processing
+    ('228.128', 'tp', 'accu'),  #Total precipitation
+    ('151.128', 'msl', 'an'),  #Mean sea level pressure
+    ('182.128', 'e', 'accu'),  #Evaporation
     # TODO not found in era-interim yet
     # ('', 'pev'),  # Potential evaporation
 ]
 for no, symbol, timestep in day_params:
+    fr = 'daily'
     for year in years:
         server.retrieve({
             'class': 'ei',
@@ -85,7 +88,7 @@ for no, symbol, timestep in day_params:
             'param': no,
             'stream': 'oper',
             'format': 'netcdf',
-            'target': f'{erainterim_dir}/ERA-Interim_{symbol}_daily_{year}.nc',
+            'target': f'{erainterim_dir}/ERA-Interim_{symbol}_{fr}_{year}.nc',
             **day_timesteps[timestep]
         })
 
@@ -109,24 +112,24 @@ month_timesteps = {
 }
 
 month_params = [
-    ('228.128', 'tp', 'accu'),  # Total precipitation
-    ('164.128', 'tcc', 'an'),  # Total cloud cover
-    ('56.162', 'p56.162', 'an'),  # Vertical integral of cloud liquid water
-    ('57.162', 'p57.162', 'an'),  # Vertical integral of cloud frozen water
-    ('137.128', 'tcwv', 'an'),  # Total column water vapour
-    ('134.128', 'sp', 'an'),  # Surface pressure
-    ('151.128', 'msl', 'an'),  # Mean sea level pressure
-    ('167.128', 't2m', 'an'),  # 2 metre temperature
-    ('229.128', 'iews', 'fc'),  # Instantaneous eastward turbulent surface stress
-    ('230.128', 'inss', 'fc'),  # Instantaneous northward turbulent surface stress
-    ('34.128', 'sst', 'an'),  # Sea surface temperature
-    ('235.128', 'skt', 'an'),  # Skin temperature
-    ('176.128', 'ssr', 'accu'),  # Surface net solar radiation
-    ('177.128', 'str', 'accu'),  # Surface net thermal radiation
-    ('147.128', 'slhf', 'accu'),  # Surface latent heat flux
-    ('146.128', 'sshf', 'accu'),  # Surface sensible heat flux
+    ('228.128', 'tp', 'accu'),  #Total precipitation
+    ('164.128', 'tcc', 'an'),  #Total cloud cover
+    ('56.162', 'p56.162', 'an'),  #Vertical integral of cloud liquid water
+    ('57.162', 'p57.162', 'an'),  #Vertical integral of cloud frozen water
+    ('137.128', 'tcwv', 'an'),  #Total column water vapour
+    ('134.128', 'sp', 'an'),  #Surface pressure
+    ('151.128', 'msl', 'an'),  #Mean sea level pressure
+    ('167.128', 't2m', 'an'),  #2 metre temperature
+    ('229.128', 'iews', 'fc'),  #Inst. eastward turbulent surface stress
+    ('230.128', 'inss', 'fc'),  #Inst. northward turbulent surface stress
+    ('34.128', 'sst', 'an'),  #Sea surface temperature
+    ('235.128', 'skt', 'an'),  #Skin temperature
+    ('176.128', 'ssr', 'accu'),  #Surface net solar radiation
+    ('177.128', 'str', 'accu'),  #Surface net thermal radiation
+    ('147.128', 'slhf', 'accu'),  #Surface latent heat flux
+    ('146.128', 'sshf', 'accu'),  #Surface sensible heat flux
     # TODO not found in era-interim yet
-    # ('157.128', 'r', ''),  # Relative humidity
+    # ('157.128', 'r', ''),  #Relative humidity
     # ('130.128', 't', ''),  #
     # ('131.128', 'u', ''),  #
     # ('132.128', 'v', ''),  #
@@ -135,17 +138,18 @@ month_params = [
 ]
 
 for no, symbol, timestep in month_params:
+    fr = 'monthly'
     for year in years:
         server.retrieve({
             'class': 'ei',
             'dataset': 'interim',
-            # All months of a year eg. 19900101/19900201/19900301/19900401/19900501/19900601/19900701/19900801/19900901/19901001/19901101/19901201
+            #All months of a year eg. 19900101/19900201/.../19901101/19901201
             'date': '/'.join([f'{year}{m:02}01' for m in range(1, 13)]),
             'expver': '1',
             'grid': '0.75/0.75',
             'param': no,
             'format': 'netcdf',
-            'target': f'{erainterim_dir}/ERA-Interim_{symbol}_monthly_{year}.nc',
+            'target': f'{erainterim_dir}/ERA-Interim_{symbol}_{fr}_{year}.nc',
             **month_timesteps[timestep]
         })
 
