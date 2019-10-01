eb95f416c141ee45a258c716a67ba82b4411bc6c
Jaro Camphuijsen <jjecamphuijsen@gmail.com>
2019-10-01 11:17:30 +0200

    fix some codacy issues

--- esmvaltool/cmorizers/obs/download_scripts/download_era_interim.py
+++ esmvaltool/cmorizers/obs/download_scripts/download_era_interim.py
@@ -9,8 +9,8 @@ For this, run pip install ecmwf-api-client
 3. Follow the instruction at:
 https://confluence.ecmwf.int/display/WEBAPI/Accessing+ECMWF+data+servers+in+batch
 
-4. Copy/paste the text in https://api.ecmwf.int/v1/key/ into a blank text file and
-save it as $HOME/.ecmwfapirc
+4. Copy/paste the text in https://api.ecmwf.int/v1/key/ into a blank text file
+and save it as $HOME/.ecmwfapirc
 
 5. Use ESMValCore/esmvalcore/config-user.yml as an template
 and set the rootpath of the output directory in RAWOBS
@@ -22,7 +22,8 @@ https://apps.ecmwf.int/codes/grib/param-db
 https://apps.ecmwf.int/datasets/data/interim-full-invariant
 
 ```bash
-python download_era-interim.py -c config-user.yml --start_year 2000 --end_year 2000
+python download_era-interim.py -c config-user.yml --start_year 2000
+--end_year 2000
 ```
 
 """
@@ -50,7 +51,7 @@ with open(config_file, 'r') as f:
 
 rawobs_dir = os.path.abspath(
     os.path.expandvars(os.path.expanduser(config['rootpath']['RAWOBS'])))
-erainterim_dir = f'{rawobs_dir}/Tier3/ERA-Interim'
+era_interim_dir = f'{rawobs_dir}/Tier3/ERA-Interim'
 os.makedirs(erainterim_dir, exist_ok=True)
 
 years = range(args.start_year, args.end_year + 1)
@@ -80,29 +81,29 @@ day_timesteps = {
         'type': 'an',
         'time': '00:00:00/06:00:00/12:00:00/18:00:00',
         'step': '0',
-        'levelist': '10/50/100/250/500/700/850/1000', #CMIP6 day table, plev8
+        'levelist': '10/50/100/250/500/700/850/1000',  # CMIP6 day table, plev8
         'levtype': 'pl',
     }
 }
 
 day_params = [
-    ('167.128', 't2m', 'an'),  #2 metre temperature
-    ('228.128', 'tp', 'accu'),  #Total precipitation
-    ('182.128', 'e', 'accu'),  #Evaporation
-    ('201.128', 'mx2t', 'fc'),  #Max. temp at 2m since previous post-processing
-    ('202.128', 'mn2t', 'fc'),  #Min. temp at 2m since previous post-processing
-    ('235.128', 'skt', 'an'),  #Skin temperature
-    ('165.128', 'u10', 'an'),  #10 metre U wind component
-    ('166.128', 'v10', 'an'),  #10 metre V wind component
-    ('168.128', 'd2m', 'an'),  #2 metre dewpoint temperature
-    ('151.128', 'msl', 'an'),  #Mean sea level pressure
-    ('144.128', 'sf', 'accu'),  #Snowfall
-    ('176.128', 'ssr', 'accu'),  #Surface net solar radiation
-    ('169.128', 'ssrd', 'accu'),  #Surface solar radiation downwards
-    ('205.128', 'ro', 'accu'),  #Runoff
-    ('238.128', 'tsn', 'an'),  #Temperature of snow layer
-    ('212.128', 'tisr', 'accu'),  #TOA incident solar radiation
-    ('164.128', 'tcc', 'an'),  #Total cloud cover
+    ('167.128', 't2m', 'an'),  # 2 metre temperature
+    ('228.128', 'tp', 'accu'),  # Total precipitation
+    ('182.128', 'e', 'accu'),  # Evaporation
+    ('201.128', 'mx2t', 'fc'),  # Max. temp at 2m since previous post-proc
+    ('202.128', 'mn2t', 'fc'),  # Min. temp at 2m since previous post-proc
+    ('235.128', 'skt', 'an'),  # Skin temperature
+    ('165.128', 'u10', 'an'),  # 10 metre U wind component
+    ('166.128', 'v10', 'an'),  # 10 metre V wind component
+    ('168.128', 'd2m', 'an'),  # 2 metre dewpoint temperature
+    ('151.128', 'msl', 'an'),  # Mean sea level pressure
+    ('144.128', 'sf', 'accu'),  # Snowfall
+    ('176.128', 'ssr', 'accu'),  # Surface net solar radiation
+    ('169.128', 'ssrd', 'accu'),  # Surface solar radiation downwards
+    ('205.128', 'ro', 'accu'),  # Runoff
+    ('238.128', 'tsn', 'an'),  # Temperature of snow layer
+    ('212.128', 'tisr', 'accu'),  # TOA incident solar radiation
+    ('164.128', 'tcc', 'an'),  # Total cloud cover
     ('129.128', 'z', '3d'),  # Geopotential
 ]
 
@@ -121,7 +122,7 @@ for no, symbol, timestep in day_params:
             'format': 'netcdf',
             'target': f'{erainterim_dir}/ERA-Interim_{symbol}_{fr}_{year}.nc',
             **day_timesteps[timestep]
-            })
+        })
 
 month_timesteps = {
     'accu': {
@@ -144,45 +145,45 @@ month_timesteps = {
         'levtype': 'pl',
         'stream': 'moda',
         'type': 'an',
-        'levelist': '1/5/10/20/30/50/70/100/150/200/' + \
-                    '250/300/400/500/600/700/850' + \
-                    '/925/1000' #CMIP6 Amon table, plev19
+        'levelist': '1/5/10/20/30/50/70/100/150/200/' +
+                    '250/300/400/500/600/700/850' +
+                    '/925/1000'  # CMIP6 Amon table, plev19
     }
 }
 
 month_params = [
-    ('167.128', 't2m', 'an'),  #2 metre temperature
-    ('228.128', 'tp', 'accu'),  #Total precipitation
-    ('182.128', 'e', 'accu'),  #Evaporation
-    ('235.128', 'skt', 'an'),  #Skin temperature
-    ('165.128', 'u10', 'an'),  #10 metre U wind component
-    ('166.128', 'v10', 'an'),  #10 metre V wind component
-    ('168.128', 'd2m', 'an'),  #2 metre dewpoint temperature
-    ('151.128', 'msl', 'an'),  #Mean sea level pressure
-    ('144.128', 'sf', 'accu'),  #Snowfall
-    ('176.128', 'ssr', 'accu'),  #Surface net solar radiation
-    ('169.128', 'ssrd', 'accu'),  #Surface solar radiation downwards
-    ('205.128', 'ro', 'accu'),  #Runoff
-    ('238.128', 'tsn', 'an'),  #Temperature of snow layer
-    ('212.128', 'tisr', 'accu'),  #TOA incident solar radiation
-    ('164.128', 'tcc', 'an'),  #Total cloud cover
-    ('56.162', 'p56.162', 'an'),  #Vertical integral of cloud liquid water
-    ('57.162', 'p57.162', 'an'),  #Vertical integral of cloud frozen water
-    ('137.128', 'tcwv', 'an'),  #Total column water vapour
-    ('134.128', 'sp', 'an'),  #Surface pressure
-    ('151.128', 'msl', 'an'),  #Mean sea level pressure
-    ('229.128', 'iews', 'fc'),  #Inst. eastward turbulent surface stress
-    ('230.128', 'inss', 'fc'),  #Inst. northward turbulent surface stress
-    ('34.128', 'sst', 'an'),  #Sea surface temperature
-    ('177.128', 'str', 'accu'),  #Surface net thermal radiation
-    ('147.128', 'slhf', 'accu'),  #Surface latent heat flux
-    ('146.128', 'sshf', 'accu'),  #Surface sensible heat flux
-    ('157.128', 'r', '3d'),  #Relative humidity
-    ('130.128', 't', '3d'),  #Temperature
-    ('131.128', 'u', '3d'),  #U component of wind
-    ('132.128', 'v', '3d'),  #V component of wind
-    ('135.128', 'w', '3d'),  #Vertical velocity
-    ('133.128', 'q', '3d'),  #Specific humidity
+    ('167.128', 't2m', 'an'),  # 2 metre temperature
+    ('228.128', 'tp', 'accu'),  # Total precipitation
+    ('182.128', 'e', 'accu'),  # Evaporation
+    ('235.128', 'skt', 'an'),  # Skin temperature
+    ('165.128', 'u10', 'an'),  # 10 metre U wind component
+    ('166.128', 'v10', 'an'),  # 10 metre V wind component
+    ('168.128', 'd2m', 'an'),  # 2 metre dewpoint temperature
+    ('151.128', 'msl', 'an'),  # Mean sea level pressure
+    ('144.128', 'sf', 'accu'),  # Snowfall
+    ('176.128', 'ssr', 'accu'),  # Surface net solar radiation
+    ('169.128', 'ssrd', 'accu'),  # Surface solar radiation downwards
+    ('205.128', 'ro', 'accu'),  # Runoff
+    ('238.128', 'tsn', 'an'),  # Temperature of snow layer
+    ('212.128', 'tisr', 'accu'),  # TOA incident solar radiation
+    ('164.128', 'tcc', 'an'),  # Total cloud cover
+    ('56.162', 'p56.162', 'an'),  # Vertical integral of cloud liquid water
+    ('57.162', 'p57.162', 'an'),  # Vertical integral of cloud frozen water
+    ('137.128', 'tcwv', 'an'),  # Total column water vapour
+    ('134.128', 'sp', 'an'),  # Surface pressure
+    ('151.128', 'msl', 'an'),  # Mean sea level pressure
+    ('229.128', 'iews', 'fc'),  # Inst. eastward turbulent surface stress
+    ('230.128', 'inss', 'fc'),  # Inst. northward turbulent surface stress
+    ('34.128', 'sst', 'an'),  # Sea surface temperature
+    ('177.128', 'str', 'accu'),  # Surface net thermal radiation
+    ('147.128', 'slhf', 'accu'),  # Surface latent heat flux
+    ('146.128', 'sshf', 'accu'),  # Surface sensible heat flux
+    ('157.128', 'r', '3d'),  # Relative humidity
+    ('130.128', 't', '3d'),  # Temperature
+    ('131.128', 'u', '3d'),  # U component of wind
+    ('132.128', 'v', '3d'),  # V component of wind
+    ('135.128', 'w', '3d'),  # Vertical velocity
+    ('133.128', 'q', '3d'),  # Specific humidity
     ('129.128', 'z', '3d'),  # Geopotential
 ]
 
@@ -193,7 +194,7 @@ for no, symbol, timestep in month_params:
         server.retrieve({
             'class': 'ei',
             'dataset': 'interim',
-            #All months of a year eg. 19900101/19900201/.../19901101/19901201
+            # All months of a year eg. 19900101/19900201/.../19901101/19901201
             'date': '/'.join([f'{year}{m:02}01' for m in range(1, 13)]),
             'expver': '1',
             'grid': '0.75/0.75',
