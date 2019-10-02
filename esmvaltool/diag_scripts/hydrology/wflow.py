e9d77e8058824c1b4428e2e205456abd85636139
Peter Kalverla <peter.kalverla@gmx.com>
2019-10-02 14:16:58 +0200

    Create draft recipe and diagnostic for WFlow

---
+++ esmvaltool/diag_scripts/hydrology/wflow.py
@@ -0,0 +1,68 @@
+"""wflow diagnostic."""
+import logging
+from pathlib import Path
+
+# import dask.array as da
+import iris
+
+from esmvaltool.diag_scripts.shared import (ProvenanceLogger,
+                                            get_diagnostic_filename,
+                                            group_metadata, run_diagnostic)
+
+logger = logging.getLogger(Path(__file__).name)
+
+
+def get_provenance_record(ancestor_file):
+    """Create a provenance record."""
+    record = {
+        'caption': "Forcings for the wflow hydrological model.",
+        'domains': ['global'],
+        'authors': [
+            'kalverla_peter',
+            'camphuijsen_jaro',
+        ],
+        'projects': [
+            'ewatercycle',
+        ],
+        'references': [
+            'acknow_project',
+        ],
+        'ancestors': [ancestor_file],
+    }
+    return record
+
+
+def main(cfg):
+    """Process data for use as input to the wflow hydrological model """
+    input_data = cfg['input_data'].values()
+    logger.info(input_data)
+    grouped_input_data = group_metadata(input_data,
+                                        'standard_name',
+                                        sort='dataset')
+
+    # for now just open and save the input/output
+    for standard_name in grouped_input_data:
+        logger.info("Processing variable %s", standard_name)
+        for attributes in grouped_input_data[standard_name]:
+            logger.info("Processing dataset %s", attributes['dataset'])
+            input_file = attributes['filename']
+            cube = iris.load_cube(input_file)
+
+            # Do stuff
+            # ...
+
+            # Save data
+            output_file = get_diagnostic_filename(
+                Path(input_file).stem + '_wflow', cfg)
+            iris.save(cube, output_file, fill_value=1.e20)
+
+            # Store provenance
+            provenance_record = get_provenance_record(input_file)
+            with ProvenanceLogger(cfg) as provenance_logger:
+                provenance_logger.log(output_file, provenance_record)
+
+
+if __name__ == '__main__':
+
+    with run_diagnostic() as config:
+        main(config)
