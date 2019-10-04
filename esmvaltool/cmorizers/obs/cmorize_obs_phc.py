b4951d5952a300221aae9fcd9ea14ff493cbe5cf
Nikolay Koldunov <koldunovn@gmail.com>
2019-10-04 21:34:17 +0200

    codacy

--- esmvaltool/cmorizers/obs/cmorize_obs_phc.py
+++ esmvaltool/cmorizers/obs/cmorize_obs_phc.py
@@ -47,7 +47,7 @@ def _fix_fx_areacello(xr_time, var):
                                  coords={
                                      'lat': xr_time.temp.coords['lat'],
                                      'lon': xr_time.temp.coords['lon']
-                                },
+                                 },
                                  dims=['lat', 'lon'],
                                  name=var)
     grid_areas_xr.attrs = OrderedDict([('cell_area', 'Ocean Grid-Cell Area'),
@@ -96,7 +96,7 @@ def extract_variable(var_info, raw_info, out_dir, attrs):
     xr_time = xr_time.assign_coords(time=[1])
     xr_time.time.attrs = OrderedDict([('standard_name', 'time'),
                                       ('units', 'days since 1950-1-1 00:00:00')
-                                     ])
+                                    ])
 
     cube = _fix_data(xr_time, var)
     fix_var_metadata(cube, var_info)
