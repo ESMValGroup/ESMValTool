760da7747f4b3708d17c9f10d3917d49e095f669
Bas Crezee <bascrezee@gmail.com>
2019-10-11 10:10:00 +0200

    minor change

--- esmvaltool/diag_scripts/mpqb/mpqb_auxmaps.py
+++ esmvaltool/diag_scripts/mpqb/mpqb_auxmaps.py
@@ -78,6 +78,7 @@ def main(cfg):
         logger.info("Opening dataset: {0}".format(dataset))
         # Opening the pair 
         cube = iris.load_cube(dataset_cfg[0]['filename'])
+        import IPython;IPython.embed()
         
         for metricname in metrics_to_calculate:
             try:
