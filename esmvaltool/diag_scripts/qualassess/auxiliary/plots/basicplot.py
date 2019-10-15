bfc9e1a48260121b048c8e0c4253d6d5c06f0b98
Benjamin Müller <b.mueller@iggf.geo.uni-muenchen.de>
2019-10-15 13:37:42 +0200

    Update basicplot.py

--- esmvaltool/diag_scripts/qualassess/auxiliary/plots/basicplot.py
+++ esmvaltool/diag_scripts/qualassess/auxiliary/plots/basicplot.py
@@ -977,7 +977,7 @@ class Plot2D(object):
                 fraction=1.,
                 extend=ext_cmap,
                 boundaries=levels)
-            cbar.ax.set_xticklabels([('{:.'+str(2)+'g}').format(float(x)) for x in cbar.ax.get_xticklabels()])
+            cbar.ax.set_xticklabels([('{:.'+str(2)+'g}').format(float(x.get_text())) for x in cbar.ax.get_xticklabels()])
         cax.set_xlabel((list(set(self.names))[0] if list(
             set(self.names))[0] else "") + list(set(self.units))[0])
 
