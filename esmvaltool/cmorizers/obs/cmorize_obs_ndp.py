23e6fbaafb776bf123accd296d0af3d4bfa41633
Manuel Schlund <manuel.schlund@dlr.de>
2019-10-15 13:21:59 +0200

    Finished NDP CMORizer

--- esmvaltool/cmorizers/obs/cmorize_obs_ndp.py
+++ esmvaltool/cmorizers/obs/cmorize_obs_ndp.py
@@ -16,28 +16,21 @@ Download and processing instructions
 
 """
 
-import gdal
-import glob
 import gzip
 import logging
 import os
 import shutil
 import tarfile
-import zipfile
-from datetime import datetime
-from PIL import Image
 
+import gdal
 import iris
 import iris.coord_categorisation
 import numpy as np
 from cf_units import Unit
 
-from esmvalcore.preprocessor import regrid
-
 from . import utilities as utils
 
 logger = logging.getLogger(__name__)
-
 
 
 def _clean(file_dir):
@@ -51,122 +44,44 @@ def _extract_variable(cmor_info, attrs, var_file, out_dir, cfg):
     """Extract variable."""
     grid_file = gdal.Open(var_file)
     array = grid_file.ReadAsArray()
-    print(np.min(array))
-    print(np.max(array))
-    print(np.mean(array))
-    print(array)
-    assert False
-
-    driver = ogr.GetDriverByName('E00GRID')
-    grid_file = driver.Open(var_file)
-    print(grid_file)
-    assert False
-    nc_files = []
-    for year in _get_years(in_dir, cfg):
-        cube_path = _get_cube_for_year(year, in_dir, cfg)
-        nc_files.append(cube_path)
-
-    # Build final cube
-    logger.info("Building final cube")
-    cubes = iris.cube.CubeList()
-    for nc_file in nc_files:
-        cube = iris.load_cube(nc_file)
-        cubes.append(cube)
-    final_cube = cubes.concatenate_cube()
-    utils.fix_var_metadata(final_cube, cmor_info)
-    utils.convert_timeunits(final_cube, 1950)
-    utils.fix_coords(final_cube)
-    if not cfg.get('regrid'):
-        utils.flip_dim_coord(final_cube, 'latitude')
-    utils.set_global_atts(final_cube, attrs)
-    utils.save_variable(final_cube,
+    for missing_value in cfg['missing_values']:
+        array = np.ma.masked_equal(array, missing_value)
+    array = array.astype(np.float)
+    np.ma.set_fill_value(array, 1e20)
+    array = np.ma.expand_dims(array, 0)
+    time = iris.coords.DimCoord([183.0],
+                                bounds=[0.0, 366.0],
+                                units=Unit('days since 2000-01-01 00:00:00'),
+                                standard_name='time',
+                                var_name='time',
+                                long_name='time')
+    lats = iris.coords.DimCoord(
+        90.0 - np.arange(array.shape[1]) * cfg['delta_degrees'],
+        standard_name='latitude',
+        var_name='lat',
+        long_name='latitude')
+    lons = iris.coords.DimCoord(
+        180.0 + np.arange(array.shape[2]) * cfg['delta_degrees'],
+        standard_name='longitude',
+        var_name='lon',
+        long_name='longitude')
+    cube = iris.cube.Cube(array,
+                          dim_coords_and_dims=[(time, 0), (lats, 1),
+                                               (lons, 2)],
+                          units=Unit('t ha-1'))
+    cube.convert_units('kg m-2')
+    utils.fix_var_metadata(cube, cmor_info)
+    utils.convert_timeunits(cube, 1950)
+    utils.fix_coords(cube)
+    utils.set_global_atts(cube, attrs)
+    utils.save_variable(cube,
                         cmor_info.short_name,
                         out_dir,
                         attrs,
                         unlimited_dimensions=['time'])
 
 
-def _get_coords(year, filename, cfg):
-    """Get correct coordinates for cube."""
-    filename = os.path.basename(filename)
-    time_units = Unit('days since 1950-1-1 00:00:00', calendar='standard')
-
-    # Extract date from filename
-    time_str = filename.replace(cfg['binary_prefix'], '')
-    month = MONTHS[time_str[4:7]]
-    day = DAYS[time_str[7:8]]
-    date = datetime(year, month, day)
-
-    # Build time coordinate
-    time_data = [time_units.date2num(date)]
-    time_coord = iris.coords.DimCoord(time_data,
-                                      standard_name='time',
-                                      long_name='time',
-                                      var_name='time',
-                                      units=time_units)
-
-    # Build latitude/Longitude coordinates
-    latitude_data = np.linspace(UPPER_LEFT_LAT, LOWER_RIGHT_LAT, N_LAT)
-    longitude_data = np.linspace(UPPER_LEFT_LON, LOWER_RIGHT_LON, N_LON)
-    lat_coord = iris.coords.DimCoord(latitude_data,
-                                     standard_name='latitude',
-                                     long_name='latitude',
-                                     var_name='lat',
-                                     units='degrees')
-    lon_coord = iris.coords.DimCoord(longitude_data,
-                                     standard_name='longitude',
-                                     long_name='longitude',
-                                     var_name='lon',
-                                     units='degrees')
-
-    return [(time_coord, 0), (lat_coord, 1), (lon_coord, 2)]
-
-
-def _get_cube_for_year(year, in_dir, cfg):
-    """Exract cube containing one year from raw file."""
-    logger.info("Processing year %i", year)
-    bin_files = glob.glob(
-        os.path.join(in_dir, f"{cfg['binary_prefix']}{year}*.bin"))
-
-    # Read files of one year
-    cubes = iris.cube.CubeList()
-    for bin_file in bin_files:
-        raw_data = np.fromfile(bin_file, DTYPE,
-                               N_LAT * N_LON).reshape(1, N_LAT, N_LON)
-        raw_data = np.ma.masked_equal(raw_data, MISSING_VALUE)
-        raw_data = raw_data.astype(np.float32)
-        raw_data /= SCALE_FACTOR
-
-        # Build coordinates and cube, regrid, and append it
-        coords = _get_coords(year, bin_file, cfg)
-        cube = iris.cube.Cube(raw_data, dim_coords_and_dims=coords)
-        if cfg.get('regrid'):
-            cube = regrid(cube, cfg['regrid']['target_grid'],
-                          cfg['regrid']['scheme'])
-        cubes.append(cube)
-
-    # Build cube for single year with monthly data
-    # (Raw data has two values per month)
-    cube = cubes.concatenate_cube()
-    iris.coord_categorisation.add_month_number(cube, 'time')
-    cube = cube.aggregated_by('month_number', iris.analysis.MEAN)
-
-    # Cache cube on disk to save memory
-    cached_path = os.path.join(in_dir, f'{year}.nc')
-    iris.save(cube, cached_path)
-    logger.info("Cached %s", cached_path)
-    return cached_path
-
-
-def _get_years(in_dir, cfg):
-    """Get all available years from input directory."""
-    bin_files = os.listdir(in_dir)
-    bin_files = [f.replace(cfg['binary_prefix'], '') for f in bin_files]
-    years = {int(f[:4]) for f in bin_files}
-    return years
-
-
-def _extract(filepath, out_dir):
+def _extract_tar(filepath, out_dir):
     """Extract `*.tar.gz` file."""
     logger.info("Starting extraction of %s to %s", filepath, out_dir)
     with tarfile.open(filepath) as tar:
@@ -194,7 +109,7 @@ def cmorization(in_dir, out_dir, cfg, _):
     filepath = os.path.join(in_dir, cfg['filename'])
     tar_file = os.path.join(in_dir, filepath)
     logger.info("Found input file '%s'", tar_file)
-    file_dir = _extract(tar_file, out_dir)
+    file_dir = _extract_tar(tar_file, out_dir)
 
     # Run the cmorization
     for (var, var_info) in cfg['variables'].items():
