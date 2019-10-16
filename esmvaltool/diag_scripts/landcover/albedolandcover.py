1dba107ecb15c06f9284e1f58282ee4ff06bfff6
Bas Crezee <bascrezee@gmail.com>
2019-10-16 09:39:41 +0200

    added projects

--- esmvaltool/diag_scripts/landcover/albedolandcover.py
+++ esmvaltool/diag_scripts/landcover/albedolandcover.py
@@ -10,6 +10,13 @@ Description
 -----------
     Computes relationship between landcover and albedo for models
     and compares this to observations.
+
+Projects
+--------
+    CMIP5
+    CMIP6 (experimental)
+
+
 """
 
 
