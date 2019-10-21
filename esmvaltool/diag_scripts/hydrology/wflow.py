175bfe6ff64395e19b6796212999e18a5403a662
Peter Kalverla <peter.kalverla@gmx.com>
2019-10-21 14:20:03 +0200

    Implement workaround for iris issue and restructure diag_script

--- esmvaltool/diag_scripts/hydrology/wflow.py
+++ esmvaltool/diag_scripts/hydrology/wflow.py
@@ -35,6 +35,46 @@ def get_provenance_record(ancestor_file):
     }
     return record
 
+def geopotential_to_height(geopotential):
+    """ Convert geopotential to geopotential height """
+    gravity = iris.coords.AuxCoord(9.80665,
+        long_name='Acceleration due to gravity',
+        units='m s-2')
+    return geopotential/gravity
+
+def lapse_rate_correction(height):
+    """ Temperature correction over a given height interval """
+    gamma = iris.coords.AuxCoord(0.0065,
+        long_name='Environmental lapse rate',
+        units='K/m')
+    return height*gamma
+
+def regrid_temperature(src_temp, src_height, target_height):
+    """ Convert temperature to target grid with lapse rate correction """
+    src_dtemp = lapse_rate_correction(src_height)
+
+    # src_slt = src_temp + dtemp
+    # ValueError: This operation cannot be performed as there are differing
+    # coordinates (time) remaining which cannot be ignored.
+    # Related (?): https://github.com/SciTools/iris/issues/2765
+
+    # Workaround: overwrite data in compatible cube
+    src_dtemp_compat = src_temp.collapsed('time', iris.analysis.MEAN)
+    src_dtemp_compat.data = src_height.data.copy()
+
+    # Convert 2m temperature to sea-level temperature (slt)
+    src_slt = src_temp + src_dtemp_compat
+
+    # Interpolate sea-level temperature to target grid
+    target_slt = preproc.regrid(src_slt, target_grid=target_height, scheme='linear')
+
+    # Convert sea-level temperature to new target elevation
+    target_dtemp = lapse_rate_correction(target_height)
+    target_dtemp_compat = target_slt.collapsed('time', iris.analysis.MEAN)
+    target_dtemp_compat.data = target_dtemp.data.copy()
+
+    target_temp = target_slt - target_dtemp_compat
+    return target_temp
 
 def main(cfg):
     """Process data for use as input to the wflow hydrological model """
@@ -71,54 +111,29 @@ def main(cfg):
 
     # Interpolating precipitation to the target grid
     # Read the target cube, which contains target grid and target elevation
-    dem_path = os.path.join(cfg['auxiliary_data_dir'],cfg['dem_file'])
-    dem_cube = iris.load_cube(dem_path)
+    dem_path = os.path.join(cfg['auxiliary_data_dir'], cfg['dem_file'])
+    dem = iris.load_cube(dem_path)
 
     # Read source orography (add era5 later) and try to make it cmor compatible
-    era_orography_path = os.path.join(
-        cfg['auxiliary_data_dir'], cfg['source_orography'])
-    era_orography_cube = iris.load_cube(era_orography_path)
-    oro.coord('longitude').long_name="Longitude"
-    oro.coord('latitude').long_name="Latitude"
+    era_orography_path = os.path.join(cfg['auxiliary_data_dir'], cfg['source_orography'])
+    oro = iris.load_cube(era_orography_path)
+    # oro.coord('longitude').long_name="Longitude"
+    # oro.coord('latitude').long_name="Latitude"
 
     ## Processing precipitation
     # Interpolate precipitation to the target grid and save new output
     pr = all_vars['precipitation_flux']
-    pr_dem = preproc.regrid(pr, target_grid=dem_cube, scheme='linear')
+    pr_dem = preproc.regrid(pr, target_grid=dem, scheme='linear')
     iris.save(pr_dem, output_file, fill_value=1.e20) #TODO: change filename
 
     ## Processing temperature
     tas = all_vars['air_temperature']
-
-    # Convert geopotential to surface elevation
-    g = iris.coords.AuxCoord(9.80665,
-                             long_name='Acceleration due to gravity',
-                             units='m s-2')
-    era_height = era_orography_cube / g
-
-    # Convert surface elevation to sea-level temperature correction
-    gamma = iris.coords.AuxCoord(0.0065,
-                                 long_name='Environmental lapse rate',
-                                 units='K/m')
-
-    # Convert surface (2m) temperature to sea-level temperature
-    t_sea_level = tas + era_height*gamma
-    #BUG this doesn't work:
-    # ValueError: This operation cannot be performed as there are differing coordinates (time, longitude, latitude) remaining which cannot be ignored.
-    # Related (?): https://github.com/SciTools/iris/issues/2765
-
-    # Interpolate sea-level temperature to target grid
-    tsl_target_grid = preproc.regrid(t_sea_level, target_grid=dem_cube, scheme='linear')
-
-    # Convert sea-level temperature to new target elevation
-    t_target_elevation = tsl_target_grid - dem_cube*gamma
+    tas_dem = regrid_temperature(tas, oro, dem)
 
     # Save output
-    iris.save(t_target_elevation, output_file, fill_value=1.e20)
+    iris.save(tas_dem, output_file, fill_value=1.e20)
 
     # TODO
-    # - Solve iris issue to make lapse rate temperature correction work
-    # - Move variable manipulation to separate functions
     # - Add function(s) to calculate potential evapotranspiration
     # - Check whether the correct units are used
     # - See whether we can work with wflow pcraster .map files directly
@@ -126,6 +141,8 @@ def main(cfg):
     # - Compare output to prepared input during workshop
     # - Save the output files with correct variable- and file-names
     #   Output format: wflow_local_forcing_ERA5_Meuse_1990_2018.nc
+
+
 
 if __name__ == '__main__':
     with run_diagnostic() as config:
