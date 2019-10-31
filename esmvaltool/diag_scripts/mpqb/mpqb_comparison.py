84b0b062f2d857c119cd92c03924ee1992f285ea
bascrezee <bascrezee@gmail.com>
2019-10-31 15:33:30 +0100

    add taylordiagram to recipe

--- esmvaltool/diag_scripts/mpqb/mpqb_comparison.py
+++ esmvaltool/diag_scripts/mpqb/mpqb_comparison.py
@@ -130,9 +130,13 @@ def compute_diagnostic(filename):
 
 def main():
     """MPQB diagnostics."""
+    # read recipe config
+    with open(os.path.join(os.path.split(__file__)[0], 'recipe_cfg.yml')) as handle:
+        recipe_cfg = yaml.safe_load(handle)
+        reference_dataset = recipe_cfg['reference_dataset']
+
     # The metrics to be calculated.
     metrics_to_calculate = ['pearsonr', 'rmsd', 'absdiff', 'reldiff', 'ubrmsd']
-    reference_dataset = 'cds-era5-land-monthly'
 
     # Get a description of the preprocessed data that we will use as input.
     input_data = cfg['input_data'].values()
--- esmvaltool/diag_scripts/mpqb/taylordiagram.py
+++ esmvaltool/diag_scripts/mpqb/taylordiagram.py
@@ -3,6 +3,7 @@ import os
 import warnings
 from pprint import pformat
 
+import yaml
 import skill_metrics
 import matplotlib.pyplot as plt
 import iris
@@ -22,8 +23,10 @@ logger = logging.getLogger(os.path.basename(__file__))
 
 
 def main(cfg):
-    #TODO: read from cfg
-    reference_dataset = 'cds-era5-land-monthly'
+    # read recipe config
+    with open(os.path.join(os.path.split(__file__)[0],'recipe_cfg.yml')) as handle:
+        recipe_cfg = yaml.safe_load(handle)
+        reference_dataset = recipe_cfg['reference_dataset']
 
     # Read all datasets that are provided.
     # Get a description of the preprocessed data that we will use as input.
