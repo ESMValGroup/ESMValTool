fd719588d303d7580e2ef8806bd8f12fc2fdab4b
jhardenberg <hardenberg@gmail.com>
2019-11-04 22:55:05 +0100

    using main function and updated references

--- esmvaltool/diag_scripts/examples/diagnostic.jl
+++ esmvaltool/diag_scripts/examples/diagnostic.jl
@@ -14,8 +14,8 @@ import JSON
 
 function provenance_record(infile)
     xprov = Dict("ancestors" => infile,
-                 "authors" => ["hard_jo", "arno_en"],
-                 "references" => ["zhang-2011"],
+                 "authors" => ["vonhardenberg_jost", "arnone_enrico"],
+                 "references" => ["zhang11wcc"],
                  "projects" => ["crescendo", "c3s-magic"],
                  "caption" => "Example diagnostic in Julia",
                  "statistics" => ["other"],
@@ -56,37 +56,39 @@ end
 # This is useful to avoid the "global scope debacle"
 # https://github.com/JuliaLang/julia/issues/28789
 # i.e. the following scope is local, as if we were in a function, not a script
-let
+function main(settings)
 
-diag_scripts_dir = ENV["diag_scripts"]
+    diag_scripts_dir = ENV["diag_scripts"]
+
+    metadata = YAML.load_file(settings["input_files"][1])
+    climofiles = collect(keys(metadata))
+    climolist = metadata[climofiles[1]]
+    varname = climolist["short_name"]
+    diag_base = climolist["diagnostic"]
+
+    println(diag_base, ": starting routine")
+    println(diag_base, ": creating work and plot directories")
+    work_dir = settings["work_dir"]
+    run_dir = settings["run_dir"]
+    mkpath(work_dir)
+    mkpath(run_dir)
+    cd(run_dir)
+
+    # Reading an example parameter from the settings
+    parameter = settings["parameter1"]
+
+    # Compute the main diagnostic
+    provenance = compute_diagnostic(metadata, diag_base,
+                                    parameter, work_dir)
+
+    # setup provenance file
+    provenance_file = string(run_dir, "/diagnostic_provenance.yml")
+
+    # Write provenance file
+    open(provenance_file, "w") do io
+        JSON.print(io, provenance, 4)
+    end
+end
+
 settings = YAML.load_file(ARGS[1])
-
-metadata = YAML.load_file(settings["input_files"][1])
-climofiles = collect(keys(metadata))
-climolist = metadata[climofiles[1]]
-varname = climolist["short_name"]
-diag_base = climolist["diagnostic"]
-
-println(diag_base, ": starting routine")
-println(diag_base, ": creating work and plot directories")
-work_dir = settings["work_dir"]
-run_dir = settings["run_dir"]
-mkpath(work_dir)
-mkpath(run_dir)
-cd(run_dir)
-
-# Reading an example parameter from the settings
-parameter = settings["parameter1"]
-
-# Compute the main diagnostic
-provenance = compute_diagnostic(metadata, diag_base, parameter, work_dir)
-
-# setup provenance file
-provenance_file = string(run_dir, "/diagnostic_provenance.yml")
-
-# Write provenance file
-open(provenance_file, "w") do io
-    JSON.print(io, provenance, 4)
-end
-
-end
+main(settings)
