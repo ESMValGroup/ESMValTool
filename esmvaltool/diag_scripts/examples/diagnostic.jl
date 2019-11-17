734de0e11444e3d9833788a79ff28fc367ab3c62
jhardenberg <hardenberg@gmail.com>
2019-11-17 18:33:46 +0100

    added reading, writing and plotting of a netcdf file

--- esmvaltool/diag_scripts/examples/diagnostic.jl
+++ esmvaltool/diag_scripts/examples/diagnostic.jl
@@ -6,11 +6,23 @@
 # Example of ESMValTool diagnostic written in Julia
 #
 # Modification history
-#    20190807-hard_jo written for v2.0
+#    20190807-vonhardenberg_jost written for v2.0
+#    20191117-vonhardenberg_jost added more realistic writing of file and plot
 # ############################################################################
 
 import YAML
 import JSON
+using NetCDF
+# Used to write output NetCDF file with original attributes
+using RainFARM
+using Statistics
+
+using PyPlot
+# Avoid plotting to screen
+pygui(false)
+
+# Provides the plotmap() function
+include(joinpath(ENV["diag_scripts"], "shared/external.jl"))
 
 function provenance_record(infile)
     xprov = Dict("ancestors" => infile,
@@ -25,7 +37,8 @@ function provenance_record(infile)
     return(xprov)
 end
 
-function compute_diagnostic(metadata, diag_base, parameter, work_dir)
+function compute_diagnostic(metadata, varname, diag_base, parameter,
+                            work_dir, plot_dir)
     provenance = Dict()
     for (infile, value) in metadata
         dataset = value["dataset"]
@@ -41,24 +54,43 @@ function compute_diagnostic(metadata, diag_base, parameter, work_dir)
         # Call here actual diagnostic
         println(diag_base, ": I am your Julia diagnostic")
 
-        # Fake output file
-        outfile = string(work_dir, "/", "outfile.txt")
-        open(outfile, "w") do io
-            println(io, "diagnostic output")
-        end 
+        # Read the variable, lon and lat
+        var = ncread(infile, varname)
+        lon = ncread(infile, "lon")
+        lat = ncread(infile, "lat")
+
+        units = ncgetatt(infile, varname, "units")
+       
+        # Compute time average and add parameter
+        varm = mean(var, dims = 3) .+ parameter
+
+        # Output filename
+        outfile = string(work_dir, "/", varname, "_", dataset, "_", exp, "_",
+                         ensemble, "_", start_year, "-",
+                         end_year, "_timmean.nc")
+ 
+        # Use the RainFARM function write_netcdf2d to write variable to 
+        # output file copying original attributes from infile
+        write_netcdf2d(outfile, varm, lon, lat, varname, infile)
+
         # Create provenance record for the output file
         xprov = provenance_record(infile)
+
+        # Plot the field
+        plotfile = string(plot_dir, "/", varname, "_", dataset, "_", exp, "_",
+                         ensemble, "_", start_year, "-",
+                         end_year, "_timmean.png")
+        title = string("Mean ", varname, " ", dataset, " ", exp, " ", ensemble,
+                       " ", start_year, "-", end_year)
+        plotmap(lon, lat, var, title = title, proj = "robinson", clabel = units)
+        savefig(plotfile)
+        xprov["plot_file"] = plotfile 
         provenance[outfile] = xprov
     end
     return provenance
 end
 
-# This is useful to avoid the "global scope debacle"
-# https://github.com/JuliaLang/julia/issues/28789
-# i.e. the following scope is local, as if we were in a function, not a script
 function main(settings)
-
-    diag_scripts_dir = ENV["diag_scripts"]
 
     metadata = YAML.load_file(settings["input_files"][1])
     climofiles = collect(keys(metadata))
@@ -69,17 +101,19 @@ function main(settings)
     println(diag_base, ": starting routine")
     println(diag_base, ": creating work and plot directories")
     work_dir = settings["work_dir"]
+    plot_dir = settings["plot_dir"]
     run_dir = settings["run_dir"]
     mkpath(work_dir)
     mkpath(run_dir)
+    mkpath(plot_dir)
     cd(run_dir)
 
     # Reading an example parameter from the settings
     parameter = settings["parameter1"]
 
     # Compute the main diagnostic
-    provenance = compute_diagnostic(metadata, diag_base,
-                                    parameter, work_dir)
+    provenance = compute_diagnostic(metadata, varname, diag_base,
+                                    parameter, work_dir, plot_dir)
 
     # setup provenance file
     provenance_file = string(run_dir, "/diagnostic_provenance.yml")
