6c0b211c5e81ab267d1368287734465e01877d1a
bascrezee <bascrezee@gmail.com>
2019-12-11 09:47:52 +0100

    pass path as argument to script

--- esmvaltool/cmorizers/obs/download_scripts/download_merra2.sh
+++ esmvaltool/cmorizers/obs/download_scripts/download_merra2.sh
@@ -9,19 +9,16 @@
 #  - Setup download with wget: https://disc.gsfc.nasa.gov/data-access#mac_linux_wget
 #  - Change the destination directory specified below
 #  - Check the years
-#  - Run this script
+#  - Run this script as follow ./download_merra2.sh path_to_save_the_files
+#
 # 
 # Additional resources:
 #    - file specification document is available here: https://gmao.gsfc.nasa.gov/pubs/docs/Bosilovich785.pdf)
 
 
-
-# The destination for the files to be downloaded
-destination=/net/exo/landclim/PROJECTS/C3S/datadir/rawobsdir/Tier3/MERRA-2/
-
 # The years that should be downloaded.
 yrs=({1980..2019})
 
 for yr in ${yrs[@]}; do
-    wget -r -nd -nH --load-cookies ~/.urs_cookies --save-cookies ~/.urs_cookies --directory-prefix=${destination} --auth-no-challenge=on --keep-session-cookies -A '*.nc4' https://goldsmr4.gesdisc.eosdis.nasa.gov/data/MERRA2_MONTHLY/M2TMNXLND.5.12.4/${yr}/ -R "*.tmp"
+    wget -r -nd -nH --load-cookies ~/.urs_cookies --save-cookies ~/.urs_cookies --directory-prefix=$1 --auth-no-challenge=on --keep-session-cookies -A '*.nc4' https://goldsmr4.gesdisc.eosdis.nasa.gov/data/MERRA2_MONTHLY/M2TMNXLND.5.12.4/${yr}/ -R "*.tmp"
 done
