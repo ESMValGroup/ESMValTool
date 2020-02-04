1fbdfe4acfd38c551490c5e0e329e8789bca2135
Jerom Aerts <jeromaerts@gmail.com>
2020-02-04 14:33:58 +0100

    Fixed bug in loop and basename

--- esmvaltool/diag_scripts/cmorizers/era5.py
+++ esmvaltool/diag_scripts/cmorizers/era5.py
@@ -10,11 +10,13 @@ from esmvaltool.diag_scripts.shared import (run_diagnostic,
 def main(cfg):
     """Rename preprocessed native6 file."""
     fixed_files = cfg['input_data']
-    for file in fixed_files:
+    
+    for file, dictionary in fixed_files.items():
         basename, _ext = os.path.splitext(os.path.basename(file))
         basename = basename.replace('native', 'OBS')
-        if cfg['diagnostic'] == 'daily':
-            basename.replace('E1hr', 'Eday')
+
+        if dictionary['diagnostic'] == 'daily':
+            basename = basename.replace('E1hr', 'Eday')
         outfile = get_diagnostic_filename(basename, cfg)
         shutil.move(file, outfile)
 
