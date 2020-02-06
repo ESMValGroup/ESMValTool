531b69eaf3292211425d682a7aa7a2387cdc3a5e
Peter Kalverla <peter.kalverla@gmx.com>
2020-02-06 16:48:02 +0100

    Clean up recipe and add diag logger

--- esmvaltool/diag_scripts/cmorizers/era5.py
+++ esmvaltool/diag_scripts/cmorizers/era5.py
@@ -1,23 +1,28 @@
 """native6 diagnostic."""
 
+import logging
 import os
 import shutil
 
-from esmvaltool.diag_scripts.shared import (run_diagnostic,
-                                            get_diagnostic_filename)
+from esmvaltool.diag_scripts.shared import (get_diagnostic_filename,
+                                            run_diagnostic)
+
+logger = logging.getLogger(os.path.basename(__file__))
 
 
 def main(cfg):
     """Rename preprocessed native6 file."""
     fixed_files = cfg['input_data']
-    
+
     for file, dictionary in fixed_files.items():
         basename, _ext = os.path.splitext(os.path.basename(file))
         basename = basename.replace('native', 'OBS')
 
         if dictionary['diagnostic'] == 'daily':
             basename = basename.replace('E1hr', 'Eday')
+
         outfile = get_diagnostic_filename(basename, cfg)
+        logger.info('Moving %s to %s', file, outfile)
         shutil.move(file, outfile)
 
 
