8f74641c552cc4e5531bf2236692f32f93ced9b5
mwjury <martin.w.jury@gmail.com>
2020-03-02 15:00:24 +0100

    Cleaning up code.

--- esmvaltool/cmorizers/obs/cmorize_obs_berkeleyearth.py
+++ esmvaltool/cmorizers/obs/cmorize_obs_berkeleyearth.py
@@ -34,52 +34,27 @@ from . import utilities as utils
 
 logger = logging.getLogger(__name__)
 
-def _extr_var_n_calc_abs_tas(short_name, var, cfg, filepath, out_dir):
-    """Extract variable."""
-    # load tas anomaly and climatology
-    with catch_warnings():
-        filterwarnings(
-            action='ignore',
-            message='Ignoring netCDF variable .* invalid units .*',
-            category=UserWarning,
-            module='iris',
-        )
-        filterwarnings(
-            action='ignore',
-            message='.* not used since it\ncannot be safely cast to variable'\
-                ' data type *',
-            category=UserWarning,
-            module='iris',
-        )
-        cubes = iris.load(filepath)
 
-    # tas anomaly
-    raw_var = var.get('raw', short_name)
-    cube_anom = cubes.extract(utils.var_name_constraint(raw_var))[0]
-
-    # tasclim
-    raw_var_clim = var.get('rawclim', short_name)
-    cube_clim = cubes.extract(utils.var_name_constraint(raw_var_clim))[0]
-
-    #######
-    # the time coordinates are a big mess (given as floats in years A.D.)
-    # best to reinitialize them from scratch
+def reinit_broken_time(cube_anom, cube_clim, climstart, climend):
+    """ the time coordinates are a big mess (given as floats in years A.D.)
+    best to reinitialize them from scratch
+    """
     logger.info("Reinitializing broken time coordinate")
     time_raw = cube_anom.coord('time')
+
     n_years, n_add_mon = len(time_raw.points) // 12, len(time_raw.points) % 12
     start_year = int(time_raw.points[0])
-    n_days = (n_years+n_add_mon/12)*365.25 + 50
-
-    # information on time for the climatology are only present in the long_name
-    climstart, climend = [int(x) for x in re.findall("\d{4}", cube_clim.long_name)]
+    n_days = (n_years + n_add_mon / 12) * 365.25 + 50  # have some extra length
+    climcenter = (climend - climstart) // 2
 
     times = iris.coords.DimCoord(np.arange(int(n_days), dtype=float),
-         var_name='time',
-         standard_name='time',
-         long_name='time',
-         units=cf_units.Unit(
-             'days since {}-01-01 00:00:00'.format(start_year),
-             calendar=cf_units.CALENDAR_STANDARD))
+                                 var_name='time',
+                                 standard_name='time',
+                                 long_name='time',
+                                 units=cf_units.Unit(
+                                     'days since {}-01-01 00:00:00'.format(
+                                         start_year),
+                                     calendar=cf_units.CALENDAR_STANDARD))
 
     # init a dummy cube to enable coord_categorisation
     dummycube = iris.cube.Cube(np.zeros(int(n_days), np.int),
@@ -89,12 +64,12 @@ def _extr_var_n_calc_abs_tas(short_name, var, cfg, filepath, out_dir):
 
     # build timecoord for the anomaly cube
     dummycube = dummycube.aggregated_by(['year', 'month'], iris.analysis.MEAN)
-    dummycube = dummycube[:(n_years*12+n_add_mon)]
+    dummycube = dummycube[:(n_years * 12 + n_add_mon)]
     timecoord_anom = dummycube.coord('time')
 
     # build timecoord for the climatology cube
     dummycube_clim = dummycube.extract(iris.Constraint(year=lambda cell:
-                                                       cell == climstart+15))
+                                       cell == climstart + climcenter))
     timecoord_clim = dummycube_clim.coord('time')
 
     # change to the new time coordinates
@@ -102,9 +77,19 @@ def _extr_var_n_calc_abs_tas(short_name, var, cfg, filepath, out_dir):
     cube_anom.add_dim_coord(timecoord_anom, 0)
     cube_clim.add_dim_coord(timecoord_clim, 0)
 
-    ############
-    ### derive absolute tas values
+    # convert time units to standard
+    utils.convert_timeunits(cube_anom, 1950)
+    utils.convert_timeunits(cube_clim, 1950)
+
+    return (cube_anom, cube_clim)
+
+
+def calc_abs_temperature(cube_anom, cube_clim, short_name):
+    """
+    derive absolute tas values
+    """
     logger.info("Deriving absolute temperature fields")
+
     # prepare cubes
     for cube in [cube_anom, cube_clim]:
         cube.attributes.pop('valid_max')
@@ -117,8 +102,8 @@ def _extr_var_n_calc_abs_tas(short_name, var, cfg, filepath, out_dir):
     units = cube_clim.units
 
     # init data array
-    dim_coords_and_dims = [(cor.copy(), i) for i, cor in enumerate(cube_anom.coords())]
-    shape = [x[0].shape[0] for x in dim_coords_and_dims]
+    crds_n_dims = [(cor.copy(), i) for i, cor in enumerate(cube_anom.coords())]
+    shape = [x[0].shape[0] for x in crds_n_dims]
     array = np.ma.ones(shape, dtype=dtype) * fill_value
     array.mask = True
     array.fill_value = fill_value
@@ -128,7 +113,7 @@ def _extr_var_n_calc_abs_tas(short_name, var, cfg, filepath, out_dir):
         with catch_warnings():
             filterwarnings(
                 action='ignore',
-                message='.* not used since it\ncannot be safely cast to'\
+                message='.* not used since it\ncannot be safely cast to'
                         ' variable data type *',
                 category=UserWarning,
                 module='iris',
@@ -138,77 +123,90 @@ def _extr_var_n_calc_abs_tas(short_name, var, cfg, filepath, out_dir):
     # build absolute tas cube
     cube_abs = iris.cube.Cube(array, var_name=var_name,
                               units=units,
-                              dim_coords_and_dims=dim_coords_and_dims)
+                              dim_coords_and_dims=crds_n_dims)
 
-    ########
-    # fix coordinates
-    logger.info("Fixing coordinates")
-    cmor_info_tas = cfg['cmor_table'].get_variable(var['mip'], short_name)
-    for cube in [cube_abs, cube_anom, cube_clim]:
-        cube.coord('latitude').var_name = 'lat'
-        cube.coord('longitude').var_name = 'lon'
-        utils.fix_coords(cube)
-        if 'height2m' in cmor_info_tas.dimensions:
-            utils.add_height2m(cube)
-
-    # cmorize, fix metadata and save cubes
-    cmor_info_tas = cfg['cmor_table'].get_variable(var['mip'], short_name)
-    attrs_tas = cfg['attributes']
-    attrs_tas['mip'] = var['mip']
-    if 'raw_units' in var:
-        cube_anom.units = var['raw_units']
-        cube_clim.units = var['raw_units']
-        cube_abs.units = var['raw_units']
-    cube_abs.convert_units(cmor_info_tas.units)
-    cube_clim.convert_units(cmor_info_tas.units)
+    return(cube_abs)
 
 
-    # tas absolute
-    logger.info("Saving absolute tas")
-    utils.fix_var_metadata(cube_abs, cmor_info_tas)
-    attrs_tas['comment'] = "Temperature time-series calculated from the anomaly"\
-        " time-series by adding the temperature climatology for "\
-        "{}-{}".format(climstart, climend)
-    utils.set_global_atts(cube_abs, attrs_tas)
-    utils.save_variable(cube_abs,
-                        short_name,
-                        out_dir,
-                        attrs_tas,
-                        unlimited_dimensions=['time'])
-
-    # tas climatology
-    logger.info("Saving tas climatology")
-    cmor_info_tas.long_name = 'Near-Surface Air Temperature climatology'
-    attrs_tas['comment'] = "Climatology {}-{}".format(climstart, climend)
-    utils.fix_var_metadata(cube_clim, cmor_info_tas)
-    utils.set_global_atts(cube_clim, attrs_tas)
-    utils.save_variable(cube_clim,
-                        'tasc',
-                        out_dir,
-                        attrs_tas,
-                        unlimited_dimensions=['time'])
+def _extr_var_n_calc_abs_tas(short_name, var, cfg, filepath, out_dir):
+    """Extract variable."""
+    # load tas anomaly, climatology and sftlf
+    with catch_warnings():
+        filterwarnings(
+            action='ignore',
+            message='Ignoring netCDF variable .* invalid units .*',
+            category=UserWarning,
+            module='iris',
+        )
+        filterwarnings(
+            action='ignore',
+            message='.* not used since it\ncannot be safely cast to variable'
+                    ' data type *',
+            category=UserWarning,
+            module='iris',
+        )
+        cubes = iris.load(filepath)
 
     # tas anomaly
-    logger.info("Saving tas anomaly")
-    cmor_info_tas.long_name = 'Near-Surface Air Temperature anomaly'
-    attrs_tas['comment'] = "Temperature anomaly with respect to the period"\
-        " {}-{}".format(climstart, climend)
-    utils.fix_var_metadata(cube_anom, cmor_info_tas)
-    utils.set_global_atts(cube_anom, attrs_tas)
-    utils.save_variable(cube_anom,
-                        'tasa',
-                        out_dir,
-                        attrs_tas,
-                        unlimited_dimensions=['time'])
+    raw_var = var.get('raw', short_name)
+    cube_anom = cubes.extract(utils.var_name_constraint(raw_var))[0]
 
-    #### sftlf
+    # tas climatology
+    raw_var_clim = var.get('rawclim', short_name)
+    cube_clim = cubes.extract(utils.var_name_constraint(raw_var_clim))[0]
+    # information on time for the climatology are only present in the long_name
+    climstart, climend = [int(x) for x in re.findall(r"\d{4}",
+                                                     cube_clim.long_name)]
+
+    # do a correct time coordinate
+    cube_anom, cube_clim = reinit_broken_time(cube_anom, cube_clim,
+                                              climstart, climend)
+
+    # derive absolute tas values
+    cube_abs = calc_abs_temperature(cube_anom, cube_clim, short_name)
+
+    # fix coordinates
+    logger.info("Fixing coordinates")
+    attrs = cfg['attributes']
+    attrs['mip'] = var['mip']
+    short_names = [short_name, var['short_anom'], var['short_clim']]
+    for s_name, cube in zip(short_names, [cube_abs, cube_anom, cube_clim]):
+        cmor_info = cfg['cmor_table'].get_variable(var['mip'], s_name)
+
+        utils.fix_coords(cube)
+        if 'height2m' in cmor_info.dimensions:
+            utils.add_height2m(cube)
+
+        cube.units = var['raw_units']
+        if s_name != 'tasa':
+            cube.convert_units(cmor_info.units)
+
+        utils.fix_var_metadata(cube, cmor_info)
+
+    # save temperature data
+    logger.info("Saving temperature data")
+    comments = {'tas': "Temperature time-series calculated from the anomaly "
+                       "time-series by adding the temperature climatology "
+                       "for {}-{}".format(climstart, climend),
+                'tasc': "Climatology {}-{}".format(climstart, climend),
+                'tasa': "Temperature anomaly with respect to the period"
+                        " {}-{}".format(climstart, climend)}
+
+    for s_name, cube in zip(short_names, [cube_abs, cube_anom, cube_clim]):
+        attrs['comment'] = comments[s_name]
+        utils.set_global_atts(cube, attrs)
+        utils.save_variable(cube,
+                            short_name,
+                            out_dir,
+                            attrs,
+                            unlimited_dimensions=['time'])
+
+    # sftlf
     # extract sftlf
     raw_var_sftlf = var.get('rawsftlf', short_name)
     cube_sftlf = cubes.extract(utils.var_name_constraint(raw_var_sftlf))[0]
 
     # fix coordinates
-    cube_sftlf.coord('latitude').var_name = 'lat'
-    cube_sftlf.coord('longitude').var_name = 'lon'
     utils.fix_coords(cube_sftlf)
 
     # cmorize sftlf units
