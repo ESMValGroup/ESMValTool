e1152ca43db562a0682a613d13841fbbdb2684fa
Björn Brötz <bjoern.broetz@dlr.de>
2020-03-09 11:14:51 +0100

    Add python based extreme event diag&recipe.

---
+++ esmvaltool/diag_scripts/extreme_events/extreme_events.py
@@ -0,0 +1,140 @@
+"""Calculates the ETCCDI Climate Change Indices."""
+import logging
+import os
+from pprint import pformat
+import numpy as np
+import iris
+
+from esmvaltool.diag_scripts.shared import (group_metadata, run_diagnostic,
+                                            select_metadata, sorted_metadata)
+from esmvaltool.diag_scripts.shared._base import (
+    ProvenanceLogger, get_diagnostic_filename, get_plot_filename)
+from esmvaltool.diag_scripts.shared.plot import quickplot
+
+logger = logging.getLogger(os.path.basename(__file__))
+
+
+def get_provenance_record(attributes, ancestor_files):
+    """Create a provenance record describing the diagnostic data and plot."""
+    caption = ("Average {long_name} between {start_year} and {end_year} "
+               "according to {dataset}.".format(**attributes))
+
+    record = {
+        'caption': caption,
+        'statistics': ['mean'],
+        'domains': ['global'],
+        'plot_type': 'zonal',
+        'authors': [
+            'andela_bouwe',
+            'righi_mattia',
+        ],
+        'references': [
+            'acknow_project',
+        ],
+        'ancestors': ancestor_files,
+    }
+    return record
+
+def _count_values_below(data, axis, threshold=273.15):
+    return np.sum(data < threshold, axis=axis, dtype=int)
+
+def _count_values(data, axis, threshold, logic):
+    if logic == 'lower':
+        return np.sum(data < threshold, axis=axis, dtype=int)
+    elif logic == 'greater':
+        return np.sum(data > threshold, axis=axis, dtype=int)
+    else:
+        raise Exception('Not implemented')
+
+def _spell(data, axis, threshold, duration):
+    pass
+
+def _select_value_per_month(data, axis, extreme):
+    pass
+
+def _outlier(data, axis, quantile, threshold, logic):
+    pass
+
+def _mean_diff(data, axis):
+    pass
+
+def _count_calc(data, axis):
+    pass
+
+def _sum(data, axis):
+    pass
+
+def compute_diagnostic(filename):
+    """Compute an example diagnostic."""
+    logger.debug("Loading %s", filename)
+    cube = iris.load_cube(filename)
+
+    count_true = iris.analysis.Aggregator('_count_values_below',
+            _count_values_below, units_func=lambda units: 1)
+
+    logger.debug("Running example computation")
+    return cube.collapsed('time', count_true)
+
+
+def plot_diagnostic(cube, basename, provenance_record, cfg):
+    """Create diagnostic data and plot it."""
+    diagnostic_file = get_diagnostic_filename(basename, cfg)
+
+    logger.info("Saving analysis results to %s", diagnostic_file)
+    iris.save(cube, target=diagnostic_file)
+
+    if cfg['write_plots'] and cfg.get('quickplot'):
+        plot_file = get_plot_filename(basename, cfg)
+        logger.info("Plotting analysis results to %s", plot_file)
+        provenance_record['plot_file'] = plot_file
+        quickplot(cube, filename=plot_file, **cfg['quickplot'])
+
+    logger.info("Recording provenance of %s:\n%s", diagnostic_file,
+                pformat(provenance_record))
+    with ProvenanceLogger(cfg) as provenance_logger:
+        provenance_logger.log(diagnostic_file, provenance_record)
+
+
+def main(cfg):
+    """Compute the time average for each input dataset."""
+    # Get a description of the preprocessed data that we will use as input.
+    input_data = cfg['input_data'].values()
+
+    # Demonstrate use of metadata access convenience functions.
+    selection = select_metadata(input_data, short_name='pr', project='CMIP5')
+    logger.info("Example of how to select only CMIP5 precipitation data:\n%s",
+                pformat(selection))
+
+    selection = sorted_metadata(selection, sort='dataset')
+    logger.info("Example of how to sort this selection by dataset:\n%s",
+                pformat(selection))
+
+    grouped_input_data = group_metadata(
+        input_data, 'alias', sort='alias')
+    logger.info(
+        "Example of how to group and sort input data by alias:"
+        "\n%s", pformat(grouped_input_data))
+
+    # Example of how to loop over variables/datasets in alphabetical order
+    for alias in grouped_input_data:
+        logger.info("Processing alias %s", alias)
+        cubes  = {}
+        for attributes in grouped_input_data[alias]:
+            logger.info("Processing dataset %s", attributes['dataset'])
+            input_file = attributes['filename']
+            cubes[attributes['short_name']] = iris.load_cube(input_file)
+        # log if all expected short_names are there
+        #call compute idices for alias with all 4 cubes
+        #call plot idices for alias
+        #cube = compute_diagnostic(input_file)
+        output_basename = os.path.splitext(
+                os.path.basename(input_file))[0] + '_mean'
+        provenance_record = get_provenance_record(
+                attributes, ancestor_files=[input_file])
+        #plot_diagnostic(cube, output_basename, provenance_record, cfg)
+
+
+if __name__ == '__main__':
+
+    with run_diagnostic() as config:
+        main(config)
