87399b0c5260494e36f87fceedc8f3f4696dfb5c
Javier Vegas-Regidor <javier.vegas@bsc.es>
2020-03-17 10:37:01 +0100

    Adress prospector issues

--- esmvaltool/diag_scripts/primavera/extreme_precipitation/extreme_precipitation.py
+++ esmvaltool/diag_scripts/primavera/extreme_precipitation/extreme_precipitation.py
@@ -1,7 +1,7 @@
 """
-Generalised extreme value analysis
+Generalised extreme value analysis.
 
-Apply GEV analysis  grid point-by-grid point
+Apply GEV analysis grid point by grid point.
 """
 
 import os
@@ -12,7 +12,9 @@ import iris
 import iris.cube
 import iris.analysis
 import iris.util
-import rpy2.robjects
+
+from rpy2.robjects import r, numpy2ri, IntVector
+from rpy2.robjects.packages import importr
 
 import esmvaltool.diag_scripts.shared
 from esmvaltool.diag_scripts.shared.plot import quickplot
@@ -21,7 +23,8 @@ import esmvaltool.diag_scripts.shared.names as n
 logger = logging.getLogger(os.path.basename(__file__))
 
 
-class ExtremePrecipitation(object):
+class ExtremeValuesAnalysis():
+    """Extreme values analysis."""
 
     def __init__(self, config):
         self.cfg = config
@@ -31,85 +34,88 @@ class ExtremePrecipitation(object):
 
         self.gev_param_symbols = ['mu', 'sigma', 'xi']
         self.gev_param_name = dict(mu='location', sigma='scale', xi='shape')
-        self.return_period = rpy2.robjects.IntVector(self.return_period)
+        self.return_period = IntVector(self.return_period)
+        self.extremes = importr('extRemes')
 
-    def _get_return_period_name(self, period):
+    @staticmethod
+    def _get_return_period_name(period):
         return '{}-year level'.format(period)
 
     def compute(self):
-        from rpy2.robjects import r, numpy2ri
-        from rpy2.robjects.packages import importr
-
-        extremes = importr('extRemes')
+        """Compute extreme values metric."""
         numpy2ri.activate()
         r['options'](warn=-1)
         for filename in self.filenames:
             logger.info('Processing %s', filename)
             cube = iris.load_cube(filename)
-            logger.info(cube)
-
+            logger.debug(cube)
             model_cube = cube[0, ...]
-            shape = model_cube.shape
-            units = model_cube.units
 
             for season in set(cube.coord('clim_season').points):
                 # Clean R objects
                 r('rm(list = ls())')
                 logger.info('Processing season %s', season)
                 season_cube = cube.extract(iris.Constraint(clim_season=season))
+                fevd, ret_values = self._compute_season(
+                    season_cube, model_cube)
+                self._plot_results(filename, season, fevd, ret_values)
+                self._save_results(filename, season, fevd, ret_values)
 
-                logger.info('GEV analysis...')
-                fevd = dict()
-                for par in self.gev_param_symbols:
-                    fevd[par] = np.full(shape, np.nan)
+    def _compute_season(self, season_cube, model_cube):
+        logger.info('GEV analysis...')
+        shape = model_cube.shape
+        units = model_cube.units
+        fevd = dict()
+        for par in self.gev_param_symbols:
+            fevd[par] = np.full(shape, np.nan)
 
-                rl = dict()
-                for period in self.return_period:
-                    rl[period] = np.full(shape, np.nan)
+        ret_values = dict()
+        for period in self.return_period:
+            ret_values[period] = np.full(shape, np.nan)
 
-                for x in range(shape[0]):
-                    for y in range(shape[1]):
-                        data = season_cube.data[..., x, y]
-                        if np.any(data):
-                            self._compute_metric(
-                                data, units, fevd, rl, extremes, x, y
-                            )
-
-                for par, data in fevd.items():
-                    fevd[par] = self._create_cube(data, par, model_cube)
-
-                for period, data in rl.items():
-                    rl[period] = self._create_cube(
-                        data,
-                        self._get_return_period_name(period),
-                        model_cube
+        for i in range(shape[0]):
+            for j in range(shape[1]):
+                data = season_cube.data[..., i, j]
+                if np.any(data):
+                    self._compute_metric(
+                        data, units, fevd, ret_values, i, j
                     )
-                self._plot_results(filename, season, fevd, rl)
-                self._save_results(filename, season, fevd, rl)
 
-    def _compute_metric(self, data, units, fevd, rl, extremes, x, y):
-        evdf = extremes.fevd(data, units=units.origin)
+        for par, data in fevd.items():
+            fevd[par] = self._create_cube(data, par, model_cube)
+
+        for period, data in ret_values.items():
+            ret_values[period] = self._create_cube(
+                data,
+                self._get_return_period_name(period),
+                model_cube
+            )
+        return fevd, ret_values
+
+    def _compute_metric(self, data, units, fevd, ret_values, i, j):
+        evdf = self.extremes.fevd(data, units=units.origin)
         results = evdf.rx2('results').rx2('par')
         # -ve mu/sigma invalid
         if results.rx2('location')[0] > 0. and results.rx2('scale')[0] > 0.:
             for par in self.gev_param_symbols:
-                fevd[par][x, y] = results.rx2(self.gev_param_name[par])[0]
-            r_level = extremes.return_level(
+                fevd[par][i, j] = results.rx2(self.gev_param_name[par])[0]
+            r_level = self.extremes.return_level(
                 evdf,
                 return_period=self.return_period,
-                qcov=extremes.make_qcov(evdf)
+                qcov=self.extremes.make_qcov(evdf)
             )
-            for data, period in zip(r_level, self.return_period):
-                rl[period][x, y] = data
+            for values, period in zip(r_level, self.return_period):
+                ret_values[period][i, j] = values
 
-    def _create_cube(self, data, name, model_cube):
+    @staticmethod
+    def _create_cube(data, name, model_cube):
         cube = model_cube.copy(data)
         cube.standard_name = None
         cube.long_name = name
         cube.var_name = None
         return cube
 
-    def _plot_results(self, filename, season, fevd, rl):
+    def _plot_results(self, filename, season, fevd, ret_values):
         if not self.cfg[n.WRITE_PLOTS]:
             return
 
@@ -138,7 +144,7 @@ class ExtremePrecipitation(object):
                 filename=par_ffp,
                 **(self.cfg.get('gev_quickplot', default_plot_config))
             )
-        for return_period in rl:
+        for return_period in ret_values:
             return_periods_path = os.path.join(
                 results_subdir,
                 'return_period_{}years.{}'.format(
@@ -147,7 +153,7 @@ class ExtremePrecipitation(object):
                 )
             )
             quickplot(
-                rl[return_period],
+                ret_values[return_period],
                 filename=return_periods_path,
                 **(self.cfg.get(
                     'return_period_quickplot', default_plot_config))
@@ -175,8 +181,9 @@ class ExtremePrecipitation(object):
 
 
 def main():
+    """Execute analysis."""
     with esmvaltool.diag_scripts.shared.run_diagnostic() as config:
-        ExtremePrecipitation(config).compute()
+        ExtremeValuesAnalysis(config).compute()
 
 
 if __name__ == "__main__":
