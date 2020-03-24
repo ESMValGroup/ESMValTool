53dedbec88c3674e6aa0248fbb5fe8f463737504
SarahAlidoost <f.alidoost@esciencecenter.nl>
2020-03-24 13:50:12 +0100

    refactor the test

--- tests/unit/test_recipes.py
+++ tests/unit/test_recipes.py
@@ -10,18 +10,17 @@ REFERENCES_PATH = Path(esmvaltool.__file__).absolute().parent / 'references'
 
 
 @pytest.mark.parametrize('recipe_file', RECIPES, ids=IDS)
-def _test_reference_tags(recipe_file):
-    recipe = yaml.safe_load(recipe_file.read_text())
-    value = recipe['documentation']['references']
-    _check_bibtex_exist(value)
-
-
-def _check_bibtex_exist(tag):
+def test_reference_tags(recipe_file):
     """Check bibtex file is added to REFERENCES_PATH."""
-    bibtex_file = REFERENCES_PATH / f'{tag}.bibtex'
-    error_msg = 'The reference file {} is not available in {}.' \
-                ' Please check instructions about' \
-                ' "Contributing a new diagnostic or recipe"' \
-                ' in https://esmvaltool.readthedocs.io/en/latest/' \
-                ''.format(bibtex_file, REFERENCES_PATH)
-    assert not bibtex_file.is_file(), error_msg
+    recipe = yaml.safe_load(recipe_file.read_text())
+    tags = recipe.get('documentation', {}).get('references', [])
+    for tag in tags:
+        bibtex_file = REFERENCES_PATH / f'{tag}.bibtex'
+        error_msg = 'The tag {} is mentioned in the recipe {}.' \
+                    ' Howevere, its reference file {}.bibtex' \
+                    ' is not available in {}.' \
+                    ' Please check instructions about' \
+                    ' "Contributing a new diagnostic or recipe"' \
+                    ' in https://esmvaltool.readthedocs.io/en/latest/' \
+                    ''.format(tag, recipe_file, tag, REFERENCES_PATH)
+        assert bibtex_file.is_file(), error_msg
