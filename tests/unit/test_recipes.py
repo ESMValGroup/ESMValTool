01ebb6705f2c5419a55a2abeb78bf90190682d0f
SarahAlidoost <f.alidoost@esciencecenter.nl>
2020-03-24 13:04:00 +0100

    add a message for assert

--- tests/unit/test_recipes.py
+++ tests/unit/test_recipes.py
@@ -19,4 +19,9 @@ def _test_reference_tags(recipe_file):
 def _check_bibtex_exist(tag):
     """Check bibtex file is added to REFERENCES_PATH."""
     bibtex_file = REFERENCES_PATH / f'{tag}.bibtex'
-    assert not bibtex_file.is_file()
+    error_msg = 'The reference file {} is not available in {}.' \
+                ' Please check instructions about' \
+                ' "Contributing a new diagnostic or recipe"' \
+                ' in https://esmvaltool.readthedocs.io/en/latest/' \
+                ''.format(bibtex_file, REFERENCES_PATH)
+    assert not bibtex_file.is_file(), error_msg
