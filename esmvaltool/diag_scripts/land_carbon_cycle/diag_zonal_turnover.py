5417d69158c78a1dc9305c55c00ab38fee4aae69
Klaus Zimmermann <klaus.zimmermann@smhi.se>
2020-04-06 10:38:28 +0200

    Cleanup

--- esmvaltool/diag_scripts/land_carbon_cycle/diag_zonal_turnover.py
+++ esmvaltool/diag_scripts/land_carbon_cycle/diag_zonal_turnover.py
@@ -3,23 +3,19 @@ compares the zonal turnover time from the models with observation from
 Carvalhais et al., 2014
 '''
 
-# operating system manipulations (e.g. path constructions)
 import os
 
-# to manipulate iris cubes
 import iris
 import matplotlib.pyplot as plt
 import numpy as np
-# import internal esmvaltool modules here
+
 from esmvaltool.diag_scripts.shared import group_metadata
 from esmvaltool.diag_scripts.shared import run_diagnostic
 
-# helper functions
 import extraUtils as xu
 from shared import _load_variable, _get_obs_data_zonal
 
 
-# Classes and settings
 def _get_fig_config(diag_config):
     '''
     get the default settings of the figure, and replace default with
@@ -43,7 +39,6 @@ def _get_fig_config(diag_config):
     return fig_config
 
 
-# calculation and data functions
 def _apply_common_mask(dat_1, dat_2):
     '''
     apply a common mask of valid grid cells across two input arrays
@@ -56,37 +51,7 @@ def _apply_common_mask(dat_1, dat_2):
     dat_2[_val_mask] = np.nan
     dat_1 = np.ma.masked_invalid(dat_1)
     dat_2 = np.ma.masked_invalid(dat_2)
-    return dat_1, dat_2
-
-
-def _get_zonal_tau(diag_config):
-    '''
-    A diagnostic function to calculate the total carbon stock from the list of
-    variables passed to the diagnostic script.
-
-    Arguments:
-        diag_config - nested dictionary of metadata
-
-    Returns:
-        string; runs the user diagnostic
-
-    '''
-
-    my_files_dict = group_metadata(diag_config['input_data'].values(),
-                                   'dataset')
-
-    fig_config = _get_fig_config(diag_config)
-    zonal_tau_mod = {}
-    for key, value in my_files_dict.items():
-        zonal_tau_mod[key] = {}
-        ctotal = _load_variable(value, 'ctotal')
-        gpp = _load_variable(value, 'gpp')
-        zonal_tau_mod[key] = _calc_zonal_tau(gpp, ctotal, fig_config)
-
-    zonal_tau_obs = _get_obs_data_zonal(diag_config)
-    # plot the figure
-    _plot_zonal_tau(zonal_tau_mod, zonal_tau_obs, diag_config)
-    return 'done plotting the zonal turnover time'
+    return (dat_1, dat_2)
 
 
 def _calc_zonal_tau(gpp, ctotal, fig_config):
@@ -125,7 +90,6 @@ def _calc_zonal_tau(gpp, ctotal, fig_config):
     return zonal_tau
 
 
-# Plotting functions
 def _plot_zonal_tau(all_mod_dat, all_obs_dat, diag_config):
     '''
     makes the maps of variables
@@ -134,12 +98,6 @@ def _plot_zonal_tau(all_mod_dat, all_obs_dat, diag_config):
         diag_config - nested dictionary of metadata
         cube - the cube to plot
         dataset - name of the dataset to plot
-
-    Returns:
-        string; makes some time-series plots
-
-    Note: this function is private; remove the '_'
-    so you can make it public.
     '''
     fig_config = _get_fig_config(diag_config)
     models = list(all_mod_dat.keys())
@@ -206,10 +164,31 @@ def _plot_zonal_tau(all_mod_dat, all_obs_dat, diag_config):
                 dpi=450)
     plt.close()
 
-    return 'Plotting complete for zonal turnover'
+
+def main(diag_config):
+    '''
+    A diagnostic function to calculate the total carbon stock from the list of
+    variables passed to the diagnostic script.
+
+    Arguments:
+        diag_config - nested dictionary of metadata
+    '''
+
+    my_files_dict = group_metadata(diag_config['input_data'].values(),
+                                   'dataset')
+
+    fig_config = _get_fig_config(diag_config)
+    zonal_tau_mod = {}
+    for key, value in my_files_dict.items():
+        zonal_tau_mod[key] = {}
+        ctotal = _load_variable(value, 'ctotal')
+        gpp = _load_variable(value, 'gpp')
+        zonal_tau_mod[key] = _calc_zonal_tau(gpp, ctotal, fig_config)
+
+    zonal_tau_obs = _get_obs_data_zonal(diag_config)
+    _plot_zonal_tau(zonal_tau_mod, zonal_tau_obs, diag_config)
 
 
-# main
 if __name__ == '__main__':
     with run_diagnostic() as config:
-        _get_zonal_tau(config)
+        main(config)
