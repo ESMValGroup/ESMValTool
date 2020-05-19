01ee4df6f8e729ebff253f8ec09ea954546008a2
Valeriu Predoi <valeriu.predoi@gmail.com>
2020-05-19 16:45:00 +0100

    fixed styling

--- esmvaltool/cmorizers/obs/download_scripts/download_cds_satellite_soil_moisture_monthly.py
+++ esmvaltool/cmorizers/obs/download_scripts/download_cds_satellite_soil_moisture_monthly.py
@@ -1,13 +1,18 @@
-"""Script to download cds-satellite-soil-moisture from the Climate Data Store(CDS)"""
+"""
+Download cds-satellite-soil-moisture.
+
+Script to download cds-satellite-soil-moisture
+from the Climate Data Store(CDS).
+"""
 
 import argparse
 import os
-from datetime import datetime
+import subprocess
+
+import yaml
 
 import cdsapi
-import yaml
-from dateutil import relativedelta
-import subprocess
+
 
 def download_cds_satellite_soil_moisture():
     """Download dataset cds-satellite-albedo."""
@@ -38,94 +43,117 @@ def download_cds_satellite_soil_moisture():
 
     client = cdsapi.Client()
 
-    
     # Download per year
     # CDR from 1978-2018 (including 2018)
-    for year in range(1978,2019):    
+    for year in range(1978, 2019):
         savename = os.path.join(
             savedir,
-            # Although tgz is requested, the file 
+            # Although tgz is requested, the file
             # actually is a tar (issue has been opened on CDS)
-            f"cds-satellite-soil-moisture_{year}.tar") 
+            f"cds-satellite-soil-moisture_{year}.tar")
         request_dictionary = {
-        'format': 'tgz',
-        'variable': 'volumetric_surface_soil_moisture',
-        'type_of_sensor': 'combined_passive_and_active',
-        'time_aggregation': 'month_average',
-        'year': [
-            f'{year}',
-        ],
-        'month': [
-            '01', '02', '03',
-            '04', '05', '06',
-            '07', '08', '09',
-            '10', '11', '12',
-        ],
-        'day': '01',
-        'type_of_record': 'cdr',
-        'version': 'v201812.0.0',        }
-        client.retrieve('satellite-soil-moisture', request_dictionary, savename)
-    # Unpack the file
-    subprocess.check_call(["tar", "-xvf", savename, '--directory', savedir])
-    # Remove the tar file since it has been extracted
-    subprocess.check_call(["rm", savename])
-    
-    
-    # Now ICDR v201812.0.1 (identical to v201812.0.0 according to data provider except for dates covered)
-    savename = os.path.join(
-    savedir,
-    f"cds-satellite-soil-moisture_v201812.0.1.tar.gz")    
-    client.retrieve(
-    'satellite-soil-moisture',
-    {
-        'format': 'tgz',
-        'variable': 'volumetric_surface_soil_moisture',
-        'type_of_sensor': 'combined_passive_and_active',
-        'time_aggregation': 'month_average',
-        'year': '2019',
-        'type_of_record': 'icdr',
-        'month': [
-            '01', '02', '03',
-            '04', '05', '06',
-            '07', '08', '09',
-        ],
-        'version': 'v201812.0.1',
-        'day': '01',
-    }, savename)
+            'format':
+            'tgz',
+            'variable':
+            'volumetric_surface_soil_moisture',
+            'type_of_sensor':
+            'combined_passive_and_active',
+            'time_aggregation':
+            'month_average',
+            'year': [
+                f'{year}',
+            ],
+            'month': [
+                '01',
+                '02',
+                '03',
+                '04',
+                '05',
+                '06',
+                '07',
+                '08',
+                '09',
+                '10',
+                '11',
+                '12',
+            ],
+            'day':
+            '01',
+            'type_of_record':
+            'cdr',
+            'version':
+            'v201812.0.0',
+        }
+        client.retrieve('satellite-soil-moisture', request_dictionary,
+                        savename)
     # Unpack the file
     subprocess.check_call(["tar", "-xvf", savename, '--directory', savedir])
     # Remove the tar file since it has been extracted
     subprocess.check_call(["rm", savename])
 
-    
-    # Now ICDR v201812.0.0 (identical to v201812.0.1 according to data provider except for dates covered)
-    savename = os.path.join(
-    savedir,
-    f"cds-satellite-soil-moisture_v201812.0.0.tar")    
+    # Now ICDR v201812.0.1 (identical to v201812.0.0 according
+    # to data provider except for dates covered)
+    savename = os.path.join(savedir,
+                            f"cds-satellite-soil-moisture_v201812.0.1.tar.gz")
     client.retrieve(
-    'satellite-soil-moisture',
-    {
-        'format': 'tgz',
-        'variable': 'volumetric_surface_soil_moisture',
-        'type_of_sensor': 'combined_passive_and_active',
-        'time_aggregation': 'month_average',
-        'year': [
-            '2019', '2020',
-        ],
-        'type_of_record': 'icdr',
-        'month': [
-            '01', '02', '03',
-            '04', '10', '11',
-            '12',
-        ],
-        'version': 'v201812.0.0',
-        'day': '01',
-    }, savename)
+        'satellite-soil-moisture', {
+            'format': 'tgz',
+            'variable': 'volumetric_surface_soil_moisture',
+            'type_of_sensor': 'combined_passive_and_active',
+            'time_aggregation': 'month_average',
+            'year': '2019',
+            'type_of_record': 'icdr',
+            'month': [
+                '01',
+                '02',
+                '03',
+                '04',
+                '05',
+                '06',
+                '07',
+                '08',
+                '09',
+            ],
+            'version': 'v201812.0.1',
+            'day': '01',
+        }, savename)
     # Unpack the file
     subprocess.check_call(["tar", "-xvf", savename, '--directory', savedir])
     # Remove the tar file since it has been extracted
-    subprocess.check_call(["rm", savename]) 
-    
-        
+    subprocess.check_call(["rm", savename])
+
+    # Now ICDR v201812.0.0 (identical to v201812.0.1 according
+    # to data provider except for dates covered)
+    savename = os.path.join(savedir,
+                            f"cds-satellite-soil-moisture_v201812.0.0.tar")
+    client.retrieve(
+        'satellite-soil-moisture', {
+            'format': 'tgz',
+            'variable': 'volumetric_surface_soil_moisture',
+            'type_of_sensor': 'combined_passive_and_active',
+            'time_aggregation': 'month_average',
+            'year': [
+                '2019',
+                '2020',
+            ],
+            'type_of_record': 'icdr',
+            'month': [
+                '01',
+                '02',
+                '03',
+                '04',
+                '10',
+                '11',
+                '12',
+            ],
+            'version': 'v201812.0.0',
+            'day': '01',
+        }, savename)
+    # Unpack the file
+    subprocess.check_call(["tar", "-xvf", savename, '--directory', savedir])
+    # Remove the tar file since it has been extracted
+    subprocess.check_call(["rm", savename])
+
+
 if __name__ == "__main__":
     download_cds_satellite_soil_moisture()
