d7267e181bbafbf7a5b7ab865bfd928adf9abd8d
SarahAlidoost <f.alidoost@esciencecenter.nl>
2020-05-29 22:30:02 +0200

    improve recipe and diagnostics, make some functions, add some todo items

--- esmvaltool/diag_scripts/kcs/scenarios_table.py
+++ esmvaltool/diag_scripts/kcs/scenarios_table.py
@@ -1,45 +1,73 @@
-"""
-Visualize temperature accross datasets
+"""Visualize temperature accross datasets.
 
-- Make a plot of the global mean temperature change according to all datasets (defined above)
-- Get the global mean temperature change for specified years and specified percentiles (Delta T). These define our scenarios.
-- Select the 30-year period from the target model (all ensemble members) where they match the Delta T for each scenario.
+- Make a plot of the global mean temperature change
+according to all datasets (defined above),
+
+- Get the global mean temperature change for specified years
+and specified percentiles (Delta T). These define our scenarios.
+
+- Select the 30-year period from the target model (all ensemble members)
+where they match the Delta T for each scenario.
 """
 
-from itertools import combinations
+from itertools import product
+from datetime import timedelta
 import matplotlib.pyplot as plt
-import pandas as pd
+import iris
 import xarray as xr
 
+from esmvaltool.diag_scripts.shared import (run_diagnostic,
+                                            get_plot_filename,
+                                            select_metadata)
 
-from esmvaltool.diag_scripts.shared import (run_diagnostic, get_plot_filename,
-                                            get_diagnostic_filename)
 
-
-def get_target_delta_T(cfg, year):
+def get_target_delta_t(metadata, year, percentile):
     """Compute target delta T for KNMI scenarios."""
     # Read the 10th percentile dataset from the preprocessed data
     # Return the value for the scenario year.
-    return multimodelstats[year]
+    attribute = f'MultiModel{percentile}'
+    multimodelstat = select_metadata(metadata, alias=attribute)
+    cube = iris.load_cube(multimodelstat[0]['filename'])
+    time = iris.Constraint(time=lambda cell: cell.point.year == year)
+    return cube.extract(time).data.data
 
 
-def get_target_model_time_bounds(target_dataset, delta_T):
+def get_mean_ensemble(metadata, model):
+    """Compute average of ensembles for target model."""
+    target_dataset = select_metadata(metadata, dataset=model)
+    sum_cube = 0
+    for ensemble in target_dataset:
+        cube = iris.load_cube(ensemble['filename'])
+        sum_cube += cube
+    return sum_cube / len(target_dataset)
+
+
+def get_target_model_time_bounds(target_dataset, delta_t, span=30):
     """Return 30-year time bounds.
 
     Get the 30-year time bounds for which the target model delta T
     matches the target delta T for a specific scenario.
     """
-    # Compute rolling average delta T of the target dataset
-    # Get the year for which this delta T most closely matches the target delta T
-    # Get the start- and end year belonging to that 30-year period
-    # Possibly, apply/report a (pattern) scaling factor in order to match the target delta T more closely.
+    # Compute rolling average delta T of the target dataset?!
+    # Possibly, apply/report a (pattern) scaling factor
+    # in order to match the target delta T more closely.
+
+    cube = target_dataset - delta_t
+    min_cube = cube.collapsed('time', iris.analysis.MIN)
+    coord = min_cube.coord('time')
+    start = coord.cell(0).point - timedelta(span / 2 * 365.24)
+    end = coord.cell(0).point + timedelta(span / 2 * 365.24)
+    return [start.year, end.year], min_cube.data
 
 
-    return start, end, target_dT
+def make_plot(metadata, scenario):
+    """Make figure 3, left graph.
 
-
-def make_plot():
+    Multimodel values as line, reference value in black square,
+    steering variables in dark dots.
+    """
     # Create a figure and a list that we'll use to store output data to csv
+
     fig, ax = plt.subplots()
 
     # Loop over all datasets
@@ -60,46 +88,43 @@ def make_plot():
     filename = get_plot_filename('temperature_change_pdf', cfg)
     fig.savefig(filename, bbox_inches='tight', dpi=300)
 
-    return
 
-
-
-def main():
-
+def main(cfg):
+    """Return scenarios tables."""
     # a list of dictionaries describing all datasets passed on to the recipe
     metadata = cfg['input_data'].values()
-
+    print(metadata)
     # Define the different scenario's
     scenarios = []
-    for year, percentile in combinations(cfg['scenarios_years'], cfg['scenarios_percentiles']):
-        delta_T = get_target_delta_T
+    combinations = product(cfg['scenario_years'], cfg['scenario_percentiles'])
+    for year, percentile in combinations:
+        cmip_dt = get_target_delta_t(metadata, year, percentile)
         scenario = {
             'year': year,
             'percentile': percentile,
-            'CMIP_delta_T_global': delta_T
-            }
+            'CMIP_delta_T_global': cmip_dt
+        }
         scenarios.append(scenario)
-
     print(scenarios)
-
-    target_datasets = select_metadata(metadata, dataset.contains(cfg['target_model']))
-
     # find all ensemble members (datasets) of the target_model
-    # average them
+    # average them TODO check for ensembles in different rcp
+    average = get_mean_ensemble(metadata, cfg['target_model'])
+
     # use the average temperature change to get the time bounds
     for scenario in scenarios:
-        cmip_dT = scenario['CMIP_delta_T_global']
-        period_bounds, target_dT = get_target_model_time_bounds(average, scenario['CMIP_delta_T_global'])
+        cmip_dt = scenario['CMIP_delta_T_global']
+        period_bounds, target_dt = get_target_model_time_bounds(
+            average, scenario['CMIP_delta_T_global']
+        )
         scenario['period_bounds'] = period_bounds
-        scenario['target_model_delta_T'] = target_dT
-        scenario['pattern_scaling_factor'] = cmip_dT / target_dT
+        scenario['target_model_delta_T'] = target_dt
+        scenario['pattern_scaling_factor'] = cmip_dt / target_dt
+    print(scenarios)
 
-
-    # Make a nice plot
-    if cfg['write_plots'] == True:
-        make_plot()
-
-    return
+    # TODO Make a nice plot
+    # if cfg['write_plots']:
+    #     make_plot()
+    # TODO add provenance
 
 
 if __name__ == '__main__':
