10916377e2e93b00a77f491966013e8c43feb592
SarahAlidoost <f.alidoost@esciencecenter.nl>
2020-07-03 13:28:28 +0200

    replace -c with --config_file in cli

--- tests/integration/test_diagnostic_run.py
+++ tests/integration/test_diagnostic_run.py
@@ -135,7 +135,7 @@ def test_diagnostic_run(tmp_path, script_file, script):
     with arguments(
             'esmvaltool',
             'run',
-            '-c',
+            '--config_file',
             config_user_file,
             str(recipe_file),
     ):
