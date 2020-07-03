e09db7bb1359251c6ae1aa0b9e6fedad4b2af8ff
bascrezee <bascrezee@gmail.com>
2020-07-03 14:08:57 +0200

    added year 2020 and const file

--- esmvaltool/cmorizers/obs/download_scripts/download_merra2.sh
+++ esmvaltool/cmorizers/obs/download_scripts/download_merra2.sh
@@ -10,14 +10,18 @@
 #  - Check the years
 #  - Run this script as follow ./download_merra2.sh path_to_save_the_files
 #
-# 
+#
 # Additional resources:
 #    - file specification document is available here: https://gmao.gsfc.nasa.gov/pubs/docs/Bosilovich785.pdf)
 
 
 # The years that should be downloaded.
-yrs=({1980..2019})
+yrs=({1980..2020})
 
 for yr in ${yrs[@]}; do
-    wget -r -nd -nH --load-cookies ~/.urs_cookies --save-cookies ~/.urs_cookies --directory-prefix=$1 --auth-no-challenge=on --keep-session-cookies -A '*_lnd_*.nc4' https://goldsmr4.gesdisc.eosdis.nasa.gov/data/MERRA2_MONTHLY/M2TMNXLND.5.12.4/${yr}/ -R "*.tmp"
+   echo $yr
+   wget -r -nd -nH --load-cookies ~/.urs_cookies --save-cookies ~/.urs_cookies --directory-prefix=$1 --auth-no-challenge=on --keep-session-cookies -A '*_lnd_*.nc4' https://goldsmr4.gesdisc.eosdis.nasa.gov/data/MERRA2_MONTHLY/M2TMNXLND.5.12.4/${yr}/ -R "*.tmp"
 done
+
+# Also download constants file
+wget --load-cookies ~/.urs_cookies --save-cookies ~/.urs_cookies directory-prefix=$1 --auth-no-challenge=on --keep-session-cookies https://goldsmr4.gesdisc.eosdis.nasa.gov/data/MERRA2_MONTHLY/M2C0NXLND.5.12.4/1980/MERRA2_100.const_2d_lnd_Nx.00000000.nc4
