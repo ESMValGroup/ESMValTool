837c092fcd1ba8043f248d4c3d6875af532b186c
Bouwe Andela <b.andela@esciencecenter.nl>
2020-08-05 13:20:26 +0200

    Add installation instruction for R and Julia dependencies tot pip install (#1787)
    * Add installation instruction for R and Julia dependencies
    
    * Add autodocsumm for building documentation

--- setup.py
+++ setup.py
@@ -69,6 +69,7 @@ REQUIREMENTS = {
     # Development dependencies
     # Use pip install -e .[develop] to install in development mode
     'develop': [
+        'autodocsumm',
         'codespell',
         'isort',
         'prospector[with_pyroma]!=1.1.6.3,!=1.1.6.4',
