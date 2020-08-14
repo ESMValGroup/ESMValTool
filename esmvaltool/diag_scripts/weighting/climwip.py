faee2f8efbfa229314191f9f3f607bb538dcc3df
Stef Smeets <s.smeets@esciencecenter.nl>
2020-08-14 14:51:56 +0200

    Add documentation and type hints

--- esmvaltool/diag_scripts/weighting/climwip.py
+++ esmvaltool/diag_scripts/weighting/climwip.py
@@ -77,42 +77,52 @@ def calculate_weights_sigmas(distances, sigmas_q, sigmas_i):
     return weights
 
 
-def weighted_distance_matrix(data):
+def weighted_distance_matrix(data_array: 'xr.DataArray'):
     pass
 
 
-def area_weighted_mean(data):
-    """Calculate mean weighted by the latitude."""
+def area_weighted_mean(data_array: 'xr.DataArray') -> 'xr.DataArray':
+    """Calculate area mean weighted by the latitude.
 
-    weights_lat = np.cos(np.radians(data.lat))
-    means = data.weighted(weights_lat).mean(dim=['lat', 'lon'])
+    Returns a data array consisting of N values, 
+    where N == number of ensemble members"""
+
+    weights_lat = np.cos(np.radians(data_array.lat))
+    means = data_array.weighted(weights_lat).mean(dim=['lat', 'lon'])
 
     return means
 
 
-def distance_matrix(data):
-    """Takes a dataset with ensemble member/lon/lat. Flattens lon/lat 
+def distance_matrix(data_array: 'xr.DataArray') -> 'xr.DataArray':
+    """Calculate the pairwise distance between model members.
+
+    Takes a dataset with ensemble member/lon/lat. Flattens lon/lat 
     into a single dimension. Calculates the distance between every 
     ensemble member.
 
     Returns 2D NxN array, where N == number of ensemble members.
     """
-    n_members = data.shape[0]
+    n_members = data_array.shape[0]
     
-    data = data.reshape(n_members, -1)
+    data_array = data_array.reshape(n_members, -1)
     
     # pdist does not work with NaN
-    idx = np.where(np.all(np.isfinite(data), axis=0))[0]
-    data = data[:, idx]
+    idx = np.where(np.all(np.isfinite(data_array), axis=0))[0]
+    data_array = data_array[:, idx]
 
-    d_matrix = squareform(pdist(data, metric='euclidean'))
+    d_matrix = squareform(pdist(data_array, metric='euclidean'))
     np.fill_diagonal(d_matrix, np.nan)
 
     return d_matrix
 
 
-def calculate_independence(data_array: 'xarray.DataArray') -> 'xarray.DataArray':
-    """calculate_independence."""
+def calculate_independence(data_array: 'xr.DataArray') -> 'xr.DataArray':
+    """Calculate_independence.
+
+    The independence is calculated as a distance matrix between 
+    the datasets defined in the `data_array`. Returned is a square matrix with
+    where the number of elements along each edge equals 
+    the number of ensemble members."""
     # TODO: use weighted_distance_matrix
     
     diff = xr.apply_ufunc(
@@ -133,18 +143,27 @@ def visualize_independence(independence):
     return returned_stuff
 
 
-def calculate_performance(model_data, obs_data):
-    """calculate_performance."""
+def calculate_performance(
+        model_data: 'xr.DataArray', 
+        obs_data: 'xr.DataArray'
+    ) -> 'xr.DataArray':
+    """Calculate_performance.
+
+    Calculate the area weighted mean between the given ensemble of model data,
+    and observation data. The observation data must have the
+    ensemble dimension squeezed or reduced. Returns an xarray.DataArray
+    containing the same number of values as members of `model_data`"""
 
     diff = model_data - obs_data
 
     performance = area_weighted_mean(diff**2)**0.5
-    
+
     return performance
 
 
 def visualize_performance(performance):
     """visualize_performance."""
+
     # TODO: complete this function
     returned_stuff = None
     return returned_stuff
@@ -152,6 +171,7 @@ def visualize_performance(performance):
 
 def calculate_weights():
     """calculate_weights."""
+
     # TODO: complete this function
     returned_stuff = None
     return returned_stuff
@@ -159,14 +179,17 @@ def calculate_weights():
 
 def visualize_weighting():
     """visualize_weighting."""
+
     # TODO: complete this function
     returned_stuff = None
     return returned_stuff
 
 
 def read_metadata(cfg, projects: list) -> dict:
-    """Read the metadata from the config file
-    Project is the list of project identifiers to select."""
+    """Read the metadata from the config file.
+
+    Project is the list of project identifiers (str) to select."""
+
     d = defaultdict(list)
 
     for project in projects:
@@ -181,7 +204,12 @@ def read_metadata(cfg, projects: list) -> dict:
 
 
 def read_input_data(datasets: list, dim: str='data_ensemble') -> 'xr.DataArray':
-    """Read the diagnostic data from the list of given datasets (metadata) into an Xarray."""
+    """Loads data from metadata.
+
+    Read the input data from the list of given data sets. `datasets` is a list
+    of metadata containing the filenames to load. Only returns the given `variable`.
+    The datasets are stacked along the `dim` dimension. Returns an xarray.DataArray."""
+
     to_concat = []
 
     for dataset in datasets:
@@ -205,12 +233,15 @@ def read_observation_data(datasets: list) -> 'xr.DataArray':
     return read_input_data(datasets, dim='obs_ensemble')
 
 
-def aggregate_obs_data(dataset, operator: str='median'):
-    """Reduce the dataset along the `obs_ensemble` dimension by applying
-    the operator. Returns an xarray with the ensemble dimension squeezed to 1D."""
+def aggregate_obs_data(data_array: 'xr.DataArray', operator: str='median') -> 'xr.DataArray':
+    """Reduce data array along ensemble dimension.
+
+    Apply the operator to squeeze the ensemble dimension by applying
+    the `operator` along the ensemble (`obs_ensemble`) dimension. Returns
+    an xarray.Dataset squeezed to 1D."""
 
     if operator == 'median':
-        return dataset.median(dim='obs_ensemble')
+        return data_array.median(dim='obs_ensemble')
     else:
         raise ValueError(f'No such operator `{operator}`')
 
@@ -250,7 +281,7 @@ def main(cfg):
         
         visualize_independence(independence)  # TODO
 
-        print(independence)
+        print(independence.values)
         print()
 
     for variable in variables:
@@ -263,11 +294,8 @@ def main(cfg):
 
         visualize_performance(performance)  # TODO
 
-        print(performance)
+        print(performance.values)
         print()
-
-
-
 
     # independences = xr.concat(independences, dim='diagnostic')
 
