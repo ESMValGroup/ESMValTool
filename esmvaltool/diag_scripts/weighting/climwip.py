b08998ba4826d3a793034a71cce0e0f2a68210f7
Peter Kalverla <peter.kalverla@gmx.com>
2020-08-17 16:01:53 +0200

    Add function to calculate weights

--- esmvaltool/diag_scripts/weighting/climwip.py
+++ esmvaltool/diag_scripts/weighting/climwip.py
@@ -12,71 +12,6 @@ from scipy.spatial.distance import pdist, squareform
 from collections import defaultdict
 
 
-
-# (iii) The point-to-point distance between model and the center of the observational spread is calculated
-def model_performance(model, obs):
-    return stats.pearsonr(model, obs)
-
-# (iv) The area-weighted root mean squared error is calculated over the selected region
-def calculate_weights(quality, independence, sigma_q, sigma_i):
-    """Calculates the (NOT normalised) weights for each model N.
-    Parameters
-    ----------
-    quality : array_like, shape (N,)
-        Array specifying the model quality.
-    independence : array_like, shape (N, N)
-        Array specifying the model independence.
-    sigma_q : float
-        Sigma value defining the form of the weighting function for the quality.
-    sigma_i : float
-        Sigma value defining the form of the weighting function for the independence.
-    Returns
-    -------
-    numerator, denominator : ndarray, shape (N,)
-    """
-    numerator = np.exp(-((quality/sigma_q)**2))
-    exp = np.exp(-((independence/sigma_i)**2))
-    sum_exp = [np.sum(np.delete(ee, ii)) for ii, ee in enumerate(exp)]  # sum i!=j
-    denominator = 1 + np.array(sum_exp)
-
-    if sigma_i == -99.:
-        denominator = (denominator * 0) + 1  # set to 1 (except NaN)
-    if sigma_q == -99.:
-        numerator = (numerator * 0) + 1  # set to 1 (except NaN)
-
-    return numerator, denominator
-
-def calculate_weights_sigmas(distances, sigmas_q, sigmas_i):
-    """Calculates the weights for each model N and combination of sigma values.
-    Parameters
-    ----------
-    distances : array_like, shape (N, N)
-        Array specifying the distances between each model.
-    sigmas_q : array_like, shape (M,)
-        Array of sigma values for the weighting function of the quality.
-    sigmas_i : array_like, shape (L,)
-        Array of sigma values for the weighting function of the independence.
-    Returns
-    -------
-    weights : ndarray, shape (M, L, N)
-        Array of weights for each model and sigma combination.
-    """
-    weights = np.zeros((len(sigmas_q), len(sigmas_i)) + distances.shape) * np.nan
-    for idx_q, sigma_q in enumerate(sigmas_q):
-        for idx_i, sigma_i in enumerate(sigmas_i):
-            for idx_d, dd in enumerate(distances):
-                # dd is the distance of each model to the idx_d-th model (='Truth')
-                nu, de = calculate_weights(dd, distances, sigma_q, sigma_i)
-                ww = nu/de
-                assert np.isnan(ww[idx_d]), 'weight for model dd should be nan'
-                ww[idx_d] = 0.  # set weight=0 to exclude the 'True' model
-                assert ww.sum() != 0, 'weights = 0! sigma_q too small?'
-                ww /= ww.sum()  # normalize weights
-                # ww[ww < 1.e-10] = 0.  # set small weights to zero  # NOTE!!
-                weights[idx_q, idx_i, idx_d] = ww
-    return weights
-
-
 def weighted_distance_matrix(data_array: 'xr.DataArray'):
     pass
 
@@ -84,7 +19,7 @@ def weighted_distance_matrix(data_array: 'xr.DataArray'):
 def area_weighted_mean(data_array: 'xr.DataArray') -> 'xr.DataArray':
     """Calculate area mean weighted by the latitude.
 
-    Returns a data array consisting of N values, 
+    Returns a data array consisting of N values,
     where N == number of ensemble members"""
 
     weights_lat = np.cos(np.radians(data_array.lat))
@@ -96,22 +31,21 @@ def area_weighted_mean(data_array: 'xr.DataArray') -> 'xr.DataArray':
 def distance_matrix(data_array: 'xr.DataArray') -> 'xr.DataArray':
     """Calculate the pairwise distance between model members.
 
-    Takes a dataset with ensemble member/lon/lat. Flattens lon/lat 
-    into a single dimension. Calculates the distance between every 
+    Takes a dataset with ensemble member/lon/lat. Flattens lon/lat
+    into a single dimension. Calculates the distance between every
     ensemble member.
 
     Returns 2D NxN array, where N == number of ensemble members.
     """
     n_members = data_array.shape[0]
-    
+
     data_array = data_array.reshape(n_members, -1)
-    
+
     # pdist does not work with NaN
     idx = np.where(np.all(np.isfinite(data_array), axis=0))[0]
     data_array = data_array[:, idx]
 
     d_matrix = squareform(pdist(data_array, metric='euclidean'))
-    np.fill_diagonal(d_matrix, np.nan)
 
     return d_matrix
 
@@ -119,22 +53,23 @@ def distance_matrix(data_array: 'xr.DataArray') -> 'xr.DataArray':
 def calculate_independence(data_array: 'xr.DataArray') -> 'xr.DataArray':
     """Calculate_independence.
 
-    The independence is calculated as a distance matrix between 
+    The independence is calculated as a distance matrix between
     the datasets defined in the `data_array`. Returned is a square matrix with
-    where the number of elements along each edge equals 
+    where the number of elements along each edge equals
     the number of ensemble members."""
     # TODO: use weighted_distance_matrix
-    
+
     diff = xr.apply_ufunc(
-        distance_matrix, data_array,
+        distance_matrix,
+        data_array,
         input_core_dims=[['model_ensemble', 'lat', 'lon']],
-        output_core_dims=[['perfect_model_ensemble', 'model_ensemble']]
-    )
+        output_core_dims=[['perfect_model_ensemble', 'model_ensemble']])
 
     diff.name = 'data'
     # diff = diff.expand_dims({'diagnostic': [idx]})
 
     return diff
+
 
 def visualize_independence(independence):
     """visualize_independence."""
@@ -143,10 +78,8 @@ def visualize_independence(independence):
     return returned_stuff
 
 
-def calculate_performance(
-        model_data: 'xr.DataArray', 
-        obs_data: 'xr.DataArray'
-    ) -> 'xr.DataArray':
+def calculate_performance(model_data: 'xr.DataArray',
+                          obs_data: 'xr.DataArray') -> 'xr.DataArray':
     """Calculate_performance.
 
     Calculate the area weighted mean between the given ensemble of model data,
@@ -169,12 +102,37 @@ def visualize_performance(performance):
     return returned_stuff
 
 
-def calculate_weights():
-    """calculate_weights."""
+def calculate_weights(performance: 'xr.DataArray',
+                      independence: 'xr.DataArray',
+                      sigma_performance: float,
+                      sigma_independence: float) -> 'xr.DataArray':
+    """Calculates the (NOT normalised) weights for each model N.
+    Parameters
+    ----------
+    performance : array_like, shape (N,)
+        Array specifying the model performance.
+    independence : array_like, shape (N, N)
+        Array specifying the model independence.
+    sigma_performance : float
+        Sigma value defining the form of the weighting function for the performance.
+    sigma_independence : float
+        Sigma value defining the form of the weighting function for the independence.
+    Returns
+    -------
+    weights : ndarray, shape (N,)
+    """
+    numerator = np.exp(-((performance / sigma_performance)**2))
+    exp = np.exp(-((independence / sigma_independence)**2))
 
-    # TODO: complete this function
-    returned_stuff = None
-    return returned_stuff
+    # Note diagonal = exp(0) = 1, thus this is equal to 1 + sum(i!=j)
+    denominator = exp.sum(axis=0)
+
+    weights = numerator / denominator
+
+    # Normalize weights
+    weights /= weights.sum()
+
+    return weights
 
 
 def visualize_weighting():
@@ -203,7 +161,8 @@ def read_metadata(cfg, projects: list) -> dict:
     return d
 
 
-def read_input_data(datasets: list, dim: str='data_ensemble') -> 'xr.DataArray':
+def read_input_data(datasets: list,
+                    dim: str = 'data_ensemble') -> 'xr.DataArray':
     """Loads data from metadata.
 
     Read the input data from the list of given data sets. `datasets` is a list
@@ -233,7 +192,8 @@ def read_observation_data(datasets: list) -> 'xr.DataArray':
     return read_input_data(datasets, dim='obs_ensemble')
 
 
-def aggregate_obs_data(data_array: 'xr.DataArray', operator: str='median') -> 'xr.DataArray':
+def aggregate_obs_data(data_array: 'xr.DataArray',
+                       operator: str = 'median') -> 'xr.DataArray':
     """Reduce data array along ensemble dimension.
 
     Apply the operator to squeeze the ensemble dimension by applying
@@ -259,13 +219,14 @@ def main(cfg):
     model_data_dict = {}
     obs_data_dict = {}
     independence_dict = {}
+    performance_dict = {}
 
     for variable, datasets in models.items():
         print(f'Reading model data for {variable}')
 
         model_data = read_model_data(datasets)
         model_data_dict[variable] = model_data
-    
+
     for variable, datasets in observations.items():
         print(f'Reading observation data for {variable}')
 
@@ -278,7 +239,7 @@ def main(cfg):
 
         independence = calculate_independence(model_data)
         independence_dict[variable] = independence
-        
+
         visualize_independence(independence)  # TODO
 
         print(independence.values)
@@ -291,16 +252,31 @@ def main(cfg):
         obs_data = obs_data_dict[variable]
 
         performance = calculate_performance(model_data, obs_data)
+        performance_dict[variable] = performance
 
         visualize_performance(performance)  # TODO
 
         print(performance.values)
         print()
 
-    # independences = xr.concat(independences, dim='diagnostic')
+    for variable in variables:
+        print(f'Calculating weights for {variable}')
 
-    # weights = calculate_weights(independence, performance)
-    # visualize_weighting(weights)
+        performance = performance_dict[variable]
+        independence = independence_dict[variable]
+
+        sigma_performance = cfg['shape_params'][variable]['sigma_d']
+        sigma_independence = cfg['shape_params'][variable]['sigma_s']
+
+        weights = calculate_weights(performance, independence,
+                                    sigma_performance, sigma_independence)
+
+        print(weights.values)
+        print()
+
+        # visualize_weighting(weights)
+
+    # independences = xr.concat(independences, dim='diagnostic')
 
 
 if __name__ == '__main__':
