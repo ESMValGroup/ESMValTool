b69c25f45c5a1f7c315a1ec296365bce65378ae9
Stef <s.smeets@esciencecenter.nl>
2020-08-25 10:51:30 +0200

    Rewrite `weighted_quantiles` function
    Avoid potential licensing issues
    Update docstring to numpy standard

--- esmvaltool/diag_scripts/weighting/weighted_temperature_graph.py
+++ esmvaltool/diag_scripts/weighting/weighted_temperature_graph.py
@@ -25,45 +25,49 @@ def read_weights(filename: str):
     return weights
 
 
-def weighted_quantile(values,
-                      quantiles,
-                      sample_weight=None,
-                      values_sorted=False,
-                      old_style=False):
-    """Very close to numpy.percentile, but supports weights.
+def weighted_quantile(values: list,
+                      quantiles: list,
+                      weights: list = None) -> 'np.array':
+    """Calculate weighted quantiles.
 
-    source: https://stackoverflow.com/a/29677616/6012085
+    Analogous to np.quantile, but supports weights.
 
-    NOTE: quantiles should be in [0, 1]!
-    :param values: numpy.array with data
-    :param quantiles: array-like with many quantiles needed
-    :param sample_weight: array-like of the same length as `array`
-    :param values_sorted: bool, if True, then will avoid sorting of
-        initial array
-    :param old_style: if True, will correct output to be consistent
-        with numpy.percentile.
-    :return: numpy.array with computed quantiles.
+    Based on: https://stackoverflow.com/a/29677616/6012085
+
+    Parameters
+    ----------
+    values: array_like
+        List of input values.
+    quantiles: array_like
+        List of quantiles between 0.0 and 1.0.
+    weights: array_like
+        List with same length as `values` containing the weights.
+
+    Returns
+    -------
+    np.array
+        Numpy array with computed quantiles.
     """
     values = np.array(values)
     quantiles = np.array(quantiles)
-    if sample_weight is None:
-        sample_weight = np.ones(len(values))
-    sample_weight = np.array(sample_weight)
-    assert np.all(quantiles >= 0) and np.all(quantiles <= 1), \
-        'quantiles should be in [0, 1]'
+    if weights is None:
+        weights = np.ones(len(values))
+    weights = np.array(weights)
 
-    if not values_sorted:
-        sorter = np.argsort(values)
-        values = values[sorter]
-        sample_weight = sample_weight[sorter]
+    if not np.all((quantiles >= 0) & (quantiles <= 1)):
+        raise ValueError('Quantiles should be between 0.0 and 1.0')
 
-    weighted_quantiles = np.cumsum(sample_weight) - 0.5 * sample_weight
-    if old_style:
-        # To be convenient with numpy.percentile
-        weighted_quantiles -= weighted_quantiles[0]
-        weighted_quantiles /= weighted_quantiles[-1]
-    else:
-        weighted_quantiles /= np.sum(sample_weight)
+    idx = np.argsort(values)
+    values = values[idx]
+    weights = weights[idx]
+
+    weighted_quantiles = np.cumsum(weights) - 0.5 * weights
+
+    # Cast weighted quantiles to 0-1 To be consistent with np.quantile
+    min_val = weighted_quantiles.min()
+    max_val = weighted_quantiles.max()
+    weighted_quantiles = (weighted_quantiles - min_val) / max_val
+
     return np.interp(quantiles, weighted_quantiles, values)
 
 
