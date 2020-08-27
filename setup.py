ac63d3b05e328010a72f7b5317733b98471308ac
Stef <s.smeets@esciencecenter.nl>
2020-08-27 15:02:08 +0200

    Revert pinning isort to avoid version collisions
    Isort 4.x is required by prospector 1.3/pylint 2.5

--- setup.py
+++ setup.py
@@ -71,7 +71,7 @@ REQUIREMENTS = {
     'develop': [
         'autodocsumm!=0.2.0',
         'codespell',
-        'isort>=5.4',
+        'isort',
         'pre-commit',
         'prospector[with_pyroma]!=1.1.6.3,!=1.1.6.4',
         'sphinx>2',
