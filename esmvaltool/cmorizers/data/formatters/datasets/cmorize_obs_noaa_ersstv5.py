b2bf57d3d20bcd5c62c0e3dd2029235d97748ec8
Björn Brötz <bjoern.broetz@dlr.de>
2020-08-28 12:06:37 +0200

    Clean-up.

--- esmvaltool/cmorizers/data/formatters/datasets/cmorize_obs_noaa_ersstv5.py
+++ esmvaltool/cmorizers/data/formatters/datasets/cmorize_obs_noaa_ersstv5.py
@@ -1,5 +1,7 @@
-"""ESMValTool CMORizer for NOAA Extended Reconstructed Sea Surface
-   Temperature (ERSST), Version 5 data.
+"""ESMValTool CMORizer for NOAA ERSST data, version 5.
+
+   This is the CMORizer script for the NOAA Extended Reconstructed Sea Surface
+   Temperature (ERSST) data of version 5.
 
 Tier
     Tier 2: open dataset.
@@ -11,7 +13,29 @@ Last access
     20200520
 
 Download and processing instructions
-    TODO
+    The data is available via an ftp-server provided by NOAA. The script
+    below can be used to automate the process.
+
+.. code-block:: bash
+
+    #!/bin/env bash
+
+    url=ftp://ftp.ncdc.noaa.gov/pub/data/cmb/ersst/v5/netcdf
+    yr=1854
+    while [ $yr -le 2019 ]
+    do
+        nm=1
+        while [ $nm -le 12 ]
+        do
+            if [ $nm -lt 10 ]
+            then
+                nm=0$nm
+            fi
+            wget $url/ersst.v5.$yr$nm.nc
+            nm=$(expr $nm + 1)
+        done
+        yr=$(expr $yr + 1)
+    done
 
 """
 
@@ -19,7 +43,6 @@ import logging
 import os
 
 import iris
-from cf_units import Unit
 
 from . import utilities as utils
 
@@ -29,9 +52,9 @@ logger = logging.getLogger(__name__)
 def _fix_time_coord(cube):
     """Set time points to central day of month."""
     time_coord = cube.coord('time')
-    old_unit = time_coord[0].units
-    new_time = [d.replace(day=15) for d in old_unit.num2date(time_coord.points)]
-    time_coord.points = old_unit.date2num(new_time)
+    _unit = time_coord[0].units
+    new_time = [d.replace(day=15) for d in _unit.num2date(time_coord.points)]
+    time_coord.points = _unit.date2num(new_time)
 
 
 def _extract_variable(raw_var, cmor_info, attrs, filepath, out_dir):
@@ -41,9 +64,7 @@ def _extract_variable(raw_var, cmor_info, attrs, filepath, out_dir):
     cube = iris.util.squeeze(cube)
     _fix_time_coord(cube)
     utils.fix_var_metadata(cube, cmor_info)
-    #utils.fix_coords(cube)
     utils.set_global_atts(cube, attrs)
-    # utils.flip_dim_coord(cube, 'latitude')
     utils.save_variable(cube,
                         var,
                         out_dir,
