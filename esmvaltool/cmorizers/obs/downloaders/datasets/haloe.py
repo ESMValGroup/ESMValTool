ea4440b08ffdf5c9c7761579d67955886b13e2a8
Javier Vegas-Regidor <javier.vegas@bsc.es>
2020-09-01 09:20:47 +0200

    Add HALOE

---
+++ esmvaltool/cmorizers/obs/downloaders/datasets/haloe.py
@@ -0,0 +1,23 @@
+"""Script to download Duveiller2018 from its webpage."""
+import logging
+import os
+
+from esmvaltool.cmorizers.obs.downloaders.wget import WGetDownloader
+from esmvaltool.cmorizers.obs.utilities import unpack_files_in_folder
+
+logger = logging.getLogger(__name__)
+
+
+def download_dataset(config, dataset, start_date, end_date, overwrite):
+    downloader = WGetDownloader(
+        config=config,
+        dataset=dataset,
+        overwrite=overwrite,
+    )
+    downloader.tier = 2
+    downloader.download_file(
+        "https://acp.copernicus.org/articles/5/2797/2005/"
+        "acp-5-2797-2005-supplement.tar",
+        wget_options=[]
+    )
+    unpack_files_in_folder(downloader.local_folder)
