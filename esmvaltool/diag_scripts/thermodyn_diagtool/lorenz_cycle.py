e2949d73cfcda2642ccb8dc171867cb305ef5b22
ValerioLembo <valerio.lembo@uni-hamburg.de>
2020-09-04 10:29:25 +0200

    Test Peter - 1

--- esmvaltool/diag_scripts/thermodyn_diagtool/lorenz_cycle.py
+++ esmvaltool/diag_scripts/thermodyn_diagtool/lorenz_cycle.py
@@ -910,14 +910,26 @@ def preproc_lec(model, wdir, pdir, input_data):
                                 dataset=model)[0]['filename']
     tas_file = e.select_metadata(input_data, short_name='tas',
                                  dataset=model)[0]['filename']
-    ua_file = e.select_metadata(input_data, short_name='ua',
-                                dataset=model)[0]['filename']
+    #ua_file = e.select_metadata(input_data, short_name='ua',
+    #                            dataset=model)[1]['filename']
+    ua_files = e.select_metadata(input_data, short_name='ua', dataset=model)
+    if len(ua_files) > 1:
+        ua_files = e.select_metadata(ua_files, variable_group='ua_2')
+    ua_file = ua_files[0]    
     uas_file = e.select_metadata(input_data, short_name='uas',
                                  dataset=model)[0]['filename']
-    va_file = e.select_metadata(input_data, short_name='va',
-                                dataset=model)[0]['filename']
-    vas_file = e.select_metadata(input_data, short_name='vas',
-                                 dataset=model)[0]['filename']
+    va_files = e.select_metadata(input_data, short_name='va', dataset=model)
+    if len(va_files) > 1:
+        va_files = e.select_metadata(va_files, variable_group='va_2')
+    va_file = va_files[0]
+    #va_file = e.select_metadata(input_data, short_name='va',
+    #                            dataset=model)[1]['filename']
+    vas_files = e.select_metadata(input_data, short_name='vas', dataset=model)
+    if len(vas_files) > 1:
+        vas_files = e.select_metadata(vas_files, variable_group='vas_2')
+    vas_file = vas_files[0]
+    #vas_file = e.select_metadata(input_data, short_name='vas',
+    #                             dataset=model)[1]['filename']
     wap_file = e.select_metadata(input_data, short_name='wap',
                                  dataset=model)[0]['filename']
     ldir = os.path.join(pdir, 'LEC_results')
