24c0d55231af0c07078be65b762ff57ec4677fe1
Valeriu Predoi <valeriu.predoi@gmail.com>
2020-09-24 15:14:46 +0100

    more simplifications and optimization

--- esmvaltool/utils/recipe_filler.py
+++ esmvaltool/utils/recipe_filler.py
@@ -1,5 +1,6 @@
 import argparse
 import os
+import shutil
 from glob import glob
 
 import yaml
@@ -11,156 +12,113 @@ dataset_order = [
     'start_year', 'end_year'
 ]
 
+# cmip eras
+cmip_eras = ["CMIP5", "CMIP6"]
+
 # The base dictionairy (all wildcards):
 base_dict = {
-    #    '[institute]':     '*',
-    #    '[dataset]':     '*',
-    #    '[project]': '*',
-    #    '[exp]':     '*',
-    #    '[frequency]':    '*',
-    #    '[ensemble]':     '*',
-    #    '[mip]':     '*',
-    #    '[modeling_realm]':     '*',
-    #    '[short_name]': '*',
-    #    '[grid]':     '*',
-    #    '[latestversion]': 'latest',
-    #    '[start_year]': '*',
-    #    '[end_year]': '*',
-    '{institute}': '*',
-    '{dataset}': '*',
-    '{project}': '*',
-    '{exp}': '*',
-    '{frequency}': '*',
-    '{ensemble}': '*',
-    '{mip}': '*',
-    '{modeling_realm}': '*',
-    '{short_name}': '*',
-    '{grid}': '*',
-    '{latestversion}': 'latest',
-    '{start_year}': '*',
-    '{end_year}': '*',
-    '{activity}': '*',
-    #
+    'institute': '*',
+    'dataset': '*',
+    'project': '*',
+    'exp': '*',
+    'frequency': '*',
+    'ensemble': '*',
+    'mip': '*',
+    'modeling_realm': '*',
+    'short_name': '*',
+    'grid': '*',
+    'latestversion': 'latest',
+    'start_year': '*',
+    'end_year': '*',
+    'activity': '*',
 }
 
 
-def rm_brackets(key):
-    """
-    Removes [ and  ] from a string
-    """
-    return key.replace('[', '').replace(']', '').replace('{',
-                                                         '').replace('}', '')
-
-
-def add_brackets(key):
-    """
-    Add { and } from to string
-    """
-    return ''.join(['{', key, '}'])
-
-
-def get_site(CMIP='CMIP5'):
+def get_site_rootpath(cmip_era):
     """
     Get site (drs) from config-user.yml
     """
-    config_yml = './config-user.yml'
+    config_yml = get_args().config_file
     yamlconf = yaml.load(open(config_yml, 'r'))
-    return yamlconf['drs'][CMIP]
+    return yamlconf['drs'][cmip_era], yamlconf['rootpath'][cmip_era]
 
 
-def get_rootpath(CMIP='CMIP5'):
-    """
-    Get Basepath from config-user.yml
-    """
-    config_yml = './config-user.yml'
-    yamlconf = yaml.load(open(config_yml, 'r'))
-    return yamlconf['rootpath'][CMIP]
-
-
-def get_input_dir(CMIP='CMIP5'):
+def get_input_dir(cmip_era):
     """
     Get input_dir from config-developer.yml
     """
-    site = get_site(CMIP)
+    site = get_site_rootpath(cmip_era)[0]
     yamlconf = read_config_developer_file()
-    return yamlconf[CMIP]['input_dir'][site]
+    return yamlconf[cmip_era]['input_dir'][site]
 
 
-def get_input_file(CMIP='CMIP5'):
+def get_input_file(cmip_era):
     """
     Get input_file from config-developer.yml
     """
     yamlconf = read_config_developer_file()
-    return yamlconf[CMIP]['input_file']
+    return yamlconf[cmip_era]['input_file']
 
 
-def determine_basepath(CMIP='CMIP5'):
+def determine_basepath(cmip_era):
     """
     Determine a basepath
     """
-    basepath = '/'.join([
-        get_rootpath(CMIP=CMIP),
-        get_input_dir(CMIP=CMIP),
-        get_input_file(CMIP=CMIP)
-    ])
+    basepath = os.path.join(
+        get_site_rootpath(cmip_era)[1], get_input_dir(cmip_era),
+        get_input_file(cmip_era))
     while basepath.find('//') > -1:
         basepath = basepath.replace('//', '/')
+
     return basepath
 
 
-def list_all_files(file_dict, CMIP, debug=True):
+def list_all_files(file_dict, cmip_era):
     """
     List all files that match the dataset dictionairy.
     """
-    basepath = determine_basepath(CMIP=CMIP)
-    mip = file_dict['{mip}']
-    short_name = file_dict['{short_name}']
-    frequency = CMOR_TABLES['CMIP5'].get_variable(mip, short_name).frequency
-    realm = CMOR_TABLES['CMIP5'].get_variable(mip, short_name).modeling_realm
-    file_dict['{frequency}'] = frequency
-    if len(realm) == 1:
-        file_dict['{modeling_realm}'] = realm[0]
-
-    # load all the files in the custom dict:
+    basepath = determine_basepath(cmip_era)
+    mip = file_dict['mip']
+    short_name = file_dict['short_name']
+    frequency = CMOR_TABLES[cmip_era].get_variable(mip, short_name).frequency
+    realms = CMOR_TABLES[cmip_era].get_variable(mip, short_name).modeling_realm
+    file_dict['frequency'] = frequency
     new_path = basepath[:]
-    for key, value in file_dict.items():
-        new_path = new_path.replace(key, str(value))
 
-    # Globs all the wildcards into a list of files.
-    files = glob(new_path)
-    return files
+    # could have multiple realms
+    all_files = []
+    for realm in realms:
+        file_dict['modeling_realm'] = realm
+
+        # load all the files in the custom dict
+        for key, value in file_dict.items():
+            new_path = new_path.replace('{' + key + '}', str(value))
+
+        # Globs all the wildcards into a list of files.
+        files = glob(new_path)
+        all_files.extend(files)
+
+    return all_files
 
 
-def get_time_range(fn_key):
-    """
-    Get a time range in years from the file name.
-    """
-    fn_key = fn_key.replace('.nc', '')
-    start_time, end_time = fn_key.split('-')
-    return start_time[:4], end_time[:4]
-
-
-def file_to_recipe_dataset(fn, CMIP, file_dict):
+def file_to_recipe_dataset(fn, cmip_era, file_dict):
     """
     Converts a filename to an recipe ready dataset.
     """
     # Add the obvious ones - ie the one you requested!
     output_dataset = {}
-    output_dataset['project'] = CMIP
+    output_dataset['project'] = cmip_era
     for key, value in file_dict.items():
         if value == '*':
             continue
-        key = rm_brackets(key)
         if key in dataset_order:
             output_dataset[key] = value
 
     # Split file name and base path into directory structure and filenames.
-    basepath, basefile = os.path.split(determine_basepath(CMIP=CMIP))
+    basepath, basefile = os.path.split(determine_basepath(cmip_era))
     fnpath, fnfile = os.path.split(fn)
-
-    # Assume basepath is separated by '/'
-    basepath_split = basepath.split('/')
-    fnpath_split = fnpath.split('/')
+    basepath_split = basepath.split(os.sep)
+    fnpath_split = fnpath.split(os.sep)
 
     # Check lengths of split lists.
     if len(basepath_split) != len(fnpath_split):
@@ -168,7 +126,6 @@ def file_to_recipe_dataset(fn, CMIP, file_dict):
 
     # iterate through directory structure looking for useful bits.
     for base_key, fn_key in zip(basepath_split, fnpath_split):
-        base_key = rm_brackets(base_key)
         if base_key not in dataset_order:
             continue
         output_dataset[base_key] = fn_key
@@ -179,7 +136,8 @@ def file_to_recipe_dataset(fn, CMIP, file_dict):
     basefile = basefile.replace('end_year', 'endyear')
 
     # Assume filename is separated by '_'
-    basefile_split = basefile.split('_')
+    basefile_split = [key.replace("{", "") for key in basefile.split('_')]
+    basefile_split = [key.replace("}", "") for key in basefile_split]
     fnfile_split = fnfile.split('_')
 
     # Check lengths of split lists.
@@ -188,42 +146,31 @@ def file_to_recipe_dataset(fn, CMIP, file_dict):
 
     # iterate through directory structure looking for useful bits.
     for base_key, fn_key in zip(basefile_split, fnfile_split):
-        base_key = rm_brackets(base_key)
-        # Check to make sure you're not adding a CMIP5 dataset into a CMIP6.
-        if base_key == 'ensemble':
-            if CMIP == 'CMIP5' and len(fn_key) != 6:
-                return None
-            if CMIP == 'CMIP6' and len(fn_key) != 8:
-                return None
-
-        # Special case for start end time
-
         if base_key == '*.nc':
-            start_year, end_year = get_time_range(fn_key)
+            fn_key = fn_key.replace('.nc', '')
+            start_year, end_year = fn_key.split('-')
             output_dataset['start_year'] = start_year
             output_dataset['end_year'] = end_year
-            assert 0
-
-        output_dataset[base_key] = fn_key
+        elif base_key == "ensemble*.nc":
+            output_dataset['ensemble'] = fn_key
+        elif base_key not in ["shortname", "ensemble*.nc", "*.nc"]:
+            output_dataset[base_key] = fn_key
 
     return output_dataset
 
 
-def condense_times(add_datasets):
+def remove_duplicates(add_datasets):
     """
     Condense the files to start_year and end_year
     Takes a list of fn:outs and joins together the ones with matching times
     """
     datasets = []
     seen = set()
-    new_l = []
-    for d in add_datasets:
-        d.pop("shortname", None)
-        d.pop("ensemble*.nc", None)
-        t = tuple(d.items())
-        if t not in seen:
-            seen.add(t)
-            datasets.append(d)
+    for dataset in add_datasets:
+        tup_dat = tuple(dataset.items())
+        if tup_dat not in seen:
+            seen.add(tup_dat)
+            datasets.append(dataset)
 
     return datasets
 
@@ -264,29 +211,29 @@ def parse_recipe_to_dicts(recipe):
             new_dict = base_dict.copy()
             # for each key in the variable:
             for var_key, var_value in var_dict.items():
-                var_key = add_brackets(var_key)
                 if var_key in new_dict:
                     new_dict[var_key] = var_value
             output_dicts[(diag, variable)] = new_dict
     return output_dicts
 
 
-def add_datasets_into_recipe(additional_datasets, recipe):
+def add_datasets_into_recipe(additional_datasets, output_recipe):
     """
     Add the datasets into a new recipe.
 
     """
-    with open(recipe, 'r') as yamlfile:
+    with open(output_recipe, 'r') as yamlfile:
         cur_yaml = yaml.safe_load(yamlfile)
         for diag_var, add_dat in additional_datasets.items():
-            if 'additional_datasets' in cur_yaml['diagnostics']:
-                cur_yaml['diagnostics'][
-                    diag_var[0]]['additional_datasets'].extend(add_dat)
-            else:
-                cur_yaml['diagnostics'][
-                    diag_var[0]]['additional_datasets'] = add_dat
+            if add_dat and add_dat not in cur_yaml['datasets']:
+                if 'additional_datasets' in cur_yaml['diagnostics']:
+                    cur_yaml['diagnostics'][
+                        diag_var[0]]['additional_datasets'].extend(add_dat)
+                else:
+                    cur_yaml['diagnostics'][
+                        diag_var[0]]['additional_datasets'] = add_dat
     if cur_yaml:
-        with open(recipe, 'w') as yamlfile:
+        with open(output_recipe, 'w') as yamlfile:
             yaml.safe_dump(cur_yaml, yamlfile)
 
 
@@ -306,28 +253,31 @@ def run():
     """
     # Get arguments
     args = get_args()
-    recipe = args.recipe
+    input_recipe = args.recipe
+    output_recipe = args.output
 
     # Convert the recipe into a yaml dict.
-    recipe_dicts = parse_recipe_to_dicts(recipe)
+    recipe_dicts = parse_recipe_to_dicts(input_recipe)
 
     # Create a list of additional_datasets for each diagnostic/variable.
     additional_datasets = {}
     for (diag, variable), recipe_dict in recipe_dicts.items():
         files_recipe_dicts = {}
-        recipe_dict['{short_name}'] = variable
-        for CMIP in ['CMIP5']:
-            files = list_all_files(recipe_dict, CMIP)
+        recipe_dict['short_name'] = variable
+        for cmip_era in cmip_eras:
+            files = list_all_files(recipe_dict, cmip_era)
             add_datasets = []
             for fn in sorted(files):
-                out = file_to_recipe_dataset(fn, CMIP, recipe_dict)
+                out = file_to_recipe_dataset(fn, cmip_era, recipe_dict)
                 if out is None:
                     continue
                 add_datasets.append(out)
-        additional_datasets[(diag, variable)] = condense_times(add_datasets)
+            additional_datasets[(diag, variable, cmip_era)] = \
+                remove_duplicates(add_datasets)
 
     # add datasets to recipe as additional_datasets
-    add_datasets_into_recipe(additional_datasets, recipe)
+    shutil.copyfile(input_recipe, output_recipe, follow_symlinks=True)
+    add_datasets_into_recipe(additional_datasets, output_recipe)
 
 
 if __name__ == "__main__":
