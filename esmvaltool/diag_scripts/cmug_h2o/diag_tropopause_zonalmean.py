2535f146fc29da3d6215e06c9c444d2a0137e3ca
“katjaweigel” <weigel@iup.physik.uni-bremen.de>
2020-10-06 21:53:03 +0200

    Updates for prospector.

--- esmvaltool/diag_scripts/cmug_h2o/diag_tropopause_zonalmean.py
+++ esmvaltool/diag_scripts/cmug_h2o/diag_tropopause_zonalmean.py
@@ -40,6 +40,44 @@ from esmvaltool.diag_scripts.shared._base import (
 logger = logging.getLogger(os.path.basename(__file__))
 
 
+def _get_data_for_agg(new_svarcube, new_tacube):
+    """Reshape data for use in iris aggregator based on two cubes."""
+    dims_to_collapse = set()
+    dims_to_collapse.update(new_svarcube.coord_dims('air_pressure'))
+    untouched_dims = set(range(new_svarcube.ndim)) -\
+        set(dims_to_collapse)
+    dims = list(untouched_dims) + list(dims_to_collapse)
+    unrolled_data = np.moveaxis(new_tacube.data, dims,
+                                range(new_svarcube.ndim))
+    return unrolled_data
+
+
+def _get_range_and_pstring(variable, mean_cube, tropopause=False):
+    """Get range for color bar and print string."""
+    if variable == "Air Temperature":
+        print_var = "Temperature [K]"
+        set_range = np.linspace(180, 230, 21)
+    elif variable == "Geopotential Height":
+        print_var = "Geopotential Height [m]"
+        set_range = np.linspace(8000, 22000, 25)
+    elif variable == "Relative Humidity":
+        print_var = "Relative Humidity [%]"
+        set_range = np.linspace(0, 100, 21)
+    elif variable == "Specific Humidity":
+        print_var = "Specific Humidity [kg/kg]"
+        if tropopause:
+            set_range = np.linspace(0.1e-5, 2.5e-5, 25)
+        else:
+            logval = np.log(np.array([1e-6, 1e-5]))
+            set_range = np.exp(np.linspace(logval[0], logval[1], 41))
+    else:
+        print_var = mean_cube.long_name
+        set_range = np.linspace(np.nanmin(mean_cube.data),
+                                np.nanmax(mean_cube.data), 21)
+
+    return {'print_var': print_var, 'set_range': set_range}
+
+
 def _get_sel_files(cfg, dataname, dim=2):
     """Get filenames from cfg for single models or multi-model mean."""
     selection = []
@@ -65,7 +103,7 @@ def _get_sel_files_var(cfg, varnames):
     return selection
 
 
-def _get_sel_files_lvardata(cfg, dataname, lvarnames):
+def _get_sel_lvardata(cfg, dataname, lvarnames):
     """Get filenames from cfg for one model and differen variable(s)."""
     selection = []
 
@@ -75,6 +113,18 @@ def _get_sel_files_lvardata(cfg, dataname, lvarnames):
             selection.append(hlp['filename'])
 
     return selection
+
+
+def _read_data(attributes, svar):
+    """Read data for ta and other variabe from files."""
+    input_file_svar = attributes['filename'].replace('/ta/', '/' + svar + '/')
+    input_file_svar = input_file_svar.replace('_ta_', '_' + svar + '_')
+
+    logger.debug("Loading %s", input_file_svar)
+    svarcube = iris.load_cube(input_file_svar)
+    svarcube = svarcube.collapsed('longitude', iris.analysis.MEAN)
+
+    return svarcube
 
 
 def cube_to_save_profile(var1, var2, names):
@@ -123,6 +173,61 @@ def get_provenance_record(ancestor_files, caption, statistics,
     return record
 
 
+def get_prof_and_plt_data(cfg, data, available_vars_min_tas):
+    """Plot data for singe data sets and get profile for each."""
+    profiles = {}
+    for svar in available_vars_min_tas:
+        profiles[svar] = {}
+
+    # Make an iris aggregator to find value based on minimum different cube.
+    min_pos = Aggregator('ag_find_min', find_min,
+                         units_func=lambda units: 1)
+
+    # interpolate to dense grid, use equal dist points in log(p)
+    logpr = np.log(np.array([25000, 2500]))
+    sample_points = [('air_pressure', np.exp(np.linspace(logpr[0], logpr[1],
+                                                         221)))]
+
+    for attributes in data['ta']:
+        logger.info("Processing dataset %s", attributes['dataset'])
+        dataset = attributes['dataset']
+
+        tacube = _read_data(attributes, 'ta')
+        new_tacube = tacube.interpolate(sample_points,
+                                        iris.analysis.Linear())
+        unrolled_data = _get_data_for_agg(new_tacube, new_tacube)
+        plot_zonal_timedev(cfg, new_tacube.collapsed('air_pressure',
+                                                     min_pos,
+                                                     data_min=unrolled_data),
+                           dataset + " Cold point tropopause ",
+                           "Air Temperature")
+
+        for svar in available_vars_min_tas:
+            svarcube = _read_data(attributes, svar)
+
+            profiles[svar][dataset] = svarcube.collapsed(['time', 'latitude'],
+                                                         iris.analysis.MEAN)
+
+            plot_zonal_mean(cfg, svarcube.collapsed(['time'],
+                                                    iris.analysis.MEAN),
+                            dataset, "Zonal mean ",
+                            svarcube.long_name)
+
+            new_svarcube = svarcube.interpolate(sample_points,
+                                                iris.analysis.Linear())
+
+            unrolled_data = _get_data_for_agg(new_svarcube, new_tacube)
+
+            plot_zonal_timedev(cfg,
+                               new_svarcube.collapsed('air_pressure',
+                                                      min_pos,
+                                                      data_min=unrolled_data),
+                               dataset + " Cold point tropopause ",
+                               new_svarcube.long_name)
+
+    return profiles
+
+
 def plot_zonal_mean(cfg, mean_cube, dataname, titlestr, variable):
     """Plot zonal mean contour."""
     # if not cfg[n.WRITE_PLOTS]:
@@ -132,29 +237,14 @@ def plot_zonal_mean(cfg, mean_cube, dataname, titlestr, variable):
     # create figure and axes instances
     fig, axx = plt.subplots(figsize=(7, 5))
 
-    if variable == "Air Temperature":
-        print_var = "Temperature [K]"
-        set_range = np.linspace(180, 230, 21)
-    elif variable == "Geopotential Height":
-        print_var = "Geopotential Height [m]"
-        set_range = np.linspace(10000, 20000, 21)
-    elif variable == "Relative Humidity":
-        print_var = "Relative Humidity [%]"
-        set_range = np.linspace(0, 100, 21)
-    elif variable == "Specific Humidity":
-        print_var = "Specific Humidity [kg/kg]"
-        logval = np.log(np.array([1e-6, 1e-5]))
-        set_range = np.exp(np.linspace(logval[0], logval[1], 41))
-    else:
-        print_var = mean_cube.long_name
-        set_range = np.linspace(np.nanmin(mean_cube.data),
-                                np.nanmax(mean_cube.data), 21)
+    rps = _get_range_and_pstring(variable, mean_cube)
+
     # draw filled contours
     cnplot = plt.contourf(
         mean_cube.coord('latitude').points,
         mean_cube.coord('air_pressure').points / 100.0,
         mean_cube.data,
-        set_range,
+        rps['set_range'],
         cmap='jet',
         # cmap='RdBu_r',
         extend='both')
@@ -166,15 +256,11 @@ def plot_zonal_mean(cfg, mean_cube, dataname, titlestr, variable):
     # add colorbar
     cbar = fig.colorbar(cnplot, ax=axx, shrink=0.8)
     # add colorbar title string
-    cbar.set_label(print_var)
+    cbar.set_label(rps['print_var'])
 
     axx.set_ylabel('Pressure [hPa]')
     axx.set_xlabel('Latitude')
     axx.set_title(titlestr + variable)
-    # axx.set_xticks([40, 65, 90, 115])
-    # axx.set_xticklabels(['40°E', '65°E', '90°E', '115°E'])
-    # axx.set_yticks([-10, 0, 10, 20, 30])
-    # axx.set_yticklabels(['10°S', '0°', '10°N', '20°N', '30°N'])
 
     fig.tight_layout()
 
@@ -185,10 +271,10 @@ def plot_zonal_mean(cfg, mean_cube, dataname, titlestr, variable):
               variable.replace(" ", "_")
     fig.savefig(get_plot_filename(figname, cfg), dpi=300)
     plt.close()
-#
-    selection = _get_sel_files_lvardata(cfg, dataname, [variable])
-#
-    provenance_record = get_provenance_record(selection,
+
+    provenance_record = get_provenance_record(_get_sel_lvardata(cfg,
+                                                                dataname,
+                                                                [variable]),
                                               caption, ['mean'], ['global'])
 #
     diagnostic_file = get_diagnostic_filename(figname, cfg)
@@ -212,39 +298,19 @@ def plot_zonal_timedev(cfg, mean_cube, titlestr, variable):
     # create figure and axes instances
     fig, axx = plt.subplots(figsize=(7, 12))
 
-    if variable == "Air Temperature":
-        print_var = "Temperature [K]"
-        set_range = np.linspace(180, 230, 21)
-    elif variable == "Geopotential Height":
-        print_var = "Geopotential Height [m]"
-        set_range = np.linspace(10000, 20000, 21)
-    elif variable == "Relative Humidity":
-        print_var = "Relative Humidity [%]"
-        set_range = np.linspace(0, 100, 21)
-    elif variable == "Specific Humidity":
-        print_var = "Specific Humidity [kg/kg]"
-        set_range = np.linspace(0.1e-5, 2.5e-5, 25)
-    else:
-        print_var = mean_cube.long_name
-        set_range = np.linspace(np.nanmin(mean_cube.data),
-                                np.nanmax(mean_cube.data), 21)
-    print("mean_cube.units")
-    print(mean_cube.units)
-    # draw filled contours
-    print("mean_cube.data.shape")
-    print(mean_cube.data.shape)
+    rps = _get_range_and_pstring(variable, mean_cube, tropopause=True)
+
     iris.coord_categorisation.add_year(mean_cube, 'time', name='year')
     iris.coord_categorisation.add_month_number(mean_cube, 'time',
                                                name='month_number')
-    print("mean_cube.coord('year').points")
-    print(mean_cube.coord('year').points)
+
     # Adjust (ncdf) time to the format matplotlib expects
     cnplot = plt.contourf(
         mean_cube.coord('latitude').points,
         mean_cube.coord('year').points +
         (mean_cube.coord('month_number').points - 1.0) / 12.0,
         mean_cube.data,
-        set_range,
+        rps['set_range'],
         cmap='rainbow',
         # cmap='RdBu_r',
         extend='both')
@@ -252,7 +318,7 @@ def plot_zonal_timedev(cfg, mean_cube, titlestr, variable):
     # add colorbar
     cbar = fig.colorbar(cnplot, ax=axx, shrink=0.8)
     # add colorbar title string
-    cbar.set_label(print_var)
+    cbar.set_label(rps['print_var'])
 
     axx.set_ylabel('Time')
     axx.set_xlabel('Latitude')
@@ -291,10 +357,10 @@ def plot_profiles(cfg, profiles, available_vars_min_tas, available_datasets):
 
         axx.set_ylabel('Pressure [hPa]')
         axx.set_ylim(250, 1)
-        axx.set_xlim(0, 1e-4)
         onedat = profiles[svar][available_datasets[0]]
         if onedat.long_name == "Specific Humidity":
             unitstr = "kg/kg"
+            axx.set_xlim(0, 1e-4)
         else:
             unitstr = str(onedat.units)
 
@@ -320,9 +386,6 @@ def main(cfg):
     logging.debug("Found variables in recipe:\n%s", available_vars)
     available_vars_min_tas = deepcopy(available_vars)
     available_vars_min_tas.remove('ta')
-    # Make an iris aggregator to find value based on minimum different cube.
-    min_pos = Aggregator('ag_find_min', find_min,
-                         units_func=lambda units: 1)
     # Get input data
     data = {}
     for varname in available_vars:
@@ -330,73 +393,9 @@ def main(cfg):
                                         short_name=varname)
         data[varname] = sorted_metadata(data[varname], sort='dataset')
 
-    profiles = {}
-    for svar in available_vars_min_tas:
-        profiles[svar] = {}
-
-    for attributes in data['ta']:
-        logger.info("Processing dataset %s", attributes['dataset'])
-        dataset = attributes['dataset']
-        for svar in available_vars_min_tas:
-            input_file_svar = attributes['filename'].replace('/ta/',
-                                                             '/' + svar + '/')
-            input_file_svar = input_file_svar.replace('_ta_', '_' + svar + '_')
-
-            logger.debug("Loading %s", input_file_svar)
-            svarcube = iris.load_cube(input_file_svar)
-            svarcube = svarcube.collapsed('longitude', iris.analysis.MEAN)
-            profiles[svar][dataset] = svarcube.collapsed(['time', 'latitude'],
-                                                         iris.analysis.MEAN)
-            logger.debug("Loading %s", attributes['filename'])
-            tacube = iris.load_cube(attributes['filename'])
-            tacube = tacube.collapsed('longitude', iris.analysis.MEAN)
-            # plev = tacube.coord('air_pressure').points
-            plot_zonal_mean(cfg, svarcube.collapsed(['time'],
-                                                    iris.analysis.MEAN),
-                            dataset, "Zonal mean ",
-                            svarcube.long_name)
-
-            # interpolate to dense grid, use equal dist points in log(p)
-            logpr = np.log(np.array([25000, 2500]))
-            sample_points = [('air_pressure', np.exp(np.linspace(logpr[0],
-                                                                 logpr[1],
-                                                                 221)))]
-
-            new_tacube = tacube.interpolate(sample_points,
-                                            iris.analysis.Linear())
-            new_svarcube = svarcube.interpolate(sample_points,
-                                                iris.analysis.Linear())
-
-            dims_to_collapse = set()
-            dims_to_collapse.update(new_svarcube.coord_dims('air_pressure'))
-            untouched_dims = set(range(new_svarcube.ndim)) -\
-                set(dims_to_collapse)
-            dims = list(untouched_dims) + list(dims_to_collapse)
-            unrolled_data = np.moveaxis(new_tacube.data, dims,
-                                        range(new_svarcube.ndim))
-
-            trop_svar = new_svarcube.collapsed('air_pressure', min_pos,
-                                               data_min=unrolled_data)
-            plot_zonal_timedev(cfg, trop_svar,
-                               dataset + " Cold point tropopause ",
-                               new_svarcube.long_name)
-
-        trop_ta = new_tacube.collapsed('air_pressure', min_pos,
-                                       data_min=unrolled_data)
-
-        plot_zonal_timedev(cfg, trop_ta,
-                           dataset + " Cold point tropopause ",
-                           "Air Temperature")
-    print("profiles")
-    print(profiles)
+    profiles = get_prof_and_plt_data(cfg, data, available_vars_min_tas)
 
     plot_profiles(cfg, profiles, available_vars_min_tas, available_datasets)
-#        plot_tp_map(cfg, trop_hus.collapsed('time', iris.analysis.MEAN),
-#                    dataset + " Cold point tropopause specific humidity",
-#                    "Specific humidity [kg/kg]")
-#        plot_tp_map(cfg, trop_hur.collapsed('time', iris.analysis.MEAN),
-#                    dataset + " Cold point tropopause relative humidity",
-#                    "Relative humidity [%]")
 
 
 if __name__ == '__main__':
