cdc3e4328b40967287be29f9bab0a9ebebb21e72
SarahAlidoost <f.alidoost@esciencecenter.nl>
2020-10-06 17:41:03 +0200

    refactor the main function

--- esmvaltool/diag_scripts/hydrology/globwat.py
+++ esmvaltool/diag_scripts/hydrology/globwat.py
@@ -15,7 +15,7 @@ import iris.analysis
 import numpy as np
 
 from esmvaltool.diag_scripts.hydrology.derive_evspsblpot import debruin_pet
-from esmvaltool.diag_scripts.shared import (group_metadata, run_diagnostic)
+from esmvaltool.diag_scripts.shared import (ProvenanceLogger, group_metadata, run_diagnostic)
 
 
 logger = logging.getLogger(Path(__file__).name)
@@ -71,13 +71,18 @@ def get_input_cubes(metadata):
         cube = iris.load_cube(filename)
         cube.data.set_fill_value(-9999)
         all_vars[short_name] = change_data_type(cube)
+
+        # convert unit of pr and pet
+        for var in ['pr', 'pet']:
+            _convert_units(all_vars[var], time_step)
         provenance['ancestors'].append(filename)
     return all_vars, provenance, time_step
 
 
-def load_target(filename):
+def load_target(cfg):
     """Load target grid."""
     logger.info("Reading digital elevation model from %s", filename)
+    filename = Path(cfg['auxiliary_data_dir']) / cfg['target_file']
     if filename.suffix.lower() == '.nc':
         cube = iris.load_cube(str(filename))
     elif filename.suffix.lower() == '.map':
@@ -106,46 +111,26 @@ def get_month_day_for_output_name(cube):
         nday = calendar.monthrange(year,int(n_month))[1]
         for daynumber in range(1, nday+1):
             days.append(str(n_month) + str(daynumber).zfill(2))
-    return months ,days
+    return months , days
 
 
 def make_output_name(cube):
     """Get output file name, specific to Globwat."""
-    monthly_pr = []
-    daily_pr = []
-    monthly_pet = []
-    daily_pet = []
-    monthly_pet_arora = []
-    daily_pet_arora = []
+    monthly = []
+    daily = []
     output_name = {'pr':{'Amon':{}, 'day':{}},
                    'pet':{'Amon':{}, 'day':{}},
                    'pet_arora':{'Amon':{}, 'day':{}}}
     months , days = get_month_day_for_output_name(cube)
-    for mip in 'Amon', 'day':
-        if mip == 'Amon':
-            for i in range(0, len(months)):
-                for shortname in ['pr', 'pet', 'pet_arora']:
-                    if shortname == 'pr':
-                        monthly_pr.append('prc'+ str(months[i]) + 'wb')
-                    elif shortname == 'pet':
-                        monthly_pet.append('eto'+ str(months[i]) + 'wb')
-                    else:
-                        monthly_pet_arora.append('eto_arora'+ str(months[i]) + 'wb')
-        elif mip == 'day':
-            for i in range(0, len(days)):
-                for shortname in ['pr', 'pet', 'pet_arora']:
-                    if shortname == 'pr':
-                        daily_pr.append('prc'+ str(days[i]) + 'wb')
-                    elif shortname == 'pet':
-                        daily_pet.append('eto'+ str(days[i]) + 'wb')
-                    else:
-                        daily_pet_arora.append('eto_arora'+ str(days[i]) + 'wb')
-    output_name['pr']['Amon'] = monthly_pr
-    output_name['pr']['day'] = daily_pr
-    output_name['pet']['Amon']  = monthly_pet
-    output_name['pet']['day'] = daily_pet
-    output_name['pet_arora']['Amon']  = monthly_pet_arora
-    output_name['pet_arora']['day']  = daily_pet_arora
+
+    names = ['prc', 'eto', 'eto_arora']
+    for shortname, name in zip(output_name.keys(), names):
+        for month in months:
+            monthly.append(name + str(month) + 'wb')
+        for day in days:
+            daily.append(name + str(day) + 'wb')
+        output_name[shortname]['Amon'] = monthly
+        output_name[shortname]['day'] = daily
     return output_name
 
 
@@ -172,6 +157,59 @@ def arora_pet(tas):
     return arora_pet
 
 
+def _convert_units(cube, time_step):
+    """Convert unit of cube."""
+
+    cube.units = cube.units / 'kg m-3'
+    cube.data = cube.core_data() / 1000.
+
+    # Unit conversion of pr and pet from 'kg m-2 s-1' to 'mm month-1'
+    if time_step['mip'] == 'Amon':
+        cube.convert_units('mm month-1')
+
+    # Unit conversion of pr and pet from 'kg m-2 s-1' to 'mm day-1'
+    elif time_step['mip'] == 'day':
+        cube.convert_units('mm day-1')
+    return cube
+
+def get_cube_info(cube):
+    """"""
+    coord_time = cube.coord('time')
+    start_year = coord_time.cell(0).point.year
+    end_year = coord_time.cell(-1).point.year
+    return start_year, end_year
+
+#TODO: We must work on saving data as ascii format.
+def save_ascii(cube, filename):
+    """"""
+    # array = sub_cube.data
+    # points = sub_cube.coord('longitude').points
+    # sub_cube.coord('longitude').points = (points + 180) % 360 - 180
+    # lon = (sub_cube.coord('longitude').points + 180) % 360 -180
+    # lat = sub_cube.coord('latitude').points
+    # sub_cube = sub_cube[:, ::-1, ...]
+    # nrows,ncols = np.shape(array)
+    # xmin,ymin,xmax,ymax = [lon.min(),lat.min(),lon.max(),lat.max()]
+    # xres = (xmax-xmin)/float(ncols)
+    # yres = (ymax-ymin)/float(nrows)
+    # transform = Affine.translation(xmin + xres / 2, ymin - xres / 2) * Affine.scale(xres, xres)
+    # file_name = output_name_amon[i]
+    # new_dataset = rasterio.open(
+    # f"{data_dir}/{file_name}.asc",
+    # 'w',
+    # driver='GTiff',
+    # height=array.shape[1],
+    # width=array.shape[0],
+    # count=1,
+    # dtype='float32',
+    # crs='+proj=latlong',
+    # transform=transform,
+    # nodata= -9999,
+    # )
+    # new_dataset.write(array, 1)
+    # new_dataset.close()
+
+
 def main(cfg):
     """Process data for GlobWat hydrological model.
 
@@ -187,11 +225,10 @@ def main(cfg):
         all_vars, provenance, time_step = get_input_cubes(metadata)
 
         # load target grid for using in regriding function
-        target_cube_path = Path(cfg['auxiliary_data_dir']) / cfg['target_file']
-        target_cube = load_target(target_cube_path)
+        target_cube = load_target(cfg)
 
-        cube = all_vars['pr']
-        logger.info("Potential evapotransporation not available, deriving and adding to all_vars dictionary")
+
+        logger.info("Calculation PET uisng debruin method")
         all_vars.update(pet = debruin_pet(
             psl=all_vars['psl'],
             rsds=all_vars['rsds'],
@@ -199,98 +236,35 @@ def main(cfg):
             tas=all_vars['tas'],
         ))
 
-        #Take tas values to calculate pet_arora
-        tas = all_vars['tas']
-        all_vars.update(pet_arora = arora_pet(tas))
-        logger.info("Converting units")
-        pr = all_vars['pr']
-        pet = all_vars['pet']
-        pr.units = pr.units / 'kg m-3'
-        pr.data = pr.core_data() / 1000.
-        pet.units = pet.units / 'kg m-3'
-        pet.data = pet.core_data() /1000.
+        logger.info("Calculation PET uisng arora method")
+        all_vars.update(pet_arora = arora_pet(all_vars['tas']))
 
-        # Unit conversion of pr and pet from 'kg m-2 s-1' to 'mm month-1'
-        if time_step['mip'] == 'Amon':
-            pr.convert_units('mm month-1')
-            pet.convert_units('mm month-1')
+        output_name = make_output_name(all_vars['pr'])
+        start_year, end_year = get_cube_info(all_vars['pr'])
 
-        # Unit conversion of pr and pet from 'kg m-2 s-1' to 'mm day-1'
-        elif time_step['mip'] == 'day':
-            pr.convert_units('mm day-1')
-            pet.convert_units('mm day-1')
-
-        coord_time = cube.coord('time')
-        output_name = make_output_name(cube)
-        start_year = coord_time.cell(0).point.year
-        end_year = coord_time.cell(-1).point.year
-        for nyear in range (start_year , end_year+1):
+        for nyear in range(start_year , end_year+1):
             for key in ['pr', 'pet', 'pet_arora']:
-                if time_step['mip'] == 'Amon':
-                    output_name_amon = output_name[key]['Amon']
-                    data_dir = Path(f"{dataset}/{nyear}/{'Monthly'}")
+                for mip, freq in zip(['Amon', 'day'], ['Monthly', 'Daily']):
+                    file_names = output_name[key][mip]
+                    data_dir = Path(f"{dataset}/{nyear}/{freq}")
                     data_dir.mkdir(parents=True, exist_ok=True)
-                    for i in range(len(output_name_amon)):
-                        key_cube = all_vars[key]
-                        for sub_cube in key_cube.slices_over('time'):
+                    for file_name in file_names:
+                        cube = all_vars[key]
+                        for sub_cube in cube.slices_over('time'):
                             # set negative values for precipitaion to zero
                             if key == 'pr':
                                 sub_cube.data[(-9999<sub_cube.data) & (sub_cube.data<0)] = 0
                             #regrid data baed on target cube
                             sub_cube_regrided = sub_cube.regrid(target_cube, iris.analysis.Linear())
                             #Save data as netcdf
-                            file_name = output_name_amon[i]
-                            iris.save(sub_cube_regrided, data_dir / f"{file_name}.nc", fill_value=-9999)
+                            path = data_dir / f"{file_name}.nc"
+                            iris.save(sub_cube_regrided, path , fill_value=-9999)
 
-                            #TODO: We must work on saving data as ascii format.
-                            # array = sub_cube.data
-                            # points = sub_cube.coord('longitude').points
-                            # sub_cube.coord('longitude').points = (points + 180) % 360 - 180
-                            # lon = (sub_cube.coord('longitude').points + 180) % 360 -180
-                            # lat = sub_cube.coord('latitude').points
-                            # sub_cube = sub_cube[:, ::-1, ...]
-                            # nrows,ncols = np.shape(array)
-                            # xmin,ymin,xmax,ymax = [lon.min(),lat.min(),lon.max(),lat.max()]
-                            # xres = (xmax-xmin)/float(ncols)
-                            # yres = (ymax-ymin)/float(nrows)
-                            # transform = Affine.translation(xmin + xres / 2, ymin - xres / 2) * Affine.scale(xres, xres)
-                            # file_name = output_name_amon[i]
-                            # new_dataset = rasterio.open(
-                            # f"{data_dir}/{file_name}.asc",
-                            # 'w',
-                            # driver='GTiff',
-                            # height=array.shape[1],
-                            # width=array.shape[0],
-                            # count=1,
-                            # dtype='float32',
-                            # crs='+proj=latlong',
-                            # transform=transform,
-                            # nodata= -9999,
-                            # )
-                            # new_dataset.write(array, 1)
-                            # new_dataset.close()
-                else:
-                    #getting output names
-                    output_name_amon = output_name[key]['Amon']
-                    output_name_day = output_name[key]['day']
-                    #making output directory
-                    data_dir = Path(f"{dataset}/{nyear}/{'Daily'}")
-                    data_dir.mkdir(parents=True, exist_ok=True)
-                    #saving output
-                    for i in range(len(output_name_day)):
-                        key_cube = all_vars[key]
-                        for sub_cube in key_cube.slices_over('time'):
-                            # set negative values for precipitaion to zero
-                            if key == 'pr':
-                                sub_cube.data[(-9999<sub_cube.data) & (sub_cube.data<0)] = 0
+                            # save_ascii(sub_cube, path)
 
-                            sub_cube_regrided = sub_cube.regrid(target_cube, iris.analysis.Linear())
-                            file_name = output_name_day[i]
-                            iris.save(sub_cube, data_dir / f"{file_name}.nc", fill_value=-9999)
-
-            # # Store provenance
-            # with ProvenanceLogger(cfg) as provenance_logger:
-            #     provenance_logger.log(output_file, provenance)
+            # Store provenance
+            with ProvenanceLogger(cfg) as provenance_logger:
+                provenance_logger.log(path, provenance)
 
 
 if __name__ == '__main__':
