6ecb8a3d07a71cfaef228b77a8ac8ce1d904a028
morobking <rob.king@metoffice.gov.uk>
2020-11-12 16:06:33 +0000

    prospector run for both python scripts. lst.py still has issues as fixing them for prospector seems to stop the python actually working.

--- esmvaltool/cmorizers/obs/cmorize_obs_esacci_lst.py
+++ esmvaltool/cmorizers/obs/cmorize_obs_esacci_lst.py
@@ -1,6 +1,5 @@
 """ESMValTool CMORizer for ESACCI-LST data.
 
-
 Tier 2 # Is this the right tier????
 Source
    /group_workspaces/jasmin2/esacci_lst/public
@@ -18,70 +17,66 @@ Modification history
 """
 
 import datetime
-import glob
 import logging
-import os
 from calendar import monthrange
 
 import iris
 
 from . import utilities as utils
 
-logger = logging.getLogger(__name__) # from OC example, dont know what this does!
+logger = logging.getLogger(__name__)
 
-# this function started from OC example
-# This function is REQUIRED and it calls the other functions I write
+
 def cmorization(in_dir, out_dir, cfg, _):
     """Cmorization func call."""
-    
-    cmor_table = cfg['cmor_table']
-    glob_attrs = cfg['attributes'] # this is needed later, not sure how it works though
-    
+    # cmor_table = cfg['cmor_table']
+    glob_attrs = cfg['attributes']
+
     # run the cmorization
 
     # vals has the info from the yml file
     # var is set up in the yml file
     for var, vals in cfg['variables'].items():
-        # leave this loop in as might be useful in future for getting other info
+        # leave this loop in as might be useful in
+        # the future for getting other info
         # like uncertainty information from the original files
-        
-        var_info = cmor_table.get_variable(vals['mip'], var)
+
         glob_attrs['mip'] = vals['mip']
-        
-        for KEY in vals.keys():logger.info("%s %s" % (KEY,vals[KEY]))
-        
+
+        for key in vals.keys():
+            logger.info("%s %s" % (key, vals[key]))
+
         variable = vals['raw']
-        platform = 'MODISA' # not currently used
+        platform = 'MODISA'  # not currently used
 
         # loop over years and months
         # get years from start_year and end_year
         # note 2003 doesn't start until July so not included at this stage
-        for YEAR in range(vals['start_year'],vals['end_year']+1): 
+        for year in range(vals['start_year'], vals['end_year'] + 1):
             this_years_cubes = iris.cube.CubeList()
-            for month in range(12): # Change this in final version
-                MONTH = month + 1
-                logger.info(MONTH)
+            for month0 in range(12):  # Change this in final version
+                month = month0 + 1
+                logger.info(month)
                 day_cube, night_cube = load_cubes(in_dir,
                                                   vals['file_day'],
                                                   vals['file_night'],
-                                                  YEAR,
-                                                  MONTH,
+                                                  year,
+                                                  month,
                                                   variable,
                                                   platform
-                )
+                                                  )
 
-                monthly_cube = make_monthly_average(day_cube, night_cube, YEAR, MONTH)
-                # day_cube and night_cube not need anymore this loop
-                
+                monthly_cube = make_monthly_average(day_cube, night_cube,
+                                                    year, month)
+
                 # use CMORizer utils
                 monthly_cube = utils.fix_coords(monthly_cube)
-                
+
                 this_years_cubes.append(monthly_cube)
-                
-                # is there anything else needed for CMOR?????
-                
+
             # Use utils save
             # This seems to save files all with the same name!
+            # Fixed by making yearly files
             this_years_cubes = this_years_cubes.merge_cube()
             this_years_cubes.long_name = 'Surface Temperature'
             this_years_cubes.standard_name = 'surface_temperature'
@@ -93,38 +88,38 @@ def cmorization(in_dir, out_dir, cfg, _):
                 glob_attrs,
             )
 
-            
-def load_cubes(in_dir,file_day,file_night,YEAR,MONTH,variable,platform):
+
+def load_cubes(in_dir, file_day, file_night, year, month, variable, platform):
     """
+    Variable description.
+
     variable = land surface temperature
-    platform = AQUA not used for now but in place for future expansion to all ESC CCI LST plaforms
+    platform = AQUA not used for now
+               but in place for future expansion to all ESC CCI LST plaforms
     """
-    logger.info('%s/%s%s%02d*.nc' % (in_dir,file_day,YEAR,MONTH))
-    day_cube = iris.load_cube('%s/%s%s%02d*.nc' % (in_dir,file_day,YEAR,MONTH),
+    logger.info('%s/%s%s%02d*.nc' % (in_dir, file_day, year, month))
+    day_cube = iris.load_cube('%s/%s%s%02d*.nc' % (in_dir, file_day,
+                                                   year, month),
                               variable)
-    night_cube = iris.load_cube('%s/%s%s%02d*.nc' % (in_dir,file_night,YEAR,MONTH),
+    night_cube = iris.load_cube('%s/%s%s%02d*.nc' % (in_dir, file_night,
+                                                     year, month),
                                 variable)
 
     return day_cube, night_cube
 
 
-def make_monthly_average(day_cube, night_cube, YEAR, MONTH):
-    """
-    Make the average LST form the day time and night time files
-    uses techniques from Patrick at AVD email 9/10/20
-    """
-
-    #for cube in day_cube + night_cube:
+def make_monthly_average(day_cube, night_cube, year, month):
+    """Make the average LST form the day time and night time files."""
     day_cube.attributes.clear()
     night_cube.attributes.clear()
 
-    #for cube in night_cube:
     co_time = night_cube.coord('time')
     co_time.points = co_time.points + 100.0
-    # maybe the arbitary difference should go on day cubes to take the timestamp to 12Z?
+    # maybe the arbitary difference should go on day cubes to
+    # take the timestamp to 12Z?
     # not really an issue when using monthly files
- 
-    result = iris.cube.CubeList([day_cube,night_cube]).concatenate_cube()
+
+    result = iris.cube.CubeList([day_cube, night_cube]).concatenate_cube()
 
     # This corrects the lonitude coord name issue
     # This should be fixed in the next version of the CCI data
@@ -132,20 +127,24 @@ def make_monthly_average(day_cube, night_cube, YEAR, MONTH):
     result.coords()[2].var_name = 'longitude'
     result.coords()[2].standard_name = 'longitude'
     result.coords()[2].long_name = 'longitude'
-    
-    monthly_cube = result.collapsed('time',iris.analysis.MEAN)
-    
+
+    monthly_cube = result.collapsed('time', iris.analysis.MEAN)
+
     # fix time coordinate bounds
     monthly_co_time = monthly_cube.coord('time')
-    
-    time_point = (datetime.datetime(YEAR,MONTH,1,0,0) - datetime.datetime(1981,1,1,0,0,0)).total_seconds()
+
+    time_point = (datetime.datetime(year, month, 1, 0, 0) -
+                  datetime.datetime(1981, 1, 1, 0, 0, 0)).total_seconds()
     monthly_co_time.points = time_point
 
-    num_days = monthrange(YEAR, MONTH)[1]
-    monthly_co_time.bounds = [time_point, time_point+((num_days-1)*24*60*60)]
+    num_days = monthrange(year, month)[1]
+    monthly_co_time.bounds = [time_point,
+                              time_point + ((num_days - 1) * 24 * 3600)]
     # should this be num_days or num_days-1 ### question for Valeriu or Axel
     # or 23:59:59 ???
 
-    monthly_cube.attributes = {'information' :'Mean of Day and Night Aqua MODIS monthly LST'}
+    monthly_cube.attributes = {'information':
+                               'Mean of Day and Night Aqua MODIS monthly LST'
+                               }
 
     return monthly_cube
--- esmvaltool/diag_scripts/lst/lst.py
+++ esmvaltool/diag_scripts/lst/lst.py
@@ -13,10 +13,11 @@ from esmvaltool.diag_scripts.shared import (
     run_diagnostic,
 )
 
-logger = logging.getLogger(__name__) # from OC example, dont know what this does!
+logger = logging.getLogger(__name__)
+
 
 def get_input_cubes(metadata):
-    # This is from 
+    # This is from
     # https://github.com/ESMValGroup/ESMValTool/blob/master/esmvaltool/diag_scripts/hydrology/lisflood.py
 
     inputs = {}
@@ -32,16 +33,20 @@ def get_input_cubes(metadata):
 
     return inputs, ancestors
 
-def make_plots(lst_diff_data,lst_diff_data_low,lst_diff_data_high, config, input_metadata):
+
+def make_plots(lst_diff_data, lst_diff_data_low, lst_diff_data_high,
+            config, input_metadata):
     # Make a timeseries plot of the difference OBS-MODEL
 
-    fig,ax = plt.subplots(figsize=(20,15))
+    fig,ax = plt.subplots(figsize=(20, 15))
 
     ax.plot(lst_diff_data.data, color='black', linewidth=4)
-    ax.plot(lst_diff_data_low.data,'--', color='blue', linewidth=3)
-    ax.plot(lst_diff_data_high.data,'--', color='blue', linewidth=3)
-    ax.fill_between(range(len(lst_diff_data.data)), lst_diff_data_low.data,lst_diff_data_high.data,
-                    color='blue',alpha=0.25)
+    ax.plot(lst_diff_data_low.data, '--', color='blue', linewidth=3)
+    ax.plot(lst_diff_data_high.data, '--', color='blue', linewidth=3)
+    ax.fill_between(range(len(lst_diff_data.data)),
+                    lst_diff_data_low.data, lst_diff_data_high.data,
+                    color='blue', alpha=0.25
+                    )
 
     # make X ticks
     x_tick_list = []
@@ -55,17 +60,17 @@ def make_plots(lst_diff_data,lst_diff_data_low,lst_diff_data_high, config, input
             x_tick_list.append('')
 
     ax.set_xticks(range(len(lst_diff_data.data)))
-    ax.set_xticklabels(x_tick_list, fontsize=18, rotation = 45)
+    ax.set_xticklabels(x_tick_list, fontsize=18, rotation=45)
 
     # make Y ticks
     Y_lower = np.floor(lst_diff_data_low.data.min())
     Y_upper = np.ceil(lst_diff_data_high.data.max())
-    ax.set_yticks(np.arange(Y_lower,Y_upper+0.1,2))
-    ax.set_yticklabels(np.arange(Y_lower,Y_upper+0.1,2), fontsize=18)
-    ax.set_ylim((Y_lower-0.1,Y_upper+0.1))
+    ax.set_yticks(np.arange(Y_lower, Y_upper + 0.1, 2))
+    ax.set_yticklabels(np.arange(Y_lower, Y_upper + 0.1, 2), fontsize=18)
+    ax.set_ylim((Y_lower - 0.1, Y_upper + 0.1))
 
-    ax.set_xlabel('Date', fontsize = 20)
-    ax.set_ylabel('Difference / K', fontsize = 20)
+    ax.set_xlabel('Date', fontsize=20)
+    ax.set_ylabel('Difference / K', fontsize=20)
 
     ax.grid()
 
@@ -74,35 +79,39 @@ def make_plots(lst_diff_data,lst_diff_data_low,lst_diff_data_high, config, input
 
     ax.set_title('Area: lon %s lat %s' % (lons[0], lats[0]), fontsize=22)
 
-    fig.suptitle('ESACCI LST - CMIP6 Historical Ensemble Mean', fontsize=24)
+    fig.suptitle('ESACCI LST - CMIP6 Historical Ensemble Mean',
+                 fontsize=24
+                 )
 
     plt.savefig('%s/timeseries.png' % config['plot_dir'])
-    plt.close('all') # Is this needed?
+    plt.close('all')  # Is this needed?
 
     return None
 
+
 def get_provenance_record(attributes, ancestor_files):
-    
+
     caption = ("Timeseries of ESA CCI LST difference to mean of model ensembles, calculated over region bounded by latitude {lat_south} to {lat_north}, longitude {lon_west} to {lon_east} and for model/ensembles {ensembles}. Shown for years {start_year} to {end_year}."
                .format(**attributes))
 
     record = {
         'caption': caption,
-        'statistics': ['mean','stddev'],
+        'statistics': ['mean', 'stddev'],
         'domains': ['reg'],
         'plot_types': ['times'],
         'authors': ['king_robert'],
-        #'references': [],
+        # 'references': [],
         'ancestors': ancestor_files
     }
 
     return record
 
+
 def diagnostic(config):
-    
+
     # this loading function is based on the hydrology py above
     input_metadata = config['input_data'].values()
-    logger.info(config)
+
     loaded_data = {}
     ancestor_list = []
     for dataset, metadata in group_metadata(input_metadata, 'dataset').items():
@@ -114,47 +123,57 @@ def diagnostic(config):
     # KEY1 model ESACCI-LST or something else
     # KEY2 is ts, the surface temperature
     # ie loaded_data['ESACCI-LST']['ts'] is the CCI cube
-    #    loaded_data['MultiModelMean']['ts'] is CMIP6 data, emsemble means, see preprocessor
+    #    loaded_data['MultiModelMean']['ts'] is CMIP6 data, emsemble means
+    #    similarly dor Std, see preprocessor
 
-    ##### The Diagnostic uses CCI - MODEL
+    # The Diagnostic uses CCI - MODEL
 
     # CMIP data had 360 day calendar, CCI data has 365 day calendar
     # Assume the loaded data is all the same shape
     loaded_data['MultiModelMean']['ts'].remove_coord('time')
-    loaded_data['MultiModelMean']['ts'].add_dim_coord(loaded_data['ESACCI-LST']['ts'].coord('time'),0)
+    loaded_data['MultiModelMean']['ts'].add_dim_coord(loaded_data['ESACCI-LST']['ts'].coord('time'), 0)
     loaded_data['MultiModelStd']['ts'].remove_coord('time')
-    loaded_data['MultiModelStd']['ts'].add_dim_coord(loaded_data['ESACCI-LST']['ts'].coord('time'),0)
+    loaded_data['MultiModelStd']['ts'].add_dim_coord(loaded_data['ESACCI-LST']['ts'].coord('time'), 0)
 
     # Make a cube of the LST difference, and with +/- std of model variation
     lst_diff_cube = loaded_data['ESACCI-LST']['ts'] - loaded_data['MultiModelMean']['ts']
-    lst_diff_cube_low = loaded_data['ESACCI-LST']['ts'] - (loaded_data['MultiModelMean']['ts']+loaded_data['MultiModelStd']['ts'])
-    lst_diff_cube_high = loaded_data['ESACCI-LST']['ts'] - (loaded_data['MultiModelMean']['ts']-loaded_data['MultiModelStd']['ts'])
+    lst_diff_cube_low = loaded_data['ESACCI-LST']['ts'] - (loaded_data['MultiModelMean']['ts'] +
+                             loaded_data['MultiModelStd']['ts']
+                             )
+    lst_diff_cube_high = loaded_data['ESACCI-LST']['ts'] - (loaded_data['MultiModelMean']['ts'] -
+                            loaded_data['MultiModelStd']['ts']
+                             )
 
     # Plotting
-    make_plots(lst_diff_cube,lst_diff_cube_low,lst_diff_cube_high, config, input_metadata)
+    make_plots(lst_diff_cube,
+                lst_diff_cube_low,
+                lst_diff_cube_high,
+                config,
+                input_metadata)
 
     # Provenance
     # Get this information form the data cubes
     data_attributes = {}
     data_attributes['start_year'] = lst_diff_cube.coord('time').units.num2date(lst_diff_cube.coord('time').points)[0].year
-    data_attributes['end_year']   = lst_diff_cube.coord('time').units.num2date(lst_diff_cube.coord('time').points)[-1].year
-    data_attributes['lat_south']  = lst_diff_cube.coord('latitude').bounds[0][0]
-    data_attributes['lat_north']  = lst_diff_cube.coord('latitude').bounds[0][1]
-    data_attributes['lon_west']   = lst_diff_cube.coord('longitude').bounds[0][0]
-    data_attributes['lon_east']   = lst_diff_cube.coord('longitude').bounds[0][1]
+    data_attributes['end_year'] = lst_diff_cube.coord('time').units.num2date(lst_diff_cube.coord('time').points)[-1].year
+    data_attributes['lat_south'] = lst_diff_cube.coord('latitude').bounds[0][0]
+    data_attributes['lat_north'] = lst_diff_cube.coord('latitude').bounds[0][1]
+    data_attributes['lon_west'] = lst_diff_cube.coord('longitude').bounds[0][0]
+    data_attributes['lon_east'] = lst_diff_cube.coord('longitude').bounds[0][1]
     data_attributes['ensembles'] = ''
 
-    for ITEM in input_metadata:
-        if 'ESACCI' in ITEM['alias'] or 'MultiModel' in ITEM['alias'] or 'OBS' in ITEM['alias']:
+    for item in input_metadata:
+        if 'ESACCI' in item['alias'] or 'MultiModel' in item['alias'] or 'OBS' in item['alias']:
             continue
-        data_attributes['ensembles'] += "%s " % ITEM['alias']
+        data_attributes['ensembles'] += "%s " % item['alias']
 
-    record = get_provenance_record(data_attributes,ancestor_list)
+    record = get_provenance_record(data_attributes, ancestor_list)
     for file in ['%s/timeseries.png' % config['plot_dir']]:
         with ProvenanceLogger(config) as provenance_logger:
             provenance_logger.log(file, record)
 
     return None
+
 
 if __name__ == '__main__':
     # always use run_diagnostic() to get the config (the preprocessor
