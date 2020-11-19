d4d1fdf438c2f3b418542ecd8a8c90f6d1f4f771
Valeriu Predoi <valeriu.predoi@gmail.com>
2020-11-19 18:52:34 +0000

    started work on tests

---
+++ tests/integration/test_recipe_filler.py
@@ -0,0 +1,126 @@
+"""Tests for _data_finder.py."""
+import contextlib
+import os
+import sys
+import shutil
+import tempfile
+
+import pytest
+import yaml
+
+import esmvalcore._config
+from esmvalcore.cmor.table import read_cmor_tables
+from esmvaltool.utils.recipe_filler import run
+
+# Initialize with standard config developer file
+esmvalcore._config.CFG = esmvalcore._config.read_config_developer_file()
+# Initialize CMOR tables
+read_cmor_tables(esmvalcore._config.CFG)
+
+# Load test configuration
+with open(os.path.join(os.path.dirname(__file__),
+                       'recipe_filler.yml')) as file:
+    CONFIG = yaml.safe_load(file)
+
+
+@contextlib.contextmanager
+def arguments(*args):
+    backup = sys.argv
+    sys.argv = list(args)
+    yield
+    sys.argv = backup
+
+
+def print_path(path):
+    """Print path."""
+    txt = path
+    if os.path.isdir(path):
+        txt += '/'
+    if os.path.islink(path):
+        txt += ' -> ' + os.readlink(path)
+    print(txt)
+
+
+def tree(path):
+    """Print path, similar to the the `tree` command."""
+    print_path(path)
+    for dirpath, dirnames, filenames in os.walk(path):
+        for dirname in dirnames:
+            print_path(os.path.join(dirpath, dirname))
+        for filename in filenames:
+            print_path(os.path.join(dirpath, filename))
+
+
+def create_file(filename):
+    """Create an empty file."""
+    dirname = os.path.dirname(filename)
+    if not os.path.exists(dirname):
+        os.makedirs(dirname)
+
+    with open(filename, 'a'):
+        pass
+
+
+def create_tree(path, filenames=None, symlinks=None):
+    """Create directory structure and files."""
+    for filename in filenames or []:
+        create_file(os.path.join(path, filename))
+
+    for symlink in symlinks or []:
+        link_name = os.path.join(path, symlink['link_name'])
+        os.symlink(symlink['target'], link_name)
+
+
+def write_config_user_file(dirname, project, file_path, drs):
+    config_file = dirname / 'config-user.yml'
+    cfg = {
+        'log_level': 'info',
+        'rootpath': {
+            project: str(dirname / file_path),
+        },
+        'drs': {
+            project: drs,
+        },
+    }
+    config_file.write_text(yaml.safe_dump(cfg, encoding=None))
+    return str(config_file)
+
+
+def write_recipe(dirname, recipe_dict):
+    recipe_file = dirname / 'recipe.yml'
+    recipe_file.write_text(yaml.safe_dump(recipe_dict, encoding=None))
+    return str(recipe_file)
+
+
+@pytest.fixture
+def root():
+    """Root function for tests."""
+    dirname = tempfile.mkdtemp()
+    yield os.path.join(dirname, 'output1')
+    print("Directory structure was:")
+    tree(dirname)
+    shutil.rmtree(dirname)
+
+
+@pytest.mark.parametrize('cfg', CONFIG['get_input_filelist'])
+def test_get_timefiltered_files(tmp_path, root, cfg):
+    """Test retrieving input filelist."""
+    create_tree(root, cfg.get('available_files'),
+                cfg.get('available_symlinks'))
+
+    # Find files
+    rootpath = {cfg['variable']['project']: [root]}
+    drs = {cfg['variable']['project']: cfg['drs']}
+
+    user_config_file = write_config_user_file(tmp_path,
+                                              cfg["variable"]["project"],
+                                              root, cfg['drs'])
+    recipe = write_recipe(tmp_path, cfg)
+
+    with arguments(
+            'recipe_filler',
+            recipe,
+            '-c',
+            user_config_file,
+    ):
+        run()
