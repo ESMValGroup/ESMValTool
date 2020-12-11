8ac0d642c3bf6666d9930c532b5ba1f7eaee1e73
Martin Jury <martin.w.jury@gmail.com>
2020-12-11 11:30:29 +0100

    fix reversed lat bnds, fix time.

--- esmvaltool/cmorizers/obs/cmorize_obs_gpcc.py
+++ esmvaltool/cmorizers/obs/cmorize_obs_gpcc.py
@@ -20,8 +20,9 @@ import logging
 import os
 import shutil
 from warnings import catch_warnings, filterwarnings
-import cf_units
-
+from cf_units import Unit
+import cftime
+import numpy as np
 import iris
 
 from . import utilities as utils
@@ -34,6 +35,34 @@ def _clean(filepath):
     if os.path.isfile(filepath):
         os.remove(filepath)
         logger.info("Removed cached file %s", filepath)
+
+
+def _get_centered_timecoord(cube):
+    # time points start at the beginning of month at 00:00:00
+
+    # # tried this, doesn't work
+    # # the correct point should be the 2nd bound?
+    # cube.coord('time').guess_bounds()
+    # cube.coord('time').points = cube.coord('time').bounds[:, 1]
+    # cube.coord('time').bounds = None
+
+    time = cube.coord('time')
+    times = time.units.num2date(time.points)
+
+    # get the bounds right
+    starts = [
+        cftime.DatetimeNoLeap(c.year, c.month, 1)
+        for c in times
+    ]
+    ends = [
+        cftime.DatetimeNoLeap(c.year, c.month +
+                                1, 1) if c.month < 12 else
+        cftime.DatetimeNoLeap(c.year + 1, 1, 1) for c in times
+    ]
+    time.bounds = time.units.date2num(np.stack([starts, ends], -1))
+
+    # get the center right
+    time.points = [np.mean((t1, t2)) for t1, t2 in time.bounds]
 
 
 def _extract_variable(short_name, var, version, cfg, filepath, out_dir):
@@ -54,12 +83,33 @@ def _extract_variable(short_name, var, version, cfg, filepath, out_dir):
     # fix calendar type
     cal_time = var.get('calendar', short_name)
     origin_time = cube.coord('time').units.origin
-    cube.coord('time').units = cf_units.Unit(origin_time, calendar=cal_time)
+    cube.coord('time').units = Unit(origin_time, calendar=cal_time)
     cube.convert_units(cmor_info.units)
     utils.convert_timeunits(cube, 1950)
 
+    _get_centered_timecoord(cube)
+    cube.coord('time').convert_units(
+        Unit('days since 1950-1-1 00:00:00', calendar='gregorian'))
+
     # Fix coordinates
-    utils.fix_coords(cube)
+    # latitude is flipped
+    utils.flip_dim_coord(cube, 'latitude')
+    # utils.fix_coords(cube) <- why are bounds for time re-guessed that are not
+    # correct??
+    utils._fix_dim_coordnames(cube)
+    # fix longitude
+    cube_coord = cube.coord('longitude')
+    if cube_coord.points[0] < 0. and \
+            cube_coord.points[-1] < 181.:
+        cube_coord.points = \
+            cube_coord.points + 180.
+        utils._fix_bounds(cube, cube_coord)
+        cube.attributes['geospatial_lon_min'] = 0.
+        cube.attributes['geospatial_lon_max'] = 360.
+        nlon = len(cube_coord.points)
+        utils._roll_cube_data(cube, nlon // 2, -1)
+    cube_coord = cube.coord('latitude')
+    utils._fix_bounds(cube, cube_coord)
 
     # Fix metadata
     attrs = cfg['attributes']
