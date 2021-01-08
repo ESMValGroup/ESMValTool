bc044762b8f1f2c0baec2a399cee7c5ea5803b3f
morobking <rob.king@metoffice.gov.uk>
2021-01-08 15:14:55 +0000

    output monthly files of needed data

--- esmvaltool/cmorizers/obs/cmorize_obs_esacci_lst_uncert.py
+++ esmvaltool/cmorizers/obs/cmorize_obs_esacci_lst_uncert.py
@@ -43,19 +43,19 @@ def cmorization(in_dir, out_dir, cfg, _):
 
     # # vals has the info from the yml file
     # # var is set up in the yml file
-    # for var, vals in cfg['variables'].items():
-    #     # leave this loop in as might be useful in
-    #     # the future for getting other info
-    #     # like uncertainty information from the original files
+    for var, vals in cfg['variables'].items():
+        # leave this loop in as might be useful in
+        # the future for getting other info
+        # like uncertainty information from the original files
 
-    #     glob_attrs['mip'] = vals['mip']
+        glob_attrs['mip'] = vals['mip']
 
-    #     for key in vals.keys():
-    #         logger.info("%s %s", key, vals[key])
+        for key in vals.keys():
+            logger.info("%s %s", key, vals[key])
 
-    #     variable = vals['raw']
-    #     # not currently used, but referenced for future
-    #     # platform = 'MODISA'
+        variable = vals['raw']
+        # not currently used, but referenced for future
+        # platform = 'MODISA'
 
     # loop over years and months
     # get years from start_year and end_year
@@ -65,20 +65,31 @@ def cmorization(in_dir, out_dir, cfg, _):
         this_years_cubes = iris.cube.CubeList()
         for month in range(1,13):
             logger.info(month)
+            logger.info(in_dir)
+            logger.info(vals['file_day'])
+            logger.info(vals['file_night'])
+            logger.info(year)
+
+
             day_cube, night_cube = load_cubes(in_dir,
                                               vals['file_day'],
                                               vals['file_night'],
                                               year,
                                               month
                                               ) # variable moved
-
-            monthly_cube = make_monthly_average(day_cube, night_cube,
+            logger.info("XXXXXXXX")
+            monthly_cubes = make_monthly_average(day_cube, night_cube,
                                                 year, month)
 
             # use CMORizer utils
-            monthly_cube = utils.fix_coords(monthly_cube)
-
-            this_years_cubes.append(monthly_cube)
+            ########################## make this a loop over cube list
+            for cube in monthly_cubes:
+                try:
+                    cube = utils.fix_coords(cube)
+                except:
+                    logger.info('skip fixing')
+                    logger.info(cube)
+                    pass
 
         # Use utils save
         # This seems to save files all with the same name!
@@ -88,9 +99,11 @@ def cmorization(in_dir, out_dir, cfg, _):
         # this_years_cubes = this_years_cubes.merge_cube()
         # this_years_cubes.long_name = 'Surface Temperature'
         # this_years_cubes.standard_name = 'surface_temperature'
+            logger.info(out_dir)
+            iris.save(monthly_cubes, '%s/test_%s_%02d.nc' % (out_dir,year,month))
 
         # utils.save_variable(
-        #     this_years_cubes,
+        #     monthly_cubes,
         #     var,
         #     out_dir,
         #     glob_attrs,
@@ -104,54 +117,84 @@ def load_cubes(in_dir, file_day, file_night, year, month):
                but in place for future expansion to all ESC CCI LST plaforms
     """
     logger.info('Loading %s/%s%s%s*.nc', in_dir, file_day, year, month)
-    day_cube = iris.load_cube('%s/%s%s%02d*.nc' % (in_dir, file_day,
+    day_cube = iris.load('%s/%s%s%02d*.nc' % (in_dir, file_day,
                                                    year, month),
                               variable_list)
     logger.info('Loading %s/%s%s%s*.nc', in_dir, file_night, year, month)
-    night_cube = iris.load_cube('%s/%s%s%02d*.nc' % (in_dir, file_night,
+    night_cube = iris.load('%s/%s%s%02d*.nc' % (in_dir, file_night,
                                                      year, month),
                                 variable_list)
 
     return day_cube, night_cube
 
 
-# def make_monthly_average(day_cube, night_cube, year, month):
-#     """Make the average LST form the day time and night time files."""
-#     day_cube.attributes.clear()
-#     night_cube.attributes.clear()
+def make_monthly_average(day_cube, night_cube, year, month):
+    """Make the average LST form the day time and night time files."""
+    for cube in day_cube:
+        cube.attributes.clear()
+        try:
+            cube.coords()[2].var_name = 'longitude'
+            cube.coords()[2].standard_name = 'longitude'
+            cube.coords()[2].long_name = 'longitude'
+        except:
+            pass
 
-#     co_time = night_cube.coord('time')
-#     co_time.points = co_time.points + 100.0
-#     # maybe the arbitary difference should go on day cubes to
-#     # take the timestamp to 12Z?
-#     # not really an issue when using monthly files
+    for cube in night_cube:
+        cube.attributes.clear()
+        try:
+            cube.coords()[2].var_name = 'longitude'
+            cube.coords()[2].standard_name = 'longitude'
+            cube.coords()[2].long_name = 'longitude'
+        except:
+            pass
 
-#     result = iris.cube.CubeList([day_cube, night_cube]).concatenate_cube()
+    co_time = night_cube[0].coord('time') # zeroth cube should be the first one because of list
+    co_time.points = co_time.points + 100.0
+    # maybe the arbitary difference should go on day cubes to
+    # take the timestamp to 12Z?
+    # not really an issue when using monthly files
 
-#     # This corrects the lonitude coord name issue
-#     # This should be fixed in the next version of the CCI data
-#     logger.info("Longitude coordinate correction being applied")
-#     result.coords()[2].var_name = 'longitude'
-#     result.coords()[2].standard_name = 'longitude'
-#     result.coords()[2].long_name = 'longitude'
+   
 
-#     monthly_cube = result.collapsed('time', iris.analysis.MEAN)
 
-#     # fix time coordinate bounds
-#     monthly_co_time = monthly_cube.coord('time')
+    # Make a time suitable time coordinate
+    # make a monthly LST value, need to do this here to get coordinate values
+    mean_lst_cubes = iris.cube.CubeList([day_cube[0], night_cube[0]]).concatenate_cube()
+    monthly_lst = mean_lst_cubes.collapsed('time', iris.analysis.MEAN)
 
-#     time_point = (datetime.datetime(year, month, 1, 0, 0) -
-#                   datetime.datetime(1981, 1, 1, 0, 0, 0)).total_seconds()
-#     monthly_co_time.points = time_point
+    # fix time coordinate bounds
+    monthly_co_time = monthly_lst.coord('time')
 
-#     num_days = monthrange(year, month)[1]
-#     monthly_co_time.bounds = [time_point,
-#                               time_point + ((num_days - 1) * 24 * 3600)]
-#     # should this be num_days or num_days-1 ### question for Valeriu or Axel
-#     # or 23:59:59 ???
+    time_point = (datetime.datetime(year, month, 1, 0, 0) -
+                  datetime.datetime(1981, 1, 1, 0, 0, 0)).total_seconds()
+    monthly_co_time.points = time_point
 
-#     monthly_cube.attributes = {'information':
-#                                'Mean of Day and Night Aqua MODIS monthly LST'
-#                                }
+    num_days = monthrange(year, month)[1]
+    monthly_co_time.bounds = [time_point,
+                              time_point + ((num_days - 1) * 24 * 3600)]
+    # should this be num_days or num_days-1 ### question for Valeriu or Axel
+    # or 23:59:59 ???
 
-#     return monthly_cube
+
+    # put all cubes here
+    output = iris.cube.CubeList()
+
+    
+    monthly_lst.attributes = {'information':
+                               'Mean of Day and Night Aqua MODIS monthly LST'
+                               }
+
+    output.append(monthly_lst)
+
+
+    for cube in day_cube:
+        cube.long_name = 'day ' + cube.long_name
+        output.append(cube)
+
+    for cube in night_cube:
+        cube.long_name = 'night ' + cube.long_name
+        output.append(cube)
+
+    logger.info(output)
+
+    return output
