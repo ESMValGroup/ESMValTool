3eecc27536cdf4c1a394f118cb1ba21eeb7c3a42
Valeriu Predoi <valeriu.predoi@gmail.com>
2021-01-20 12:04:41 +0000

    unpinned matplotlib

--- setup.py
+++ setup.py
@@ -39,7 +39,7 @@ REQUIREMENTS = {
         'jinja2',
         'joblib',
         'lime',
-        'matplotlib>=3,<3.3',
+        'matplotlib',  # bug in 3.3.1, 3.3.3 fine
         'natsort',
         'nc-time-axis',  # needed by iris.plot
         'netCDF4',
