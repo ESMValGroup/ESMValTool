1117939bc1c19d534b4e72cbd1f909d6f5f7016a
Valeriu Predoi <valeriu.predoi@gmail.com>
2021-01-28 12:41:45 +0000

    pin mpich

--- setup.py
+++ setup.py
@@ -44,6 +44,7 @@ REQUIREMENTS = {
         'nc-time-axis',  # needed by iris.plot
         'netCDF4',
         'numpy',
+        'mpich<3.4',
         'pandas',
         'pyproj>=2.1'
         'pyyaml',
