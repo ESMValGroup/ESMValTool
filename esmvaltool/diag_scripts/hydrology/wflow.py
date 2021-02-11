6269955bea4cbc5e0ade70897894720d6cb114ea
SarahAlidoost <f.alidoost@esciencecenter.nl>
2021-02-11 16:57:18 +0100

    replace lazy_regrid with regrid from esmvalcore

--- esmvaltool/diag_scripts/hydrology/wflow.py
+++ esmvaltool/diag_scripts/hydrology/wflow.py
@@ -2,12 +2,12 @@
 import logging
 from pathlib import Path
 
-import iris
 import numpy as np
 from osgeo import gdal
+import iris
 
+from esmvalcore.preprocessor import regrid
 from esmvaltool.diag_scripts.hydrology.derive_evspsblpot import debruin_pet
-from esmvaltool.diag_scripts.hydrology.lazy_regrid import lazy_regrid
 from esmvaltool.diag_scripts.shared import (ProvenanceLogger,
                                             get_diagnostic_filename,
                                             group_metadata, run_diagnostic)
@@ -97,7 +97,7 @@ def regrid_temperature(src_temp, src_height, target_height, scheme):
     src_slt = src_temp.copy(data=src_temp.core_data() + src_dtemp.core_data())
 
     # Interpolate sea-level temperature to target grid
-    target_slt = lazy_regrid(src_slt, target_height, scheme)
+    target_slt = regrid(src_slt, target_height, scheme)
 
     # Convert sea-level temperature to new target elevation
     target_dtemp = lapse_rate_correction(target_height)
@@ -216,7 +216,7 @@ def main(cfg):
 
         logger.info("Processing variable precipitation_flux")
         scheme = cfg['regrid']
-        pr_dem = lazy_regrid(all_vars['pr'], dem, scheme)
+        pr_dem = regrid(all_vars['pr'], dem, scheme)
 
         logger.info("Processing variable temperature")
         tas_dem = regrid_temperature(
@@ -229,12 +229,12 @@ def main(cfg):
         logger.info("Processing variable potential evapotranspiration")
         if 'evspsblpot' in all_vars:
             pet = all_vars['evspsblpot']
-            pet_dem = lazy_regrid(pet, dem, scheme)
+            pet_dem = regrid(pet, dem, scheme)
         else:
             logger.info("Potential evapotransporation not available, deriving")
-            psl_dem = lazy_regrid(all_vars['psl'], dem, scheme)
-            rsds_dem = lazy_regrid(all_vars['rsds'], dem, scheme)
-            rsdt_dem = lazy_regrid(all_vars['rsdt'], dem, scheme)
+            psl_dem = regrid(all_vars['psl'], dem, scheme)
+            rsds_dem = regrid(all_vars['rsds'], dem, scheme)
+            rsdt_dem = regrid(all_vars['rsdt'], dem, scheme)
             pet_dem = debruin_pet(
                 tas=tas_dem,
                 psl=psl_dem,
