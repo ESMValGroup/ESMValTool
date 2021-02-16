af8339af08f0dee27c2a4964dd6b2c3d2802dedc
Bouwe Andela <b.andela@esciencecenter.nl>
2021-02-16 12:17:56 +0100

    Remove write_netcdf option from save_data function

--- esmvaltool/diag_scripts/shared/_base.py
+++ esmvaltool/diag_scripts/shared/_base.py
@@ -127,12 +127,12 @@ def save_data(basename, provenance, cfg, cube, **kwargs):
     if 'target' in kwargs:
         raise ValueError(
             "Please use the `basename` argument to specify the output file")
-    if cfg.get('write_netcdf', True):
-        filename = get_diagnostic_filename(basename, cfg)
-        logger.info("Saving analysis results to %s", filename)
-        iris.save(cube, target=filename, **kwargs)
-        with ProvenanceLogger(cfg) as provenance_logger:
-            provenance_logger.log(filename, provenance)
+
+    filename = get_diagnostic_filename(basename, cfg)
+    logger.info("Saving analysis results to %s", filename)
+    iris.save(cube, target=filename, **kwargs)
+    with ProvenanceLogger(cfg) as provenance_logger:
+        provenance_logger.log(filename, provenance)
 
 
 class ProvenanceLogger:
