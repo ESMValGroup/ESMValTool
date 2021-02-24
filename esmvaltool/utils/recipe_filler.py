c8c0cf15ca2e2a577d69467cb674d35933f0c458
Valeriu Predoi <valeriu.predoi@gmail.com>
2021-02-24 12:55:45 +0000

    port all private funcs from esmvalcore

--- esmvaltool/utils/recipe_filler.py
+++ esmvaltool/utils/recipe_filler.py
@@ -40,19 +40,248 @@ Caveats:
 Have fun!
 """
 import argparse
+import datetime
 import itertools
 import logging
 import os
 import shutil
+import time
+import warnings
 from glob import glob
+from pathlib import Path
 
 import yaml
 from ruamel.yaml import YAML
-from esmvalcore._config import (configure_logging, read_config_developer_file,
-                                read_config_user_file)
-from esmvalcore.cmor.table import CMOR_TABLES
+
+import esmvalcore
+from esmvalcore.cmor.table import CMOR_TABLES, read_cmor_tables
 
 logger = logging.getLogger(__name__)
+
+CFG = {}
+
+
+# standard libs from esmvalcore ported here to avoid private func import
+def load_config_developer(cfg_file=None):
+    """Load the config developer file and initialize CMOR tables."""
+    cfg_developer = read_config_developer_file(cfg_file)
+    for key, value in cfg_developer.items():
+        CFG[key] = value
+    read_cmor_tables(CFG)
+
+
+def _purge_file_handlers(cfg: dict) -> None:
+    """Remove handlers with filename set.
+
+    This is used to remove file handlers which require an output
+    directory to be set.
+    """
+    cfg['handlers'] = {
+        name: handler
+        for name, handler in cfg['handlers'].items()
+        if 'filename' not in handler
+    }
+    prev_root = cfg['root']['handlers']
+    cfg['root']['handlers'] = [
+        name for name in prev_root if name in cfg['handlers']
+    ]
+
+
+def _update_stream_level(cfg: dict, level=None):
+    """Update the log level for the stream handlers."""
+    handlers = cfg['handlers']
+
+    for handler in handlers.values():
+        if level is not None and 'stream' in handler:
+            if handler['stream'] in ('ext://sys.stdout', 'ext://sys.stderr'):
+                handler['level'] = level.upper()
+
+
+def _get_log_files(cfg: dict, output_dir: str = None) -> list:
+    """Initialize log files for the file handlers."""
+    log_files = []
+
+    handlers = cfg['handlers']
+
+    for handler in handlers.values():
+        filename = handler.get('filename', None)
+
+        if filename:
+            if not os.path.isabs(filename):
+                handler['filename'] = os.path.join(output_dir, filename)
+            log_files.append(handler['filename'])
+
+    return log_files
+
+
+def configure_logging(cfg_file: str = None,
+                      output_dir: str = None,
+                      console_log_level: str = None) -> list:
+    """Configure logging.
+
+    Parameters
+    ----------
+    cfg_file : str, optional
+        Logging config file. If `None`, defaults to `configure-logging.yml`
+    output_dir : str, optional
+        Output directory for the log files. If `None`, log only to the console.
+    console_log_level : str, optional
+        If `None`, use the default (INFO).
+
+    Returns
+    -------
+    log_files : list
+        Filenames that will be logged to.
+    """
+    if cfg_file is None:
+        cfg_loc = Path(esmvalcore.__file__ + "esmvalcore")
+        cfg_file = cfg_loc.parents[0] / '_config/config-logging.yml'
+
+    cfg_file = Path(cfg_file).absolute()
+
+    with open(cfg_file) as file_handler:
+        cfg = yaml.safe_load(file_handler)
+
+    if output_dir is None:
+        _purge_file_handlers(cfg)
+
+    log_files = _get_log_files(cfg, output_dir=output_dir)
+    _update_stream_level(cfg, level=console_log_level)
+
+    logging.config.dictConfig(cfg)
+    logging.Formatter.converter = time.gmtime
+    logging.captureWarnings(True)
+
+    return log_files
+
+
+def read_config_developer_file(cfg_file=None):
+    """Read the developer's configuration file."""
+    if cfg_file is None:
+        cfg_loc = Path(esmvalcore.__file__ + "esmvalcore")
+        cfg_file = cfg_loc.parents[0] / 'config-developer.yml'
+
+    with open(cfg_file, 'r') as file:
+        cfg = yaml.safe_load(file)
+
+    return cfg
+
+
+def _normalize_path(path):
+    """Normalize paths.
+
+    Expand ~ character and environment variables and convert path to absolute.
+
+    Parameters
+    ----------
+    path: str
+        Original path
+
+    Returns
+    -------
+    str:
+        Normalized path
+    """
+    if path is None:
+        return None
+    return os.path.abspath(os.path.expanduser(os.path.expandvars(path)))
+
+
+def read_config_user_file(config_file, folder_name, options=None):
+    """Read config user file and store settings in a dictionary."""
+    if not config_file:
+        config_file = '~/.esmvaltool/config-user.yml'
+    config_file = os.path.abspath(
+        os.path.expandvars(os.path.expanduser(config_file)))
+    # Read user config file
+    if not os.path.exists(config_file):
+        print(f"ERROR: Config file {config_file} does not exist")
+
+    with open(config_file, 'r') as file:
+        cfg = yaml.safe_load(file)
+
+    # DEPRECATED: remove in v2.4
+    for setting in ('write_plots', 'write_netcdf'):
+        if setting in cfg:
+            msg = (
+                f"Using '{setting}' in {config_file} is deprecated and will "
+                "be removed in ESMValCore version 2.4. For diagnostics "
+                "that support this setting, it should be set in the "
+                "diagnostic script section of the recipe instead. "
+                f"Remove the setting from {config_file} to get rid of this "
+                "warning message.")
+            print(f"Warning: {msg}")
+            warnings.warn(DeprecationWarning(msg))
+
+    if options is None:
+        options = dict()
+    for key, value in options.items():
+        cfg[key] = value
+        # DEPRECATED: remove in v2.4
+        if key in ('write_plots', 'write_netcdf'):
+            msg = (
+                f"Setting '{key}' from the command line is deprecated and "
+                "will be removed in ESMValCore version 2.4. For diagnostics "
+                "that support this setting, it should be set in the "
+                "diagnostic script section of the recipe instead.")
+            print(f"Warning: {msg}")
+            warnings.warn(DeprecationWarning(msg))
+
+    # set defaults
+    defaults = {
+        'compress_netcdf': False,
+        'exit_on_warning': False,
+        'output_file_type': 'png',
+        'output_dir': 'esmvaltool_output',
+        'auxiliary_data_dir': 'auxiliary_data',
+        'save_intermediary_cubes': False,
+        'remove_preproc_dir': True,
+        'max_parallel_tasks': None,
+        'run_diagnostic': True,
+        'profile_diagnostic': False,
+        'config_developer_file': None,
+        'drs': {},
+        # DEPRECATED: remove default settings below in v2.4
+        'write_plots': True,
+        'write_netcdf': True,
+    }
+
+    for key in defaults:
+        if key not in cfg:
+            logger.info(
+                "No %s specification in config file, "
+                "defaulting to %s", key, defaults[key])
+            cfg[key] = defaults[key]
+
+    cfg['output_dir'] = _normalize_path(cfg['output_dir'])
+    cfg['auxiliary_data_dir'] = _normalize_path(cfg['auxiliary_data_dir'])
+
+    cfg['config_developer_file'] = _normalize_path(
+        cfg['config_developer_file'])
+
+    for key in cfg['rootpath']:
+        root = cfg['rootpath'][key]
+        if isinstance(root, str):
+            cfg['rootpath'][key] = [_normalize_path(root)]
+        else:
+            cfg['rootpath'][key] = [_normalize_path(path) for path in root]
+
+    # insert a directory date_time_recipe_usertag in the output paths
+    now = datetime.datetime.utcnow().strftime("%Y%m%d_%H%M%S")
+    new_subdir = '_'.join((folder_name, now))
+    cfg['output_dir'] = os.path.join(cfg['output_dir'], new_subdir)
+
+    # create subdirectories
+    cfg['preproc_dir'] = os.path.join(cfg['output_dir'], 'preproc')
+    cfg['work_dir'] = os.path.join(cfg['output_dir'], 'work')
+    cfg['plot_dir'] = os.path.join(cfg['output_dir'], 'plots')
+    cfg['run_dir'] = os.path.join(cfg['output_dir'], 'run')
+
+    # Read developer configuration file
+    load_config_developer(cfg['config_developer_file'])
+
+    return cfg
+
 
 HEADER = r"""
 ______________________________________________________________________
