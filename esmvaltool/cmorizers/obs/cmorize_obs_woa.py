034cfff416750d8fc9c7d3b6f5b6b126d2279719
Tomas Lovato <tomas.lovato@cmcc.it>
2021-03-08 10:07:41 +0100

    Consolidate WOA cmorizer for 2018 dataset (#1812)
    - use WOA 2018 data at 1 degree monthly fields
    - set to OBS6 Info to enable the derivation of surface and Oyr variables
    - add automatic download if raw data are not available

--- esmvaltool/cmorizers/obs/cmorize_obs_woa.py
+++ esmvaltool/cmorizers/obs/cmorize_obs_woa.py
@@ -4,29 +4,18 @@ Tier
    Tier 2: other freely-available dataset.
 
 Source
-   WOA13: https://data.nodc.noaa.gov/woa/WOA13/DATAv2/
-   WOA18: https://www.nodc.noaa.gov/OC5/woa18/woa18data.html
+   WOA18: https://www.ncei.noaa.gov/access/world-ocean-atlas-2018/
 
 Last access
-   WOA13: 20190131
-   WOA18: 20200911
+   WOA18: 20210311
 
 Download and processing instructions
-   Download the following files:
-     WOA13:
-     temperature/netcdf/decav81B0/1.00/woa13_decav81B0_t00_01.nc
-     salinity/netcdf/decav81B0/1.00/woa13_decav81B0_s00_01.nc
-     oxygen/netcdf/all/1.00/woa13_all_o00_01.nc
-     nitrate/netcdf/all/1.00/woa13_all_n00_01.nc
-     phosphate/netcdf/all/1.00/woa13_all_p00_01.nc
-     silicate/netcdf/all/1.00/woa13_all_i00_01.nc
-     WOA18:
-     https://data.nodc.noaa.gov/thredds/fileServer/ncei/woa/temperature/decav/1.00/woa18_decav_t00_01.nc
-     https://data.nodc.noaa.gov/thredds/fileServer/ncei/woa/salinity/decav/1.00/woa18_decav_s00_01.nc
+   All handled by the script (download only if local data are missing)
 
 Modification history
+   20210311-lovato_tomas: consolidate WOA18 and use OBS6
    20200911-bock_lisa: extend to WOA18
-   20130328-lovato_tomas: cmorizer revision
+   20190328-lovato_tomas: cmorizer revision
    20190131-predoi_valeriu: adapted to v2.
    20190131-demora_lee: written.
 
@@ -34,11 +23,16 @@ Modification history
 
 import logging
 import os
-
+from warnings import catch_warnings, filterwarnings
+import requests
 import iris
+import numpy as np
+from cf_units import Unit
 
-from .utilities import (constant_metadata, convert_timeunits, fix_coords,
-                        fix_var_metadata, save_variable, set_global_atts)
+from esmvalcore.preprocessor import annual_statistics
+
+from .utilities import (constant_metadata, fix_coords, fix_var_metadata,
+                        save_variable, set_global_atts)
 
 logger = logging.getLogger(__name__)
 
@@ -47,38 +41,86 @@ def _fix_data(cube, var):
     """Specific data fixes for different variables."""
     logger.info("Fixing data ...")
     with constant_metadata(cube):
-        mll_to_mol = ['po4', 'si', 'no3']
-        if var in mll_to_mol:
-            cube /= 1000.  # Convert from ml/l to mol/m^3
-        elif var in ['tos', 'thetao']:
+        if var == 'thetao':
             cube += 273.15  # Convert to Kelvin
         elif var == 'o2':
             cube *= 44.661 / 1000.  # Convert from ml/l to mol/m^3
     return cube
 
 
-def extract_variable(var_info, raw_info, out_dir, attrs, year):
-    """Extract to all vars."""
-    var = var_info.short_name
-    cubes = iris.load(raw_info['file'])
-    rawvar = raw_info['name']
+def collect_files(in_dir, var, cfg):
+    """ compose input file list and download if missing """
+    file_list = []
+    var_dict = cfg['variables'][var]
+    in_dir = os.path.join(in_dir, var_dict['name'])
+    # create list of monthly data
+    for mon in range(1, 13):
+        fname = var_dict['file'] + "{:02d}".format(mon) + '_01.nc'
+        in_file = os.path.join(in_dir, fname)
 
-    for cube in cubes:
-        if cube.var_name == rawvar:
-            logger.info("CMORizing var %s", var)
-            if var in ['tos', 'sos']:
-                logger.info("Extract level")
-                level_constraint = iris.Constraint(cube.var_name,
-                                                   depth=0)
-                cube = cube.extract(level_constraint)
+        # download if missing
+        if not os.path.isfile(in_file):
+            if not os.path.isdir(in_dir):
+                os.makedirs(in_dir)
+            logger.info('Input file %s is missing. Start download ... ', fname)
+            url = os.path.join(cfg['attributes']['source'], var_dict['name'],
+                               'netcdf', var_dict['file'].split('_')[1],
+                               cfg['custom']['resolution'], fname)
+            url_file = requests.get(url)
+            open(in_file, 'wb').write(url_file.content)
 
-            fix_var_metadata(cube, var_info)
-            convert_timeunits(cube, year)
-            fix_coords(cube)
-            _fix_data(cube, var)
-            set_global_atts(cube, attrs)
-            save_variable(
-                cube, var, out_dir, attrs, unlimited_dimensions=['time'])
+        file_list.append(in_file)
+
+    return file_list
+
+
+def extract_variable(in_files, out_dir, attrs, raw_info, cmor_table):
+    """Extract variables and create OBS dataset."""
+    var = raw_info['var']
+    var_info = cmor_table.get_variable(raw_info['mip'], var)
+    rawvar = raw_info['raw_var']
+    with catch_warnings():
+        filterwarnings(
+            action='ignore',
+            message='Ignoring netCDF variable .* invalid units .*',
+            category=UserWarning,
+            module='iris',
+        )
+        cubes = iris.load(in_files, rawvar)
+    iris.util.equalise_attributes(cubes)
+    cube = cubes.concatenate_cube()
+
+    # set reference time
+    year = raw_info['reference_year']
+    cube.coord('time').points = np.arange(0.5, 12.5)
+    cube.coord('time').units = Unit('months since ' + str(year) +
+                                    '-01-01 00:00:00',
+                                    calendar='gregorian')
+
+    fix_var_metadata(cube, var_info)
+    fix_coords(cube)
+    _fix_data(cube, var)
+    set_global_atts(cube, attrs)
+    save_variable(cube, var, out_dir, attrs, unlimited_dimensions=['time'])
+
+    # compute derived vars
+    if 'srf_var' in raw_info:
+        var_info = cmor_table.get_variable(raw_info['mip'],
+                                           raw_info['srf_var'])
+        logger.info("Extract surface OBS for %s", raw_info['srf_var'])
+        level_constraint = iris.Constraint(cube.var_name, depth=0)
+        cube = cube.extract(level_constraint)
+        fix_var_metadata(cube, var_info)
+        save_variable(cube,
+                      raw_info['srf_var'],
+                      out_dir,
+                      attrs,
+                      unlimited_dimensions=['time'])
+
+    if raw_info['compute_Oyr']:
+        attrs['mip'] = 'Oyr'
+        cube = annual_statistics(cube, 'mean')
+        save_variable(cube, var, out_dir, attrs, unlimited_dimensions=['time'])
 
 
 def cmorization(in_dir, out_dir, cfg, _):
@@ -88,11 +130,13 @@ def cmorization(in_dir, out_dir, cfg, _):
 
     # run the cmorization
     for var, vals in cfg['variables'].items():
-        for year in cfg['custom']['years']:
-            inpfile = os.path.join(in_dir, vals['file'])
-            logger.info("CMORizing var %s from file %s", var, inpfile)
-            var_info = cmor_table.get_variable(vals['mip'], var)
-            logger.info("units = %s", var_info.units)
-            raw_info = {'name': vals['raw'], 'file': inpfile}
-            glob_attrs['mip'] = vals['mip']
-            extract_variable(var_info, raw_info, out_dir, glob_attrs, year)
+        in_files = collect_files(in_dir, var, cfg)
+        logger.info("CMORizing var %s from input set %s", var, vals['name'])
+        raw_info = cfg['variables'][var]
+        raw_info.update({
+            'var': var,
+            'reference_year': cfg['custom']['reference_year'],
+            'compute_Oyr': cfg['custom']['compute_Oyr']
+        })
+        glob_attrs['mip'] = vals['mip']
+        extract_variable(in_files, out_dir, glob_attrs, raw_info, cmor_table)
