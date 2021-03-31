73c3ff4956a91c846d0cebb3c1212f6b0e145647
“katjaweigel” <weigel@iup.physik.uni-bremen.de>
2021-03-31 11:43:05 +0200

    Allow multiple models and optional color bar maximum.

---
+++ esmvaltool/diag_scripts/pv_capacityfactor/PV_CF.R
@@ -0,0 +1,15 @@
+library(ggplot2)
+library(plyr)
+rsds2CF <- function(rsds, t2m) {
+# power <- pc$fun(wind)
+  CF <- 0.9*(1-0.00042*(t2m+273.15-25.))*rsds/1000 # nolint
+}
+WPD <- function(wind, ro) {
+  return(0.5 * ro * wind^3)
+}
+bump <- function(x) {
+  f <- function(y) {
+    exp(-1 / y^2)
+  }
+  return(f(x) / (f(x) + f(1 - x)))
+}
