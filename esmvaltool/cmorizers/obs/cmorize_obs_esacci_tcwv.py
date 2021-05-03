569f5eaec78392fd1ee8892280514cabe58613c1
“katjaweigel” <weigel@iup.physik.uni-bremen.de>
2021-05-03 13:56:08 +0200

    Prospector and yamllint checks.

--- esmvaltool/cmorizers/obs/cmorize_obs_esacci_tcwv.py
+++ esmvaltool/cmorizers/obs/cmorize_obs_esacci_tcwv.py
@@ -23,8 +23,9 @@ import os
 import iris
 
 from esmvalcore.preprocessor import concatenate
-from .utilities import (convert_timeunits, fix_coords, fix_var_metadata,
-                        save_variable, set_global_atts)
+from esmvaltool.cmorizers.obs.utilities import (convert_timeunits, fix_coords,
+                                                fix_var_metadata,
+                                                save_variable, set_global_atts)
 
 logger = logging.getLogger(__name__)
 
@@ -47,19 +48,20 @@ def extract_variable(var_info, raw_info, attrs, year):
 
 
 def cmorization(in_dir, out_dir, cfg, _):
-    cmor_table = cfg['cmor_table']
+    """Cmorize data."""
+    # cmor_table = cfg['cmor_table']
     glob_attrs = cfg['attributes']
 
     # run the cmorization
     for var, vals in cfg['variables'].items():
-        var_info = cmor_table.get_variable(vals['mip'], var)
+        var_info = cfg['cmor_table'].get_variable(vals['mip'], var)
         glob_attrs['mip'] = vals['mip']
         raw_info = {'name': vals['raw'], 'file': vals['file']}
         inpfile = os.path.join(in_dir, cfg['filename'])
         logger.info("CMORizing var %s from file type %s", var, inpfile)
-        years = range(vals['start_year'], vals['end_year'] + 1)
+        # years = range(vals['start_year'], vals['end_year'] + 1)
         months = ["0" + str(mo) for mo in range(1, 10)] + ["10", "11", "12"]
-        for year in years:
+        for year in range(vals['start_year'], vals['end_year'] + 1):
             monthly_cubes = []
             for month in months:
                 raw_info['file'] = inpfile.format(year=year, month=month)
@@ -67,8 +69,8 @@ def cmorization(in_dir, out_dir, cfg, _):
                             raw_info['file'])
                 cube = extract_variable(var_info, raw_info, glob_attrs, year)
                 monthly_cubes.append(cube)
-            yearly_cube = concatenate(monthly_cubes)
-            save_variable(yearly_cube,
+            # yearly_cube = concatenate(monthly_cubes)
+            save_variable(concatenate(monthly_cubes),
                           var,
                           out_dir,
                           glob_attrs,
