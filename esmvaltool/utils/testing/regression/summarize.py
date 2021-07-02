67fd120241ea95d759860b9640a10c9bf3e1474a
Bouwe Andela <b.andela@esciencecenter.nl>
2021-07-02 10:36:56 +0200

    Add documentation

--- esmvaltool/utils/testing/regression/summarize.py
+++ esmvaltool/utils/testing/regression/summarize.py
@@ -1,10 +1,11 @@
-import sys
+"""Write an index.html file in a directory containing recipe runs."""
+import argparse
 import textwrap
 from pathlib import Path
 
 
-def write_summary(lines):
-
+def write_index_html(lines, output_dir):
+    """Write lines to index.html."""
     header = textwrap.dedent("""
     <!doctype html>
     <html>
@@ -21,31 +22,38 @@ def write_summary(lines):
     """)
     lines = ["      " + line for line in lines]
     text = header + "\n".join(lines) + footer
-    Path('index.html').write_text(text)
+
+    index_file = output_dir / 'index.html'
+    index_file.write_text(text)
+    print(f"Wrote file://{index_file.absolute()}")
 
 
 def link(url, text):
+    """Format text as html link."""
     return '<a href="' + url + '">' + text + '</a>'
 
 
-def td(txt):
-    return "<td>" + txt + "</td>"
-
-
-def th(txt):
-    return "<th>" + txt + "</th>"
-
-
 def tr(entries):
+    """Format text entries as html table row."""
     return "<tr>" + "  ".join(entries) + "</tr>"
 
 
-def generate_summary(output_dir):
+def th(txt):
+    """Format text as html table header."""
+    return "<th>" + txt + "</th>"
 
+
+def td(txt):
+    """Format text as html table data."""
+    return "<td>" + txt + "</td>"
+
+
+def generate_summary(output_dir):
+    """Generate the lines of text for the summary view."""
     lines = []
 
-    table_header = ["status", "recipe"]
-    lines.append(tr(th(txt) for txt in table_header))
+    column_titles = ["status", "recipe"]
+    lines.append(tr(th(txt) for txt in column_titles))
 
     for recipe_dir in sorted(Path(output_dir).glob('recipe_*')):
         log = recipe_dir / 'run' / 'main_log.txt'
@@ -61,10 +69,17 @@ def generate_summary(output_dir):
     return lines
 
 
-def main(output_dir):
+def main():
+    """Run the program."""
+    parser = argparse.ArgumentParser(description=__doc__)
+    parser.add_argument('output_dir',
+                        default='.',
+                        type=Path,
+                        help='ESMValTool output directory.')
+    args = parser.parse_args()
 
-    write_summary(generate_summary(output_dir))
+    write_index_html(generate_summary(args.output_dir), args.output_dir)
 
 
 if __name__ == '__main__':
-    main(sys.argv[1])
+    main()
