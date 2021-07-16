1580440c17da3810c9c5d1bc2d107bbf4b1e8608
Jon Lillis <jonathan.lillis@hotmail.com>
2021-07-16 15:27:33 +0100

    Group data by model.

--- esmvaltool/diag_scripts/radiation_budget/radiation_budget.py
+++ esmvaltool/diag_scripts/radiation_budget/radiation_budget.py
@@ -2,7 +2,7 @@ import logging
 
 import iris
 
-from esmvaltool.diag_scripts.shared import run_diagnostic
+from esmvaltool.diag_scripts.shared import group_metadata, run_diagnostic
 
 
 def load_cubes(filenames):
@@ -25,6 +25,23 @@ def load_cubes(filenames):
     return cubes
 
 
+def get_filenames(group):
+    """Return all the filenames for the group.
+
+    Parameters
+    ----------
+    group : list(dict)
+        The grouped metadata describing preprocessed data.
+
+    Returns
+    -------
+    list
+        All the filenames for the group.
+    """
+    filenames = [item["filename"] for item in group]
+    return filenames
+
+
 def main(config):
     """Radiation budget for HadGEM3 vs UKESM1.
 
@@ -33,9 +50,17 @@ def main(config):
     config : dict
         The ESMValTool configuration.
     """
+    logger = logging.getLogger(__name__)
+
     input_data = config["input_data"]
-    filenames = input_data.keys()
-    _ = load_cubes(filenames)
+    datasets = group_metadata(input_data.values(), "dataset")
+
+    for dataset, group in datasets.items():
+        # 'dataset' is the name of the dataset.
+        # 'group' is a list of dictionaries containing metadata.
+        logger.info(f"Processing data for {dataset}")
+        filenames = get_filenames(group)
+        _ = load_cubes(filenames)
 
 
 if __name__ == "__main__":
