65a94fe7bec5ed597bb7678c4ab025baded42b45
Javier Vegas-Regidor <javier.vegas@bsc.es>
2021-08-04 13:31:23 +0200

    Fix some tests

--- tests/unit/cmorizers/test_datasets_info.py
+++ tests/unit/cmorizers/test_datasets_info.py
@@ -1,18 +1,19 @@
-import os
 import datetime
+import os
+
 import yamale
 import yaml
 
-yaml_folder = os.path.abspath(os.path.join(
-    os.path.dirname(__file__), '../../..', 'esmvaltool/cmorizers/data'))
+yaml_folder = os.path.abspath(
+    os.path.join(os.path.dirname(__file__), '../../..',
+                 'esmvaltool/cmorizers/data'))
 
-recipes_folder = os.path.abspath(os.path.join(
-    os.path.dirname(__file__), '../../..', 'esmvaltool/recipes'))
+recipes_folder = os.path.abspath(
+    os.path.join(os.path.dirname(__file__), '../../..', 'esmvaltool/recipes'))
 
 
 def test_only_datasets_are_present():
-    recipe = yamale.make_data(
-        os.path.join(yaml_folder, 'datasets.yml'))
+    recipe = yamale.make_data(os.path.join(yaml_folder, 'datasets.yml'))
     schema = yamale.make_schema(
         os.path.join(yaml_folder, 'datasets_schema.yml'))
     yamale.validate(schema, recipe)
@@ -22,8 +23,8 @@ def test_latest_version_format():
     with open(os.path.join(yaml_folder, 'datasets.yml'), 'r') as file:
         cfg = yaml.safe_load(file)
     for dataset_info in cfg['datasets'].values():
-        datetime.datetime.strptime(
-            str(dataset_info['last_access']), "%Y%m%d")
+        datetime.datetime.strptime(str(dataset_info['last_access']),
+                                   "%Y-%m-%d")
 
 
 def test_datasets_are_added_to_test_recipe():
