955c0d94e932921bd0497085020c8fbb5d7f1b67
Valeriu Predoi <valeriu.predoi@gmail.com>
2021-08-20 13:08:18 +0100

    pin esmvalcore to latest

--- setup.py
+++ setup.py
@@ -33,7 +33,7 @@ REQUIREMENTS = {
         'ecmwf-api-client',
         'eofs',
         'ESMPy',
-        'esmvalcore>=2.3.0,<2.4',
+        'esmvalcore>=2.3.1,<2.4',
         'fiona',
         'GDAL',
         'jinja2',
