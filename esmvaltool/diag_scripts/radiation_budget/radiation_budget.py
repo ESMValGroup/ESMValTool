d0c2310c70148b2742d9e8113523503daf6a0405
Jon Lillis <jonathan.lillis@hotmail.com>
2021-09-06 12:29:28 +0100

    Address Codacy '1 blank line required between summary line and description'

--- esmvaltool/diag_scripts/radiation_budget/radiation_budget.py
+++ esmvaltool/diag_scripts/radiation_budget/radiation_budget.py
@@ -1,7 +1,10 @@
-"""PLACEHOLDER modified from AA docs: The aim of monitoring the energy budget
-is to understand the (im)balance of energy flux between the atmosphere and the
-surface of a model due to its link with the hydrological cycle and climate
-change."""
+"""Radiation budget diagnostic script.
+
+PLACEHOLDER modified from AA docs: The aim of monitoring the energy
+budget is to understand the (im)balance of energy flux between the
+atmosphere and the surface of a model due to its link with the
+hydrological cycle and climate change.
+"""
 
 # To run the doctests:
 # % cd ESMValTool/esmvaltool/
