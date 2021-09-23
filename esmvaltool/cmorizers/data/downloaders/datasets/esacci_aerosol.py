8ddfa55a674fc8f7e5d700ee6f55364636087767
Javier Vegas-Regidor <javier.vegas@bsc.es>
2021-09-23 15:36:49 +0200

    Fix ESACCI Aerosol download

--- esmvaltool/cmorizers/data/downloaders/datasets/esacci_aerosol.py
+++ esmvaltool/cmorizers/data/downloaders/datasets/esacci_aerosol.py
@@ -38,7 +38,7 @@ def download_dataset(config, dataset, start_date, end_date, overwrite):
 
     while loop_date <= end_date:
         year = loop_date.year
-        if year < 2002:
+        if year < 2003:
             downloader.set_cwd('ATSR2_SU/L3/v4.21/MONTHLY')
         else:
             downloader.set_cwd('AATSR_SU/L3/v4.21/MONTHLY')
