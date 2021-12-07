44d293740b8ac93e05cbcbe56b03bd7591e7d0b7
Javier Vegas-Regidor <javier.vegas@bsc.es>
2021-12-07 12:37:03 +0100

    Fix CDS Soil Moisture

--- esmvaltool/cmorizers/data/downloaders/cds.py
+++ esmvaltool/cmorizers/data/downloaders/cds.py
@@ -54,7 +54,12 @@ class CDSDownloader(BaseDownloader):
         self._request_dict = request_dictionary
         self.extra_name = extra_name
 
-    def download(self, year, month, day=None, file_format='tar'):
+    def download(self,
+                 year,
+                 month,
+                 day=None,
+                 file_pattern=None,
+                 file_format='tar'):
         """Download a specific month from the CDS.
 
         Parameters
@@ -75,13 +80,13 @@ class CDSDownloader(BaseDownloader):
             else:
                 request_dict['day'] = f"{day:02d}"
 
-        date_str = f"{self.dataset}{self.extra_name}_{year}{month:02d}"
+        date_str = f"{year}{month:02d}"
         if day:
             if not isinstance(day, Iterable):
                 date_str += f"{day:02d}"
 
         os.makedirs(self.local_folder, exist_ok=True)
-        file_path = f"{self.dataset}_{date_str}.{file_format}"
+        file_path = f"{file_pattern}_{date_str}.{file_format}"
         self.download_request(file_path, request_dict)
 
     def download_request(self, filename, request=None):
--- esmvaltool/cmorizers/data/downloaders/datasets/cds_satellite_soil_moisture.py
+++ esmvaltool/cmorizers/data/downloaders/datasets/cds_satellite_soil_moisture.py
@@ -49,33 +49,57 @@ def download_dataset(config, dataset, dataset_info, start_date, end_date,
         overwrite=overwrite,
     )
 
+    monthly_downloaders = {}
+    daily_downloaders = {}
+
+    for sensor in ['combined_passive_and_active', 'passive', 'active']:
+        monthly_downloaders[sensor] = get_downloader(config, dataset,
+                                                     dataset_info, overwrite,
+                                                     sensor, 'month')
+        daily_downloaders[sensor] = get_downloader(config, dataset,
+                                                   dataset_info, overwrite,
+                                                   sensor, 'day')
     while loop_date <= end_date:
-        downloader.download(loop_date.year, loop_date.month)
+        for sensor, downloader in monthly_downloaders.items():
+            pattern = f'cds-satellite-soil-moisture_cdr_{sensor}_monthly'
+            downloader.download(loop_date.year,
+                                loop_date.month,
+                                file_pattern=pattern)
         loop_date += relativedelta.relativedelta(months=1)
 
+    loop_date = start_date
+    while loop_date <= end_date:
+        for sensor, downloader in daily_downloaders.items():
+            downloader.download(
+                loop_date.year, loop_date.month, [
+                    f'{i+1:02d}' for i in range(
+                        calendar.monthrange(loop_date.year, loop_date.month)
+                        [1])
+                ], f'cds-satellite-soil-moisture_cdr_{sensor}_daily')
+        loop_date += relativedelta.relativedelta(months=1)
+    unpack_files_in_folder(downloader.local_folder)
+
+
+def get_downloader(config, dataset, dataset_info, overwrite, sensor,
+                   frequency):
+    if sensor == 'active':
+        variable = 'surface_soil_moisture'
+    else:
+        variable = 'volumetric_surface_soil_moisture'
     downloader = CDSDownloader(
         product_name='satellite-soil-moisture',
         request_dictionary={
             'format': 'tgz',
-            'variable': 'volumetric_surface_soil_moisture',
-            'type_of_sensor': 'combined_passive_and_active',
+            'variable': variable,
+            'type_of_sensor': sensor,
             'day': '01',
             'type_of_record': 'cdr',
             'version': 'v201912.0.0',
-            'time_aggregation': 'day_average',
+            'time_aggregation': f'{frequency}_average',
         },
         config=config,
         dataset=dataset,
         dataset_info=dataset_info,
         overwrite=overwrite,
-        extra_name='_monthly',
     )
-    loop_date = start_date
-    while loop_date <= end_date:
-        downloader.download(loop_date.year, loop_date.month, [
-            f'{i+1:02d}' for i in range(
-                calendar.monthrange(loop_date.year, loop_date.month)[1])
-        ])
-        loop_date += relativedelta.relativedelta(months=1)
-
-    unpack_files_in_folder(downloader.local_folder)
+    return downloader
