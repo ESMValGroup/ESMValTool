a3e0aa754a5f16347512af8766b505c8a70ebd53
Birgit Hassler <birgit.hassler@dlr.de>
2022-01-18 13:49:06 +0100

    Hopefully the last act of cleaning up the code

--- esmvaltool/diag_scripts/mpqb/mpqb_utils.py
+++ esmvaltool/diag_scripts/mpqb/mpqb_utils.py
@@ -7,7 +7,7 @@ import yaml
 def get_mpqb_cfg(cfgtype, cfgkey):
     """cfgtype: colormap, datasetcolor, datasetname"""
     # First read cfg file
-    cfg_filename = os.path.join(os.path.split(__file__)[0], 'mpqb_cfg.yml')
+    cfg_filename = os.path.join(os.path.split(__file__)[0], 'mpqb_cfg_xch4.yml')
     with open(cfg_filename, 'r') as handle:
         mpqb_cfg = yaml.safe_load(handle)
     # Defaults to specified defaults in yml file
