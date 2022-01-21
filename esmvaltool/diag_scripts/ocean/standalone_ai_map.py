628423a1756ca6bfbdb309bb62b5bbfe25d8a53b
Lee de Mora <ledm@pml.ac.uk>
2022-01-21 14:21:42 +0000

    working - ish plate carree

--- esmvaltool/diag_scripts/ocean/standalone_ai_map.py
+++ esmvaltool/diag_scripts/ocean/standalone_ai_map.py
@@ -11,15 +11,21 @@ import matplotlib
 # Force matplotlib to not use any Xwindows backend.
 matplotlib.use('Agg')
 
+
 import iris
 
 import numpy as np
 
 from shelve import open as shopen
-import os
+import os, shutil
 from glob import glob
 from netCDF4 import Dataset, num2date
 from matplotlib import pyplot
+import matplotlib.patches as mpatches
+
+import cartopy.crs as ccrs
+import cartopy.feature as cfeature
+
 
 from esmvaltool.diag_scripts.ocean import diagnostic_tools as diagtools
 from esmvalcore.preprocessor._time import extract_time, annual_statistics, regrid_time
@@ -38,32 +44,37 @@ def compute_radius(ortho, radius_degrees, proj= ccrs.PlateCarree(), lat=0, lon=0
     _, y1 = ortho.transform_point(lon, phi1, proj)
     return abs(y1)
 
-def add_shadow(fig,ax,):
+def add_shadow(fig,ax, proj,):
 
     num_shadows = 10
-    for w, in np.arange(0, 2, num_shadows):
-        h = w/2.
+    for i,w in enumerate(np.linspace(0., 2., num_shadows+1)): 
+        h = w/8.
         alpha = 1./num_shadows
-        ellipse = mpl.patches.Ellipse(xy=(0, -0.1), width=w, height=h, c=(0,0,0,alpha), transform=ax.transAxes)
+        ellipse = mpatches.Ellipse(xy=(0, -0.1), width=w, height=h, fc=(0,0,0,alpha), transform=ax.transAxes,zorder = 40)
         ax.add_artist(ellipse)
 
     return fig, ax
 
-def main():
-    path = 'images/AI_map.png'
-    fig = plt.figure()
-    proj = ccrs.Orthographic(-10, -20)
-    ax = fig.add_subplot(1, 1, 1, projection=proj)
 
-    ax.add_feature(cfeature.OCEAN, zorder=0)
-    ax.add_feature(cfeature.LAND, zorder=0, edgecolor='black')
+def main():
+    path = diagtools.folder('images/')+'AI_map_4.png'
+    #if os.path.exists(path):
+        
+    fig = pyplot.figure()
+    #proj = ccrs.Orthographic(-10, -20)
+    proj = ccrs.PlateCarree()
+    ax = fig.add_subplot(1, 1, 1, projection=proj)
+#   ax.stock_img()
+
+    #ax.add_feature(cfeature.OCEAN, zorder=2)
+    #ax.add_feature(cfeature.LAND, zorder=2, edgecolor='black')
 
 
     # lat_bnd = 20.
     # lon_bnd = 30.
     central_longitude = -14.25 #W #-160.+3.5
     central_latitude = -7.56
-    # ax0.set_extent([central_longitude-lon_bnd,
+    # ax.set_extent([central_longitude-lon_bnd,
     #                central_longitude+lon_bnd,
     #                central_latitude-lat_bnd,
     #                central_latitude+lat_bnd, ])
@@ -71,16 +82,29 @@ def main():
     # Add transect transect at longitude 23°W, latitude -3°:3° and depth from 0 to 400m
 
     r_ortho = compute_radius(proj, 3., proj=proj, lat = central_latitude, lon=central_longitude,)
-    ax0.add_patch(mpatches.Circle(xy=[central_longitude, central_latitude], radius=r_ortho, color='black', alpha=0.3, transform=proj, zorder=30))
+    ax.add_patch(mpatches.Circle(xy=[central_longitude, central_latitude], radius=r_ortho, color='black', alpha=0.3, transform=proj, zorder=30))
 
-    ax0.plot([-23, -23], [-3, -3], k='green', lw=2., transform=ccrs.PlateCarree())
+    ax.plot([-23, -23], [-3, 3], c='green', lw=2., transform=proj, ) #transform=ccrs.PlateCarree(), zorder=20,)
 
     #ax.set_global()
-    ax0.gridlines()
-    fig, ax0 = add_shadow(fig, ax0)
-    #plt.show()
+    ax.gridlines()
+    # ax.background_img(name='50-natural-earth-1-downsampled.png')
+    ax.stock_img()
+
+
+    fig, ax = add_shadow(fig, ax, proj)
+    #pyplot.show()
     print('saving figure to:', path)
-    plt.savefig(path)
+    pyplot.savefig(path)
+    pyplot.close()
+    
+    copy_path = diagtools.folder('/home/users/ldemora/Master_CRACAB_plots/figure1')+os.path.basename(path)
+    print('copying to ', copy_path)
+    shutil.copy(path, copy_path)
+    
+    print('Now run on pmpc:')
+    print('rsync -avP ldemora@xfer1.jasmin.ac.uk:/home/users/ldemora/Master_CRACAB_plots /users/modellers/ledm/ImagesFromJasmin/.')
+
 
 if __name__ == '__main__':
     main()
