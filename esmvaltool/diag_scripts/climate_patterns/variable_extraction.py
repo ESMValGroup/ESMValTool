51a2f81557bc90f6fbe4577e599350ad173a46ea
mo-gregmunday <gregory.munday@metoffice.gov.uk>
2022-03-28 08:55:07 +0100

    Added anomaly dataset generation

--- esmvaltool/diag_scripts/climate_patterns/variable_extraction.py
+++ esmvaltool/diag_scripts/climate_patterns/variable_extraction.py
@@ -47,41 +47,27 @@ def prepare_cube(cube, dataset):
     # time extraction
     if dataset["exp"] == "historical":
         cube_30yr = cube.extract(
-            iris.Constraint(year=lambda cell: 2010 >= cell > 1980))
-    elif dataset["exp"] == "ssp370":
-        cube_30yr = cube.extract(
-            iris.Constraint(year=lambda cell: 2080 > cell >= 2050))
+            iris.Constraint(time=lambda t: 1980 <= t.point.year <= 2010,
+                            month_number=lambda t: 1 <= t.point <= 12))
+        cube_aggregated = make_monthly_climatology(cube_30yr)
+        cube_clipped = cube_aggregated.extract(
+            iris.Constraint(latitude=lambda cell: 82.5 >= cell >= -55))
+    else:
+        # constrain latitudes to fit IMOGEN framework grid size
+        cube_clipped = cube.extract(
+            iris.Constraint(latitude=lambda cell: 82.5 >= cell >= -55))
 
-    # aggregation, latitude clipping
-    cube_aggregated = make_monthly_climatology(cube_30yr)
-
-    # anomaly generation
-    anomaly = make_variable_anomaly(cube_30yr, cube_aggregated)
-
-    cube_clipped = cube_aggregated.extract(
-        iris.Constraint(latitude=lambda cell: 82.5 >= cell >= -55))
-
-    anomaly_clipped = anomaly.extract(
-        iris.Constraint(latitude=lambda cell: 82.5 >= cell >= -55))
-
-    return cube_clipped, anomaly_clipped
+    return cube_clipped
 
 
 def make_monthly_climatology(cube):
-    if not cube.coords("month"):
-        iris.coord_categorisation.add_month(cube, "time", "month")
-    cube_month_climatol = cube.aggregated_by(["month"], iris.analysis.MEAN)
+    if not cube.coords("month_number"):
+        iris.coord_categorisation.add_month_number(cube, "time",
+                                                   "month_number")
+    cube_month_climatol = cube.aggregated_by("month_number",
+                                             iris.analysis.MEAN)
 
     return cube_month_climatol
-
-
-def make_variable_anomaly(cube_normal, cube_aggregated):
-    if not cube_normal.coords("month"):
-        iris.coord_categorisation.add_month(cube_normal, "time", "month")
-    anomaly = cube_normal.aggregated_by(["month"],
-                                        iris.analysis.MAX) - cube_aggregated
-
-    return anomaly
 
 
 def rename_variables(cube):
@@ -114,38 +100,10 @@ def rename_variables(cube):
     return cube
 
 
-def main(cfg):
-    # Gets a description of the preprocessed data that we will use as input.
-    input_data = cfg["input_data"].values()
-    hist_list = iris.load([])
-    scenario_list = iris.load([])
-    anomaly_list = iris.load([])
-
-    for dataset in input_data:
-        input_file = dataset["filename"]
-
-        # manipulating single cube
-        cube_initial = compute_diagnostic(input_file)
-        cube_prepped, anomaly_prepped = prepare_cube(cube_initial, dataset)
-
-        # renaming cube function
-        cube_renamed = rename_variables(cube_prepped)
-        anomaly_renamed = rename_variables(anomaly_prepped)
-
-        # appending
-        if dataset["exp"] == "historical":
-            hist_list.append(cube_renamed)
-            anomaly_list.append(anomaly_renamed)
-        elif dataset["exp"] == "ssp370":
-            scenario_list.append(cube_renamed)
-        else:
-            pass
-
-    # calculate diurnal temperature range cube
+def calculate_diurnal_range(hist_list, scenario_list):
     temp_range_list_hist = iris.load([])
     temp_range_list_scen = iris.load([])
-    temp_range_list_anom = iris.load([])
-    comb_list = [hist_list, scenario_list, anomaly_list]
+    comb_list = [hist_list, scenario_list]
 
     for i in range(len(comb_list)):
         for cube in comb_list[i]:
@@ -153,23 +111,49 @@ def main(cfg):
                     or cube.var_name == "tasmin") and cube in hist_list:
                 temp_range_list_hist.append(cube)
             elif (cube.var_name == "tasmax"
-                  or cube.var_name == "tasmin" and cube in scenario_list):
+                  or cube.var_name == "tasmin") and cube in scenario_list:
                 temp_range_list_scen.append(cube)
-            elif (cube.var_name == "tasmax"
-                  or cube.var_name == "tasmin" and cube in anomaly_list):
-                temp_range_list_anom.append(cube)
             else:
                 pass
 
     derived_variable_hist = diurnal_temp_range(temp_range_list_hist)
     derived_variable_scenario = diurnal_temp_range(temp_range_list_scen)
-    derived_variable_anom = diurnal_temp_range(temp_range_list_anom)
+
+    return derived_variable_hist, derived_variable_scenario
+
+
+def main(cfg):
+    # gets a description of the preprocessed data that we will use as input.
+    input_data = cfg["input_data"].values()
+    hist_list = iris.load([])
+    scenario_list = iris.load([])
+
+    for dataset in input_data:
+        input_file = dataset["filename"]
+
+        # manipulating single cube
+        cube_initial = compute_diagnostic(input_file)
+        cube_prepped = prepare_cube(cube_initial, dataset)
+
+        # renaming cubes
+        cube_renamed = rename_variables(cube_prepped)
+
+        # appending
+        if dataset["exp"] == "historical":
+            hist_list.append(cube_renamed)
+        elif dataset["exp"] == "ssp370":
+            scenario_list.append(cube_renamed)
+        else:
+            pass
+
+    # calculate diurnal temperature range cube
+    derived_variable_hist, derived_variable_scenario = calculate_diurnal_range(
+        hist_list, scenario_list)
 
     # creating cube list without tasmax or tasmin
     # (since we just wanted the diurnal range)
     hist_list_final = iris.load([])
     scen_list_final = iris.load([])
-    anom_list_final = iris.load([])
 
     for cube in hist_list:
         if not (cube.var_name == "tasmax" or cube.var_name == "tasmin"):
@@ -179,13 +163,18 @@ def main(cfg):
         if not (cube.var_name == "tasmax" or cube.var_name == "tasmin"):
             scen_list_final.append(cube)
 
-    for cube in anomaly_list:
-        if not (cube.var_name == "tasmax" or cube.var_name == "tasmin"):
-            anom_list_final.append(cube)
-
     hist_list_final.append(derived_variable_hist)
     scen_list_final.append(derived_variable_scenario)
-    anom_list_final.append(derived_variable_anom)
+
+    # calculate anomaly
+    anom_list_final = scen_list_final.copy()
+
+    # calc the anom by subtracting the monthly climatology from
+    # the time series
+    for i in range(len(scen_list_final)):
+        i_months = anom_list_final[i].coord(
+            'month_number').points - 1  # -1 because months are numbered 1..12
+        anom_list_final[i].data -= hist_list_final[i][i_months].data
 
     # list of variable cube lists
     list_of_cubelists = [hist_list_final, scen_list_final, anom_list_final]
