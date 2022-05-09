62c41f21e6033343cf0b12dbd7e76e22c556ffcb
Remi Kazeroni <remi.kazeroni@dlr.de>
2022-05-09 09:35:48 +0200

    fix docstring

--- esmvaltool/cmorizers/data/downloaders/datasets/kadow2020.py
+++ esmvaltool/cmorizers/data/downloaders/datasets/kadow2020.py
@@ -19,6 +19,10 @@ def download_dataset(config, dataset, dataset_info, start_date, end_date,
         Name of the dataset
     dataset_info : dict
          Dataset information from the datasets.yml file
+    start_date : datetime
+        Start of the interval to download
+    end_date : datetime
+        End of the interval to download
     overwrite : bool
         Overwrite already downloaded files
     """
