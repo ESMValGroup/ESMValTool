56628628140049dd511902670dc365ad50d1399d
Manuel Schlund <manuel.schlund@dlr.de>
2022-05-12 17:21:20 +0200

    Implemented timeseries for monitor/multi_datasets.py

--- esmvaltool/diag_scripts/monitor/multi_datasets.py
+++ esmvaltool/diag_scripts/monitor/multi_datasets.py
@@ -34,7 +34,7 @@ facet_used_for_labels: str, optional (default: 'dataset')
     select the correct ``plot_kwargs`` for the different datasets (see
     configuration options for the different plot types below).
 figure_kwargs: dict, optional
-    Optional keyword arguments of :func:`matplotlib.pyplot.figure`.
+    Optional keyword arguments for :func:`matplotlib.pyplot.figure`.
 plots: dict, optional
     Plot types plotted by this diagnostic. Dictionary keys must be
     'timeseries', 'map', or 'profile'. Dictionary values are dictionaries used
@@ -47,22 +47,31 @@ seaborn_settings: dict, optional
 
 Configuration options for timeseries
 ------------------------------------
+annual_mean_kwargs: dict, optional
+    Optional keyword arguments for :func:`iris.plot.plot` for plotting annual
+    means. Use ``annual_mean_kwargs`` to not show annual means.
 legend_kwargs: dict, optional
-    Optional keyword arguments of :func:`matplotlib.pyplot.legend`. Use
+    Optional keyword arguments for :func:`matplotlib.pyplot.legend`. Use
     ``legend_kwargs: false`` to not show legends.
 plot_kwargs: dict, optional
-    Additional keyword arguments for :func:`iris.plot.plot`. Dictionary keys
-    are elements identified by ``facet_used_for_labels`` or ``default``, e.g.,
+    Optional keyword arguments for :func:`iris.plot.plot`. Dictionary keys are
+    elements identified by ``facet_used_for_labels`` or ``default``, e.g.,
     ``CMIP6`` if ``facet_used_for_labels: project`` or ``historical`` if
     ``facet_used_for_labels: exp``. Dictionary values are dictionaries used as
-    keyword arguments for :func:`iris.plot.plot`. Examples: ``default:
-    linestyle: '-', CMIP6: {color: red, linestyle: '--'}, OBS: {color:
-    black}``.
+    keyword arguments for :func:`iris.plot.plot`. String arguments can include
+    facets in curly brackets which will be derived from the corresponding
+    dataset, e.g., ``{project}``, ``{short_name}``, ``{exp}``. Examples:
+    ``default: {linestyle: '-', label: '{project}'}, CMIP6: {color: red,
+    linestyle: '--'}, OBS: {color: black}``.
 pyplot_kwargs: dict, optional
     Optional calls to functions of :mod:`matplotlib.pyplot`. Dictionary keys
     are functions of :mod:`matplotlib.pyplot`. Dictionary values are used as
-    single  argument for these functions. Examples: ``title: Awesome Plot,
-    ``xlabel: x``, ``xlim: [0, 5]``.
+    single argument for these functions. String arguments can include facets in
+    curly brackets which will be derived from the datasets plotted in the
+    corresponding plot, e.g., ``{short_name}``, ``{exp}``. Facets like
+    ``{project}`` that vary between the different datasets will be transformed
+    to something like  ``ambiguous_project``.  Examples: ``title: 'Awesome Plot
+    of {long_name}', ``xlabel: '{short_name}'``, ``xlim: [0, 5]``.
 
 """
 import logging
@@ -73,12 +82,12 @@ from pprint import pformat
 import iris
 import matplotlib.pyplot as plt
 import seaborn as sns
+from iris.coord_categorisation import add_year
 
 import esmvaltool.diag_scripts.shared.iris_helpers as ih
 from esmvaltool.diag_scripts.monitor.monitor_base import MonitorBase
 from esmvaltool.diag_scripts.shared import (
     ProvenanceLogger,
-    get_plot_filename,
     group_metadata,
     run_diagnostic,
 )
@@ -97,14 +106,13 @@ class MultiDatasets(MonitorBase):
         self.cfg = deepcopy(self.cfg)
         self.cfg.setdefault('facet_used_for_labels', 'dataset')
         self.cfg.setdefault('figure_kwargs', {})
-        self.cfg.setdefault('legend_kwargs', {})
         self.cfg.setdefault('savefig_kwargs', {
             'bbox_inches': 'tight',
             'dpi': 300,
             'orientation': 'landscape',
         })
         self.cfg.setdefault('seaborn_settings', {})
-        logger.info("Using key '%s' to create titles for datasets",
+        logger.info("Using facet '%s' to create labels",
                     self.cfg['facet_used_for_labels'])
 
         # Load input data
@@ -113,11 +121,13 @@ class MultiDatasets(MonitorBase):
 
         # Check given plot types
         self.supported_plot_types = ['timeseries', 'map', 'profile']
-        for plot_type in self.plots:
+        for (plot_type, plot_options) in self.plots.items():
             if plot_type not in self.supported_plot_types:
                 raise ValueError(
                     f"Got unexpected plot type '{plot_type}' for option "
                     f"'plots', expected one of {self.supported_plot_types}")
+            if plot_options is None:
+                self.plots[plot_type] = {}
 
         # Check that facet_used_for_labels is present for every dataset
         for dataset in self.input_data:
@@ -130,12 +140,35 @@ class MultiDatasets(MonitorBase):
         # Load seaborn settings
         sns.set(**self.cfg['seaborn_settings'])
 
+    def _get_label(self, dataset):
+        """Get label of dataset."""
+        return dataset[self.cfg['facet_used_for_labels']]
+
     def _get_plot_kwargs(self, plot_type, dataset):
         """Get keyword arguments for plot functions."""
         all_plot_kwargs = self.plots[plot_type].get('plot_kwargs', {})
+
+        # First get default kwargs, then overwrite them with dataset-specific
+        # ones
         plot_kwargs = all_plot_kwargs.get('default', {})
-        label = dataset[self.cfg['facet_used_for_labels']]
+        label = self._get_label(dataset)
         plot_kwargs.update(all_plot_kwargs.get(label, {}))
+
+        # Replace facets with dataset entries for string arguments
+        for (key, val) in plot_kwargs.items():
+            if isinstance(val, str):
+                try:
+                    val = val.format(**dataset)
+                except KeyError as exc:
+                    raise ValueError(
+                        f"Not all necessary facets in {plot_type} plot_kwargs "
+                        f"'{key}: {val}' available for dataset"
+                        f"\n{pformat(dataset)}") from exc
+                plot_kwargs[key] = val
+
+        # Set default label
+        plot_kwargs.setdefault('label', label)
+
         return plot_kwargs
 
     def _load_and_preprocess_data(self):
@@ -149,10 +182,18 @@ class MultiDatasets(MonitorBase):
             dataset['cube'] = cube
         return input_data
 
-    def _process_pyplot_kwargs(self, plot_type):
+    def _process_pyplot_kwargs(self, plot_type, dataset):
         """Process functions for :mod:`matplotlib.pyplot`."""
         pyplot_kwargs = self.plots[plot_type].get('pyplot_kwargs', {})
         for (func, arg) in pyplot_kwargs.items():
+            if isinstance(arg, str):
+                try:
+                    arg = arg.format(**dataset)
+                except KeyError as exc:
+                    raise ValueError(
+                        f"Not all necessary facets in {plot_type} "
+                        f"pyplot_kwargs '{func}: {arg}' available for dataset"
+                        f"\n{pformat(dataset)}") from exc
             getattr(plt, func)(arg)
 
     @staticmethod
@@ -165,11 +206,28 @@ class MultiDatasets(MonitorBase):
                 f"{expected_dimensions} for plot {plot_type}, got "
                 f"{cube.ndim:d}D cube: {cube.summary(shorten=True)}")
 
+    @staticmethod
+    def _get_multi_dataset_facets(datasets):
+        """Derive common facets for multiple datasets."""
+        all_keys = {key for dataset in datasets for key in dataset}
+        multi_dataset_facets = {}
+        for key in all_keys:
+            if all(d.get(key) == datasets[0].get(key) for d in datasets):
+                multi_dataset_facets[key] = datasets[0][key]
+            else:
+                multi_dataset_facets[key] = f'ambiguous_{key}'
+        return multi_dataset_facets
+
     def create_timeseries_plot(self, datasets):
         """Create time series plot."""
         plot_type = 'timeseries'
         if plot_type not in self.plots:
             return
+
+        if not datasets:
+            raise ValueError("No input data given")
+
+        logger.info("Plotting %s", plot_type)
         plt.figure(**self.cfg['figure_kwargs'])
 
         # Plot all datasets in single figure
@@ -179,43 +237,56 @@ class MultiDatasets(MonitorBase):
             cube = dataset['cube']
             self._check_cube_dimensions(cube, ['time'], plot_type)
 
-            # Plot monthly means
-        #     plot_kwargs = get_plot_kwargs(cfg, dataset)
-        #     iris.plot.plot(cube, label=dataset[cfg['facet_used_for_labels']], **plot_kwargs)
+            # Plot original time series
+            plot_kwargs = self._get_plot_kwargs(plot_type, dataset)
+            iris.plot.plot(cube, **plot_kwargs)
 
-        #     # Plot annual means
-        #     iris.coord_categorisation.add_year(cube, 'time')
-        #     cube = cube.aggregated_by('year', iris.analysis.MEAN)
-        #     plot_kwargs['linestyle'] = ':'
-        #     iris.plot.plot(cube, **plot_kwargs)
+            # Plot annual means if desired
+            annual_mean_kwargs = self.plots[
+                plot_type].get('annual_mean_kwargs', {})
+            if annual_mean_kwargs is not False:
+                logger.debug("Plotting annual means")
+                if not cube.coords('year'):
+                    add_year(cube, 'time')
+                annual_mean_cube = cube.aggregated_by('year',
+                                                      iris.analysis.MEAN)
+                plot_kwargs.pop('label', None)
+                plot_kwargs.update(annual_mean_kwargs)
+                iris.plot.plot(annual_mean_cube, **plot_kwargs)
 
-        # # Plot appearance
-        # long_name = input_data[0]['long_name']
-        # short_name = input_data[0]['short_name']
-        # units = cube.units
-        # plt.title(f"Global Mean {long_name}")
-        # plt.xlabel("Year")
-        # plt.ylabel(f"{short_name} [{units}]")
-        # plt.legend()
+        # Default plot appearance
+        multi_dataset_facets = self._get_multi_dataset_facets(datasets)
+        plt.title(multi_dataset_facets['long_name'])
+        plt.xlabel('Time')
+        plt.ylabel(
+            f"{multi_dataset_facets['short_name']} "
+            f"[{multi_dataset_facets['units']}]")
 
-        # # Save plot
-        # plot_path = get_plot_filename(short_name, cfg)
-        # plt.savefig(plot_path, bbox_inches='tight', orientation='landscape')
-        # logger.info("Wrote %s", plot_path)
-        # plt.close()
+        # Legend
+        legend_kwargs = self.plots[plot_type].get('legend_kwargs', {})
+        if legend_kwargs is not False:
+            plt.legend(**legend_kwargs)
 
-        # # Provenance tracking
-        # caption = (f"Monthly mean (solid lines) and annual mean (dashed lines) "
-        #            f"time series of {input_data[0]['long_name']} for various "
-        #            f"datasets.")
-        # provenance_record = {
-        #     'ancestors': ancestors,
-        #     'authors': ['schlund_manuel'],
-        #     'caption': caption,
-        #     'plot_types': ['line'],
-        # }
-        # with ProvenanceLogger(cfg) as provenance_logger:
-        #     provenance_logger.log(plot_path, provenance_record)
+        # Customize plot appearance
+        self._process_pyplot_kwargs(plot_type, multi_dataset_facets)
+
+        # Save plot
+        plot_path = self.get_plot_path(plot_type, multi_dataset_facets)
+        plt.savefig(plot_path, **self.cfg['savefig_kwargs'])
+        logger.info("Wrote %s", plot_path)
+        plt.close()
+
+        # Provenance tracking
+        caption = (f"Time series of {multi_dataset_facets['long_name']} for "
+                   f"various datasets.")
+        provenance_record = {
+            'ancestors': ancestors,
+            'authors': ['schlund_manuel'],
+            'caption': caption,
+            'plot_types': ['line'],
+        }
+        with ProvenanceLogger(self.cfg) as provenance_logger:
+            provenance_logger.log(plot_path, provenance_record)
 
     def compute(self):
         """Plot preprocessed data."""
