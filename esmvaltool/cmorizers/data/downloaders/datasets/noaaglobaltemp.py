a5e70ad146df026b502698c11b5225fb6e33aa77
Lisa Bock <lisa.bock@dlr.de>
2022-05-13 09:22:30 +0200

    python cmorizer for noaaglobaltemp

---
+++ esmvaltool/cmorizers/data/downloaders/datasets/noaaglobaltemp.py
@@ -0,0 +1,41 @@
+"""Script to download NOAAGlobalTemp from its webpage."""
+import logging
+import os
+
+from esmvaltool.cmorizers.data.downloaders.wget import WGetDownloader
+
+logger = logging.getLogger(__name__)
+
+
+def download_dataset(config, dataset, dataset_info, start_date, end_date,
+                     overwrite):
+    """Download dataset.
+
+    Parameters
+    ----------
+    config : dict
+        ESMValTool's user configuration
+    dataset : str
+        Name of the dataset
+    dataset_info : dict
+         Dataset information from the datasets.yml file
+    start_date : datetime
+        Start of the interval to download
+    end_date : datetime
+        End of the interval to download
+    overwrite : bool
+        Overwrite already downloaded files
+    """
+    downloader = WGetDownloader(
+        config=config,
+        dataset=dataset,
+        dataset_info=dataset_info,
+        overwrite=overwrite,
+    )
+
+    os.makedirs(downloader.local_folder, exist_ok=True)
+    downloader.download_file(
+        "https://www.ncei.noaa.gov/data/noaa-global-surface-temperature/"
+        "v5/access/gridded/"
+        "NOAAGlobalTemp_v5.0.0_gridded_s188001_e202203_c20220408T133258.nc",
+        wget_options=[])
