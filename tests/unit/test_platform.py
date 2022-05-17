01965187180ac368e0213d11820f215acddb77a5
Valeriu Predoi <valeriu.predoi@gmail.com>
2022-05-17 12:58:29 +0100

    bespoke test for psy stuff

--- tests/unit/test_platform.py
+++ tests/unit/test_platform.py
@@ -1,4 +1,19 @@
+import os
 import sys
 
-print("Platform is dumdum...", sys.platform)
-print(x)
+from psyplot.config.logsetup import _get_home
+
+
+def test_psy_stuff(name='psyplot', env_key='PSYPLOTCONFIGDIR'):
+    print("Platform is dumdum...", sys.platform)
+    p = None
+    h = _get_home()
+    print("Home is dumdum...", h)
+    if ((sys.platform.startswith('linux') or sys.platform == 'darwin') and
+            h is not None):
+        p = os.path.join(h, '.config/' + name)
+    elif h is not None:
+        p = os.path.join(h, '.' + name)
+
+    if not os.path.exists(p):
+        os.makedirs(p)
