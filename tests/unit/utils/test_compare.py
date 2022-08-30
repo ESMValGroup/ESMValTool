3cc9d6368f975bc1660a6dd19a70c33aa5137389
Valeriu Predoi <valeriu.predoi@gmail.com>
2022-08-30 13:02:25 +0100

    print out test results in case of failure

--- tests/unit/utils/test_compare.py
+++ tests/unit/utils/test_compare.py
@@ -10,5 +10,7 @@ def test_get_recipe_name_from_file(tmp_path):
     """Test the string extractor for recipe name."""
     path_to_file = tmp_path / "recipe_python_20220317_162441" / "run"
     os.makedirs(path_to_file)
-    expected = get_recipe_name_from_file(path_to_file)
-    assert expected == "name_from_file0/recipe_python"
+    obtained = get_recipe_name_from_file(path_to_file)
+    print("Obtained: %s", obtained)
+    print("Expected: name_from_file0/recipe_python")
+    assert obtained == "name_from_file0/recipe_python"
