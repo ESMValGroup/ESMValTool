ff203fe26176315dfc409f42c3b72f0c1144c484
Katherine Tomkins <katherine.tomkins@metoffice.gov.uk>
2022-09-08 16:57:13 +0100

    downloads the main branches of tool and core

--- esmvaltool/utils/recipe_run_workflow/recipe_run_workflow/app/install_cold/bin/install_cold.sh
+++ esmvaltool/utils/recipe_run_workflow/recipe_run_workflow/app/install_cold/bin/install_cold.sh
@@ -3,17 +3,14 @@
 BASH_XTRACEFD=1
 set -eux
 
-# Remove source directory, if it exists.
-if [[ -d ${SOURCE_DIR} ]]; then
-    rm -rf "${SOURCE_DIR}"
+# Remove source directories, if they exists.
+if [[ -d ${ESMVALTOOL_DIR} ]]; then
+    rm -rf "${ESMVALTOOL_DIR}"
 fi
 
-# Checkout ESMValTool.
-#git clone -b "${BRANCH}" "${ESMVALTOOL_URL}" "${SOURCE_DIR}"
-
-# Install ESMValTool.
-#cd "${SOURCE_DIR}"
-#pip install . --prefix="${RRW_INSTALL_ROOT}"
+if [[ -d ${ESMVALCORE_DIR} ]]; then
+    rm -rf "${ESMVALCORE_DIR}"
+fi
 
 SOURCE_PATH="${CYLC_WORKFLOW_RUN_DIR}/site/${SITE}-env"
 TARGET_DIR="${CYLC_WORKFLOW_RUN_DIR}/bin"
@@ -24,3 +21,11 @@ mkdir "${TARGET_DIR}"
 
 # Copy the environment file to the 'bin' directory.
 cp "${SOURCE_PATH}" "${TARGET_DIR}/${ENV_FILE}"
+
+# Checkout ESMValTool.
+git clone -b "${BRANCH}" "${ESMVALTOOL_URL}" "${ESMVALTOOL_DIR}"
+git clone -b "${BRANCH}" "${ESMVALCORE_URL}" "${ESMVALCORE_DIR}"
+
+# Install ESMValTool.
+cd "${ESMVALTOOL_DIR}"
+# pip install . --prefix="${CYLC_WORKFLOW_RUN_DIR}"
