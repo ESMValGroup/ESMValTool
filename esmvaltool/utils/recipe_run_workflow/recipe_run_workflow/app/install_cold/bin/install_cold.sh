dd2fe0c94de5652bd2fc751ce0d4918753950d51
Katherine Tomkins <katherine.tomkins@metoffice.gov.uk>
2022-09-08 14:43:18 +0100

    #2786: update install cold with site specific config

--- esmvaltool/utils/recipe_run_workflow/recipe_run_workflow/app/install_cold/bin/install_cold.sh
+++ esmvaltool/utils/recipe_run_workflow/recipe_run_workflow/app/install_cold/bin/install_cold.sh
@@ -1,14 +1,26 @@
 #!/bin/bash
+# Send the output from 'set -x' to 'stdout' rather than 'stderr'.
+BASH_XTRACEFD=1
 set -eux
 
 # Remove source directory, if it exists.
 if [[ -d ${SOURCE_DIR} ]]; then
     rm -rf "${SOURCE_DIR}"
 fi
-printf 'This is running.'
+
 # Checkout ESMValTool.
 #git clone -b "${BRANCH}" "${ESMVALTOOL_URL}" "${SOURCE_DIR}"
 
 # Install ESMValTool.
 #cd "${SOURCE_DIR}"
 #pip install . --prefix="${RRW_INSTALL_ROOT}"
+
+SOURCE_PATH="${CYLC_WORKFLOW_RUN_DIR}/site/${SITE}-env"
+TARGET_DIR="${CYLC_WORKFLOW_RUN_DIR}/bin"
+ENV_FILE="rrw-env"
+
+# Create the 'bin' directory in the installed workflow.
+mkdir "${TARGET_DIR}"
+
+# Copy the environment file to the 'bin' directory.
+cp "${SOURCE_PATH}" "${TARGET_DIR}/${ENV_FILE}"
