4047cb2d2e55d3c79ef24da30f1dd1f5382d2d96
Katherine Tomkins <katherine.tomkins@metoffice.gov.uk>
2022-09-14 12:38:32 +0100

    #2787 Install ESMValTool and Core in install_cold task

--- esmvaltool/utils/recipe_test_workflow/recipe_test_workflow/app/install_cold/bin/install_cold.sh
+++ esmvaltool/utils/recipe_test_workflow/recipe_test_workflow/app/install_cold/bin/install_cold.sh
@@ -3,6 +3,17 @@
 BASH_XTRACEFD=1
 set -eux
 
+# Remove esmvaltool and esmvalcore directories, if they exist.
+if [[ -d ${ESMVALTOOL_DIR} ]]; then
+    rm -rf "${ESMVALTOOL_DIR}"
+fi
+
+if [[ -d ${ESMVALCORE_DIR} ]]; then
+    rm -rf "${ESMVALCORE_DIR}"
+fi
+
+# Copy the site specific environment launch file to the cylc run /bin
+# Where it will be identified by rose task-run
 SOURCE_PATH="${CYLC_WORKFLOW_RUN_DIR}/site/${SITE}-env"
 TARGET_DIR="${CYLC_WORKFLOW_RUN_DIR}/bin"
 ENV_FILE="rtw-env"
@@ -12,3 +23,19 @@ mkdir "${TARGET_DIR}"
 
 # Copy the environment file to the 'bin' directory.
 cp "${SOURCE_PATH}" "${TARGET_DIR}/${ENV_FILE}"
+
+# Launch the local site environment
+rtw-env
+
+
+# Checkout main branch for ESMValTool and ESMValCore from github.
+git clone -b "${BRANCH}" "${ESMVALTOOL_URL}" "${ESMVALTOOL_DIR}"
+git clone -b "${BRANCH}" "${ESMVALCORE_URL}" "${ESMVALCORE_DIR}"
+
+# add esmvaltool to PYTHONPATH
+cd "${ESMVALTOOL_DIR}"
+export PYTHONPATH=`pwd`
+
+# add esmvalcore to PYTHONPATH
+cd "${ESMVALCORE_DIR}"
+export PYTHONPATH=`pwd`
