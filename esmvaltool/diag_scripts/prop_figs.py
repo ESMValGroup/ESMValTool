be05ce954739d8c386a35b53e7ee15fa51b6e261
Nathan Gillett <rng001@hpcr5-vis3.science.gc.ca>
2022-09-14 05:19:56 +0000

    working version

--- esmvaltool/diag_scripts/prop_figs.py
+++ esmvaltool/diag_scripts/prop_figs.py
@@ -50,39 +50,54 @@ def main(cfg):
         'dataset',
         sort='short_name',
     )
+    plot_dir=cfg['plot_dir']
+    plt.ioff() #Turn off interactive plotting.
 
 #    print ('input_data',input_data)
     for dataset in input_data:
         logger.info("Processing model %s", dataset)
-        grouped_input_data=group_metadata(input_data[dataset],'variable_group',sort='short_name')
+        grouped_input_data=group_metadata(input_data[dataset],'variable_group',sort='exp')
         for short_name in grouped_input_data:
-            for attributes in grouped_input_data[short_name]:
-                input_file = attributes['filename']
-                cube = compute_diagnostic(input_file)
+            logger.info("Processing variable %s", short_name)
+            grouped_input_data_var=group_metadata(grouped_input_data[short_name],'exp',sort='exp')
+            
+            for exp in grouped_input_data_var:
+                logger.info("Processing experiment %s", exp)
+                for attributes in grouped_input_data_var[exp]:
+                    input_file = attributes['filename']
+                    if exp == 'abrupt-4xCO2':
+                        cube1 = compute_diagnostic(input_file)
+                    if exp == 'piControl':
+                        cube2 = compute_diagnostic(input_file)
 
 #                print ('cube',cube)
 #                help (cube)
-                print ('short_name',short_name)
-                if short_name == 'tas':
-                    tas=cube.data
-                    print ('tas',tas)
-                if short_name == 'nbp':
-                    land_cumflux=cube.data
-                    print ('land_cumflux',land_cumflux)
-                if short_name == 'fgco2':
-                    ocean_cumflux=cube.data
-                    print ('ocean_cumflux',ocean_cumflux)
-                atmos_co2=0.
-                print (cube.time)    
-#        ocean_cumflux=np.cumsum(atmos_ocean_flux[:])/1000.
-#        land_cumflux=np.cumsum(atmos_land_flux[:])/1000.
-        emiss=atmos_co2+ocean_cumflux+land_cumflux
-
+            print ('short_name',short_name)
+            if short_name == 'tas':
+                tas=cube1.data-cube2.data
+                print ('tas',tas)
+            if short_name == 'co2':
+                atmos_co2=(cube1.data[:,0]-cube2.data[:,0])*2.13*1e3 #Convert to Eg
+                print ('atmos_co2',atmos_co2)
+            if short_name == 'nbp':
+                atmos_land_flux=(cube1.data-cube2.data)*31556952/1e12 #in PgC/yr
+                print ('atmos_land_flux',atmos_land_flux)
+            if short_name == 'fgco2':
+                atmos_ocean_flux=(cube1.data-cube2.data)*31556952/1e12 #in PgC/yr
+                print ('atmos_ocean_flux',atmos_ocean_flux)
+            if short_name == 'hfds':
+                q=cube1.data-cube2.data
+                print ('q',q)
+#                print (cube.time)    
+        ocean_cumflux=np.cumsum(atmos_ocean_flux[:])/1000.
+        land_cumflux=np.cumsum(atmos_land_flux[:])/1000.
+        emiss=atmos_co2[:]+ocean_cumflux[:]+land_cumflux[:]
+        year=list(np.arange(151))
         plt.figure(figsize=[5,10])
         plt.subplot(311)
         plt.plot(year[:],q[:],color='black')
         plt.xlabel('Year')
-        plt.ylabel('Global average heat flux (W/m^2)')
+        plt.ylabel('Global heat flux (W)')
         plt.title('(a) Atmosphere-ocean heat flux')
         plt.subplot(312)
         plt.xlabel('Year')
@@ -91,11 +106,11 @@ def main(cfg):
         plt.plot(year[:],atmos_ocean_flux[:],color='black')
         plt.subplot(313)
         plt.xlabel('Year')
-        plt.ylabel('Ratio of heat to carbon flux (W/m^2/(PgC/yr))')
+        plt.ylabel('Ratio of heat to carbon flux (W/(PgC/yr))')
         plt.title('(c) Ratio of heat to carbon flux')
         plt.plot(year[:],q[:]/atmos_ocean_flux[:],color='black')
         plt.tight_layout()
-        plt.savefig('/home/rng/plots/tcre/fluxes.png')
+        plt.savefig(plot_dir+'/fluxes.png')
         plt.close()
         
         plt.figure(figsize=[5,10])
@@ -104,9 +119,9 @@ def main(cfg):
         plt.xlabel('Year')
         plt.ylabel('GSAT anomaly (K)')
         plt.title('(a) Warming in 4xCO2')
-        plt.axis([0,250,0,10])
+        plt.axis([0,150,0,10])
         plt.subplot(312)
-        plt.fill_between(year[:],np.zeros(250),atmos_co2[:],color="red")
+        plt.fill_between(year[:],np.zeros(151),atmos_co2[:],color="red")
         plt.xlabel('Year')
         plt.ylabel('Atmosphere/ocean/land CO2 (EgC)')
         plt.title('(b) Carbon pool anomalies in 4xCO2')
@@ -117,7 +132,7 @@ def main(cfg):
         plt.text (100,2.1,'Ocean',fontsize =11,fontweight='bold', va='center')
         plt.text (100,3.0,'Land',fontsize =11,fontweight='bold', va='center')
         plt.legend(loc="upper left")
-        plt.axis([0,250,0,5])
+        plt.axis([0,150,0,5])
         
         plt.subplot(313)
         plt.xlabel('Year')
@@ -127,13 +142,12 @@ def main(cfg):
         plt.plot(year[:],tas[:]/(ocean_cumflux[:]+atmos_co2[:]),linestyle='dashdot',color='black',label='\\u0394T/(atmos+ocean carbon)')
         plt.plot(year[:],tas[:]/emiss,color="black",label='\\u0394T/(atmos+ocean+land carbon)')
         plt.legend(loc="upper right")
-        plt.axis([0,250,0,18])
+        plt.axis([0,150,0,18])
 
         plt.tight_layout()
-        plt.savefig('/home/rng/plots/tcre/tcre.png')
+        plt.savefig(plot_dir+'/tcre.png')
         plt.close()
 
-        plt.show()
 
 
                 
