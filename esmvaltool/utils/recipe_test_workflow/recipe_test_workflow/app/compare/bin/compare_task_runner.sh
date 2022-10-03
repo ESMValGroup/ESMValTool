92ea7acfaf4438fadbc1d3ea5d953e16857075ac
Theo Geddes <theodore.geddes@metoffice.gov.uk>
2022-10-03 15:48:52 +0100

    change KGO_PATH to KGO_ROOT_PATH

--- esmvaltool/utils/recipe_test_workflow/recipe_test_workflow/app/compare/bin/compare_task_runner.sh
+++ esmvaltool/utils/recipe_test_workflow/recipe_test_workflow/app/compare/bin/compare_task_runner.sh
@@ -8,7 +8,7 @@ FOLDER_NAME=$(find "${OUTPUT_DIR}" -type d -name "recipe_${KGO_METRIC}*")
 # Add cd to python installation here
 
 
-KGO=$(find "${KGO_PATH}" -type d -name "recipe_${KGO_METRIC}*")
+KGO=$(find "${KGO_ROOT_PATH}" -type d -name "recipe_${KGO_METRIC}*")
 # Create a variable that defines the path to the compare script.
 COMPARE_SCRIPT=${SSS_TAG_DIR}/lib/python3.10/site-packages/esmvaltool/utils/testing/regression/compare.py
 
