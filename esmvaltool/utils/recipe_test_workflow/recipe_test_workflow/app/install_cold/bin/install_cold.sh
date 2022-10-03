e88bc49c476b81e4c4eef66cce70fb68c05a1dff
Katherine Tomkins <katherine.tomkins@metoffice.gov.uk>
2022-10-03 16:51:54 +0100

    Add install cold task (#2803)
    * #2787 Install ESMValTool and Core in install_cold task
    
    * #2821: PR comments part 1
    
    * #2821: PR comments part 2
    
    * Update esmvaltool/utils/recipe_test_workflow/recipe_test_workflow/app/install_cold/bin/install_cold.sh
    
    Co-authored-by: Emma Hogan <ehogan@users.noreply.github.com>
    
    * Update esmvaltool/utils/recipe_test_workflow/recipe_test_workflow/app/install_cold/bin/install_cold.sh
    
    Co-authored-by: Emma Hogan <ehogan@users.noreply.github.com>
    
    * Update esmvaltool/utils/recipe_test_workflow/recipe_test_workflow/app/install_cold/bin/install_cold.sh
    
    Co-authored-by: Emma Hogan <ehogan@users.noreply.github.com>
    
    * #2821: PR comments part 3
    
    * #2821: PR comments part 4
    
    * #2821: PR comments part 5
    
    Co-authored-by: Emma Hogan <ehogan@users.noreply.github.com>

--- esmvaltool/utils/recipe_test_workflow/recipe_test_workflow/app/install_cold/bin/install_cold.sh
+++ esmvaltool/utils/recipe_test_workflow/recipe_test_workflow/app/install_cold/bin/install_cold.sh
@@ -3,6 +3,10 @@
 BASH_XTRACEFD=1
 set -eux
 
+
+# Copy the site specific environment file to the 'bin' directory in the
+# installed Cylc workflow (this directory is automatically added to the
+# ${PATH} by Cylc).
 SOURCE_PATH="${CYLC_WORKFLOW_RUN_DIR}/site/${SITE}-env"
 TARGET_DIR="${CYLC_WORKFLOW_RUN_DIR}/bin"
 ENV_FILE="rtw-env"
@@ -12,3 +16,18 @@ mkdir "${TARGET_DIR}"
 
 # Copy the environment file to the 'bin' directory.
 cp "${SOURCE_PATH}" "${TARGET_DIR}/${ENV_FILE}"
+
+# Remove the ESMValTool and ESMValCore directories, if they exist.
+if [[ -d ${ESMVALTOOL_DIR} ]]; then
+    rm -rf "${ESMVALTOOL_DIR}"
+fi
+
+if [[ -d ${ESMVALCORE_DIR} ]]; then
+    rm -rf "${ESMVALCORE_DIR}"
+fi
+
+# Checkout the specified branch for ESMValCore and ESMValTool. Use the
+# quiet ('-q') option to prevent the progress status from being reported
+# (which is done via done via 'stderr').
+git clone -q -b "${BRANCH}" "${ESMVALTOOL_URL}" "${ESMVALTOOL_DIR}"
+git clone -q -b "${BRANCH}" "${ESMVALCORE_URL}" "${ESMVALCORE_DIR}"
