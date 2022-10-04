3828f5194850e79049dda114b27ce0611963dfe0
mo-tgeddes <108924122+mo-tgeddes@users.noreply.github.com>
2022-10-04 10:26:35 +0100

    Update esmvaltool/utils/recipe_test_workflow/recipe_test_workflow/app/compare/bin/compare_task_runner.sh
    Co-authored-by: Emma Hogan <ehogan@users.noreply.github.com>

--- esmvaltool/utils/recipe_test_workflow/recipe_test_workflow/app/compare/bin/compare_task_runner.sh
+++ esmvaltool/utils/recipe_test_workflow/recipe_test_workflow/app/compare/bin/compare_task_runner.sh
@@ -7,7 +7,7 @@ FOLDER_NAME="${OUTPUT_DIR}/recipe_${KGO_METRIC}*"
 KGO="${KGO_ROOT_PATH}/recipe_${KGO_METRIC}*"
 
 # Create a variable that defines the path to the compare script.
-COMPARE_SCRIPT=${ESMVALTOOL_DIR}/esmvaltool/utils/testing/regression/compare.py
+COMPARE_SCRIPT="${ESMVALTOOL_DIR}/esmvaltool/utils/testing/regression/compare.py"
 
 # Run the compare script.
 python "${COMPARE_SCRIPT}" ${FOLDER_NAME} ${KGO}
