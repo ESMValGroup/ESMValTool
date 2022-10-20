a79e4f80c47e8e57ecbe4bfe4b159954ad75438b
Theo Geddes <theodore.geddes@metoffice.gov.uk>
2022-10-20 14:16:10 +0100

    seperate tasks

--- esmvaltool/utils/recipe_test_workflow/recipe_test_workflow/app/install_cold/bin/install_cold.sh
+++ esmvaltool/utils/recipe_test_workflow/recipe_test_workflow/app/install_cold/bin/install_cold.sh
@@ -17,21 +17,6 @@ mkdir "${TARGET_DIR}"
 # Copy the environment file to the 'bin' directory.
 cp "${SOURCE_PATH}" "${TARGET_DIR}/${ENV_FILE}"
 
-# Remove the ESMValTool and ESMValCore directories, if they exist.
-if [[ -d ${ESMVALTOOL_DIR} ]]; then
-    rm -rf "${ESMVALTOOL_DIR}"
-fi
-
-if [[ -d ${ESMVALCORE_DIR} ]]; then
-    rm -rf "${ESMVALCORE_DIR}"
-fi
-
-# Checkout the specified branch for ESMValCore and ESMValTool. Use the
-# quiet ('-q') option to prevent the progress status from being reported
-# (which is done via done via 'stderr').
-git clone -q -b "${BRANCH}" "${ESMVALTOOL_URL}" "${ESMVALTOOL_DIR}"
-git clone -q -b "${BRANCH}" "${ESMVALCORE_URL}" "${ESMVALCORE_DIR}"
-
 # Manually and temporarily install imagehash (it will be included in the
 # next ESMValTool community environment).
 rtw-env pip install imagehash -t "${CYLC_WORKFLOW_RUN_DIR}/lib/python"
