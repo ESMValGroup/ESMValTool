86db0ad16143c47249554b1a6289c0371dce7865
Valeriu Predoi <valeriu.predoi@gmail.com>
2022-10-26 14:25:06 +0100

    improve parser script

--- esmvaltool/utils/batch-jobs/parse_recipes_output.py
+++ esmvaltool/utils/batch-jobs/parse_recipes_output.py
@@ -18,20 +18,31 @@ def parse_output_file():
     pattern = "*.out*"
     files = parse_slurm_output(dirname, pattern)
     success_rec = []
+    diag_fail_rec = []
+    missing_data = []
     for fil in files:
         with open(fil, "r") as outfile:
             lines = outfile.readlines()
             for line in lines:
                 if "Run was successful\n" in line:
                     success_rec.append(fil)
+                elif "esmvalcore._task.DiagnosticError" in line:
+                    diag_fail_rec.append(fil)
+                elif "ERROR   Missing data for preprocessor" in line:
+                    missing_data.append(fil)
 
     # typical list elem
     # /home/b/b382109/output_v270/recipe_schlund20jgr_gpp_change_1pct.2378956.out
-    recipe_outs = [os.path.basename(ofile) for ofile in success_rec]
-    recipe_outs = [f.split(".")[0] + ".yml" for f in recipe_outs]
-    
-    # return a list of success story recipes
-    return recipe_outs
+    ok_recipe_outs = [os.path.basename(ofile) for ofile in success_rec]
+    ok_recipe_outs = [f.split(".")[0] + ".yml" for f in ok_recipe_outs]
+    df_recipe_outs = [os.path.basename(ofile) for ofile in diag_fail_rec]
+    df_recipe_outs = [f.split(".")[0] + ".yml" for f in df_recipe_outs]
+    md_recipe_outs = [os.path.basename(ofile) for ofile in missing_data]
+    md_recipe_outs = [f.split(".")[0] + ".yml" for f in md_recipe_outs]
+ 
+    return (sorted(set(ok_recipe_outs)),
+            sorted(set(df_recipe_outs)),
+            sorted(set(md_recipe_outs)))
 
 
 def display_in_md():
@@ -40,16 +51,34 @@ def display_in_md():
     print(f"## Recipe running session {todaynow}\n")
     with open("all_recipes.txt", "r") as allrecs:
         all_recs = [l.strip() for l in allrecs.readlines()]
-    recipe_list = sorted(parse_output_file())
+
+    # parse different types of recipe outcomes
+    recipe_list, failed, missing_dat = parse_output_file()
     print("### Succesfully run recipes\n\n")
-    print(f"{len(recipe_list)} out of {len(all_recs)} so far\n")
+    print(f"{len(recipe_list)} out of {len(all_recs)}\n")
     for rec in recipe_list:
         print("- " + rec)
 
-    # look at fails or still running
-    bad_recs = [l for l in all_recs if l not in recipe_list]
+    # surely failed with diagnostic error
+    print("\n### Recipes that failed with DiagnosticError\n")
+    print(f"{len(failed)} out of {len(all_recs)}\n")
+    for rec in failed:
+        print("- " + rec)
+
+    # missing data
+    print("\n### Recipes that failed of Missing Data\n")
+    print(f"{len(missing_dat)} out of {len(all_recs)}\n")
+    for rec in missing_dat:
+        print("- " + rec)
+
+    # look at other fails or still running
+    bad_recs = [
+        l for l in all_recs
+        if l not in recipe_list and l not in failed
+        and l not in missing_dat
+    ]
     bad_recs = sorted(bad_recs)
-    print("\n### Recipes that failed (or are still running)\n")
+    print("\n### Recipes that failed of other reasons  or are still running\n")
     print(f"{len(bad_recs)} out of {len(all_recs)} so far\n")
     for rec in bad_recs:
         print("- " + rec)
