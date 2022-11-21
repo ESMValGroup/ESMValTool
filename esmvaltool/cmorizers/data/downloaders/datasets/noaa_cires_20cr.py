4478fd4080d5430cfabd847c50a7baed3d1f9dd0
Lisa Bock <lisa.bock@dlr.de>
2022-11-21 12:35:06 +0100

    fix downloader

--- esmvaltool/cmorizers/data/downloaders/datasets/noaa_cires_20cr.py
+++ esmvaltool/cmorizers/data/downloaders/datasets/noaa_cires_20cr.py
@@ -1,8 +1,8 @@
 """Script to download NOAA-CIRES-20CR."""
 import logging
-#import os
+import os
 
-from esmvaltool.cmorizers.data.downloaders.wget import FTPDownloader
+from esmvaltool.cmorizers.data.downloaders.ftp import FTPDownloader
 
 logger = logging.getLogger(__name__)
 
@@ -42,14 +42,3 @@ def download_dataset(config, dataset, dataset_info, start_date, end_date,
     downloader.download_file("gaussian/monolevel/tcdc.eatm.mon.mean.nc", sub_folder='surface_gauss')
     downloader.download_file("gaussian/monolevel/ulwrf.ntat.mon.mean.nc", sub_folder='surface_gauss')
     downloader.download_file("gaussian/monolevel/uswrf.ntat.mon.mean.nc", sub_folder='surface_gauss')
-
-    #os.makedirs(downloader.local_folder, exist_ok=True)
-
-    #url = "https://downloads.psl.noaa.gov/Datasets/20thC_ReanV2/Monthlies/"
-
-    #downloader.download_file(url + "monolevel/cldwtr.eatm.mon.mean.nc", wget_options=[])
-    #downloader.download_file(url + "monolevel/pr_wtr.eatm.mon.mean.nc", wget_options=[])
-    #downloader.download_file(url + "pressure/shum.mon.mean.nc", wget_options=[])
-    #downloader.download_file(url + "gaussian/monolevel/tcdc.eatm.mon.mean.nc", wget_options=[])
-    #downloader.download_file(url + "gaussian/monolevel/ulwrf.ntat.mon.mean.nc", wget_options=[])
-    #downloader.download_file(url + "gaussian/monolevel/uswrf.ntat.mon.mean.nc", wget_options=[])
