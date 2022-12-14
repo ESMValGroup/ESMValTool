68c73b288f057c9e9e81219fe75f50c9667bc0ee
Valeriu Predoi <valeriu.predoi@gmail.com>
2022-12-14 17:16:39 +0000

    Pin shapely<2.0.0 for linux64 (#2970)

--- setup.py
+++ setup.py
@@ -60,7 +60,7 @@ REQUIREMENTS = {
         'scitools-iris',
         'seaborn',
         'seawater',
-        'shapely',
+        'shapely<2.0.0',  # github.com/ESMValGroup/ESMValTool/issues/2965
         'xarray',
         'xesmf==0.3.0',
         'xgboost>1.6.1',  # github.com/ESMValGroup/ESMValTool/issues/2779
