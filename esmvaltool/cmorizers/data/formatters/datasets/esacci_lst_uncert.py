644db4e727c0c5d7c1f279ad1ac8364ff35e0be3
morobking <rob.king@metoffice.gov.uk>
2022-12-21 17:09:21 +0000

    Most issues from proscpector on cmoriser python resolved. Still some to do.

--- esmvaltool/cmorizers/data/formatters/datasets/esacci_lst_uncert.py
+++ esmvaltool/cmorizers/data/formatters/datasets/esacci_lst_uncert.py
@@ -9,90 +9,64 @@ Download and processing instructions
    BOTH DAY and NIGHT files are needed for each month
 Currently set to work with only the MODIS AQUA L3 monthly data
 Modification history
-   20201222 Started by Robert King, based on the CMUG WP5.3 cmorizer with no uncertanties
+   20201222 Started by Robert King, based on the 
+   CMUG WP5.3 cmorizer with no uncertanties
 """
 
 import datetime
 import logging
-from calendar import monthrange
-
 import iris
 import cf_units as Unit
 
-#from . import utilities as utils
-from ...utilities import fix_coords, save_variable
+from ...utilities import fix_coords
 
 logger = logging.getLogger(__name__)
 
-#def cmorization(in_dir, out_dir, cfg, _):
 def cmorization(in_dir, out_dir, cfg, cfg_user, start_date, end_date):
     """Cmorization func call."""
 
     glob_attrs = cfg['attributes']
-    cmor_table = cfg['cmor_table']
 
-    # This loop makes it easier for the CMUG WP5.4 work
     # variable_list contains the variable list
     # variable_keys has the short 'code' as a key for the variables.
-    # both these lists are in made in teh same order
-    # variable_list = []
-    # variable_keys = []
+    # both these lists are in made in the same order
 
-    # VAR_LIST = {'day':[],
-    #             'night':[]
-    #              }
-
-    # VAR_KEYS = {'day':[],
-    #             'night':[]
-    #              }
     # vals has the info from the yml file
     # var is set up in the yml file
-    print(cfg['variables'].items())
-    for var, vals in cfg['variables'].items():
-        print('#####################')
-        print(var)
-        print(vals)
-    
+    for var, vals in cfg['variables'].items(): 
         glob_attrs['mip'] = vals['mip']
 
         # loop over years and months
         # get years from start_year and end_year
-        output = iris.cube.CubeList()
         for year in range(vals['start_year'], vals['end_year'] + 1):
-            print('*******')
-            print(year)
-
-            this_years_cubes = iris.cube.CubeList()
-            for month in range(1,13):
+            for month in range(1, 13):
                 logger.info(year)
                 logger.info(month)
-                print(vals['file'])
                 cubes = load_cubes(in_dir,
-                                  vals['file'],
-                                  year,
-                                  month,
-                                  vals['raw'],#variable_list
-                                  
-                ) 
-                                
+                                   vals['file'],
+                                   year,
+                                   month,
+                                   vals['raw'],
+                                   )
+
                 #  make time coords
-                time_point = Unit.date2num(datetime.datetime(year,month,1),
+                time_point = Unit.date2num(datetime.datetime(year, month, 1),
                                            'hours since 1970-01-01 00:00:00',
-                                           Unit.CALENDAR_STANDARD)
+                                           Unit.CALENDAR_STANDARD
+                                           )
 
-                time_coord = iris.coords.DimCoord(time_point, 
+                time_units = 'hours since 1970-01-01'
+                time_coord = iris.coords.DimCoord(time_point,
                                                   standard_name='time',
-                                                  long_name='time', 
-                                                  var_name='time', 
-                                                  units='hours since 1970-01-01',
-                                                  bounds=None,#bounds, 
-                                                  attributes=None, 
-                                                  coord_system=None, 
+                                                  long_name='time',
+                                                  var_name='time',
+                                                  units=time_units,
+                                                  bounds=None,
+                                                  attributes=None,
+                                                  coord_system=None,
                                                   circular=False
-                                              )
+                                                  )
 
-
-# #                 for i,cube in enumerate(cubes):
                 cubes.attributes = {}
                 cubes.attributes['var'] = var
 
@@ -103,14 +77,10 @@ def cmorization(in_dir, out_dir, cfg, cfg_user, start_date, end_date):
 
                 cubes.add_dim_coord(time_coord, 0)
 
-
                 if cubes.long_name == 'land surface temperature':
                     cubes.long_name = 'surface_temperature'
                     cubes.standard_name = 'surface_temperature'
 
-                 # use CMORizer utils
-
-# #                 for cube in output:
                 try:
                     cubes = fix_coords(cubes)
                     print('FIXED')
@@ -118,9 +88,6 @@ def cmorization(in_dir, out_dir, cfg, cfg_user, start_date, end_date):
                     print('NOT NOT NOT FIXED')
                     logger.info('skip fixing')
                     logger.info(cubes.long_name)
-
-                # Using  utils save :This seems to save files all with the same name!
-                # so need to write this
 
                 try:
                     cubes.coords()[2].standard_name = 'longitude'
@@ -134,31 +101,22 @@ def cmorization(in_dir, out_dir, cfg, cfg_user, start_date, end_date):
                     cubes.var_name = 'ts'
 
                 iris.save(cubes,
-                          '%s/OBS_ESACCI_LST_UNCERTS_sat_1.00_Amon_%s_%s.nc' % 
-                          (out_dir, var_name, '%s%02d'%(year,month))
-                )
-                print('############### SAVED %s' % var_name)
-
-# #             # Not sure how to use the util saver with cube lists
-# #             # utils.save_variable(
-# #             #     monthly_cubes,
-# #             #     var,
-# #             #     out_dir,
-# #             #     glob_attrs,
-# #             # )
+                          '%s/OBS_ESACCI_LST_UNCERTS_sat_1.00_Amon_%s_%s.nc' %
+                          (out_dir, var_name, '%s%02d'%(year, month))
+                          )
 
 
-# no need to pass variable into any more, use global variable_list
 def load_cubes(in_dir, file, year, month, variable_list):
     """
+    Load files into cubes based on variables wanted in variable_list.
+
     variable = land surface temperature
     platform = AQUA not used for now
                but in place for future expansion to all ESC CCI LST plaforms
     """
-    logger.info('Loading %s/%s%s%s*.nc', in_dir, file, year, month)
+    logger.info(f'Loading {in_dir}/{file}{year}{month}.nc')
     cube = iris.load_cube('%s/%s%s%02d*.nc' % (in_dir, file,
-                                          year, month),
-                     variable_list)
+                                               year, month),
+                          variable_list)
 
     return cube
-
