4e9e544164ec7a6fc7cc96a36b2363bbf826380a
Valeriu Predoi <valeriu.predoi@gmail.com>
2023-01-04 14:39:40 +0000

    remove probe print statement

--- tests/integration/test_recipe_filler.py
+++ tests/integration/test_recipe_filler.py
@@ -141,7 +141,6 @@ def test_adding_datasets(tmp_path, root, cfg):
         autofilled_recipe = yaml.safe_load(file)
         diag = autofilled_recipe["diagnostics"]["test_diagnostic"]
         var = diag["variables"]["test_var"]
-        print("VAR", var)
         assert "additional_datasets" in var
 
 
