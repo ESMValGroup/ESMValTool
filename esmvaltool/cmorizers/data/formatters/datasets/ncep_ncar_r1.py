7e925fc03b80a060d1f661a3452781e3f3e04429
Birgit Hassler <33543691+hb326@users.noreply.github.com>
2023-01-20 15:03:15 +0100

    Update esmvaltool/cmorizers/data/formatters/datasets/ncep_ncar_r1.py
    Co-authored-by: Valeriu Predoi <valeriu.predoi@gmail.com>

--- esmvaltool/cmorizers/data/formatters/datasets/ncep_ncar_r1.py
+++ esmvaltool/cmorizers/data/formatters/datasets/ncep_ncar_r1.py
@@ -72,9 +72,12 @@ logger = logging.getLogger(__name__)
 
 
 def _fix_units(cube, definition):
-    """Fix issues with the units."""
-    """Exception is 'pr' since the units in the
-       raw file are not recoginzed correctly."""
+    """
+    Fix issues with the units.
+    
+    Exception is `pr` since the units in the
+    raw file are not recognized correctly.
+    """
 
     if cube.var_name != 'pr':
         cube.convert_units(definition.units)
