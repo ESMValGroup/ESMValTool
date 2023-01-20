b6bc8434c1e2cc8d68c7f2c6e40383334b7299bd
morobking <rob.king@metoffice.gov.uk>
2023-01-20 18:21:28 +0000

    prospector run on esacci_sm.py minor comments remain

--- esmvaltool/cmorizers/data/formatters/datasets/esacci_sm.py
+++ esmvaltool/cmorizers/data/formatters/datasets/esacci_sm.py
@@ -1,47 +1,35 @@
-#  CMORiser for ESA CCI SOil Moisture V7.1 
-
 """ESMValTool CMORizer for ESACCI-Soil Moisture data.
+
 Tier
    Tier 2: other freely-available dataset.
 Source
+   ftp.geo.tuwien.ac.at
+   An account can be obtained from https://esa-soilmoisture-cci.org/
 
 Download and processing instructions
-   Put all files under a single directory (no subdirectories with years)
-   in ${RAWOBS}/Tier2/ESACCI-SM
+   Put all the daily files under a single directory with no subdirectories
+   with years in ${RAWOBS}/Tier2/ESACCI-SM
 
 This is for the daily COMBINED product.
 """
-
-import datetime
 import logging
-from calendar import monthrange
-
 import iris
-
 from ...utilities import fix_coords, save_variable
 
 logger = logging.getLogger(__name__)
 
+
 def cmorization(in_dir, out_dir, cfg, cfg_user, start_date, end_date):
     """Cmorization func call."""
-    # cmor_table = cfg['cmor_table']
     glob_attrs = cfg['attributes']
 
-    # run the cmorization
-
+    # Run the cmorization
     # vals has the info from the yml file
     # var is set up in the yml file
-    print(cfg)
 
     for var, vals in cfg['variables'].items():
-        print('##############')
-        print(var)
-        print('............................')
-        print(vals)
-        # leave this loop in as might be useful in
-        # the future for getting other info
-        # like uncertainty information from the original files
-
+        # leave this loop in as might be useful in the future for getting
+        # other info like uncertainty information from the original files
         glob_attrs['mip'] = vals['mip']
 
         for key in vals.keys():
@@ -49,49 +37,47 @@ def cmorization(in_dir, out_dir, cfg, cfg_user, start_date, end_date):
 
         variable = vals['raw']
 
-        # loop over years and months
-        # get years from start_year and end_year
+        # loop over years
         for year in range(vals['start_year'], vals['end_year'] + 1):
-            print(year)
-            print(in_dir)
             cubes = load_cubes(in_dir,
-                              vals['file'],
-                              year,
-                              variable
-                              )
+                               vals['file'],
+                               year,
+                               variable
+                               )
 
-            # now sort things
+            # now sort coordinates
             cubes = fix_coords(cubes)
 
             # save this year's data
-            save_variable(
-                cubes,
-                var,
-                out_dir,
-                glob_attrs,
-            )
+            save_variable(cubes,
+                          var,
+                          out_dir,
+                          glob_attrs
+                          )
 
-def attribute_callback(cube, field, filename):
+
+def attr_ancil_callback(cube, field, filename):
+    """Callback function for loading cubes.
+
+    Removes cube attributes and unwanted ancillary variables.
+    """
     cube.attributes = None
 
     for coord in cube.ancillary_variables():
         cube.remove_ancillary_variable(coord)
 
-    #cube.remove_ancillary_variable('Observation Timestamp')
-    #cube.remove_ancillary_variable('soil_moisture_content standard_error')
-    #cube.remove_ancillary_variable('soil_moisture_content status_flag')
-
 
 def load_cubes(in_dir, filepath, year, variable):
-    """Need to write description.
-    """
-    logger.info('Loading %s/%s%s*.nc', in_dir, filepath, year)
-    cube = iris.load(f'{in_dir}/{filepath}{year}*.nc',
-                          variable,
-                          callback = attribute_callback
-                      )
-    cube = cube.concatenate_cube()
-    print(cube)
+    """Load ESA CCI SM netcdf files.
 
+    Also remove attributes and ancillary variables to allow all of the
+    year's data to concatenate into a single cube.
+    """
+    logger.info(f'Loading {in_dir}/{filepath}{year}*.nc')
+    cube = iris.load(f'{in_dir}/{filepath}{year}*.nc',
+                     variable,
+                     callback=attr_ancil_callback
+                     )
+    cube = cube.concatenate_cube()
 
     return cube
