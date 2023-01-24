c0ea211dc1a5706b4778aa9867b2240e0dd8b286
morobking <rob.king@metoffice.gov.uk>
2023-01-24 17:36:50 +0000

    cmsaf lst data loads, fixing needs checking

--- esmvaltool/cmorizers/data/formatters/datasets/cmsaf_lst.py
+++ esmvaltool/cmorizers/data/formatters/datasets/cmsaf_lst.py
@@ -33,3 +33,67 @@ def cmorization(in_dir, out_dir, cfg, cfg_user, start_date, end_date):
     """Cmorization func call."""
 
 
+    glob_attrs = cfg['attributes']
+
+    # Run the cmorization
+    # vals has the info from the yml file
+    # var is set up in the yml file
+
+    for var, vals in cfg['variables'].items():
+        # leave this loop in as might be useful in the future for getting
+        # other info like uncertainty information from the original files
+        glob_attrs['mip'] = vals['mip']
+
+        for key in vals.keys():
+            logger.info("%s %s", key, vals[key])
+
+        variable = vals['raw']
+        print(in_dir)
+        print(vals['file'])
+        # loop over years
+        for year in range(vals['start_year'], vals['end_year'] + 1):
+            for month in range(1,3):
+                # do this montnthly due to data size
+                cubes = load_cubes(in_dir,
+                                   vals['file'],
+                                   year,
+                                   month,
+                                   variable
+                               )
+
+                # now sort coordinates
+                cubes = fix_coords(cubes)
+
+                # save this year's data
+                save_variable(cubes,
+                              var,
+                              out_dir,
+                              glob_attrs
+                          )
+
+
+def attr_ancil_callback(cube, field, filename):
+    """Callback function for loading cubes.
+    Removes cube attributes and unwanted ancillary variables.
+    """
+    cube.attributes = None
+
+    #for coord in cube.ancillary_variables():
+    #    cube.remove_ancillary_variable(coord)
+
+
+def load_cubes(in_dir, filepath, year, month, variable):
+    """Load ESA CCI SM netcdf files.
+    Also remove attributes and ancillary variables to allow all of the
+    year's data to concatenate into a single cube.
+    """
+    logger.info(f'Loading {in_dir}/{filepath}{year}{month:02d}*.nc')
+    cube = iris.load(f'{in_dir}/{filepath}{year}{month:02d}*.nc',
+                     variable,
+                     callback=attr_ancil_callback
+                     )
+    print(cube[0])
+    print(cube[1])
+    cube = cube.concatenate_cube()
+    print(cube)
+    return 
