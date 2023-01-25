62a2b7b868232ae876c8e076a913284f6b1c5c1d
Breixo Soliño <Breixo.SolinoFernandez@dlr.de>
2023-01-25 14:49:02 +0100

    Refactored to make closer to other esmvaltool cmorizers

--- esmvaltool/cmorizers/data/formatters/datasets/esdc.py
+++ esmvaltool/cmorizers/data/formatters/datasets/esdc.py
@@ -19,6 +19,7 @@ Download and processing instructions
 """
 import logging
 from pathlib import Path
+from copy import deepcopy
 
 import xarray as xr
 import cf_units
@@ -30,13 +31,16 @@ from esmvaltool.cmorizers.data import utilities as utils
 logger = logging.getLogger(__name__)
 
 
-def fix_cube(short_name, var, cube, cfg):
+def fix_cube(var, cube, cfg):
     """General fixes for all cubes."""
-    cmor_info = cfg['cmor_table'].get_variable(var['mip'], short_name)
-    # TODO DELETE
-    # import pdb
-    # pdb.set_trace()
-    # END TODO
+    cmor_info = cfg['cmor_table'].get_variable(var['mip'], var['short_name'])
+
+    # Set correct names
+    cube.var_name = cmor_info.short_name
+    if cmor_info.standard_name:
+        cube.standard_name = cmor_info.standard_name
+    cube.long_name = cmor_info.long_name
+
     # Set calendar to gregorian instead of proleptic gregorian
     old_unit = cube.coord('time').units
     if old_unit.calendar == 'proleptic_gregorian':
@@ -48,32 +52,59 @@ def fix_cube(short_name, var, cube, cfg):
     if 'height2m' in cmor_info.dimensions:
         utils.add_height2m(cube)
     cube = monthly_statistics(cube, operator="mean")  # Fix frequency
+    
     # Fix metadata
     attrs = cfg['attributes']
     attrs['mip'] = var['mip']
-    # TODO DELETE
-    logger.info("CMOR standard name: %s", cmor_info.standard_name)
-    logger.info(cmor_info)
-    # if 'standard_name' in attrs.keys():
-    #     logger.info(attrs['standard_name'])
-    # END TODO
-    # attrs['standard_name'] = cmor_info.standard_name
     utils.fix_var_metadata(cube, cmor_info)
     utils.set_global_atts(cube, attrs)
-    # TODO DELETE
-    if 'standard_name' in attrs.keys():
-        logger.info(f"Has standard_name = {attrs['standard_name']}")
-    # END TODO
+    
     return cube
+
+
+def open_zarr(path):
+    logger.info('Opening zarr in "%s"', path)
+    try:
+        zarr_dataset = xr.open_dataset(path, engine='zarr')
+        return zarr_dataset
+    except KeyError as exception:
+        # Happens when the zarr folder is missing metadata, e.g. when
+        # it is a zarr array instead of a zarr dataset.
+        logger.info('Could not open zarr dataset "%s": "KeyError: %s"',
+                    path, exception)
+        logger.info('Skipping path')
+
+
+def extract_variable(in_files, var, cfg, out_dir):
+    attributes = deepcopy(cfg['attributes'])
+    all_attributes = {
+        **attributes,
+        **var
+    }  # add the mip to the other attributes
+    raw_name = var['raw']
+    for path in in_files:
+        zarr_dataset = open_zarr(path)
+        if zarr_dataset is None:
+            continue
+        cube_xr = zarr_dataset[raw_name]
+
+        # Invalid standard names must be removed before converting to iris
+        standard_name = cube_xr.attrs.get('standard_name', None)
+        if standard_name is not None and standard_name not in iris.std_names.STD_NAMES:
+            del cube_xr.attrs['standard_name']
+            logger.info('Removed invalid standard name "%s".', standard_name)
+
+        cube_iris = cube_xr.to_iris()
+        cube = fix_cube(var, cube_iris, cfg)
+
+        utils.save_variable(cube=cube,
+                            var=var['short_name'],
+                            outdir=out_dir,
+                            attrs=all_attributes)
 
 
 def cmorization(in_dir, out_dir, cfg, cfg_user, start_date, end_date):
     """Cmorize the dataset."""
-    # 1. find the input data
-    logger.debug("in_dir: '%s'", in_dir)
-    logger.debug("cfg: '%s'", cfg)
-    logger.debug("cfg_user: '%s'", cfg_user)
-
     if start_date:
         logger.warning('start_date set to "%s", but will be ignored',
                        start_date)
@@ -82,61 +113,17 @@ def cmorization(in_dir, out_dir, cfg, cfg_user, start_date, end_date):
 
     attributes = cfg['attributes']
     variables = cfg['variables']
-
     version = attributes['version']
-
     filename_pattern = cfg['filename'].format(grid=attributes['grid'],
                                               chunking=attributes['chunking'],
                                               version=version)
 
-    def cmorize_cube(path):
-        logger.info('Opening zarr in "%s"', path)
-        try:
-            dataset = xr.open_dataset(path, engine='zarr')
-        except KeyError as exception:
-            # Happens when the zarr folder is missing metadata, e.g. when
-            # it is a zarr array instead of a zarr dataset.
-            logger.info('Could not open zarr dataset "%s": "KeyError: %s"',
-                        path, exception)
-            logger.info('Skipping path')
-            return
-
-        for short_name, var in variables.items():
-            all_attributes = {
-                **attributes,
-                **var
-            }  # add the mip to the other attributes
-            raw_name = var['raw']
-            cube_xr = dataset[raw_name]
-
-            # TODO DELETE
-            # logger.info(cube_xr)
-            # logger.info(cube_xr.attrs)
-            if 'standard_name' in cube_xr.attrs and cube_xr.attrs['standard_name'] not in iris.std_names.STD_NAMES:
-                logger.info("Standard name '%s' incompatible. Removing...", cube_xr.attrs['standard_name'])
-                del cube_xr.attrs['standard_name']
-            # END TODO
-
-            cube_iris = cube_xr.to_iris()
-
-            # 2. apply the necessary fixes
-            cube = fix_cube(short_name, var, cube_iris, cfg)
-
-            # 3. store the data with the correct filename
-            utils.save_variable(cube=cube,
-                                var=short_name,
-                                outdir=out_dir,
-                                attrs=all_attributes)
-
     local_path = Path(in_dir, f'v{version}')
-    matches = list(local_path.glob(filename_pattern))
+    in_files = list(local_path.glob(filename_pattern))
     logger.debug('Pattern %s matched: %s', Path(local_path, filename_pattern),
-                 matches)
+                 in_files)
 
-    if len(matches) != 0:
-        for match in matches:
-            cmorize_cube(match)
-    else:
+    if len(in_files) == 0:
         logger.info(
             'No local matches for pattern "%s", '
             'attempting connection to the cloud.',
@@ -145,4 +132,9 @@ def cmorization(in_dir, out_dir, cfg, cfg_user, start_date, end_date):
             logger.warning(
                 'Detected a wildcard character in path (*), '
                 'online connection to \"%s\" may not work', filename_pattern)
-        cmorize_cube(f'{attributes["source"]}/v{version}/{filename_pattern}')
+        in_files.append(f'{attributes["source"]}/v{version}/{filename_pattern}')
+
+    for short_name, var in variables.items():
+        if 'short_name' not in var:
+            var['short_name'] = short_name
+        extract_variable(in_files, var, cfg, out_dir)
