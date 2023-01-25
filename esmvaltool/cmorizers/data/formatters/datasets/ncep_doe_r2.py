b9c2e82981b6d1e2b1a6c63b831cfeae176254de
Birgit Hassler <33543691+hb326@users.noreply.github.com>
2023-01-25 15:31:00 +0100

    Update esmvaltool/cmorizers/data/formatters/datasets/ncep_doe_r2.py
    Co-authored-by: Rémi Kazeroni <70641264+remi-kazeroni@users.noreply.github.com>

--- esmvaltool/cmorizers/data/formatters/datasets/ncep_doe_r2.py
+++ esmvaltool/cmorizers/data/formatters/datasets/ncep_doe_r2.py
@@ -10,25 +10,19 @@ Last access
     20220906
 
 Download and processing instructions
-    To facilitate the download, the links to the ftp server are provided.
-    Since the filenames are sometimes identical across different
-    save the data in two subdirectories in input_dir_path.
-    Subdirectory pressure/:
-      ftp://ftp.cdc.noaa.gov/Projects/Datasets/ncep.reanalysis2/Monthlies/
+    To facilitate the download, the links to the https server are provided.
+    
+      https://downloads.psl.noaa.gov/Datasets/ncep.reanalysis2/Monthlies/
       pressure/
         rhum.mon.mean.nc
         air.mon.mean.nc
-
-    Subdirectory surface/:
-      ftp://ftp.cdc.noaa.gov/Datasets/ncep.reanalysis2/Monthlies/gaussian_grid
+      https://downloads.psl.noaa.gov/Datasets/ncep.reanalysis2/Monthlies/
+      gaussian_grid/
         tcdc.eatm.mon.mean.nc
-      ftp://ftp.cdc.noaa.gov/Datasets/ncep.reanalysis2/Monthlies/surface
+      https://downlooads.psl.noaa.gov/Datasets/ncep.reanalysis2/Monthlies/
+      surface/
         pr_wtr.eatm.mon.mean.nc
 
-    #Select the section "Pressure" and "Surface" and download the variables
-    #listed below. Since raw data on pressure levels and for surface have the
-    #same file and variable name, save the data in two different subdirectories
-    #"press" and "surf" in input_dir_path.
 
 Caveats
 
