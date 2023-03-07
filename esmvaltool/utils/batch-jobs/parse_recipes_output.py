59916b35396783b0fc7f1f0ba259c75dec846bae
Valeriu Predoi <valeriu.predoi@gmail.com>
2023-03-07 15:17:31 +0000

    add docstings and notes to recipe parser

--- esmvaltool/utils/batch-jobs/parse_recipes_output.py
+++ esmvaltool/utils/batch-jobs/parse_recipes_output.py
@@ -1,12 +1,37 @@
-"""Parse recipes output after a mega run."""
+"""
+Parse recipes run output.
+
+Parse typical batch job output files like .out and .err
+to identify recipes that have succeeded or failed; display
+results in a convenient Markdown format, to be added to
+a GitHub issue or any other such documentation.
+"""
 import datetime
 import os
 
 import glob
 
 
+# User change needed
+# directory where SLURM output files (.out and .err) are
+# written to, e.g. on Levante for user b382109
+# SLURM_OUT_DIR = "/home/b/b382109/output_v270"
+SLURM_OUT_DIR = ""
+# SLURM output file pattern (extension); usually all SLURM
+# output is held in .out, unless there are internal/system errors
+# so this is what you need most if the times
+GLOB_PATTERN = "*.out*"
+
+
 def parse_slurm_output(dirname, pattern):
-    """Parse the out dir from slurm."""
+    """
+    Parse the out dir from SLURM.
+
+    Perform a glob on dirname/pattern where dirname
+    is the directory where SLURM output is stored, and
+    pattern is the out file pattern, like .out. Returns
+    all the files in dirname that have pattern extension.
+    """
     pat = os.path.join(dirname, pattern)
     files = glob.glob(pat)
 
@@ -14,10 +39,15 @@ def parse_slurm_output(dirname, pattern):
 
 
 def parse_output_file():
-    """Parse .out files in a given dir."""
-    dirname = "/home/b/b382109/output_v270"
-    pattern = "*.out*"
-    files = parse_slurm_output(dirname, pattern)
+    """
+    Parse .out files in a given dir.
+
+    Returns a tuple of lists of sorted .out files for each
+    of these criteria: recipes that ran successfulltm recipes
+    that failed with diagnostic errors, recipes that failed
+    due to missing data.
+    """
+    files = parse_slurm_output(SLURM_OUT_DIR, GLOB_PATTERN)
     success_rec = []
     diag_fail_rec = []
     missing_data = []
