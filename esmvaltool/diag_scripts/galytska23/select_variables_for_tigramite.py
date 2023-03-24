e82f060969294d87be749444f7c76a5aa234a56e
Evgenia Galytska <egalytska@iup.physik.uni-bremen.de>
2023-03-24 13:51:41 +0100

    hopefully improved the diagnostics

--- esmvaltool/diag_scripts/galytska23/select_variables_for_tigramite.py
+++ esmvaltool/diag_scripts/galytska23/select_variables_for_tigramite.py
@@ -1,7 +1,12 @@
 """Personal diagnostics for Arctic-midlatitude research."""
 
-# to manipulate iris cubes
+# import libraries
 import iris
+import matplotlib.pyplot as plt
+import numpy as np
+from pathlib import Path
+import logging
+
 from esmvalcore.preprocessor import (
     anomalies,
     area_statistics,
@@ -16,24 +21,32 @@ from esmvaltool.diag_scripts.shared import (
 )
 from esmvaltool.diag_scripts.shared._base import get_diagnostic_filename
 
+logger = logging.getLogger(Path(__file__).stem)
+
+plot_timeseries= False
 
 def calculate_polar_vortex(dict_item):
     """Calculate polar vortex."""
     var = iris.load_cube(dict_item['filename'])
     var = var.collapsed('air_pressure', iris.analysis.MEAN)
+    # change the sign of polar vortex so the positive values
+    # (negative geopotential height anomalies) stand for 
+    # the strong polar vortex, similarly to   
+    # Kretschmer et al., 2016 and Galytska et al., 2023
     var.data *= -1
     var.var_name = 'PV'
     return var
 
 
 def calculate_slp(dict_item):
-    """Get surface pressre and calculate hPa from Pa."""
+    """Get surface pressure."""
     var = iris.load_cube(dict_item['filename'])
+    # calculate hPa from Pa.
     var.data /= 100
     return var
 
 
-def prepare_for_heat_flux(dict_item):
+def prepare_heat_flux(dict_item):
     """Prepare variables for the heat flux calculations."""
     var = iris.load_cube(dict_item['filename'])
     var_avg = area_statistics(var, operator='mean')
@@ -51,13 +64,11 @@ def calculate_heat_flux(list_va_ta):
     return hf_anom_zm
 
 
-def run_my_diagnostic(cfg):
-    """Save variables into .nc files."""
-    # assemble the data dictionary keyed by dataset name
-    # via usage of group_metadata function
-    my_files_dict = group_metadata(cfg['input_data'].values(), 'dataset')
-    for key, value in my_files_dict.items():
-        diagnostic_file = get_diagnostic_filename(key, cfg)
+def calculate_variables(input_dict):
+    """Calculate all necessary variables."""
+    dictionary = {}
+    for key, value in input_dict.items():
+        dictionary.setdefault(key,{})    
         tmp_list = []
         for item in value:
             if item['preprocessor'] == 'pv':
@@ -81,26 +92,62 @@ def run_my_diagnostic(cfg):
                 sic_ok = iris.load_cube(item['filename'])
                 sic_ok.var_name = 'Ok_sic'
             elif item['preprocessor'] == 'heat_flux':
-                var = prepare_for_heat_flux(item)
+                var = prepare_heat_flux(item)
                 tmp_list.append(var)
         heat_flux = calculate_heat_flux(tmp_list)
+        dictionary[key].setdefault ('PV', polar_vortex)
+        dictionary[key].setdefault ('Arctic_temperature', tas)
+        dictionary[key].setdefault ('Psl_Ural', psl_ural)
+        dictionary[key].setdefault ('Psl_Sib', psl_sib)
+        dictionary[key].setdefault ('Psl_Aleut', psl_aleut)
+        dictionary[key].setdefault ('BK_sic', sic_bk)
+        dictionary[key].setdefault ('Ok_sic', sic_ok)
+        dictionary[key].setdefault ('heat_flux', heat_flux)
+    return dictionary
+
+
+def plot_selected_timeseries(dictionary):
+    """Plot timeseries of indicated variables."""
+    #provide the variables in the list below
+    var_names =['heat_flux', 'PV']
+    for var in var_names: 
+        fig = plt.figure(figsize=(14,4))
+        sns.set_theme()
+        for key in dictionary: 
+            time_orig = dictionary[key][var].coord('time')
+            times = np.asarray(time_orig.units.num2date(time_orig.points))
+            time_pts=[t.strftime('%Y-%m') for t in times]   
+            plt.plot(time_pts, dictionary[key][var].data)
+            plt.title (var)
+            plt.ylabel ('Anomalies')
+            plt.xticks (rotation = 45, ha="right", rotation_mode='anchor')
+        #save_figure(?,  cfg) 
+
+
+def run_my_diagnostic(cfg):
+    """Calculate and save final variables into .nc files."""
+    my_files_dict = group_metadata(cfg['input_data'].values(), 'dataset')
+    print ('my_files_dict', my_files_dict)
+    all_variables = calculate_variables (my_files_dict)
+    for key in my_files_dict.keys():
+        diagnostic_file = get_diagnostic_filename(key, cfg)
+        var = all_variables[key]
         if key == "ERA5":
             cube_list = iris.cube.CubeList([
-                polar_vortex, tas, psl_ural, psl_sib,
-                psl_aleut, heat_flux])
-            iris.save(cube_list, diagnostic_file)
+                var['PV'], var['Arctic_temperature'], var['Psl_Ural'], 
+                var['Psl_Sib'], var['Psl_Aleut'], var['heat_flux']])
         elif key == "HadISST":
-            cube_list = iris.cube.CubeList([sic_bk, sic_ok])
-            iris.save(cube_list, diagnostic_file)
+            cube_list = iris.cube.CubeList([
+                var['BK_sic'], var ['Ok_sic']])
         else:
             cube_list = iris.cube.CubeList([
-                polar_vortex, tas, psl_ural, psl_sib,
-                psl_aleut, heat_flux, sic_bk, sic_ok
-            ])
-            iris.save(cube_list, diagnostic_file)
+                var['PV'], var['Arctic_temperature'], var['Psl_Ural'], 
+                var['Psl_Sib'], var['Psl_Aleut'], var['heat_flux'],
+                var['BK_sic'], var ['Ok_sic']])
+        iris.save(cube_list, diagnostic_file)
 
-    return '.nc files are saved'
-
+#    if plot_timeseries == "True":
+#
 
 if __name__ == '__main__':
     with run_diagnostic() as config:
