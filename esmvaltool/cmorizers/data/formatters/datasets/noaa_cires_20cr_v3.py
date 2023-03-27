243f6eec4a0efa28a72b641145fbabcd35c449b6
Lisa Bock <lisa.bock@dlr.de>
2023-03-27 14:15:31 +0200

    add v3 formatter

--- esmvaltool/cmorizers/data/formatters/datasets/noaa_cires_20cr_v3.py
+++ esmvaltool/cmorizers/data/formatters/datasets/noaa_cires_20cr_v3.py
@@ -1,4 +1,4 @@
-"""ESMValTool CMORizer for NOAA-CIRES-20CR-V2 data.
+"""ESMValTool CMORizer for NOAA-CIRES-20CR-V3 data.
 
 Tier
     Tier 2: other freely-available dataset.
@@ -7,18 +7,20 @@ Source
     https://psl.noaa.gov/data/gridded/data.ncep.reanalysis2.html
 
 Last access
-    20220906
+    20230327
 
 Download and processing instructions
     To facilitate the download, the links to the ftp server are provided.
 
-    ftp://ftp.cdc.noaa.gov/Projects/20thC_ReanV2/Monthlies/
+    ftp://ftp.cdc.noaa.gov/Datasets/20thC_ReanV3/Monthlies/
 
         pr_wtr.eatm.mon.mean.nc
         cldwtr.eatm.mon.mean.nc
         tcdc.eatm.mon.mean.nc
         ulwrf.ntat.mon.mean.nc
         uswrf.ntat.mon.mean.nc
+        csulf.ntat.mon.mean.nc
+        csusf.ntat.mon.mean.nc
         shum.mon.mean.nc
 
 Caveats
