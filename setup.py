85f7e4cfcb08d2ccb102f6a6833a4e0a64659b60
Valeriu Predoi <valeriu.predoi@gmail.com>
2023-03-29 15:11:35 +0100

    try low pinning sphinx

--- setup.py
+++ setup.py
@@ -85,8 +85,8 @@ REQUIREMENTS = {
     'doc': [
         'autodocsumm>=0.2.2',
         'nbsphinx',
-        'sphinx',  # >=6.1.3',
-        'sphinx_rtd_theme<1.2.0',
+        'sphinx<6',  # >=6.1.3',
+        'sphinx_rtd_theme==1.2.0',
     ],
     # Development dependencies
     # Use pip install -e .[develop] to install in development mode
