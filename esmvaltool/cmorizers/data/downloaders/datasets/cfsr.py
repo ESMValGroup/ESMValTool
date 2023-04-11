ac2567b7317d7c5c9a68ba7ea2a8098838314fd0
Lisa Bock <lisa.bock@dlr.de>
2023-04-11 13:31:51 +0200

    add cfsr downloader

---
+++ esmvaltool/cmorizers/data/downloaders/datasets/cfsr.py
@@ -0,0 +1,93 @@
+"""Script to download CFSR from RDA."""
+import logging
+import os
+
+from datetime import datetime
+
+from dateutil import relativedelta
+
+from esmvaltool.cmorizers.data.downloaders.wget import WGetDownloader
+from esmvaltool.cmorizers.data.utilities import unpack_files_in_folder
+
+
+logger = logging.getLogger(__name__)
+
+
+def download_dataset(config, dataset, dataset_info, start_date, end_date,
+                     overwrite):
+    """Download dataset.
+
+    Parameters
+    ----------
+    config : dict
+        ESMValTool's user configuration
+    dataset : str
+        Name of the dataset
+    dataset_info : dict
+         Dataset information from the datasets.yml file
+    start_date : datetime
+        Start of the interval to download
+    end_date : datetime
+        End of the interval to download
+    overwrite : bool
+        Overwrite already downloaded files
+    """
+    downloader = WGetDownloader(
+        config=config,
+        dataset=dataset,
+        dataset_info=dataset_info,
+        overwrite=overwrite,
+    )
+
+    os.makedirs(downloader.local_folder, exist_ok=True)
+
+    user = os.environ.get("rda-user")
+    if user is None:
+        user = str(input("RDA user name? "))
+        if user == "":
+            errmsg = ("A RDA account is required to download JRA-55 data."
+                      " Please visit https://rda.ucar.edu/login/register/"
+                      " to create an account at the Research Data Archive"
+                      " (RDA) if needed.")
+            logger.error(errmsg)
+            raise ValueError
+
+    passwd = os.environ.get("rda-passwd")
+    if passwd is None:
+        passwd = str(input("RDA password? "))
+
+    if start_date is None:
+        # start_date = datetime(1979, 1, 1)
+        start_date = datetime(2009, 1, 1)
+    if end_date is None:
+        end_date = datetime(2010, 12, 31)
+    loop_date = start_date
+    print(loop_date)
+
+    options = ["-O", "Authentication.log", "--save-cookies=auth.rda_ucar_edu",
+               f"--post-data=\"email={user}&passwd={passwd}&action=login\""]
+
+    # login to Research Data Archive (RDA)
+
+    downloader.login("https://rda.ucar.edu/cgi-bin/login", options)
+
+    url = "https://rda.ucar.edu/data/ds093.2/regular/"
+    download_options = ["--load-cookies=auth.rda_ucar_edu"]
+
+    # download data
+    while loop_date <= end_date:
+        year = loop_date.year
+
+        fname = f'flxf01.gdas.{year}.tar'
+        downloader.download_file(url + fname, download_options)
+        fname = f'pgbhnl.gdas.{year}.tar'
+        downloader.download_file(url + fname, download_options)
+
+        loop_date += relativedelta.relativedelta(year=1)
+
+    unpack_files_in_folder(downloader.local_folder)
+
+    if os.path.exists("Authentication.log"):
+        os.remove("Authentication.log")
+    if os.path.exists("auth.rda_ucar_edu"):
+        os.remove("auth.rda_ucar_edu")
