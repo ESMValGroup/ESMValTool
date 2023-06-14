45cd1043fc70c932b363feb036e12f34bd1e345c
Axel Lauer <axel.lauer@dlr.de>
2023-06-14 09:50:49 +0200

    first version of new JRA-25 downloader

--- esmvaltool/cmorizers/data/downloaders/datasets/jra_25.py
+++ esmvaltool/cmorizers/data/downloaders/datasets/jra_25.py
@@ -40,73 +40,33 @@ def download_dataset(config, dataset, dataset_info, start_date, end_date,
 
     os.makedirs(downloader.local_folder, exist_ok=True)
 
-    user = os.environ.get("rda-user")
-    if user is None:
-        user = str(input("RDA user name? "))
-        if user == "":
-            errmsg = ("A RDA account is required to download JRA-55 data."
-                      " Please visit https://rda.ucar.edu/login/register/"
-                      " to create an account at the Research Data Archive"
-                      " (RDA) if needed.")
-            logger.error(errmsg)
-            raise ValueError
-
-    passwd = os.environ.get("rda-passwd")
-    if passwd is None:
-        passwd = str(input("RDA password? "))
-
     if start_date is None:
-        start_date = datetime(1958, 1, 1)
+        start_date = datetime(1979, 1, 1)
     if end_date is None:
-        end_date = datetime(2022, 12, 31)
+        end_date = datetime(2007, 12, 31)
     loop_date = start_date
-
-    options = ["-O", "Authentication.log", "--save-cookies=auth.rda_ucar_edu",
-               f"--post-data=\"email={user}&passwd={passwd}&action=login\""]
-
-    # login to Research Data Archive (RDA)
-
-    downloader.login("https://rda.ucar.edu/cgi-bin/login", options)
 
     # download files
 
-    url = "https://rda.ucar.edu/data/ds625.1"
-    download_options = ["--load-cookies=auth.rda_ucar_edu"]
+    url = 'https://data.rda.ucar.edu/ds625.1'
+    download_options = ["--no-check-certificate"]
 
-    # define variables to download
+    # define files to download
 
-    var = {"011_tmp": "anl_surf125",
-           "039_vvel": "anl_p125",
-           "071_tcdc": "fcst_surf125",
-           "054_pwat": "fcst_column125",
-           "058_cice": "fcst_column125",
-           "160_csusf": "fcst_phy2m125",
-           "162_csulf": "fcst_phy2m125",
-           "211_uswrf": "fcst_phy2m125",
-           "212_ulwrf": "fcst_phy2m125",
-           "227_cw": "fcst_column125",
-           "228_clwc": "fcst_p125",
-           "229_ciwc": "fcst_p125"}
+    files = ['fcst_phy2m',
+             'anl_chipsi',
+             'anl_p']
 
     # download data
 
     while loop_date <= end_date:
         year = loop_date.year
+        month = f'{loop_date.month:0>2}'
 
-        for varname, channel in var.items():
-            fname = f"{channel}.{varname}.{year}01_{year}12"
+        for basename in files:
+            fname = f'{basename}.{year}{month}.nc'
             # download file
-            downloader.download_file(url + f"/{channel}/{year}/" +
-                                     fname, download_options)
-            # add file extension ".grb"
-            os.rename(downloader.local_folder + "/" + fname,
-                      downloader.local_folder + "/" + fname + ".grb")
+            downloader.download_file(url + '/' + basename + '/' + fname,
+                                     download_options)
 
-        loop_date += relativedelta.relativedelta(years=1)
-
-    # clean up temporary files
-
-    if os.path.exists("Authentication.log"):
-        os.remove("Authentication.log")
-    if os.path.exists("auth.rda_ucar_edu"):
-        os.remove("auth.rda_ucar_edu")
+        loop_date += relativedelta.relativedelta(months=1)
