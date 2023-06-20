1baaa65d917c56437125c2b348b855b985b9bf6b
Lisa Bock <lisa.bock@dlr.de>
2023-06-20 13:35:57 +0200

    clean download script

--- esmvaltool/cmorizers/data/downloaders/datasets/esacci_cloud_daily.py
+++ esmvaltool/cmorizers/data/downloaders/datasets/esacci_cloud_daily.py
@@ -5,7 +5,6 @@ from datetime import datetime
 
 from dateutil import relativedelta
 
-from esmvaltool.cmorizers.data.downloaders.ftp import CCIDownloader, FTPDownloader
 from esmvaltool.cmorizers.data.downloaders.wget import WGetDownloader
 
 logger = logging.getLogger(__name__)
@@ -33,7 +32,7 @@ def download_dataset(config, dataset, dataset_info, start_date, end_date,
     if start_date is None:
         start_date = datetime(2000, 1, 1)
     if end_date is None:
-        end_date = datetime(2000, 1, 31)
+        end_date = datetime(2000, 12, 31)
     loop_date = start_date
 
     downloader = WGetDownloader(
@@ -47,28 +46,10 @@ def download_dataset(config, dataset, dataset_info, start_date, end_date,
 
     wget_options = ['-r', '-nH', '-e', 'robots=off', '--cut-dirs=9', '--no-parent', '--reject="index.html*"']
 
-    #def download(file, wget_options=[]):
-    #    downloader.download_file(
-    #            'https://public.satproj.klima.dwd.de/data/ESA_Cloud_CCI/CLD_PRODUCTS/v3.0/L3U/AVHRR-PM' + file,
-    #        wget_options)
-
-    #source_file = "/AVHRR_NOAA-14/2000/01/20000101-ESACCI-L3U_CLOUD-CLD_PRODUCTS-AVHRR_NOAA-14-fv3.0.nc"
-    #download(source_file, ['-r', '-nH', '-e', 'robots=off', '--cut-dirs=9', '--no-parent', '--reject="index.html"'])
-
-    #folder = base_path + f'*/2000/2/'
-    #downloader.download_folder(folder, wget_options)
-
     while loop_date <= end_date:
         year = loop_date.year
         month = loop_date.month
         folder = base_path + f'{year}/{month:02}'
         print(folder)
         downloader.download_folder(folder, wget_options)
-        #for folder in downloader.list_folders(base_path):
-        #    print(folder)
-        #    for year_folder in downloader.list_folders(folder):
-        #        print(year_folder)
-        #        if int(year_folder) == year:
-        #            folder = f'{folder}/{year_folder}/{month}'
-        #            downloader.download_folder(base_path + folder, wget_options)
         loop_date += relativedelta.relativedelta(months=1)
