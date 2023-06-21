b346a207c9b7230278560c8d97029671716c8591
Lisa Bock <lisa.bock@dlr.de>
2023-06-21 16:13:14 +0200

    add vars to formatter

--- esmvaltool/cmorizers/data/formatters/datasets/esacci_cloud.py
+++ esmvaltool/cmorizers/data/formatters/datasets/esacci_cloud.py
@@ -21,9 +21,9 @@ import os
 import iris
 from cf_units import Unit
 from iris import NameConstraint
-from calendar import monthrange
 
 from esmvaltool.cmorizers.data import utilities as utils
+from esmvalcore.preprocessor import regrid
 
 logger = logging.getLogger(__name__)
 
@@ -32,10 +32,12 @@ def _extract_variable(short_name, var, year, month, cfg, in_dir,
                       out_dir):
     """Extract variable."""
     cube = iris.cube.CubeList()
-    num_days = monthrange(year, month)[1]
-    filelist = glob.glob(os.path.join(in_dir, var['file']))
+    #filename = f'{year}{month:02}0*' + var['file']
+    filename = f'{year}{month:02}01' + var['file']
+    filelist = glob.glob(os.path.join(in_dir, filename))
     for filename in sorted(filelist):
         logger.info("CMORizing file %s", filename)
+
         # load data
         raw_var = var.get('raw', short_name)
         daily_cube = iris.load_cube(filename, NameConstraint(var_name=raw_var))
@@ -49,21 +51,29 @@ def _extract_variable(short_name, var, year, month, cfg, in_dir,
 
     cube = cube.concatenate_cube()
 
+    # regridding from 0.05x0.05 to 0.5x0.5
+    cube = regrid(cube, target_grid='0.5x0.5', scheme='area_weighted')
+
     # fix time units
     cube.coord('time').convert_units(
         Unit('days since 1950-1-1 00:00:00', calendar='gregorian'))
 
+    print(cfg['cmor_table'])
+    #for i in cfg['cmor_table']:
+    #    print(i)
+    print(var['mip'])
     cmor_info = cfg['cmor_table'].get_variable(var['mip'], short_name)
+    print(*cmor_info)
 
     # Fix coordinates
     utils.fix_dim_coordnames(cube)
     ## fix flipped latitude
     #utils.flip_dim_coord(cube, 'latitude')
     #utils.fix_dim_coordnames(cube)
-    #cube_coord = cube.coord('latitude')
-    #utils.fix_bounds(cube, cube_coord)
-    #cube_coord = cube.coord('longitude')
-    #utils.fix_bounds(cube, cube_coord)
+    cube_coord = cube.coord('latitude')
+    utils.fix_bounds(cube, cube_coord)
+    cube_coord = cube.coord('longitude')
+    utils.fix_bounds(cube, cube_coord)
 
     # Fix metadata and  update version information
     attrs = copy.deepcopy(cfg['attributes'])
@@ -85,9 +95,9 @@ def cmorization(in_dir, out_dir, cfg, cfg_user, start_date, end_date):
     # Run the cmorization
     for (short_name, var) in cfg['variables'].items():
         logger.info("CMORizing variable '%s'", short_name)
-        months = [1]
         for year in range(glob_attrs['start_year'],
                           glob_attrs['end_year'] + 1):
-            for month in months:
+            for month in range(1,2):
+                print(month)
                 _extract_variable(short_name, var, year, month, cfg, in_dir,
                                      out_dir)
