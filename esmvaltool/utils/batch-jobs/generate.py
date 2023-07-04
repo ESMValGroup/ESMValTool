45908ff4748fb49de51838c96e9df60e214bbffb
Remi Kazeroni <remi.kazeroni@dlr.de>
2023-07-04 17:18:10 +0200

    improved handling of max_parallel_tasks

--- esmvaltool/utils/batch-jobs/generate.py
+++ esmvaltool/utils/batch-jobs/generate.py
@@ -51,6 +51,9 @@ conda_path = 'PATH_TO/mambaforge/etc/profile.d/conda.sh'
 # Full path to config_file
 # If none, ~/.esmvaltool/config-user.yml is used
 config_file = ''
+# Set max_parallel_tasks
+# If none, read from config_file
+default_max_parallel_tasks = 8
 
 # List of recipes that require non-default SLURM options set above
 SPECIAL_RECIPES = {
@@ -194,33 +197,28 @@ SPECIAL_RECIPES = {
     },
 }
 
+# These recipes cannot be run with the default number of parallel
+# tasks (max_parallel_tasks=8).
 # These recipes either use CMIP3 input data
 # (see https://github.com/ESMValGroup/ESMValCore/issues/430)
-# and recipes where tasks require the full compute node memory.
-ONE_TASK_RECIPES = [
-    'recipe_bock20jgr_fig_1-4',
-    'recipe_bock20jgr_fig_6-7',
-    'recipe_bock20jgr_fig_8-10',
-    'recipe_flato13ipcc_figure_96',
-    'recipe_flato13ipcc_figures_938_941_cmip3',
-    'recipe_ipccwg1ar6ch3_fig_3_9',
-    'recipe_ipccwg1ar6ch3_fig_3_42_a',
-    'recipe_ipccwg1ar6ch3_fig_3_43',
-    'recipe_check_obs',
-    'recipe_collins13ipcc',
-    'recipe_lauer22jclim_fig3-4_zonal',
-    'recipe_lauer22jclim_fig5_lifrac',
-    'recipe_smpi',
-    'recipe_smpi_4cds',
-    'recipe_wenzel14jgr',
-    ]
-
-# These recipes can only be run on Levante compute nodes
-# with 1024 GB of memory and an appropriate number of tasks.
-THREE_TASK_RECIPES = [
-    'recipe_lauer22jclim_fig3-4_zonal.yml',
-    'recipe_lauer22jclim_fig5_lifrac.yml',
-]
+# or require a large fraction of the compute node memory.
+MAX_PARALLEL_TASKS = {
+    'recipe_bock20jgr_fig_1-4': 1,
+    'recipe_bock20jgr_fig_6-7': 1,
+    'recipe_bock20jgr_fig_8-10': 1,
+    'recipe_flato13ipcc_figure_96': 1,
+    'recipe_flato13ipcc_figures_938_941_cmip3': 1,
+    'recipe_ipccwg1ar6ch3_fig_3_9': 1,
+    'recipe_ipccwg1ar6ch3_fig_3_42_a': 1,
+    'recipe_ipccwg1ar6ch3_fig_3_43': 1,
+    'recipe_check_obs': 1,
+    'recipe_collins13ipcc': 1,
+    'recipe_lauer22jclim_fig3-4_zonal': 3,
+    'recipe_lauer22jclim_fig5_lifrac': 3,
+    'recipe_smpi': 1,
+    'recipe_smpi_4cds': 1,
+    'recipe_wenzel14jgr': 1,
+    }
 
 
 def generate_submit():
@@ -291,10 +289,15 @@ def generate_submit():
             else:
                 file.write(f'esmvaltool run --config_file '
                            f'{str(config_file)} {str(recipe)}')
-            if recipe.stem in ONE_TASK_RECIPES:
-                file.write(' --max_parallel_tasks=1')
-            if recipe.stem in THREE_TASK_RECIPES:
-                file.write(' --max_parallel_tasks=3')
+            # max_parallel_tasks
+            # Default
+            if not MAX_PARALLEL_TASKS.get(recipe.stem, None):
+                file.write(f' --max_parallel_tasks='
+                           f'{default_max_parallel_tasks}\n')
+            # Special requirements
+            else:
+                file.write(f' --max_parallel_tasks='
+                           f'{MAX_PARALLEL_TASKS[recipe.stem]}\n')
 
         if submit:
             subprocess.check_call(['sbatch', filename])
