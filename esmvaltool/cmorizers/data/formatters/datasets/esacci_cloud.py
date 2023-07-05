3b92c578c3b66ef8deb9ee91483095201b8ac068
Lisa Bock <lisa.bock@dlr.de>
2023-07-05 12:40:08 +0200

    try to fix cmorizer

--- esmvaltool/cmorizers/data/formatters/datasets/esacci_cloud.py
+++ esmvaltool/cmorizers/data/formatters/datasets/esacci_cloud.py
@@ -18,10 +18,14 @@ import glob
 import logging
 import os
 
+import cf_units
 import iris
 import numpy as np
 from cf_units import Unit
 from iris import NameConstraint
+from calendar import monthrange
+from datetime import datetime
+from dateutil import relativedelta
 
 from esmvaltool.cmorizers.data import utilities as utils
 from esmvalcore.preprocessor import regrid
@@ -32,23 +36,114 @@ logger = logging.getLogger(__name__)
 def _extract_variable(short_name, var, year, month, cfg, in_dir,
                       out_dir):
     """Extract variable."""
+
+    def adjust_dim(dim):
+        if dim == 0:
+            return dim
+        return dim + 1
+
     cube = iris.cube.CubeList()
     filename = f'{year}{month:02}*' + var['file']
     #filename = f'{year}{month:02}01' + var['file']
     filelist = glob.glob(os.path.join(in_dir, filename))
-    for filename in sorted(filelist):
-        logger.info("CMORizing file %s", filename)
+    print(filelist)
+    num_days = monthrange(year, month)[1]
+    #for filename in sorted(filelist):
+    for iday in range(1, num_days+1):
 
-        # load data
-        raw_var = var.get('raw', short_name)
-        daily_cube = iris.load_cube(filename, NameConstraint(var_name=raw_var))
-        daily_cube.attributes.clear()
+        print(iday)
+        ifile = glob.glob(os.path.join(in_dir, f'{year}{month:02}' + f'{iday:02}' + var['file']))
+        #ifile = os.path.join(in_dir, f'{year}{month:02}' + f'{iday:02}' + var['file'])
+        print(ifile)
 
-        # Fix coordinates
-        daily_cube = utils.fix_coords(daily_cube)
+        #if ifile in filelist:
+        #if os.path.isfile(ifile[0]):
+        if ifile:
+            logger.info("CMORizing file %s", ifile)
 
+            # load data
+            raw_var = var.get('raw', short_name)
+            daily_cube = iris.load_cube(ifile, NameConstraint(var_name=raw_var))
+            daily_cube.attributes.clear()
+
+            # Fix coordinates
+            daily_cube = utils.fix_coords(daily_cube)
+            utils.convert_timeunits(daily_cube, 1950)
+
+            if iday == 1:
+                fill_cube = daily_cube
+                print(fill_cube.coord('time').points)
+
+        else:
+
+            logger.info("Fill missing day %s in month %s and year %s", iday, month, year)
+
+            #daily_cube = fill_cube
+
+            #daily_cube = iris.cube.Cube(fill_cube.data)
+            #daily_cube.coords = fill_cube.coords
+            #daily_cube.metadata = fill_cube.metadata
+            #daily_cube.data = np.ma.zeros(daily_cube.data)
+
+            dim_coords = fill_cube.coords(dim_coords=True)
+            aux_coords = fill_cube.coords(dim_coords=False)
+            dim_coords_and_dims = [(coord, adjust_dim(fill_cube.coord_dims(coord)[0]))
+                                   for coord in dim_coords]
+            aux_coords_and_dims = [(coord, (adjust_dim(d)
+                                            for d in fill_cube.coord_dims(coord)))
+                                   for coord in aux_coords]
+            
+            daily_cube = iris.cube.Cube(
+                    fill_cube.data,
+                    fill_cube.standard_name,
+                    fill_cube.long_name,
+                    fill_cube.var_name,
+                    fill_cube.units,
+                    fill_cube.attributes,
+                    fill_cube.cell_methods,
+                    dim_coords_and_dims,
+                    aux_coords_and_dims,
+            )
+            
+            # Read dataset time unit and calendar from file
+            dataset_time_unit = str(fill_cube.coord('time').units)
+            dataset_time_calender = fill_cube.coord('time').units.calendar
+            # Convert datetime
+            newtime = datetime(year=year, month=month, day=iday)
+            newtime = cf_units.date2num(newtime, dataset_time_unit,
+                                        dataset_time_calender)
+            daily_cube.coord('time').points = float(newtime)
+
+            #coord_time = fill_cube.coord('time')
+            #time = coord_time.points
+            #print(time)
+            #times = datetime(year=year, month=month, day=iday)
+            #print(times)
+            #print(time.strftime("%Y%m%d"))
+            #print(daily_cube.coord('time').points + relativedelta.relativedelta(days=1))
+            #daily_cube.coord('time').points = float(time.strftime("%Y%m%d"))
+            #daily_cube.coord('time').points = float(time_step) + 1.
+            #daily_cube.coord('time').points += relativedelta.relativedelta(days=1)
+            #new_time = time + float(iday) - 1.
+            #daily_cube.coord('time') = iris.coords.DimCoord(time_units.date2num(times),
+            #                                                var_name='time',
+            #                                                standard_name='time',
+            #                                                long_name='time',
+            #                                                units=time_units)
+
+            #daily_cube = fill_cube.filled(np.nan)
+            #daily_cube.data = 0. #np.nan
+            #print(fill_cube.coord('time').points)
+
+        ## Fix coordinates
+        utils.fix_coords(daily_cube)
+
+        print(daily_cube.coord('time'))
+        print(daily_cube.dim_coords)
         cube.append(daily_cube)
 
+    print(cube)
+    print([c.dim_coords[0] for c in cube])
     cube = cube.concatenate_cube()
 
     # regridding from 0.05x0.05 to 0.5x0.5
@@ -63,17 +158,11 @@ def _extract_variable(short_name, var, year, month, cfg, in_dir,
         if 'raw_units' in var:
             cube.units = var['raw_units']
         cube.convert_units(cmor_info.units)
+
     utils.convert_timeunits(cube, 1950)
 
-    # Fix coordinates
-    utils.fix_dim_coordnames(cube)
-    ## fix flipped latitude
-    #utils.flip_dim_coord(cube, 'latitude')
-    #utils.fix_dim_coordnames(cube)
-    cube_coord = cube.coord('latitude')
-    utils.fix_bounds(cube, cube_coord)
-    cube_coord = cube.coord('longitude')
-    utils.fix_bounds(cube, cube_coord)
+    ## Fix coordinates
+    utils.fix_coords(cube)
 
     # Fix metadata and  update version information
     attrs = copy.deepcopy(cfg['attributes'])
