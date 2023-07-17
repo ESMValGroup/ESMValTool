290420a1fb8cf465d9bcb24b77ad2cab89e29732
alistair.sellar <alistair.sellar@metoffice.gov.uk>
2023-07-17 08:33:05 +0100

    Add role for diagnostic scripts

--- doc/sphinx/source/conf.py
+++ doc/sphinx/source/conf.py
@@ -474,5 +474,30 @@ def recipe_role(name, rawtext, text, lineno, inliner, options={}, content=[]):
     return [node], []
 
 
+def diag_role(name, rawtext, text, lineno, inliner, options={}, content=[]):
+    """Link to a diagnostic script
+
+    Returns 2 part tuple containing list of nodes to insert into the
+    document and a list of system messages.  Both are allowed to be
+    empty.
+
+    :param name: The role name used in the document.
+    :param rawtext: The entire markup snippet, with role.
+    :param text: The text marked with the role.
+    :param lineno: The line number where rawtext appears in the input.
+    :param inliner: The inliner instance that called us.
+    :param options: Directive options for customization.
+    :param content: The directive content for customization.
+    """
+    node = nodes.reference(
+        rawtext,
+        text,
+        refuri=f"{baseurl}/esmvaltool/diag_scripts/{text}",
+        **options,
+    )
+    return [node], []
+
+
 def setup(app):
     app.add_role("recipe", recipe_role)
+    app.add_role("diag", diag_role)
