1354646ee09e986742691a7dfc451200701e37dd
Manuel Schlund <manuel.schlund@dlr.de>
2023-07-18 13:59:33 +0200

    Added CMORizer for MOBO-DIC2004-2019

---
+++ esmvaltool/cmorizers/data/formatters/datasets/mobo_dic2004_2019.py
@@ -0,0 +1,17 @@
+"""ESMValTool CMORizer for MOBO-DIC2004-2019 data.
+
+Tier
+    Tier 2: other freely-available dataset.
+
+Source
+    https://www.ncei.noaa.gov/data/oceans/ncei/ocads/data/0277099/
+
+Last access
+    20230718
+
+Download and processing instructions
+    Download the file MOBO_MPI_DIC_2004-2019_Oct2022.nc
+
+"""
+
+from .mobo_dic_mpim import cmorization  # noqa
