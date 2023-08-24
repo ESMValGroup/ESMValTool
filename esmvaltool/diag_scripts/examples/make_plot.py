1a428efb08253a71f021229089d6d9910c61dda2
Bouwe Andela <b.andela@esciencecenter.nl>
2023-08-24 12:25:14 +0200

    Improve plots and add reference

--- esmvaltool/diag_scripts/examples/make_plot.py
+++ esmvaltool/diag_scripts/examples/make_plot.py
@@ -2,7 +2,6 @@
 import logging
 from pathlib import Path
 
-import cf_units
 import iris
 import matplotlib.pyplot as plt
 
@@ -11,39 +10,59 @@ from esmvaltool.diag_scripts.shared import run_diagnostic, save_figure
 logger = logging.getLogger(Path(__file__).stem)
 
 
-def get_provenance_record(ancestor_files):
-    """Create a provenance record describing the diagnostic data and plot."""
-    record = {
+def main(cfg):
+    """Plot part of figure_9.3a from IPCC AR6."""
+    colors = {
+        'historical-ssp126': '#6a798f',
+        'historical-ssp585': '#e6ced3',
+    }
+    labels = {
+        'historical-ssp126': 'Historical and SSP1-2.6',
+        'historical-ssp585': 'Historical and SSP5-8.5',
+    }
+
+    # Group input data by experiment
+    groups = {}
+    for filename, attributes in cfg['input_data'].items():
+        exp = attributes['exp']
+        if exp not in groups:
+            groups[exp] = {}
+        groups[exp][attributes['dataset']] = filename
+
+    # Loop over experiments to populate plot
+    for exp, group in groups.items():
+        mean = iris.load_cube(group['MultiModelMean'])
+        iris.quickplot.plot(
+            mean,
+            color=colors.get(exp),
+            label=labels.get(exp, exp),
+        )
+
+        p17 = iris.load_cube(group['MultiModelP17'])
+        p83 = iris.load_cube(group['MultiModelP83'])
+        time_coord = mean.coord('time')
+        time_axis = time_coord.units.num2date(time_coord.core_points())
+        plt.fill_between(
+            time_axis,
+            p17.core_data(),
+            p83.core_data(),
+            color=colors.get(exp),
+            label='Likely (17% - 83%) ranges',
+        )
+
+    plt.title('Global mean sea surface temperature')
+    plt.legend(loc='upper left')
+
+    filename = 'IPCC_AR6_figure_9.3a_1850-2100'
+    provenance_record = {
         'caption': "Part of figure 9.3a from IPCC AR6.",
         'authors': [
             'kalverla_peter',
             'andela_bouwe',
         ],
-        'references': [
-            'acknow_project',  # TODO: replace by correct reference
-        ],
-        'ancestors': ancestor_files,
+        'references': ['fox-kemper21ipcc'],
+        'ancestors': list(cfg['input_data'].keys()),
     }
-    return record
-
-
-def main(cfg):
-    """Compute the time average for each input dataset."""
-    for attributes in cfg['input_data'].values():
-        logger.info("Processing dataset %s", attributes['alias'])
-        input_file = attributes['filename']
-        cube = iris.load_cube(input_file)
-
-        time_coord = cube.coord('time')
-        time_coord.units = cf_units.Unit(time_coord.units.origin,
-                                         calendar='gregorian')
-        iris.quickplot.plot(cube, label=attributes['alias'].replace('_', ' '))
-
-    plt.legend()
-
-    ancestor_files = list(cfg['input_data'].keys())
-    provenance_record = get_provenance_record(ancestor_files)
-    filename = 'IPCC_AR6_figure_9.3a_1850-2100'
     save_figure(filename, provenance_record, cfg)
 
 
