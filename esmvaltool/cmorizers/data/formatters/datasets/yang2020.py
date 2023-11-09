4348eaff125bb699d9e90144304c52d1fd2f012b
Joakim Low <joakim.low@smhi.se>
2023-11-09 14:09:41 +0100

    Cleanup and some restructuring of diagnostic.

--- esmvaltool/cmorizers/data/formatters/datasets/yang2020.py
+++ esmvaltool/cmorizers/data/formatters/datasets/yang2020.py
@@ -16,7 +16,7 @@ from esmvaltool.cmorizers.data import utilities as utils
 logger = logging.getLogger(__name__)
 
 
-def _build_cube(dataset):
+def build_cube(dataset):
     # dn2o -> Dpn2o:
     #    N2O disequilibrium or N2O partial pressure difference across
     #    the ocean–atmosphere interface
@@ -24,7 +24,7 @@ def _build_cube(dataset):
     #    (-> Pa?)
     #
 
-    Dpn2o = dataset.variables["dn2o_EnsMean_natm"][:, :, :]  # natm
+    dpn2o = dataset.variables["dn2o_EnsMean_natm"][:, :, :]  # natm
     lonobs = dataset.variables["lon"][:]
     latobs = dataset.variables["lat"][:]
 
@@ -32,9 +32,9 @@ def _build_cube(dataset):
     # is 1979-01-01 to 2019-01-01, see section 'Methods & Sampling' here:
     # https://www.bco-dmo.org/dataset/810032
     #
-    time_units = Unit('days since 1950-01-01 00:00:00', calendar='standard')
+    time_units = Unit("days since 1950-01-01 00:00:00", calendar="standard")
     time_points = time_units.date2num(
-        [datetime(1999, m, 15) for m in range(1, 13)]
+        [datetime(1999, m, 15) for m in range(1, 13)],
     )
     time_bounds = [
         [datetime(1979, m, 1), datetime(2018, m + 1, 1)] for m in range(1, 12)
@@ -45,26 +45,26 @@ def _build_cube(dataset):
     time_coord = iris.coords.DimCoord(
         time_points,
         bounds=time_bounds,
-        var_name='time',
-        standard_name='time',
-        long_name='time',
+        var_name="time",
+        standard_name="time",
+        long_name="time",
         units=time_units,
         climatological=True,
     )
 
     lon_coord = iris.coords.DimCoord(
         lonobs,
-        var_name='lon',
-        standard_name='longitude',
-        long_name='Longitude',
-        units='degrees_east',
+        var_name="lon",
+        standard_name="longitude",
+        long_name="Longitude",
+        units="degrees_east",
     )
     lat_coord = iris.coords.DimCoord(
         latobs,
-        var_name='lat',
-        standard_name='latitude',
-        long_name='latitude',
-        units='degrees_north',
+        var_name="lat",
+        standard_name="latitude",
+        long_name="latitude",
+        units="degrees_north",
     )
 
     coord_spec = [
@@ -73,38 +73,40 @@ def _build_cube(dataset):
         (lon_coord, 2),
     ]
     cube = iris.cube.Cube(
-        Dpn2o,
-        var_name='Dpn2o',
+        dpn2o,
+        var_name="Dpn2o",
         dim_coords_and_dims=coord_spec,
-        units='natm',
+        units="natm",
     )
     return cube
 
 
-def _extract_variable(var, cmor_table, attrs, filepath, out_dir):
+def extract_variable(var, cmor_table, attrs, filepath, out_dir):
     """Extract variable."""
     cmor_info = cmor_table.get_variable(var, var)
 
-    with netCDF4.Dataset(filepath, 'r') as dataset:
-        cube = _build_cube(dataset)
+    with netCDF4.Dataset(filepath, "r") as dataset:
+        cube = build_cube(dataset)
         utils.fix_var_metadata(cube, cmor_info)
         utils.convert_timeunits(cube, 1950)
         utils.fix_coords(cube)
         utils.set_global_atts(cube, attrs)
-        utils.save_variable(cube,
-                            var,
-                            out_dir,
-                            attrs,
-                            unlimited_dimensions=['time'])
+        utils.save_variable(
+            cube,
+            var,
+            out_dir,
+            attrs,
+            unlimited_dimensions=["time"],
+        )
 
 
 def cmorization(in_dir, out_dir, cfg, cfg_user, start_date, end_date):
     """Cmorization func call."""
-    glob_attrs = cfg['attributes']
-    cmor_table = cfg['cmor_table']
+    glob_attrs = cfg["attributes"]
+    cmor_table = cfg["cmor_table"]
 
-    for var, var_info in cfg['variables'].items():
-        filepath = pathlib.Path(in_dir) / var_info['filename']
+    for var, var_info in cfg["variables"].items():
+        filepath = pathlib.Path(in_dir) / var_info["filename"]
         logger.info(f"CMORizing variable {var} from {filepath}")
-        glob_attrs['mip'] = var_info['mip']
-        _extract_variable(var, cmor_table, glob_attrs, filepath, out_dir)
+        glob_attrs["mip"] = var_info["mip"]
+        extract_variable(var, cmor_table, glob_attrs, filepath, out_dir)
--- esmvaltool/diag_scripts/n2o_emissions/dpn2o_plot.py
+++ esmvaltool/diag_scripts/n2o_emissions/dpn2o_plot.py
@@ -1,4 +1,4 @@
-"""N2O emissions diagnostic."""
+"""N2O emissions plot diagnostic."""
 import iris
 import logging
 import matplotlib
@@ -10,7 +10,6 @@ from esmvaltool.diag_scripts.shared import (
     run_diagnostic,
     save_data,
     save_figure,
-    select_metadata,
 )
 from esmvaltool.diag_scripts.shared.plot import quickplot
 
@@ -18,17 +17,17 @@ logger = logging.getLogger(Path(__file__).stem)
 
 
 COLOR_MAPS = {
-    'wbor': matplotlib.colors.LinearSegmentedColormap(
-        'wbor',
+    "wbor": matplotlib.colors.LinearSegmentedColormap(
+        "wbor",
         {
-            'red': [
+            "red": [
                 (0.0, 0.968, 0.968),
                 (0.375, 0.28, 0.28),
                 (0.625, 0.925, 0.925),
                 (0.875, 1.0, 1.0),
                 (1.0, 0.5, 0.5),
             ],
-            'green': [
+            "green": [
                 (0.0, 0.957, 0.957),
                 (0.125, 0.7978, 0.79787),
                 (0.375, 0.6383, 0.6383),
@@ -36,140 +35,126 @@ COLOR_MAPS = {
                 (0.875, 0.0, 0.0),
                 (1.0, 0.0, 0.0),
             ],
-            'blue': [
+            "blue": [
                 (0, 0.97, 0.97),
                 (0.125, 0.86, 0.86),
                 (0.375, 0.77, 0.77),
                 (0.625, 0.0, 0.0),
                 (1.0, 0.0, 0.0),
             ],
-        }
-    ),
-    'GMT_polar': matplotlib.colors.LinearSegmentedColormap(
-        'GMT_polar',
-        {
-            'red': [(0.0, 0.0, 0.0), (0.5, 1.0, 1.0), (1.0, 1.0, 1.0)],
-            'green': [(0.0, 0.0, 0.0), (0.5, 1.0, 1.0), (1.0, 0.0, 0.0)],
-            'blue': [(0.0, 1.0, 1.0), (0.5, 1.0, 1.0), (1.0, 0.0, 0.0)],
         },
-        matplotlib.rcParams['image.lut'],
-    )
+    ),
+    "GMT_polar": matplotlib.colors.LinearSegmentedColormap(
+        "GMT_polar",
+        {
+            "red": [(0.0, 0.0, 0.0), (0.5, 1.0, 1.0), (1.0, 1.0, 1.0)],
+            "green": [(0.0, 0.0, 0.0), (0.5, 1.0, 1.0), (1.0, 0.0, 0.0)],
+            "blue": [(0.0, 1.0, 1.0), (0.5, 1.0, 1.0), (1.0, 0.0, 0.0)],
+        },
+        matplotlib.rcParams["image.lut"],
+    ),
 }
 
 
 def get_provenance_record(caption, ancestor_files):
     """Create a provenance record describing the diagnostic data and plot."""
     record = {
-        'caption': caption,
-        'statistics': ['mean'],
-        'domains': ['global'],
-        'plot_types': ['zonal'],
-        'authors': ['righi_mattia'],
-        'references': ['acknow_project'],
-        'ancestors': ancestor_files,
+        "caption": caption,
+        "ancestors": ancestor_files,
     }
     return record
 
 
 def prep_config(cfg):
-    """Additional plot configuration."""
-    for plot_type in ['data_plot', 'diff_plot']:
+    """Prepare plot configuration."""
+    for plot_type in ["data_plot", "diff_plot"]:
         plot_cfg = cfg.get(plot_type)
 
         if plot_cfg is not None:
-            plot_cfg = cfg.get(plot_type)
-
-            cmap = plot_cfg.get('cmap')
+            # Check if color map is set to one of the custom ones
+            # defined by this diagnostic.
+            cmap = plot_cfg.get("cmap")
             if cmap in COLOR_MAPS:
-                plot_cfg['cmap'] = COLOR_MAPS[cmap]
+                plot_cfg["cmap"] = COLOR_MAPS[cmap]
 
-            vmin = plot_cfg.get('vmin')
-            vmax = plot_cfg.get('vmax')
-            levs = plot_cfg.get('levs')
-            if vmin is not None and vmax is not None and levs is not None:
-                plot_cfg['levels'] = np.linspace(vmin, vmax, levs)
-                plot_cfg['extend'] = 'both'
-
-
-def prep_cube(filename):
-    """Load and squeeze cube."""
-    logger.debug('Loading %s', filename)
-    cube = iris.load_cube(filename)
-    cube = iris.util.squeeze(cube)
-    return cube
+            # Get plot limits and levels if they are defined
+            # in the recipe.
+            vmin = plot_cfg.get("vmin")
+            vmax = plot_cfg.get("vmax")
+            levs = plot_cfg.get("levs")
+            if None not in [vmin, vmax, levs]:
+                plot_cfg["levels"] = np.linspace(vmin, vmax, levs)
+                plot_cfg["extend"] = "both"
 
 
-def _plot(cube, basename, provenance_record, plot_type, cfg):
-    """Save diagnostic data and plot it."""
+def save_data_plot(cube, basename, provenance_record, plot_type, cfg):
+    """Save diagnostic data and plot figure."""
     save_data(basename, provenance_record, cfg, cube)
     if cfg.get(plot_type):
         quickplot(cube, **cfg[plot_type])
         save_figure(basename, provenance_record, cfg)
 
 
-def plot_diff(cube, attributes, obs_cube, obs_attributes, cfg):
-    input_file = attributes['filename']
-    obs_input_file = obs_attributes['filename']
+def plot_diff(cube, input_file, obs_cube, obs_input_file, cfg):
     input_file_stem = Path(input_file).stem
     obs_input_file_stem = Path(obs_input_file).stem
 
     # Produce caption and provenance.
-    caption = f'Difference between {input_file_stem} and {obs_input_file_stem}'
+    caption = f"Difference between {input_file_stem} and {obs_input_file_stem}"
     provenance_record = get_provenance_record(
         caption, ancestor_files=[input_file, obs_input_file]
     )
 
     # Compute difference and plot.
     cube_diff = cube - obs_cube
-    output_basename = f'diff_{input_file_stem}_{obs_input_file_stem}'
-    _plot(cube_diff, output_basename, provenance_record, 'diff_plot', cfg)
+    output_basename = f"diff_{input_file_stem}_{obs_input_file_stem}"
+    save_data_plot(cube_diff, output_basename, provenance_record, "diff_plot", cfg)
 
 
-def plot_dataset(cube, attributes, cfg):
-    input_file = attributes['filename']
+def plot_dataset(cube, input_file, cfg):
     input_file_stem = Path(input_file).stem
 
     # Produce caption and provenance.
     caption = input_file_stem
     provenance_record = get_provenance_record(
-        caption, ancestor_files=[input_file]
+        caption,
+        ancestor_files=[input_file],
     )
 
     output_basename = input_file_stem
-    _plot(cube, output_basename, provenance_record, 'data_plot', cfg)
+    save_data_plot(cube, output_basename, provenance_record, "data_plot", cfg)
 
 
 def main(cfg):
     """Compute the time average for each input dataset."""
 
+    # Modify configuration if necessary.
     prep_config(cfg)
 
     # Get a description of the preprocessed data that we will use as input.
-    input_data = cfg['input_data'].values()
-
-    # Get Yang2020 climatology from among the datasets.
-    obs_clim_list = select_metadata(input_data, dataset='Yang2020')
-    attributes_yang2020 = obs_clim_list[0] if len(obs_clim_list) == 1 else None
-
-    cube_yang2020 = prep_cube(attributes_yang2020['filename'])
-    plot_dataset(cube_yang2020, attributes_yang2020, cfg)
+    input_data = cfg["input_data"].values()
 
     # Loop over datasets.
-    grouped_datasets = group_metadata(input_data, 'dataset', sort='activity')
-    for (dataset, attributes_by_activity) in grouped_datasets.items():
-        if dataset != attributes_yang2020['dataset']:
-            for attributes in attributes_by_activity:
+    datasets = group_metadata(input_data, "dataset")
+    for model_dataset, group in datasets.items():
+        logger.info(f"Processing dataset {model_dataset}")
+        logger.info(group)
 
-                logger.info(
-                    f'Processing dataset {dataset} ({attributes["activity"]})'
-                )
-                cube = prep_cube(attributes['filename'])
-                plot_dataset(cube, attributes, cfg)
-                plot_diff(
-                    cube, attributes, cube_yang2020, attributes_yang2020, cfg
-                )
+        for attributes in group:
+            ref_dataset = attributes["reference_dataset"]
+            ref_filename = datasets[ref_dataset][0]["filename"]
+            ref_cube = iris.load_cube(ref_filename)
+
+            if ref_dataset == attributes["dataset"]:
+                plot_dataset(ref_cube, ref_filename, cfg)
+
+            else:
+                filename = attributes["filename"]
+                cube = iris.load_cube(filename)
+                plot_dataset(cube, filename, cfg)
+                plot_diff(cube, filename, ref_cube, ref_filename, cfg)
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     with run_diagnostic() as config:
         main(config)
