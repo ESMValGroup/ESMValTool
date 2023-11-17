7afb2dce7da4db674470ac4bd5f1ddec6dce9ecc
Alistair Sellar <alistair.sellar@metoffice.gov.uk>
2023-11-17 19:37:07 +0000

    Make imagehash package available

--- esmvaltool/utils/recipe_test_workflow/recipe_test_workflow/app/compare/bin/compare_task_runner.sh
+++ esmvaltool/utils/recipe_test_workflow/recipe_test_workflow/app/compare/bin/compare_task_runner.sh
@@ -9,5 +9,8 @@ KGO="${KGO_ROOT_PATH}/recipe_${KGO_METRIC}*"
 # Create a variable that defines the path to the compare script.
 COMPARE_SCRIPT="${ESMVALTOOL_DIR}/esmvaltool/utils/testing/regression/compare.py"
 
+# Pick up local installation of imagehash. See #TODO raise issue
+export PYTHONPATH=${PYTHONPATH}:${HOME}/.local/lib/python3.11/site-packages
+
 # Run the compare script.
 python "${COMPARE_SCRIPT}" ${KGO} ${FOLDER_NAME}
