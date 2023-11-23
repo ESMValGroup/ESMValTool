95495d96489f20ddaae289c1f74835d800572960
Alistair Sellar <alistair.sellar@metoffice.gov.uk>
2023-11-23 13:30:29 +0000

    Replace TODOs with issues

--- esmvaltool/utils/recipe_test_workflow/recipe_test_workflow/app/compare/bin/compare_task_runner.sh
+++ esmvaltool/utils/recipe_test_workflow/recipe_test_workflow/app/compare/bin/compare_task_runner.sh
@@ -9,7 +9,7 @@ KGO="${KGO_ROOT_PATH}/recipe_${KGO_METRIC}*"
 # Create a variable that defines the path to the compare script.
 COMPARE_SCRIPT="${ESMVALTOOL_DIR}/esmvaltool/utils/testing/regression/compare.py"
 
-# Pick up local installation of imagehash. See #TODO raise issue
+# Pick up local installation of imagehash. Slightly fragile: see #3436
 export PYTHONPATH=${PYTHONPATH}:${HOME}/.local/lib/python3.11/site-packages
 
 # Run the compare script.
