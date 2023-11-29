09e81ed7a680eee6bbd1fe8d4df545df6002ce2c
Felicity Chun <felicity.chun@anu.edu.au>
2023-11-29 12:46:22 +1100

    agcd.py remove additional line

--- esmvaltool/cmorizers/data/formatters/datasets/agcd.py
+++ esmvaltool/cmorizers/data/formatters/datasets/agcd.py
@@ -26,10 +26,7 @@ logger = logging.getLogger(__name__)
 
 
 def _get_filepaths(in_dir, basename):
-    """Find correct name of file (extend basename with timestamp).
-        
-        Searches sub folders of raw data directory
-        """
+    """Find correct name of file (extend basename with timestamp)."""
     regex = re.compile(basename)
     return_files = []
     for root, _, files in os.walk(in_dir, followlinks=True):
