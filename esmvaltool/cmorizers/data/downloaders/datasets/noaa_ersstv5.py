fb05c1016ed081bddf7bfa4bbe5a64031cb9909a
rbeucher <romain.beucher@anu.edu.au>
2023-11-30 12:16:51 +1100

    Add downloaders NOAA-ERSST

---
+++ esmvaltool/cmorizers/data/downloaders/datasets/noaa_ersstv5.py
@@ -0,0 +1,42 @@
+"""Script to download NOAA-ERSST-V5."""
+import logging
+
+from esmvaltool.cmorizers.data.downloaders.wget import WGetDownloader
+
+logger = logging.getLogger(__name__)
+
+
+def download_dataset(config, dataset, dataset_info, start_date, end_date,
+                     overwrite):
+    """Download dataset.
+
+    Parameters
+    ----------
+    config : dict
+        ESMValTool's user configuration
+    dataset : str
+        Name of the dataset
+    dataset_info : dict
+         Dataset information from the datasets.yml file
+    start_date : datetime
+        Start of the interval to download
+    end_date : datetime
+        End of the interval to download
+    overwrite : bool
+        Overwrite already downloaded files
+    """
+    downloader = WGetDownloader(
+        config=config,
+        dataset=dataset,
+        dataset_info=dataset_info,
+        overwrite=overwrite,
+    )
+
+    downloader.download_file(
+        "https://www1.ncdc.noaa.gov/pub/data/cmb/ersst/v5/netcdf/ersst.v5.1854-2019.nc",
+        wget_options=[],
+    )
+    downloader.download_file(
+        "https://www1.ncdc.noaa.gov/pub/data/cmb/ersst/v5/netcdf/ersst.v5.fldmean.1854-2019.nc",
+        wget_options=[],
+    )
