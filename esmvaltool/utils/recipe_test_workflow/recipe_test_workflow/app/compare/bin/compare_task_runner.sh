ed59b2c02c0692180a39202ff48c266b40538dff
Alistair Sellar <alistair.sellar@metoffice.gov.uk>
2023-12-04 15:33:16 +0000

    Edit PYTHONPATH via jasmin-env instead of compare script

--- esmvaltool/utils/recipe_test_workflow/recipe_test_workflow/app/compare/bin/compare_task_runner.sh
+++ esmvaltool/utils/recipe_test_workflow/recipe_test_workflow/app/compare/bin/compare_task_runner.sh
@@ -9,8 +9,5 @@ KGO="${KGO_ROOT_PATH}/recipe_${KGO_METRIC}*"
 # Create a variable that defines the path to the compare script.
 COMPARE_SCRIPT="${ESMVALTOOL_DIR}/esmvaltool/utils/testing/regression/compare.py"
 
-# Pick up local installation of imagehash. Slightly fragile: see #3436
-export PYTHONPATH=${PYTHONPATH}:${HOME}/.local/lib/python3.11/site-packages
-
 # Run the compare script.
 python "${COMPARE_SCRIPT}" ${KGO} ${FOLDER_NAME}
