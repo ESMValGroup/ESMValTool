24e549b8a8480f195ad39a08e75c1b3eb0a74411
Gulcin G <gulcin.gulletutan@gmail.com>
2024-02-06 20:21:48 +0100

    adding new variables- 1 model & 4 diff variables

--- esmvaltool/diag_scripts/o3_feb.py
+++ esmvaltool/diag_scripts/o3_feb.py
@@ -15,7 +15,7 @@ logger = logging.getLogger(Path(__file__).stem)
 def plotting_support(cube, key, linestyle='-', marker=None, color=None):
     """Helper function for pretty plotting."""
     print(f"Plotting {key} - Cube data: {cube.data}")
-        
+
     if cube.coords('time', dim_coords=True):
         ih.unify_time_coord(cube)
 
@@ -42,7 +42,7 @@ def plot_anomalies(input_file, name, variable_group, color=None):
         logger.error(f"Error plotting: {e}")
         return None
 
-def create_subplot(ax, data_list, variable_group, min_time, max_time):
+def create_subplot(ax, data_list, variable_group):
     """Plot subplot over time considering minimum and maximum time across dataset."""
     for i, data in enumerate(data_list):
         color = plt.cm.tab10(i % 10)
@@ -51,7 +51,7 @@ def create_subplot(ax, data_list, variable_group, min_time, max_time):
     ax.set_ylabel(f"{data_list[0]['cube'].var_name.upper()} Anomalies, " + str(data_list[0]['cube'].units))
     ax.set_title(f"Time series of monthly mean {data_list[0]['cube'].var_name.upper()} anomalies - {variable_group}")
     ax.legend([data['name'] for data in data_list], loc="center", bbox_to_anchor=(1, 0.5), borderaxespad=0.)
-    ax.set_xlim(min_time, max_time)
+    ax.set_xlim('2010', '2014')
     ax.set_xlabel('Time')
     ax.xaxis.set_major_formatter(plt.matplotlib.dates.DateFormatter('%Y-%m'))
     ax.xaxis.set_major_locator(plt.matplotlib.dates.AutoDateLocator())
@@ -85,10 +85,6 @@ def main(cfg):
         if data:
             data_by_variable_group[variable_group].append(data)
 
-    # Min and max time values over all datasets
-    min_time = min(data['cube'].coord('time').points.min() for data_list in data_by_variable_group.values() for data in data_list)
-    max_time = max(data['cube'].coord('time').points.max() for data_list in data_by_variable_group.values() for data in data_list)
-
     # Check the data_list
     for variable_group, data_list in data_by_variable_group.items():
         if not data_list:
@@ -96,15 +92,14 @@ def main(cfg):
 
         plt.figure(figsize=(8, 6))
 
-        create_subplot(plt.gca(), data_list, variable_group, min_time, max_time)
+        create_subplot(plt.gca(), data_list, variable_group)
          
         # Save separate figures for each variable
         plt.savefig(f'plot_{variable_group}.png')
 
 if __name__ == '__main__':
     with run_diagnostic() as config:
-#        start_year = 1970  # I specified desired start and end year because it doesn't work well without this
-#        end_year = 2014
         main(config)
+
 
 
--- esmvaltool/diag_scripts/o3_feb_extract_region.py
+++ esmvaltool/diag_scripts/o3_feb_extract_region.py
@@ -2,6 +2,7 @@ import logging
 from pathlib import Path
 import iris
 import matplotlib.pyplot as plt
+from iris import Constraint
 from esmvalcore.preprocessor import (
     extract_levels,
     anomalies,
@@ -21,6 +22,21 @@ def plotting_support(cube, key, linestyle='-', marker=None, color=None):
 
     plt.plot(cube.coord('time').points, cube.data, label=key, linestyle=linestyle, marker=marker, color=color)
 
+def extract_region(cube):
+
+#    latitude_constraint = iris.Constraint(
+#        latitude=lambda cell: extract_region['start latitude'] <= cell <= extract_region['end latitude']
+#    longitude_constraint = iris.Constraint(
+#        longitude=lambda cell: extract_region['start longitude'] <= cell <= extract_region['end longitude']
+
+#    cube = cube.extract(latitude_constraint & longitude_constraint)
+
+    cube = cube.extract(iris.Constraint(latitude=lambda cell: -20 <= cell <= 20),
+                       iris.Constraint(longitude=lambda cell: 0 <= cell <= 360))
+
+
+    return cube
+
 def plot_anomalies(input_file, name, variable_group, color=None):
     """
     Plot anomalies over time using the provided plotting support function.
@@ -32,6 +48,9 @@ def plot_anomalies(input_file, name, variable_group, color=None):
         # Extract time and anomalies
         if cube.coords('time', dim_coords=True):
             ih.unify_time_coord(cube)
+        
+        # extract region
+        cube = extract_region(cube)
 
         # Plotting function by using the plotting_support function
         plotting_support(cube, key=name, linestyle='-', marker=None, color=color)
@@ -51,7 +70,7 @@ def create_subplot(ax, data_list, variable_group, min_time, max_time):
     ax.set_ylabel(f"{data_list[0]['cube'].var_name.upper()} Anomalies, " + str(data_list[0]['cube'].units))
     ax.set_title(f"Time series of monthly mean {data_list[0]['cube'].var_name.upper()} anomalies - {variable_group}")
     ax.legend([data['name'] for data in data_list], loc="center", bbox_to_anchor=(1, 0.5), borderaxespad=0.)
-    ax.set_xlim(min_time, max_time)
+    ax.set_xlim('2010', '2014')
     ax.set_xlabel('Time')
     ax.xaxis.set_major_formatter(plt.matplotlib.dates.DateFormatter('%Y-%m'))
     ax.xaxis.set_major_locator(plt.matplotlib.dates.AutoDateLocator())
@@ -85,10 +104,6 @@ def main(cfg):
         if data:
             data_by_variable_group[variable_group].append(data)
 
-    # Min and max time values over all datasets
-    min_time = min(data['cube'].coord('time').points.min() for data_list in data_by_variable_group.values() for data in data_list)
-    max_time = max(data['cube'].coord('time').points.max() for data_list in data_by_variable_group.values() for data in data_list)
-
     # Check the data_list
     for variable_group, data_list in data_by_variable_group.items():
         if not data_list:
@@ -96,15 +111,14 @@ def main(cfg):
 
         plt.figure(figsize=(8, 6))
 
-        create_subplot(plt.gca(), data_list, variable_group, min_time, max_time)
+        create_subplot(plt.gca(), data_list, variable_group)
          
         # Save separate figures for each variable
         plt.savefig(f'plot_{variable_group}.png')
 
 if __name__ == '__main__':
     with run_diagnostic() as config:
-#        start_year = 1970  # I specified desired start and end year because it doesn't work well without this
-#        end_year = 2014
         main(config)
+
 
 
