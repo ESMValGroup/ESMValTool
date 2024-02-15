881e8ae13584ede8b454e51ef4549fd49cab0981
Axel Lauer <axel.lauer@dlr.de>
2024-02-15 13:09:47 +0100

    snapshot 2024-02-15

--- esmvaltool/cmorizers/data/formatters/datasets/esacci_snow.py
+++ esmvaltool/cmorizers/data/formatters/datasets/esacci_snow.py
@@ -27,8 +27,10 @@ from datetime import datetime
 from dateutil import relativedelta
 
 import iris
+from dask import array as da
 from iris.util import equalise_attributes, unify_time_units
 from esmvalcore.cmor.table import CMOR_TABLES
+from esmvalcore.preprocessor import regrid
 
 from esmvaltool.cmorizers.data.utilities import (
     fix_var_metadata,
@@ -38,6 +40,28 @@ from esmvaltool.cmorizers.data.utilities import (
 
 logger = logging.getLogger(__name__)
 
+
+def _fix_coordinates(cube, definition):
+    """Fix coordinates."""
+    axis2def = {'T': 'time', 'X': 'longitude', 'Y': 'latitude'}
+    axes = ['T', 'X', 'Y']
+
+    for axis in axes:
+        coord_def = definition.coordinates.get(axis2def[axis])
+        if coord_def:
+            coord = cube.coord(axis=axis)
+            if axis == 'T':
+                coord.convert_units('days since 1850-1-1 00:00:00.0')
+            coord.standard_name = coord_def.standard_name
+            coord.var_name = coord_def.out_name
+            coord.long_name = coord_def.long_name
+            coord.points = coord.core_points().astype('float64')
+            if len(coord.points) > 1:
+                if coord.bounds is not None:
+                    coord.bounds = None
+                coord.guess_bounds()
+
+    return cube
 
 def _extract_variable(in_files, var, cfg, out_dir):
     logger.info("CMORizing variable '%s' from input files '%s'",
@@ -49,65 +73,45 @@ def _extract_variable(in_files, var, cfg, out_dir):
     definition = cmor_table.get_variable(var['mip'], var['short_name'])
 
     # load all input files (1 year) into 1 cube
-    cube_list = iris.load_raw(in_files)
+    # --> drop attributes that differ among input files
+    cube_list = iris.load_raw(in_files, var['raw'])
+    # global attributes to remove
     drop_attrs = [
         'source', 'date_created', 'history', 'tracking_id',
         'id', 'time_coverage_start', 'time_coverage_end', 'platform'
     ]
-
+    # variable attributes to remove
+    drop_var_attrs = [
+        'flag_meanings', 'flag_values', 'grid_mapping', 'actual_range'
+    ]
     for cube in cube_list:
         for attr in drop_attrs:
-            cube.attributes.pop(attr)
-        cube.coord('time').points = cube.coord('time').core_points().astype(
-            'float64')
+            if attr in cube.attributes.keys():
+                cube.attributes.pop(attr)
+        for attr in drop_var_attrs:
+            if attr in cube.attributes.keys():
+                cube.attributes.pop(attr)
+
+    # Remove ancillary data in all cubes to allow concatenating cubes
+    for cube in cube_list:
+        for ancillary_variable in cube.ancillary_variables():
+            cube.remove_ancillary_variable(ancillary_variable)
+                
     iris.util.unify_time_units(cube_list)
     cube = cube_list.concatenate_cube()
 
-#    # keep the following raw cube attributes
-#    attrs_to_keep = [
-#        "institution", "Institution",
-#        "institute_id", "VersionID",
-#        "experiment_id",
-#        "source", "Source",  # overrides empty string default
-#        "model_id", "ModelID",
-#        "contact", "Contact",
-#        "references",
-#        "tracking_id",
-#        "mip_specs",  # described by "mip" already
-#        "source_id", "SourceID",
-#        "product", "Product",
-#        "frequency", "Frequency",
-#        "creation_date",
-#        "project_id", "ProjectID",
-#        "table_id", "TableID",
-#        "title", "Title",
-#        "modeling_realm",
-#        "doi",
-#        "VersionID",  # described by "version" already
-#    ]
-#
-#    attrs_to_keep_exist = [
-#        att for att in cube.attributes if att in attrs_to_keep
-#    ]
-#    for att in attrs_to_keep_exist:
-#        attributes[att] = cube.attributes[att]
-#
-#    utils.set_global_atts(cube, attributes)
+    cube.coord('time').points = cube.coord('time').core_points().astype(
+        'float64')
 
     # Set correct names
     cube.var_name = definition.short_name
-    # cube.standard_name = definition.standard_name
+    cube.standard_name = definition.standard_name
     cube.long_name = definition.long_name
 
-
-
-
-    # Fix units (if needed)
-    # input variable reports m-3 m-3 instead of m3 m-3
-    if cube.var_name == "sm":
-        cube.units = definition.units
-    # Convert units to CMOR units
-    cube.convert_units(definition.units)
+    # Fix units
+    # input variable for snc (sncf) reports 'percent' --> rename to '%'
+    # input variable for snw (swe) reports 'mm' --> rename to 'kg m-2'
+    cube.units = definition.units
 
     # Fix data type
     cube.data = cube.core_data().astype('float32')
@@ -116,20 +120,35 @@ def _extract_variable(in_files, var, cfg, out_dir):
     cube.coord('longitude').points = cube.coord('longitude').points + 180.
     nlon = len(cube.coord('longitude').points)
     cube.data = da.roll(cube.core_data(), int(nlon / 2), axis=-1)
+    cube.attributes.update({"geospatial_lon_min": "0",
+                            "geospatial_lon_max": "360"})
 
     # Fix coordinates
     cube = _fix_coordinates(cube, definition)
-
     cube.coord('latitude').attributes = None
     cube.coord('longitude').attributes = None
 
-    cube = _fix_time_monthly(cube)
+    # remove flags and other invalid points
+    missingvalue = 1e20
+    if cube.var_name == 'snc':
+        cube.data = da.where(cube.data > 100, missingvalue, cube.data)
+    elif cube.var_name == 'snw':
+        cube.data = da.where(cube.data < 0, missingvalue, cube.data)
 
+    # regridding from 0.05x0.05 to 0.5x0.5 to save space
+    #cube = regrid(cube, target_grid='0.5x0.5', scheme='area_weighted')
+    cube.attributes.update({"geospatial_lon_resolution": "0.5",
+                            "geospatial_lat_resolution": "0.5",
+                            "spatial_resolution": "0.5"})
+
+    # Save results
+    attributes['fill_value'] = missingvalue
     logger.debug("Saving cube\n%s", cube)
     logger.debug("Setting time dimension to UNLIMITED while saving!")
-    utils.save_variable(cube, cube.var_name,
-                        out_dir, attributes,
-                        unlimited_dimensions=['time'])
+    save_variable(cube, cube.var_name,
+                  out_dir, attributes,
+                  unlimited_dimensions=['time']
+                 )
     logger.info("Finished CMORizing %s", ', '.join(in_files))
 
 def cmorization(in_dir, out_dir, cfg, cfg_user, start_date, end_date):
@@ -140,22 +159,25 @@ def cmorization(in_dir, out_dir, cfg, cfg_user, start_date, end_date):
                 glob_attrs['tier'], glob_attrs['dataset_id'])
     logger.info("Input data from: %s", in_dir)
     logger.info("Output will be written to: %s", out_dir)
-    logger.info('Cmorizing ESACCI-SNOW version %s', glob_attrs['version'])
+    logger.info('CMORizing ESACCI-SNOW version %s', glob_attrs['version'])
 
     if start_date is None:
-        start_date = datetime(1985,1,1) #1979, 1, 1)
+        start_date = datetime(1979, 1, 1)
     if end_date is None:
-        end_date = datetime(1985,1,1) #2019, 12, 31)
+        end_date = datetime(2019, 12, 31)
 
     for short_name, var in cfg['variables'].items():
         if 'short_name' not in var:
             var['short_name'] = short_name
         loop_date = start_date
         while loop_date <= end_date:
-            filepattern = os.path.join(in_dir, var['file'].format(year=loop_date.year))
+            filepattern = os.path.join(
+                in_dir, var['file'].format(year=loop_date.year)
+                )
             in_files = glob.glob(filepattern)
             if not in_files:
-                logger.info(f'{loop_date.year}: no data not found for variable {short_name}')
+                logger.info(f'{loop_date.year}: no data not found for '
+                            f'variable {short_name}')
             else:
                 _extract_variable(in_files, var, cfg, out_dir)
 
