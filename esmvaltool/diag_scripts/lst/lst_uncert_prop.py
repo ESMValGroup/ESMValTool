2be9282e9236f643e9bfde84741b55102a301a92
morobking <rob.king@metoofice.gov.uk>
2024-04-16 12:53:30 +0100

    Total day/night uncertainty add, sampling uncertainity fixed, n_fill etc... defnied

--- esmvaltool/diag_scripts/lst/lst_uncert_prop.py
+++ esmvaltool/diag_scripts/lst/lst_uncert_prop.py
@@ -125,7 +125,7 @@ def _diagnostic(config):
     # lst_unc_loc_sfc DEPENDS ON SENSOR
     # see Table 1 and then table 4
     # MODIS Aqua and Terra = GSW eq 6 = eq 1 eq_arithmetic_mean.
-    # lst_unc_ran eq 4 !!! TO DO !!!
+    # lst_unc_ran eq 4 eq_propagate_random_with_sampling
     # total uncert  eq 9 eq_sum_in_quadrature
 
     lst_unc_variables = ['lst_unc_loc_atm', 'lst_unc_sys',
@@ -133,33 +133,64 @@ def _diagnostic(config):
 
     propagated_values = {}
 
+    time_coord = {}
+    time_coord['day'] = loaded_data['ESACCI-LST']['ts_day'].coord('time')
+    time_coord['night'] = loaded_data['ESACCI-LST']['ts_night'].coord('time')
+
     # for now
     lat_len = len(loaded_data['ESACCI-LST']['ts_day'].coord('latitude').points)
     lon_len = len(loaded_data['ESACCI-LST']['ts_day'].coord('longitude').points)
     n_use = lat_len*lon_len*0.75 # for testing
 
+    n_fill = {}
+    n_use = {}
+    n_fill['day'] = np.array([np.sum(item) for item in loaded_data['ESACCI-LST']['ts_day'].data.mask])
+    n_fill['night'] = np.array([np.sum(item) for item in loaded_data['ESACCI-LST']['ts_night'].data.mask])
+    
+    n_use['day'] = (lat_len*lon_len) - n_fill['day']
+    n_use['night'] = (lat_len*lon_len)
+
     for time in ['day', 'night']:
         
         propagated_values[f'ts_{time}'] = eq_arithmetic_mean(loaded_data['ESACCI-LST'][f'ts_{time}'])
         propagated_values[f'lst_unc_loc_atm_{time}'] = eq_weighted_sqrt_mean(loaded_data['ESACCI-LST'][f'lst_unc_loc_atm_{time}'],
-                                                                             n_use)
+                                                                             n_use[f'{time}'])
         # no spatial propagation
         propagated_values[f'lst_unc_sys_{time}'] = loaded_data['ESACCI-LST'][f'lst_unc_sys_{time}']
 
         propagated_values[f'lst_unc_loc_sfc_{time}'] = eq_arithmetic_mean(loaded_data['ESACCI-LST'][f'lst_unc_loc_sfc_{time}'])
 
-        propagated_values[f'lst_unc_ran_{time}'] = eq_propagate_random_with_sampling(loaded_data['ESACCI-LST'][f'lst_unc_ran_{time}'],
-                                                                                     loaded_data['ESACCI-LST'][f'ts_{time}'])
+        propagated_values[f'lst_unc_ran_{time}'], propagated_values[f'lst_sampling_{time}'] = eq_propagate_random_with_sampling(loaded_data['ESACCI-LST'][f'lst_unc_ran_{time}'],
+                                                                                                                                loaded_data['ESACCI-LST'][f'ts_{time}'],
+                                                                                                                                n_use[f'{time}'], n_fill[f'{time}'])
 
-
+    print('*************************************')
+    # for KEY in propagated_values.keys(): 
+    #     try:
+    #         print(KEY, propagated_values[KEY].coord('time') == time_coord['day'])
+    #         print(KEY, propagated_values[KEY].coord('time') == time_coord['night'])
+    #     except:
+    #         pass
     for time in ['day', 'night']:
-       
+    #     for variable in lst_unc_variables:
+    #         print(time, variable, propagated_values[f'{variable}_{time}'])
+
         time_cubelist = iris.cube.CubeList([propagated_values[f'{variable}_{time}']
                                             for variable in lst_unc_variables])
+        print(time_cubelist)
 
         propagated_values[f'lst_total_unc_{time}'] = eq_sum_in_quadrature(time_cubelist)
-                                
-                                        
+                   
+    # # # # # # print('*************************************')
+    # # # # # # for KEY in propagated_values.keys():                              
+    # # # # # #     try:
+    # # # # # #         print(KEY, propagated_values[KEY].data)
+    # # # # # #     except:
+    # # # # # #         print(KEY, propagated_values[KEY])
+
+
+    print('mmmmmmmmmmmmmmmmmmmmm')
+    print(type(propagated_values[f'lst_sampling_day']))
 
     test_plot(propagated_values)
     
@@ -178,14 +209,17 @@ def test_plot(propagated_values):
         iplt.plot(propagated_values[f'lst_unc_sys_{time}'], c='b', label=f'lst_unc_sys_{time}')
         iplt.plot(propagated_values[f'lst_unc_ran_{time}'], c='m', label=f'lst_unc_ran_{time}')
         
+        iplt.plot(propagated_values[f'lst_sampling_{time}'], c='c',label=f'lst_sampling_{time}')
         iplt.plot(propagated_values[f'lst_total_unc_{time}'], c='k',label=f'lst_total_unc_{time}')
 
         plt.legend()
 
         plt.savefig(f'test_{time}.png')
 
+
+
 # make function for each propagation equation
-def eq_propagate_random_with_sampling(cube_unc_ran, cube_ts):
+def eq_propagate_random_with_sampling(cube_unc_ran, cube_ts, n_fill, n_use):
     """Propagate radom uncertatinty using the sampling uncertainty
     ATBD eq 4
 
@@ -201,21 +235,24 @@ def eq_propagate_random_with_sampling(cube_unc_ran, cube_ts):
     cube_unc_ran: The cube with the lst_unc_ran day/night data
     cube_ts:      The lst use for day/night as appropriate
     """
-    lat_len = len(cube_ts.coord('latitude').points)
-    lon_len = len(cube_ts.coord('longitude').points)
-    n_total = lon_len * lat_len
+    
+    n_total = n_fill + n_use
 
-    n_fill = np.sum(cube_ts.data.mask)
-    print(f'{n_fill=}')
+    #n_fill = np.sum(cube_ts.data.mask)
+    #print(f'{n_fill=}')
 
     unc_ran_mean = eq_arithmetic_mean(cube_unc_ran)
     
     lst_variance = cube_ts.collapsed(['latitude','longitude'], iris.analysis.VARIANCE)
-    unc_sampling = (n_fill * lst_variance) / (n_total - 1)
+    #unc_sampling = (n_fill * lst_variance) / (n_total - 1)
+    factor = n_fill/(n_total - 1)
+    print(factor)
 
-    output = eq_sum_in_quadrature(iris.cube.CubeList([unc_ran_mean]))
+    unc_sampling = iris.analysis.maths.multiply(lst_variance, n_fill/(n_total-1))
+    output = eq_sum_in_quadrature(iris.cube.CubeList([unc_ran_mean**2, unc_sampling]))
+    output  = iris.analysis.maths.exponentiate(output, 0.5)
 
-    return output
+    return output, unc_sampling
     
 def eq_arithmetic_mean(cube):
     """Arithmetic mean of cube, across latitude and longitude
@@ -233,10 +270,10 @@ def eq_sum_in_quadrature(cubelist):
     Input:
     cubelist : A cubelist of 1D cubes
     """
-
+    print('................')
     # dont want to inplace replace the input
     newlist=cubelist.copy()
-
+    print(newlist)
     for cube in newlist:
         iris.analysis.maths.exponentiate(cube, 2, in_place=True)
 
@@ -257,8 +294,9 @@ def eq_weighted_sqrt_mean(cube, n_use):
     n_use: the number of useable pixels - NEED TO IMPLIMENT A CHECK ON MASKS BEING THE SAME
     """
 
-    output = (1/np.sqrt(n_use)) * cube.collapsed(['latitude', 'longitude'], iris.analysis.MEAN)
-    
+    #output = (1/np.sqrt(n_use)) * cube.collapsed(['latitude', 'longitude'], iris.analysis.MEAN)
+    output = iris.analysis.maths.multiply(cube.collapsed(['latitude', 'longitude'], iris.analysis.MEAN),
+                                 1/np.sqrt(n_use))
     return output
 
 if __name__ == '__main__':
