38a42e60e54defb28883f4ae11f29ad0c27919cd
Ed <146008263+mo-gill@users.noreply.github.com>
2024-05-21 12:50:31 +0100

    #3600: Store RTW files in share directory in installed workflow

--- esmvaltool/utils/recipe_test_workflow/recipe_test_workflow/app/install_env_file/bin/install_env_file.sh
+++ esmvaltool/utils/recipe_test_workflow/recipe_test_workflow/app/install_env_file/bin/install_env_file.sh
@@ -3,15 +3,15 @@
 BASH_XTRACEFD=1
 set -eux
 
-# Copy the site specific environment file to the 'bin' directory in the
+# Copy the site specific environment file to the 'share/bin' directory in the
 # installed Cylc workflow (this directory is automatically added to the
 # ${PATH} by Cylc).
 SOURCE_PATH="${CYLC_WORKFLOW_RUN_DIR}/site/${SITE}-env"
-TARGET_DIR="${CYLC_WORKFLOW_RUN_DIR}/bin"
+TARGET_DIR="${CYLC_WORKFLOW_SHARE_DIR}/bin"
 ENV_FILE="rtw-env"
 
-# Create the 'bin' directory in the installed workflow.
+# Create the 'share/bin' directory in the installed workflow.
 mkdir "${TARGET_DIR}"
 
-# Copy the environment file to the 'bin' directory.
+# Copy the environment file to the 'share/bin' directory.
 cp "${SOURCE_PATH}" "${TARGET_DIR}/${ENV_FILE}"
