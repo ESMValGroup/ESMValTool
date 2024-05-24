3c6bc268df4ced0193e5b7ba23e0dc140d26ef17
Ed Gillett <146008263+mo-gill@users.noreply.github.com>
2024-05-24 15:23:11 +0100

    #3074: Fix project title due to visual overlap.

--- esmvaltool/utils/recipe_test_workflow/doc/source/conf.py
+++ esmvaltool/utils/recipe_test_workflow/doc/source/conf.py
@@ -16,7 +16,7 @@
 
 # -- Project information -----------------------------------------------------
 
-project = 'Recipe Test Workflow (RTW)'
+project = 'RTW'
 version = "0.1.0"
 
 # -- General configuration ---------------------------------------------------
@@ -39,7 +39,7 @@ exclude_patterns = []
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = 'sphinx_rtd_theme'
+html_theme = 'pydata_sphinx_theme'
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
