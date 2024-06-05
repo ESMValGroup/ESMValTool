50a5e6380da46dae14ef953e373424b6befc87c9
alistair.sellar <16133375+alistairsellar@users.noreply.github.com>
2024-06-05 12:48:05 +0100

    Add init.py file with command group and compare command

---
+++ esmvaltool/utils/develop/__init__.py
@@ -0,0 +1,29 @@
+"""Interface to utility commands for develop command group"""
+
+import sys
+from pathlib import Path
+from esmvaltool.utils.develop import compare
+
+class DevelopCommand():
+    """Development utilities"""
+
+    def compare(self,
+                reference_dir, 
+                current_dir, 
+                verbose=False):
+        """Compare a recipe run to a reference run.
+        Returns True if the runs were identical, False otherwise.
+    
+        Parameters
+        ----------
+        reference_dir : str
+            Results directory from reference run
+        current_dir : str
+            Results directory from run to be tested
+        verbose : bool
+            Produce verbose output
+        """
+        
+        same = compare.compare(Path(reference_dir), Path(current_dir), verbose)
+
+        sys.exit(int(same))
