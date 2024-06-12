a0584e83978f7e10990601ad523ab4db0df5ea5b
Valeriu Predoi <valeriu.predoi@gmail.com>
2024-06-12 16:59:24 +0100

    comment out all imports from shared init

--- esmvaltool/diag_scripts/shared/__init__.py
+++ esmvaltool/diag_scripts/shared/__init__.py
@@ -1,22 +1,22 @@
 """Code that is shared between multiple diagnostic scripts."""
 # from . import io, iris_helpers, names, plot
-from ._base import (
-    ProvenanceLogger,
-    extract_variables,
-    get_cfg,
-    get_diagnostic_filename,
-    get_plot_filename,
-    group_metadata,
-    run_diagnostic,
-    save_data,
-    save_figure,
-    select_metadata,
-    sorted_group_metadata,
-    sorted_metadata,
-    variables_available,
-)
-from ._diag import Datasets, Variable, Variables
-from ._validation import apply_supermeans, get_control_exper_obs
+# from ._base import (
+#    ProvenanceLogger,
+#    extract_variables,
+#    get_cfg,
+#    get_diagnostic_filename,
+#    get_plot_filename,
+#    group_metadata,
+#   run_diagnostic,
+#    save_data,
+#    save_figure,
+#    select_metadata,
+#    sorted_group_metadata,
+#    sorted_metadata,
+#    variables_available,
+#)
+# from ._diag import Datasets, Variable, Variables
+# from ._validation import apply_supermeans, get_control_exper_obs
 
 __all__ = [
     # Main entry point for diagnostics
