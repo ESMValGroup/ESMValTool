04e1d62ea5b06f6fc555f53f9cb935c3e8114076
Lisa Bock <lisa.bock@dlr.de>
2024-06-28 15:43:29 +0200

    add monthly sst

--- esmvaltool/cmorizers/data/formatters/datasets/esacci_sst.py
+++ esmvaltool/cmorizers/data/formatters/datasets/esacci_sst.py
@@ -1,32 +1,30 @@
 """ESMValTool CMORizer for ESACCI-SST data.
 
 Tier
-   Tier 2: other freely-available dataset.
+   Tier 3: need to register at CEDA
 
 Source
-   http://surftemp.net/regridding/index.html
+   https://catalogue.ceda.ac.uk/uuid/4a9654136a7148e39b7feb56f8bb02d2
 
 Last access
-   20201214
+   20240628
 
 Download and processing instructions
-   Download the following files:
-     Go to http://surftemp.net/regridding/index.html
-     and request regridded data with the following options:
-       Time Resolution: monthly
-       Longitude Resolution: 0.5
-       Latitude Resolution: 0.5
-       Start Date: 1982-01-01
-       End Date: 2019-12-31
-       Exclude data above sea ice threshold: True
-         (Threshold: 100 %)
-       Include post-hoc SST bias adjustments: True
-       Output Absolute or Anomaly SST: absolute
-       Generate Sea Ice Fraction: True
-       Error Correlation in Time (Days): 7
-       Error Correlation In Space (Degrees): 3.0
+   A donwnloader is provided by ESMValTool. First you need
+   to register.
+   Go to https://services.ceda.ac.uk/cedasite/register/info/
+   and create an account at CEDA if needed.
+  
+?       Exclude data above sea ice threshold: True
+?         (Threshold: 100 %)
+?       Include post-hoc SST bias adjustments: True
+?       Output Absolute or Anomaly SST: absolute
+?       Generate Sea Ice Fraction: True
+?       Error Correlation in Time (Days): 7
+?       Error Correlation In Space (Degrees): 3.0
 
 Modification history
+   20240618-bock_lisa: update for v3.0
    20201204-roberts_charles: written.
    20201214-predoi_valeriu: approved.
    20201214-lauer_axel: approved.
@@ -40,7 +38,7 @@ import iris
 from esmvalcore.cmor.fixes import get_time_bounds
 from esmvaltool.cmorizers.data import utilities as utils
 from esmvalcore.preprocessor import concatenate
-from esmvalcore.preprocessor import regrid
+from esmvalcore.preprocessor import regrid, monthly_statistics
 
 from ...utilities import (
     convert_timeunits,
@@ -86,18 +84,17 @@ def cmorization(in_dir, out_dir, cfg, cfg_user, start_date, end_date):
         var_info = cmor_table.get_variable(vals['mip'], var)
         glob_attrs['mip'] = vals['mip']
         raw_info = {'name': vals['raw']}
-        #inpfile = os.path.join(in_dir, cfg['filename'])
         inpfile_pattern = os.path.join(in_dir, '{year}*'+vals['filename'])
         logger.info("CMORizing var %s from file type %s", var, inpfile_pattern)
         for year in range(vals['start_year'], vals['end_year'] + 1):
             logger.info("Processing year %s", year)
-            for month in range(1,13):
+            mon_cubes = []
+            #for month in range(1,13):
+            for month in range(1,4):
                 data_cubes = []
-                #year_inpfile_pattern = inpfile_pattern.format(year=year)
                 month_inpfile_pattern = inpfile_pattern.format(year=str(year)+"{:02}".format(month))
                 logger.info("Pattern: %s", month_inpfile_pattern)
                 inpfiles = sorted(glob.glob(month_inpfile_pattern))
-                #inpfiles = sorted(glob.glob(year_inpfile_pattern))
                 logger.info("Found input files: %s", inpfiles)
                 for inpfile in inpfiles:
                     raw_info['file'] = inpfile
@@ -105,12 +102,22 @@ def cmorization(in_dir, out_dir, cfg, cfg_user, start_date, end_date):
                                 raw_info['file'])
                     data_cubes.append(
                         extract_variable(var_info, raw_info, glob_attrs, year))
-                yearly_cube = concatenate(data_cubes)
+                monthly_cube = concatenate(data_cubes)
                 # Fix monthly time bounds
-                time = yearly_cube.coord('time')
+                time = monthly_cube.coord('time')
                 time.bounds = get_time_bounds(time, vals['frequency'])
-                save_variable(yearly_cube,
+                mon_cubes.append(monthly_cube)
+                #mon_cubes.append(monthly_cube.collapsed('time', iris.analysis.MEAN))
+                save_variable(monthly_cube,
                               var,
                               out_dir,
                               glob_attrs,
                               unlimited_dimensions=['time'])
+            yearly_cube = monthly_statistics(concatenate(mon_cubes), operator='mean')
+            #yearly_cube = concatenate(mon_cubes)
+            glob_attrs['mip'] = 'Omon'
+            save_variable(yearly_cube,
+                          var,
+                          out_dir,
+                          glob_attrs,
+                          unlimited_dimensions=['time'])
