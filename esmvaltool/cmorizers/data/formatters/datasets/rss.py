Added filter calculation to python cmorizer and fixed some codacy issues.

esmvaltool/cmorizers/data/formatters/datasets/rss.py
@@ -23,9 +23,10 @@ Modification history
 import glob
 import logging
 import os
-import xarray as xr
 from copy import deepcopy
 
+import numpy as np
+import xarray as xr
 from dask import array as da
 from esmvalcore.cmor.table import CMOR_TABLES
 
@@ -33,15 +34,43 @@ from esmvaltool.cmorizers.data import utilities as utils
 
 logger = logging.getLogger(__name__)
 
+
 def _load_cube(in_files, var):
-    
+    """Load cube."""
     dataset = xr.open_mfdataset(in_files, engine="netcdf4")
     da_tmp = dataset[var['raw']]
     cube = da_tmp.to_iris()
 
-    cube.data = da.ma.masked_equal(cube.core_data(), -500)
+    
+    ano_tmp = dataset[var['anomaly']]
+    anocube = ano_tmp.to_iris()
+    clim_tmp = dataset[var['climatology']]
+    climcube = clim_tmp.to_iris()
 
-    return cube
+    logger.info("anocube KW")
+    logger.info(anocube)
+    logger.info("climcube KW")
+    logger.info(climcube)
+
+
+    cube.data = da.ma.masked_equal(cube.core_data(), -500)
+    anocube.data = da.ma.masked_equal(anocube.core_data(), -500)
+    climcube.data = da.ma.masked_equal(climcube.core_data(), -500)
+
+    outputano = anocube.data
+    
+    for iii in range(0, len(anocube.coord('time').points) - 1, 12):
+        for jjj in range(0, 12):
+            if iii + jjj < len(anocube.coord('time').points):
+                # Necessary, because last year is incomplete
+                outputano[iii + jjj, :, :] = anocube.data[iii + jjj, :, :] + \
+                                             climcube.data[jjj, :, :]
+    
+
+    anocube.data = np.where(np.isfinite(outputano), 1, 1.e+20)
+
+
+    return (cube, anocube)
 
 
 def _fix_coordinates(cube, definition):
@@ -62,7 +91,7 @@ def _fix_coordinates(cube, definition):
                 coord.guess_bounds()
         else:
             raise logger.error("Bounds for coordinate %s "
-                               "cannot be guessed", coord)
+                               "cannot be guessed", coord.standard_name)
 
     return cube
 
@@ -77,7 +106,12 @@ def _extract_variable(in_files, var, cfg, out_dir):
     cmor_table = CMOR_TABLES[attributes['project_id']]
     definition = cmor_table.get_variable(var['mip'], var['short_name'])
 
-    cube = _load_cube(in_files, var)
+
+    logger.info("var KW")
+    logger.info(var)
+
+
+    (cube, anocube) = _load_cube(in_files, var)
 
     # keep the following raw cube attributes
     attrs_to_keep = [
@@ -116,11 +150,14 @@ def _extract_variable(in_files, var, cfg, out_dir):
 
     # Fix data type
     cube.data = cube.core_data().astype('float32')
+    anocube.data = anocube.core_data().astype('float32')
 
     # Roll longitude
-    cube.coord('longitude').points = cube.coord('longitude').points + 180.
-    nlon = len(cube.coord('longitude').points)
-    cube.data = da.roll(cube.core_data(), int(nlon / 2), axis=-1)
+    # cube.coord('longitude').points = cube.coord('longitude').points + 180.
+    # anocube.coord('longitude').points = anocube.coord('longitude').points + 180.
+    # nlon = len(cube.coord('longitude').points)
+    # cube.data = da.roll(cube.core_data(), int(nlon / 2), axis=-1)
+    # anocube.data = da.roll(anocube.core_data(), int(nlon / 2), axis=-1)
 
     # Fix coordinates
     cube = _fix_coordinates(cube, definition)
@@ -128,9 +165,15 @@ def _extract_variable(in_files, var, cfg, out_dir):
     cube.coord('latitude').attributes = None
     cube.coord('longitude').attributes = None
 
+    anocube_new = deepcopy(cube)
+    anocube_new.data = anocube.data
+
     logger.debug("Saving cube\n%s", cube)
     logger.debug("Setting time dimension to UNLIMITED while saving!")
     utils.save_variable(cube, cube.var_name,
+                        out_dir, attributes,
+                        unlimited_dimensions=['time'])
+    utils.save_variable(anocube_new, "filter",
                         out_dir, attributes,
                         unlimited_dimensions=['time'])
     logger.info("Finished CMORizing %s", ', '.join(in_files))
@@ -151,6 +194,6 @@ def cmorization(in_dir, out_dir, cfg, cfg_user, start_date, end_date):
         filepattern = os.path.join(in_dir, var['file'].format())
         in_files = glob.glob(filepattern)
         if not in_files:
-                logger.warning('%s does not exist', filepattern)
-                continue
+            logger.warning('%s does not exist', filepattern)
+            continue
         _extract_variable(in_files, var, cfg, out_dir)

esmvaltool/diag_scripts/ipcc_ar6/santer21jclimfig.py
@@ -2,6 +2,7 @@
 # -*- coding: utf-8 -*-
 
 """Diagnostic script to plot figure 3.12 of IPCC AR 6 chapter 3
+
 (based on Santer et al. (2007) and Santer et al. (2021)).
 
 ##############################################################################
@@ -56,7 +57,8 @@ logger = logging.getLogger(os.path.basename(__file__))
 def _apply_filter(cfg, cube):
     """Apply filter from RSS Anomalies to all data and calculates mean."""
     if "sample_obs" in cfg:
-        filt = iris.load(os.path.join(cfg["auxiliary_data_dir"], cfg["sample_obs"]))[0]
+        filt = iris.load(os.path.join(cfg["auxiliary_data_dir"],
+                                      cfg["sample_obs"]))[0]
 
         for iii, dim_str in enumerate(["time", "latitude", "longitude"]):
             filt = _fit_dim(filt, cube, dim_str, iii)
@@ -77,6 +79,7 @@ def _fit_dim(filt, cube, dim_str, dim_nr):
     """Adjust array for given dimension."""
     dimfil = filt.coord(dim_str)
     dimcu = cube.coord(dim_str)
+    
     if dim_str == "time":
         start = dimcu.units.num2date(dimcu.points[0])
         end = dimcu.units.num2date(dimcu.points[-1])
@@ -89,11 +92,11 @@ def _fit_dim(filt, cube, dim_str, dim_nr):
         edim = dimfil.nearest_neighbour_index(end)
 
     if dim_nr == 0:
-        filt = filt[sdim : edim + 1, :, :]
+        filt = filt[sdim:edim + 1, :, :]
     elif dim_nr == 1:
-        filt = filt[:, sdim : edim + 1, :]
+        filt = filt[:, sdim:edim + 1, :]
     elif dim_nr == 2:
-        filt = filt[:, :, sdim : edim + 1]
+        filt = filt[:, :, sdim:edim + 1]
 
     return filt
 
@@ -117,8 +120,8 @@ def _calculate_anomalies(cube):
     data_in = cube.data
     data_new = np.full(data_in.shape, 0.5)
     for iii in range((startind[0])[0], (endind[0])[-1], 12):
-        data_new[iii : iii + 12] = (
-            ((cube.data[iii : iii + 12] - c_n.data) / c_n.data)
+        data_new[iii:iii + 12] = (
+            ((cube.data[iii:iii + 12] - c_n.data) / c_n.data)
             * 100.0 * 120.0
         )
 
@@ -200,10 +203,12 @@ def _get_valid_datasets(input_data):
         period["end_year"]
     ) == max(period["end_year"]):
         period["common_period"] = (
-            str(min(period["start_year"])) + " - " + str(min(period["end_year"]))
+            str(min(period["start_year"])) + " - "
+            + str(min(period["end_year"]))
         )
 
-    if min(period["slat"]) == max(period["slat"]) and min(period["elat"]) == max(
+    if min(period["slat"]) == max(
+        period["slat"]) and min(period["elat"]) == max(
         period["elat"]
     ):
         shem = _get_hem_letter(min(period["slat"]))
@@ -234,7 +239,8 @@ def _plot_extratrends(cfg, extratrends, trends, period, axx_lim):
     for xtrmdl in cfg["add_model_dist"]:
         alias = list(extratrends[xtrmdl].keys())[0]
         valid_datasets.append(
-            select_metadata(cfg["input_data"].values(), alias=alias)[0]["filename"]
+            select_metadata(cfg["input_data"].values(),
+                            alias=alias)[0]["filename"]
         )
         if alias in trends["cmip6"].keys():
             style = plot.get_dataset_style(xtrmdl, style_file="cmip6")
@@ -266,16 +272,16 @@ def _plot_extratrends(cfg, extratrends, trends, period, axx_lim):
         )
     axx_lim["maxh"] = (1.0 + 0.5 * axx_lim["factor"]) * np.max(hbin)
     caption = _plot_obs(trends, axx, axx_lim)
-    caption = _plot_settings(cfg, axx, fig, "fig2", period, axx_lim) + caption
+    caption = _plot_settings(cfg, axx, period, axx_lim) + caption
+    fig.tight_layout()
+    fig.savefig(get_plot_filename("fig2", cfg), dpi=300)
     plt.close()
 
-    vars = list(extract_variables(cfg).keys())
-    var = " ".join(vars)
-    print_long_name = extract_variables(cfg)[var]["long_name"]
+    var = " ".join(list(extract_variables(cfg).keys()))
     caption = (
         "Probability density function of the decadal trend in "
         + "the "
-        + print_long_name
+        + extract_variables(cfg)[var]["long_name"]
         + caption
     )
 
@@ -309,37 +315,40 @@ def _plot_obs(trends, axx, axx_lim):
     if trends["obs"]:
         obs_str = " Vertical lines show the trend for"
         for iii, obsname in enumerate(trends["obs"].keys()):
+
             obscoli = float(iii)
-            if iii > 4:
-                obscoli = float(iii) - 4.5
-            if iii > 8:
-                obscoli = float(iii) - 8.75
             if iii > 12:
                 obscoli = float(iii) - 12.25
-            plotobscol = (1.0 - 0.25 * obscoli, 0.25 * obscoli, 0.5 + obscoli * 0.1)
+            elif iii > 8:
+                obscoli = float(iii) - 8.75
+            elif iii > 4:
+                obscoli = float(iii) - 4.5
+            else:
+                obscoli = float(iii)
+
             if obsname == "RSS":
                 plotobscol = (
                     221 / 255.0,
                     84 / 255.0,
                     46 / 255.0,
                 )
-                # obsnamep = 'RSS'
-            if obsname == "CRU":
+            elif obsname == "CRU":
                 plotobscol = (0.8, 0.4, 0.1, 1)
-            if obsname == "ERA5":
-                # obscol = [(0.8, 0.4, 0.1, 1), (1, 0, 0.2, 1),
-                #          (0.8, 0.1, 0.4, 1), (1, 0.6, 0, 1)]
-                # plotobscol = (1, 0, 0.2, 1)
+            elif obsname == "ERA5":
                 plotobscol = (
                     128 / 255.0,
                     54 / 255.0,
                     168 / 255.0,
                 )
-                # obsnamep = 'ERA5.1'
-            if obsname == "MERRA2":
+            elif obsname == "MERRA2":
                 plotobscol = (0.8, 0.1, 0.4, 1)
-            if obsname == "NCEP-NCAR-R1":
+            elif obsname == "NCEP-NCAR-R1":
                 plotobscol = (1, 0.6, 0, 1)
+            else:
+                plotobscol = (1.0 - 0.25 * obscoli,
+                              0.25 * obscoli,
+                              0.5 + obscoli * 0.1)
+
             axx.vlines(
                 trends["obs"][obsname],
                 0,
@@ -360,45 +369,69 @@ def _get_ax_limits(cfg, trends):
     """
     axx_lim = {"factor": 0.3}
 
-    if "histmin" in cfg.keys():
+    if "histmin" in cfg.keys() and "histmax" in cfg.keys():
         histmin = cfg["histmin"]
         xmin = histmin
-    else:
-        histmin = 1000
-        for label in ["cmip5", "cmip6", "obs"]:
-            if trends[label]:
-                histmin = np.min(
-                    np.array(
-                        [
-                            histmin,
-                            np.min(np.fromiter(trends[label].values(),
-                            dtype=float)),
-                        ]
-                    )
-                )
-
-    if "histmax" in cfg.keys():
         histmax = cfg["histmax"]
         xmax = histmax
     else:
-        histmax = -1000
-        for label in ["cmip5", "cmip6", "obs"]:
-            if trends[label]:
-                histmax = np.max(
-                    np.array(
-                        [
-                            histmax,
-                            np.max(np.fromiter(trends[label].values(),
-                            dtype=float)),
-                        ]
+        if "histmin" in cfg.keys():
+            histmin = cfg["histmin"]
+            xmin = histmin
+            histmax = -1000
+            for label in ["cmip5", "cmip6", "obs"]:
+                if trends[label]:
+                    histmax = np.max(
+                        np.array(
+                            [
+                                histmax,
+                                np.max(np.fromiter(trends[label].values(),
+                                dtype=float)),
+                            ]
+                        )
                     )
-                )
-
-    if "histmin" not in cfg.keys():
-        xmin = histmin - axx_lim["factor"] * abs(abs(histmax) - abs(histmin))
-
-    if "histmax" not in cfg.keys():
-        xmax = histmax + axx_lim["factor"] * abs(abs(histmax) - abs(histmin))
+            xmax = histmax + axx_lim["factor"] * abs(abs(histmax) - abs(histmin))
+        elif "histmax" in cfg.keys():
+            histmax = cfg["histmax"]
+            xmax = histmax
+            histmin = 1000
+            for label in ["cmip5", "cmip6", "obs"]:
+                if trends[label]:
+                    histmin = np.min(
+                        np.array(
+                            [
+                                histmin,
+                                np.min(np.fromiter(trends[label].values(),
+                                dtype=float)),
+                            ]
+                        )
+                    )
+            xmin = histmin - axx_lim["factor"] * abs(abs(histmax) - abs(histmin))
+        else:
+            histmin = 1000
+            histmax = -1000
+            for label in ["cmip5", "cmip6", "obs"]:
+                if trends[label]:
+                    histmin = np.min(
+                        np.array(
+                            [
+                                histmin,
+                                np.min(np.fromiter(trends[label].values(),
+                                dtype=float)),
+                            ]
+                        )
+                    )                
+                    histmax = np.max(
+                        np.array(
+                            [
+                                histmax,
+                                np.max(np.fromiter(trends[label].values(),
+                                dtype=float)),
+                            ]
+                        )
+                    ) 
+            xmin = histmin - axx_lim["factor"] * abs(abs(histmax) - abs(histmin))
+            xmax = histmax + axx_lim["factor"] * abs(abs(histmax) - abs(histmin))
 
     # Saving values in dictionary axx_lim:
     axx_lim["xmin"] = xmin
@@ -429,7 +462,8 @@ def _plot_trends(cfg, trends, valid_datasets, period, axx_lim):
     # categorical_colors_rgb_0-255/cmip_cat.txt
     # CMIP5
     if trends["cmip5"]:
-        res_ar["artrend_c5"] = np.fromiter(trends["cmip5"].values(), dtype=float)
+        res_ar["artrend_c5"] = np.fromiter(trends["cmip5"].values(),
+                                           dtype=float)
         res_ar["weights_c5"] = np.fromiter(trends["cmip5weights"].values(),
                                            dtype=float)
         res_ar["kde1_c5"] = stats.gaussian_kde(
@@ -456,11 +490,13 @@ def _plot_trends(cfg, trends, valid_datasets, period, axx_lim):
 
     # CMIP6
     if trends["cmip6"]:
-        res_ar["artrend_c6"] = np.fromiter(trends["cmip6"].values(), dtype=float)
+        res_ar["artrend_c6"] = np.fromiter(trends["cmip6"].values(),
+                                           dtype=float)
         res_ar["weights_c6"] = np.fromiter(trends["cmip6weights"].values(),
                                            dtype=float)
         res_ar["kde1_c6"] = stats.gaussian_kde(
-            res_ar["artrend_c6"], weights=res_ar["weights_c6"], bw_method="scott"
+            res_ar["artrend_c6"], weights=res_ar["weights_c6"],
+            bw_method="scott"
         )
         hbinx2, bins1, patches = axx.hist(
             res_ar["artrend_c6"],
@@ -483,11 +519,12 @@ def _plot_trends(cfg, trends, valid_datasets, period, axx_lim):
     # Find the highest bin in the histograms for the y-axis limit
     axx_lim["maxh"] = maxh * (1.0 + 0.5 * axx_lim["factor"])
     caption = _plot_obs(trends, axx, axx_lim)
-    caption = _plot_settings(cfg, axx, fig, "fig1", period, axx_lim) + caption
+    caption = _plot_settings(cfg, axx, period, axx_lim) + caption
+    fig.tight_layout()
+    fig.savefig(get_plot_filename("fig1", cfg), dpi=300)
     plt.close()
 
-    vars = list(extract_variables(cfg).keys())
-    var = " ".join(vars)
+    var = " ".join(list(extract_variables(cfg).keys()))
     print_long_name = extract_variables(cfg)[var]["long_name"]
     caption = (
         "Probability density function of the decadal trend in "
@@ -502,11 +539,8 @@ def _plot_trends(cfg, trends, valid_datasets, period, axx_lim):
 
     diagnostic_file = get_diagnostic_filename("fig1", cfg)
 
-    logger.info("Saving analysis results to %s", diagnostic_file)
-
-    vars = list(extract_variables(cfg).keys())
-    var = " ".join(vars)
-    print_long_name = extract_variables(cfg)[var]["long_name"]
+    logger.info("Saving analysis results to %s", 
+                diagnostic_file)
 
     list_dict = {}
     list_dict["data"] = [res_ar["xhist"]]
@@ -538,7 +572,8 @@ def _plot_trends(cfg, trends, valid_datasets, period, axx_lim):
         list_dict["name"].append(
             {
                 "var_name": var + "_trend_distribution_cmip5",
-                "long_name": print_long_name + " Trends " + "distribution CMIP5",
+                "long_name": print_long_name + " Trends "
+                             + "distribution CMIP5",
                 "units": "1",
             }
         )
@@ -563,7 +598,8 @@ def _plot_trends(cfg, trends, valid_datasets, period, axx_lim):
         list_dict["name"].append(
             {
                 "var_name": var + "_trend_distribution_cmip6",
-                "long_name": print_long_name + " Trends " + "distribution CMIP6",
+                "long_name": print_long_name + " Trends "
+                             + "distribution CMIP6",
                 "units": "1",
             }
         )
@@ -589,7 +625,7 @@ def _plot_trends(cfg, trends, valid_datasets, period, axx_lim):
         provenance_logger.log(diagnostic_file, provenance_record)
 
 
-def _plot_settings(cfg, axx, fig, figname, period, axx_lim):
+def _plot_settings(cfg, axx, period, axx_lim):
     """Define Settings for pdf figures."""
     for spine in ["top", "right"]:
         axx.spines[spine].set_visible(False)
@@ -609,15 +645,12 @@ def _plot_settings(cfg, axx, fig, figname, period, axx_lim):
     if "common_lats" in period.keys():
         add = " between " + period["common_lats"] + add
 
-    vars = list(extract_variables(cfg).keys())
-    var = " ".join(vars)
+    var = " ".join(list(extract_variables(cfg).keys()))
     print_long_name = extract_variables(cfg)[var]["long_name"]
     axx.set_title("Trends in" + print_long_name + add)
     axx.set_xlabel("Trend (%/decade)")
 
     axx.set_xlim(axx_lim["xmin"], axx_lim["xmax"])
-    fig.tight_layout()
-    fig.savefig(get_plot_filename(figname, cfg), dpi=300)
 
     return add
 
@@ -632,9 +665,7 @@ def _set_extratrends_dict(cfg):
 
 def _write_list_dict(cfg, trends, res_ar):
     """Collect data for provenance."""
-
-    vars = list(extract_variables(cfg).keys())
-    var = " ".join(vars)
+    var = " ".join(list(extract_variables(cfg).keys()))
     print_long_name = extract_variables(cfg)[var]["long_name"]
 
     list_dict = {}
@@ -768,8 +799,7 @@ def main(cfg):
 
     for dataset_path in input_data:
         project = dataset_path["project"]
-        cube_load = iris.load(dataset_path["filename"])[0]
-        cube = _apply_filter(cfg, cube_load)
+        cube = _apply_filter(cfg, iris.load(dataset_path["filename"])[0])
         cat.add_month_number(cube, "time", name="month_number")
         cat.add_year(cube, "time", name="year")
         alias = dataset_path["alias"]