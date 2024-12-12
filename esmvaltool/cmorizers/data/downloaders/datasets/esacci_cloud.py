e32ea85a98f7c9ec62332624399c4262bae20b8f
Diego Cammarano <diegocamm@gmail.com>
2024-12-12 18:34:31 +0100

    clean code for codacy

--- esmvaltool/cmorizers/data/downloaders/datasets/esacci_cloud.py
+++ esmvaltool/cmorizers/data/downloaders/datasets/esacci_cloud.py
@@ -1,14 +1,14 @@
 """Script to download daily and monthly ESACCI-CLOUD."""
-import glob
 import logging
-import os
 from datetime import datetime
 from dateutil import relativedelta
 from esmvaltool.cmorizers.data.downloaders.wget import WGetDownloader
 
 logger = logging.getLogger(__name__)
 
-def download_dataset(config, dataset, dataset_info, start_date, end_date, overwrite):
+
+def download_dataset(config, dataset, dataset_info, start_date,
+                     end_date, overwrite):
     """Download dataset.
 
     Parameters
@@ -40,26 +40,35 @@ def download_dataset(config, dataset, dataset_info, start_date, end_date, overwr
     )
 
     # Base paths for L3U (daily data) and L3C (monthly data)
-    base_path_L3U = 'https://public.satproj.klima.dwd.de/data/ESA_Cloud_CCI/CLD_PRODUCTS/v3.0/L3U/'
-    base_path_L3C = 'https://public.satproj.klima.dwd.de/data/ESA_Cloud_CCI/CLD_PRODUCTS/v3.0/L3C/'
+    base_path_l3u = ('https://public.satproj.klima.dwd.de/data/ESA_Cloud_CCI/'
+                     'CLD_PRODUCTS/v3.0/L3U/')
+    base_path_l3c = ('https://public.satproj.klima.dwd.de/data/ESA_Cloud_CCI/'
+                     'CLD_PRODUCTS/v3.0/L3C/')
 
     # File patterns for daily (L3U) and monthly (L3C) data
-    files_L3U = [
+    files_l3u = [
         "*-ESACCI-L3U_CLOUD-CLD_MASKTYPE-AVHRR_*-fv3.0.nc",
         "*-ESACCI-L3U_CLOUD-CLD_PRODUCTS-AVHRR_*-fv3.0.nc"
     ]
-    files_L3C = [
+    files_l3c = [
         "*-ESACCI-L3C_CLOUD-CLD_PRODUCTS-AVHRR_*-fv3.0.nc"
     ]
 
-    wget_options = ['-r', '-nH', '-e', 'robots=off', '--cut-dirs=9', '--no-parent', '--reject="index.html"', '--accept=*.nc']
+    wget_options = [
+        '-r',
+        '-nH',                 # Disable the creation of directory structure
+        '-e', 'robots=off',    # Ignore robots.txt
+        '--cut-dirs=9',
+        '--no-parent',         # Don't ascend to the parent directory
+        '--reject="index.html"',  # Reject any HTML files
+        '--accept=*.nc'      # Accept only .nc files
+    ]
 
     while loop_date <= end_date:
         year = loop_date.year
         month = loop_date.month
         date = f'{year}{month:02}'
 
-        # Determine the satellite logic for both L3U (daily data) and L3C (monthly data)
         if int(date) in range(198201, 198601):
             sat_am = 'AVHRR-PM/AVHRR_NOAA-7/'
             sat_pm = 'AVHRR-PM/AVHRR_NOAA-9/'
@@ -85,29 +94,34 @@ def download_dataset(config, dataset, dataset_info, start_date, end_date, overwr
             sat_am = 'AVHRR-AM/AVHRR_METOPA/'
             sat_pm = 'AVHRR-PM/AVHRR_NOAA-19/'
         else:
-            logger.error("Number of instrument is not defined for date %s", date)
+            logger.error("Number of instrument is not defined for date %s",
+                         date)
 
         # Download daily data from L3U
         for sat in (sat_am, sat_pm):
             logger.info("Downloading daily data (L3U) for sat = %s", sat)
             if sat != '':
-                folder_L3U = base_path_L3U + sat + f'{year}/{month:02}'
-                logger.info("Download folder for daily data (L3U): %s", folder_L3U)
+                folder_l3u = base_path_l3u + sat + f'{year}/{month:02}'
+                logger.info("Download folder for daily data (L3U): %s",
+                            folder_l3u)
                 try:
-                    downloader.download_file(folder_L3U, wget_options)
+                    downloader.download_file(folder_l3u, wget_options)
                 except Exception as e:
-                    logger.error("Failed to download daily data from %s: %s", folder_L3U, str(e))
+                    logger.error("Failed to download daily data from %s: %s",
+                                 folder_l3u, str(e))
 
         # Download monthly data from L3C
         for sat in (sat_am, sat_pm):
             logger.info("Downloading monthly data (L3C) for sat = %s", sat)
             if sat != '':
-                folder_L3C = base_path_L3C + sat + f'{year}/'
-                logger.info("Download folder for monthly data (L3C): %s", folder_L3C)
+                folder_l3c = base_path_l3c + sat + f'{year}/'
+                logger.info("Download folder for monthly data (L3C): %s",
+                            folder_l3c)
                 try:
-                    downloader.download_file(folder_L3C, wget_options)
+                    downloader.download_file(folder_l3c, wget_options)
                 except Exception as e:
-                    logger.error("Failed to download monthly data from %s: %s", folder_L3C, str(e))
+                    logger.error("Failed to download monthly data from %s: %s",
+                                 folder_l3c, str(e))
 
         # Increment the loop_date by one month
         loop_date += relativedelta.relativedelta(months=1)
