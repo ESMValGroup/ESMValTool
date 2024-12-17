7c8eed7da195d23236e1d26d5162326b481701cc
Lisa Bock <lisa.bock@dlr.de>
2024-12-17 17:04:20 +0100

    fix downloader for monthly and daily data

--- esmvaltool/cmorizers/data/downloaders/datasets/esacci_cloud.py
+++ esmvaltool/cmorizers/data/downloaders/datasets/esacci_cloud.py
@@ -29,9 +29,9 @@ def download_dataset(config, dataset, dataset_info, start_date, end_date,
         Overwrite already downloaded files
     """
     if start_date is None:
-        start_date = datetime(2000, 1, 1)
+        start_date = datetime(2003, 1, 1)
     if end_date is None:
-        end_date = datetime(2007, 12, 31)
+        end_date = datetime(2003, 12, 31)
     loop_date = start_date
 
     downloader = WGetDownloader(
@@ -47,22 +47,11 @@ def download_dataset(config, dataset, dataset_info, start_date, end_date,
     base_path_l3c = ('https://public.satproj.klima.dwd.de/data/ESA_Cloud_CCI/'
                      'CLD_PRODUCTS/v3.0/L3C/')
 
-    # File patterns for daily (L3U) and monthly (L3C) data
-    files_l3u = [
-        "*-ESACCI-L3U_CLOUD-CLD_MASKTYPE-AVHRR_*-fv3.0.nc",
-        "*-ESACCI-L3U_CLOUD-CLD_PRODUCTS-AVHRR_*-fv3.0.nc"
-    ]
-    files_l3c = ["*-ESACCI-L3C_CLOUD-CLD_PRODUCTS-AVHRR_*-fv3.0.nc"]
-
     wget_options = [
         '-r',
-        '-nH',  # Disable the creation of directory structure
-        '-e',
-        'robots=off',  # Ignore robots.txt
-        '--cut-dirs=9',
+        '-e robots=off',  # Ignore robots.txt
         '--no-parent',  # Don't ascend to the parent directory
         '--reject="index.html"',  # Reject any HTML files
-        '--accept=*.nc'  # Accept only .nc files
     ]
 
     while loop_date <= end_date:
@@ -70,59 +59,74 @@ def download_dataset(config, dataset, dataset_info, start_date, end_date,
         month = loop_date.month
         date = f'{year}{month:02}'
 
-        if int(date) in range(198201, 198601):
-            sat_am = 'AVHRR-PM/AVHRR_NOAA-7/'
+        if int(date) in range(198201, 198502):
+            sat_am = ''
+            sat_pm = 'AVHRR-PM/AVHRR_NOAA-7/'
+        elif int(date) in range(198502, 198811):
+            sat_am = ''
             sat_pm = 'AVHRR-PM/AVHRR_NOAA-9/'
-        elif int(date) in range(198601, 198901):
-            sat_am = 'AVHRR-PM/AVHRR_NOAA-9/'
+        elif int(date) in range(198811, 199109):
+            sat_am = ''
             sat_pm = 'AVHRR-PM/AVHRR_NOAA-11/'
-        elif int(date) in range(198901, 199501):
-            sat_am = 'AVHRR-PM/AVHRR_NOAA-11/'
+        elif int(date) in range(199109, 199409):
+            sat_am = 'AVHRR-AM/AVHRR_NOAA-12/'
+            sat_pm = 'AVHRR-PM/AVHRR_NOAA-11/'
+        elif int(date) in range(199409, 199502):
+            sat_am = 'AVHRR-AM/AVHRR_NOAA-12/'
+            sat_pm = ''
+        elif int(date) in range(199502, 199901):
+            sat_am = 'AVHRR-AM/AVHRR_NOAA-12/'
             sat_pm = 'AVHRR-PM/AVHRR_NOAA-14/'
-        elif int(date) in range(199501, 200101):
-            sat_am = 'AVHRR-PM/AVHRR_NOAA-14/'
+        elif int(date) in range(199901, 200104):
+            sat_am = 'AVHRR-AM/AVHRR_NOAA-15/'
+            sat_pm = 'AVHRR-PM/AVHRR_NOAA-14/'
+        elif int(date) in range(200104, 200211):
+            sat_am = 'AVHRR-AM/AVHRR_NOAA-15/'
             sat_pm = 'AVHRR-PM/AVHRR_NOAA-16/'
-        elif int(date) in range(200101, 200501):
+        elif int(date) in range(200211, 200509):
             sat_am = 'AVHRR-AM/AVHRR_NOAA-17/'
             sat_pm = 'AVHRR-PM/AVHRR_NOAA-16/'
-        elif int(date) in range(200501, 200701):
+        elif int(date) in range(200509, 200707):
             sat_am = 'AVHRR-AM/AVHRR_NOAA-17/'
             sat_pm = 'AVHRR-PM/AVHRR_NOAA-18/'
-        elif int(date) in range(200701, 200901):
+        elif int(date) in range(200707, 200906):
             sat_am = 'AVHRR-AM/AVHRR_METOPA/'
             sat_pm = 'AVHRR-PM/AVHRR_NOAA-18/'
-        elif int(date) in range(200901, 201701):
+        elif int(date) in range(200906, 201701):
             sat_am = 'AVHRR-AM/AVHRR_METOPA/'
             sat_pm = 'AVHRR-PM/AVHRR_NOAA-19/'
         else:
-            logger.error("Number of instrument is not defined for date %s",
+            logger.error("Data for this date %s is not available",
                          date)
-
-        # Download daily data from L3U
-        for sat in (sat_am, sat_pm):
-            logger.info("Downloading daily data (L3U) for sat = %s", sat)
-            if sat != '':
-                folder_l3u = base_path_l3u + sat + f'{year}/{month:02}'
-                logger.info("Download folder for daily data (L3U): %s",
-                            folder_l3u)
-                try:
-                    downloader.download_file(folder_l3u, wget_options)
-                except Exception as e:
-                    logger.error("Failed to download daily data from %s: %s",
-                                 folder_l3u, str(e))
 
         # Download monthly data from L3C
         for sat in (sat_am, sat_pm):
-            logger.info("Downloading monthly data (L3C) for sat = %s", sat)
             if sat != '':
+                # monthly data
+                logger.info("Downloading monthly data (L3C) for sat = %s", sat)
                 folder_l3c = base_path_l3c + sat + f'{year}/'
+                wget_options_l3c = wget_options.copy()
+                wget_options_l3c.append(f'--accept={date}*.nc')
                 logger.info("Download folder for monthly data (L3C): %s",
                             folder_l3c)
                 try:
-                    downloader.download_file(folder_l3c, wget_options)
+                    downloader.download_file(folder_l3c, wget_options_l3c)
                 except Exception as e:
                     logger.error("Failed to download monthly data from %s: %s",
                                  folder_l3c, str(e))
+
+                # daily data
+                logger.info("Downloading daily data (L3U) for sat = %s", sat)
+                folder_l3u = base_path_l3u + sat + f'{year}/{month:02}'
+                wget_options_l3u = wget_options.copy()
+                wget_options_l3u.append(f'--accept={date}*CLD_MASKTYPE*.nc,{date}*CLD_PRODUCTS*.nc')
+                logger.info("Download folder for daily data (L3U): %s",
+                            folder_l3u)
+                try:
+                    downloader.download_file(folder_l3u, wget_options_l3u)
+                except Exception as e:
+                    logger.error("Failed to download daily data from %s: %s",
+                                 folder_l3u, str(e))
 
         # Increment the loop_date by one month
         loop_date += relativedelta.relativedelta(months=1)
