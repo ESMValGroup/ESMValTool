5333f6ec603c52da7dcc15aeab9cd6fa0e0e5889
Julien Lenhardt <julien.lenhardt@smhi.se>
2024-12-17 17:33:06 +0100

    Additional files for new NOAA GML surface flask measurements datasets

---
+++ esmvaltool/cmorizers/data/downloaders/datasets/noaa_gml_surface_flask_n2o.py
@@ -0,0 +1,38 @@
+"""Script to download NOAA Global Monitoring Lab surface flask data
+for N2O from NOAA's archive."""
+import logging
+
+from esmvaltool.cmorizers.data.downloaders.wget import WGetDownloader
+
+logger = logging.getLogger(__name__)
+
+
+def download_dataset(config, dataset, dataset_info,
+                     start_date, end_date, overwrite):
+    """Download dataset.
+
+    Parameters
+    ----------
+    config : dict
+        ESMValTool's user configuration
+    dataset : str
+        Name of the dataset
+    dataset_info : dict
+         Dataset information from the datasets.yml file
+    start_date : datetime
+        Start of the interval to download
+    end_date : datetime
+        End of the interval to download
+    overwrite : bool
+        Overwrite already downloaded files
+    """
+    downloader = WGetDownloader(
+        config=config,
+        dataset=dataset,
+        dataset_info=dataset_info,
+        overwrite=overwrite,
+    )
+    downloader.download_file(
+        "https://gml.noaa.gov/aftp/data/trace_gases/n2o/flask/surface/n2o_surface-flask_ccgg_text.tar.gz",
+        wget_options=[],
+    )
