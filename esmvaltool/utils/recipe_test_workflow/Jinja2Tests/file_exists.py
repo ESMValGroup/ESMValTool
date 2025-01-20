06e50cd87dbc8ef608d28356a6dc74f499a34a7c
Chris Billows <152496175+chrisbillowsMO@users.noreply.github.com>
2025-01-20 17:50:44 +0000

    #3724: Custom Jinja2Test for missing site file

---
+++ esmvaltool/utils/recipe_test_workflow/Jinja2Tests/file_exists.py
@@ -0,0 +1,6 @@
+from pathlib import Path
+
+def file_exists(file_path):
+    run_directory = Path.cwd()
+    site_recipes_file_path = run_directory / file_path
+    return site_recipes_file_path.is_file()
