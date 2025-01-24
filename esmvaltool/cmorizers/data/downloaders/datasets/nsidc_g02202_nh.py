295027c0f860568f154f6b2332de61da9f07d10a
Romain Beucher <romain.beucher@anu.edu.au>
2025-01-24 11:19:40 +1100

    Update downloader, add North Hemisphere

--- esmvaltool/cmorizers/data/downloaders/datasets/nsidc_g02202_nh.py
+++ esmvaltool/cmorizers/data/downloaders/datasets/nsidc_g02202_nh.py
@@ -1,7 +1,6 @@
-"""Script to download NSIDC-G02202-sh."""
+"""Script to download NSIDC-G02202-nh."""
 import logging
 from datetime import datetime
-
 from dateutil import relativedelta
 
 from esmvaltool.cmorizers.data.downloaders.wget import WGetDownloader
@@ -44,15 +43,15 @@ def download_dataset(config, dataset, dataset_info, start_date, end_date,
 
     # need area file
     area_dat = ('ftp://sidads.colorado.edu/DATASETS/seaice'
-                '/polar-stereo/tools/pss25area_v3.dat')
+                '/polar-stereo/tools/psn25area_v3.dat')
     downloader.download_folder(area_dat, [])
 
     anc_path = ('https://noaadata.apps.nsidc.org/NOAA/G02202_V5/'
-                'ancillary/G02202-ancillary-pss25-v05r00.nc')
+                'ancillary/G02202-ancillary-psn25-v05r00.nc')
     downloader.download_folder(anc_path, [])
 
-    base_path = ('https://noaadata.apps.nsidc.org/NOAA/G02202_V5/south/monthly'
-                 '/sic_pss25_{year}{month:02d}_{other}_v05r00.nc')
+    base_path = ('https://noaadata.apps.nsidc.org/NOAA/G02202_V5/north/monthly'
+                 '/sic_psn25_{year}{month:02d}_{other}_v05r00.nc')
 
     datels = [datetime(1978, 11, 1), datetime(1987, 7, 30),
               datetime(1991, 12, 30), datetime(1995, 9, 30),
