a7ebe03da700e27811546fea34b552d3da920f22
Romain Beucher <romain.beucher@anu.edu.au>
2025-01-24 12:20:09 +1100

    Update NSIDC g02202, Fix #3868

--- esmvaltool/cmorizers/data/formatters/datasets/nsidc_g02202_nh.py
+++ esmvaltool/cmorizers/data/formatters/datasets/nsidc_g02202_nh.py
@@ -1,4 +1,4 @@
-"""ESMValTool CMORizer for Sea Ice Concentration CDR (Southern Hemisphere).
+"""ESMValTool CMORizer for Sea Ice Concentration CDR (Northern Hemisphere).
 
 Tier
    Tier 3: restricted dataset.
@@ -11,12 +11,12 @@ Last access
 
 Download and processing instructions
     Download data from:
-    https://noaadata.apps.nsidc.org/NOAA/G02202_V5/south/monthly
+    https://noaadata.apps.nsidc.org/NOAA/G02202_V5/north/monthly
     lat and lon from:
     https://noaadata.apps.nsidc.org/NOAA/G02202_V5/ancillary/
     area file:
     ftp://sidads.colorado.edu/DATASETS/seaice/polar-stereo/tools/
-    pss25area_v3.dat
+    psn25area_v3.dat
 
     https://nsidc.org/sites/default/files/documents/user-guide/g02202-v005-userguide.pdf
 
@@ -27,14 +27,12 @@ import os
 import re
 
 import numpy as np
+
 import iris
 from cf_units import Unit
 from iris.coords import AuxCoord
 
-from esmvalcore.cmor._fixes.common import OceanFixGrid
-from esmvalcore.cmor.fixes import get_time_bounds
 from esmvaltool.cmorizers.data import utilities as utils
-
 
 logger = logging.getLogger(__name__)
 
@@ -73,7 +71,7 @@ def _create_coord(cubes, var_name, standard_name):
         standard_name=standard_name,
         long_name=cube.long_name,
         var_name=var_name,
-        units='degrees'
+        units='degrees'  # cube.units,
     )
     return coord
 
@@ -87,27 +85,24 @@ def _extract_variable(raw_var, cmor_info, attrs, filepath, out_dir, latlon):
     cube = cubes.concatenate_cube()
     iris.util.promote_aux_coord_to_dim_coord(cube, 'projection_y_coordinate')
     iris.util.promote_aux_coord_to_dim_coord(cube, 'projection_x_coordinate')
+    cube.coord('projection_y_coordinate').rename('y')
+    cube.coord('projection_x_coordinate').rename('x')
 
     cube.add_aux_coord(latlon[0], (1, 2))
     cube.add_aux_coord(latlon[1], (1, 2))
-
     # add coord typesi
     area_type = AuxCoord([1.0], standard_name='area_type', var_name='type',
                          long_name='Sea Ice area type')
     cube.add_aux_coord(area_type)
 
+    # cube.convert_units(cmor_info.units)
     cube.units = '%'
     cube.data[cube.data > 100] = np.nan
     cube = cube * 100
 
+    # utils.fix_coords(cube) #latlon multidimensional
     utils.fix_var_metadata(cube, cmor_info)
     utils.set_global_atts(cube, attrs)
-    # latlon are multidimensional, create bounds
-    siconc = OceanFixGrid(cmor_info)
-    cube = siconc.fix_metadata(cubes=[cube])[0]
-    # time bounds
-    cube.coord('time').bounds = get_time_bounds(cube.coord('time'),
-                                                cmor_info.frequency)
 
     utils.save_variable(cube,
                         var,
@@ -138,9 +133,8 @@ def _create_areacello(cfg, in_dir, sample_cube, glob_attrs, out_dir):
                           long_name=var_info.long_name,
                           var_name=var_info.short_name,
                           units='m2',
-                          # time is index 0, add cell index dim
-                          dim_coords_and_dims=[(sample_cube.coords()[1], 0),
-                                               (sample_cube.coords()[2], 1)])
+                          dim_coords_and_dims=[(sample_cube.coord('y'), 0),
+                                               (sample_cube.coord('x'), 1)])
     cube.add_aux_coord(lat_coord, (0, 1))
     cube.add_aux_coord(sample_cube.coord('longitude'), (0, 1))
     utils.fix_var_metadata(cube, var_info)
@@ -155,10 +149,9 @@ def cmorization(in_dir, out_dir, cfg, cfg_user, start_date, end_date):
     cmor_table = cfg['cmor_table']
 
     # get aux nc file
-    cubesaux = iris.load(os.path.join(in_dir, 'G02202-ancillary-pss25-v05r00.nc'))
+    cubesaux = iris.load(os.path.join(in_dir, 'G02202-ancillary-psn25-v05r00.nc'))
     lat_coord = _create_coord(cubesaux, 'lat', 'latitude')
     lon_coord = _create_coord(cubesaux, 'lon', 'longitude')
-
     year = 1978
     # split by year..
     sample_cube = None
@@ -167,8 +160,7 @@ def cmorization(in_dir, out_dir, cfg, cfg_user, start_date, end_date):
         filepaths = _get_filepaths(in_dir, cfg['filename'], year)
 
         if len(filepaths) > 0:
-            logger.info("Year %d: Found %d files in '%s'",
-                        year, len(filepaths), in_dir)
+            logger.info("Found %d files in '%s'", len(filepaths), in_dir)
 
             for (var, var_info) in cfg['variables'].items():
                 logger.info("CMORizing variable '%s'", var)
@@ -181,8 +173,10 @@ def cmorization(in_dir, out_dir, cfg, cfg_user, start_date, end_date):
                                                           lon_coord])
 
         else:
-            logger.info("No files found year: %d basename: %s",
-                        year, cfg['filename'])
+            logger.info("No files found ")
+            logger.info("year: %d basename: %s", year, cfg['filename'])
 
-    if sample_cube is not None:
-        _create_areacello(cfg, in_dir, sample_cube, glob_attrs, out_dir)
+        year += 1
+
+        if sample_cube is not None:
+            _create_areacello(cfg, in_dir, sample_cube, glob_attrs, out_dir)
