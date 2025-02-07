5f0d8e6c9745a90e14d2819cf29be1fd129d738c
jlenh <julien.lenhardt@gmail.com>
2025-02-07 12:36:29 +0100

    Update downloaders

--- esmvaltool/cmorizers/data/downloaders/datasets/noaa_gml_surface_flask_ch4.py
+++ esmvaltool/cmorizers/data/downloaders/datasets/noaa_gml_surface_flask_ch4.py
@@ -7,8 +7,7 @@ from esmvaltool.cmorizers.data.downloaders.wget import WGetDownloader
 logger = logging.getLogger(__name__)
 
 
-def download_dataset(config, dataset, dataset_info,
-                     start_date, end_date, overwrite):
+def download_dataset(config, dataset, dataset_info, overwrite):
     """Download dataset.
 
     Parameters
@@ -19,10 +18,6 @@ def download_dataset(config, dataset, dataset_info,
         Name of the dataset
     dataset_info : dict
          Dataset information from the datasets.yml file
-    start_date : datetime
-        Start of the interval to download
-    end_date : datetime
-        End of the interval to download
     overwrite : bool
         Overwrite already downloaded files
     """
