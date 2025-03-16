2041d6a6dc30e8d9e4f6abcc5c9c4476a39c927c
Javier Vegas-Regidor <javi_vegas@msn.com>
2025-03-16 14:18:29 +0100

    Update branch

--- esmvaltool/diag_scripts/spy4cast/diagnostic_nn4cast.py
+++ esmvaltool/diag_scripts/spy4cast/diagnostic_nn4cast.py
@@ -1,15 +1,13 @@
 """Python example diagnostic."""
 import logging
-import os
 from pathlib import Path
-from pprint import pformat
+import nn4cast.predefined_classes
+
 
 
 from esmvaltool.diag_scripts.shared import (
     group_metadata,
     run_diagnostic,
-    select_metadata,
-    sorted_metadata,
 )
 from spy4cast import spy4cast, Month, Region
 from spy4cast.dataset import Dataset
@@ -45,10 +43,17 @@ def main(cfg):
     # Get a description of the preprocessed data that we will use as input.
     input_data = cfg['input_data'].values()
 
+    hyperparameters = cfg['hyperparameters'].copy()
+    hyperparameters['outputs_path'] = cfg['work_dir']
+
     variables = group_metadata(input_data, 'variable_group', sort='dataset')
+   
     single_predictand = False
     if len(variables['predictand']) == 1:
         # Por si cogemos uno solo, sino nos vamos al alias que tiene toda la info. El Slice lo haces en la recipe, en la receta metes el predictando y el predictor
+        predictand = variables['predictand'][0]
+        hyperparameters['path_y'] = predictand['filename']
+        hyperparameters['name_y'] = predictand['short_name']
         predictand = variables['predictand'][0]
         z = Dataset(name=predictand['filename'], folder=cfg['work_dir']).open(var=predictand['short_name'])
         z_ppcessed = spy4cast.Preprocess(z, period=7, order=4)
@@ -69,30 +74,19 @@ def main(cfg):
                 continue
 
         predictor = variables['predictor'][0]
-        y = Dataset(name=predictor['filename'], folder=cfg['work_dir']).open(var=predictor['short_name'])
-        # y._region = Region(-30, 30, 80, -120, Month.JUN, Month.AUG, 1970, 2000)
-        y_ppcessed = spy4cast.Preprocess(y, period=7, order=4)
-        y_ppcessed.save('save_preprocessed_y_', folder=cfg['work_dir'])
+        hyperparameters['path_x'] = predictor['filename']
+        hyperparameters['name_x'] = predictor['short_name']
 
         if not single_predictand:
             predictand = variables['predictand'][0]
-            z = Dataset(name=predictand['filename'], folder=cfg['work_dir']).open(var=predictand['short_name'])
-            z_ppcessed = spy4cast.Preprocess(z, period=7, order=4)
-            z_ppcessed.save(f'preprocessed_z_{predictand["alias"]}', folder=cfg['work_dir'])
+            hyperparameters['path_y'] = predictand['filename']
+            hyperparameters['name_y'] = predictand['short_name']
 
-        nm = cfg['modes']
-        alpha = cfg['alpha']
-        mca = spy4cast.MCA(y_ppcessed, z_ppcessed, nm, alpha, sig='monte-carlo', montecarlo_iterations=10)
-        logger.info("Saving MCA...")
-        mca.save(f'mca_{dataset}', folder=cfg['work_dir']) #Aquí es donde lo guarda
-        logger.info("Plotting...")
-        mca.plot(save_fig=True, cmap='viridis', folder=cfg['plot_dir'], name=f'mca-{dataset}.png',signs=[True,True,True],width_ratios=[0.5,1,1])
-        logger.info('Finished!!!')
-
-        if cfg['cross_validation']: # Meter cross si podemos
-            cross = spy4cast.Crossvalidation(y_ppcessed, z_ppcessed, nm, alpha)
-            cross.save(f'cross_{dataset}', folder=cfg['work_dir'])
-            cross.plot(save_fig=True, folder=cfg['plot_dir'], name=f'crossvalidation-{dataset}.png')
+        dictionary_preprocess= nn4cast.predefined_classes.Preprocess(dictionary_hyperparams= hyperparameters)
+        outputs_hold_out = nn4cast.predefined_classes.Model_build_and_test(dictionary_hyperparams= hyperparameters, dictionary_preprocess= dictionary_preprocess, cross_validation= False, n_cv_folds=0)
+        outputs_cross_validation= nn4cast.predefined_classes.Model_build_and_test(dictionary_hyperparams= hyperparameters, dictionary_preprocess= dictionary_preprocess, cross_validation= True, n_cv_folds=4, plot_differences=False, importances=True, region_importances=[[50,65],[-25,-10]])
+        nn4cast.predefined_classes.Results_plotter(hyperparameters, dictionary_preprocess, rang_x=2.5, rang_y=10, predictions=outputs_cross_validation['predictions'], observations=outputs_cross_validation['observations'], years_to_plot=[1962,1963], plot_with_contours=False, importances=outputs_cross_validation['importances'], region_importances=outputs_cross_validation['region_attributed'])
+        eof_analysis = nn4cast.predefined_classes.PC_analysis(hyperparameters, outputs_cross_validation['predictions'], outputs_cross_validation['observations'], n_modes=4, n_clusters=3, cmap='RdBu_r')
 
 
 if __name__ == '__main__':
