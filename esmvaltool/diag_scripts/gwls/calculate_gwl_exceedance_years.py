b903fa688add39df7b026686d6e097b1d71b5c63
rswamina <ranjinis@gmail.com>
2025-03-25 12:33:42 +0000

    precommit fixes

--- esmvaltool/diag_scripts/gwls/calculate_gwl_exceedance_years.py
+++ esmvaltool/diag_scripts/gwls/calculate_gwl_exceedance_years.py
@@ -1,4 +1,5 @@
 """Calculate Global Warming Level exceedance years."""
+
 import logging
 import os
 from pathlib import Path
@@ -23,94 +24,102 @@ def log_provenance(provenance, filename, cfg):
 
 
 def calculate_moving_average_centered(a, window_size):
-    '''Calculate rolling means centred at a point'''
+    """Calculate rolling means centred at a point"""
 
-    if (window_size < 2):
+    if window_size < 2:
         raise ValueError("Window size should be greater than 2")
 
-    return pd.Series(a).rolling(window=window_size,
-                                center=True).mean().to_numpy()
+    return (
+        pd.Series(a).rolling(window=window_size, center=True).mean().to_numpy()
+    )
 
 
 def calculate_gwl_exceedance_years(input_data_sorted, gwls, window_size):
-    '''Calculate GWL exceedance years for models and ensemble members'''
+    """Calculate GWL exceedance years for models and ensemble members"""
 
-    col_names = ['Project', 'Exp', 'Model', 'Ens', 'GWL', 'Exceedance_Year']
+    col_names = ["Project", "Exp", "Model", "Ens", "GWL", "Exceedance_Year"]
     gwl_df = pd.DataFrame(columns=col_names)
 
     for data in input_data_sorted:
-        project = data['project']
-        exp = data['exp']
-        dataset = data['dataset']
-        ensemble = data['ensemble']
+        project = data["project"]
+        exp = data["exp"]
+        dataset = data["dataset"]
+        ensemble = data["ensemble"]
 
-        logger.info('Processing %s %s %s %s %s ', project, exp, dataset,
-                    ensemble)
-        anomaly_ts_cube = iris.load_cube(data['filename'])
-        years = anomaly_ts_cube.coord('year').points
+        logger.info(
+            "Processing %s %s %s %s %s ", project, exp, dataset, ensemble
+        )
+        anomaly_ts_cube = iris.load_cube(data["filename"])
+        years = anomaly_ts_cube.coord("year").points
         start_year = years[0]
         end_year = years[len(years) - 1]
-        logger.info('Start and end years and averaging over  %s %s %s ',
-                    str(start_year), str(end_year), str(window_size))
+        logger.info(
+            "Start and end years and averaging over  %s %s %s ",
+            str(start_year),
+            str(end_year),
+            str(window_size),
+        )
         smoothed_ts = calculate_moving_average_centered(
-            anomaly_ts_cube.data, window_size)
+            anomaly_ts_cube.data, window_size
+        )
 
         for gwl in gwls:
             logger.info("GWL temperature %s", float(gwl))
-            logger.info('Size of array %s ', str(smoothed_ts.shape))
+            logger.info("Size of array %s ", str(smoothed_ts.shape))
             # only write exceedance_year if gwl is exceeded in the time series
-            if (np.nanmax(smoothed_ts) > float(gwl)):
+            if np.nanmax(smoothed_ts) > float(gwl):
                 exceedance_year = start_year + np.argmax(
-                    smoothed_ts > float(gwl))
-                new_record_df = pd.DataFrame([{
-                    'Project':
-                    data['project'],
-                    'Exp':
-                    data['exp'],
-                    'Model':
-                    data['dataset'],
-                    'Ens':
-                    data['ensemble'],
-                    'GWL':
-                    gwl,
-                    'Exceedance_Year':
-                    exceedance_year
-                }])
-                logger.info('Exceedance year %s ', exceedance_year)
+                    smoothed_ts > float(gwl)
+                )
+                new_record_df = pd.DataFrame(
+                    [
+                        {
+                            "Project": data["project"],
+                            "Exp": data["exp"],
+                            "Model": data["dataset"],
+                            "Ens": data["ensemble"],
+                            "GWL": gwl,
+                            "Exceedance_Year": exceedance_year,
+                        }
+                    ]
+                )
+                logger.info("Exceedance year %s ", exceedance_year)
                 gwl_df = pd.concat([gwl_df, new_record_df], ignore_index=True)
 
     return gwl_df
 
 
 def main(cfg):
-    input_data = cfg['input_data'].values()
+    input_data = cfg["input_data"].values()
 
     # initialize pandas data frame to store GWL exceedance information
-    window_size = cfg['window_size']
-    gwls = cfg['gwls']
+    window_size = cfg["window_size"]
+    gwls = cfg["gwls"]
 
     # group preproocessed input by project
     input_data_sorted = sorted_metadata(
-        input_data, ['project', 'exp', 'dataset', 'ensemble'])
+        input_data, ["project", "exp", "dataset", "ensemble"]
+    )
 
     # calculate GWL exceedance years and return in a dataframe
-    gwl_df = calculate_gwl_exceedance_years(input_data_sorted, gwls,
-                                            window_size)
-    gwl_file = os.path.join(cfg['work_dir'], 'GWL_exceedance_years.csv')
-    gwl_df.to_csv(gwl_file, sep=',', index=False)
+    gwl_df = calculate_gwl_exceedance_years(
+        input_data_sorted, gwls, window_size
+    )
+    gwl_file = os.path.join(cfg["work_dir"], "GWL_exceedance_years.csv")
+    gwl_df.to_csv(gwl_file, sep=",", index=False)
 
     provenance_dict = {
-        'caption': 'CSV file of Global Warming level Exceedances',
-        'ancestors': [d["filename"] for d in input_data],
-        'authors': ['swaminathan_ranjini'],
-        'references': ['swaminathan22jclim'],
-        'projects': ['ukesm'],
-        'domains': ['global'],
+        "caption": "CSV file of Global Warming level Exceedances",
+        "ancestors": [d["filename"] for d in input_data],
+        "authors": ["swaminathan_ranjini"],
+        "references": ["swaminathan22jclim"],
+        "projects": ["ukesm"],
+        "domains": ["global"],
     }
     # write provenance record
     log_provenance(provenance_dict, gwl_file, cfg)
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     with run_diagnostic() as config:
         main(config)
--- esmvaltool/diag_scripts/gwls/plot_gwl_exceedance_mm_stats.py
+++ esmvaltool/diag_scripts/gwls/plot_gwl_exceedance_mm_stats.py
@@ -1,4 +1,5 @@
 """Plot multimodel variable stats at Global Warming Level exceedance years."""
+
 import logging
 import os
 from pathlib import Path
@@ -29,58 +30,65 @@ def log_provenance(provenance, filename, cfg):
         provenance_logger.log(filename, provenance)
 
 
-def calculate_gwl_mm_stats(project_data, gwl_subset_df, window_size, mean_file,
-                           stdev_file):
+def calculate_gwl_mm_stats(
+    project_data, gwl_subset_df, window_size, mean_file, stdev_file
+):
     """Calculate multimodel stats around GWL exceedance years."""
     cubes = iris.cube.CubeList()
 
-    datasets = gwl_subset_df['Model'].unique()
+    datasets = gwl_subset_df["Model"].unique()
     for dataset in datasets:
         # get every exp for which the data is available for
         all_exps_in_dataset = select_metadata(project_data, dataset=dataset)
         for el in all_exps_in_dataset:
-            exp = el['exp']
-            path = el['filename']
+            exp = el["exp"]
+            path = el["filename"]
             cube = iris.load_cube(path)
             ih.prepare_cube_for_merging(cube, path)
             # extract window period
             year_of_exceedance = gwl_subset_df[
-                (gwl_subset_df['Model'] == dataset)
-                & (gwl_subset_df['Exp'] == exp)]['Exceedance_Year'].values[0]
-            if (np.isnan(year_of_exceedance)):
+                (gwl_subset_df["Model"] == dataset)
+                & (gwl_subset_df["Exp"] == exp)
+            ]["Exceedance_Year"].values[0]
+            if np.isnan(year_of_exceedance):
                 continue
             start_year = int(year_of_exceedance - ((window_size - 1) / 2))
             end_year = int(year_of_exceedance + ((window_size - 1) / 2))
-            logger.info('Model: %s, Exp : %s start year: %s, endyear: %s',
-                        dataset, exp, str(start_year), str(end_year))
+            logger.info(
+                "Model: %s, Exp : %s start year: %s, endyear: %s",
+                dataset,
+                exp,
+                str(start_year),
+                str(end_year),
+            )
             # Start year Jan 1st (1, 1) and end_year Dec 31st (12, 31)
             cube = extract_time(cube, start_year, 1, 1, end_year, 12, 31)
-            cube = cube.collapsed('time', iris.analysis.MEAN)
+            cube = cube.collapsed("time", iris.analysis.MEAN)
             cubes.append(cube)
 
-
-# find index of time coord so this can be unified across data sets
-# before merging as the time data points are different across models/scenarios
+    # find index of time coord so this can be unified across data sets
+    # before merging as the time data points are different across models/scenarios
     index = 0
     for coord in cubes[0].aux_coords:
-        if (coord.standard_name == 'time'):
+        if coord.standard_name == "time":
             break
         else:
             index = index + 1
     for c in cubes[1:]:
-        c.remove_coord('time')
+        c.remove_coord("time")
         c.add_aux_coord(cubes[0].aux_coords[index])
 
-    if (len(cubes) == 0):
+    if len(cubes) == 0:
         logger.info("No model instances exceed this GWL.")
-    elif (len(cubes) == 1):
+    elif len(cubes) == 1:
         iris.save(cubes[0], mean_file)
         logger.info("No standard deviation calculated for a single instance.")
     else:
         mm_cube = cubes.merge_cube()
-        mm_mean_cube = mm_cube.collapsed(['cube_label'], iris.analysis.MEAN)
-        mm_stdev_cube = mm_cube.collapsed(['cube_label'],
-                                          iris.analysis.STD_DEV)
+        mm_mean_cube = mm_cube.collapsed(["cube_label"], iris.analysis.MEAN)
+        mm_stdev_cube = mm_cube.collapsed(
+            ["cube_label"], iris.analysis.STD_DEV
+        )
         iris.save(mm_mean_cube, mean_file)
         iris.save(mm_stdev_cube, stdev_file)
 
@@ -88,47 +96,52 @@ def calculate_gwl_mm_stats(project_data, gwl_subset_df, window_size, mean_file,
 
 
 def main(cfg):
-
-    input_data = cfg['input_data'].values()
-    window_size = cfg['window_size']
-    gwls = cfg['gwls']
-    pattern = cfg['pattern']
-    cmap_mean_str = cfg['quickplot']['cmap_mean']
-    cmap_stdev_str = cfg['quickplot']['cmap_stdev']
+    input_data = cfg["input_data"].values()
+    window_size = cfg["window_size"]
+    gwls = cfg["gwls"]
+    pattern = cfg["pattern"]
+    cmap_mean_str = cfg["quickplot"]["cmap_mean"]
+    cmap_stdev_str = cfg["quickplot"]["cmap_stdev"]
 
     gwl_filename = io.get_ancestor_file(cfg, pattern)
-    logger.info('GWL exceedance years file is %s', gwl_filename)
+    logger.info("GWL exceedance years file is %s", gwl_filename)
     gwl_df = pd.read_csv(gwl_filename)
-    sep = '_'
+    sep = "_"
 
     projects = []
     for data in input_data:
         # select by by project
-        if (data['project'] not in projects):
-            projects.append(data['project'])
+        if data["project"] not in projects:
+            projects.append(data["project"])
 
-    logger.info('List of Projects: %s', projects)
+    logger.info("List of Projects: %s", projects)
 
     for project in projects:
         # get all data sets for project across experiments
         project_data = select_metadata(input_data, project=project)
         for gwl in gwls:
-            gwl_subset_df = gwl_df[gwl_df['GWL'] == gwl]
-            if (gwl_subset_df.shape[0] > 0):
+            gwl_subset_df = gwl_df[gwl_df["GWL"] == gwl]
+            if gwl_subset_df.shape[0] > 0:
                 logger.info(
                     "Calculating means and standard deviations for GWL: %s",
-                    str(gwl))
-                filename = sep.join([project, 'mm_mean', str(gwl)]) + '.nc'
-                mean_file = os.path.join(cfg['work_dir'], filename)
-                filename = sep.join([project, 'mm_stdev', str(gwl)]) + '.nc'
-                stdev_file = os.path.join(cfg['work_dir'], filename)
+                    str(gwl),
+                )
+                filename = sep.join([project, "mm_mean", str(gwl)]) + ".nc"
+                mean_file = os.path.join(cfg["work_dir"], filename)
+                filename = sep.join([project, "mm_stdev", str(gwl)]) + ".nc"
+                stdev_file = os.path.join(cfg["work_dir"], filename)
 
-                calculate_gwl_mm_stats(project_data, gwl_subset_df,
-                                       window_size, mean_file, stdev_file)
-                filename = sep.join([project, 'mm_mean', str(gwl)]) + '.png'
-                mean_plot_file = os.path.join(cfg['plot_dir'], filename)
-                filename = sep.join([project, 'mm_stdev', str(gwl)]) + '.png'
-                stdev_plot_file = os.path.join(cfg['plot_dir'], filename)
+                calculate_gwl_mm_stats(
+                    project_data,
+                    gwl_subset_df,
+                    window_size,
+                    mean_file,
+                    stdev_file,
+                )
+                filename = sep.join([project, "mm_mean", str(gwl)]) + ".png"
+                mean_plot_file = os.path.join(cfg["plot_dir"], filename)
+                filename = sep.join([project, "mm_stdev", str(gwl)]) + ".png"
+                stdev_plot_file = os.path.join(cfg["plot_dir"], filename)
 
                 # plot mean and stdev if they exist
                 if os.path.isfile(mean_file):
@@ -136,25 +149,30 @@ def main(cfg):
                     cube = iris.load_cube(mean_file)
                     plt.axes(projection=ccrs.Robinson())
 
-                    if (cfg['quickplot']['title_var'] == 'Temperature'):
-                        cube.convert_units('Celsius')
+                    if cfg["quickplot"]["title_var"] == "Temperature":
+                        cube.convert_units("Celsius")
                         qplt.contourf(
                             cube,
                             norm=colors.CenteredNorm(),
                             cmap=cmap_mean_str,
                         )
 
-                    if (cfg['quickplot']['title_var'] == 'Precipitation'):
+                    if cfg["quickplot"]["title_var"] == "Precipitation":
                         cube = cube * 86400
-                        cube.units = 'mm/day'
+                        cube.units = "mm/day"
                         qplt.contourf(
                             cube,
                             cmap=cmap_mean_str,
                         )
 
                     plt.gca().coastlines()
-                    title_str = 'Multimodel mean of ' + cfg['quickplot'][
-                        'title_var'] + ' at ' + str(gwl) + r'$^\circ$ C'
+                    title_str = (
+                        "Multimodel mean of "
+                        + cfg["quickplot"]["title_var"]
+                        + " at "
+                        + str(gwl)
+                        + r"$^\circ$ C"
+                    )
                     plt.title(title_str)
 
                     plt.tight_layout()
@@ -167,24 +185,24 @@ def main(cfg):
                         d["filename"] for d in project_data
                     ]
                     provenance_dict = {
-                        'caption': title_str,
-                        'ancestors': ancestors_file,
-                        'authors': ['swaminathan_ranjini'],
-                        'references': ['swaminathan22jclim'],
-                        'projects': ['ukesm'],
-                        'domains': ['global'],
+                        "caption": title_str,
+                        "ancestors": ancestors_file,
+                        "authors": ["swaminathan_ranjini"],
+                        "references": ["swaminathan22jclim"],
+                        "projects": ["ukesm"],
+                        "domains": ["global"],
                     }
                     # write provenance record
                     log_provenance(provenance_dict, mean_file, cfg)
 
                     provenance_dict = {
-                        'caption': title_str,
-                        'ancestors': ancestors_file,
-                        'authors': ['swaminathan_ranjini'],
-                        'references': ['swaminathan22jclim'],
-                        'projects': ['ukesm'],
-                        'domains': ['global'],
-                        'plot_type': ['map']
+                        "caption": title_str,
+                        "ancestors": ancestors_file,
+                        "authors": ["swaminathan_ranjini"],
+                        "references": ["swaminathan22jclim"],
+                        "projects": ["ukesm"],
+                        "domains": ["global"],
+                        "plot_type": ["map"],
                     }
                     # write provenance record
                     log_provenance(provenance_dict, mean_plot_file, cfg)
@@ -194,45 +212,49 @@ def main(cfg):
                     plt.axes(projection=ccrs.Robinson())
                     qplt.contourf(cube, cmap=cmap_stdev_str)
                     plt.gca().coastlines()
-                    if (cfg['quickplot']['title_var'] == 'Temperature'):
-                        cube.units = 'Celsius'
+                    if cfg["quickplot"]["title_var"] == "Temperature":
+                        cube.units = "Celsius"
 
-                    if (cfg['quickplot']['title_var'] == 'Precipitation'):
+                    if cfg["quickplot"]["title_var"] == "Precipitation":
                         cube = cube * 86400
-                        cube.units = 'mm/day'
+                        cube.units = "mm/day"
 
-                    title_str = 'Multimodel standard deviation of ' + cfg[
-                        'quickplot']['title_var'] + ' at ' + str(
-                            gwl) + r'$^\circ$ C'
+                    title_str = (
+                        "Multimodel standard deviation of "
+                        + cfg["quickplot"]["title_var"]
+                        + " at "
+                        + str(gwl)
+                        + r"$^\circ$ C"
+                    )
                     plt.title(title_str)
                     plt.tight_layout()
                     plt.savefig(stdev_plot_file)
                     plt.close()
 
                     provenance_dict = {
-                        'caption': title_str,
-                        'ancestors': ancestors_file,
-                        'authors': ['swaminathan_ranjini'],
-                        'references': ['swaminathan22jclim'],
-                        'projects': ['ukesm'],
-                        'domains': ['global'],
+                        "caption": title_str,
+                        "ancestors": ancestors_file,
+                        "authors": ["swaminathan_ranjini"],
+                        "references": ["swaminathan22jclim"],
+                        "projects": ["ukesm"],
+                        "domains": ["global"],
                     }
                     # write provenance record
                     log_provenance(provenance_dict, stdev_file, cfg)
 
                     provenance_dict = {
-                        'caption': title_str,
-                        'ancestors': ancestors_file,
-                        'authors': ['swaminathan_ranjini'],
-                        'references': ['swaminathan22jclim'],
-                        'projects': ['ukesm'],
-                        'domains': ['global'],
-                        'plot_type': ['map']
+                        "caption": title_str,
+                        "ancestors": ancestors_file,
+                        "authors": ["swaminathan_ranjini"],
+                        "references": ["swaminathan22jclim"],
+                        "projects": ["ukesm"],
+                        "domains": ["global"],
+                        "plot_type": ["map"],
                     }
                     # write provenance record
                     log_provenance(provenance_dict, stdev_plot_file, cfg)
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     with run_diagnostic() as config:
         main(config)
