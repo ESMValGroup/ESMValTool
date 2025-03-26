8b881c4a3c629ec9838718d5f400f6d0502a6ace
rswamina <ranjinis@gmail.com>
2025-03-26 14:53:12 +0000

    fixing codacy errors

--- esmvaltool/diag_scripts/gwls/plot_gwl_exceedance_mm_stats.py
+++ esmvaltool/diag_scripts/gwls/plot_gwl_exceedance_mm_stats.py
@@ -102,11 +102,11 @@ def calculate_mm_stats(cubes, mean_file, stdev_file):
         iris.save(mm_mean_cube, mean_file)
         iris.save(mm_stdev_cube, stdev_file)
 
-    return
+    return None
 
 
 def main(cfg):
-    """Execute function to calculate and plot GWL statistics."""
+    """Execute GWL statistics."""
     input_data = cfg["input_data"].values()
     window_size = cfg["window_size"]
     gwls = cfg["gwls"]
