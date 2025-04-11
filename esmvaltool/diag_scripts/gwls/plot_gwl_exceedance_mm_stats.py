87ef735fbd14adb5dc82be6f47ad55085398aa64
rswamina <42945525+rswamina@users.noreply.github.com>
2025-04-11 16:04:25 +0100

    Update esmvaltool/diag_scripts/gwls/plot_gwl_exceedance_mm_stats.py
    Change made based on feedback received in technical review.
    
    Co-authored-by: Valeriu Predoi <valeriu.predoi@gmail.com>

--- esmvaltool/diag_scripts/gwls/plot_gwl_exceedance_mm_stats.py
+++ esmvaltool/diag_scripts/gwls/plot_gwl_exceedance_mm_stats.py
@@ -90,7 +90,7 @@ def calculate_mm_stats(cubes, mean_file, stdev_file):
         c.remove_coord("time")
         c.add_aux_coord(cubes[0].aux_coords[index])
 
-    if len(cubes) == 0:
+    if not cubes:
         logger.info("No model instances exceed this GWL.")
     elif len(cubes) == 1:
         iris.save(cubes[0], mean_file)
