be813e8f9ecf2b5d95c7ac3de9f15d73f0fc4019
Manuel Schlund <manuel.schlund@dlr.de>
2025-04-14 17:22:49 +0200

    Added test cases for map plots

--- tests/sample_data/diag_scripts/data/generate_sample_data.py
+++ tests/sample_data/diag_scripts/data/generate_sample_data.py
@@ -1,4 +1,6 @@
 #!/usr/bin/env python
+from __future__ import annotations
+
 from pathlib import Path
 
 import iris
@@ -19,16 +21,16 @@ VAR_METADATA: dict[str, dict[str, str]] = {
 }
 
 
-def data(arr: list) -> np.ndarray:
-    """Get masked float32 data."""
-    return np.ma.masked_invalid(np.array(arr, dtype=np.float32))
+def data(arr: list | np.ndarray) -> np.ndarray:
+    """Get float32 data (all values in [-0.5, 0.5] will be masked)."""
+    return np.ma.masked_inside(np.array(arr, dtype=np.float32), -0.5, 0.5)
 
 
 def save_cube(
     filename: str,
     var_name: str,
     coords: list[DimCoord],
-    cube_data: list,
+    cube_data: list | np.ndarray,
 ) -> None:
     """Save cube."""
     dim_coord_spec = [(c, i) for (i, c) in enumerate(coords)]
@@ -42,6 +44,34 @@ def save_cube(
 def main() -> None:
     """Generate sample data."""
     # Define coordinates
+    hour_coord = DimCoord(
+        [1, 2, 4],
+        var_name="hour",
+        long_name="hour",
+        units="1",
+    )
+    lat_coord = DimCoord(
+        [10.0, 30.0, 50.0],
+        bounds=[[0.0, 20.0], [20.0, 40.0], [40.0, 60.0]],
+        var_name="lat",
+        standard_name="latitude",
+        long_name="Latitude",
+        units="degrees_north",
+    )
+    lon_coord = DimCoord(
+        [20.0, 60.0, 100.0],
+        bounds=[[0.0, 40.0], [40.0, 80.0], [80.0, 120.0]],
+        var_name="lon",
+        standard_name="longitude",
+        long_name="Longitude",
+        units="degrees_east",
+    )
+    month_number_coord = DimCoord(
+        [1, 2, 4],
+        var_name="month_number",
+        long_name="month_number",
+        units="1",
+    )
     time_coord = DimCoord(
         [15.0, 46.0, 75.0],
         bounds=[[0.0, 31.0], [31.0, 60.0], [60.0, 91.0]],
@@ -50,20 +80,11 @@ def main() -> None:
         long_name="time",
         units="days since 2000-01-01",
     )
-    month_number_coord = DimCoord(
-        [1, 2, 4],
-        var_name="month_number",
-        long_name="month_number",
-        units="1",
-    )
-    hour_coord = DimCoord(
-        [1, 2, 4], var_name="hour", long_name="hour", units="1"
-    )
 
-    # 1D time
-    save_cube("tas_amon_1d_time_0.nc", "tas", [time_coord], [2.0, 3.0, np.nan])
-    save_cube("tas_amon_1d_time_1.nc", "tas", [time_coord], [1.0, -1.0, 2.0])
-    save_cube("tas_amon_1d_time_2.nc", "tas", [time_coord], [-1.0, -3.0, 1.5])
+    # 1D hour
+    save_cube("tas_amon_1d_hour_0.nc", "tas", [hour_coord], [2.0, 3.0, 0.0])
+    save_cube("tas_amon_1d_hour_1.nc", "tas", [hour_coord], [1.0, -1.0, 2.0])
+    save_cube("tas_amon_1d_hour_2.nc", "tas", [hour_coord], [-1.0, -3.0, 1.5])
 
     # 1D month_number
     save_cube(
@@ -85,10 +106,30 @@ def main() -> None:
         [-1.0, -3.0, 1.5],
     )
 
-    # 1D hour
-    save_cube("tas_amon_1d_hour_0.nc", "tas", [hour_coord], [2.0, 3.0, np.nan])
-    save_cube("tas_amon_1d_hour_1.nc", "tas", [hour_coord], [1.0, -1.0, 2.0])
-    save_cube("tas_amon_1d_hour_2.nc", "tas", [hour_coord], [-1.0, -3.0, 1.5])
+    # 1D time
+    save_cube("tas_amon_1d_time_0.nc", "tas", [time_coord], [2.0, 3.0, 0.0])
+    save_cube("tas_amon_1d_time_1.nc", "tas", [time_coord], [1.0, -1.0, 2.0])
+    save_cube("tas_amon_1d_time_2.nc", "tas", [time_coord], [-1.0, -3.0, 1.5])
+
+    # 2D lat/lon
+    save_cube(
+        "tas_amon_2d_lat_lon_0.nc",
+        "tas",
+        [lat_coord, lon_coord],
+        np.arange(9).reshape(3, 3),
+    )
+    save_cube(
+        "tas_amon_2d_lat_lon_1.nc",
+        "tas",
+        [lat_coord, lon_coord],
+        np.arange(9).reshape(3, 3) * 2 + 2,
+    )
+    save_cube(
+        "tas_amon_2d_lat_lon_2.nc",
+        "tas",
+        [lat_coord, lon_coord],
+        np.arange(9).reshape(3, 3) * -1,
+    )
 
 
 if __name__ == "__main__":
