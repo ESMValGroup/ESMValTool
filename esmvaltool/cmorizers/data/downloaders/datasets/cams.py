5550caae0ec3d2b4efe588fa819acc5de5d848df
Bettina Gier <gier@uni-bremen.de>
2025-05-20 12:17:36 +0200

    fix downloader

--- esmvaltool/cmorizers/data/downloaders/datasets/cams.py
+++ esmvaltool/cmorizers/data/downloaders/datasets/cams.py
@@ -5,9 +5,10 @@ from esmvaltool.cmorizers.data.utilities import unpack_files_in_folder
 
 
 def download_dataset(
-    config, dataset, dataset_info, overwrite, start_year=1979, end_year=2023
+    config, dataset, dataset_info, start_date, end_date, overwrite
 ):
     """Download dataset.
+
     Parameters
     ----------
     config : dict
@@ -16,13 +17,17 @@ def download_dataset(
         Name of the dataset
     dataset_info : dict
          Dataset information from the datasets.yml file
+    start_date : datetime
+        Start of the interval to download
+    end_date : datetime
+        End of the interval to download
     overwrite : bool
         Overwrite already downloaded files
-    start_year : datetime
-        Start of the interval to download
-    end_year : datetime
-        End of the interval to download
     """
+    if start_date is None:
+        start_date = 1979
+    if end_date is None:
+        end_date = 2023
 
     downloader = CDSDownloader(
         product_name="cams-global-greenhouse-gas-inversion",
@@ -40,7 +45,7 @@ def download_dataset(
         cds_url="https://ads.atmosphere.copernicus.eu/api",
     )
 
-    for year in range(start_year, end_year + 1, 1):
+    for year in range(start_date, end_date + 1, 1):
         downloader.download_year(year)
 
     unpack_files_in_folder(downloader.local_folder)
