db1e4c62514daf92a3b98248d2cf9c8a1116545d
Valeriu Predoi <valeriu.predoi@gmail.com>
2025-05-27 15:40:44 +0100

    Support Python 3.13 (#3805)

--- setup.py
+++ setup.py
@@ -220,6 +220,7 @@ setup(
         "Programming Language :: Python :: 3",
         "Programming Language :: Python :: 3.11",
         "Programming Language :: Python :: 3.12",
+        "Programming Language :: Python :: 3.13",
         "Topic :: Scientific/Engineering",
         "Topic :: Scientific/Engineering :: Atmospheric Science",
         "Topic :: Scientific/Engineering :: GIS",
