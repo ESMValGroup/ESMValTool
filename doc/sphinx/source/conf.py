d5a93b04c1a91c993b3e0104b01b1a7aa494cfa1
Lukas Ruhe <lukas@uni-bremen.de>
2025-05-28 11:14:03 +0200

    improve look and feel, extra css file

--- doc/sphinx/source/conf.py
+++ doc/sphinx/source/conf.py
@@ -197,7 +197,12 @@ html_logo = 'figures/ESMValTool-logo-2.png'
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
-html_static_path = ["figures/ESMValTool-logo-2-dark.png"]
+html_static_path = [
+    "figures/ESMValTool-logo-2-dark.png",
+    "_static",
+]
+
+html_css_files = ["custom.css"]
 
 # Add any extra paths that contain custom files (such as robots.txt or
 # .htaccess) here, relative to this directory. These files are copied
