c5bdcdae1497d52e2967dcb946e4c435903b11fb
Chris Billows <christopher.billows@metoffice.gov.uk>
2025-06-10 12:21:08 +0200

    #4036: Fix dkrz bash script

--- esmvaltool/utils/recipe_test_workflow/app/generate_report/bin/set_container_versions.sh
+++ esmvaltool/utils/recipe_test_workflow/app/generate_report/bin/set_container_versions.sh
@@ -1,21 +1,14 @@
 #!/bin/bash
 # Send the output from 'set -x' to 'stdout' rather than 'stderr'.
 BASH_XTRACEFD=1
-set -eux
+set -eu
 
-echo Running new bash script
+YESTERDAYS_CONTAINER_PATH="${ROSE_DATACPT10M}/container/esmvaltool.sif"
 
-export CAPTURE_OUTPUT=True
-export QUIET_MODE=True
-export YESTERDAYS_CONTAINER_PATH="${ROSE_DATACPT10M}/container/esmvaltool.sif"
-
-ESMVAL_VERSIONS_CURRENT=$(env-file esmvaltool version)
+ESMVAL_VERSIONS_CURRENT=$(QUIET_MODE=True CAPTURE_OUTPUT=True env-file esmvaltool version)
 export ESMVAL_VERSIONS_CURRENT
 
 if [[ -f ${YESTERDAYS_CONTAINER_PATH} ]]; then
-    ESMVAL_VERSIONS_PREVIOUS=$(CONTAINER_PATH=${YESTERDAYS_CONTAINER_PATH} env-file esmvaltool version)
+    ESMVAL_VERSIONS_PREVIOUS=$(QUIET_MODE=True CAPTURE_OUTPUT=True CONTAINER_PATH=${YESTERDAYS_CONTAINER_PATH} env-file esmvaltool version)
     export ESMVAL_VERSIONS_PREVIOUS
 fi
-
-echo ESMVal Current Versions:  "$ESMVAL_VERSIONS_CURRENT"
-echo ESMVal Previous Versions: "$ESMVAL_VERSIONS_PREVIOUS"
