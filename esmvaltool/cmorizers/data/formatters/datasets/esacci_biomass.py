19ca537582d366c082c9defd7ccfd8046ecc98ce
Axel Lauer <axel.lauer@dlr.de>
2025-07-22 15:53:21 +0200

    snapshot 2025-07-22

--- esmvaltool/cmorizers/data/formatters/datasets/esacci_biomass.py
+++ esmvaltool/cmorizers/data/formatters/datasets/esacci_biomass.py
@@ -1,4 +1,4 @@
-"""ESMValTool CMORizer for ESACCI-BIOMASS agb data.
+"""ESMValTool CMORizer for ESACCI-BIOMASS above-ground biomass (agb) data.
 
 Tier
     Tier 2: other freely-available dataset.
@@ -18,15 +18,21 @@ Download and processing instructions
 import glob
 import logging
 import os
-from datetime import datetime
+import datetime
 
 import iris
 import numpy as np
 
+from copy import deepcopy
+from dask import array as da
+
+from esmvalcore.cmor.table import CMOR_TABLES
+from esmvalcore.preprocessor import extract_time 
+
 from esmvaltool.cmorizers.data.utilities import (
-    add_typebare,
     fix_coords,
     fix_var_metadata,
+    flip_dim_coord,
     save_variable,
     set_global_atts,
 )
@@ -35,177 +41,130 @@ from esmvaltool.cmorizers.data.utilities import (
 logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger(__name__)
 
-# Enable the new split-attributes handling mode
-iris.FUTURE.save_split_attrs = True
+
+def _extract_variable(in_files, var, cfg, out_dir):
+    logger.info("CMORizing variable '%s' from input files '%s'",
+                var['short_name'], ', '.join(in_files))
+    attributes = deepcopy(cfg['attributes'])
+    attributes['mip'] = var['mip']
+    attributes['raw'] = var['raw']
+    attributes['frequency'] = var['frequency']
+    cmor_table = CMOR_TABLES[attributes['project_id']]
+    definition = cmor_table.get_variable(var['mip'], var['short_name'])
+
+    # load all input files (1 year) into 1 cube
+    cube_list = iris.load(in_files, var['raw'])
+
+    time_unit = 'days since 1850-01-01 00:00:00'
+    time_calendar = 'standard'
+
+    drop_attrs = ["valid_max", "valid_min"]
+
+    for cube in cube_list:
+        # set correct names
+        cube.var_name = definition.short_name
+        cube.standard_name = definition.standard_name
+        cube.long_name = definition.long_name
+
+        for attr in drop_attrs:
+            if attr in cube.attributes:
+                cube.attributes.pop(attr)
+
+        cube.coord("time").points = (
+            cube.coord("time").core_points().astype("float64")
+        )
+
+        # fix units
+        cube.convert_units(definition.units)
+
+        # set global attributes
+        set_global_atts(cube, attributes)
+
+        # roll longitude (-180...180 --> 0...360)
+        cube.coord('longitude').points = cube.coord('longitude').points + 180.
+        nlon = len(cube.coord('longitude').points)
+        cube.data = da.roll(cube.core_data(), int(nlon / 2), axis=2)
+
+        # remove rouding errors introduced by da.roll
+        loncoord = cube.coord("longitude")
+        latcoord = cube.coord("latitude")
+        loncoord.points = np.round(loncoord.core_points(), 3)
+        latcoord.points = np.round(latcoord.core_points(), 3)
+
+        # flip latitudes
+        flip_dim_coord(cube, "latitude")
+
+        # fix coordinates
+        cube = _fix_coordinates(cube, definition)
+        cube.coord("latitude").attributes = None
+        cube.coord("longitude").attributes = None
+
+        # save each year to a separate output file
+        timecoord = cube.coord("time")
+        for time in timecoord.units.num2date(timecoord.points):
+            # extract current year
+            outcube = extract_time(cube, time.year, 1, 1, time.year, 12, 31)
+            # adjust time bounds to (year-01-01 00:00, year+1-01-01 00:00)
+            out_timecoord = outcube.coord("time")
+            start_date = datetime.datetime(time.year, 1, 1)
+            end_date = datetime.datetime(time.year + 1, 1, 1)
+            out_timecoord.bounds = np.array([
+                out_timecoord.units.date2num(start_date),
+                out_timecoord.units.date2num(end_date)])
+            # write output to file
+            logger.debug("Saving cube\n%s", outcube)
+            logger.debug("Setting time dimension to UNLIMITED while saving!")
+            version = attributes['version']
+            save_variable(outcube, var['short_name'],
+                          out_dir, attributes,
+                          unlimited_dimensions=['time'])
+
+    logger.info("Finished CMORizing %s", ', '.join(in_files))
 
 
-def average_block(data, block_size):
-    """Average the data within each block of size block_size.
+def _fix_coordinates(cube, definition):
+    """Fix coordinates."""
+    axis2def = {"T": "time", "X": "longitude", "Y": "latitude"}
+    axes = ["T", "X", "Y"]
 
-    Parameters
-    ----------
-    data : numpy.ndarray
-        The input data array to be block averaged.
-    block_size : int
-        The size of the block used for averaging. The data is averaged
-        within non-overlapping blocks of this size along the spatial dimensions
-        (latitude and longitude).
+    for axis in axes:
+        coord_def = definition.coordinates.get(axis2def[axis])
+        if coord_def:
+            coord = cube.coord(axis=axis)
+            if axis == "T":
+                coord.convert_units("days since 1850-1-1 00:00:00.0")
+                coord.points = coord.core_points().astype("float64")
+                if coord.bounds is not None:
+                    coord.bounds = None
+                    
+            if len(coord.points) > 1:
+                if coord.bounds is not None:
+                    coord.bounds = None
+                coord.guess_bounds()
+            coord.standard_name = coord_def.standard_name
+            coord.var_name = coord_def.out_name
+            coord.long_name = coord_def.long_name
 
-    Returns
-    -------
-    numpy.ndarray
-        The block-averaged data array.
-    """
-    shape = data.shape
-    reshaped_data = data.reshape(
-        shape[0],
-        shape[1] // block_size,
-        block_size,
-        shape[2] // block_size,
-        block_size,
-    )
-    averaged_data = reshaped_data.mean(axis=(2, 4))
-    return averaged_data
-
-
-def regrid_iris(cube):
-    """Regrid the cubes using block averaging.
-
-    Parameters
-    ----------
-    cube : iris.cube.Cube
-        The input data cube to be regridded.
-
-    Returns
-    -------
-    iris.cube.Cube
-        The regridded data cube.
-
-    Notes
-    -----
-    The block size is set to 100, which means the data will be averaged within
-    non-overlapping blocks of 100x100 grid cells along the spatial dimensions.
-    """
-    logger.info("Regridding using block averaging")
-
-    block_size = 100  # Number of grid cells to average in each block
-
-    combined_data = average_block(cube.data, block_size)
-
-    # Define target latitude and longitude ranges
-    target_lats = np.linspace(
-        90 - 0.5 * (180 / combined_data.shape[1]),
-        -90 + 0.5 * (180 / combined_data.shape[1]),
-        combined_data.shape[1],
-    )
-    target_lons = np.linspace(
-        -180 + 0.5 * (360 / combined_data.shape[2]),
-        180 - 0.5 * (360 / combined_data.shape[2]),
-        combined_data.shape[2],
-    )
-
-    combined_cube = iris.cube.Cube(
-        combined_data,
-        dim_coords_and_dims=[
-            (cube.coord("time"), 0),
-            (
-                iris.coords.DimCoord(
-                    target_lats, standard_name="latitude", units="degrees"
-                ),
-                1,
-            ),
-            (
-                iris.coords.DimCoord(
-                    target_lons, standard_name="longitude", units="degrees"
-                ),
-                2,
-            ),
-        ],
-    )
-
-    combined_cube.coord("latitude").guess_bounds()
-    combined_cube.coord("longitude").guess_bounds()
-
-    return combined_cube
-
-
-def regrid_fix(cube, glob_attrs, var_name, var_info):
-    """Regrid cube and fixes.
-
-    Regrids the cube, fixes metadata, coordinates and glob_attrs.
-
-    Parameters
-    ----------
-    cube: iris.cube.Cube
-          Data cube to be regridded.
-
-    vals: dict
-          Variable long_name.
-
-    glob_attrs: dict
-          Dictionary holding cube metadata attributes.
-
-    var_name: str
-          Variable name.
-
-    var_info: dict
-          Dictionary holding cube metadata attributes.
-
-    Returns
-    -------
-    cube: iris.cube.Cube
-        data cube regridded and with fixed coordinates.
-    """
-    logger.info("Regridding cube for %s", var_name)
-    regridded_cube = regrid_iris(cube)
-    fix_var_metadata(regridded_cube, var_info)
-    regridded_cube = fix_coords(regridded_cube)
-    set_global_atts(regridded_cube, glob_attrs)
-
-    return regridded_cube
+    return cube
 
 
 def cmorization(in_dir, out_dir, cfg, cfg_user, start_date, end_date):
     """Cmorize data."""
     glob_attrs = cfg["attributes"]
 
-    for year in range(start_date.year, end_date.year + 1):
-        inpfile_pattern = os.path.join(in_dir, cfg["filename"])
-        year_inpfile_pattern = inpfile_pattern.format(year=year)
-        inpfiles = sorted(glob.glob(year_inpfile_pattern))
-        for inpfile in inpfiles:
-            cubes = iris.load(inpfile)
-            for var_name, vals in cfg["variables"].items():
-                var_info = cfg["cmor_table"].get_variable(
-                    vals["mip"], var_name
-                )
-                glob_attrs["mip"] = vals["mip"]
-                glob_attrs["frequency"] = vals["frequency"]
-                if var_name == "shrubFrac":
-                    cube = (
-                        cubes.extract_cube("SHRUBS-BD")
-                        + cubes.extract_cube("SHRUBS-BE")
-                        + cubes.extract_cube("SHRUBS-ND")
-                        + cubes.extract_cube("SHRUBS-NE")
-                    )
-                elif var_name == "treeFrac":
-                    cube = (
-                        cubes.extract_cube("TREES-BD")
-                        + cubes.extract_cube("TREES-BE")
-                        + cubes.extract_cube("TREES-ND")
-                        + cubes.extract_cube("TREES-NE")
-                    )
-                else:
-                    cube = cubes.extract_cube(vals["long_name"])
-                regridded_cube = regrid_fix(
-                    cube, glob_attrs, var_name, var_info
-                )
-                if var_name == "baresoilFrac":
-                    add_typebare(regridded_cube)
-                save_variable(
-                    regridded_cube,
-                    var_name,
-                    out_dir,
-                    glob_attrs,
-                    unlimited_dimensions=["time"],
-                )
+    logger.info("Starting cmorization for tier%s OBS files: %s",
+                glob_attrs['tier'], glob_attrs['dataset_id'])
+    logger.info("Input data from: %s", in_dir)
+    logger.info("Output will be written to: %s", out_dir)
+    logger.info('CMORizing ESACCI-BIOMASS version %s', glob_attrs['version'])
+
+    for short_name, var in cfg['variables'].items():
+        filepattern = os.path.join(in_dir, var['filename'])
+        in_files = glob.glob(filepattern)
+        if 'short_name' not in var:
+            var['short_name'] = short_name
+        if not in_files:
+            logger.info('%d: no data not found for '
+                        'variable %s', short_name)
+        else:
+            _extract_variable(in_files, var, cfg, out_dir)
