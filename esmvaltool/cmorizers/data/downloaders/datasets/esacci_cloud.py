0b4062dca7b25dc99c4cbae7ca7215ad6f5eff33
Axel Lauer <axel.lauer@dlr.de>
2025-07-28 15:09:55 +0200

    reverted downloader esacci_cloud.py

--- esmvaltool/cmorizers/data/downloaders/datasets/esacci_cloud.py
+++ esmvaltool/cmorizers/data/downloaders/datasets/esacci_cloud.py
@@ -12,7 +12,7 @@ logger = logging.getLogger(__name__)
 
 
 def download_dataset(
-    config, dataset, dataset_info, start_date, end_date, overwrite,
+    config, dataset, dataset_info, start_date, end_date, overwrite
 ):
     """Download dataset.
 
