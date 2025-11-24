76c116a7f60d748267a879df6a1a4703375cdb41
Naomi Parsons <naomi.parsons@metoffice.gov.uk>
2025-11-24 13:30:50 +0000

    pre-commit changes

--- esmvaltool/diag_scripts/seaice/seaice_sensitivity.py
+++ esmvaltool/diag_scripts/seaice/seaice_sensitivity.py
@@ -41,10 +41,10 @@ def create_category_dict(cfg):
     logger.debug("Creating blank dictionary.")
     # Create blank dictionary of correct structure
     category_dict = {
-        'models': {},
-        'tasa_obs': {},
-        'siconc_obs': {},
-        'cross-dataset-obs': {},
+        "models": {},
+        "tasa_obs": {},
+        "siconc_obs": {},
+        "cross-dataset-obs": {},
     }
 
     # Read the data from the config object
@@ -52,46 +52,51 @@ def create_category_dict(cfg):
 
     # Iterate over the datasets to add to the dictionary
     for input in input_data:
-
         # Check for tasa observations
-        if input['variable_group'] == 'tasa_obs':
-            category_dict['tasa_obs'][input['dataset']] = {}
+        if input["variable_group"] == "tasa_obs":
+            category_dict["tasa_obs"][input["dataset"]] = {}
 
         # Check for siconc observations
-        elif input['variable_group'] == 'siconc_obs':
-            category_dict['siconc_obs'][input['dataset']] = {}
+        elif input["variable_group"] == "siconc_obs":
+            category_dict["siconc_obs"][input["dataset"]] = {}
 
         # Everything else should be a model
         else:
             # Add the model dataset if not already present (appears twice, for tas and siconc)
-            if input['dataset'] not in category_dict['models']:
-                category_dict['models'][input['dataset']] = {}
+            if input["dataset"] not in category_dict["models"]:
+                category_dict["models"][input["dataset"]] = {}
 
             # Add labelling info
-            if 'label_dataset' in input and input['label_dataset']:
-                category_dict['models'][input['dataset']]['label'] = 'to_label'
-                logger.info("Dataset %s will be labelled", input['dataset'])
+            if "label_dataset" in input and input["label_dataset"]:
+                category_dict["models"][input["dataset"]]["label"] = "to_label"
+                logger.info("Dataset %s will be labelled", input["dataset"])
             else:
-                category_dict['models'][input['dataset']]['label'] = 'unlabelled'
-                logger.info("Not labelling dataset %s in plots", input['dataset'])
+                category_dict["models"][input["dataset"]]["label"] = (
+                    "unlabelled"
+                )
+                logger.info(
+                    "Not labelling dataset %s in plots", input["dataset"]
+                )
 
     return category_dict
 
 
 def fetch_cube(dataset, variable, cfg):
     """Fetch a data cube for a dataset and variable using info from the config."""
-    logger.debug("Fetching cube for dataset: %s, variable: %s", dataset, variable)
+    logger.debug(
+        "Fetching cube for dataset: %s, variable: %s", dataset, variable
+    )
 
     # Read the data from the config object
     input_data = cfg["input_data"].values()
 
     # Find the correct filepath for the dataset
     for input in input_data:
-        if input['dataset'] == dataset:
+        if input["dataset"] == dataset:
             # Also check the variable matches as models have two entries
             # Only matching the first three letters to avoid issues with sic vs siconc
-            if input['short_name'][:3] == variable[:3]:
-                filepath = input['filename']
+            if input["short_name"][:3] == variable[:3]:
+                filepath = input["filename"]
                 break
 
     # Load the cube using iris
@@ -104,7 +109,7 @@ def calculate_annual_trend(cube):
     logger.debug("Calculating annual trend for cube %s.", cube.name())
 
     # Depending on preprocessor, coord may be 'year' or 'time'
-    if 'year' in cube.coords():
+    if "year" in cube.coords():
         no_years = [i for i in range(len(cube.coord("years").points))]
     else:
         no_years = [i for i in range(len(cube.coord("time").points))]
@@ -121,8 +126,8 @@ def calculate_direct_stats(dataset, cfg):
     logger.debug("Calculating direct sensitivity for dataset %s.", dataset)
 
     # Fetch the required cubes
-    siconc_cube = fetch_cube(dataset, 'siconc', cfg)
-    tas_cube = fetch_cube(dataset, 'tas', cfg)
+    siconc_cube = fetch_cube(dataset, "siconc", cfg)
+    tas_cube = fetch_cube(dataset, "tas", cfg)
 
     # Calculate direct regression (tas as independent)
     direct_sensitivity = linregress(tas_cube.data, siconc_cube.data)
@@ -133,11 +138,15 @@ def calculate_direct_stats(dataset, cfg):
 
 def calculate_cross_dataset_stats(tasa_dataset, siconc_dataset, cfg):
     """Calculate the sensitivity of siconc to tasa across (obs) datasets."""
-    logger.debug("Calculating cross sensitivity for datasets %s, %s.", tasa_dataset, siconc_dataset)
+    logger.debug(
+        "Calculating cross sensitivity for datasets %s, %s.",
+        tasa_dataset,
+        siconc_dataset,
+    )
 
     # Fetch the required cubes
-    siconc_cube = fetch_cube(siconc_dataset, 'siconc', cfg)
-    tasa_cube = fetch_cube(tasa_dataset, 'tasa', cfg)
+    siconc_cube = fetch_cube(siconc_dataset, "siconc", cfg)
+    tasa_cube = fetch_cube(tasa_dataset, "tasa", cfg)
 
     # Calculate direct regression (tasa as independent)
     direct_sensitivity = linregress(tasa_cube.data, siconc_cube.data)
@@ -151,51 +160,63 @@ def write_values_to_dict(data_dict, cfg):
     logger.debug("Writing values to dictionary.")
 
     # Calculate all the values for the models
-    for model_dataset in data_dict['models'].keys():
+    for model_dataset in data_dict["models"].keys():
         # Calculate annual tas trend
-        tas_cube = fetch_cube(model_dataset, 'tas', cfg)
+        tas_cube = fetch_cube(model_dataset, "tas", cfg)
         ann_tas_trend = calculate_annual_trend(tas_cube)
-        data_dict['models'][model_dataset]['annual_tas_trend'] = ann_tas_trend
+        data_dict["models"][model_dataset]["annual_tas_trend"] = ann_tas_trend
 
         # Calculate annual siconc trend
-        siconc_cube = fetch_cube(model_dataset, 'siconc', cfg)
+        siconc_cube = fetch_cube(model_dataset, "siconc", cfg)
         ann_siconc_trend = calculate_annual_trend(siconc_cube)
-        data_dict['models'][model_dataset]['annual_siconc_trend'] = ann_siconc_trend
+        data_dict["models"][model_dataset]["annual_siconc_trend"] = (
+            ann_siconc_trend
+        )
 
         # Calculate direct sensitivity of siconc to tas
         direct_sensitivity = calculate_direct_stats(model_dataset, cfg)
-        data_dict['models'][model_dataset]['direct_sensitivity'] = direct_sensitivity.slope
-        data_dict['models'][model_dataset]['direct_r_value'] = direct_sensitivity.rvalue
-        data_dict['models'][model_dataset]['direct_p_value'] = direct_sensitivity.pvalue
+        data_dict["models"][model_dataset]["direct_sensitivity"] = (
+            direct_sensitivity.slope
+        )
+        data_dict["models"][model_dataset]["direct_r_value"] = (
+            direct_sensitivity.rvalue
+        )
+        data_dict["models"][model_dataset]["direct_p_value"] = (
+            direct_sensitivity.pvalue
+        )
 
     # Calculate just the tasa trend for the tasa observations
-    for obs_dataset in data_dict['tasa_obs'].keys():
+    for obs_dataset in data_dict["tasa_obs"].keys():
         # Calculate annual tas trend
-        tasa_cube = fetch_cube(obs_dataset, 'tasa', cfg)
+        tasa_cube = fetch_cube(obs_dataset, "tasa", cfg)
         ann_tasa_trend = calculate_annual_trend(tasa_cube)
         # Still labelling in final dictionary as tas for consistency with models
-        data_dict['tasa_obs'][obs_dataset]['annual_tas_trend'] = ann_tasa_trend
+        data_dict["tasa_obs"][obs_dataset]["annual_tas_trend"] = ann_tasa_trend
 
     # Calculate just the siconc trend for the siconc observations
-    for obs_dataset in data_dict['siconc_obs'].keys():
+    for obs_dataset in data_dict["siconc_obs"].keys():
         # Calculate annual siconc trend
-        siconc_cube = fetch_cube(obs_dataset, 'siconc', cfg)
+        siconc_cube = fetch_cube(obs_dataset, "siconc", cfg)
         ann_siconc_trend = calculate_annual_trend(siconc_cube)
-        data_dict['siconc_obs'][obs_dataset]['annual_siconc_trend'] = ann_siconc_trend
+        data_dict["siconc_obs"][obs_dataset]["annual_siconc_trend"] = (
+            ann_siconc_trend
+        )
 
     # Calculate cross-dataset statistics between tasa and siconc observations
-    for tasa_dataset in data_dict['tasa_obs'].keys():
-        for siconc_dataset in data_dict['siconc_obs'].keys():
+    for tasa_dataset in data_dict["tasa_obs"].keys():
+        for siconc_dataset in data_dict["siconc_obs"].keys():
             # Calculate cross-dataset sensitivity of siconc to tasa
-            cross_sensitivity = calculate_cross_dataset_stats(tasa_dataset, siconc_dataset, cfg)
+            cross_sensitivity = calculate_cross_dataset_stats(
+                tasa_dataset, siconc_dataset, cfg
+            )
             # Determine structure of dictionary to store values
             key_name = f"{siconc_dataset}_to_{tasa_dataset}"
-            data_dict['cross-dataset-obs'][key_name] = {}
-            inner_dict = data_dict['cross-dataset-obs'][key_name]
+            data_dict["cross-dataset-obs"][key_name] = {}
+            inner_dict = data_dict["cross-dataset-obs"][key_name]
             # Store the values
-            inner_dict['direct_sensitivity'] = cross_sensitivity.slope
-            inner_dict['direct_r_value'] = cross_sensitivity.rvalue
-            inner_dict['direct_p_value'] = cross_sensitivity.pvalue
+            inner_dict["direct_sensitivity"] = cross_sensitivity.slope
+            inner_dict["direct_r_value"] = cross_sensitivity.rvalue
+            inner_dict["direct_p_value"] = cross_sensitivity.pvalue
 
     return data_dict
 
@@ -375,7 +396,7 @@ def roach_style_plot_from_dict(data_dictionary, titles_dictionary, cfg):
     ax.set_ylabel(r"Trend in SIA ($million \ km^2 \ decade^{-1}$)")
 
     # Iterate over the models sub-dictionary
-    for dataset, inner_dict in data_dictionary['models'].items():
+    for dataset, inner_dict in data_dictionary["models"].items():
         # Determine the position of the point
         x = 10 * inner_dict["annual_tas_trend"]  # for equivalence to decades
         y = (
@@ -401,17 +422,19 @@ def roach_style_plot_from_dict(data_dictionary, titles_dictionary, cfg):
             plt.annotate(dataset, xy=(x, y), xytext=(x + 0.01, y - 0.005))
 
     # Add the observations
-    siconc_dict = data_dictionary['siconc_obs']
-    tasa_dict = data_dictionary['tasa_obs']
+    siconc_dict = data_dictionary["siconc_obs"]
+    tasa_dict = data_dictionary["tasa_obs"]
 
     # Iterate over the pairs in cross-dataset-obs
-    for pair, inner_dict in data_dictionary['cross-dataset-obs'].items():
+    for pair, inner_dict in data_dictionary["cross-dataset-obs"].items():
         # Retrieve the names of the datasets from the pair string
         siconc_ds, tasa_ds = pair.split("_to_")
 
         # Determine the position of the point, from other dictionaries
-        x = 10 * tasa_dict[tasa_ds]['annual_tas_trend']  # This was labelled as tas, not tasa
-        y = 10 * siconc_dict[siconc_ds]['annual_siconc_trend']
+        x = (
+            10 * tasa_dict[tasa_ds]["annual_tas_trend"]
+        )  # This was labelled as tas, not tasa
+        y = 10 * siconc_dict[siconc_ds]["annual_siconc_trend"]
 
         # Determine the colour of the point from the inner dictionary
         r_corr = inner_dict["direct_r_value"]
@@ -424,7 +447,16 @@ def roach_style_plot_from_dict(data_dictionary, titles_dictionary, cfg):
 
         # Plot the point
         plt.scatter(
-            x, y, marker="s", s=150, c=[r_corr], hatch=h, cmap=cmap, norm=norm, zorder=0, edgecolors="black",
+            x,
+            y,
+            marker="s",
+            s=150,
+            c=[r_corr],
+            hatch=h,
+            cmap=cmap,
+            norm=norm,
+            zorder=0,
+            edgecolors="black",
         )
 
     # Add a colour bar
@@ -446,19 +478,23 @@ def main(cfg):
     data_dict = create_category_dict(cfg)
 
     # Calculate and write values to the dictionary
-    logger.info(f"Calculating and writing values to dictionary.")
+    logger.info("Calculating and writing values to dictionary.")
     data_dict = write_values_to_dict(data_dict, cfg)
 
     # Write the model and obs dictionaries to csv files
-    logger.info(f"Writing dictionaries to csv files.")
-    write_dictionary_to_csv(data_dict['models'], 'models_values', cfg)
-    write_dictionary_to_csv(data_dict['tasa_obs'], 'tasa_obs_values', cfg)
-    write_dictionary_to_csv(data_dict['siconc_obs'], 'siconc_obs_values', cfg)
+    logger.info("Writing dictionaries to csv files.")
+    write_dictionary_to_csv(data_dict["models"], "models_values", cfg)
+    write_dictionary_to_csv(data_dict["tasa_obs"], "tasa_obs_values", cfg)
+    write_dictionary_to_csv(data_dict["siconc_obs"], "siconc_obs_values", cfg)
 
     # Write the cross-dataset obs dictionary to csv files (separately for each pair)
-    for pair in data_dict['cross-dataset-obs'].keys():
-        data_dict['cross-dataset-obs'][pair] = data_dict['cross-dataset-obs'][pair]
-        write_dictionary_to_csv(data_dict['cross-dataset-obs'][pair], f"{pair}", cfg)
+    for pair in data_dict["cross-dataset-obs"].keys():
+        data_dict["cross-dataset-obs"][pair] = data_dict["cross-dataset-obs"][
+            pair
+        ]
+        write_dictionary_to_csv(
+            data_dict["cross-dataset-obs"][pair], f"{pair}", cfg
+        )
 
     # Create a single provenance record for the csv files
     with ProvenanceLogger(cfg) as provenance_logger:
@@ -473,7 +509,7 @@ def main(cfg):
 
     # Plot the sensitivities, uses model data only (and obs from recipe)
     logger.info("Creating Notz-style plot")
-    notz_style_plot_from_dict(data_dict['models'], titles_and_obs_dict, cfg)
+    notz_style_plot_from_dict(data_dict["models"], titles_and_obs_dict, cfg)
     logger.info("Creating Roach-style plot")
     roach_style_plot_from_dict(data_dict, titles_and_obs_dict, cfg)
 
