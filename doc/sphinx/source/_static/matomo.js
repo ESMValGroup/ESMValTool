a72e81982b623b1c0e9312fab59f442214baa39c
Bouwe Andela <b.andela@esciencecenter.nl>
2026-01-23 12:16:17 +0100

    Add Matomo

---
+++ doc/sphinx/source/_static/matomo.js
@@ -0,0 +1,19 @@
+<!-- Matomo -->
+<script>
+  var _paq = window._paq = window._paq || [];
+  /* tracker methods like "setCustomDimension" should be called before "trackPageView" */
+  _paq.push(["setCookieDomain", "*.esmvaltool.org"]);
+  _paq.push(["setDoNotTrack", true]);
+  _paq.push(["disableCookies"]);
+  _paq.push(['trackPageView']);
+  _paq.push(['enableLinkTracking']);
+  (function() {
+    var u="//matomo.research.software/";
+    _paq.push(['setTrackerUrl', u+'matomo.php']);
+    _paq.push(['setSiteId', '10']);
+    var d=document, g=d.createElement('script'), s=d.getElementsByTagName('script')[0];
+    g.async=true; g.src=u+'matomo.js'; s.parentNode.insertBefore(g,s);
+  })();
+</script>
+<noscript><p><img referrerpolicy="no-referrer-when-downgrade" src="//matomo.research.software/matomo.php?idsite=10&amp;rec=1" style="border:0;" alt="" /></p></noscript>
+<!-- End Matomo Code -->
