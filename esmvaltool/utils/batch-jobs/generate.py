4d9e7294c47b5bd4d190b97944529bf69e804658
Manuel Schlund <manuel.schlund@dlr.de>
2026-03-04 18:53:50 +0100

    Retire eyring13jgr recipe

--- esmvaltool/utils/batch-jobs/generate.py
+++ esmvaltool/utils/batch-jobs/generate.py
@@ -101,9 +101,6 @@ SPECIAL_RECIPES = {
     "recipe_eyring06jgr": {
         "partition": "#SBATCH --partition=compute \n",
     },
-    "recipe_eyring13jgr_12": {
-        "partition": "#SBATCH --partition=compute \n",
-    },
     "recipe_flato13ipcc_figures_938_941_cmip6": {
         "partition": "#SBATCH --partition=compute \n",
     },
